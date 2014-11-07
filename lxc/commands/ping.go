4abf45b73b6b776595eab9b6e19f5a8163b7d3af
SilverWyrda <gdouezangrard@gmail.com>
2014-11-07 05:53:09 +0100

    Change client to use proper flags and subcommands parsing.
    Signed-off-by: Douézan-Grard Guillaume <gdouezangrard@gmail.com>

---
+++ lxc/commands/ping.go
@@ -0,0 +1,50 @@
+package commands
+
+import (
+	"errors"
+	"log"
+
+	"github.com/codegangsta/cli"
+
+	"github.com/lxc/lxd"
+	//"github.com/lxc/lxd/lxc/modules/settings"
+
+)
+
+var (
+	ErrPingArgs = errors.New("ping: too many subcommand arguments")
+)
+
+var Ping = cli.Command{
+	Name:  "ping",
+	Usage: "Pings the lxd instance",
+	Description: "Check if the lxd instance is up and working.",
+	Action: runPing,
+	Flags:  []cli.Flag{},
+}
+
+func runPing(ctx *cli.Context) {
+	args := ctx.Args()
+
+	if len(args) > 1 {
+		log.Fatal(ErrPingArgs)
+	}
+
+	config, err := lxd.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var remote string
+	if len(args) == 1 {
+		remote = args[0]
+	} else {
+		remote = config.DefaultRemote
+	}
+
+	// NewClient will ping the server to test the connection before returning.
+	_, _, err = lxd.NewClient(config, remote)
+	log.Fatal(err)
+}
+
+/* vim: set noet ts=4 sw=4 sts=4: */
