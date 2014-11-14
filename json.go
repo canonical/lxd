7b8ff4849196330899e6fbd94dc878b7c9145538
Tycho Andersen <tycho.andersen@canonical.com>
2014-11-13 19:17:31 -0600

    Move toward the actual REST api w/ json
    This patch moves us more toward the rest API in the spec. ping and container
    creation are now restificated (with the small caveat that create is currently a
    sync response, I will implement async ASAP).
    
    I've defined several helpers (/lxd/response.go) for generating the API-style
    response json objects. I've also defined (/client.go) a Response that
    represents a response from the API.
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

---
+++ json.go
@@ -0,0 +1,37 @@
+package lxd
+
+import (
+	"fmt"
+)
+
+type Jmap map[string]interface{}
+
+func (m Jmap) GetString(key string) (string, error) {
+	if val, ok := m[key]; !ok {
+		return "", fmt.Errorf("Response was missing `%s`", key)
+	} else if val, ok := val.(string); !ok {
+		return "", fmt.Errorf("`%s` was not a string", key)
+	} else {
+		return val, nil
+	}
+}
+
+func (m Jmap) GetMap(key string) (Jmap, error) {
+	if val, ok := m[key]; !ok {
+		return nil, fmt.Errorf("Response was missing `%s`", key)
+	} else if val, ok := val.(map[string]interface{}); !ok {
+		return nil, fmt.Errorf("`%s` was not a map, got %T", key, m[key])
+	} else {
+		return val, nil
+	}
+}
+
+func (m Jmap) GetInt(key string) (int, error) {
+	if val, ok := m[key]; !ok {
+		return -1, fmt.Errorf("Response was missing `%s`", key)
+	} else if val, ok := val.(float64); !ok {
+		return -1, fmt.Errorf("`%s` was not an int", key)
+	} else {
+		return int(val), nil
+	}
+}
