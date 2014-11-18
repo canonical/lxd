250dbb7580047933fcae7dc7b73aed1674b5efd3
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-11-18 13:10:30 -0600

    move 'set' from remote to config where it belongs
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxc/remote.go
+++ lxc/remote.go
@@ -149,41 +149,6 @@ func (c *remoteCmd) run(config *lxd.Config, args []string) error {
 		}
 		fmt.Println(config.DefaultRemote)
 		return nil
-	case "set":
-		if len(args) != 4 && len(args) != 3 {
-			return errArgs
-		}
-
-		action := args[1]
-		if len(args) == 4 {
-			action = args[2]
-		}
-		if action == "password" {
-			server := ""
-			password := args[2]
-			if len(args) == 4 {
-				servername := fmt.Sprintf("%s:", args[1])
-				r, ok := config.Remotes[servername]
-				if !ok {
-					return fmt.Errorf("remote .%s. doesn't exist", servername)
-				}
-				server = r.Addr
-				fmt.Printf("using servername .%s.", servername)
-				password = args[3]
-			}
-
-			c, _, err := lxd.NewClient(config, server)
-			if err != nil {
-				return fmt.Errorf("failed to create client object: %q", err)
-				return err
-			}
-
-			_, err = c.SetRemotePwd(password)
-			return err
-		}
-
-		return fmt.Errorf("Only 'password' can be set currently")
-
 	}
 
 	return lxd.SaveConfig(*configPath, config)
