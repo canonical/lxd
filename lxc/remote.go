44e5317c3f787317e7974d3fe6994d95f0d3d3e7
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-11-19 14:50:27 -0600

    have client store and verify server certificates
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxc/remote.go
+++ lxc/remote.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"code.google.com/p/go.crypto/ssh/terminal"
 	"fmt"
+	"os"
 	"github.com/lxc/lxd"
 	"os"
 )
@@ -70,6 +71,17 @@ func addServer(config *lxd.Config, server string) error {
 	return nil
 }
 
+func removeCertificate(remote string) {
+	homedir := os.Getenv("HOME")
+	if homedir == "" {
+		return
+	}
+	certf := fmt.Sprintf("%s/.config/lxd/servercerts/%s", homedir, remote)
+	lxd.Debugf("Trying to remove %s\n", certf)
+
+	os.Remove(certf)
+}
+
 func (c *remoteCmd) run(config *lxd.Config, args []string) error {
 	if len(args) < 1 {
 		return errArgs
@@ -110,9 +122,10 @@ func (c *remoteCmd) run(config *lxd.Config, args []string) error {
 			config.DefaultRemote = ""
 		}
 
-		// TODO - remove stored server certificate
-
 		delete(config.Remotes, args[1])
+
+		removeCertificate(args[1])
+
 	case "list":
 		for name, rc := range config.Remotes {
 			fmt.Println(fmt.Sprintf("%s <%s>", name, rc.Addr))
