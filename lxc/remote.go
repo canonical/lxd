f7d6d8ea20f4e227192a34fe153ab023fd3526bd
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-11-19 16:50:32 -0600

    fix pathname to remove when removing server cert
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxc/remote.go
+++ lxc/remote.go
@@ -72,7 +72,7 @@ func removeCertificate(remote string) {
 	if homedir == "" {
 		return
 	}
-	certf := fmt.Sprintf("%s/.config/lxd/servercerts/%s", homedir, remote)
+	certf := fmt.Sprintf("%s/.config/lxd/servercerts/%s.cert", homedir, remote)
 	lxd.Debugf("Trying to remove %s\n", certf)
 
 	os.Remove(certf)
