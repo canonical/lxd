c4927cf0c674ee47050b6c3ea0e5aa4580aa45fd
Stéphane Graber <stgraber@ubuntu.com>
2014-11-19 22:54:09 -0600

    travis: Print what commit isn't signed-off
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/signoff.sh
+++ test/signoff.sh
@@ -8,6 +8,7 @@ test_commits_signed_off() {
   git remote add lxc https://github.com/lxc/lxd
   git fetch --depth=1 lxc master
   for i in $(git cherry lxc/master | grep '^+' | cut -d' ' -f2); do
-    git show "$i" | grep 'Signed-off-by'
+    git show "$i" | grep -q 'Signed-off-by' || \
+        ( echo "Commit without sign-off:" ; git show "$1" ; false )
   done
 }
