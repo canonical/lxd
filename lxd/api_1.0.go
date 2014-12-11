5c2839d8c2f82118c1dd556ea27dc1cd0ace7c2f
Tycho Andersen <tycho.andersen@canonical.com>
2014-12-11 16:19:04 -0600

    Set the password via the spec's rest API
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"syscall"
 
 	"github.com/lxc/lxd"
+	"golang.org/x/crypto/scrypt"
 	"gopkg.in/lxc/go-lxc.v2"
 )
 
@@ -86,4 +92,76 @@ func api10Get(d *Daemon, w http.ResponseWriter, r *http.Request) {
 	SyncResponse(true, body, w)
 }
 
-var api10Cmd = Command{"", true, api10Get, nil, nil, nil}
+type apiPut struct {
+	Config []lxd.Jmap `json:"config"`
+}
+
+const (
+	PW_SALT_BYTES = 32
+	PW_HASH_BYTES = 64
+)
+
+func api10Put(d *Daemon, w http.ResponseWriter, r *http.Request) {
+	req := apiPut{}
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		InternalError(w, err)
+		return
+	}
+
+	if err := json.Unmarshal(buf, &req); err != nil {
+		BadRequest(w, err)
+		return
+	}
+
+	for _, elt := range req.Config {
+		key, err := elt.GetString("key")
+		if err != nil {
+			continue
+		}
+		if key == "trust-password" {
+			password, err := elt.GetString("value")
+			if err != nil {
+				continue
+			}
+
+			lxd.Debugf("setting new password")
+			salt := make([]byte, PW_SALT_BYTES)
+			_, err = io.ReadFull(rand.Reader, salt)
+			if err != nil {
+				InternalError(w, err)
+				return
+			}
+
+			hash, err := scrypt.Key([]byte(password), salt, 1<<14, 8, 1, PW_HASH_BYTES)
+			if err != nil {
+				InternalError(w, err)
+				return
+			}
+
+			passfname := lxd.VarPath("adminpwd")
+			passOut, err := os.OpenFile(passfname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+			defer passOut.Close()
+			if err != nil {
+				InternalError(w, err)
+				return
+			}
+
+			_, err = passOut.Write(salt)
+			if err != nil {
+				InternalError(w, err)
+				return
+			}
+
+			_, err = passOut.Write(hash)
+			if err != nil {
+				InternalError(w, err)
+				return
+			}
+		}
+	}
+
+	EmptySyncResponse(w)
+}
+
+var api10Cmd = Command{"", true, api10Get, api10Put, nil, nil}
