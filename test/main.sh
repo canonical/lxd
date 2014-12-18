17b7dd3d39ac8bd862813330a3fcf1b0c3e8c4d3
Tycho Andersen <tycho.andersen@canonical.com>
2014-12-18 14:03:48 -0600

    Add some tests for basic usage and snapshotting
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 export PATH=../lxc:../lxd:$PATH
-export LXD_DIR=$(mktemp -d)
+
+# /tmp isn't moutned exec on most systems, so we can't actually start
+# contianers that are created there.
+export LXD_DIR=$(mktemp -d -p $(pwd))
 RESULT=failure
 lxd_pid=0
 
@@ -18,10 +21,22 @@ trap cleanup EXIT HUP INT TERM
 
 . ./remote.sh
 . ./signoff.sh
+. ./basic.sh
+. ./snapshots.sh
 
 echo "Spawning lxd"
 lxd --tcp 127.0.0.1:8443 &
 lxd_pid=$!
+
+BASEURL=https://127.0.0.1:8443
+my_curl() {
+  curl -k -s --cert ~/.config/lxc/client.crt --key ~/.config/lxc/client.key $@
+}
+
+wait_for() {
+  op=$($@ | jq -r .operation)
+  my_curl -X POST $BASEURL$op/wait
+}
 
 echo "Confirming lxd is responsive"
 alive=0
@@ -37,5 +52,18 @@ test_remote
 
 echo "TEST: commit sign-off"
 test_commits_signed_off
+
+# Only run the tests below if we're not in travis, since travis itself is using
+# openvz containers and the full test suite won't work.
+if [ -n "$TRAVIS_PULL_REQUEST" ]; then
+  RESULT=success
+  exit
+fi
+
+echo "TEST: basic usage"
+test_basic_usage
+
+echo "TEST: snapshots"
+test_snapshots
 
 RESULT=success
