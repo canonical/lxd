564ebea0b4bfa3adfb449aedaf55468c5abeb4fb
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-02-02 06:40:40 +0100

    implement uid shifting
    The Idmap type is used by a new program fuidshift, which can shift
    the userids on disk from host into container.  The same class can
    later be used by the lxd container creation code.
    
    The program must be called by root to shift uids, but has a test
    mode which can be called by non-root users to show what uid/gid
    conversions would be done.
    
    add a fuidshift test
    
    Changelog v2:
      remove dead code
      point out in warning msg that in case of error we are continuing
      don't skip directories
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

---
+++ test/fuidshift.sh
@@ -0,0 +1,44 @@
+test_common_fuidshift() {
+	# test some bad arguments
+	fail=0
+	fuidshift > /dev/null 2>&1 && fail=1
+	fuidshift -t > /dev/null 2>&1 && fail=1
+	fuidshift /tmp -t b:0 > /dev/null 2>&1 && fail=1
+	fuidshift /tmp -t x:0:0:0 > /dev/null 2>&1 && fail=1
+	[ $fail -ne 1 ]
+}
+
+test_nonroot_fuidshift() {
+	test_common_fuidshift
+
+	u=$(id -u)
+	g=$(id -g)
+	u1=$((u+1))
+	g1=$((g+1))
+
+	touch ${LXD_FUIDMAP_DIR}/x1
+	fuidshift ${LXD_FUIDMAP_DIR}/x1 -t u:$u:100000:1 g:$g:100000:1 | tee /dev/stderr | grep "to 100000 100000" > /dev/null || fail=1
+	if [ $fail -eq 1 ]; then
+		echo "failed to shift own uid to container root"
+		false
+	fi
+	fuidshift ${LXD_FUIDMAP_DIR}/x1 -t u:$u1:10000:1 g:$g1:100000:1 | tee /dev/stderr | grep "to -1 -1" > /dev/null || fail=1
+	if [ $fail -eq 1 ]; then
+		echo "wrongly shifted invalid uid to container root"
+		false
+	fi
+}
+
+test_root_fuidshift() {
+	test_nonroot_fuidshift
+
+	# Todo - test ranges
+}
+
+test_fuidshift() {
+	if [ $(id -u) -ne 0 ]; then
+		test_nonroot_fuidshift
+	else
+		test_root_fuidshift
+	fi
+}
