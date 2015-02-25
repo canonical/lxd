76d86ea2998a6d226abd28fa4b784d708cb8083d
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-02-25 13:51:37 -0600

    Cleanup snapshots
    containerSnapshotsGet:
    	First, the return urls should include the original container
    	name.
    	Second, the sql container %s and %d instead of ?.
    
    	implement ListSnapshots in client
    
    	implement 'lxc list <container>'
    
    	implement snapshot delete
    
    	fix rootfs on snapshot creation
    
    	update snapshot testcase
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- test/snapshots.sh
+++ test/snapshots.sh
@@ -1,10 +1,17 @@
 test_snapshots() {
-  shasum=`sha256sum ubuntu*.xz | awk '{ print $1 }'`
+  lxc init ubuntu foo
 
-  lxc init $shasum foo
+  lxc snapshot foo
+  [ -d "$LXD_DIR/lxc/foo/snapshots/snap0" ]
+
+  lxc snapshot foo
+  [ -d "$LXD_DIR/lxc/foo/snapshots/snap1" ]
 
   lxc snapshot foo tester
   [ -d "$LXD_DIR/lxc/foo/snapshots/tester" ]
+
+  lxc delete foo/snap0
+  [ ! -d "$LXD_DIR/lxc/foo/snapshots/snap0" ]
 
   # no CLI for this, so we use the API directly
   wait_for my_curl -X POST $BASEURL/1.0/containers/foo/snapshots/tester -d "{\"name\":\"tester2\"}"
@@ -13,4 +20,7 @@ test_snapshots() {
   # no CLI for this, so we use the API directly
   wait_for my_curl -X DELETE $BASEURL/1.0/containers/foo/snapshots/tester2
   [ ! -d "$LXD_DIR/lxc/foo/snapshots/tester2" ]
+
+  lxc delete foo
+  [ ! -d "$LXD_DIR/lxc/foo" ]
 }
