c165847ac9fd53a08d7f8d535b2b7e22033740de
Stéphane Graber <stgraber@ubuntu.com>
2015-02-26 12:25:27 -0500

    tests: A few fixes
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/static_analysis.sh
+++ test/static_analysis.sh
@@ -6,13 +6,16 @@ static_analysis() {
   toplevel="$(git rev-parse --show-toplevel)"
 
   # go tools
+  git add -u :/
   (cd $toplevel && go fmt ./... && go vet ./...)
   git diff --exit-code
 
   # make sure the .pot is updated
+  cp $toplevel/po/lxd.pot $toplevel/po/lxd.pot.bak
   hash1=$(safe_pot_hash)
   (cd $toplevel && make i18n)
   hash2=$(safe_pot_hash)
+  mv $toplevel/po/lxd.pot.bak $toplevel/po/lxd.pot
   if [ "$hash1" != "$hash2" ]; then
     echo "please update the .pot file in your commit (make i18n)" && false
   fi
