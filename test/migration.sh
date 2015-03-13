6d2571736d81a00e8cfd372b7690f7c86afd2b3a
Tycho Andersen <tycho.andersen@canonical.com>
2015-03-13 14:44:28 -0600

    robustificate testing
    1. local LXD_DIR
    2. make lxd nonlive tests start/stop the container too, just to be sure
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- test/migration.sh
+++ test/migration.sh
@@ -2,22 +2,28 @@ test_migration() {
   export LXD2_DIR=$(mktemp -d -p $(pwd))
   chmod 777 "${LXD2_DIR}"
   spawn_lxd 127.0.0.1:8444 "${LXD2_DIR}"
-  lxd2_pid=$1
+  lxd2_pid=$!
 
+  (echo y; sleep 3; echo foo) | lxc remote add lxd2 127.0.0.1:8444
+
+  lxc image list
   lxc init testimage nonlive
-  lxc move nonlive lxd2:
-  [ -d "$LXD2_DIR/lxc/nonlive/rootfs" ]
+  lxc move local:nonlive lxd2:
 
   if [ -n "$TRAVIS_PULL_REQUEST" ]; then
     return
   fi
+
+  lxc start lxd2:nonlive
+  lxc list lxd2: | grep RUNNING | grep nonlive
+  lxc stop lxd2:nonlive --force
+  [ -d "$LXD2_DIR/lxc/nonlive/rootfs" ]
 
   if [ -z "$(which criu)" ]; then
       echo "==> Skipping live migration tests; no criu binary found"
       return
   fi
 
-  (echo y; sleep 3; echo foo) | lxc remote add lxd2 127.0.0.1:8444
   lxc launch testimage migratee
 
   lxc move migratee lxd2:migratee
