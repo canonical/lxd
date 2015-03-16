dea15026e9b6a2a6b8e4b1d1af5a955b76e36966
Tycho Andersen <tycho.andersen@canonical.com>
2015-03-16 13:12:53 -0600

    implement local container moves via a simple 'mv'
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- lxc/move.go
+++ lxc/move.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gosexy/gettext"
 	"github.com/lxc/lxd"
+	"gopkg.in/lxc/go-lxc.v2"
 )
 
 type moveCmd struct {
@@ -25,6 +26,34 @@ func (c *moveCmd) flags() {}
 func (c *moveCmd) run(config *lxd.Config, args []string) error {
 	if len(args) != 2 {
 		return errArgs
+	}
+
+	sourceRemote, sourceName := config.ParseRemoteAndContainer(args[0])
+	destRemote, destName := config.ParseRemoteAndContainer(args[1])
+
+	// As an optimization, if the source an destination are the same, do
+	// this via a simple rename. This only works for containers that aren't
+	// running, containers that are running should be live migrated (of
+	// course, this changing of hostname isn't supported right now, so this
+	// simply won't work).
+	if sourceRemote == destRemote {
+		source, err := lxd.NewClient(config, sourceRemote)
+		if err != nil {
+			return err
+		}
+
+		status, err := source.ContainerStatus(sourceName)
+		if err != nil {
+			return err
+		}
+
+		if status.State() != lxc.RUNNING {
+			rename, err := source.Rename(sourceName, destName)
+			if err != nil {
+				return err
+			}
+			return source.WaitForSuccess(rename.Operation)
+		}
 	}
 
 	// A move is just a copy followed by a delete.
