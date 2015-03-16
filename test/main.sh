3e8a29e8e2f2a6ec749ee6839c601ea308e10ac2
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-03-16 12:16:48 -0500

    tests: use port 18443 and 18444 rather than 8443 and 8444
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- test/main.sh
+++ test/main.sh
@@ -9,7 +9,7 @@ chmod 777 "${LXD_DIR}"
 export LXD_CONF=$(mktemp -d)
 export LXD_FUIDMAP_DIR=${LXD_DIR}/fuidmap
 mkdir -p ${LXD_FUIDMAP_DIR}
-BASEURL=https://127.0.0.1:8443
+BASEURL=https://127.0.0.1:18443
 RESULT=failure
 lxd_pid=0
 
@@ -20,7 +20,7 @@ fi
 
 echo "==> Running the LXD testsuite"
 
-BASEURL=https://127.0.0.1:8443
+BASEURL=https://127.0.0.1:18443
 my_curl() {
   curl -k -s --cert "${LXD_CONF}/client.crt" --key "${LXD_CONF}/client.key" $@
 }
@@ -94,12 +94,12 @@ spawn_lxd() {
   LXD_DIR=$2 lxc config set password foo
 }
 
-spawn_lxd 127.0.0.1:8443 $LXD_DIR
+spawn_lxd 127.0.0.1:18443 $LXD_DIR
 lxd_pid=$!
 
 export LXD2_DIR=$(mktemp -d -p $(pwd))
 chmod 777 "${LXD2_DIR}"
-spawn_lxd 127.0.0.1:8444 "${LXD2_DIR}"
+spawn_lxd 127.0.0.1:18444 "${LXD2_DIR}"
 lxd2_pid=$!
 
 # allow for running a specific set of tests
