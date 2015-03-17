e50b952d20f3acc3b91f2765b54b38e64b4ae44f
Tycho Andersen <tycho.andersen@canonical.com>
2015-03-17 10:21:28 -0600

    Add support for filters in `lxc list`
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- lxc/list.go
+++ lxc/list.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gosexy/gettext"
 	"github.com/lxc/lxd"
+	"github.com/lxc/lxd/shared"
 	"github.com/olekukonko/tablewriter"
 )
 
@@ -21,16 +22,77 @@ func (c *listCmd) usage() string {
 	return gettext.Gettext(
 		"Lists the available resources.\n" +
 			"\n" +
-			"lxc list [resource]\n" +
+			"lxc list [resource] [filters]\n" +
 			"\n" +
-			"Currently resource must be a defined remote, and list only lists\n" +
-			"the defined containers.\n")
-
+			"The filters are:\n" +
+			"* A single keyword like \"web\" which will list any container with \"web\" in its name.\n" +
+			"* A key/value pair referring to a configuration item. For those, the namespace can be abreviated to the smallest unambiguous identifier:\n" +
+			"* \"user.blah=abc\" will list all containers with the \"blah\" user property set to \"abc\"\n" +
+			"* \"u.blah=abc\" will do the same\n" +
+			"* \"security.privileged=1\" will list all privileged containers\n" +
+			"* \"s.privileged=1\" will do the same\n")
 }
 
 func (c *listCmd) flags() {}
 
-func listContainers(d *lxd.Client, cts []string, showsnaps bool) error {
+// This seems a little excessive.
+func dotPrefixMatch(short string, full string) bool {
+	fullMembs := strings.Split(full, ".")
+	shortMembs := strings.Split(short, ".")
+
+	if len(fullMembs) != len(shortMembs) {
+		return false
+	}
+
+	for i, _ := range fullMembs {
+		if !strings.HasPrefix(fullMembs[i], shortMembs[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func shouldShow(filters []string, state *shared.ContainerState) bool {
+	for _, filter := range filters {
+		if strings.Contains(filter, "=") {
+			membs := strings.SplitN(filter, "=", 2)
+
+			key := membs[0]
+			var value string
+			if len(membs) < 2 {
+				value = ""
+			} else {
+				value = membs[1]
+			}
+
+			found := false
+			for configKey, configValue := range state.Config {
+				if dotPrefixMatch(key, configKey) {
+					if value == configValue {
+						found = true
+						break
+					} else {
+						// the property was found but didn't match
+						return false
+					}
+				}
+			}
+
+			if !found {
+				return false
+			}
+		} else {
+			if !strings.Contains(state.Name, filter) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func listContainers(d *lxd.Client, cts []string, filters []string, showsnaps bool) error {
 	data := [][]string{}
 
 	for _, ct := range cts {
@@ -43,6 +105,10 @@ func listContainers(d *lxd.Client, cts []string, showsnaps bool) error {
 			return fmt.Errorf(gettext.Gettext("Container not found"))
 		} else {
 			return err
+		}
+
+		if !shouldShow(filters, c) {
+			continue
 		}
 
 		if c.Status.State == "RUNNING" {
@@ -98,17 +164,23 @@ func listContainers(d *lxd.Client, cts []string, showsnaps bool) error {
 }
 
 func (c *listCmd) run(config *lxd.Config, args []string) error {
-	if len(args) > 1 {
-		return errArgs
-	}
-
 	var remote string
 	var name string
-	if len(args) == 1 {
-		remote, name = config.ParseRemoteAndContainer(args[0])
-	} else {
+
+	filters := []string{}
+
+	if len(args) == 0 {
 		remote = config.DefaultRemote
 		name = ""
+	} else {
+		filters = args
+		if strings.Contains(args[0], ":") {
+			remote, name = config.ParseRemoteAndContainer(args[0])
+			filters = args[1:]
+		} else if !strings.Contains(args[0], "=") {
+			remote = config.DefaultRemote
+			name = ""
+		}
 	}
 
 	d, err := lxd.NewClient(config, remote)
@@ -117,16 +189,14 @@ func (c *listCmd) run(config *lxd.Config, args []string) error {
 	}
 
 	var cts []string
-	showsnapshots := false
 	if name == "" {
 		cts, err = d.ListContainers()
 		if err != nil {
 			return err
 		}
 	} else {
-		showsnapshots = true
 		cts = []string{name}
 	}
 
-	return listContainers(d, cts, showsnapshots)
+	return listContainers(d, cts, filters, len(cts) == 1)
 }
