87621d987775ebd695a138d34254f07535f314b9
John Brooker <john.edw.brooker@hp.com>
2015-03-25 15:12:08 -0700

    Vagrant - Vagrant config for lxd startup
    this tooling:
    * uses 14.04 official image
    * Install the ubuntu-lxc ppa
    * Install go
    * Build, setup LXD from source
    * Startup LXD using upstart

---
+++ scripts/vagrant/install-go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xe
+export DEBIAN_FRONTEND=noninteractive
+
+which add-apt-repository || sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:ubuntu-lxc/lxd-daily
+sudo apt-get update
+which go || sudo apt-get install -y golang
+
+[ -e $HOME/go ] || mkdir -p $HOME/go
+
+cat << 'EOF' | sudo tee /etc/profile.d/S99go.sh
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+EOF
+
