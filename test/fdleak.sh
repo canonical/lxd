1b5120490d6e7039b7811e2f79758fa8ca0232d3
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-04-29 12:18:10 +0200

    add a test for fd leaks across basic operations
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

---
+++ test/fdleak.sh
@@ -0,0 +1,19 @@
+test_fdleak() {
+    if ! lxc image alias list | grep -q ^testimage$; then
+        if [ -e "$LXD_TEST_IMAGE" ]; then
+            lxc image import $LXD_TEST_IMAGE --alias testimage
+        else
+            ../scripts/lxd-images import busybox --alias testimage
+        fi
+    fi
+    lxd1_pid=`ps -ef | grep lxd | grep -v grep | awk '/127.0.0.1:18443/ { print $2 }'`
+    echo "lxd1_pid is $lxd1_pid"
+    beforefds=`/bin/ls /proc/$lxd1_pid/fd | wc -l`
+    lxc launch testimage leaktest1
+    lxc exec leaktest1 -- ps -ef
+    lxc stop leaktest1
+    lxc delete leaktest1
+    afterfds=`/bin/ls /proc/$lxd1_pid/fd | wc -l`
+    leakedfds=$((afterfds - beforefds))
+    [ $leakedfds -eq 0 ] || { echo "$leakedfds FDS leaked"; false; }
+}
