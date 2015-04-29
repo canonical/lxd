c23ecff2b12f9dd2c21676841791598cf4651a1c
Tycho Andersen <tycho.andersen@canonical.com>
2015-04-29 07:50:12 -0600

    don't run parts of leaktest in travis
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- test/fdleak.sh
+++ test/fdleak.sh
@@ -9,9 +9,11 @@ test_fdleak() {
     lxd1_pid=`ps -ef | grep lxd | grep -v grep | awk '/127.0.0.1:18443/ { print $2 }'`
     echo "lxd1_pid is $lxd1_pid"
     beforefds=`/bin/ls /proc/$lxd1_pid/fd | wc -l`
-    lxc launch testimage leaktest1
-    lxc exec leaktest1 -- ps -ef
-    lxc stop leaktest1
+    lxc init testimage leaktest1
+    lxc info leaktest1
+    [ -n "$TRAVIS_PULL_REQUEST" ] || lxc start leaktest1
+    [ -n "$TRAVIS_PULL_REQUEST" ] || lxc exec leaktest1 -- ps -ef
+    [ -n "$TRAVIS_PULL_REQUEST" ] || lxc stop leaktest1
     lxc delete leaktest1
     afterfds=`/bin/ls /proc/$lxd1_pid/fd | wc -l`
     leakedfds=$((afterfds - beforefds))
