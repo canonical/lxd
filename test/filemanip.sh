f20c9bb29de5ca79bb3e234471eeea91492f5882
Tycho Andersen <tycho.andersen@canonical.com>
2015-06-25 17:39:41 -0600

    use setn()s in file push/pull
    This prevents a security issue with interpreting symlink paths, as well as
    avoids any such other issues by simply setns()ing into the container's
    namespace to manipulate files.
    
    Closes #789
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

---
+++ test/filemanip.sh
@@ -0,0 +1,27 @@
+test_filemanip() {
+  if [ -n "$TRAVIS_PULL_REQUEST" ]; then
+    return
+  fi
+
+  if ! lxc image alias list | grep -q "^| testimage\s*|.*$"; then
+    if [ -e "$LXD_TEST_IMAGE" ]; then
+        lxc image import $LXD_TEST_IMAGE --alias testimage
+    else
+        ../scripts/lxd-images import busybox --alias testimage
+    fi
+  fi
+
+  # Check that symlinks are interpreted relative to the container's root
+  mkdir -p /tmp/outside
+
+  lxc launch testimage filemanip
+  lxc exec filemanip -- mkdir /tmp/outside
+  lxc exec filemanip -- ln -s /tmp/outside /tmp/inside
+  lxc file push main.sh filemanip/tmp/inside/
+
+  [ ! -f /tmp/outside/main.sh ]
+  [ -f ${LXD_DIR}/lxc/filemanip/rootfs/tmp/outside/main.sh ]
+
+  rm -rf /tmp/outside
+  lxc delete filemanip
+}
