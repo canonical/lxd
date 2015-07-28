ceb3104a6cb606e9890705d30aefe6f53c64d6f5
Stéphane Graber <stgraber@ubuntu.com>
2015-07-28 18:00:58 +0200

    Fix devlxd under gccgo
    Closes #807
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/devlxd_gccgo.go
+++ lxd/devlxd_gccgo.go
@@ -1,28 +1,50 @@
 // +build gccgo
+// +build cgo
 
 package main
 
-// These are stubs since gccgo doesn't have GetsockoptUcred yet (this was added
-// in golang 1.4, so presumably when gccgo catches up we can get rid of this hack).
-
 import (
-	"net"
-	"net/http"
-
-	"gopkg.in/lxc/go-lxc.v2"
-
-	"github.com/lxc/lxd/shared"
+	"errors"
 )
 
-func setupDevLxdMount(c *lxc.Container) error {
-	return nil
-}
+/*
+#define _GNU_SOURCE
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
 
-func createAndBindDevLxd() (*net.UnixListener, error) {
-	shared.Logf("WARNING: devlxd not supported under gccgo")
-	return nil, nil
-}
+void getucred(int sock, uint *uid, uint *gid, int *pid) {
+	struct ucred peercred;
+	socklen_t len;
 
-func devLxdServer(d *Daemon) http.Server {
-	return http.Server{}
+	len = sizeof(struct ucred);
+
+	if (getsockopt(sock, SOL_SOCKET, SO_PEERCRED, &peercred, &len) != 0 || len != sizeof(peercred)) {
+		fprintf(stderr, "getsockopt failed: %s\n", strerror(errno));
+		return;
+	}
+
+	*uid = peercred.uid;
+	*gid = peercred.gid;
+	*pid = peercred.pid;
+
+	return;
+}
+*/
+import "C"
+
+func getUcred(fd int) (uint32, uint32, int32, error) {
+	uid := C.uint(0)
+	gid := C.uint(0)
+	pid := C.int(-1)
+
+	C.getucred(C.int(fd), &uid, &gid, &pid)
+
+	if uid == 0 || gid == 0 || pid == -1 {
+		return 0, 0, -1, errors.New("Failed to get the ucred")
+	}
+
+	return uint32(uid), uint32(gid), int32(pid), nil
 }
