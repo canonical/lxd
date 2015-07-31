10ba9ff63e4ef04c49a412500690409245107bd5
René Jochum <rene@jochums.at>
2015-07-31 13:55:43 +0200

    Improve speedtest scripts a bit.
    Signed-off-by: René Jochum <rene@jochums.at>

--- test/speedtest_create.sh
+++ test/speedtest_create.sh
@@ -11,23 +11,34 @@ if [ "x${count}" == "x" ]; then
   exit 1
 fi
 
-if [ "x${3}" != "xnotime" ]; then
+if [ "x${2}" != "xnotime" ]; then
   time ${0} ${count} notime
   exit 0
 fi
 
 ${MYDIR}/../scripts/lxd-images import busybox --alias busybox
-lxc init busybox "${CNAME}"
+
+PIDS=""
 for c in $(seq 1 $count); do
-  lxc copy "${CNAME}" "${CNAME}${c}"
+  lxc init busybox "${CNAME}${c}" 2>&1 &
+  PIDS="$PIDS $!"
+done
+
+for pid in $PIDS; do
+  wait $pid
 done
 
 echo -e "\nlxc list: All shutdown"
 time lxc list 1>/dev/null
 
-lxc start "${CNAME}"
+PIDS=""
 for c in $(seq 1 $count); do
-  lxc start "${CNAME}${c}"
+  lxc start "${CNAME}${c}" 2>&1 &
+  PIDS="$PIDS $!"
+done
+
+for pid in $PIDS; do
+  wait $pid
 done
 
 echo -e "\nlxc list: All started"
