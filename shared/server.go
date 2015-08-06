5c7bb8bf1d1c2f0098c7f53af64f81ff51badadf
Stéphane Graber <stgraber@ubuntu.com>
2015-08-06 10:31:47 -0400

    Support copies from local (unix socket) to remote (https)
    Closes #598
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/server.go
+++ shared/server.go
@@ -1,14 +1,15 @@
 package shared
 
 type ServerStateEnvironment struct {
-	Architectures      []int  `json:"architectures"`
-	BackingFS          string `json:"backing_fs"`
-	Driver             string `json:"driver"`
-	DriverVersion      string `json:"driver_version"`
-	Kernel             string `json:"kernel"`
-	KernelArchitecture string `json:"kernel_architecture"`
-	KernelVersion      string `json:"kernel_version"`
-	Version            string `json:"version"`
+	Addresses          []string `json:"addresses"`
+	Architectures      []int    `json:"architectures"`
+	BackingFS          string   `json:"backing_fs"`
+	Driver             string   `json:"driver"`
+	DriverVersion      string   `json:"driver_version"`
+	Kernel             string   `json:"kernel"`
+	KernelArchitecture string   `json:"kernel_architecture"`
+	KernelVersion      string   `json:"kernel_version"`
+	Version            string   `json:"version"`
 }
 
 type ServerState struct {
