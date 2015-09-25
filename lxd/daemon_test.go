a5a1b881c92cc8e8c08f2536a913c0811ff8ad21
Stéphane Graber <stgraber@ubuntu.com>
2015-09-25 15:23:55 -0400

    tests: Fix go tests to respect LXD_DIR
    To do so, use the test daemon everywhere.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/daemon_test.go
+++ lxd/daemon_test.go
@@ -1,69 +1,26 @@
 package main
 
-import (
-	"sync"
-	"testing"
-)
+func (suite *lxdTestSuite) Test_config_value_set_empty_removes_val() {
+	var err error
+	d := suite.d
 
-func mockStartDaemon() (*Daemon, error) {
-	d := &Daemon{
-		IsMock:                true,
-		imagesDownloading:     map[string]chan bool{},
-		imagesDownloadingLock: sync.RWMutex{},
-	}
-
-	if err := d.Init(); err != nil {
-		return nil, err
-	}
-
-	// Call this after Init so we have a log object.
-	storageConfig := make(map[string]interface{})
-	d.Storage = &storageLogWrapper{w: &storageMock{d: d}}
-	if _, err := d.Storage.Init(storageConfig); err != nil {
-		return nil, err
-	}
-
-	return d, nil
-}
-
-func Test_config_value_set_empty_removes_val(t *testing.T) {
-	d, err := mockStartDaemon()
-	if err != nil {
-		t.Errorf("daemon, err='%s'", err)
-	}
-	defer d.Stop()
-
-	if err = d.ConfigValueSet("storage.lvm_vg_name", "foo"); err != nil {
-		t.Error("couldn't set value", err)
-	}
+	err = d.ConfigValueSet("storage.lvm_vg_name", "foo")
+	suite.Req.Nil(err)
 
 	val, err := d.ConfigValueGet("storage.lvm_vg_name")
-	if err != nil {
-		t.Error("Error getting val")
-	}
-	if val != "foo" {
-		t.Error("Expected foo, got ", val)
-	}
+	suite.Req.Nil(err)
+	suite.Req.Equal(val, "foo")
 
 	err = d.ConfigValueSet("storage.lvm_vg_name", "")
-	if err != nil {
-		t.Error("error setting to ''")
-	}
+	suite.Req.Nil(err)
 
 	val, err = d.ConfigValueGet("storage.lvm_vg_name")
-	if err != nil {
-		t.Error("Error getting val")
-	}
-	if val != "" {
-		t.Error("Expected '', got ", val)
-	}
+	suite.Req.Nil(err)
+	suite.Req.Equal(val, "")
 
 	valMap, err := d.ConfigValuesGet()
-	if err != nil {
-		t.Error("Error getting val")
-	}
-	if key, present := valMap["storage.lvm_vg_name"]; present {
-		t.Errorf("un-set key should not be in values map, it is '%v'", key)
-	}
+	suite.Req.Nil(err)
 
+	_, present := valMap["storage.lvm_vg_name"]
+	suite.Req.False(present)
 }
