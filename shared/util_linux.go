82183467cbd53608a6b8bcbbfda616631e0fe0ad
anatoly techtonik <techtonik@gmail.com>
2015-10-18 20:21:54 +0300

    Move GetFileStat into shared/util_linux.go
    This function is not available on Windows and unused by lxc client
    
    Signed-off-by: anatoly techtonik <techtonik@gmail.com>

--- shared/util_linux.go
+++ shared/util_linux.go
@@ -13,6 +13,27 @@ import (
 	"github.com/chai2010/gettext-go/gettext"
 )
 
+func GetFileStat(p string) (uid int, gid int, major int, minor int,
+	inode uint64, nlink int, err error) {
+	var stat syscall.Stat_t
+	err = syscall.Lstat(p, &stat)
+	if err != nil {
+		return
+	}
+	uid = int(stat.Uid)
+	gid = int(stat.Gid)
+	inode = uint64(stat.Ino)
+	nlink = int(stat.Nlink)
+	major = -1
+	minor = -1
+	if stat.Mode&syscall.S_IFBLK != 0 || stat.Mode&syscall.S_IFCHR != 0 {
+		major = int(stat.Rdev / 256)
+		minor = int(stat.Rdev % 256)
+	}
+
+	return
+}
+
 // #cgo LDFLAGS: -lutil -lpthread
 /*
 #define _GNU_SOURCE
