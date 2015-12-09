1ae2850e4cffdfca477c79528300e5e11a7154b5
Jeroen Simonetti <jeroen@simonetti.nl>
2015-12-09 20:52:39 +0100

    Add anonymous functions
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

---
+++ shared/log.go
@@ -0,0 +1,43 @@
+package shared
+
+import (
+	"runtime"
+)
+
+var Logfunc func(string, string, ...interface{})
+var Debugf func(string, ...interface{})
+var Logf func(string, ...interface{})
+
+var Log logger
+
+type logger struct{}
+
+func (l *logger) Debug(msg string, ctx ...interface{}) {
+	Logfunc("debug", msg, ctx)
+}
+func (l *logger) Info(msg string, ctx ...interface{}) {
+	Logfunc("info", msg, ctx)
+}
+func (l *logger) Warn(msg string, ctx ...interface{}) {
+	Logfunc("warn", msg, ctx)
+}
+func (l *logger) Error(msg string, ctx ...interface{}) {
+	Logfunc("error", msg, ctx)
+}
+func (l *logger) Crit(msg string, ctx ...interface{}) {
+	Logfunc("crit", msg, ctx)
+}
+
+type Ctx map[string]interface{}
+
+func init() {
+	Logfunc = func(string, string, ...interface{}) {}
+	Debugf = func(string, ...interface{}) {}
+	Logf = func(string, ...interface{}) {}
+}
+
+func PrintStack() {
+	buf := make([]byte, 1<<16)
+	runtime.Stack(buf, true)
+	Debugf("%s", buf)
+}
