3c8fda0b0c6baf7882ecf01c315565ab826e10d1
Stéphane Graber <stgraber@ubuntu.com>
2015-12-15 17:59:18 -0500

    tests: Add btrfs backend
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ test/backends/btrfs.sh
@@ -0,0 +1,32 @@
+btrfs_setup() {
+  local LXD_DIR
+  LXD_DIR=$1
+
+  echo "==> Setting up btrfs backend in ${LXD_DIR}"
+
+  if ! which btrfs >/dev/null 2>&1; then
+    echo "Couldn't find the btrfs binary"; false
+  fi
+
+  truncate -s 100G "${TEST_DIR}/$(basename "${LXD_DIR}").btrfs"
+  mkfs.btrfs "${TEST_DIR}/$(basename "${LXD_DIR}").btrfs"
+
+  mount -o loop "${TEST_DIR}/$(basename "${LXD_DIR}").btrfs" "${LXD_DIR}"
+}
+
+btrfs_configure() {
+  local LXD_DIR
+  LXD_DIR=$1
+
+  echo "==> Configuring btrfs backend in ${LXD_DIR}"
+}
+
+btrfs_teardown() {
+  local LXD_DIR
+  LXD_DIR=$1
+
+  echo "==> Tearing down btrfs backend in ${LXD_DIR}"
+
+  umount -l "${LXD_DIR}"
+  rm -f "${TEST_DIR}/$(basename "${LXD_DIR}").btrfs"
+}
