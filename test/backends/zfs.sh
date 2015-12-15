b2162d4d691316e99fe2653d41ce7fd81b464976
Stéphane Graber <stgraber@ubuntu.com>
2015-12-15 14:57:54 -0500

    tests: Wait up to 5s for zpool destroy to succeed
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/backends/zfs.sh
+++ test/backends/zfs.sh
@@ -2,8 +2,10 @@ zfs_setup() {
   local LXD_DIR
   LXD_DIR=$1
 
+  echo "==> Setting up ZFS backend"
+
   if ! which zfs >/dev/null 2>&1; then
-    echo "couldn't find zfs binary"; false
+    echo "Couldn't find zfs binary"; false
   fi
 
   truncate -s 100G "${LXD_DIR}/zfspool"
@@ -16,6 +18,8 @@ zfs_configure() {
   local LXD_DIR
   LXD_DIR=$1
 
+  echo "==> Configuring ZFS backend"
+
   lxc config set storage.zfs_pool_name lxdtest-$(basename "${LXD_DIR}")
 }
 
@@ -23,5 +27,22 @@ zfs_teardown() {
   local LXD_DIR
   LXD_DIR=$1
 
-  zpool destroy -f lxdtest-$(basename "${LXD_DIR}")
+  echo "==> Tearing down ZFS backend"
+
+  # Wait up to 5s for zpool destroy to succeed
+  SUCCESS=0
+  for i in $(seq 10); do
+    zpool destroy -f lxdtest-$(basename "${LXD_DIR}") >/dev/null 2>&1 || true
+    if ! zpool list -o name -H | grep -q ^lxdtest-$(basename "${LXD_DIR}"); then
+      SUCCESS=1
+      break
+    fi
+
+    sleep 0.5
+  done
+
+  if [ "${SUCCESS}" = "0" ]; then
+    echo "Failed to destroy the zpool"
+    false
+  fi
 }
