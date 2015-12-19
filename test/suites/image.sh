5260a2aefca9c0eb65ffa883172be31fb182298f
Stéphane Graber <stgraber@ubuntu.com>
2015-12-18 21:41:20 -0500

    tests: Speed up testsuite
    Use --force-local whenever possible, this bypasses the SSL certificate
    loading which is terribly slow on some Go versions.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/image.sh
+++ test/suites/image.sh
@@ -3,29 +3,29 @@
 test_image_expiry() {
   ensure_import_testimage
 
-  if ! lxc remote list | grep -q l1; then
-    lxc remote add l1 "${LXD_ADDR}" --accept-certificate --password foo
+  if ! lxc_remote remote list | grep -q l1; then
+    lxc_remote remote add l1 "${LXD_ADDR}" --accept-certificate --password foo
   fi
-  if ! lxc remote list | grep -q l2; then
-    lxc remote add l2 "${LXD2_ADDR}" --accept-certificate --password foo
+  if ! lxc_remote remote list | grep -q l2; then
+    lxc_remote remote add l2 "${LXD2_ADDR}" --accept-certificate --password foo
   fi
-  lxc init l1:testimage l2:c1
-  fp=$(lxc image info testimage | awk -F: '/^Fingerprint/ { print $2 }' | awk '{ print $1 }')
+  lxc_remote init l1:testimage l2:c1
+  fp=$(lxc_remote image info testimage | awk -F: '/^Fingerprint/ { print $2 }' | awk '{ print $1 }')
   [ ! -z "${fp}" ]
   fpbrief=$(echo "${fp}" | cut -c 1-10)
 
-  lxc image list l2: | grep -q "${fpbrief}"
+  lxc_remote image list l2: | grep -q "${fpbrief}"
 
-  lxc remote set-default l2
-  lxc config set images.remote_cache_expiry 0
-  lxc remote set-default local
+  lxc_remote remote set-default l2
+  lxc_remote config set images.remote_cache_expiry 0
+  lxc_remote remote set-default local
 
-  ! lxc image list l2: | grep -q "${fpbrief}"
+  ! lxc_remote image list l2: | grep -q "${fpbrief}"
 
-  lxc delete l2:c1
+  lxc_remote delete l2:c1
 
   # rest the default expiry
-  lxc remote set-default l2
-  lxc config set images.remote_cache_expiry 10
-  lxc remote set-default local
+  lxc_remote remote set-default l2
+  lxc_remote config set images.remote_cache_expiry 10
+  lxc_remote remote set-default local
 }
