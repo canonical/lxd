245a4cfffdf5ef5b2b822b6b0242102020a51f28
Joel Seguillon <joel.seguillon@gmail.com>
2016-02-02 22:29:52 +0100

    Adding ^ and $ to filter value before testing as regexpo
    Signed-off-by: Joel Seguillon <joel.seguillon@gmail.com>

--- lxc/list.go
+++ lxc/list.go
@@ -6,7 +6,7 @@ import (
 	"sort"
 	"strings"
 	"regexp"
-	
+
 	"github.com/olekukonko/tablewriter"
 
 	"github.com/lxc/lxd"
@@ -114,7 +114,13 @@ func shouldShow(filters []string, state *shared.ContainerState) bool {
 			found := false
 			for configKey, configValue := range state.Config {
 				if dotPrefixMatch(key, configKey) {
-					r, err := regexp.Compile(value)
+					//try to test filter value as a regexp
+					regexpValue := value
+					if !(strings.Contains(value, "^") || strings.Contains(value, "$")) {
+							regexpValue = "^" + regexpValue + "$"
+					}
+					r, err := regexp.Compile(regexpValue)
+					//if not regexp compatible use original value
 					if err != nil {
 						if value == configValue {
 							found = true
