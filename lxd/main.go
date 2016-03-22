e19174ce9b260f4cf0f5fe12dc3696cb8b92efd4
Serge Hallyn <serge.hallyn@ubuntu.com>
2016-03-22 00:58:12 -0700

    lxd init: recommend port 8843
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxd/main.go
+++ lxd/main.go
@@ -722,7 +722,7 @@ func setupLXD() error {
 
 		if askBool("Would you like LXD to be available over the network (yes/no)? ") {
 			networkAddress = askString("Address to bind LXD to (not including port): ")
-			networkPort = askInt("Port to bind LXD to: ", 1, 65535)
+			networkPort = askInt("Port to bind LXD to (8443 recommended): ", 1, 65535)
 			trustPassword = askPassword("Trust password for new clients: ")
 		}
 	}
