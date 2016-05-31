0ed0a45fac6af821ca5c47de0e152a6c0c51c149
Tycho Andersen <tycho.andersen@canonical.com>
2016-05-31 09:55:40 -0600

    GET of a nonexistent file 404s
    Closes #2059
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- test/suites/filemanip.sh
+++ test/suites/filemanip.sh
@@ -2,6 +2,7 @@
 
 test_filemanip() {
   ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
 
   lxc launch testimage filemanip
   lxc exec filemanip -- ln -s /tmp/ /tmp/outside
@@ -9,6 +10,10 @@ test_filemanip() {
 
   [ ! -f /tmp/main.sh ]
   lxc exec filemanip -- ls /tmp/main.sh
+
+  # missing files should return 404
+  err=$(my_curl -o /dev/null -w "%{http_code}" -X GET "https://${LXD_ADDR}/1.0/containers/filemanip/files?path=/tmp/foo")
+  [ "${err}" -eq "404" ]
 
   lxc delete filemanip -f
 }
