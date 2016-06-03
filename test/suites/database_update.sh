cdc0c946ea81d4f25f30f0d3177d9b031f19d79d
Stéphane Graber <stgraber@ubuntu.com>
2016-06-03 14:54:32 -0400

    patches: Fix DB test, we now have 17 tables
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/database_update.sh
+++ test/suites/database_update.sh
@@ -11,7 +11,7 @@ test_database_update(){
   spawn_lxd "${LXD_MIGRATE_DIR}"
 
   # Assert there are enough tables.
-  expected_tables=16
+  expected_tables=17
   tables=$(sqlite3 "${MIGRATE_DB}" ".dump" | grep -c "CREATE TABLE")
   [ "${tables}" -eq "${expected_tables}" ] || { echo "FAIL: Wrong number of tables after database migration. Found: ${tables}, expected ${expected_tables}"; false; }
 
