22e53578e375dc3d54bc9b0ff54cc22ba3d1173b
Rene Fragoso <ctrlrsf@gmail.com>
2016-06-05 01:28:25 -0400

    Add container_last_used_at extension to api_extensions list
    Signed-off-by: Rene Fragoso <ctrlrsf@gmail.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -55,6 +55,7 @@ func api10Get(d *Daemon, r *http.Request) Response {
 		 *  - New HTTPs authentication mechanisms or protocols
 		 */
 		"api_extensions": []string{
+			"container_last_used_at",
 			"storage_zfs_remove_snapshots",
 			"container_host_shutdown_timeout",
 			"container_syscall_filtering",
