d49e384fc18d71964602b98626a5e555eb878c51
Stéphane Graber <stgraber@ubuntu.com>
2016-06-22 11:57:45 -0400

    Check errors from ioutil.ReadAll
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/images.go
+++ lxd/images.go
@@ -1069,7 +1069,11 @@ func imagePatch(d *Daemon, r *http.Request) Response {
 		return PreconditionFailed(err)
 	}
 
-	body, _ := ioutil.ReadAll(r.Body)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return InternalError(err)
+	}
+
 	rdr1 := ioutil.NopCloser(bytes.NewBuffer(body))
 	rdr2 := ioutil.NopCloser(bytes.NewBuffer(body))
 
