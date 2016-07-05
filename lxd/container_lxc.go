472e650df709f91c1d24182ba36f425a21daa197
Stéphane Graber <stgraber@ubuntu.com>
2016-07-05 11:11:26 +0100

    Allow /dev/net/tun by default
    It's perfectly safe inside both privileged and unprivileged containers
    so there's no reason to have people jump through hoops for that.
    
    Related to #2172
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/container_lxc.go
+++ lxd/container_lxc.go
@@ -326,6 +326,7 @@ func (c *containerLXC) initLXC() error {
 
 	bindMounts := []string{
 		"/dev/fuse",
+		"/dev/net/tun",
 		"/proc/sys/fs/binfmt_misc",
 		"/sys/firmware/efi/efivars",
 		"/sys/fs/fuse/connections",
@@ -393,6 +394,7 @@ func (c *containerLXC) initLXC() error {
 			"c 5:1 rwm",    // /dev/console
 			"c 5:2 rwm",    // /dev/ptmx
 			"c 10:229 rwm", // /dev/fuse
+			"c 10:200 rwm", // /dev/net/tun
 		}
 
 		for _, dev := range devices {
