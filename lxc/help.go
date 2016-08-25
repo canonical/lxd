aba29e691f10093bc362487aef7a20461e8801f0
Carlos <cneirabustos@gmail.com>
2016-08-25 17:27:35 -0300

    :q
    Signed-off-by: Carlos <cneirabustos@gmail.com>

--- lxc/help.go
+++ lxc/help.go
@@ -39,7 +39,7 @@ func (c *helpCmd) run(_ *lxd.Config, args []string) error {
 			if !ok {
 				fmt.Fprintf(os.Stderr, i18n.G("error: unknown command: %s")+"\n", name)
 			} else {
-				fmt.Fprintf(os.Stdout, cmd.usage()+"\n")
+				fmt.Fprintf(os.StdOut, cmd.usage()+"\n")
 			}
 		}
 		return nil
