d519c4d13483fc2409f070a5fb995eb6cbafa29b
Carlos Neira <cneirabustos@gmail.com>
2016-08-26 18:50:11 -0300

    lxc: Add a manpage command
    Closes #2280
    Closes #2325
    
    Signed-off-by: Carlos <cneirabustos@gmail.com>

---
+++ lxc/manpage.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/lxc/lxd"
+	"github.com/lxc/lxd/shared/i18n"
+)
+
+type manpageCmd struct{}
+
+func (c *manpageCmd) showByDefault() bool {
+	return false
+}
+
+func (c *manpageCmd) usage() string {
+	return i18n.G(
+		`Prints all subcommands help to create a lxd manpage`)
+}
+
+func (c *manpageCmd) flags() {
+}
+
+func (c *manpageCmd) run(_ *lxd.Config, args []string) error {
+	if len(args) > 0 {
+		return errArgs
+	}
+	for k, _ := range commands {
+		commands["help"].run(nil, []string{k})
+	}
+	return nil
+}
