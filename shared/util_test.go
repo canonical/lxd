f8b9dd92f31aeffe061f1bc249b7458c34282fca
Christian Brauner <christian.brauner@canonical.com>
2016-09-12 13:14:31 +0200

    tests: add test for GetAllXattr()
    Signed-off-by: Christian Brauner <christian.brauner@canonical.com>

--- shared/util_test.go
+++ shared/util_test.go
@@ -4,11 +4,87 @@ import (
 	"bytes"
 	"crypto/rand"
 	"fmt"
+	"golang.org/x/sys/unix"
 	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
 )
+
+func TestGetAllXattr(t *testing.T) {
+	var (
+		err       error
+		testxattr = map[string]string{
+			"user.checksum": "asdfsf13434qwf1324",
+			"user.random":   "This is a test",
+		}
+	)
+	xattrFile, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.Remove(xattrFile.Name())
+	xattrFile.Close()
+
+	xattrDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.Remove(xattrDir)
+
+	for k, v := range testxattr {
+		if err = unix.Setxattr(xattrFile.Name(), k, []byte(v), 0); err != nil {
+			t.Error(err)
+			return
+		}
+		if err = unix.Setxattr(xattrDir, k, []byte(v), 0); err != nil {
+			t.Error(err)
+			return
+		}
+	}
+
+	// Test retrieval of extended attributes for regular files.
+	h, err := GetAllXattr(xattrFile.Name())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if h == nil {
+		t.Errorf("Expected to find extended attributes but did not find any.")
+		return
+	}
+
+	for k, v := range h {
+		found, ok := h[k]
+		if !ok || found != testxattr[k] {
+			t.Errorf("Expected to find extended attribute %s with a value of %s on regular file but did not find it.", k, v)
+			return
+		}
+	}
+
+	// Test retrieval of extended attributes for directories.
+	h, err = GetAllXattr(xattrDir)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if h == nil {
+		t.Errorf("Expected to find extended attributes but did not find any.")
+		return
+	}
+
+	for k, v := range h {
+		found, ok := h[k]
+		if !ok || found != testxattr[k] {
+			t.Errorf("Expected to find extended attribute %s with a value of %s on directory but did not find it.", k, v)
+			return
+		}
+	}
+}
 
 func TestFileCopy(t *testing.T) {
 	helloWorld := []byte("hello world\n")
