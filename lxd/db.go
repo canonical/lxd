11969fbe6c57064a11a78d76c5710ee4a08e8dfd
Stéphane Graber <stgraber@ubuntu.com>
2016-09-17 18:03:13 -0400

    db: Fix int64 handling
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/db.go
+++ lxd/db.go
@@ -367,6 +367,9 @@ func doDbQueryScan(db *sql.DB, q string, args []interface{}, outargs []interface
 			case int:
 				integer := 0
 				ptrargs[i] = &integer
+			case int64:
+				integer := int64(0)
+				ptrargs[i] = &integer
 			default:
 				return [][]interface{}{}, fmt.Errorf("Bad interface type: %s", t)
 			}
@@ -382,6 +385,8 @@ func doDbQueryScan(db *sql.DB, q string, args []interface{}, outargs []interface
 				newargs[i] = *ptrargs[i].(*string)
 			case int:
 				newargs[i] = *ptrargs[i].(*int)
+			case int64:
+				newargs[i] = *ptrargs[i].(*int64)
 			default:
 				return [][]interface{}{}, fmt.Errorf("Bad interface type: %s", t)
 			}
