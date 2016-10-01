c4d649b1cd7e30625a3353701f6a8dcf9e5f3e40
Christian Brauner <christian.brauner@canonical.com>
2016-10-01 16:06:51 +0200

    lxd/migrate: Do() push mode
    The migrationSink method Do() gains push support.
    
    Signed-off-by: Christian Brauner <christian.brauner@canonical.com>

--- lxd/migrate.go
+++ lxd/migrate.go
@@ -611,36 +611,57 @@ func (s *migrationSink) Metadata() interface{} {
 func (c *migrationSink) Do(migrateOp *operation) error {
 	var err error
 
-	// Start the storage for this container (LVM mount/umount)
-	c.src.container.StorageStart()
-
-	c.src.controlConn, err = c.connectWithSecret(c.src.controlSecret)
-	if err != nil {
-		c.src.container.StorageStop()
-		return err
-	}
-	defer c.src.disconnect()
-
-	c.src.fsConn, err = c.connectWithSecret(c.src.fsSecret)
-	if err != nil {
-		c.src.container.StorageStop()
-		c.src.sendControl(err)
-		return err
+	if c.push {
+		<-c.allConnected
 	}
 
-	if c.src.live {
-		c.src.criuConn, err = c.connectWithSecret(c.src.criuSecret)
+	disconnector := c.src.disconnect
+	if c.push {
+		disconnector = c.dest.disconnect
+	}
+
+	if c.push {
+		defer disconnector()
+	} else {
+		c.src.controlConn, err = c.connectWithSecret(c.src.controlSecret)
 		if err != nil {
-			c.src.container.StorageStop()
+			return err
+		}
+		defer c.src.disconnect()
+
+		c.src.fsConn, err = c.connectWithSecret(c.src.fsSecret)
+		if err != nil {
 			c.src.sendControl(err)
 			return err
 		}
+
+		if c.src.live {
+			c.src.criuConn, err = c.connectWithSecret(c.src.criuSecret)
+			if err != nil {
+				c.src.sendControl(err)
+				return err
+			}
+		}
+	}
+
+	receiver := c.src.recv
+	if c.push {
+		receiver = c.dest.recv
+	}
+
+	sender := c.src.send
+	if c.push {
+		sender = c.dest.send
+	}
+
+	controller := c.src.sendControl
+	if c.push {
+		controller = c.dest.sendControl
 	}
 
 	header := MigrationHeader{}
-	if err := c.src.recv(&header); err != nil {
-		c.src.container.StorageStop()
-		c.src.sendControl(err)
+	if err := receiver(&header); err != nil {
+		controller(err)
 		return err
 	}
 
@@ -664,9 +685,8 @@ func (c *migrationSink) Do(migrateOp *operation) error {
 		resp.Fs = &myType
 	}
 
-	if err := c.src.send(&resp); err != nil {
-		c.src.container.StorageStop()
-		c.src.sendControl(err)
+	if err := sender(&resp); err != nil {
+		controller(err)
 		return err
 	}
 
@@ -709,7 +729,11 @@ func (c *migrationSink) Do(migrateOp *operation) error {
 				snapshots = header.Snapshots
 			}
 
-			if err := mySink(c.src.live, c.src.container, header.Snapshots, c.src.fsConn, srcIdmap); err != nil {
+			fsConn := c.src.fsConn
+			if c.push {
+				fsConn = c.dest.fsConn
+			}
+			if err := mySink(c.src.live, c.src.container, header.Snapshots, fsConn, srcIdmap); err != nil {
 				fsTransfer <- err
 				return
 			}
@@ -732,7 +756,11 @@ func (c *migrationSink) Do(migrateOp *operation) error {
 
 			defer os.RemoveAll(imagesDir)
 
-			if err := RsyncRecv(shared.AddSlash(imagesDir), c.src.criuConn); err != nil {
+			criuConn := c.src.criuConn
+			if c.push {
+				criuConn = c.dest.criuConn
+			}
+			if err := RsyncRecv(shared.AddSlash(imagesDir), criuConn); err != nil {
 				restore <- err
 				return
 			}
@@ -756,32 +784,31 @@ func (c *migrationSink) Do(migrateOp *operation) error {
 		restore <- nil
 	}(c)
 
-	source := c.src.controlChannel()
-
-	defer c.src.container.StorageStop()
+	var source <-chan MigrationControl
+	if c.push {
+		source = c.dest.controlChannel()
+	} else {
+		source = c.src.controlChannel()
+	}
 
 	for {
 		select {
 		case err = <-restore:
-			c.src.sendControl(err)
+			controller(err)
 			return err
 		case msg, ok := <-source:
 			if !ok {
-				c.src.disconnect()
+				disconnector()
 				return fmt.Errorf("Got error reading source")
 			}
 			if !*msg.Success {
-				c.src.disconnect()
+				disconnector()
 				return fmt.Errorf(*msg.Message)
 			} else {
 				// The source can only tell us it failed (e.g. if
 				// checkpointing failed). We have to tell the source
 				// whether or not the restore was successful.
 				shared.LogDebugf("Unknown message %v from source", msg)
-				err = c.src.container.TemplateApply("copy")
-				if err != nil {
-					return err
-				}
 			}
 		}
 	}
