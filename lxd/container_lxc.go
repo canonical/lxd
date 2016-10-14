fc07a8593c9c24d0824efb916f83e7d381bf0114
Stéphane Graber <stgraber@ubuntu.com>
2016-10-14 15:10:45 -0400

    Remove legacy code from OnStop
    This changes OnStop to assume LXC 2.0.0 or higher.
    
    So it now relies on LXC_TARGET being set in the environment (and will
    fail if it's not) and also depends on LXC interrupting container restart
    on hook failures.
    
    This allows the removal of a good chunk of code and improved logging of
    what's going on with the container.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/container_lxc.go
+++ lxd/container_lxc.go
@@ -1753,6 +1753,12 @@ func (c *containerLXC) Shutdown(timeout time.Duration) error {
 }
 
 func (c *containerLXC) OnStop(target string) error {
+	// Validate target
+	if !shared.StringInSlice(target, []string{"stop", "reboot"}) {
+		shared.LogError("Container sent invalid target to OnStop", log.Ctx{"container": c.Name(), "target": target})
+		return fmt.Errorf("Invalid stop target: %s", target)
+	}
+
 	// Get operation
 	op, _ := c.getOperation("")
 	if op != nil && !shared.StringInSlice(op.action, []string{"stop", "shutdown"}) {
@@ -1772,12 +1778,18 @@ func (c *containerLXC) OnStop(target string) error {
 		return err
 	}
 
-	// Unload the apparmor profile
-	if err := AADestroy(c); err != nil {
-		shared.LogError("failed to destroy apparmor namespace", log.Ctx{"container": c.Name(), "err": err})
+	// Log user actions
+	if op == nil {
+		ctxMap := log.Ctx{"name": c.name,
+			"action":    target,
+			"created":   c.creationDate,
+			"ephemeral": c.ephemeral,
+			"used":      c.lastUsedDate,
+			"stateful":  false}
+
+		shared.LogInfo(fmt.Sprintf("Container initiated %s", target), ctxMap)
 	}
 
-	// FIXME: The go routine can go away once we can rely on LXC_TARGET
 	go func(c *containerLXC, target string, op *lxcContainerOperation) {
 		c.fromHook = false
 
@@ -1786,63 +1798,35 @@ func (c *containerLXC) OnStop(target string) error {
 			defer op.Done(nil)
 		}
 
-		if target == "unknown" && op != nil {
-			target = "stop"
-		}
+		// Wait for other post-stop actions to be done
+		c.IsRunning()
 
-		if target == "unknown" {
-			time.Sleep(5 * time.Second)
-
-			newContainer, err := containerLoadByName(c.daemon, c.Name())
-			if err != nil {
-				return
-			}
-
-			if newContainer.Id() != c.id {
-				return
-			}
-
-			if newContainer.IsRunning() {
-				return
-			}
+		// Unload the apparmor profile
+		err = AADestroy(c)
+		if err != nil {
+			shared.LogError("Failed to destroy apparmor namespace", log.Ctx{"container": c.Name(), "err": err})
 		}
 
 		// Clean all the unix devices
 		err = c.removeUnixDevices()
 		if err != nil {
-			shared.LogError("Unable to remove unix devices", log.Ctx{"err": err})
+			shared.LogError("Unable to remove unix devices", log.Ctx{"container": c.Name(), "err": err})
 		}
 
 		// Clean all the disk devices
 		err = c.removeDiskDevices()
 		if err != nil {
-			shared.LogError("Unable to remove disk devices", log.Ctx{"err": err})
+			shared.LogError("Unable to remove disk devices", log.Ctx{"container": c.Name(), "err": err})
 		}
 
 		// Clean all network filters
 		err = c.removeNetworkFilters()
 		if err != nil {
-			shared.LogError("Unable to remove network filters", log.Ctx{"err": err})
+			shared.LogError("Unable to remove network filters", log.Ctx{"container": c.Name(), "err": err})
 		}
 
 		// Reboot the container
 		if target == "reboot" {
-
-			/* This part is a hack to workaround a LXC bug where a
-			   failure from a post-stop script doesn't prevent the container to restart. */
-			ephemeral := c.ephemeral
-			args := containerArgs{
-				Architecture: c.Architecture(),
-				Config:       c.LocalConfig(),
-				Devices:      c.LocalDevices(),
-				Ephemeral:    false,
-				Profiles:     c.Profiles(),
-			}
-			c.Update(args, false)
-			c.Stop(false)
-			args.Ephemeral = ephemeral
-			c.Update(args, true)
-
 			// Start the container again
 			c.Start(false)
 			return
@@ -1852,10 +1836,7 @@ func (c *containerLXC) OnStop(target string) error {
 		deviceTaskSchedulerTrigger("container", c.name, "stopped")
 
 		// Record current state
-		err = dbContainerSetState(c.daemon.db, c.id, "STOPPED")
-		if err != nil {
-			return
-		}
+		dbContainerSetState(c.daemon.db, c.id, "STOPPED")
 
 		// Destroy ephemeral containers
 		if c.ephemeral {
