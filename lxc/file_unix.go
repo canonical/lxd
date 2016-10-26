a782d6ee08f5168db286c0913b2cbdbdc6eefcd4
Tycho Andersen <tycho.andersen@canonical.com>
2016-10-26 11:20:20 -0600

    only normalize paths on linux
    Prepending / on windows seems like a bad idea ;)
    
    Closes #2565
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- lxc/file_unix.go
+++ lxc/file_unix.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 	"syscall"
 )
 
@@ -18,4 +19,19 @@ func (c *fileCmd) getOwner(f *os.File) (os.FileMode, int, int, error) {
 	gid := int(fInfo.Sys().(*syscall.Stat_t).Gid)
 
 	return mode, uid, gid, nil
+}
+
+func (c *fileCmd) normalize(path string, target string) string {
+	/* Fix up the path. Let's:
+	 * 1. re-add the leading / that got stripped from the SplitN
+	 * 2. clean it and remove any /./, /../, /////, etc.
+	 * 3. keep the trailing slash if it had one, since we use it via
+	 *    filepath.Split below
+	 */
+	path = filepath.Clean("/" + path)
+	if target[len(target)-1] == '/' {
+		path = path + "/"
+	}
+
+	return path
 }
--- lxc/file_windows.go
+++ lxc/file_windows.go
@@ -9,3 +9,7 @@ import (
 func (c *fileCmd) getOwner(f *os.File) (os.FileMode, int, int, error) {
 	return os.FileMode(0), -1, -1, nil
 }
+
+func (c *fileCmd) normalize(path string, target string) string {
+	return path
+}
