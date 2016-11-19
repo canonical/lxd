9b248604ed8a6a96ec03e39f7cc7eb0dce8e89ed
Stéphane Graber <stgraber@ubuntu.com>
2016-11-19 18:54:31 -0500

    test: Don't depend on main.sh for filemanip
    Otherwise if main.sh is owned by a host uid which cannot be mapped into
    the container, the transfer will fail.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/filemanip.sh
+++ test/suites/filemanip.sh
@@ -4,12 +4,14 @@ test_filemanip() {
   ensure_import_testimage
   ensure_has_localhost_remote "${LXD_ADDR}"
 
+  echo "test" > "${TEST_DIR}"/filemanip
+
   lxc launch testimage filemanip
   lxc exec filemanip -- ln -s /tmp/ /tmp/outside
-  lxc file push main.sh filemanip/tmp/outside/
+  lxc file push "${TEST_DIR}"/filemanip filemanip/tmp/outside/
 
-  [ ! -f /tmp/main.sh ]
-  lxc exec filemanip -- ls /tmp/main.sh
+  [ ! -f /tmp/filemanip ]
+  lxc exec filemanip -- ls /tmp/filemanip
 
   # missing files should return 404
   err=$(my_curl -o /dev/null -w "%{http_code}" -X GET "https://${LXD_ADDR}/1.0/containers/filemanip/files?path=/tmp/foo")
