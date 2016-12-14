168732aefa9ac1af16345f411d4bb6a2f1774fcc
Stéphane Graber <stgraber@ubuntu.com>
2016-12-14 15:17:42 -0500

    main: Move netcat to own file
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/rsync.go
+++ lxd/rsync.go
@@ -7,7 +7,6 @@ import (
 	"net"
 	"os"
 	"os/exec"
-	"sync"
 
 	"github.com/gorilla/websocket"
 
@@ -173,44 +172,4 @@ func RsyncRecv(path string, conn *websocket.Conn, writeWrapper func(io.WriteClos
 	<-writeDone
 
 	return err
-}
-
-// Netcat is called with:
-//
-//    lxd netcat /path/to/unix/socket
-//
-// and does unbuffered netcatting of to socket to stdin/stdout. Any arguments
-// after the path to the unix socket are ignored, so that this can be passed
-// directly to rsync as the sync command.
-func Netcat(args []string) error {
-	if len(args) < 2 {
-		return fmt.Errorf("Bad arguments %q", args)
-	}
-
-	uAddr, err := net.ResolveUnixAddr("unix", args[1])
-	if err != nil {
-		return err
-	}
-
-	conn, err := net.DialUnix("unix", nil, uAddr)
-	if err != nil {
-		return err
-	}
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	go func() {
-		io.Copy(os.Stdout, conn)
-		conn.Close()
-		wg.Done()
-	}()
-
-	go func() {
-		io.Copy(conn, os.Stdin)
-	}()
-
-	wg.Wait()
-
-	return nil
 }
