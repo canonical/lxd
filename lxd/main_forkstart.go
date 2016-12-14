346f9afbe5e203d14ec32ba61deb1b9701c18ea5
Stéphane Graber <stgraber@ubuntu.com>
2016-12-14 15:11:54 -0500

    main: Move forkstart to own file
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ lxd/main_forkstart.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"gopkg.in/lxc/go-lxc.v2"
+
+	"github.com/lxc/lxd/shared"
+)
+
+/*
+ * This is called by lxd when called as "lxd forkstart <container>"
+ * 'forkstart' is used instead of just 'start' in the hopes that people
+ * do not accidentally type 'lxd start' instead of 'lxc start'
+ */
+func cmdForkStart(args []string) error {
+	if len(args) != 4 {
+		return fmt.Errorf("Bad arguments: %q", args)
+	}
+
+	name := args[1]
+	lxcpath := args[2]
+	configPath := args[3]
+
+	c, err := lxc.NewContainer(name, lxcpath)
+	if err != nil {
+		return fmt.Errorf("Error initializing container for start: %q", err)
+	}
+
+	err = c.LoadConfigFile(configPath)
+	if err != nil {
+		return fmt.Errorf("Error opening startup config file: %q", err)
+	}
+
+	/* due to https://github.com/golang/go/issues/13155 and the
+	 * CollectOutput call we make for the forkstart process, we need to
+	 * close our stdin/stdout/stderr here. Collecting some of the logs is
+	 * better than collecting no logs, though.
+	 */
+	os.Stdin.Close()
+	os.Stderr.Close()
+	os.Stdout.Close()
+
+	// Redirect stdout and stderr to a log file
+	logPath := shared.LogPath(name, "forkstart.log")
+	if shared.PathExists(logPath) {
+		os.Remove(logPath)
+	}
+
+	logFile, err := os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0644)
+	if err == nil {
+		syscall.Dup3(int(logFile.Fd()), 1, 0)
+		syscall.Dup3(int(logFile.Fd()), 2, 0)
+	}
+
+	return c.Start()
+}
