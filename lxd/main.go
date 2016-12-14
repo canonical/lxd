bf14ee961865117691f14a497486adf761db1b3c
Stéphane Graber <stgraber@ubuntu.com>
2016-12-14 15:15:27 -0500

    main: Move migratedumpsuccess to own file
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/main.go
+++ lxd/main.go
@@ -353,22 +353,3 @@ func cmdWaitReady() error {
 
 	return nil
 }
-
-func cmdMigrateDumpSuccess(args []string) error {
-	if len(args) != 3 {
-		return fmt.Errorf("bad migrate dump success args %s", args)
-	}
-
-	c, err := lxd.NewClient(&lxd.DefaultConfig, "local")
-	if err != nil {
-		return err
-	}
-
-	conn, err := c.Websocket(args[1], args[2])
-	if err != nil {
-		return err
-	}
-	conn.Close()
-
-	return c.WaitForSuccess(args[1])
-}
