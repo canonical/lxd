35b985ff4834b802937f7053d5c1f11416afef57
Stéphane Graber <stgraber@ubuntu.com>
2016-12-15 18:20:08 -0500

    daemon: Common codepath for http client
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -107,10 +107,10 @@ type Command struct {
 	patch         func(d *Daemon, r *http.Request) Response
 }
 
-func (d *Daemon) httpGetSync(url string, certificate string) (*lxd.Response, error) {
+func (d *Daemon) httpClient(certificate string) (*http.Client, error) {
 	var err error
-
 	var cert *x509.Certificate
+
 	if certificate != "" {
 		certBlock, _ := pem.Decode([]byte(certificate))
 		if certBlock == nil {
@@ -139,12 +139,23 @@ func (d *Daemon) httpGetSync(url string, certificate string) (*lxd.Response, err
 		Transport: tr,
 	}
 
+	return &myhttp, nil
+}
+
+func (d *Daemon) httpGetSync(url string, certificate string) (*lxd.Response, error) {
+	var err error
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("User-Agent", shared.UserAgent)
+
+	myhttp, err := d.httpClient(certificate)
+	if err != nil {
+		return nil, err
+	}
 
 	r, err := myhttp.Do(req)
 	if err != nil {
@@ -166,32 +177,9 @@ func (d *Daemon) httpGetSync(url string, certificate string) (*lxd.Response, err
 func (d *Daemon) httpGetFile(url string, certificate string) (*http.Response, error) {
 	var err error
 
-	var cert *x509.Certificate
-	if certificate != "" {
-		certBlock, _ := pem.Decode([]byte(certificate))
-		if certBlock == nil {
-			return nil, fmt.Errorf("Invalid certificate")
-		}
-
-		cert, err = x509.ParseCertificate(certBlock.Bytes)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	tlsConfig, err := shared.GetTLSConfig("", "", "", cert)
+	myhttp, err := d.httpClient(certificate)
 	if err != nil {
 		return nil, err
-	}
-
-	tr := &http.Transport{
-		TLSClientConfig:   tlsConfig,
-		Dial:              shared.RFC3493Dialer,
-		Proxy:             d.proxy,
-		DisableKeepAlives: true,
-	}
-	myhttp := http.Client{
-		Transport: tr,
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
