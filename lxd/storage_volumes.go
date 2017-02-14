ce88f5b724c898786a2e38e1104ddd69eb068d6a
Christian Brauner <christian.brauner@ubuntu.com>
2017-02-14 21:29:16 +0100

    storage volumes: implement api endpoints
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

---
+++ lxd/storage_volumes.go
@@ -0,0 +1,461 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/lxc/lxd/shared"
+	"github.com/lxc/lxd/shared/api"
+	"github.com/lxc/lxd/shared/version"
+)
+
+// /1.0/storage-pools/{name}/volumes
+// List all storage volumes attached to a given storage pool.
+func storagePoolVolumesGet(d *Daemon, r *http.Request) Response {
+	poolName := mux.Vars(r)["name"]
+
+	recursionStr := r.FormValue("recursion")
+	recursion, err := strconv.Atoi(recursionStr)
+	if err != nil {
+		recursion = 0
+	}
+
+	// Retrieve ID of the storage pool (and check if the storage pool
+	// exists).
+	poolID, err := dbStoragePoolGetID(d.db, poolName)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	// Get all volumes currently attached to the storage pool by ID of the
+	// pool.
+	volumes, err := dbStoragePoolVolumesGet(d.db, poolID)
+	if err != nil && err != NoSuchObjectError {
+		return SmartError(err)
+	}
+
+	resultString := []string{}
+	for _, volume := range volumes {
+		apiEndpoint, err := storagePoolVolumeTypeNameToApiEndpoint(volume.Type)
+		if err != nil {
+			return InternalError(err)
+		}
+
+		if recursion == 0 {
+			resultString = append(resultString, fmt.Sprintf("/%s/storage-pools/%s/volumes/%s/%s", version.APIVersion, poolName, apiEndpoint, volume.Name))
+		} else {
+			volumeUsedBy, err := storagePoolVolumeUsedByGet(d, volume.Name)
+			if err != nil {
+				return InternalError(err)
+			}
+			volume.UsedBy = volumeUsedBy
+		}
+	}
+
+	if recursion == 0 {
+		return SyncResponse(true, resultString)
+	}
+
+	return SyncResponse(true, volumes)
+}
+
+var storagePoolVolumesCmd = Command{name: "storage-pools/{name}/volumes", get: storagePoolVolumesGet}
+
+// /1.0/storage-pools/{name}/volumes/{type}
+// List all storage volumes of a given volume type for a given storage pool.
+func storagePoolVolumesTypeGet(d *Daemon, r *http.Request) Response {
+	// Get the name of the pool the storage volume is supposed to be
+	// attached to.
+	poolName := mux.Vars(r)["name"]
+
+	recursionStr := r.FormValue("recursion")
+	recursion, err := strconv.Atoi(recursionStr)
+	if err != nil {
+		recursion = 0
+	}
+
+	// Get the name of the volume type.
+	volumeTypeName := mux.Vars(r)["type"]
+
+	// Convert the volume type name to our internal integer representation.
+	volumeType, err := storagePoolVolumeTypeNameToType(volumeTypeName)
+	if err != nil {
+		return BadRequest(err)
+	}
+	// Check that the storage volume type is valid.
+	if !shared.IntInSlice(volumeType, supportedVolumeTypes) {
+		return BadRequest(fmt.Errorf("Invalid storage volume type %s.", volumeTypeName))
+	}
+
+	// Retrieve ID of the storage pool (and check if the storage pool
+	// exists).
+	poolID, err := dbStoragePoolGetID(d.db, poolName)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	// Get the names of all storage volumes of a given volume type currently
+	// attached to the storage pool.
+	volumes, err := dbStoragePoolVolumesGetType(d.db, volumeType, poolID)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	resultString := []string{}
+	resultMap := []*api.StorageVolume{}
+	for _, volume := range volumes {
+		if recursion == 0 {
+			apiEndpoint, err := storagePoolVolumeTypeToApiEndpoint(volumeType)
+			if err != nil {
+				return InternalError(err)
+			}
+			resultString = append(resultString, fmt.Sprintf("/%s/storage-pools/%s/volumes/%s/%s", version.APIVersion, poolName, apiEndpoint, volume))
+		} else {
+			_, vol, err := dbStoragePoolVolumeGetType(d.db, volume, volumeType, poolID)
+			if err != nil {
+				continue
+			}
+
+			volumeUsedBy, err := storagePoolVolumeUsedByGet(d, vol.Name)
+			if err != nil {
+				return InternalError(err)
+			}
+			vol.UsedBy = volumeUsedBy
+
+			resultMap = append(resultMap, vol)
+		}
+	}
+
+	if recursion == 0 {
+		return SyncResponse(true, resultString)
+	}
+
+	return SyncResponse(true, resultMap)
+}
+
+// /1.0/storage-pools/{name}/volumes/{type}
+// Create a storage volume of a given volume type in a given storage pool.
+func storagePoolVolumesTypePost(d *Daemon, r *http.Request) Response {
+	req := api.StorageVolume{}
+
+	// Parse the request.
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return BadRequest(err)
+	}
+
+	// Sanity checks.
+	if req.Name == "" {
+		return BadRequest(fmt.Errorf("No name provided"))
+	}
+
+	// Check that the name of the new storage volume is valid. (For example.
+	// zfs pools cannot contain "/" in their names.)
+	err = storageValidName(req.Name)
+	if err != nil {
+		return BadRequest(err)
+	}
+
+	// Check that the user gave use a storage volume type for the storage
+	// volume we are about to create.
+	if req.Type == "" {
+		return BadRequest(fmt.Errorf("You must provide a storage volume type of the storage volume."))
+	}
+
+	// Convert the volume type name to our internal integer representation.
+	volumeType, err := storagePoolVolumeTypeNameToType(req.Type)
+	if err != nil {
+		return BadRequest(err)
+	}
+
+	// We currently only allow to create storage volumes of type
+	// storagePoolVolumeTypeCustom. So check, that nothing else was
+	// requested.
+	if volumeType != storagePoolVolumeTypeCustom {
+		return BadRequest(fmt.Errorf("Currently not allowed to create storage volumes of type %s.", req.Type))
+	}
+
+	// Check if the user gave us a valid pool name in which the new storage
+	// volume is supposed to be created.
+	poolName := mux.Vars(r)["name"]
+
+	// Load storage pool the volume will be attached to.
+	poolID, poolStruct, err := dbStoragePoolGet(d.db, poolName)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	// Check that a storage volume of the same storage volume type does not
+	// already exist.
+	volumeID, _ := dbStoragePoolVolumeGetTypeID(d.db, req.Name, volumeType, poolID)
+	if volumeID > 0 {
+		return BadRequest(fmt.Errorf("A storage volume of type %s does already exist.", req.Type))
+	}
+
+	// Make sure that we don't pass a nil to the next function.
+	if req.Config == nil {
+		req.Config = map[string]string{}
+	}
+
+	// Validate the requested storage volume configuration.
+	err = storageVolumeValidateConfig(poolName, req.Config, poolStruct)
+	if err != nil {
+		return BadRequest(err)
+	}
+
+	// Create the database entry for the storage volume.
+	_, err = dbStoragePoolVolumeCreate(d.db, req.Name, volumeType, poolID, req.Config)
+	if err != nil {
+		return InternalError(fmt.Errorf("Error inserting %s of type %s into database: %s", poolName, req.Type, err))
+	}
+
+	s, err := storagePoolVolumeInit(d, poolName, req.Name, volumeType)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	// Create storage volume.
+	err = s.StoragePoolVolumeCreate()
+	if err != nil {
+		dbStoragePoolVolumeDelete(d.db, req.Name, volumeType, poolID)
+		return InternalError(err)
+	}
+
+	apiEndpoint, err := storagePoolVolumeTypeToApiEndpoint(volumeType)
+	if err != nil {
+		return InternalError(err)
+	}
+	return SyncResponseLocation(true, nil, fmt.Sprintf("/%s/storage-pools/%s/volumes/%s", version.APIVersion, poolName, apiEndpoint))
+}
+
+var storagePoolVolumesTypeCmd = Command{name: "storage-pools/{name}/volumes/{type}", get: storagePoolVolumesTypeGet, post: storagePoolVolumesTypePost}
+
+// /1.0/storage-pools/{pool}/volumes/{type}/{name}
+// Get storage volume of a given volume type on a given storage pool.
+func storagePoolVolumeTypeGet(d *Daemon, r *http.Request) Response {
+	// Get the name of the storage volume.
+	volumeName := mux.Vars(r)["name"]
+
+	// Get the name of the storage pool the volume is supposed to be
+	// attached to.
+	poolName := mux.Vars(r)["pool"]
+
+	// Get the name of the volume type.
+	volumeTypeName := mux.Vars(r)["type"]
+
+	// Convert the volume type name to our internal integer representation.
+	volumeType, err := storagePoolVolumeTypeNameToType(volumeTypeName)
+	if err != nil {
+		return BadRequest(err)
+	}
+	// Check that the storage volume type is valid.
+	if !shared.IntInSlice(volumeType, supportedVolumeTypes) {
+		return BadRequest(fmt.Errorf("Invalid storage volume type %s.", volumeTypeName))
+	}
+
+	// Get the ID of the storage pool the storage volume is supposed to be
+	// attached to.
+	poolID, err := dbStoragePoolGetID(d.db, poolName)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	// Get the storage volume.
+	_, volume, err := dbStoragePoolVolumeGetType(d.db, volumeName, volumeType, poolID)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	volumeUsedBy, err := storagePoolVolumeUsedByGet(d, volume.Name)
+	if err != nil {
+		return InternalError(err)
+	}
+	volume.UsedBy = volumeUsedBy
+
+	etag := []interface{}{volume.Name, volume.Type, volume.UsedBy, volume.Config}
+
+	return SyncResponseETag(true, volume, etag)
+}
+
+// /1.0/storage-pools/{pool}/volumes/{type}/{name}
+func storagePoolVolumeTypePut(d *Daemon, r *http.Request) Response {
+	// Get the name of the storage volume.
+	volumeName := mux.Vars(r)["name"]
+
+	// Get the name of the storage pool the volume is supposed to be
+	// attached to.
+	poolName := mux.Vars(r)["pool"]
+
+	// Get the name of the volume type.
+	volumeTypeName := mux.Vars(r)["type"]
+
+	// Convert the volume type name to our internal integer representation.
+	volumeType, err := storagePoolVolumeTypeNameToType(volumeTypeName)
+	if err != nil {
+		return BadRequest(err)
+	}
+	// Check that the storage volume type is valid.
+	if !shared.IntInSlice(volumeType, supportedVolumeTypes) {
+		return BadRequest(fmt.Errorf("Invalid storage volume type %s.", volumeTypeName))
+	}
+
+	poolID, pool, err := dbStoragePoolGet(d.db, poolName)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	// Get the existing storage volume.
+	_, volume, err := dbStoragePoolVolumeGetType(d.db, volumeName, volumeType, poolID)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	// Validate the ETag
+	etag := []interface{}{volume.Name, volume.Type, volume.UsedBy, volume.Config}
+
+	err = etagCheck(r, etag)
+	if err != nil {
+		return PreconditionFailed(err)
+	}
+
+	req := api.StorageVolume{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return BadRequest(err)
+	}
+
+	// Validate the configuration
+	err = storageVolumeValidateConfig(req.Name, req.Config, pool)
+	if err != nil {
+		return BadRequest(err)
+	}
+
+	err = storagePoolVolumeUpdate(d, poolName, req.Name, volumeType, req.Config)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	return EmptySyncResponse
+}
+
+// /1.0/storage-pools/{pool}/volumes/{type}/{name}
+func storagePoolVolumeTypePatch(d *Daemon, r *http.Request) Response {
+	// Get the name of the storage volume.
+	volumeName := mux.Vars(r)["name"]
+
+	// Get the name of the storage pool the volume is supposed to be
+	// attached to.
+	poolName := mux.Vars(r)["pool"]
+
+	// Get the name of the volume type.
+	volumeTypeName := mux.Vars(r)["type"]
+
+	// Convert the volume type name to our internal integer representation.
+	volumeType, err := storagePoolVolumeTypeNameToType(volumeTypeName)
+	if err != nil {
+		return BadRequest(err)
+	}
+	// Check that the storage volume type is valid.
+	if !shared.IntInSlice(volumeType, supportedVolumeTypes) {
+		return BadRequest(fmt.Errorf("Invalid storage volume type %s.", volumeTypeName))
+	}
+
+	// Get the ID of the storage pool the storage volume is supposed to be
+	// attached to.
+	poolID, pool, err := dbStoragePoolGet(d.db, poolName)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	// Get the existing storage volume.
+	_, volume, err := dbStoragePoolVolumeGetType(d.db, volumeName, volumeType, poolID)
+	if err != nil {
+		return SmartError(err)
+	}
+
+	// Validate the ETag
+	etag := []interface{}{volume.Name, volume.Type, volume.UsedBy, volume.Config}
+
+	err = etagCheck(r, etag)
+	if err != nil {
+		return PreconditionFailed(err)
+	}
+
+	req := api.StorageVolume{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return BadRequest(err)
+	}
+
+	if req.Config == nil {
+		req.Config = map[string]string{}
+	}
+
+	for k, v := range volume.Config {
+		_, ok := req.Config[k]
+		if !ok {
+			req.Config[k] = v
+		}
+	}
+
+	// Validate the configuration
+	err = storageVolumeValidateConfig(volumeName, req.Config, pool)
+	if err != nil {
+		return BadRequest(err)
+	}
+
+	err = storagePoolVolumeUpdate(d, poolName, req.Name, volumeType, req.Config)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	return EmptySyncResponse
+}
+
+// /1.0/storage-pools/{pool}/volumes/{type}/{name}
+func storagePoolVolumeTypeDelete(d *Daemon, r *http.Request) Response {
+	// Get the name of the storage volume.
+	volumeName := mux.Vars(r)["name"]
+
+	// Get the name of the storage pool the volume is supposed to be
+	// attached to.
+	poolName := mux.Vars(r)["pool"]
+
+	// Get the name of the volume type.
+	volumeTypeName := mux.Vars(r)["type"]
+
+	// Convert the volume type name to our internal integer representation.
+	volumeType, err := storagePoolVolumeTypeNameToType(volumeTypeName)
+	if err != nil {
+		return BadRequest(err)
+	}
+	// Check that the storage volume type is valid.
+	if !shared.IntInSlice(volumeType, supportedVolumeTypes) {
+		return BadRequest(fmt.Errorf("Invalid storage volume type %s.", volumeTypeName))
+	}
+
+	s, err := storagePoolVolumeInit(d, poolName, volumeName, volumeType)
+	if err != nil {
+		return NotFound
+	}
+
+	err = s.StoragePoolVolumeDelete()
+	if err != nil {
+		return InternalError(err)
+	}
+
+	poolID, err := dbStoragePoolGetID(d.db, poolName)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	err = dbStoragePoolVolumeDelete(d.db, volumeName, volumeType, poolID)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	return EmptySyncResponse
+}
+
+var storagePoolVolumeTypeCmd = Command{name: "storage-pools/{pool}/volumes/{type}/{name}", get: storagePoolVolumeTypeGet, put: storagePoolVolumeTypePut, patch: storagePoolVolumeTypePatch, delete: storagePoolVolumeTypeDelete}
---
+++ lxd/storage_volumes_utils.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/lxc/lxd/shared"
+	"github.com/lxc/lxd/shared/version"
+)
+
+const (
+	storagePoolVolumeTypeContainer = iota
+	storagePoolVolumeTypeImage
+	storagePoolVolumeTypeCustom
+)
+
+// Leave the string type in here! This guarantees that go treats this is as a
+// typed string constant. Removing it causes go to treat these as untyped string
+// constants which is not what we want.
+const (
+	storagePoolVolumeTypeNameContainer string = "container"
+	storagePoolVolumeTypeNameImage     string = "image"
+	storagePoolVolumeTypeNameCustom    string = "custom"
+)
+
+// Leave the string type in here! This guarantees that go treats this is as a
+// typed string constant. Removing it causes go to treat these as untyped string
+// constants which is not what we want.
+const (
+	storagePoolVolumeApiEndpointContainers string = "containers"
+	storagePoolVolumeApiEndpointImages     string = "images"
+	storagePoolVolumeApiEndpointCustom     string = "custom"
+)
+
+var supportedVolumeTypes = []int{storagePoolVolumeTypeContainer, storagePoolVolumeTypeImage, storagePoolVolumeTypeCustom}
+
+func storagePoolVolumeTypeNameToType(volumeTypeName string) (int, error) {
+	switch volumeTypeName {
+	case storagePoolVolumeTypeNameContainer:
+		return storagePoolVolumeTypeContainer, nil
+	case storagePoolVolumeTypeNameImage:
+		return storagePoolVolumeTypeImage, nil
+	case storagePoolVolumeTypeNameCustom:
+		return storagePoolVolumeTypeCustom, nil
+	}
+
+	return -1, fmt.Errorf("Invalid storage volume type name.")
+}
+
+func storagePoolVolumeTypeNameToApiEndpoint(volumeTypeName string) (string, error) {
+	switch volumeTypeName {
+	case storagePoolVolumeTypeNameContainer:
+		return storagePoolVolumeApiEndpointContainers, nil
+	case storagePoolVolumeTypeNameImage:
+		return storagePoolVolumeApiEndpointImages, nil
+	case storagePoolVolumeTypeNameCustom:
+		return storagePoolVolumeApiEndpointCustom, nil
+	}
+
+	return "", fmt.Errorf("Invalid storage volume type name.")
+}
+
+func storagePoolVolumeTypeToName(volumeType int) (string, error) {
+	switch volumeType {
+	case storagePoolVolumeTypeContainer:
+		return storagePoolVolumeTypeNameContainer, nil
+	case storagePoolVolumeTypeImage:
+		return storagePoolVolumeTypeNameImage, nil
+	case storagePoolVolumeTypeCustom:
+		return storagePoolVolumeTypeNameCustom, nil
+	}
+
+	return "", fmt.Errorf("Invalid storage volume type.")
+}
+
+func storagePoolVolumeTypeToApiEndpoint(volumeType int) (string, error) {
+	switch volumeType {
+	case storagePoolVolumeTypeContainer:
+		return storagePoolVolumeApiEndpointContainers, nil
+	case storagePoolVolumeTypeImage:
+		return storagePoolVolumeApiEndpointImages, nil
+	case storagePoolVolumeTypeCustom:
+		return storagePoolVolumeApiEndpointCustom, nil
+	}
+
+	return "", fmt.Errorf("Invalid storage volume type.")
+}
+
+func storagePoolVolumeUpdate(d *Daemon, poolName string, volumeName string, volumeType int, newConfig map[string]string) error {
+	s, err := storagePoolVolumeInit(d, poolName, volumeName, volumeType)
+	if err != nil {
+		return err
+	}
+
+	oldWritable := s.GetStoragePoolVolumeWritable()
+	newWritable := oldWritable
+
+	// Backup the current state
+	oldConfig := map[string]string{}
+	err = shared.DeepCopy(&oldWritable.Config, &oldConfig)
+	if err != nil {
+		return err
+	}
+
+	// Define a function which reverts everything.  Defer this function
+	// so that it doesn't need to be explicitly called in every failing
+	// return path. Track whether or not we want to undo the changes
+	// using a closure.
+	undoChanges := true
+	defer func() {
+		if undoChanges {
+			s.SetStoragePoolVolumeWritable(&oldWritable)
+		}
+	}()
+
+	// Diff the configurations
+	changedConfig := []string{}
+	userOnly := true
+	for key := range oldConfig {
+		if oldConfig[key] != newConfig[key] {
+			if !strings.HasPrefix(key, "user.") {
+				userOnly = false
+			}
+
+			if !shared.StringInSlice(key, changedConfig) {
+				changedConfig = append(changedConfig, key)
+			}
+		}
+	}
+
+	for key := range newConfig {
+		if oldConfig[key] != newConfig[key] {
+			if !strings.HasPrefix(key, "user.") {
+				userOnly = false
+			}
+
+			if !shared.StringInSlice(key, changedConfig) {
+				changedConfig = append(changedConfig, key)
+			}
+		}
+	}
+
+	// Skip on no change
+	if len(changedConfig) == 0 {
+		return nil
+	}
+
+	// Update the storage pool
+	if !userOnly {
+		err = s.StoragePoolVolumeUpdate(changedConfig)
+		if err != nil {
+			return err
+		}
+	}
+
+	newWritable.Config = newConfig
+
+	// Apply the new configuration
+	s.SetStoragePoolVolumeWritable(&newWritable)
+
+	poolID, err := dbStoragePoolGetID(d.db, poolName)
+	if err != nil {
+		return err
+	}
+
+	// Update the database
+	err = dbStoragePoolVolumeUpdate(d.db, volumeName, volumeType, poolID, newConfig)
+	if err != nil {
+		return err
+	}
+
+	// Success, update the closure to mark that the changes should be kept.
+	undoChanges = false
+
+	return nil
+}
+
+func storagePoolVolumeUsedByGet(d *Daemon, volumeName string) ([]string, error) {
+	// Look for containers using the interface
+	cts, err := dbContainersList(d.db, cTypeRegular)
+	if err != nil {
+		return []string{}, err
+	}
+
+	volumeUsedBy := []string{}
+	for _, ct := range cts {
+		// We're not accessing any storage here.
+		c, err := containerLoadByName(d, ct)
+		if err != nil {
+			return []string{}, err
+		}
+
+		for _, d := range c.LocalDevices() {
+			if d["type"] != "disk" {
+				continue
+			}
+
+			containerAsVolume := fmt.Sprintf("%s/%s", storagePoolVolumeApiEndpointContainers, volumeName)
+			if (d["source"] == volumeName) || (d["source"] == containerAsVolume) {
+				volumeUsedBy = append(volumeUsedBy, fmt.Sprintf("/%s/containers/%s", version.APIVersion, ct))
+			}
+		}
+	}
+
+	return volumeUsedBy, nil
+}
