259ef51a77103cf36da4bbe6274fa0cda04dd90f
Stéphane Graber <stgraber@ubuntu.com>
2017-02-15 16:02:19 -0500

    api: Use consistent json and yaml field names
    This will allow us to change the struct members as we see fit without
    impacting the output of our commands and on-disk files.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/api/operation.go
+++ shared/api/operation.go
@@ -6,14 +6,14 @@ import (
 
 // Operation represents a LXD background operation
 type Operation struct {
-	ID         string                 `json:"id"`
-	Class      string                 `json:"class"`
-	CreatedAt  time.Time              `json:"created_at"`
-	UpdatedAt  time.Time              `json:"updated_at"`
-	Status     string                 `json:"status"`
-	StatusCode StatusCode             `json:"status_code"`
-	Resources  map[string][]string    `json:"resources"`
-	Metadata   map[string]interface{} `json:"metadata"`
-	MayCancel  bool                   `json:"may_cancel"`
-	Err        string                 `json:"err"`
+	ID         string                 `json:"id" yaml:"id"`
+	Class      string                 `json:"class" yaml:"class"`
+	CreatedAt  time.Time              `json:"created_at" yaml:"created_at"`
+	UpdatedAt  time.Time              `json:"updated_at" yaml:"updated_at"`
+	Status     string                 `json:"status" yaml:"status"`
+	StatusCode StatusCode             `json:"status_code" yaml:"status_code"`
+	Resources  map[string][]string    `json:"resources" yaml:"resources"`
+	Metadata   map[string]interface{} `json:"metadata" yaml:"metadata"`
+	MayCancel  bool                   `json:"may_cancel" yaml:"may_cancel"`
+	Err        string                 `json:"err" yaml:"err"`
 }
