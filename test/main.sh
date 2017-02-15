6843e4cea04f8ac7e53de62d56b2fcb6fcf391ae
Stéphane Graber <stgraber@ubuntu.com>
2017-02-15 18:10:19 -0500

    tests: The monitor can exit on its own
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/main.sh
+++ test/main.sh
@@ -382,7 +382,7 @@ wipe() {
 
   # shellcheck disable=SC2009
   ps aux | grep lxc-monitord | grep "${1}" | awk '{print $2}' | while read -r pid; do
-    kill -9 "${pid}"
+    kill -9 "${pid}" || true
   done
 
   if [ -f "${TEST_DIR}/loops" ]; then
