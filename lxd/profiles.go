654309fd0b9fe0a4bb2a66947a8a16419bfce866
Christian Brauner <christian.brauner@ubuntu.com>
2017-02-16 21:11:06 +0100

    lxd/profiles: verify root disk devices
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/profiles.go
+++ lxd/profiles.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"reflect"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -137,103 +136,19 @@ func getContainersWithProfile(d *Daemon, profile string) []container {
 }
 
 func profilePut(d *Daemon, r *http.Request) Response {
+	// Get the profile
 	name := mux.Vars(r)["name"]
+	id, profile, err := dbProfileGet(d.db, name)
+	if err != nil {
+		return InternalError(fmt.Errorf("Failed to retrieve profile='%s'", name))
+	}
 
 	req := api.ProfilePut{}
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		return BadRequest(err)
 	}
 
-	// Sanity checks
-	err := containerValidConfig(req.Config, true, false)
-	if err != nil {
-		return BadRequest(err)
-	}
-
-	err = containerValidDevices(req.Devices, true, false)
-	if err != nil {
-		return BadRequest(err)
-	}
-
-	// Get the container list
-	containers := getContainersWithProfile(d, name)
-
-	// Update the database
-	id, profile, err := dbProfileGet(d.db, name)
-	if err != nil {
-		return InternalError(fmt.Errorf("Failed to retrieve profile='%s'", name))
-	}
-
-	tx, err := dbBegin(d.db)
-	if err != nil {
-		return InternalError(err)
-	}
-
-	if profile.Description != req.Description {
-		err = dbProfileDescriptionUpdate(tx, id, req.Description)
-		if err != nil {
-			tx.Rollback()
-			return InternalError(err)
-		}
-	}
-
-	// Optimize for description-only changes
-	if reflect.DeepEqual(profile.Config, req.Config) && reflect.DeepEqual(profile.Devices, req.Devices) {
-		err = txCommit(tx)
-		if err != nil {
-			return InternalError(err)
-		}
-
-		return EmptySyncResponse
-	}
-
-	err = dbProfileConfigClear(tx, id)
-	if err != nil {
-		tx.Rollback()
-		return InternalError(err)
-	}
-
-	err = dbProfileConfigAdd(tx, id, req.Config)
-	if err != nil {
-		tx.Rollback()
-		return SmartError(err)
-	}
-
-	err = dbDevicesAdd(tx, "profile", id, req.Devices)
-	if err != nil {
-		tx.Rollback()
-		return SmartError(err)
-	}
-
-	err = txCommit(tx)
-	if err != nil {
-		return InternalError(err)
-	}
-
-	// Update all the containers using the profile. Must be done after txCommit due to DB lock.
-	failures := map[string]error{}
-	for _, c := range containers {
-		err = c.Update(containerArgs{
-			Architecture: c.Architecture(),
-			Ephemeral:    c.IsEphemeral(),
-			Config:       c.LocalConfig(),
-			Devices:      c.LocalDevices(),
-			Profiles:     c.Profiles()}, true)
-
-		if err != nil {
-			failures[c.Name()] = err
-		}
-	}
-
-	if len(failures) != 0 {
-		msg := "The following containers failed to update (profile change still saved):\n"
-		for cname, err := range failures {
-			msg += fmt.Sprintf(" - %s: %s\n", cname, err)
-		}
-		return InternalError(fmt.Errorf("%s", msg))
-	}
-
-	return EmptySyncResponse
+	return doProfileUpdate(d, name, id, profile, req)
 }
 
 // The handler for the post operation.
