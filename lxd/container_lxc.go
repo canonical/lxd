691c079b32be430e19d8b743f0e5fbe6b2732bbd
Stéphane Graber <stgraber@ubuntu.com>
2017-02-23 18:33:47 -0500

    Remove debugging during idmap changes
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/container_lxc.go
+++ lxd/container_lxc.go
@@ -1471,7 +1471,7 @@ func (c *containerLXC) startCommon() (string, error) {
 	}
 
 	if !reflect.DeepEqual(idmap, lastIdmap) {
-		shared.LogDebugf("Container idmap changed, remapping: %s => %s", lastIdmap, idmap)
+		shared.LogDebugf("Container idmap changed, remapping")
 
 		err := c.StorageStart()
 		if err != nil {
