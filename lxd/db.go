c9a8fb0eae9f63c549f72cab581f543414536b32
Stéphane Graber <stgraber@ubuntu.com>
2017-02-28 16:40:54 -0500

    Detect downgrades with newer DB and fail
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/db.go
+++ lxd/db.go
@@ -246,6 +246,11 @@ func initializeDbObject(d *Daemon, path string) (err error) {
 		return fmt.Errorf("Error creating database: %s", err)
 	}
 
+	// Detect LXD downgrades
+	if dbGetSchema(d.db) > dbGetLatestSchema() {
+		return fmt.Errorf("The database schema is more recent than LXD's schema.")
+	}
+
 	// Apply any update
 	err = dbUpdatesApplyAll(d)
 	if err != nil {
