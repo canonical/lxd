20928941e80f27bdca3cc343750c104eaf035dea
Christian Brauner <christian.brauner@ubuntu.com>
2017-03-17 15:58:36 +0100

    api_internal: improve backup handling
    - main_import: use correct POST behavior
      Since we add a new "force" field to our internal container import POST method
      it seems appropriate to switch the whole implementation to proper POST
      behavior.  The whole request is now sent in the body and not as part of the
      query strings.  This change in behavior should be fine since a) it's interal
      api b) we don't expect users to use two different LXD binaries when
      importing.
    - When a user tries to import a container that already has an existing db entry
      either in the containers table or the storage volumes table "force" must be
      set to true in the POST request. In this case any existing db entries will be
      wiped and will be newly created based on the information from the backup.yaml
      file.
    - It can be the case that the snapshots listed in the backup.yaml file do not
      match with the reality where reality means snapshots existing on disk
      discoverable by parsing through
      "${LXD_DIR}"/lxd/storage-pools/"${pool-name}"/snapshots/"${container-Name}".
      I think there are two interesting cases to consider:
      1. Snapshots listed in the backup.yaml file that do not exist on disk anymore.
         This is handled by logging a warning and simply skipping the restore.
      2. Snapshots existing on disk but are not listed in backup.yaml.  This is
         handled by logging a warning and simply recreating these snapshots based
         on the parent container's configuration.
    main: add --force to lxd import
    
    Closes #3005.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/api_internal.go
+++ lxd/api_internal.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -11,6 +14,7 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/lxc/lxd/shared"
+	"github.com/lxc/lxd/shared/api"
 	"github.com/lxc/lxd/shared/osarch"
 
 	log "gopkg.in/inconshreveable/log15.v2"
@@ -107,40 +111,66 @@ func slurpBackupFile(path string) (*backupFile, error) {
 		return nil, err
 	}
 
-	sf := backupFile{}
+	backup := backupFile{}
 
-	if err := yaml.Unmarshal(data, &sf); err != nil {
+	if err := yaml.Unmarshal(data, &backup); err != nil {
 		return nil, err
 	}
 
-	return &sf, nil
+	return &backup, nil
+}
+
+type internalImportPost struct {
+	Name  string `json:"name" yaml:"name"`
+	Force bool   `json:"force" yaml:"force"`
 }
 
 func internalImport(d *Daemon, r *http.Request) Response {
-	name := r.FormValue("target")
-	if name == "" {
-		return BadRequest(fmt.Errorf("target is required"))
+	req := &internalImportPost{}
+	// Parse the request.
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return BadRequest(err)
 	}
 
-	storagePoolNames, err := dbStoragePools(d.db)
+	if req.Name == "" {
+		return BadRequest(fmt.Errorf("The name of the container is required."))
+	}
+
+	storagePoolsPath := shared.VarPath("storage-pools")
+	storagePoolsDir, err := os.Open(storagePoolsPath)
 	if err != nil {
 		return InternalError(err)
 	}
 
+	// Get a list of all storage pools.
+	storagePoolNames, err := storagePoolsDir.Readdirnames(-1)
+	if err != nil {
+		storagePoolsDir.Close()
+		return InternalError(err)
+	}
+	storagePoolsDir.Close()
+
+	// Detect the container's mountpoint.
 	containerMntPoints := []string{}
+	containerPoolName := ""
 	for _, poolName := range storagePoolNames {
-		containerMntPoint := getContainerMountPoint(poolName, name)
+		containerMntPoint := getContainerMountPoint(poolName, req.Name)
 		if shared.PathExists(containerMntPoint) {
 			containerMntPoints = append(containerMntPoints, containerMntPoint)
+			containerPoolName = poolName
 		}
 	}
 
+	// Sanity checks.
 	if len(containerMntPoints) > 1 {
-		return BadRequest(fmt.Errorf("The container \"%s\" seems to exist on another storage pool.", name))
+		return BadRequest(fmt.Errorf("The container \"%s\" seems to exist on another storage pool.", req.Name))
 	} else if len(containerMntPoints) != 1 {
-		return BadRequest(fmt.Errorf("The container \"%s\" does not seem to exist on any storage pool.", name))
+		return BadRequest(fmt.Errorf("The container \"%s\" does not seem to exist on any storage pool.", req.Name))
 	}
 
+	// User needs to make sure that we can access the directory where
+	// backup.yaml lives.
 	containerMntPoint := containerMntPoints[0]
 	isEmpty, err := shared.PathIsEmpty(containerMntPoint)
 	if err != nil {
@@ -151,40 +181,250 @@ func internalImport(d *Daemon, r *http.Request) Response {
 		return BadRequest(fmt.Errorf("The container's directory \"%s\" appears to be empty. Please ensure that the container's storage volume is mounted.", containerMntPoint))
 	}
 
-	sf, err := slurpBackupFile(shared.VarPath("containers", name, "backup.yaml"))
+	// Read in the backup.yaml file.
+	backup, err := slurpBackupFile(shared.VarPath("containers", req.Name, "backup.yaml"))
 	if err != nil {
 		return SmartError(err)
 	}
 
-	baseImage := sf.Container.Config["volatile.base_image"]
-	for k := range sf.Container.Config {
-		if strings.HasPrefix(k, "volatile") {
-			delete(sf.Container.Config, k)
+	// Try to retrieve the storage pool the container supposedly lives on.
+	var poolErr error
+	poolID, pool, poolErr := dbStoragePoolGet(d.db, containerPoolName)
+	if poolErr != nil {
+		if poolErr != NoSuchObjectError {
+			return SmartError(poolErr)
 		}
 	}
 
-	arch, err := osarch.ArchitectureId(sf.Container.Architecture)
+	if backup.Pool == nil {
+		// We don't know what kind of storage type the pool is.
+		return BadRequest(fmt.Errorf("No storage pool struct in the backup file found. The storage pool needs to be recovered manually."))
+	}
+
+	if poolErr == NoSuchObjectError {
+		// Create the storage pool db entry if it doesn't exist.
+		err := storagePoolDBCreate(d, containerPoolName, backup.Pool.Driver, backup.Pool.Config)
+		if err != nil {
+			return InternalError(err)
+		}
+
+		poolID, err = dbStoragePoolGetID(d.db, containerPoolName)
+		if err != nil {
+			return InternalError(err)
+		}
+	} else {
+		if backup.Pool.Name != containerPoolName {
+			return BadRequest(fmt.Errorf("The storage pool \"%s\" the container was detected on does not match the storage pool \"%s\" specified in the backup file.", backup.Pool.Name, containerPoolName))
+		}
+
+		if backup.Pool.Driver != pool.Driver {
+			return BadRequest(fmt.Errorf("The storage pool's \"%s\" driver \"%s\" conflicts with the driver \"%s\" recorded in the container's backup file.", containerPoolName, pool.Driver, backup.Pool.Driver))
+		}
+	}
+
+	// Check if a storage volume entry for the container already exists.
+	_, volume, ctVolErr := dbStoragePoolVolumeGetType(d.db, req.Name, storagePoolVolumeTypeContainer, poolID)
+	if ctVolErr != nil {
+		if ctVolErr != NoSuchObjectError {
+			return InternalError(ctVolErr)
+		}
+	}
+	// If a storage volume entry exists only proceed if force was specified.
+	if ctVolErr == nil && !req.Force {
+		return BadRequest(fmt.Errorf("Storage volume for container \"%s\" already exists in the database. Set \"force\" to overwrite.", req.Name))
+	}
+
+	// Check if an entry for the container already exists in the db.
+	_, containerErr := dbContainerId(d.db, req.Name)
+	if containerErr != nil {
+		if containerErr != sql.ErrNoRows {
+			return InternalError(containerErr)
+		}
+	}
+	// If a db entry exists only proceed if force was specified.
+	if containerErr == nil && !req.Force {
+		return BadRequest(fmt.Errorf("Entry for container \"%s\" already exists in the database. Set \"force\" to overwrite.", req.Name))
+	}
+
+	// Detect discrepancy between snapshots recorded in "backup.yaml" and
+	// those actually existing on disk.
+	snapshotsPath := getSnapshotMountPoint(containerPoolName, req.Name)
+	snapshotsDir, err := os.Open(snapshotsPath)
+	if err != nil {
+		return InternalError(err)
+	}
+
+	// Get a list of all snapshots that exist on disk.
+	snapshotNames, err := snapshotsDir.Readdirnames(-1)
+	if err != nil {
+		snapshotsDir.Close()
+		return InternalError(err)
+	}
+	snapshotsDir.Close()
+
+	onDiskSnapshots := map[string]*api.ContainerSnapshot{}
+	for _, snapName := range snapshotNames {
+		fullSnapName := fmt.Sprintf("%s/%s", req.Name, snapName)
+		snapshotMntPoint := getSnapshotMountPoint(containerPoolName, fullSnapName)
+		if shared.PathExists(snapshotMntPoint) {
+			onDiskSnapshots[fullSnapName] = nil
+		}
+	}
+
+	// Pre-check snapshots to ensure that nothings gets messed up because
+	// "force" is missing.
+	for _, snap := range backup.Snapshots {
+		// Kick out any snapshots that do not exist on-disk anymore.
+		_, ok := onDiskSnapshots[snap.Name]
+		if !ok {
+			shared.LogWarnf("The snapshot \"%s\" for container \"%s\" does not exist on disk anymore. Skipping...", snap.Name, req.Name)
+			continue
+		}
+
+		onDiskSnapshots[snap.Name] = snap
+
+		// Check if an entry for the snapshot already exists in the db.
+		_, snapErr := dbContainerId(d.db, snap.Name)
+		if snapErr != nil {
+			if snapErr != sql.ErrNoRows {
+				return InternalError(snapErr)
+			}
+		}
+
+		// If a db entry exists only proceed if force was specified.
+		if snapErr == nil && !req.Force {
+			return BadRequest(fmt.Errorf("Entry for snapshot \"%s\" already exists in the database. Set \"force\" to overwrite.", snap.Name))
+		}
+
+		// Check if a storage volume entry for the snapshot already exists.
+		_, _, snapVolErr := dbStoragePoolVolumeGetType(d.db, snap.Name, storagePoolVolumeTypeContainer, poolID)
+		if snapVolErr != nil {
+			if snapVolErr != NoSuchObjectError {
+				return InternalError(snapVolErr)
+			}
+		}
+
+		// If a storage volume entry exists only proceed if force was specified.
+		if snapVolErr == nil && !req.Force {
+			return BadRequest(fmt.Errorf("Storage volume for snapshot \"%s\" already exists in the database. Set \"force\" to overwrite.", snap.Name))
+		}
+	}
+
+	if backup.Volume == nil {
+		return BadRequest(fmt.Errorf("No storage volume struct in the backup file found. The storage volume needs to be recovered manually."))
+	}
+
+	if ctVolErr == nil {
+		if volume.Name != backup.Volume.Name {
+			return BadRequest(fmt.Errorf("The name \"%s\" of the storage volume is not identical to the container's name \"%s\".", volume.Name, req.Name))
+		}
+
+		if volume.Type != backup.Volume.Type {
+			return BadRequest(fmt.Errorf("The type \"%s\" of the storage volume is not identical to the container's type \"%s\".", volume.Type, backup.Volume.Type))
+		}
+
+		// Remove the storage volume db entry for the container since
+		// force was specified.
+		err := dbStoragePoolVolumeDelete(d.db, req.Name, storagePoolVolumeTypeContainer, poolID)
+		if err != nil {
+			return SmartError(err)
+		}
+	}
+
+	if containerErr == nil {
+		// Remove the storage volume db entry for the container since
+		// force was specified.
+		err := dbContainerRemove(d.db, req.Name)
+		if err != nil {
+			return InternalError(err)
+		}
+	}
+
+	baseImage := backup.Container.Config["volatile.base_image"]
+	for k := range backup.Container.Config {
+		if strings.HasPrefix(k, "volatile") {
+			delete(backup.Container.Config, k)
+		}
+	}
+
+	arch, err := osarch.ArchitectureId(backup.Container.Architecture)
 	if err != nil {
 		return SmartError(err)
 	}
 	_, err = containerCreateInternal(d, containerArgs{
 		Architecture: arch,
 		BaseImage:    baseImage,
-		Config:       sf.Container.Config,
-		CreationDate: sf.Container.CreatedAt,
-		LastUsedDate: sf.Container.LastUsedAt,
+		Config:       backup.Container.Config,
+		CreationDate: backup.Container.CreatedAt,
+		LastUsedDate: backup.Container.LastUsedAt,
 		Ctype:        cTypeRegular,
-		Devices:      sf.Container.Devices,
-		Ephemeral:    sf.Container.Ephemeral,
-		Name:         sf.Container.Name,
-		Profiles:     sf.Container.Profiles,
-		Stateful:     sf.Container.Stateful,
+		Devices:      backup.Container.Devices,
+		Ephemeral:    backup.Container.Ephemeral,
+		Name:         backup.Container.Name,
+		Profiles:     backup.Container.Profiles,
+		Stateful:     backup.Container.Stateful,
 	})
 	if err != nil {
 		return SmartError(err)
 	}
 
-	for _, snap := range sf.Snapshots {
+	for snapName, snap := range onDiskSnapshots {
+		// Check if an entry for the snapshot already exists in the db.
+		_, snapErr := dbContainerId(d.db, snapName)
+		if snapErr != nil {
+			if snapErr != sql.ErrNoRows {
+				return InternalError(snapErr)
+			}
+		}
+
+		// If a db entry exists only proceed if force was specified.
+		if snapErr == nil && !req.Force {
+			return BadRequest(fmt.Errorf("Entry for snapshot \"%s\" already exists in the database. Set \"force\" to overwrite.", snapName))
+		}
+
+		// Check if a storage volume entry for the snapshot already exists.
+		_, _, csVolErr := dbStoragePoolVolumeGetType(d.db, snapName, storagePoolVolumeTypeContainer, poolID)
+		if csVolErr != nil {
+			if csVolErr != NoSuchObjectError {
+				return InternalError(csVolErr)
+			}
+		}
+
+		// If a storage volume entry exists only proceed if force was specified.
+		if csVolErr == nil && !req.Force {
+			return BadRequest(fmt.Errorf("Storage volume for snapshot \"%s\" already exists in the database. Set \"force\" to overwrite.", snapName))
+		}
+
+		if snapErr == nil {
+			err := dbContainerRemove(d.db, snapName)
+			if err != nil {
+				return InternalError(err)
+			}
+		}
+
+		if csVolErr == nil {
+			err := dbStoragePoolVolumeDelete(d.db, snapName, storagePoolVolumeTypeContainer, poolID)
+			if err != nil {
+				return SmartError(err)
+			}
+		}
+
+		// Snapshot exists on disk but does not have an entry in the
+		// "backup.yaml" file. Recreate it by copying the parent
+		// container's settings.
+		if snap == nil {
+			shared.LogWarnf("The snapshot \"%s\" for the container \"%s\" exists on disk but not in the backup file. Restoring with parent container's settings.", snapName, req.Name)
+			snap = &api.ContainerSnapshot{}
+			snap.Config = backup.Container.Config
+			snap.CreationDate = backup.Container.CreatedAt
+			snap.LastUsedDate = backup.Container.LastUsedAt
+			snap.Devices = backup.Container.Devices
+			snap.Ephemeral = backup.Container.Ephemeral
+			snap.Profiles = backup.Container.Profiles
+			snap.Stateful = backup.Container.Stateful
+			snap.Architecture = backup.Container.Architecture
+		}
+
 		baseImage := snap.Config["volatile.base_image"]
 		for k := range snap.Config {
 			if strings.HasPrefix(k, "volatile") {
@@ -206,7 +446,7 @@ func internalImport(d *Daemon, r *http.Request) Response {
 			Ctype:        cTypeSnapshot,
 			Devices:      snap.Devices,
 			Ephemeral:    snap.Ephemeral,
-			Name:         snap.Name,
+			Name:         snapName,
 			Profiles:     snap.Profiles,
 			Stateful:     snap.Stateful,
 		})
