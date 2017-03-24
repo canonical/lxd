2a4dcb5ead5720e36c71326f51817218eea8663c
Stéphane Graber <stgraber@ubuntu.com>
2017-03-24 15:54:30 -0400

    Fix soft limit logic to use float64
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/container_lxc.go
+++ lxd/container_lxc.go
@@ -1018,7 +1018,7 @@ func (c *containerLXC) initLXC() error {
 					}
 				}
 				// Set soft limit to value 10% less than hard limit
-				err = lxcSetConfigItem(cc, "lxc.cgroup.memory.soft_limit_in_bytes", fmt.Sprintf("%d", valueInt*0.9))
+				err = lxcSetConfigItem(cc, "lxc.cgroup.memory.soft_limit_in_bytes", fmt.Sprintf("%.0f", float64(valueInt)*0.9))
 				if err != nil {
 					return err
 				}
@@ -3006,8 +3006,15 @@ func (c *containerLXC) Update(args containerArgs, userRequested bool) error {
 							return err
 						}
 					}
+
 					// Set soft limit to value 10% less than hard limit
-					err = c.CGroupSet("memory.soft_limit_in_bytes", memory*0.9)
+					valueInt, err := strconv.ParseInt(memory, 10, 64)
+					if err != nil {
+						revertMemory()
+						return err
+					}
+
+					err = c.CGroupSet("memory.soft_limit_in_bytes", fmt.Sprintf("%.0f", float64(valueInt)*0.9))
 					if err != nil {
 						revertMemory()
 						return err
