4e62c2eadaa645d71c75b0c1747c345c0a06b93a
Alberto Donato <alberto.donato@canonical.com>
2017-04-19 12:27:07 +0200

    Use flake8 instead of separate pyflakes and pep8
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

--- test/suites/static_analysis.sh
+++ test/suites/static_analysis.sh
@@ -10,12 +10,19 @@ test_static_analysis() {
 
     cd ../
     # Python3 static analysis
-    pep8 test/deps/import-busybox
-    pyflakes3 test/deps/import-busybox
+    if which flake8 >/dev/null 2>&1; then
+      flake8 test/deps/import-busybox
+    else
+      echo "flake8 not found, python static analysis disabled"
+    fi  
 
     # Shell static analysis
-    shellcheck test/main.sh test/suites/* test/backends/*
-
+    if which shellcheck >/dev/null 2>&1; then
+      shellcheck test/main.sh test/suites/* test/backends/*
+    else
+      echo "shellcheck not found, shell static analysis disabled"
+    fi  
+        
     # Go static analysis
     ## Functions starting by empty line
     OUT=$(grep -r "^$" -B1 . | grep "func " | grep -v "}$" || true)
