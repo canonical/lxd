45fe6a55e0093bc7d44047fea878dfa44d2e6056
Christian Brauner <christian.brauner@ubuntu.com>
2017-04-20 11:45:00 +0200

    util_linux: add function to detect errno
    Closes #2494.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- shared/util_linux.go
+++ shared/util_linux.go
@@ -702,3 +702,23 @@ func ExecReaderToChannel(r io.Reader, bufferSize int, exited <-chan bool, fd int
 
 	return ch
 }
+
+// Detect whether err is an errno.
+func GetErrno(err error) (errno error, iserrno bool) {
+	sysErr, ok := err.(*os.SyscallError)
+	if ok {
+		return sysErr.Err, true
+	}
+
+	pathErr, ok := err.(*os.PathError)
+	if ok {
+		return pathErr.Err, true
+	}
+
+	tmpErrno, ok := err.(syscall.Errno)
+	if ok {
+		return tmpErrno, true
+	}
+
+	return nil, false
+}
