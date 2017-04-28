e2a6411f0e274fe1fcdbc0a844fb4617e7eb21a7
Stéphane Graber <stgraber@ubuntu.com>
2017-04-28 13:18:15 -0400

    shared/termios: Make golint clean
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/termios/termios_windows.go
+++ shared/termios/termios_windows.go
@@ -6,12 +6,15 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+// State contains the state of a terminal.
 type State terminal.State
 
+// IsTerminal returns true if the given file descriptor is a terminal.
 func IsTerminal(fd int) bool {
 	return terminal.IsTerminal(fd)
 }
 
+// GetState returns the current state of a terminal which may be useful to restore the terminal after a signal.
 func GetState(fd int) (*State, error) {
 	state, err := terminal.GetState(fd)
 	if err != nil {
@@ -22,10 +25,12 @@ func GetState(fd int) (*State, error) {
 	return &currentState, nil
 }
 
+// GetSize returns the dimensions of the given terminal.
 func GetSize(fd int) (int, int, error) {
 	return terminal.GetSize(fd)
 }
 
+// MakeRaw put the terminal connected to the given file descriptor into raw mode and returns the previous state of the terminal so that it can be restored.
 func MakeRaw(fd int) (*State, error) {
 	state, err := terminal.MakeRaw(fd)
 	if err != nil {
@@ -36,6 +41,7 @@ func MakeRaw(fd int) (*State, error) {
 	return &oldState, nil
 }
 
+// Restore restores the terminal connected to the given file descriptor to a previous state.
 func Restore(fd int, state *State) error {
 	newState := terminal.State(*state)
 
