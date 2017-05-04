ac1446c72841fc886e79d1ca6db95ca49da539e8
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-05-04 17:54:19 +0200

    Consolidate interactive/auto init logic with the preseed one
    Change initAutoOrInteractive to use the same code path that
    initPreseeds uses to create or update the entities using the API.
    
    This is done by making initAutoOrInteractive fill a cmdInitData
    structure that has the same schema as the preseed YAML.
    
    The resulting code is slightly shorter and less redundant.
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

---
+++ test/suites/init_interactive.sh
@@ -0,0 +1,41 @@
+test_init_interactive() {
+  # - lxd init
+  LXD_INIT_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
+  chmod +x "${LXD_INIT_DIR}"
+  spawn_lxd "${LXD_INIT_DIR}" false
+
+  (
+    set -e
+    # shellcheck disable=SC2034
+    LXD_DIR=${LXD_INIT_DIR}
+
+    # XXX We need to remove the eth0 device from the default profile, which
+    #     is typically attached by spawn_lxd.
+    if lxc profile show default | grep -q eth0; then
+      lxc network detach-profile lxdbr0 default eth0
+    fi
+
+    cat <<EOF | lxd init
+yes
+my-storage-pool
+dir
+no
+no
+yes
+lxdt$$
+auto
+none
+EOF
+
+    lxc info | grep -q 'images.auto_update_interval: "0"'
+    lxc network list | grep -q "lxdt$$"
+    lxc storage list | grep -q "my-storage-pool"
+    lxc profile show default | grep -q "pool: my-storage-pool"
+    lxc profile show default | grep -q "parent: lxdt$$"
+    lxc profile delete default
+    lxc network delete lxdt$$
+  )
+  kill_lxd "${LXD_INIT_DIR}"
+
+  return
+}
