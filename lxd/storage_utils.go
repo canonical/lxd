7e42a94e4e105c234c61a6c88eed99ec4b5a2b29
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-05 01:23:37 +0200

    storage: add lxdResolveMountoptions()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/storage_utils.go
+++ lxd/storage_utils.go
@@ -11,37 +11,62 @@ import (
 // Export the mount options map since we might find it useful in other parts of
 // LXD.
 type mountOptions struct {
-	unset bool
-	flag  uintptr
+	capture bool
+	flag    uintptr
 }
 
 var MountOptions = map[string]mountOptions{
-	"async":         {true, syscall.MS_SYNCHRONOUS},
-	"atime":         {true, syscall.MS_NOATIME},
-	"bind":          {false, syscall.MS_BIND},
-	"defaults":      {false, 0},
-	"dev":           {true, syscall.MS_NODEV},
-	"diratime":      {true, syscall.MS_NODIRATIME},
-	"dirsync":       {false, syscall.MS_DIRSYNC},
-	"exec":          {true, syscall.MS_NOEXEC},
-	"lazytime":      {false, MS_LAZYTIME},
-	"mand":          {false, syscall.MS_MANDLOCK},
-	"noatime":       {false, syscall.MS_NOATIME},
-	"nodev":         {false, syscall.MS_NODEV},
-	"nodiratime":    {false, syscall.MS_NODIRATIME},
-	"noexec":        {false, syscall.MS_NOEXEC},
-	"nomand":        {true, syscall.MS_MANDLOCK},
-	"norelatime":    {true, syscall.MS_RELATIME},
-	"nostrictatime": {true, syscall.MS_STRICTATIME},
-	"nosuid":        {false, syscall.MS_NOSUID},
-	"rbind":         {false, syscall.MS_BIND | syscall.MS_REC},
-	"relatime":      {false, syscall.MS_RELATIME},
-	"remount":       {false, syscall.MS_REMOUNT},
-	"ro":            {false, syscall.MS_RDONLY},
-	"rw":            {true, syscall.MS_RDONLY},
-	"strictatime":   {false, syscall.MS_STRICTATIME},
-	"suid":          {true, syscall.MS_NOSUID},
-	"sync":          {false, syscall.MS_SYNCHRONOUS},
+	"async":         {false, syscall.MS_SYNCHRONOUS},
+	"atime":         {false, syscall.MS_NOATIME},
+	"bind":          {true, syscall.MS_BIND},
+	"defaults":      {true, 0},
+	"dev":           {false, syscall.MS_NODEV},
+	"diratime":      {false, syscall.MS_NODIRATIME},
+	"dirsync":       {true, syscall.MS_DIRSYNC},
+	"exec":          {false, syscall.MS_NOEXEC},
+	"lazytime":      {true, MS_LAZYTIME},
+	"mand":          {true, syscall.MS_MANDLOCK},
+	"noatime":       {true, syscall.MS_NOATIME},
+	"nodev":         {true, syscall.MS_NODEV},
+	"nodiratime":    {true, syscall.MS_NODIRATIME},
+	"noexec":        {true, syscall.MS_NOEXEC},
+	"nomand":        {false, syscall.MS_MANDLOCK},
+	"norelatime":    {false, syscall.MS_RELATIME},
+	"nostrictatime": {false, syscall.MS_STRICTATIME},
+	"nosuid":        {true, syscall.MS_NOSUID},
+	"rbind":         {true, syscall.MS_BIND | syscall.MS_REC},
+	"relatime":      {true, syscall.MS_RELATIME},
+	"remount":       {true, syscall.MS_REMOUNT},
+	"ro":            {true, syscall.MS_RDONLY},
+	"rw":            {false, syscall.MS_RDONLY},
+	"strictatime":   {true, syscall.MS_STRICTATIME},
+	"suid":          {false, syscall.MS_NOSUID},
+	"sync":          {true, syscall.MS_SYNCHRONOUS},
+}
+
+func lxdResolveMountoptions(options string) (uintptr, string) {
+	mountFlags := uintptr(0)
+	tmp := strings.SplitN(options, ",", -1)
+	for i := 0; i < len(tmp); i++ {
+		opt := tmp[i]
+		do, ok := MountOptions[opt]
+		if !ok {
+			continue
+		}
+
+		if do.capture {
+			mountFlags |= do.flag
+		} else {
+			mountFlags &= ^do.flag
+		}
+
+		copy(tmp[i:], tmp[i+1:])
+		tmp[len(tmp)-1] = ""
+		tmp = tmp[:len(tmp)-1]
+		i--
+	}
+
+	return mountFlags, strings.Join(tmp, ",")
 }
 
 // Useful functions for unreliable backends
