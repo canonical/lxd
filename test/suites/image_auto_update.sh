cb0e27f90fb11e1b47bf54adfbe3e8363d1a42e7
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-05-15 08:37:03 +0200

    Fix regression in image auto-update logic
    From what I understand, it seems that this commit:
    
    https://github.com/lxc/lxd/commit/bbeae91ab4c219ee0f9a1bbda521f3442f71d700
    
    introduced a regression in the auto-update logic. The code that was
    setting the AutoUpdate flag in the *api.Image structure in line 541:
    
    https://github.com/lxc/lxd/commit/bbeae91ab4c219ee0f9a1bbda521f3442f71d700#diff-6182827642cd00e061c6ffe52891acc5L541
    
    was moved down in the function:
    
    https://github.com/lxc/lxd/commit/bbeae91ab4c219ee0f9a1bbda521f3442f71d700#diff-6182827642cd00e061c6ffe52891acc5R482
    
    however the new location is after the call to dbImageInsert:
    
    ```
    	err = dbImageInsert(d.db, info.Fingerprint, info.Filename, info.Size, info.Public, info.AutoUpdate, info.Architecture, info.CreatedAt, info.ExpiresAt, info.Properties)
    ```
    
    that actually makes use of such setting. This results in the
    auto_update column in the database always being 0, no matter what, and
    the server logic to auto-update images will never kick in.
    
    This commit should also fix another (non fatal) regression introduced
    by:
    
    https://github.com/lxc/lxd/commit/b6496584e71a6ff337e84530d3253203ff95a169
    
    where appending the empty string "" to the poolNames to be processed
    ends up triggering a call to ```doDeleteImageFromPool()``` from
    ``autoUpdateImage``:
    
    https://github.com/lxc/lxd/blob/34acd41dea1268712dac04b8ad9cfa92b2d7905e/lxd/images.go#L975
    
    which as far as I understand will always end up with an error in case
    poolName is the empty string, so a spurious error will be logged.
    
    Note that auto-update regression affects the latest 2.13 release, but
    not 2.12.
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

---
+++ test/suites/image_auto_update.sh
@@ -0,0 +1,72 @@
+test_image_auto_update() {
+  # XXX this test appears to be flaky when running on Jenkins
+  # against the LVM backend. Needs further investigation.
+  # shellcheck disable=2153
+  backend=$(storage_backend "$LXD_DIR")
+  if [ "${backend}" = "lvm" ]; then
+      return 0
+  fi
+
+  if lxc image alias list | grep -q "^| testimage\s*|.*$"; then
+      lxc image delete testimage
+  fi
+
+  # shellcheck disable=2039
+  local LXD2_DIR LXD2_ADDR
+  LXD2_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
+  chmod +x "${LXD2_DIR}"
+  spawn_lxd "${LXD2_DIR}" true
+  LXD2_ADDR=$(cat "${LXD2_DIR}/lxd.addr")
+
+  (LXD_DIR=${LXD2_DIR} deps/import-busybox --alias testimage --public)
+  fp1=$(LXD_DIR=${LXD2_DIR} lxc image info testimage | awk -F: '/^Fingerprint/ { print $2 }' | awk '{ print $1 }')
+
+  lxc remote add l2 "${LXD2_ADDR}" --accept-certificate --password foo
+  lxc init l2:testimage c1
+
+  # Now the first image image is in the local store, since it was
+  # downloaded to create c1.
+  alias=$(lxc image info "${fp1}" | awk -F: '/^    Alias/ { print $2 }' | awk '{ print $1 }')
+  [ "${alias}" = "testimage" ]
+
+  # Delete the first image from the remote store and replace it with a
+  # new one with a different fingerprint (passing "--template create"
+  # will do that).
+  (LXD_DIR=${LXD2_DIR} lxc image delete testimage)
+  (LXD_DIR=${LXD2_DIR} deps/import-busybox --alias testimage --public --template create)
+  fp2=$(LXD_DIR=${LXD2_DIR} lxc image info testimage | awk -F: '/^Fingerprint/ { print $2 }' | awk '{ print $1 }')
+  [ "${fp1}" != "${fp2}" ]
+
+  # Restart the server to force an image refresh immediately
+  # shellcheck disable=2153
+  shutdown_lxd "${LXD_DIR}"
+  respawn_lxd "${LXD_DIR}"
+
+  # Check that the first image got deleted from the local storage
+  #
+  # XXX: Since the auto-update logic runs asynchronously we need to wait
+  #      a little bit before it actually completes.
+  retries=10
+  while [ "${retries}" != "0" ]; do
+    if lxc image info "${fp1}" > /dev/null 2>&1; then
+	sleep 2
+	retries=$((retries-1))
+	continue
+    fi
+    break
+  done
+
+  if [ "${retries}" -eq 0 ]; then
+      echo "First image ${fp1} not deleted from local store"
+      return 1
+  fi
+
+  # The second image replaced the first one in the local storage.
+  alias=$(lxc image info "${fp2}" | awk -F: '/^    Alias/ { print $2 }' | awk '{ print $1 }')
+  [ "${alias}" = "testimage" ]
+
+  lxc delete c1
+  lxc remote remove l2
+  lxc image delete "${fp2}"
+  kill_lxd "$LXD2_DIR"
+}
