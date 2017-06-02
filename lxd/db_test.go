755bc74f1d70e063bfdbf29235bb0ae9415549f9
Stéphane Graber <stgraber@ubuntu.com>
2017-06-02 15:11:24 -0400

    images: Skip cached images without auto-update
    This changes dbImageSourceGetCachedFingerprint to only consider images
    that match the source AND that are configured to auto-update.
    
    Without this, LXD may pick a very seriously outdated image rather than
    one which got updated over the past X hours (6 by default).
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/db_test.go
+++ lxd/db_test.go
@@ -21,7 +21,7 @@ const DB_FIXTURES string = `
     INSERT INTO containers_config (container_id, key, value) VALUES (1, 'thekey', 'thevalue');
     INSERT INTO containers_devices (container_id, name, type) VALUES (1, 'somename', 1);
     INSERT INTO containers_devices_config (key, value, container_device_id) VALUES ('configkey', 'configvalue', 1);
-    INSERT INTO images (fingerprint, filename, size, architecture, creation_date, expiry_date, upload_date) VALUES ('fingerprint', 'filename', 1024, 0,  1431547174,  1431547175,  1431547176);
+    INSERT INTO images (fingerprint, filename, size, architecture, creation_date, expiry_date, upload_date, auto_update) VALUES ('fingerprint', 'filename', 1024, 0,  1431547174,  1431547175,  1431547176, 1);
     INSERT INTO images_aliases (name, image_id, description) VALUES ('somealias', 1, 'some description');
     INSERT INTO images_properties (image_id, type, key, value) VALUES (1, 0, 'thekey', 'some value');
     INSERT INTO profiles_config (profile_id, key, value) VALUES (2, 'thekey', 'thevalue');
