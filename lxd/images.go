449c46ad0764e0cb0dadd68d19a521d3122ca503
Stéphane Graber <stgraber@ubuntu.com>
2017-06-26 18:57:05 -0400

    lxd/images: Always expand fingerprint
    This fixes an issue where partial fingerprints weren't expanded for
    private image copies. LXD will now allow querying the image with a
    partial fingerprint using the secret string, LXD will then use that data
    to get the full fingerprint and proceeed with the download using that.
    
    Closes #3424
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/images.go
+++ lxd/images.go
@@ -1038,13 +1038,13 @@ func imageGet(d *Daemon, r *http.Request) Response {
 	public := !d.isTrustedClient(r)
 	secret := r.FormValue("secret")
 
-	if public == true && imageValidSecret(fingerprint, secret) == true {
-		public = false
-	}
-
-	info, response := doImageGet(d, fingerprint, public)
+	info, response := doImageGet(d, fingerprint, false)
 	if response != nil {
 		return response
+	}
+
+	if !info.Public && public && !imageValidSecret(info.Fingerprint, secret) {
+		return NotFound
 	}
 
 	return SyncResponse(true, info)
@@ -1226,13 +1226,13 @@ func imageExport(d *Daemon, r *http.Request) Response {
 	public := !d.isTrustedClient(r)
 	secret := r.FormValue("secret")
 
-	if public == true && imageValidSecret(fingerprint, secret) == true {
-		public = false
-	}
-
-	_, imgInfo, err := dbImageGet(d.db, fingerprint, public, false)
+	_, imgInfo, err := dbImageGet(d.db, fingerprint, false, false)
 	if err != nil {
 		return SmartError(err)
+	}
+
+	if !imgInfo.Public && public && !imageValidSecret(imgInfo.Fingerprint, secret) {
+		return NotFound
 	}
 
 	imagePath := shared.VarPath("images", imgInfo.Fingerprint)
@@ -1276,7 +1276,7 @@ func imageExport(d *Daemon, r *http.Request) Response {
 
 func imageSecret(d *Daemon, r *http.Request) Response {
 	fingerprint := mux.Vars(r)["fingerprint"]
-	_, _, err := dbImageGet(d.db, fingerprint, false, false)
+	_, imgInfo, err := dbImageGet(d.db, fingerprint, false, false)
 	if err != nil {
 		return SmartError(err)
 	}
@@ -1291,7 +1291,7 @@ func imageSecret(d *Daemon, r *http.Request) Response {
 	meta["secret"] = secret
 
 	resources := map[string][]string{}
-	resources["images"] = []string{fingerprint}
+	resources["images"] = []string{imgInfo.Fingerprint}
 
 	op, err := operationCreate(operationClassToken, resources, meta, nil, nil, nil)
 	if err != nil {
