87a423683e9f4230dba82392f547f0466b2e7121
Stéphane Graber <stgraber@ubuntu.com>
2017-07-06 18:15:29 -0400

    tests: Validate that the right busybox is present
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/main.sh
+++ test/main.sh
@@ -236,6 +236,16 @@ ensure_import_testimage() {
     if [ -e "${LXD_TEST_IMAGE:-}" ]; then
       lxc image import "${LXD_TEST_IMAGE}" --alias testimage
     else
+      if [ ! -e "/bin/busybox" ]; then
+        echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
+        exit 1
+      fi
+
+      if ldd /bin/busybox >/dev/null 2>&1; then
+        echo "The testsuite requires /bin/busybox to be a static binary"
+        exit 1
+      fi
+
       deps/import-busybox --alias testimage
     fi
   fi
