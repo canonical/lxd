d63c32acafe1480439036de5c08c116646c44f8c
Stéphane Graber <stgraber@ubuntu.com>
2017-07-10 13:09:51 -0400

    zfs: Set canmount=noauto on all mountable datasets
    We don't want the container host to randomly mount stuff behind our back.
    
    Closes #3437
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/migration.sh
+++ test/suites/migration.sh
@@ -74,7 +74,7 @@ migration() {
   # perform existence check for various files.
   lxc_remote start l2:nonlive
   # FIXME: make this backend agnostic
-  if [ "$lxd2_backend" != "lvm" ]; then
+  if [ "$lxd2_backend" != "lvm" ] && [ "$lxd2_backend" != "zfs" ]; then
     [ -d "${lxd2_dir}/containers/nonlive/rootfs" ]
   fi
   lxc_remote stop l2:nonlive
@@ -91,7 +91,7 @@ migration() {
   lxc_remote start l2:nonlive
   [ -d "${LXD_DIR}/containers/nonlive2" ]
   # FIXME: make this backend agnostic
-  if [ "$lxd2_backend" != "lvm" ]; then
+  if [ "$lxd2_backend" != "lvm" ] && [ "$lxd2_backend" != "zfs" ]; then
     [ -d "${lxd2_dir}/containers/nonlive/rootfs/bin" ]
   fi
 
@@ -102,7 +102,7 @@ migration() {
 
   lxc_remote copy l1:nonlive2/snap0 l2:nonlive3 --mode=relay
   # FIXME: make this backend agnostic
-  if [ "$lxd2_backend" != "lvm" ]; then
+  if [ "$lxd2_backend" != "lvm" ] && [ "$lxd2_backend" != "zfs" ]; then
     [ -d "${lxd2_dir}/containers/nonlive3/rootfs/bin" ]
   fi
   lxc_remote delete l2:nonlive3 --force
