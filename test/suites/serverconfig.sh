bc9bb1027d2b357565f6290ab6e66feaef3d389b
Alberto Donato <alberto.donato@canonical.com>
2017-07-19 14:58:20 +0200

    server: support for macaroons-based authentication
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

--- test/suites/serverconfig.sh
+++ test/suites/serverconfig.sh
@@ -14,4 +14,13 @@ test_server_config() {
 
   # test untrusted server GET
   my_curl -X GET "https://$(cat "${LXD_SERVERCONFIG_DIR}/lxd.addr")/1.0" | grep -v -q environment
+
+  # test authentication type
+  curl --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0" | jq .metadata.auth_methods | grep tls
+  # only tls is enabled by default
+  ! curl --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0" | jq .metadata.auth_methods | grep macaroons
+  lxc config set core.macaroon.endpoint "https://localhost:8081"
+  # macaroons are also enabled
+  curl --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0" | jq .metadata.auth_methods | grep macaroons
+  lxc config unset core.macaroon.endpoint
 }
