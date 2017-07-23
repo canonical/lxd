35ee24a138ba146fc5e67bb5869e65759abfd985
Christian Brauner <christian.brauner@ubuntu.com>
2017-07-23 16:38:32 +0200

    test: ceph storage driver
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

---
+++ test/backends/ceph.sh
@@ -0,0 +1,29 @@
+ceph_setup() {
+  # shellcheck disable=2039
+  local LXD_DIR
+
+  LXD_DIR=$1
+
+  echo "==> Setting up CEPH backend in ${LXD_DIR}"
+}
+
+ceph_configure() {
+  # shellcheck disable=2039
+  local LXD_DIR
+
+  LXD_DIR=$1
+
+  echo "==> Configuring CEPH backend in ${LXD_DIR}"
+
+  lxc storage create "lxdtest-$(basename "${LXD_DIR}")" ceph
+  lxc profile device add default root disk path="/" pool="lxdtest-$(basename "${LXD_DIR}")"
+}
+
+ceph_teardown() {
+  # shellcheck disable=2039
+  local LXD_DIR
+
+  LXD_DIR=$1
+
+  echo "==> Tearing down CEPH backend in ${LXD_DIR}"
+}
