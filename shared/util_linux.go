555e6a7e59e6a842e4e897446cf258adacdd9d62
Alberto Donato <alberto.donato@canonical.com>
2017-08-02 11:03:04 +0200

    shift xattr ACLs uid/gid
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

--- shared/util_linux.go
+++ shared/util_linux.go
@@ -19,7 +19,7 @@ import (
 	"github.com/lxc/lxd/shared/logger"
 )
 
-// #cgo LDFLAGS: -lutil -lpthread
+// #cgo LDFLAGS: -lutil -lpthread -lacl
 /*
 #define _GNU_SOURCE
 #include <errno.h>
@@ -36,6 +36,7 @@ import (
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/un.h>
+#include <sys/acl.h>
 
 #ifndef AT_SYMLINK_FOLLOW
 #define AT_SYMLINK_FOLLOW    0x400
@@ -248,6 +249,69 @@ func ShiftOwner(basepath string, path string, uid int, gid int) error {
 	r := C.shiftowner(cbasepath, cpath, C.int(uid), C.int(gid))
 	if r != 0 {
 		return fmt.Errorf("Failed to change ownership of: %s", path)
+	}
+	return nil
+}
+
+func ShiftACL(path string, shiftIds func(uid int64, gid int64) (int64, int64)) error {
+	finfo, err := os.Lstat(path)
+	if err != nil {
+		return err
+	}
+	if finfo.Mode()&os.ModeSymlink != 0 {
+		return nil
+	}
+
+	cpath := C.CString(path)
+	defer C.free(unsafe.Pointer(cpath))
+
+	acl := C.acl_get_file(cpath, C.ACL_TYPE_ACCESS)
+	if acl == nil {
+		return nil
+	}
+	defer C.acl_free(unsafe.Pointer(acl))
+
+	for entryId := C.ACL_FIRST_ENTRY; ; entryId = C.ACL_NEXT_ENTRY {
+		var ent C.acl_entry_t
+		var tag C.acl_tag_t
+		updateACL := false
+
+		ret := C.acl_get_entry(acl, C.int(entryId), &ent)
+		if ret != 1 {
+			break
+		}
+
+		ret = C.acl_get_tag_type(ent, &tag)
+		if ret == -1 {
+			return fmt.Errorf("Failed to change ACLs on %s", path)
+		}
+
+		idp := (*C.id_t)(C.acl_get_qualifier(ent))
+		if idp == nil {
+			continue
+		}
+
+		var newId int64
+		switch tag {
+		case C.ACL_USER:
+			newId, _ = shiftIds((int64)(*idp), -1)
+			updateACL = true
+
+		case C.ACL_GROUP:
+			_, newId = shiftIds(-1, (int64)(*idp))
+			updateACL = true
+		}
+
+		if updateACL {
+			ret = C.acl_set_qualifier(ent, unsafe.Pointer(&newId))
+			if ret == -1 {
+				return fmt.Errorf("Failed to change ACLs on %s", path)
+			}
+			ret = C.acl_set_file(cpath, C.ACL_TYPE_ACCESS, acl)
+			if ret == -1 {
+				return fmt.Errorf("Failed to change ACLs on %s", path)
+			}
+		}
 	}
 	return nil
 }
