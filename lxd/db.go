4f4cd90f07f1cadd98484811494b0ae323c60cb5
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-08-16 11:36:03 +0000

    Drop dependencies on Daemon in db.go
    This branch drops any mention of the Daemon structure in db.go, and is
    a step towards making the db functionality self-contained (in order to
    put it in a separate db/ package).
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db.go
+++ lxd/db.go
@@ -224,6 +224,18 @@ func init() {
 	sql.Register("sqlite3_with_fk", &sqlite3.SQLiteDriver{ConnectHook: enableForeignKeys})
 }
 
+// Open the database with the correct parameters for LXD.
+func openDb(path string) (*sql.DB, error) {
+	timeout := 5 // TODO - make this command-line configurable?
+
+	// These are used to tune the transaction BEGIN behavior instead of using the
+	// similar "locking_mode" pragma (locking for the whole database connection).
+	openPath := fmt.Sprintf("%s?_busy_timeout=%d&_txlock=exclusive", path, timeout*1000)
+
+	// Open the database. If the file doesn't exist it is created.
+	return sql.Open("sqlite3_with_fk", openPath)
+}
+
 // Create the initial (current) schema for a given SQLite DB connection.
 func createDb(db *sql.DB) (err error) {
 	latestVersion := dbGetSchema(db)
