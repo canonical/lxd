abbfddb4325ffc2619b7d2c8c2a0354c6772fc65
Alberto Donato <alberto.donato@canonical.com>
2017-08-17 13:28:37 +0200

    tests: lints
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

--- test/includes/lxd.sh
+++ test/includes/lxd.sh
@@ -14,6 +14,7 @@ spawn_lxd() {
     storage=${1}
     shift
 
+    # shellcheck disable=SC2153
     if [ "$LXD_BACKEND" = "random" ]; then
         lxd_backend="$(random_storage_backend)"
     else
@@ -212,6 +213,7 @@ shutdown_lxd() {
     local LXD_DIR
 
     daemon_dir=${1}
+    # shellcheck disable=2034
     LXD_DIR=${daemon_dir}
     daemon_pid=$(cat "${daemon_dir}/lxd.pid")
     echo "==> Killing LXD at ${daemon_dir}"
