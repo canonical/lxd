26527dc6438ad439e05a8ce163ce2bf702fcb0d3
Alberto Donato <alberto.donato@canonical.com>
2017-08-17 13:50:40 +0200

    tests: split out test setup related helper functions
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

---
+++ test/includes/setup.sh
@@ -0,0 +1,29 @@
+# Test setup helper functions.
+
+ensure_has_localhost_remote() {
+    # shellcheck disable=SC2039
+    local addr=${1}
+    if ! lxc remote list | grep -q "localhost"; then
+        lxc remote add localhost "https://${addr}" --accept-certificate --password foo
+    fi
+}
+
+ensure_import_testimage() {
+    if ! lxc image alias list | grep -q "^| testimage\s*|.*$"; then
+        if [ -e "${LXD_TEST_IMAGE:-}" ]; then
+            lxc image import "${LXD_TEST_IMAGE}" --alias testimage
+        else
+            if [ ! -e "/bin/busybox" ]; then
+                echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
+                exit 1
+            fi
+
+            if ldd /bin/busybox >/dev/null 2>&1; then
+                echo "The testsuite requires /bin/busybox to be a static binary"
+                exit 1
+            fi
+
+            deps/import-busybox --alias testimage
+        fi
+    fi
+}
