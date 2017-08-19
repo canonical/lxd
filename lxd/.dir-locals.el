5787580e09a3f2b7db452a8c20c7bf0e99ab152e
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-08-19 20:58:27 +0000

    Add sqlite submodule
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/.dir-locals.el
+++ lxd/.dir-locals.el
@@ -1,3 +1,18 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-((go-mode . ((go-test-args . "-tags libsqlite3"))))
+((go-mode
+  . ((go-test-args . "-tags libsqlite3 -timeout 10s")
+     (eval
+      . (set
+	 (make-local-variable 'flycheck-go-build-tags)
+	 '("libsqlite3")))
+     (eval
+      . (let* ((locals-path
+     		(let ((d (dir-locals-find-file ".")))
+     		  (if (stringp d) (file-name-directory d) (car d))))
+	       (go-wrapper (s-concat locals-path ".go-wrapper"))
+	       (go-rename-wrapper (s-concat locals-path ".go-rename-wrapper")))
+     	  (progn
+	    (set (make-local-variable 'go-command) go-wrapper)
+	    (set (make-local-variable 'flycheck-go-build-executable) go-wrapper)
+	    (set (make-local-variable 'go-rename-command) go-rename-wrapper)))))))
