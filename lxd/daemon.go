54bb34d0fa912c12f9208ad5781d74880fc72625
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-08-22 12:56:56 +0000

    Move global aaStacking global variable to sys.OS
    The AppArmor-stacking detection code has been moved to a new
    lxd/util/apparmor.go file, which is in turn used by sys.OS for its
    initialization. All references to aaStacking have been replaced to
    references to a non-global sys.OS object available through the
    container model.
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -13,7 +13,6 @@ import (
 	"net/url"
 	"os"
 	"runtime"
-	"strconv"
 	"strings"
 	"sync"
 	"syscall"
@@ -39,7 +38,6 @@ import (
 // AppArmor
 var aaAdmin = false
 var aaConfined = false
-var aaStacking = false
 var aaStacked = false
 
 // CGroup
@@ -335,51 +333,6 @@ func (d *Daemon) Init() error {
 
 	/* Detect user namespaces */
 	runningInUserns = shared.RunningInUserNS()
-
-	/* Detect AppArmor stacking support */
-	aaCanStack := func() bool {
-		contentBytes, err := ioutil.ReadFile("/sys/kernel/security/apparmor/features/domain/stack")
-		if err != nil {
-			return false
-		}
-
-		if string(contentBytes) != "yes\n" {
-			return false
-		}
-
-		contentBytes, err = ioutil.ReadFile("/sys/kernel/security/apparmor/features/domain/version")
-		if err != nil {
-			return false
-		}
-
-		content := string(contentBytes)
-
-		parts := strings.Split(strings.TrimSpace(content), ".")
-
-		if len(parts) == 0 {
-			logger.Warn("unknown apparmor domain version", log.Ctx{"version": content})
-			return false
-		}
-
-		major, err := strconv.Atoi(parts[0])
-		if err != nil {
-			logger.Warn("unknown apparmor domain version", log.Ctx{"version": content})
-			return false
-		}
-
-		minor := 0
-		if len(parts) == 2 {
-			minor, err = strconv.Atoi(parts[1])
-			if err != nil {
-				logger.Warn("unknown apparmor domain version", log.Ctx{"version": content})
-				return false
-			}
-		}
-
-		return major >= 1 && minor >= 2
-	}
-
-	aaStacking = aaCanStack()
 
 	/* Detect existing AppArmor stack */
 	if shared.PathExists("/sys/kernel/security/apparmor/.ns_stacked") {
