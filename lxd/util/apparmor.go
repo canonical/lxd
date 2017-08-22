fa5002511a9b8fe3d47b584d82abcf343009e1f5
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-08-22 15:05:04 +0000

    Move global aaConfined global variable to sys.OS
    Mechanical move of the aaConfined global variable to the
    instance-level sys.OS.AppArmorConfined attribute.
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/util/apparmor.go
+++ lxd/util/apparmor.go
@@ -52,3 +52,13 @@ func AppArmorCanStack() bool {
 
 	return major >= 1 && minor >= 2
 }
+
+// AppArmorProfile returns the current apparmor profile.
+func AppArmorProfile() string {
+	contents, err := ioutil.ReadFile("/proc/self/attr/current")
+	if err == nil {
+		return strings.TrimSpace(string(contents))
+	}
+
+	return ""
+}
