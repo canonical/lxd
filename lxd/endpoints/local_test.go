a121b9d13cf941c3e1881641b0427bad8ef8786d
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-08-29 07:54:31 +0000

    Add a endpoints.Endpoints class for managing HTTP endpoints
    This commit adds a new lxd/endpoints sub-package encapsulating the
    logic to setup and teardown the various LXD HTTP API endpoints. It
    closely reproduces the existing logic in daemon.go, with a couple of
    important differences:
    
    - Extensive comments explaining how the various endpoints behave and
      their startup/shutdown sequence.
    
    - There are now unit tests for all happy paths and most of the unhappy
      ones.
    
    - The Socket wrapper with its CloseOnExit flag has been removed, since
      the Go issue described in #1078 is now solved in Go >= 1.6.
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

---
+++ lxd/endpoints/local_test.go
@@ -0,0 +1,108 @@
+package endpoints_test
+
+import (
+	"io/ioutil"
+	"net"
+	"os"
+	"testing"
+
+	"github.com/lxc/lxd/shared"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// If no socket-based activation is detected, a new local unix socket will be
+// created.
+func TestEndpoints_LocalCreateUnixSocket(t *testing.T) {
+	endpoints, config, cleanup := newEndpoints(t)
+	defer cleanup()
+
+	require.NoError(t, endpoints.Up(config))
+
+	path := endpoints.LocalSocketPath()
+	assert.NoError(t, httpGetOverUnixSocket(path))
+
+	// The unix socket file gets removed after shutdown.
+	cleanup()
+	assert.Equal(t, false, shared.PathExists(path))
+}
+
+// If socket-based activation is detected, it will be used for binding the API
+// Endpoints' unix socket.
+func TestEndpoints_LocalSocketBasedActivation(t *testing.T) {
+	listener := newUnixListener(t)
+	defer listener.Close() // This will also remove the underlying file
+
+	file, err := listener.File()
+	require.NoError(t, err)
+	defer file.Close()
+
+	endpoints, config, cleanup := newEndpoints(t)
+	defer cleanup()
+
+	setupSocketBasedActivation(endpoints, file)
+
+	require.NoError(t, endpoints.Up(config))
+
+	assertNoSocketBasedActivation(t)
+
+	path := endpoints.LocalSocketPath()
+	assert.NoError(t, httpGetOverUnixSocket(path))
+
+	// The unix socket file does not get removed after shutdown (thanks to
+	// this change in Go 1.6:
+	//
+	// https://github.com/golang/go/commit/a4fd325c178ea29f554d69de4f2c3ffa09b53874
+	//
+	// which prevents listeners created from file descriptors from removing
+	// their socket files on close).
+	cleanup()
+	assert.Equal(t, true, shared.PathExists(path))
+}
+
+// If a custom group for the unix socket is specified, but no such one exists,
+// an error is returned.
+func TestEndpoints_LocalUnknownUnixGroup(t *testing.T) {
+	endpoints, config, cleanup := newEndpoints(t)
+	defer cleanup()
+
+	config.LocalUnixSocketGroup = "xquibaz"
+	err := endpoints.Up(config)
+
+	assert.EqualError(
+		t, err, "local endpoint: cannot get group ID of 'xquibaz': unknown group xquibaz")
+}
+
+// If another LXD endpoints is already listening on the unix socket, an error is
+// returned.
+func TestEndpoints_LocalAlreadyRunning(t *testing.T) {
+	endpoints1, config1, cleanup1 := newEndpoints(t)
+	defer cleanup1()
+
+	require.NoError(t, endpoints1.Up(config1))
+
+	endpoints2, config2, cleanup2 := newEndpoints(t)
+	config2.Dir = config1.Dir
+	defer cleanup2()
+
+	err := endpoints2.Up(config2)
+	assert.EqualError(t, err, "local endpoint: LXD is already running")
+}
+
+// Create a UnixListener using a random and unique file name.
+func newUnixListener(t *testing.T) *net.UnixListener {
+	file, err := ioutil.TempFile("", "lxd-endpoints-test")
+	require.NoError(t, err)
+
+	path := file.Name()
+	file.Close()
+	os.Remove(path)
+
+	addr, err := net.ResolveUnixAddr("unix", path)
+	require.NoError(t, err)
+
+	listener, err := net.ListenUnix("unix", addr)
+
+	require.NoError(t, err)
+	return listener
+}
