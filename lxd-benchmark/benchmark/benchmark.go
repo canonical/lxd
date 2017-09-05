dba67f928cb79cc01c45dc0c8f0b36b3b4102b7b
Alberto Donato <alberto.donato@canonical.com>
2017-09-05 12:40:01 +0200

    benchmark: extract printTestConfig function
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

--- lxd-benchmark/benchmark/benchmark.go
+++ lxd-benchmark/benchmark/benchmark.go
@@ -47,27 +47,10 @@ func SpawnContainers(c lxd.ContainerServer, count int, parallel int, image strin
 		batch = len(cpus)
 	}
 
+	printTestConfig(count, batch, image, privileged, freeze)
+
 	batches := count / batch
 	remainder := count % batch
-
-	// Print the test header
-	privilegedStr := "unprivileged"
-	if privileged {
-		privilegedStr = "privileged"
-	}
-	mode := "normal startup"
-	if freeze {
-		mode = "start and freeze"
-	}
-	fmt.Printf("Test variables:\n")
-	fmt.Printf("  Container count: %d\n", count)
-	fmt.Printf("  Container mode: %s\n", privilegedStr)
-	fmt.Printf("  Startup mode: %s\n", mode)
-	fmt.Printf("  Image: %s\n", image)
-	fmt.Printf("  Batches: %d\n", batches)
-	fmt.Printf("  Batch size: %d\n", batch)
-	fmt.Printf("  Remainder: %d\n", remainder)
-	fmt.Printf("\n")
 
 	// Pre-load the image
 	var fingerprint string
@@ -329,4 +312,27 @@ func DeleteContainers(c lxd.ContainerServer, parallel int) error {
 
 func logf(format string, args ...interface{}) {
 	fmt.Printf(fmt.Sprintf("[%s] %s\n", time.Now().Format(time.StampMilli), format), args...)
+}
+
+func printTestConfig(count int, batchSize int, image string, privileged bool, freeze bool) {
+	privilegedStr := "unprivileged"
+	if privileged {
+		privilegedStr = "privileged"
+	}
+	mode := "normal startup"
+	if freeze {
+		mode = "start and freeze"
+	}
+
+	batches := count / batchSize
+	remainder := count % batchSize
+	fmt.Printf("Test variables:\n")
+	fmt.Printf("  Container count: %d\n", count)
+	fmt.Printf("  Container mode: %s\n", privilegedStr)
+	fmt.Printf("  Startup mode: %s\n", mode)
+	fmt.Printf("  Image: %s\n", image)
+	fmt.Printf("  Batches: %d\n", batches)
+	fmt.Printf("  Batch size: %d\n", batchSize)
+	fmt.Printf("  Remainder: %d\n", remainder)
+	fmt.Printf("\n")
 }
