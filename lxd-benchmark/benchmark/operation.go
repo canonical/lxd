2f16f39ef65f7fdb3af559b29f2d9566c0f7a62c
Alberto Donato <alberto.donato@canonical.com>
2017-09-06 09:52:22 +0200

    benchmark: add CreateContainers function
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

--- lxd-benchmark/benchmark/operation.go
+++ lxd-benchmark/benchmark/operation.go
@@ -5,6 +5,30 @@ import (
 	"github.com/lxc/lxd/shared/api"
 )
 
+func createContainer(c lxd.ContainerServer, fingerprint string, name string, privileged bool) error {
+	config := map[string]string{}
+	if privileged {
+		config["security.privileged"] = "true"
+	}
+	config["user.lxd-benchmark"] = "true"
+
+	req := api.ContainersPost{
+		Name: name,
+		Source: api.ContainerSource{
+			Type:        "image",
+			Fingerprint: fingerprint,
+		},
+	}
+	req.Config = config
+
+	op, err := c.CreateContainer(req)
+	if err != nil {
+		return err
+	}
+
+	return op.Wait()
+}
+
 func stopContainer(c lxd.ContainerServer, name string) error {
 	op, err := c.UpdateContainerState(
 		name, api.ContainerStatePut{Action: "stop", Timeout: -1, Force: true}, "")
