013e3f1b325bea50dd21498932fadfae143d86da
Stéphane Graber <stgraber@ubuntu.com>
2017-09-20 12:57:39 -0400

    tls: Add some more TLS ciphers
    Closes #3822
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -836,6 +836,12 @@ func (d *Daemon) Init() error {
 			MinVersion:   tls.VersionTLS12,
 			MaxVersion:   tls.VersionTLS12,
 			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA},
 			PreferServerCipherSuites: true,
