dbdb5db247f112aa2863c7314d82d4b4f4b01bc6
Alberto Donato <alberto.donato@canonical.com>
2017-09-21 09:45:25 +0200

    shared/osarch: add function for parsing /etc/os-release
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

---
+++ shared/osarch/release_test.go
@@ -0,0 +1,90 @@
+package osarch
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/lxc/lxd/shared/testhelpers"
+)
+
+type releaseTestSuite struct {
+	suite.Suite
+}
+
+func TestReleaseTestSuite(t *testing.T) {
+	suite.Run(t, new(releaseTestSuite))
+}
+
+func (s *releaseTestSuite) TestGetLSBRelease() {
+	content := `NAME="Ubuntu"
+ID="ubuntu"
+VERSION_ID="16.04"
+`
+	filename, cleanup := testhelpers.WriteTempFile(s.Suite, "", "os-release", content)
+	defer cleanup()
+
+	lsbRelease, err := getLSBRelease(filename)
+	s.Nil(err)
+	s.Equal(
+		map[string]string{
+			"NAME":       "Ubuntu",
+			"ID":         "ubuntu",
+			"VERSION_ID": "16.04",
+		}, lsbRelease)
+}
+
+func (s *releaseTestSuite) TestGetLSBReleaseSingleQuotes() {
+	content := `NAME='Ubuntu'`
+	filename, cleanup := testhelpers.WriteTempFile(s.Suite, "", "os-release", content)
+	defer cleanup()
+
+	lsbRelease, err := getLSBRelease(filename)
+	s.Nil(err)
+	s.Equal(map[string]string{"NAME": "Ubuntu"}, lsbRelease)
+}
+
+func (s *releaseTestSuite) TestGetLSBReleaseNoQuotes() {
+	content := `NAME=Ubuntu`
+	filename, cleanup := testhelpers.WriteTempFile(s.Suite, "", "os-release", content)
+	defer cleanup()
+
+	lsbRelease, err := getLSBRelease(filename)
+	s.Nil(err)
+	s.Equal(map[string]string{"NAME": "Ubuntu"}, lsbRelease)
+}
+
+func (s *releaseTestSuite) TestGetLSBReleaseSkipCommentsEmpty() {
+	content := `
+NAME="Ubuntu"
+
+ID="ubuntu"
+# skip this line
+VERSION_ID="16.04"
+`
+	filename, cleanup := testhelpers.WriteTempFile(s.Suite, "", "os-release", content)
+	defer cleanup()
+
+	lsbRelease, err := getLSBRelease(filename)
+	s.Nil(err)
+	s.Equal(
+		map[string]string{
+			"NAME":       "Ubuntu",
+			"ID":         "ubuntu",
+			"VERSION_ID": "16.04",
+		}, lsbRelease)
+}
+
+func (s *releaseTestSuite) TestGetLSBReleaseInvalidLine() {
+	content := `
+NAME="Ubuntu"
+this is invalid
+ID="ubuntu"
+`
+	filename, cleanup := testhelpers.WriteTempFile(s.Suite, "", "os-release", content)
+	defer cleanup()
+
+	_, err := getLSBRelease(filename)
+	s.EqualError(err, fmt.Sprintf("%s: invalid format on line 3", filename))
+}
