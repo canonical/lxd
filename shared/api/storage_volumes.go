52163f788f2a267003679108a8a17076792e2bb4
Alberto Donato <alberto.donato@canonical.com>
2017-10-06 12:55:44 +0200

    shared/api: split storage in separate files for pools and volumes
    Signed-off-by: Alberto Donato <alberto.donato@canonical.com>

--- shared/api/storage_volumes.go
+++ shared/api/storage_volumes.go
@@ -1,36 +1,5 @@
 package api
 
-// StoragePoolsPost represents the fields of a new LXD storage pool
-//
-// API extension: storage
-type StoragePoolsPost struct {
-	StoragePoolPut `yaml:",inline"`
-
-	Name   string `json:"name" yaml:"name"`
-	Driver string `json:"driver" yaml:"driver"`
-}
-
-// StoragePool represents the fields of a LXD storage pool.
-//
-// API extension: storage
-type StoragePool struct {
-	StoragePoolPut `yaml:",inline"`
-
-	Name   string   `json:"name" yaml:"name"`
-	Driver string   `json:"driver" yaml:"driver"`
-	UsedBy []string `json:"used_by" yaml:"used_by"`
-}
-
-// StoragePoolPut represents the modifiable fields of a LXD storage pool.
-//
-// API extension: storage
-type StoragePoolPut struct {
-	Config map[string]string `json:"config" yaml:"config"`
-
-	// API extension: entity_description
-	Description string `json:"description" yaml:"description"`
-}
-
 // StorageVolumesPost represents the fields of a new LXD storage pool volume
 //
 // API extension: storage
@@ -59,12 +28,6 @@ type StorageVolumePut struct {
 
 	// API extension: entity_description
 	Description string `json:"description" yaml:"description"`
-}
-
-// Writable converts a full StoragePool struct into a StoragePoolPut struct
-// (filters read-only fields).
-func (storagePool *StoragePool) Writable() StoragePoolPut {
-	return storagePool.StoragePoolPut
 }
 
 // Writable converts a full StorageVolume struct into a StorageVolumePut struct
