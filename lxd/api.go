9da0e8d9586a6c7608f2caad5d74a7a873170a81
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-10-06 18:41:45 +0000

    Extract initialization of the REST and /dev/lxd http Server
    This is a mechanical move of the REST-API-related setup code in
    daemon.go to a new api.go file. It makes the code in the Daemon a bit
    more terse and allows for easy re-use of the REST http.Server in tests.
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

---
+++ lxd/api.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"net/http"
+
+	log "github.com/lxc/lxd/shared/log15"
+
+	"github.com/gorilla/mux"
+	"github.com/lxc/lxd/shared/logger"
+)
+
+// RestServer creates an http.Server capable of handling requests against the LXD REST
+// API endpoint.
+func RestServer(d *Daemon) *http.Server {
+	/* Setup the web server */
+	mux := mux.NewRouter()
+	mux.StrictSlash(false)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		SyncResponse(true, []string{"/1.0"}).Render(w)
+	})
+
+	for _, c := range api10 {
+		d.createCmd(mux, "1.0", c)
+	}
+
+	for _, c := range apiInternal {
+		d.createCmd(mux, "internal", c)
+	}
+
+	mux.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("Sending top level 404", log.Ctx{"url": r.URL})
+		w.Header().Set("Content-Type", "application/json")
+		NotFound.Render(w)
+	})
+
+	return &http.Server{Handler: &lxdHttpServer{r: mux, d: d}}
+}
+
+type lxdHttpServer struct {
+	r *mux.Router
+	d *Daemon
+}
+
+func (s *lxdHttpServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	allowedOrigin := daemonConfig["core.https_allowed_origin"].Get()
+	origin := req.Header.Get("Origin")
+	if allowedOrigin != "" && origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+	}
+
+	allowedMethods := daemonConfig["core.https_allowed_methods"].Get()
+	if allowedMethods != "" && origin != "" {
+		rw.Header().Set("Access-Control-Allow-Methods", allowedMethods)
+	}
+
+	allowedHeaders := daemonConfig["core.https_allowed_headers"].Get()
+	if allowedHeaders != "" && origin != "" {
+		rw.Header().Set("Access-Control-Allow-Headers", allowedHeaders)
+	}
+
+	// OPTIONS request don't need any further processing
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	// Call the original server
+	s.r.ServeHTTP(rw, req)
+}
