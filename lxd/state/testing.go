c36d45bde70edaf71ab11298fb4a0fb08e26d1a1
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-10-12 16:52:25 +0000

    Add testing facilities for state.State and sys.OS
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

---
+++ lxd/state/testing.go
@@ -0,0 +1,29 @@
+package state
+
+import (
+	"testing"
+
+	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/lxd/sys"
+)
+
+// NewTestState returns a State object initialized with testable instances of
+// the node/cluster databases and of the OS facade.
+//
+// Return the newly created State object, along with a function that can be
+// used for cleaning it up.
+func NewTestState(t *testing.T) (*State, func()) {
+	node, nodeCleanup := db.NewTestNode(t)
+	cluster, clusterCleanup := db.NewTestCluster(t)
+	os, osCleanup := sys.NewTestOS(t)
+
+	cleanup := func() {
+		nodeCleanup()
+		clusterCleanup()
+		osCleanup()
+	}
+
+	state := NewState(node, cluster, nil, os)
+
+	return state, cleanup
+}
