ce2148d1026bf7e4a6c53c5c861c0e89350e7d95
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-10-16 12:23:07 +0000

    Make NewTestOS also setup the testing certificates
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/daemon_integration_test.go
+++ lxd/daemon_integration_test.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"testing"
 
 	lxd "github.com/lxc/lxd/client"
@@ -32,21 +29,16 @@ func newDaemon(t *testing.T) (*Daemon, func()) {
 	// Logging
 	resetLogger := logging.Testing(t)
 
-	// Test directory
-	dir, err := ioutil.TempDir("", "lxd-integration-test")
-	require.NoError(t, err)
-
-	// Test certificates
-	require.NoError(t, os.Mkdir(filepath.Join(dir, "var"), 0755))
-	require.NoError(t, setupTestCerts(filepath.Join(dir, "var")))
+	// OS
+	os, osCleanup := sys.NewTestOS(t)
 
 	// Daemon
-	daemon := NewDaemon(newConfig(), newOS(dir))
+	daemon := NewDaemon(newConfig(), os)
 	require.NoError(t, daemon.Init())
 
 	cleanup := func() {
 		require.NoError(t, daemon.Stop())
-		require.NoError(t, os.RemoveAll(dir))
+		osCleanup()
 		resetLogger()
 	}
 
@@ -58,36 +50,4 @@ func newConfig() *DaemonConfig {
 	return &DaemonConfig{
 		RaftLatency: 0.2,
 	}
-}
-
-// Create a new sys.OS object for testing purposes.
-func newOS(dir string) *sys.OS {
-	return &sys.OS{
-		// FIXME: setting mock mode can be avoided once daemon tasks
-		// are fixed to exit gracefully. See daemon.go.
-		MockMode: true,
-
-		VarDir:   filepath.Join(dir, "var"),
-		CacheDir: filepath.Join(dir, "cache"),
-		LogDir:   filepath.Join(dir, "log"),
-	}
-}
-
-// Populate the given test LXD directory with server certificates.
-//
-// Since generating certificates is CPU intensive, they will be simply
-// symlink'ed from the test/deps/ directory.
-func setupTestCerts(dir string) error {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	deps := filepath.Join(cwd, "..", "test", "deps")
-	for _, f := range []string{"server.crt", "server.key"} {
-		err := os.Symlink(filepath.Join(deps, f), filepath.Join(dir, f))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
