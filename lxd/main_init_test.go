e6123b189318a3c645bb6bfa0a3d631396504740
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-10-18 14:53:46 +0000

    Retry failed cluster transactions
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/main_init_test.go
+++ lxd/main_init_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/api"
 	"github.com/lxc/lxd/shared/cmd"
+	"github.com/lxc/lxd/shared/logging"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -27,6 +28,7 @@ type cmdInitTestSuite struct {
 }
 
 func (suite *cmdInitTestSuite) SetupTest() {
+	logging.Testing(suite.T())
 	suite.lxdTestSuite.SetupTest()
 	suite.streams = cmd.NewMemoryStreams("")
 	suite.context = cmd.NewMemoryContext(suite.streams)
