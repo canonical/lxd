72a2fa8ff7aa339a5a7c0ab25039c0e17f211491
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-10-19 11:10:47 +0000

    Add helpers to update the heartbeat column of a nodes row in the db
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db/node.go
+++ lxd/db/node.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/lxc/lxd/lxd/db/cluster"
 	"github.com/lxc/lxd/lxd/db/query"
 	"github.com/lxc/lxd/shared/version"
@@ -9,12 +12,19 @@ import (
 
 // NodeInfo holds information about a single LXD instance in a cluster.
 type NodeInfo struct {
-	ID            int64  // Stable node identifier
-	Name          string // User-assigned name of the node
-	Address       string // Network address of the node
-	Description   string // Node description (optional)
-	Schema        int    // Schema version of the LXD code running the node
-	APIExtensions int    // Number of API extensions of the LXD code running on the node
+	ID            int64     // Stable node identifier
+	Name          string    // User-assigned name of the node
+	Address       string    // Network address of the node
+	Description   string    // Node description (optional)
+	Schema        int       // Schema version of the LXD code running the node
+	APIExtensions int       // Number of API extensions of the LXD code running on the node
+	Heartbeat     time.Time // Timestamp of the last heartbeat
+}
+
+// IsDown returns true if the last heartbeat time of the node is older than 20
+// seconds.
+func (n NodeInfo) IsDown() bool {
+	return n.Heartbeat.Before(time.Now().Add(-20 * time.Second))
 }
 
 // Nodes returns all LXD nodes part of the cluster.
@@ -31,9 +41,14 @@ func (c *ClusterTx) Nodes() ([]NodeInfo, error) {
 			&nodes[i].Description,
 			&nodes[i].Schema,
 			&nodes[i].APIExtensions,
+			&nodes[i].Heartbeat,
 		}
 	}
-	stmt := "SELECT id, name, address, description, schema, api_extensions FROM nodes ORDER BY id"
+	stmt := `
+SELECT id, name, address, description, schema, api_extensions, heartbeat
+  FROM nodes
+    ORDER BY id
+`
 	err := query.SelectObjects(c.tx, dest, stmt)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to fecth nodes")
@@ -47,4 +62,21 @@ func (c *ClusterTx) NodeAdd(name string, address string) (int64, error) {
 	columns := []string{"name", "address", "schema", "api_extensions"}
 	values := []interface{}{name, address, cluster.SchemaVersion, len(version.APIExtensions)}
 	return query.UpsertObject(c.tx, "nodes", columns, values)
+}
+
+// NodeHeartbeat updates the heartbeat column of the node with the given address.
+func (c *ClusterTx) NodeHeartbeat(address string, heartbeat time.Time) error {
+	stmt := "UPDATE nodes SET heartbeat=? WHERE address=?"
+	result, err := c.tx.Exec(stmt, heartbeat, address)
+	if err != nil {
+		return err
+	}
+	n, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return fmt.Errorf("expected to update one row and not %d", n)
+	}
+	return nil
 }
--- lxd/db/node_test.go
+++ lxd/db/node_test.go
@@ -2,6 +2,7 @@ package db_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/lxc/lxd/lxd/db"
 	"github.com/lxc/lxd/lxd/db/cluster"
@@ -28,4 +29,24 @@ func TestNodeAdd(t *testing.T) {
 	assert.Equal(t, "1.2.3.4:666", node.Address)
 	assert.Equal(t, cluster.SchemaVersion, node.Schema)
 	assert.Equal(t, len(version.APIExtensions), node.APIExtensions)
+	assert.False(t, node.IsDown())
+}
+
+// Update the heartbeat of a node.
+func TestNodeHeartbeat(t *testing.T) {
+	tx, cleanup := db.NewTestClusterTx(t)
+	defer cleanup()
+
+	_, err := tx.NodeAdd("buzz", "1.2.3.4:666")
+	require.NoError(t, err)
+
+	err = tx.NodeHeartbeat("1.2.3.4:666", time.Now().Add(-time.Minute))
+	require.NoError(t, err)
+
+	nodes, err := tx.Nodes()
+	require.NoError(t, err)
+	require.Len(t, nodes, 1)
+
+	node := nodes[0]
+	assert.True(t, node.IsDown())
 }
