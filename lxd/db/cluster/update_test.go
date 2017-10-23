3e9410c1464a9a20d812a74478161e504f6247ee
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-10-23 11:09:05 +0000

    Add networks and networks_config table to cluster db
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db/cluster/update_test.go
+++ lxd/db/cluster/update_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/lxc/lxd/lxd/db/cluster"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,19 +14,22 @@ func TestUpdateFromV0(t *testing.T) {
 	db, err := schema.ExerciseUpdate(1, nil)
 	require.NoError(t, err)
 
-	_, err = db.Exec("INSERT INTO nodes VALUES (1, 'foo', 'blah', '1.2.3.4:666', 1, 32, ?)", time.Now())
+	stmt := "INSERT INTO nodes VALUES (1, 'foo', 'blah', '1.2.3.4:666', 1, 32, ?)"
+	_, err = db.Exec(stmt, time.Now())
 	require.NoError(t, err)
 
 	// Unique constraint on name
-	_, err = db.Exec("INSERT INTO nodes VALUES (2, 'foo', 'gosh', '5.6.7.8:666', 5, 20, ?)", time.Now())
+	stmt = "INSERT INTO nodes VALUES (2, 'foo', 'gosh', '5.6.7.8:666', 5, 20, ?)"
+	_, err = db.Exec(stmt, time.Now())
 	require.Error(t, err)
 
 	// Unique constraint on address
-	_, err = db.Exec("INSERT INTO nodes VALUES (3, 'bar', 'gasp', '1.2.3.4:666', 9, 11), ?)", time.Now())
+	stmt = "INSERT INTO nodes VALUES (3, 'bar', 'gasp', '1.2.3.4:666', 9, 11), ?)"
+	_, err = db.Exec(stmt, time.Now())
 	require.Error(t, err)
 }
 
-func TestUpdateFromV1(t *testing.T) {
+func TestUpdateFromV1_Config(t *testing.T) {
 	schema := cluster.Schema()
 	db, err := schema.ExerciseUpdate(2, nil)
 	require.NoError(t, err)
@@ -36,4 +40,49 @@ func TestUpdateFromV1(t *testing.T) {
 	// Unique constraint on key.
 	_, err = db.Exec("INSERT INTO config VALUES (2, 'foo', 'gosh')")
 	require.Error(t, err)
+}
+
+func TestUpdateFromV1_Network(t *testing.T) {
+	schema := cluster.Schema()
+	db, err := schema.ExerciseUpdate(2, nil)
+	require.NoError(t, err)
+
+	_, err = db.Exec("INSERT INTO networks VALUES (1, 'foo', 'blah')")
+	require.NoError(t, err)
+
+	// Unique constraint on name.
+	_, err = db.Exec("INSERT INTO networks VALUES (2, 'foo', 'gosh')")
+	require.Error(t, err)
+}
+
+func TestUpdateFromV1_NetworkConfig(t *testing.T) {
+	schema := cluster.Schema()
+	db, err := schema.ExerciseUpdate(2, nil)
+	require.NoError(t, err)
+
+	_, err = db.Exec("INSERT INTO networks VALUES (1, 'foo', 'blah')")
+	require.NoError(t, err)
+
+	_, err = db.Exec("INSERT INTO networks_config VALUES (1, 1, 'bar', 'baz')")
+	require.NoError(t, err)
+
+	// Unique constraint on network_id/key.
+	_, err = db.Exec("INSERT INTO networks_config VALUES (2, 1, 'bar', 'egg')")
+	require.Error(t, err)
+
+	// Reference constraint on network_id.
+	_, err = db.Exec("INSERT INTO networks_config VALUES (3, , 'fuz', 'buz')")
+	require.Error(t, err)
+
+	// Cascade deletes
+	result, err := db.Exec("DELETE FROM networks")
+	require.NoError(t, err)
+	n, err := result.RowsAffected()
+	require.NoError(t, err)
+	assert.Equal(t, int64(1), n)
+	result, err = db.Exec("DELETE FROM networks_config")
+	require.NoError(t, err)
+	n, err = result.RowsAffected()
+	require.NoError(t, err)
+	assert.Equal(t, int64(0), n) // The row was already deleted by the previous query
 }
