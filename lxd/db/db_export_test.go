ae624546126e070d640194fdf4e5c04eba578573
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-10-26 09:47:49 +0000

    Add internal "lxd sql" command to run arbitrary SQL queries
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db/db_export_test.go
+++
@@ -1,9 +0,0 @@
-package db
-
-import "database/sql"
-
-// DB returns the low level database handle to the cluster gRPC SQL database
-// handler. Used by tests for introspecing the database with raw SQL.
-func (c *Cluster) DB() *sql.DB {
-	return c.db
-}
