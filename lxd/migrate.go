981a4fdb80f69d5a00115ade489ef8017235ed4e
Adrian Reber <areber@redhat.com>
2017-11-30 10:20:42 +0100

    migrate: older than lxc 2.0.4 will fail
    Testing the older than liblxc 2.0.4 code paths does not seem to work
    anymore. This adds a debug message for that code path:
    
     liblxc version is older than 2.0.4 and the live migration will probably fail
    
    Signed-off-by: Adrian Reber <areber@redhat.com>
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/migrate.go
+++ lxd/migrate.go
@@ -497,6 +497,7 @@ func (s *migrationSourceWs) Do(migrateOp *operation) error {
 				logger.Debugf("Dump finished, continuing with restore...")
 			}
 		} else {
+			logger.Debugf("liblxc version is older than 2.0.4 and the live migration will probably fail")
 			defer os.RemoveAll(checkpointDir)
 			err = s.container.Migrate(lxc.MIGRATE_DUMP, checkpointDir, "migration", true, false)
 			if err != nil {
