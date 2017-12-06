edc4227e28b75775596324afda2eb62b06a83434
Stéphane Graber <stgraber@ubuntu.com>
2017-12-05 22:18:50 -0500

    networks: Extend allowed character set for interfaces
    Closes #4042
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/networks_utils.go
+++ lxd/networks_utils.go
@@ -472,7 +472,7 @@ func networkValidName(value string) error {
 	}
 
 	// Validate the character set
-	match, _ := regexp.MatchString("^[-a-zA-Z0-9]*$", value)
+	match, _ := regexp.MatchString("^[-_a-zA-Z0-9.]*$", value)
 	if !match {
 		return fmt.Errorf("Interface name contains invalid characters")
 	}
