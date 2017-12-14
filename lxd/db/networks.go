c49a64c45d91411b71e879aa22231d39d76a67a9
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-12-14 08:41:11 +0000

    Add db APIs for creating networks in the PENDING state
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db/networks.go
+++ lxd/db/networks.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/lxc/lxd/lxd/db/query"
+	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/api"
 )
 
@@ -55,6 +56,23 @@ func (c *ClusterTx) NetworkIDs() (map[string]int64, error) {
 	return ids, nil
 }
 
+// NetworkID returns the ID of the network with the given name.
+func (c *ClusterTx) NetworkID(name string) (int64, error) {
+	stmt := "SELECT id FROM networks WHERE name=?"
+	ids, err := query.SelectIntegers(c.tx, stmt, name)
+	if err != nil {
+		return -1, err
+	}
+	switch len(ids) {
+	case 0:
+		return -1, NoSuchObjectError
+	case 1:
+		return int64(ids[0]), nil
+	default:
+		return -1, fmt.Errorf("more than one network has the given name")
+	}
+}
+
 // NetworkConfigAdd adds a new entry in the networks_config table
 func (c *ClusterTx) NetworkConfigAdd(networkID, nodeID int64, config map[string]string) error {
 	return networkConfigAdd(c.tx, networkID, nodeID, config)
@@ -70,6 +88,128 @@ func (c *ClusterTx) NetworkNodeJoin(networkID, nodeID int64) error {
 	values := []interface{}{networkID, nodeID}
 	_, err := query.UpsertObject(c.tx, "networks_nodes", columns, values)
 	return err
+}
+
+// NetworkNodeConfigs returns the node-specific configuration of all
+// nodes grouped by node name, for the given networkID.
+//
+// If the network is not defined on all nodes, an error is returned.
+func (c *ClusterTx) NetworkNodeConfigs(networkID int64) (map[string]map[string]string, error) {
+	// Fetch all nodes.
+	nodes, err := c.Nodes()
+	if err != nil {
+		return nil, err
+	}
+
+	// Fetch the names of the nodes where the storage network is defined.
+	stmt := `
+SELECT nodes.name FROM nodes
+  LEFT JOIN networks_nodes ON networks_nodes.node_id = nodes.id
+  LEFT JOIN networks ON networks_nodes.network_id = networks.id
+WHERE networks.id = ? AND networks.state = ?
+`
+	defined, err := query.SelectStrings(c.tx, stmt, networkID, networkPending)
+	if err != nil {
+		return nil, err
+	}
+
+	// Figure which nodes are missing
+	missing := []string{}
+	for _, node := range nodes {
+		if !shared.StringInSlice(node.Name, defined) {
+			missing = append(missing, node.Name)
+		}
+	}
+
+	if len(missing) > 0 {
+		return nil, fmt.Errorf("Network not defined on nodes: %s", strings.Join(missing, ", "))
+	}
+
+	configs := map[string]map[string]string{}
+	for _, node := range nodes {
+		config, err := query.SelectConfig(c.tx, "networks_config", "node_id=?", node.ID)
+		if err != nil {
+			return nil, err
+		}
+		configs[node.Name] = config
+	}
+
+	return configs, nil
+}
+
+// NetworkCreatePending creates a new pending network on the node with
+// the given name.
+func (c *ClusterTx) NetworkCreatePending(node, name string, conf map[string]string) error {
+	// First check if a network with the given name exists, and, if
+	// so, that it's in the pending state.
+	network := struct {
+		id    int64
+		state int
+	}{}
+
+	var errConsistency error
+	dest := func(i int) []interface{} {
+		// Sanity check that there is at most one pool with the given name.
+		if i != 0 {
+			errConsistency = fmt.Errorf("more than one network exists with the given name")
+		}
+		return []interface{}{&network.id, &network.state}
+	}
+	stmt := "SELECT id, state FROM networks WHERE name=?"
+	err := query.SelectObjects(c.tx, dest, stmt, name)
+	if err != nil {
+		return err
+	}
+	if errConsistency != nil {
+		return errConsistency
+	}
+
+	var networkID = network.id
+	if networkID == 0 {
+		// No existing network with the given name was found, let's create
+		// one.
+		columns := []string{"name"}
+		values := []interface{}{name}
+		networkID, err = query.UpsertObject(c.tx, "networks", columns, values)
+		if err != nil {
+			return err
+		}
+	} else {
+		// Check that the existing network  is in the pending state.
+		if network.state != networkPending {
+			return fmt.Errorf("network is not in pending state")
+		}
+	}
+
+	// Get the ID of the node with the given name.
+	nodeInfo, err := c.NodeByName(node)
+	if err != nil {
+		return err
+	}
+
+	// Check that no network entry for this node and network exists yet.
+	count, err := query.Count(
+		c.tx, "networks_nodes", "network_id=? AND node_id=?", networkID, nodeInfo.ID)
+	if err != nil {
+		return err
+	}
+	if count != 0 {
+		return DbErrAlreadyDefined
+	}
+
+	// Insert the node-specific configuration.
+	columns := []string{"network_id", "node_id"}
+	values := []interface{}{networkID, nodeInfo.ID}
+	_, err = query.UpsertObject(c.tx, "networks_nodes", columns, values)
+	if err != nil {
+		return err
+	}
+	err = c.NetworkConfigAdd(networkID, nodeInfo.ID, conf)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *Cluster) Networks() ([]string, error) {
@@ -253,7 +393,7 @@ func (c *Cluster) NetworkCreate(name, description string, config map[string]stri
 		return -1, err
 	}
 
-	result, err := tx.Exec("INSERT INTO networks (name, description) VALUES (?, ?)", name, description)
+	result, err := tx.Exec("INSERT INTO networks (name, description, state) VALUES (?, ?, ?)", name, description, networkCreated)
 	if err != nil {
 		tx.Rollback()
 		return -1, err
