4be619a4473fc7ed283c718eff292a8bcf66e15d
Stéphane Graber <stgraber@ubuntu.com>
2018-01-08 12:01:55 -0500

    scripts/vagrant: The LXD PPA is gone
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- scripts/vagrant/install-go.sh
+++ scripts/vagrant/install-go.sh
@@ -3,8 +3,6 @@
 set -xe
 export DEBIAN_FRONTEND=noninteractive
 
-which add-apt-repository || (sudo apt-get update ; sudo apt-get install -y software-properties-common)
-sudo add-apt-repository ppa:ubuntu-lxc/lxd-git-master
 sudo apt-get update
 which go || sudo apt-get install -y golang
 
