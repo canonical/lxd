f9a9661b04230ee76fd8ee12db4fb385ddb6107e
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-01-13 16:32:45 +0000

    Support managing logs on containers running on other nodes
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/container_logs.go
+++ lxd/container_logs.go
@@ -23,6 +23,15 @@ func containerLogsGet(d *Daemon, r *http.Request) Response {
 	 */
 	name := mux.Vars(r)["name"]
 
+	// Handle requests targeted to a container on a different node
+	response, err := ForwardedResponseIfContainerIsRemote(d, r, name)
+	if err != nil {
+		return SmartError(err)
+	}
+	if response != nil {
+		return response
+	}
+
 	if err := containerValidName(name); err != nil {
 		return BadRequest(err)
 	}
@@ -63,6 +72,16 @@ func validLogFileName(fname string) bool {
 
 func containerLogGet(d *Daemon, r *http.Request) Response {
 	name := mux.Vars(r)["name"]
+
+	// Handle requests targeted to a container on a different node
+	response, err := ForwardedResponseIfContainerIsRemote(d, r, name)
+	if err != nil {
+		return SmartError(err)
+	}
+	if response != nil {
+		return response
+	}
+
 	file := mux.Vars(r)["file"]
 
 	if err := containerValidName(name); err != nil {
@@ -83,6 +102,16 @@ func containerLogGet(d *Daemon, r *http.Request) Response {
 
 func containerLogDelete(d *Daemon, r *http.Request) Response {
 	name := mux.Vars(r)["name"]
+
+	// Handle requests targeted to a container on a different node
+	response, err := ForwardedResponseIfContainerIsRemote(d, r, name)
+	if err != nil {
+		return SmartError(err)
+	}
+	if response != nil {
+		return response
+	}
+
 	file := mux.Vars(r)["file"]
 
 	if err := containerValidName(name); err != nil {
