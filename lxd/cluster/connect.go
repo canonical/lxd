833920cc2033dcd317d4c2de9359a994753b2c5b
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-01-13 11:22:11 +0000

    Add cluster.ConnectIfContainerIsRemote helper
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/cluster/connect.go
+++ lxd/cluster/connect.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	lxd "github.com/lxc/lxd/client"
+	"github.com/lxc/lxd/lxd/db"
 	"github.com/lxc/lxd/shared"
 )
 
@@ -26,4 +27,25 @@ func Connect(address string, cert *shared.CertInfo, notify bool) (lxd.ContainerS
 
 	url := fmt.Sprintf("https://%s", address)
 	return lxd.ConnectLXD(url, args)
+}
+
+// ConnectIfContainerIsRemote figures out the address of the node which is
+// running the container with the given name. If it's not the local node will
+// connect to it and return the connected client, otherwise it will just return
+// nil.
+func ConnectIfContainerIsRemote(cluster *db.Cluster, name string, cert *shared.CertInfo) (lxd.ContainerServer, error) {
+	var address string // Node address
+	err := cluster.Transaction(func(tx *db.ClusterTx) error {
+		var err error
+		address, err = tx.ContainerNodeAddress(name)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	if address == "" {
+		// The container is running right on this node, no need to connect.
+		return nil, nil
+	}
+	return Connect(address, cert, false)
 }
