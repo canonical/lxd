ad23990a7382d40e363acdf563c5e416ad3d7ff7
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-01-13 15:25:34 +0000

    Handle GET container state also for containers on other nodes
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/container_state.go
+++ lxd/container_state.go
@@ -15,11 +15,20 @@ import (
 
 func containerState(d *Daemon, r *http.Request) Response {
 	name := mux.Vars(r)["name"]
+
+	// Handle requests targeted to a container on a different node
+	response, err := ForwardedResponseIfContainerIsRemote(d, r, name)
+	if err != nil {
+		return SmartError(err)
+	}
+	if response != nil {
+		return response
+	}
+
 	c, err := containerLoadByName(d.State(), name)
 	if err != nil {
 		return SmartError(err)
 	}
-
 	state, err := c.RenderState()
 	if err != nil {
 		return InternalError(err)
