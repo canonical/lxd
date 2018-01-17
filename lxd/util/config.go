5a323da4770625fa3b8815e483cde07a553ca670
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-01-17 12:41:33 +0000

    Improve error message when storage or nework configs don't match
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/util/config.go
+++ lxd/util/config.go
@@ -1,16 +1,49 @@
 package util
 
-// CompareConfigs compares two config maps and returns true if they are equal.
-func CompareConfigs(config1, config2 map[string]string) bool {
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/lxc/lxd/shared"
+)
+
+// CompareConfigs compares two config maps and returns an error if they differ.
+func CompareConfigs(config1, config2 map[string]string, exclude []string) error {
+	if exclude == nil {
+		exclude = []string{}
+	}
+
+	delta := []string{}
 	for key, value := range config1 {
+		if shared.StringInSlice(key, exclude) {
+			continue
+		}
 		if config2[key] != value {
-			return false
+			delta = append(delta, key)
 		}
 	}
 	for key, value := range config2 {
+		if shared.StringInSlice(key, exclude) {
+			continue
+		}
 		if config1[key] != value {
-			return false
+			present := false
+			for i := range delta {
+				if delta[i] == key {
+					present = true
+				}
+				break
+			}
+			if !present {
+				delta = append(delta, key)
+			}
 		}
 	}
-	return true
+	sort.Strings(delta)
+	if len(delta) > 0 {
+		return fmt.Errorf("different values for keys: %s", strings.Join(delta, ", "))
+	}
+
+	return nil
 }
