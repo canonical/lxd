dbfcc6edbc2d976a6c9dec00ac3e8c7d8b191084
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-01-21 14:18:08 +0000

    Add StorageVolumeNodeAddresses returning node addresses of a volume
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db/storage_volumes.go
+++ lxd/db/storage_volumes.go
@@ -3,10 +3,55 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"sort"
 
 	"github.com/lxc/lxd/lxd/db/query"
-	_ "github.com/mattn/go-sqlite3"
 )
+
+// StorageVolumeNodeAddresses returns the addresses of all nodes on which the
+// volume with the given name if defined.
+//
+// The empty string is used in place of the address of the current node.
+func (c *ClusterTx) StorageVolumeNodeAddresses(poolID int64, name string, typ int) ([]string, error) {
+	nodes := []struct {
+		id      int64
+		address string
+	}{}
+	dest := func(i int) []interface{} {
+		nodes = append(nodes, struct {
+			id      int64
+			address string
+		}{})
+		return []interface{}{&nodes[i].id, &nodes[i].address}
+
+	}
+	stmt := `
+SELECT nodes.id, nodes.address
+  FROM nodes JOIN storage_volumes ON storage_volumes.node_id=nodes.id
+    WHERE storage_volumes.storage_pool_id=? AND storage_volumes.name=? AND storage_volumes.type=?
+`
+	err := query.SelectObjects(c.tx, dest, stmt, poolID, name, typ)
+	if err != nil {
+		return nil, err
+	}
+
+	addresses := []string{}
+	for _, node := range nodes {
+		address := node.address
+		if node.id == c.nodeID {
+			address = ""
+		}
+		addresses = append(addresses, address)
+	}
+
+	sort.Strings(addresses)
+
+	if len(addresses) == 0 {
+		return nil, NoSuchObjectError
+	}
+
+	return addresses, nil
+}
 
 // StorageVolumeNodeGet returns the name of the node a storage volume is on.
 func (c *Cluster) StorageVolumeNodeGet(volumeID int64) (string, error) {
---
+++ lxd/db/storage_volumes_test.go
@@ -0,0 +1,55 @@
+package db_test
+
+import (
+	"testing"
+
+	"github.com/lxc/lxd/lxd/db"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Addresses of all nodes with matching volume name are returned.
+func TestStorageVolumeNodeAddresses(t *testing.T) {
+	tx, cleanup := db.NewTestClusterTx(t)
+	defer cleanup()
+
+	nodeID1 := int64(1) // This is the default local node
+
+	nodeID2, err := tx.NodeAdd("node2", "1.2.3.4:666")
+	require.NoError(t, err)
+
+	nodeID3, err := tx.NodeAdd("node3", "5.6.7.8:666")
+	require.NoError(t, err)
+
+	poolID := addPool(t, tx, "pool1")
+	addVolume(t, tx, poolID, nodeID1, "volume1")
+	addVolume(t, tx, poolID, nodeID2, "volume1")
+	addVolume(t, tx, poolID, nodeID3, "volume2")
+	addVolume(t, tx, poolID, nodeID2, "volume2")
+
+	addresses, err := tx.StorageVolumeNodeAddresses(poolID, "volume1", 1)
+	require.NoError(t, err)
+
+	assert.Equal(t, []string{"", "1.2.3.4:666"}, addresses)
+}
+
+func addPool(t *testing.T, tx *db.ClusterTx, name string) int64 {
+	stmt := `
+INSERT INTO storage_pools(name, driver) VALUES (?, 'dir')
+`
+	result, err := tx.Tx().Exec(stmt, name)
+	require.NoError(t, err)
+
+	id, err := result.LastInsertId()
+	require.NoError(t, err)
+
+	return id
+}
+
+func addVolume(t *testing.T, tx *db.ClusterTx, poolID, nodeID int64, name string) {
+	stmt := `
+INSERT INTO storage_volumes(storage_pool_id, node_id, name, type) VALUES (?, ?, ?, 1)
+`
+	_, err := tx.Tx().Exec(stmt, poolID, nodeID, name)
+	require.NoError(t, err)
+}
