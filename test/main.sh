a460a4f4c575108aebb9b2e5ee7919192e76e1ca
Stéphane Graber <stgraber@ubuntu.com>
2018-01-24 16:46:52 -0500

    Re-enable clustering tests
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/main.sh
+++ test/main.sh
@@ -195,14 +195,11 @@ run_test test_macaroon_auth "macaroon authentication"
 run_test test_console "console"
 run_test test_proxy_device "proxy device"
 run_test test_storage_local_volume_handling "storage local volume handling"
-# FIXME: clustering tests are currently not working on CI
-if ! [ -e "/lxc-ci/build/cache/sqlite" ]; then
-    run_test test_clustering_membership "clustering membership"
-    run_test test_clustering_containers "clustering containers"
-    run_test test_clustering_storage "clustering storage"
-    run_test test_clustering_network "clustering network"
-    run_test test_clustering_upgrade "clustering upgrade"
-fi
+run_test test_clustering_membership "clustering membership"
+run_test test_clustering_containers "clustering containers"
+run_test test_clustering_storage "clustering storage"
+run_test test_clustering_network "clustering network"
+run_test test_clustering_upgrade "clustering upgrade"
 
 # shellcheck disable=SC2034
 TEST_RESULT=success
