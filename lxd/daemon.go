124b0ca8873cd22c9e7343a3585822194ae5f3d0
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-03-08 13:00:07 +0000

    Cope with broken foreign key references when upgrading
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -487,6 +487,20 @@ func (d *Daemon) init() error {
 		logger.Infof("Migrating data from lxd.db to db.bin")
 		err = d.cluster.ImportPreClusteringData(dump)
 		if err != nil {
+			// Restore the local sqlite3 backup and wipe the raft
+			// directory, so users can fix problems and retry.
+			path := filepath.Join(d.os.VarDir, "lxd.db")
+			copyErr := shared.FileCopy(path+".bak", path)
+			if copyErr != nil {
+				// Ignore errors here, there's not much we can do
+				logger.Errorf("Failed to restore lxd.db: %v", copyErr)
+			}
+			rmErr := os.RemoveAll(filepath.Join(d.os.VarDir, "raft"))
+			if rmErr != nil {
+				// Ignore errors here, there's not much we can do
+				logger.Errorf("Failed to cleanup raft db: %v", rmErr)
+			}
+
 			return fmt.Errorf("Failed to migrate data to db.bin: %v", err)
 		}
 	}
