ff49f289f2592e3c10f181b930dcd61c78c75b95
Stéphane Graber <stgraber@ubuntu.com>
2018-03-22 17:40:22 -0400

    tests: Don't use finger
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/remote.sh
+++ test/suites/remote.sh
@@ -2,7 +2,7 @@ test_remote_url() {
   # shellcheck disable=2153
   for url in "${LXD_ADDR}" "https://${LXD_ADDR}"; do
     lxc_remote remote add test "${url}" --accept-certificate --password foo
-    lxc_remote finger test:
+    lxc_remote info test:
     lxc_remote config trust list | grep @ | awk '{print $2}' | while read -r line ; do
       lxc_remote config trust remove "\"${line}\""
     done
