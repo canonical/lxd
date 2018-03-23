3c15de9510b86aaf023e32430f1d7ab0fb2b411e
Stéphane Graber <stgraber@ubuntu.com>
2018-03-22 21:10:53 -0400

    lxc/monitor: Port to cobra
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxc/monitor.go
+++ lxc/monitor.go
@@ -5,80 +5,62 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 
-	"github.com/lxc/lxd/lxc/config"
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/api"
-	"github.com/lxc/lxd/shared/gnuflag"
+	cli "github.com/lxc/lxd/shared/cmd"
 	"github.com/lxc/lxd/shared/i18n"
 	"github.com/lxc/lxd/shared/log15"
 	"github.com/lxc/lxd/shared/logging"
 )
 
-type typeList []string
+type cmdMonitor struct {
+	global *cmdGlobal
 
-func (f *typeList) String() string {
-	return fmt.Sprint(*f)
+	flagType     []string
+	flagPretty   bool
+	flagLogLevel string
 }
 
-func (f *typeList) Set(value string) error {
-	if value == "" {
-		return fmt.Errorf("Invalid type: %s", value)
-	}
+func (c *cmdMonitor) Command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = i18n.G("monitor [<remote>:]")
+	cmd.Short = i18n.G("Monitor a local or remote LXD server")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Monitor a local or remote LXD server
 
-	if f == nil {
-		*f = make(typeList, 1)
-	} else {
-		*f = append(*f, value)
-	}
-	return nil
-}
-
-type monitorCmd struct {
-	typeArgs typeList
-	pretty   bool
-	logLevel string
-}
-
-func (c *monitorCmd) showByDefault() bool {
-	return false
-}
-
-func (c *monitorCmd) usage() string {
-	return i18n.G(
-		`Usage: lxc monitor [<remote>:] [--type=TYPE...] [--pretty]
-
-Monitor a local or remote LXD server.
-
-By default the monitor will listen to all message types.
-
-Message types to listen for can be specified with --type.
-
-*Examples*
-lxc monitor --type=logging
+By default the monitor will listen to all message types.`))
+	cmd.Example = cli.FormatSection("", i18n.G(
+		`lxc monitor --type=logging
     Only show log messages.
 
 lxc monitor --pretty --type=logging --loglevel=info
     Show a pretty log of messages with info level or higher.
 
 lxc monitor --type=lifecycle
-    Only show lifecycle events.
-`)
+    Only show lifecycle events.`))
+	cmd.Hidden = true
+
+	cmd.RunE = c.Run
+	cmd.Flags().BoolVar(&c.flagPretty, "pretty", false, i18n.G("Pretty rendering"))
+	cmd.Flags().StringArrayVar(&c.flagType, "type", nil, i18n.G("Event type to listen for")+"``")
+	cmd.Flags().StringVar(&c.flagLogLevel, "loglevel", "", i18n.G("Minimum level for log messages")+"``")
+
+	return cmd
 }
 
-func (c *monitorCmd) flags() {
-	gnuflag.BoolVar(&c.pretty, "pretty", false, i18n.G("Pretty rendering"))
-	gnuflag.Var(&c.typeArgs, "type", i18n.G("Event type to listen for"))
-	gnuflag.StringVar(&c.logLevel, "loglevel", "", i18n.G("Minimum level for log messages"))
-}
+func (c *cmdMonitor) Run(cmd *cobra.Command, args []string) error {
+	conf := c.global.conf
 
-func (c *monitorCmd) run(conf *config.Config, args []string) error {
 	var err error
 	var remote string
 
-	if len(args) > 1 {
-		return errArgs
+	// Sanity checks
+	exit, err := c.global.CheckArgs(cmd, args, 0, 1)
+	if exit {
+		return err
 	}
 
 	if len(args) == 0 {
@@ -104,8 +86,8 @@ func (c *monitorCmd) run(conf *config.Config, args []string) error {
 	}
 
 	logLvl := log15.LvlDebug
-	if c.logLevel != "" {
-		logLvl, err = log15.LvlFromString(c.logLevel)
+	if c.flagLogLevel != "" {
+		logLvl, err = log15.LvlFromString(c.flagLogLevel)
 		if err != nil {
 			return err
 		}
@@ -113,7 +95,7 @@ func (c *monitorCmd) run(conf *config.Config, args []string) error {
 
 	handler := func(message interface{}) {
 		// Special handling for logging only output
-		if c.pretty && len(c.typeArgs) == 1 && shared.StringInSlice("logging", c.typeArgs) {
+		if c.flagPretty && len(c.flagType) == 1 && shared.StringInSlice("logging", c.flagType) {
 			render, err := json.Marshal(&message)
 			if err != nil {
 				fmt.Printf("error: %s\n", err)
@@ -171,7 +153,7 @@ func (c *monitorCmd) run(conf *config.Config, args []string) error {
 		fmt.Printf("%s\n\n", render)
 	}
 
-	_, err = listener.AddHandler(c.typeArgs, handler)
+	_, err = listener.AddHandler(c.flagType, handler)
 	if err != nil {
 		return err
 	}
