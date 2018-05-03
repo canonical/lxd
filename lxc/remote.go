c2b2dc23e38260989e4a843624531f95e1515e03
David Negreira <David@otherreality.net>
2018-05-03 21:01:05 +0200

    lxc remote list: add format option
    Signed-off-by: David Negreira <David@otherreality.net>

--- lxc/remote.go
+++ lxc/remote.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"crypto/x509"
+	"encoding/csv"
+	"encoding/json"
 	"encoding/pem"
 	"fmt"
 	"net"
@@ -14,6 +16,7 @@ import (
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 	"golang.org/x/crypto/ssh/terminal"
+	"gopkg.in/yaml.v2"
 
 	"github.com/lxc/lxd/client"
 	"github.com/lxc/lxd/lxc/config"
@@ -402,8 +405,9 @@ func (c *cmdRemoteGetDefault) Run(cmd *cobra.Command, args []string) error {
 
 // List
 type cmdRemoteList struct {
-	global *cmdGlobal
-	remote *cmdRemote
+	global     *cmdGlobal
+	remote     *cmdRemote
+	flagFormat string
 }
 
 func (c *cmdRemoteList) Command() *cobra.Command {
@@ -415,6 +419,7 @@ func (c *cmdRemoteList) Command() *cobra.Command {
 		`List the available remotes`))
 
 	cmd.RunE = c.Run
+	cmd.Flags().StringVar(&c.flagFormat, "format", "table", i18n.G("Format (csv|json|table|yaml)")+"``")
 
 	return cmd
 }
@@ -455,20 +460,51 @@ func (c *cmdRemoteList) Run(cmd *cobra.Command, args []string) error {
 		data = append(data, []string{strName, rc.Addr, rc.Protocol, rc.AuthType, strPublic, strStatic})
 	}
 
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetAutoWrapText(false)
-	table.SetAlignment(tablewriter.ALIGN_LEFT)
-	table.SetRowLine(true)
-	table.SetHeader([]string{
+	header := []string{
 		i18n.G("NAME"),
 		i18n.G("URL"),
 		i18n.G("PROTOCOL"),
 		i18n.G("AUTH TYPE"),
 		i18n.G("PUBLIC"),
-		i18n.G("STATIC")})
-	sort.Sort(byName(data))
-	table.AppendBulk(data)
-	table.Render()
+		i18n.G("STATIC")}
+
+	switch c.flagFormat {
+	case listFormatTable:
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetAutoWrapText(false)
+		table.SetAlignment(tablewriter.ALIGN_LEFT)
+		table.SetRowLine(true)
+		table.SetHeader(header)
+		sort.Sort(byName(data))
+		table.AppendBulk(data)
+		table.Render()
+	case listFormatCSV:
+		sort.Sort(byName(data))
+		data = append(data, []string{})
+		copy(data[1:], data[0:])
+		data[0] = header
+		w := csv.NewWriter(os.Stdout)
+		w.WriteAll(data)
+		if err := w.Error(); err != nil {
+			return err
+		}
+	case listFormatJSON:
+		data := conf.Remotes
+		enc := json.NewEncoder(os.Stdout)
+		err := enc.Encode(data)
+		if err != nil {
+			return err
+		}
+	case listFormatYAML:
+		data := conf.Remotes
+		out, err := yaml.Marshal(data)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s", out)
+	default:
+		return fmt.Errorf(i18n.G("Invalid format %q"), c.flagFormat)
+	}
 
 	return nil
 }
