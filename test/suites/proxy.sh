f95eff709297f376e9c3365b11e8060d2deaef75
Christian Brauner <christian.brauner@ubuntu.com>
2018-06-12 15:54:37 +0200

    tests: add udp proxy tests
    Closes #4566.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- test/suites/proxy.sh
+++ test/suites/proxy.sh
@@ -1,8 +1,13 @@
 test_proxy_device() {
   test_proxy_device_tcp
+  test_proxy_device_udp
+  test_proxy_device_udp_unix
+  test_proxy_device_unix_udp
   test_proxy_device_unix
   test_proxy_device_tcp_unix
   test_proxy_device_unix_tcp
+  test_proxy_device_udp_tcp
+  test_proxy_device_tcp_udp
 }
 
 test_proxy_device_tcp() {
@@ -58,6 +63,9 @@ test_proxy_device_tcp() {
   fi
 
   rm -f proxyTest.out
+
+  # Give the procies some time to shut down
+  sleep 3
 
   # Cleanup
   lxc delete -f proxyTester
@@ -133,6 +141,9 @@ test_proxy_device_unix() {
 
   rm -f "${OUTFILE}" "${HOST_SOCK}"
 
+  # Give the procies some time to shut down
+  sleep 3
+
   # Cleanup
   lxc delete -f proxyTester
 }
@@ -207,6 +218,9 @@ test_proxy_device_tcp_unix() {
 
   rm -f "${OUTFILE}"
 
+  # Give the procies some time to shut down
+  sleep 3
+
   # Cleanup
   lxc delete -f proxyTester
 }
@@ -265,6 +279,331 @@ test_proxy_device_unix_tcp() {
   fi
 
   rm -f "${OUTFILE}" "${HOST_SOCK}"
+
+  # Give the procies some time to shut down
+  sleep 3
+
+  # Cleanup
+  lxc delete -f proxyTester
+}
+
+test_proxy_device_udp() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  # Setup
+  MESSAGE="Proxy device test string"
+  HOST_UDP_PORT=$(local_tcp_port)
+  lxc launch testimage proxyTester
+
+  # Initial test
+  lxc config device add proxyTester proxyDev proxy "listen=udp:127.0.0.1:$HOST_UDP_PORT" connect=udp:127.0.0.1:4321 bind=host
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 4321 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly send data from host to container"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Restart the container
+  lxc restart -f proxyTester
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 4321 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart on container restart"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Change the port
+  lxc config device set proxyTester proxyDev connect udp:127.0.0.1:1337
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 1337 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart when config was updated"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Give the procies some time to shut down
+  sleep 3
+
+  # Cleanup
+  lxc delete -f proxyTester
+}
+
+test_proxy_device_udp_unix() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  # Setup
+  MESSAGE="Proxy device test string"
+  HOST_UDP_PORT=$(local_tcp_port)
+  OUTFILE="${TEST_DIR}/proxyTest.out"
+  lxc launch testimage proxyTester
+
+  # Initial test
+  lxc config device add proxyTester proxyDev proxy "listen=udp:127.0.0.1:${HOST_UDP_PORT}" connect=unix:/tmp/"lxdtest-$(basename "${LXD_DIR}").sock" bind=host
+  (
+    cd "${LXD_DIR}/containers/proxyTester/rootfs/tmp/" || exit
+    umask 0000
+    rm -f "lxdtest-$(basename "${LXD_DIR}").sock"
+    exec nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -l -U "lxdtest-$(basename "${LXD_DIR}").sock" > "${OUTFILE}"
+  ) &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat "${OUTFILE}")" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly send data from host to container"
+    false
+  fi
+
+  rm -f "${OUTFILE}"
+
+  # Restart the container
+  lxc restart -f proxyTester
+  (
+    cd "${LXD_DIR}/containers/proxyTester/rootfs/tmp/" || exit
+    umask 0000
+    rm -f "lxdtest-$(basename "${LXD_DIR}").sock"
+    exec nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -l -U "lxdtest-$(basename "${LXD_DIR}").sock" > "${OUTFILE}"
+  ) &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat "${OUTFILE}")" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart on container restart"
+    false
+  fi
+
+  rm -f "${OUTFILE}"
+
+  # Change the socket
+  lxc config device set proxyTester proxyDev connect unix:/tmp/"lxdtest-$(basename "${LXD_DIR}")-2.sock"
+  (
+    cd "${LXD_DIR}/containers/proxyTester/rootfs/tmp/" || exit
+    umask 0000
+    rm -f "lxdtest-$(basename "${LXD_DIR}")-2.sock"
+    exec nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -l -U "lxdtest-$(basename "${LXD_DIR}")-2.sock" > "${OUTFILE}"
+  ) &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat "${OUTFILE}")" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart when config was updated"
+    false
+  fi
+
+  rm -f "${OUTFILE}"
+
+  # Give the procies some time to shut down
+  sleep 3
+
+  # Cleanup
+  lxc delete -f proxyTester
+}
+
+test_proxy_device_unix_udp() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  # Setup
+  MESSAGE="Proxy device test string"
+  OUTFILE="${TEST_DIR}/proxyTest.out"
+  HOST_SOCK="${TEST_DIR}/lxdtest-$(basename "${LXD_DIR}")-host.sock"
+  lxc launch testimage proxyTester
+
+  # Initial test
+  lxc config device add proxyTester proxyDev proxy "listen=unix:${HOST_SOCK}" connect=udp:127.0.0.1:4321 bind=host
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 4321 > "${OUTFILE}" &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -U "${HOST_SOCK#$(pwd)/}"
+
+  if [ "$(cat "${OUTFILE}")" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly send data from host to container"
+    false
+  fi
+
+  rm -f "${OUTFILE}" "${HOST_SOCK}"
+
+  # Restart the container
+  lxc restart -f proxyTester
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 4321 > "${OUTFILE}" &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -U "${HOST_SOCK#$(pwd)/}"
+
+  if [ "$(cat "${OUTFILE}")" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart on container restart"
+    false
+  fi
+
+  rm -f "${OUTFILE}" "${HOST_SOCK}"
+
+  # Change the port
+  lxc config device set proxyTester proxyDev connect udp:127.0.0.1:1337
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 1337 > "${OUTFILE}" &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -U "${HOST_SOCK#$(pwd)/}"
+
+  if [ "$(cat "${OUTFILE}")" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart when config was updated"
+    false
+  fi
+
+  rm -f "${OUTFILE}" "${HOST_SOCK}"
+
+  # Give the procies some time to shut down
+  sleep 3
+
+  # Cleanup
+  lxc delete -f proxyTester
+}
+
+test_proxy_device_udp_tcp() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  # Setup
+  MESSAGE="Proxy device test string"
+  HOST_UDP_PORT=$(local_tcp_port)
+  lxc launch testimage proxyTester
+
+  # Initial test
+  lxc config device add proxyTester proxyDev proxy "listen=udp:127.0.0.1:$HOST_UDP_PORT" connect=tcp:127.0.0.1:4321 bind=host
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -6 -l 4321 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly send data from host to container"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Restart the container
+  lxc restart -f proxyTester
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -6 -l 4321 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart on container restart"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Change the port
+  lxc config device set proxyTester proxyDev connect tcp:127.0.0.1:1337
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -6 -l 1337 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 -u 127.0.0.1 "${HOST_UDP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart when config was updated"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Give the procies some time to shut down
+  sleep 3
+
+  # Cleanup
+  lxc delete -f proxyTester
+}
+
+test_proxy_device_tcp_udp() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  # Setup
+  MESSAGE="Proxy device test string"
+  HOST_TCP_PORT=$(local_tcp_port)
+  lxc launch testimage proxyTester
+
+  # Initial test
+  lxc config device add proxyTester proxyDev proxy "listen=tcp:127.0.0.1:$HOST_TCP_PORT" connect=udp:127.0.0.1:4321 bind=host
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 4321 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 127.0.0.1 "${HOST_TCP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly send data from host to container"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Restart the container
+  lxc restart -f proxyTester
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 4321 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 127.0.0.1 "${HOST_TCP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart on container restart"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Change the port
+  lxc config device set proxyTester proxyDev connect udp:127.0.0.1:1337
+  nsenter -n -U -t "$(lxc query /1.0/containers/proxyTester/state | jq .pid)" -- nc -u -l 1337 > proxyTest.out &
+  sleep 2
+
+  echo "${MESSAGE}" | nc -w1 127.0.0.1 "${HOST_TCP_PORT}"
+
+  if [ "$(cat proxyTest.out)" != "${MESSAGE}" ]; then
+    cat "${LXD_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device did not properly restart when config was updated"
+    false
+  fi
+
+  rm -f proxyTest.out
+
+  # Give the procies some time to shut down
+  sleep 3
 
   # Cleanup
   lxc delete -f proxyTester
