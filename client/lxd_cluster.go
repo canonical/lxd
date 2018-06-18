7a98eb69572374783a1cd9fa9826b019469a9c5e
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-06-18 16:15:30 +0000

    client: Add extended cluster join API
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- client/lxd_cluster.go
+++ client/lxd_cluster.go
@@ -23,10 +23,16 @@ func (r *ProtocolLXD) GetCluster() (*api.Cluster, string, error) {
 	return cluster, etag, nil
 }
 
-// UpdateCluster requests to bootstrap a new cluster
+// UpdateCluster requests to bootstrap a new cluster or join an existing one.
 func (r *ProtocolLXD) UpdateCluster(cluster api.ClusterPut, ETag string) (Operation, error) {
 	if !r.HasExtension("clustering") {
 		return nil, fmt.Errorf("The server is missing the required \"clustering\" API extension")
+	}
+
+	if cluster.ServerAddress != "" || cluster.ClusterPassword != "" || len(cluster.MemberConfig) > 0 {
+		if !r.HasExtension("clustering_join") {
+			return nil, fmt.Errorf("The server is missing the required \"clustering_join\" API extension")
+		}
 	}
 
 	op, _, err := r.queryOperation("PUT", "/cluster", cluster, "")
