0b35bf18c3332544cce02c3bcab5a18fb4e7ff80
Stéphane Graber <stgraber@ubuntu.com>
2018-06-21 17:42:08 -0400

    lxd/certificates: Log password failures
    Closes #4678
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/certificates.go
+++ lxd/certificates.go
@@ -20,6 +20,8 @@ import (
 	"github.com/lxc/lxd/shared/api"
 	"github.com/lxc/lxd/shared/logger"
 	"github.com/lxc/lxd/shared/version"
+
+	log "github.com/lxc/lxd/shared/log15"
 )
 
 var certificatesCmd = Command{
@@ -120,7 +122,9 @@ func certificatesPost(d *Daemon, r *http.Request) Response {
 	if err != nil {
 		return SmartError(err)
 	}
+
 	if d.checkTrustedClient(r) != nil && util.PasswordCheck(secret, req.Password) != nil {
+		logger.Warn("Bad trust password", log.Ctx{"url": r.URL.RequestURI(), "ip": r.RemoteAddr})
 		return Forbidden(nil)
 	}
 
