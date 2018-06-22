29e24ec9b5ca9fa699a21edc72729bb152884236
Thomas Hipp <thomas.hipp@canonical.com>
2018-06-22 12:59:57 +0200

    test: Fix static analysis
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- test/suites/static_analysis.sh
+++ test/suites/static_analysis.sh
@@ -29,14 +29,14 @@ test_static_analysis() {
     fi
 
     ## Mixed tabs/spaces in scripts
-    OUT=$(grep -Pr "\t" . | grep "\.sh:" || true)
+    OUT=$(grep -Pr '\t' . | grep '\.sh:' || true)
     if [ -n "${OUT}" ]; then
       echo "ERROR: mixed tabs and spaces in script: ${OUT}"
       false
     fi
 
     ## Trailing whitespace in scripts
-    OUT=$(grep -r " $" . | grep "\.sh:" || true)
+    OUT=$(grep -r " $" . | grep '\.sh:' || true)
     if [ -n "${OUT}" ]; then
       echo "ERROR: trailing whitespace in script: ${OUT}"
       false
