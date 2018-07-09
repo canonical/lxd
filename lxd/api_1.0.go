3da45d3e0d0dfc474daac25dbe9ea54a672256f8
Christian Brauner <christian.brauner@ubuntu.com>
2018-07-09 12:39:49 +0200

    api 1.0: move struct
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -20,6 +20,14 @@ import (
 	"github.com/pkg/errors"
 )
 
+var api10Cmd = Command{
+	name:         "",
+	untrustedGet: true,
+	get:          api10Get,
+	put:          api10Put,
+	patch:        api10Patch,
+}
+
 var api10 = []Command{
 	containersCmd,
 	containerCmd,
@@ -401,5 +409,3 @@ func doApi10UpdateTriggers(d *Daemon, nodeChanged, clusterChanged map[string]str
 	}
 	return nil
 }
-
-var api10Cmd = Command{name: "", untrustedGet: true, get: api10Get, put: api10Put, patch: api10Patch}
