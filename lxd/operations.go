7f602a799a4240d42b0bf0cf1d634ac2e7862327
Christian Brauner <christian.brauner@ubuntu.com>
2018-07-09 12:44:14 +0200

    operations: move structs
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/operations.go
+++ lxd/operations.go
@@ -22,6 +22,28 @@ import (
 	"github.com/lxc/lxd/shared/version"
 )
 
+var operationCmd = Command{
+	name:   "operations/{id}",
+	get:    operationAPIGet,
+	delete: operationAPIDelete,
+}
+
+var operationsCmd = Command{
+	name: "operations",
+	get:  operationsAPIGet,
+}
+
+var operationWait = Command{
+	name: "operations/{id}/wait",
+	get:  operationAPIWaitGet,
+}
+
+var operationWebsocket = Command{
+	name:         "operations/{id}/websocket",
+	untrustedGet: true,
+	get:          operationAPIWebsocketGet,
+}
+
 var operationsLock sync.Mutex
 var operations map[string]*operation = make(map[string]*operation)
 
@@ -503,8 +525,6 @@ func operationAPIDelete(d *Daemon, r *http.Request) Response {
 	return EmptySyncResponse
 }
 
-var operationCmd = Command{name: "operations/{id}", get: operationAPIGet, delete: operationAPIDelete}
-
 func operationsAPIGet(d *Daemon, r *http.Request) Response {
 	var md shared.Jmap
 
@@ -543,8 +563,6 @@ func operationsAPIGet(d *Daemon, r *http.Request) Response {
 	return SyncResponse(true, md)
 }
 
-var operationsCmd = Command{name: "operations", get: operationsAPIGet}
-
 func operationAPIWaitGet(d *Daemon, r *http.Request) Response {
 	timeout, err := shared.AtoiEmptyDefault(r.FormValue("timeout"), -1)
 	if err != nil {
@@ -569,8 +587,6 @@ func operationAPIWaitGet(d *Daemon, r *http.Request) Response {
 
 	return SyncResponse(true, body)
 }
-
-var operationWait = Command{name: "operations/{id}/wait", get: operationAPIWaitGet}
 
 type operationWebSocket struct {
 	req *http.Request
@@ -658,5 +674,3 @@ func operationAPIWebsocketGet(d *Daemon, r *http.Request) Response {
 	}
 	return &forwardedOperationWebSocket{req: r, id: id, source: source}
 }
-
-var operationWebsocket = Command{name: "operations/{id}/websocket", untrustedGet: true, get: operationAPIWebsocketGet}
