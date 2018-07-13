3e5f831afc52936696d44cfff19ed51205d964e8
Christian Brauner <christian.brauner@ubuntu.com>
2018-07-13 14:19:11 +0200

    api: add StorageVolumeSnapshotPost struct
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- shared/api/storage_pool_volume_snapshot.go
+++ shared/api/storage_pool_volume_snapshot.go
@@ -2,6 +2,14 @@ package api
 
 import ()
 
+// StorageVolumeSnapshotPost represents the fields required to rename/move a LXD storage volume snapshot
+//
+// API extension: storage_api_volume_snapshots
+type StorageVolumeSnapshotPost struct {
+	Name string `json:"name" yaml:"name"`
+	Type string `json:"type" yaml:"type"`
+}
+
 // StorageVolumeSnapshot represents a LXD storage volume snapshot
 //
 // API extension: storage_api_volume_snapshots
