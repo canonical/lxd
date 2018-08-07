4047425603dddd9f58413f6e20a00512ebbe20b5
Stéphane Graber <stgraber@ubuntu.com>
2018-08-07 16:04:44 -0400

    lxd/containers: Add helpers for retrieving containers
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/container.go
+++ lxd/container.go
@@ -1049,3 +1049,63 @@ func containerLoadByName(s *state.State, name string) (container, error) {
 
 	return containerLXCLoad(s, args)
 }
+
+func containerLoadAll(s *state.State) ([]container, error) {
+	// Get all the container arguments
+	var cts []db.ContainerArgs
+	err := s.Cluster.Transaction(func(tx *db.ClusterTx) error {
+		var err error
+		cts, err = tx.ContainerArgsList()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Load the container structs
+	containers := []container{}
+	for _, args := range cts {
+		ct, err := containerLXCLoad(s, args)
+		if err != nil {
+			return nil, err
+		}
+
+		containers = append(containers, ct)
+	}
+
+	return containers, nil
+}
+
+func containerLoadNodeAll(s *state.State) ([]container, error) {
+	// Get all the container arguments
+	var cts []db.ContainerArgs
+	err := s.Cluster.Transaction(func(tx *db.ClusterTx) error {
+		var err error
+		cts, err = tx.ContainerArgsNodeList()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Load the container structs
+	containers := []container{}
+	for _, args := range cts {
+		ct, err := containerLXCLoad(s, args)
+		if err != nil {
+			return nil, err
+		}
+
+		containers = append(containers, ct)
+	}
+
+	return containers, nil
+}
