55cf67653aebcfeca490750a3d02378810918ad8
Christian Brauner <christian.brauner@ubuntu.com>
2018-09-13 09:03:34 +0200

    network_linux: add netns_getifaddrs()
    This commit introduces my concept of a network namespace aware
    getifaddrs(), i.e. netns_getifaddrs(). This presupposes a kernel that
    carries my IF{L}A_TARGET_NETNSID patches:
    
    struct netns_ifaddrs {
    	struct netns_ifaddrs *ifa_next;
    
    	// Can - but shouldn't be - NULL.
    	char *ifa_name;
    
    	// This field is not present struct ifaddrs
    	int ifa_ifindex;
    
    	unsigned ifa_flags;
    
    	// This field is not present struct ifaddrs
    	int ifa_mtu;
    
    	// This field is not present struct ifaddrs
    	int ifa_prefixlen;
    
    	struct sockaddr *ifa_addr;
    	struct sockaddr *ifa_netmask;
    	union {
    		struct sockaddr *ifu_broadaddr;
    		struct sockaddr *ifu_dstaddr;
    	} ifa_ifu;
    
    	// If you don't know what this is for don't touch it.
    	void *ifa_data;
    };
    
    which is a superset of struct ifaddrs. It contains additional
    information such as the mtu, ifindex of the interface and the prefix
    length of the address.
    Note that the field ordering is different. So don't get any ideas of
    using memcpy() to copy from an old struct ifaddrs into a struct
    netns_ifaddrs.
    
    int netns_getifaddrs(struct netns_ifaddrs **ifap, __s32 netns_id,
                         bool *netnsid_aware)
    
    takes a network namespace identifier as argument which identifies the
    target network namespace.
    If successfull, i.e. netns_getifaddrs() returns 0, callers should check
    the bool *netnsid_aware return argument. If it is true then
    RTM_GET{ADDR,LINK} requests are fully netnsid aware. If it is false then
    they are not and the information returned in struct netns_ifaddrs does
    *not* contain correct information about the target network namespace
    identified by netnsid.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- shared/network_linux.go
+++ shared/network_linux.go
@@ -1,14 +1,164 @@
 // +build linux
+// +build cgo
 
 package shared
 
 import (
+	"fmt"
 	"io"
+	"os"
+	"strings"
 
 	"github.com/gorilla/websocket"
 
+	"github.com/lxc/lxd/shared/api"
 	"github.com/lxc/lxd/shared/logger"
 )
+
+/*
+#include "../shared/netns_getifaddrs.c"
+*/
+import "C"
+
+func NetnsGetifaddrs(initPID int32) (map[string]api.ContainerStateNetwork, error) {
+	var netnsid_aware C.bool
+	var ifaddrs *C.struct_netns_ifaddrs
+	var netnsID C.__s32
+
+	if initPID > 0 {
+		f, err := os.Open(fmt.Sprintf("/proc/%d/ns/net", initPID))
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+
+		netnsID = C.netns_get_nsid(C.__s32(f.Fd()))
+		if netnsID < 0 {
+			return nil, fmt.Errorf("Failed to retrieve network namespace id")
+		}
+	} else {
+		netnsID = -1
+	}
+
+	ret := C.netns_getifaddrs(&ifaddrs, netnsID, &netnsid_aware)
+	if ret < 0 {
+		return nil, fmt.Errorf("Failed to retrieve network interfaces and addresses")
+	}
+	defer C.netns_freeifaddrs(ifaddrs)
+
+	if netnsID >= 0 && !netnsid_aware {
+		return nil, fmt.Errorf("Netlink requests are not fully network namespace id aware")
+	}
+
+	// We're using the interface name as key here but we should really
+	// switch to the ifindex at some point to handle ip aliasing correctly.
+	networks := map[string]api.ContainerStateNetwork{}
+
+	for addr := ifaddrs; addr != nil; addr = addr.ifa_next {
+		var address [C.INET6_ADDRSTRLEN]C.char
+		addNetwork, networkExists := networks[C.GoString(addr.ifa_name)]
+		if !networkExists {
+			addNetwork = api.ContainerStateNetwork{
+				Addresses: []api.ContainerStateNetworkAddress{},
+				Counters:  api.ContainerStateNetworkCounters{},
+			}
+		}
+
+		if addr.ifa_addr.sa_family == C.AF_INET || addr.ifa_addr.sa_family == C.AF_INET6 {
+			netState := "down"
+			netType := "unknown"
+
+			if (addr.ifa_flags & C.IFF_BROADCAST) > 0 {
+				netType = "broadcast"
+			}
+
+			if (addr.ifa_flags & C.IFF_LOOPBACK) > 0 {
+				netType = "loopback"
+			}
+
+			if (addr.ifa_flags & C.IFF_POINTOPOINT) > 0 {
+				netType = "point-to-point"
+			}
+
+			if (addr.ifa_flags & C.IFF_UP) > 0 {
+				netState = "up"
+			}
+
+			family := "inet"
+			if addr.ifa_addr.sa_family == C.AF_INET6 {
+				family = "inet6"
+			}
+
+			addr_ptr := C.get_addr_ptr(addr.ifa_addr)
+			if addr_ptr == nil {
+				return nil, fmt.Errorf("Failed to retrieve valid address pointer")
+			}
+
+			address_str := C.inet_ntop(C.int(addr.ifa_addr.sa_family), addr_ptr, &address[0], C.INET6_ADDRSTRLEN)
+			if address_str == nil {
+				return nil, fmt.Errorf("Failed to retrieve address string")
+			}
+
+			if addNetwork.Addresses == nil {
+				addNetwork.Addresses = []api.ContainerStateNetworkAddress{}
+			}
+
+			goAddrString := C.GoString(address_str)
+			scope := "global"
+			if strings.HasPrefix(goAddrString, "127") {
+				scope = "local"
+			}
+
+			if goAddrString == "::1" {
+				scope = "local"
+			}
+
+			if strings.HasPrefix(goAddrString, "169.254") {
+				scope = "link"
+			}
+
+			if strings.HasPrefix(goAddrString, "fe80:") {
+				scope = "link"
+			}
+
+			address := api.ContainerStateNetworkAddress{}
+			address.Family = family
+			address.Address = goAddrString
+			address.Netmask = fmt.Sprintf("%d", int(addr.ifa_prefixlen))
+			address.Scope = scope
+
+			addNetwork.Addresses = append(addNetwork.Addresses, address)
+			addNetwork.State = netState
+			addNetwork.Type = netType
+			addNetwork.Mtu = int(addr.ifa_mtu)
+		} else if addr.ifa_addr.sa_family == C.AF_PACKET {
+
+			if (addr.ifa_flags & C.IFF_LOOPBACK) == 0 {
+				var buf [1024]C.char
+
+				hwaddr := C.get_packet_address(addr.ifa_addr, &buf[0], 1024)
+				if hwaddr == nil {
+					return nil, fmt.Errorf("Failed to retrieve hardware address")
+				}
+
+				addNetwork.Hwaddr = C.GoString(hwaddr)
+			}
+
+			stats := (*C.struct_rtnl_link_stats)(addr.ifa_data)
+			if stats != nil {
+				addNetwork.Counters.BytesReceived = int64(stats.rx_bytes)
+				addNetwork.Counters.BytesSent = int64(stats.tx_bytes)
+				addNetwork.Counters.PacketsReceived = int64(stats.rx_packets)
+				addNetwork.Counters.PacketsSent = int64(stats.tx_packets)
+			}
+		}
+		ifName := C.GoString(addr.ifa_name)
+
+		networks[ifName] = addNetwork
+	}
+
+	return networks, nil
+}
 
 func WebsocketExecMirror(conn *websocket.Conn, w io.WriteCloser, r io.ReadCloser, exited chan bool, fd int) (chan bool, chan bool) {
 	readDone := make(chan bool, 1)
