48d4404565f4f6c7d82297b24d31277b8d0bcd7a
Thomas Hipp <thomas.hipp@canonical.com>
2019-01-14 14:47:44 +0100

    shared/api: Support updating container snapshots
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- shared/api/container_snapshot.go
+++ shared/api/container_snapshot.go
@@ -20,17 +20,31 @@ type ContainerSnapshotPost struct {
 	Live bool `json:"live,omitempty" yaml:"live,omitempty"`
 }
 
+// ContainerSnapshotPut represents the modifiable fields of a LXD container snapshot
+// API extension: snapshot_expiry
+type ContainerSnapshotPut struct {
+	Architecture string                       `json:"architecture" yaml:"architecture"`
+	Config       map[string]string            `json:"config" yaml:"config"`
+	Devices      map[string]map[string]string `json:"devices" yaml:"devices"`
+	Ephemeral    bool                         `json:"ephemeral" yaml:"ephemeral"`
+	Profiles     []string                     `json:"profiles" yaml:"profiles"`
+	ExpiresAt    time.Time                    `json:"expires_at" yaml:"expires_at"`
+}
+
 // ContainerSnapshot represents a LXD conainer snapshot
 type ContainerSnapshot struct {
-	Architecture    string                       `json:"architecture" yaml:"architecture"`
-	Config          map[string]string            `json:"config" yaml:"config"`
+	ContainerSnapshotPut `yaml:",inline"`
+
 	CreatedAt       time.Time                    `json:"created_at" yaml:"created_at"`
-	Devices         map[string]map[string]string `json:"devices" yaml:"devices"`
-	Ephemeral       bool                         `json:"ephemeral" yaml:"ephemeral"`
 	ExpandedConfig  map[string]string            `json:"expanded_config" yaml:"expanded_config"`
 	ExpandedDevices map[string]map[string]string `json:"expanded_devices" yaml:"expanded_devices"`
 	LastUsedAt      time.Time                    `json:"last_used_at" yaml:"last_used_at"`
 	Name            string                       `json:"name" yaml:"name"`
-	Profiles        []string                     `json:"profiles" yaml:"profiles"`
 	Stateful        bool                         `json:"stateful" yaml:"stateful"`
+}
+
+// Writable converts a full ContainerSnapshot struct into a ContainerSnapshotPut struct
+// (filters read-only fields)
+func (c *ContainerSnapshot) Writable() ContainerSnapshotPut {
+	return c.ContainerSnapshotPut
 }
