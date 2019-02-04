97974d6fb4e29c30d64ff6e366966ed71bb2ac2c
Stéphane Graber <stgraber@ubuntu.com>
2019-02-04 17:41:22 +0100

    lxd/containers: Always delete container on create error
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/container_lxc.go
+++ lxd/container_lxc.go
@@ -378,6 +378,7 @@ func containerLXCCreate(s *state.State, args db.ContainerArgs) (container, error
 	volumeConfig := map[string]string{}
 	err = storageVolumeFillDefault(storagePool, volumeConfig, pool)
 	if err != nil {
+		c.Delete()
 		return nil, err
 	}
 
