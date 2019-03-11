edd3640eb2ac63a520764b7d41ec499b5f464765
Stéphane Graber <stgraber@ubuntu.com>
2019-03-11 11:20:30 -0400

    lxd: Fix targeting for /1.0 and /1.0/resources
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -103,6 +103,12 @@ func api10Get(d *Daemon, r *http.Request) Response {
 		return SyncResponseETag(true, srv, nil)
 	}
 
+	// If a target was specified, forward the request to the relevant node.
+	response := ForwardedResponseIfTargetIsRemote(d, r)
+	if response != nil {
+		return response
+	}
+
 	srv.Auth = "trusted"
 
 	uname, err := shared.Uname()
@@ -205,6 +211,12 @@ func api10Get(d *Daemon, r *http.Request) Response {
 }
 
 func api10Put(d *Daemon, r *http.Request) Response {
+	// If a target was specified, forward the request to the relevant node.
+	response := ForwardedResponseIfTargetIsRemote(d, r)
+	if response != nil {
+		return response
+	}
+
 	req := api.ServerPut{}
 	if err := shared.ReadToJSON(r.Body, &req); err != nil {
 		return BadRequest(err)
@@ -247,6 +259,12 @@ func api10Put(d *Daemon, r *http.Request) Response {
 }
 
 func api10Patch(d *Daemon, r *http.Request) Response {
+	// If a target was specified, forward the request to the relevant node.
+	response := ForwardedResponseIfTargetIsRemote(d, r)
+	if response != nil {
+		return response
+	}
+
 	render, err := daemonConfigRender(d.State())
 	if err != nil {
 		return InternalError(err)
