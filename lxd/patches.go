0a0b21e2d3c37210cd07559198ce051982f9be1c
Stéphane Graber <stgraber@ubuntu.com>
2019-04-08 16:01:55 -0400

    lxd/patches: Fix LVM VG name
    Closes #5643
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/patches.go
+++ lxd/patches.go
@@ -143,9 +143,14 @@ func patchRenameCustomVolumeLVs(name string, d *Daemon) error {
 			return err
 		}
 
+		vgName := poolName
+		if pool.Config["lvm.vg_name"] != "" {
+			vgName = pool.Config["lvm.vg_name"]
+		}
+
 		for _, volume := range volumes {
-			oldName := fmt.Sprintf("%s/custom_%s", poolName, volume)
-			newName := fmt.Sprintf("%s/custom_%s", poolName, containerNameToLVName(volume))
+			oldName := fmt.Sprintf("%s/custom_%s", vgName, volume)
+			newName := fmt.Sprintf("%s/custom_%s", vgName, containerNameToLVName(volume))
 
 			exists, err := storageLVExists(newName)
 			if err != nil {
@@ -156,7 +161,7 @@ func patchRenameCustomVolumeLVs(name string, d *Daemon) error {
 				continue
 			}
 
-			err = lvmLVRename(poolName, oldName, newName)
+			err = lvmLVRename(vgName, oldName, newName)
 			if err != nil {
 				return err
 			}
