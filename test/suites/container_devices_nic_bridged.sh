97285b443d96dbb0e720569e14cd793626d8067d
Thomas Parrott <thomas.parrott@canonical.com>
2019-05-09 11:59:15 +0100

    test: Added more tests for container nics
    Split them into a file per nic type.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ test/suites/container_devices_nic_bridged.sh
@@ -0,0 +1,119 @@
+test_container_devices_nic_bridged() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  veth_host_name="veth$$"
+  ct_name="nt$$"
+  ipRand=$(shuf -i 0-9 -n 1)
+  brName="lxdt$$"
+
+  # Standard bridge with random subnet and a bunch of options
+  lxc network create "${brName}"
+  lxc network set "${brName}" dns.mode dynamic
+  lxc network set "${brName}" dns.domain blah
+  lxc network set "${brName}" ipv4.routing false
+  lxc network set "${brName}" ipv6.routing false
+  lxc network set "${brName}" ipv6.dhcp.stateful true
+
+  # Test pre-launch profile config is applied at launch.
+  lxc profile copy default "${ct_name}"
+  lxc profile device set "${ct_name}" eth0 limits.ingress 1Mbit
+  lxc profile device set "${ct_name}" eth0 limits.egress 2Mbit
+  lxc profile device set "${ct_name}" eth0 host_name "${veth_host_name}"
+  lxc profile device set "${ct_name}" eth0 mtu "1400"
+  lxc launch testimage "${ct_name}" -p "${ct_name}"
+
+  # Check profile limits are applied on boot.
+  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check profile custom MTU is applied on boot.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Test hot plugging a container nic with different settings to profile with the same name.
+  lxc config device add "${ct_name}" eth0 nic \
+    nictype=bridged \
+    name=eth0 \
+    parent=${brName} \
+    limits.ingress=3Mbit \
+    limits.egress=4Mbit \
+    host_name="${veth_host_name}" \
+    mtu=1401
+
+  # Check limits are applied on hot-plug.
+  if ! tc class show dev "${veth_host_name}" | grep "3Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+  if ! tc filter show dev "${veth_host_name}" egress | grep "4Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check custom MTU is applied on hot-plug.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1401" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Test removing hot plugged device and check profile nic is restored.
+  lxc config device remove "${ct_name}" eth0
+
+  # Check profile limits are applie on hot-removal.
+  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check profile custom MTU is applied on hot-removal.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Test hot plugging a container nic then updating it.
+  lxc config device add "${ct_name}" eth0 nic \
+    nictype=bridged \
+    name=eth0 \
+    parent=${brName} \
+    host_name="${veth_host_name}"
+
+  lxc config device set "${ct_name}" eth0 limits.ingress 3Mbit
+  lxc config device set "${ct_name}" eth0 limits.egress 4Mbit
+  lxc config device set "${ct_name}" eth0 mtu 1402
+
+  # Check limits are applied on update.
+  if ! tc class show dev "${veth_host_name}" | grep "3Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+  if ! tc filter show dev "${veth_host_name}" egress | grep "4Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check custom MTU is applied update.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1402" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Cleanup.
+  lxc config device remove "${ct_name}" eth0
+  lxc delete "${ct_name}" -f
+  lxc network delete "${brName}"
+  lxc profile delete "${ct_name}"
+}
---
+++ test/suites/container_devices_nic_p2p.sh
@@ -0,0 +1,116 @@
+test_container_devices_nic_p2p() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  veth_host_name="veth$$"
+  ct_name="nt$$"
+  ipRand=$(shuf -i 0-9 -n 1)
+
+  # Test pre-launch profile config is applied at launch.
+  lxc profile copy default ${ct_name}
+  lxc profile device set ${ct_name} eth0 limits.ingress 1Mbit
+  lxc profile device set ${ct_name} eth0 limits.egress 2Mbit
+  lxc profile device set ${ct_name} eth0 host_name "${veth_host_name}"
+  lxc profile device set ${ct_name} eth0 mtu "1400"
+  lxc profile device set ${ct_name} eth0 nictype "p2p"
+  lxc launch testimage "${ct_name}" -p ${ct_name}
+
+  # Check profile limits are applied on boot.
+  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check profile custom MTU is applied on boot.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Test hot plugging a container nic with different settings to profile with the same name.
+  lxc config device add "${ct_name}" eth0 nic \
+    nictype=p2p \
+    name=eth0 \
+    limits.ingress=3Mbit \
+    limits.egress=4Mbit \
+    host_name="${veth_host_name}p2p" \
+    mtu=1401
+
+  # Check limits are applied on hot-plug.
+  if ! tc class show dev "${veth_host_name}p2p" | grep "3Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+  if ! tc filter show dev "${veth_host_name}p2p" egress | grep "4Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check custom MTU is applied  on hot-plug.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1401" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Test removing hot plugged device and check profile nic is restored.
+  lxc config device remove "${ct_name}" eth0
+
+  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+
+  # Check profile limits are applied on hot-removal.
+  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check profile custom MTU is applied on hot-removal.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Test hot plugging a container nic then updating it.
+  lxc config device add "${ct_name}" eth0 nic \
+    nictype=p2p \
+    name=eth0 \
+    host_name="${veth_host_name}"
+
+  lxc config device set "${ct_name}" eth0 limits.ingress 3Mbit
+  lxc config device set "${ct_name}" eth0 limits.egress 4Mbit
+  lxc config device set "${ct_name}" eth0 mtu 1402
+
+  # Check limits are applied on update.
+  if ! tc class show dev "${veth_host_name}" | grep "3Mbit" ; then
+    echo "limits.ingress invalid"
+    false
+  fi
+  if ! tc filter show dev "${veth_host_name}" egress | grep "4Mbit" ; then
+    echo "limits.egress invalid"
+    false
+  fi
+
+  # Check custom MTU is applied update.
+  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1402" ; then
+    echo "mtu invalid"
+    false
+  fi
+
+  # Cleanup.
+  lxc config device remove "${ct_name}" eth0
+  lxc delete "${ct_name}" -f
+  lxc profile delete "${ct_name}"
+
+  # Test adding a p2p device to a running container without host_name and no limits.
+  lxc launch testimage "${ct_name}"
+  lxc config device add "${ct_name}" eth0 nic \
+    nictype=p2p
+  lxc config device remove "${ct_name}" eth0
+  lxc delete "${ct_name}" -f
+}
