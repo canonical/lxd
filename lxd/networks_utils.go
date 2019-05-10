f727ab9427b8e2a3ad1d9a924fcf7328f376afbb
Thomas Parrott <thomas.parrott@canonical.com>
2019-05-10 09:09:28 +0100

    networks/utils: Adds networkGetDevMTU function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/networks_utils.go
+++ lxd/networks_utils.go
@@ -1045,3 +1045,19 @@ func networkClearLease(s *state.State, name string, network string, hwaddr strin
 
 	return nil
 }
+
+// networkGetDevMTU retrieves the current MTU setting for a named network device.
+func networkGetDevMTU(devName string) (uint64, error) {
+	content, err := ioutil.ReadFile(fmt.Sprintf("/sys/class/net/%s/mtu", devName))
+	if err != nil {
+		return 0, err
+	}
+
+	// Parse value
+	mtu, err := strconv.ParseUint(strings.TrimSpace(string(content)), 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return mtu, nil
+}
