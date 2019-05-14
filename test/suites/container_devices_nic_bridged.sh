10de6a56e8ff8df779fb603bb9c8e5123aceab38
Thomas Parrott <thomas.parrott@canonical.com>
2019-05-14 17:54:49 +0100

    test: Re-works nic p2p and bridged tests to check for static routes working
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/suites/container_devices_nic_bridged.sh
+++ test/suites/container_devices_nic_bridged.sh
@@ -2,8 +2,9 @@ test_container_devices_nic_bridged() {
   ensure_import_testimage
   ensure_has_localhost_remote "${LXD_ADDR}"
 
-  veth_host_name="veth$$"
-  ct_name="nt$$"
+  vethHostName="veth$$"
+  ctName="nt$$"
+  ctMAC="0A:92:a7:0d:b7:D9"
   ipRand=$(shuf -i 0-9 -n 1)
   brName="lxdt$$"
 
@@ -15,46 +16,65 @@ test_container_devices_nic_bridged() {
   lxc network set "${brName}" ipv6.routing false
   lxc network set "${brName}" ipv6.dhcp.stateful true
   lxc network set "${brName}" bridge.hwaddr 00:11:22:33:44:55
+  lxc network set "${brName}" ipv4.address 192.0.2.1/24
+  lxc network set "${brName}" ipv6.address 2001:db8::1/64
   [ "$(cat /sys/class/net/${brName}/address)" = "00:11:22:33:44:55" ]
 
-  # Test pre-launch profile config is applied at launch.
-  lxc profile copy default "${ct_name}"
-  lxc profile device set "${ct_name}" eth0 ipv4.routes "192.0.2.1${ipRand}/32"
-  lxc profile device set "${ct_name}" eth0 ipv6.routes "2001:db8::1${ipRand}/128"
-  lxc profile device set "${ct_name}" eth0 limits.ingress 1Mbit
-  lxc profile device set "${ct_name}" eth0 limits.egress 2Mbit
-  lxc profile device set "${ct_name}" eth0 host_name "${veth_host_name}"
-  lxc profile device set "${ct_name}" eth0 mtu "1400"
-  lxc launch testimage "${ct_name}" -p "${ct_name}"
+  # Test pre-launch profile config is applied at launch
+  lxc profile copy default "${ctName}"
+  lxc profile device set "${ctName}" eth0 ipv4.routes "192.0.2.1${ipRand}/32"
+  lxc profile device set "${ctName}" eth0 ipv6.routes "2001:db8::1${ipRand}/128"
+  lxc profile device set "${ctName}" eth0 limits.ingress 1Mbit
+  lxc profile device set "${ctName}" eth0 limits.egress 2Mbit
+  lxc profile device set "${ctName}" eth0 host_name "${vethHostName}"
+  lxc profile device set "${ctName}" eth0 mtu "1400"
+  lxc profile device set "${ctName}" eth0 hwaddr "${ctMAC}"
+  lxc profile device set "${ctName}" eth0 parent "${brName}"
+  lxc profile device set "${ctName}" eth0 nictype "bridged"
+  lxc launch testimage "${ctName}" -p "${ctName}"
 
   # Check profile routes are applied on boot.
-  if ! ip -4 r list dev "${veth_host_name}" | grep "192.0.2.1${ipRand}" ; then
+  if ! ip -4 r list dev "${brName}" | grep "192.0.2.1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}" | grep "2001:db8::1${ipRand}" ; then
+  if ! ip -6 r list dev "${brName}" | grep "2001:db8::1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
 
   # Check profile limits are applied on boot.
-  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+  if ! tc class show dev "${vethHostName}" | grep "1Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
-  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+  if ! tc filter show dev "${vethHostName}" egress | grep "2Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
-  # Check profile custom MTU is applied on boot.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+  # Check profile custom MTU is applied in container on boot.
+  if ! lxc exec "${ctName}" -- grep "1400" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
+  # Check profile custom MAC is applied in container on boot.
+  if ! lxc exec "${ctName}" -- grep -i "${ctMAC}" /sys/class/net/eth0/address ; then
+    echo "mac invalid"
+    false
+  fi
+
+  # Add IP alias to container and check routes actually work.
+  lxc exec "${ctName}" -- ip -4 addr add "192.0.2.1${ipRand}/32" dev eth0
+  lxc exec "${ctName}" -- ip -4 route add default dev eth0
+  ping -c2 -W1 "192.0.2.1${ipRand}"
+  lxc exec "${ctName}" -- ip -6 addr add "2001:db8::1${ipRand}/128" dev eth0
+  sleep 1 #Wait for link local gateway advert.
+  ping6 -c2 -W1 "2001:db8::1${ipRand}"
+
   # Test hot plugging a container nic with different settings to profile with the same name.
-  lxc config device add "${ct_name}" eth0 nic \
+  lxc config device add "${ctName}" eth0 nic \
     nictype=bridged \
     name=eth0 \
     parent=${brName} \
@@ -62,106 +82,152 @@ test_container_devices_nic_bridged() {
     ipv6.routes="2001:db8::2${ipRand}/128" \
     limits.ingress=3Mbit \
     limits.egress=4Mbit \
-    host_name="${veth_host_name}" \
+    host_name="${vethHostName}" \
+    hwaddr="${ctMAC}" \
     mtu=1401
 
+  # Check profile routes are removed on hot-plug.
+  if ip -4 r list dev "${brName}" | grep "192.0.2.1${ipRand}" ; then
+    echo "ipv4.routes remain"
+    false
+  fi
+  if ip -6 r list dev "${brName}" | grep "2001:db8::1${ipRand}" ; then
+    echo "ipv4.routes remain"
+    false
+  fi
+
   # Check routes are applied on hot-plug.
-  if ! ip -4 r list dev "${veth_host_name}" | grep "192.0.2.2${ipRand}" ; then
+  if ! ip -4 r list dev "${brName}" | grep "192.0.2.2${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}" | grep "2001:db8::2${ipRand}" ; then
+  if ! ip -6 r list dev "${brName}" | grep "2001:db8::2${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
 
   # Check limits are applied on hot-plug.
-  if ! tc class show dev "${veth_host_name}" | grep "3Mbit" ; then
+  if ! tc class show dev "${vethHostName}" | grep "3Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
-  if ! tc filter show dev "${veth_host_name}" egress | grep "4Mbit" ; then
+  if ! tc filter show dev "${vethHostName}" egress | grep "4Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
   # Check custom MTU is applied on hot-plug.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1401" ; then
+  if ! lxc exec "${ctName}" -- grep "1401" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
+  # Check custom MAC is applied on hot-plug.
+  if ! lxc exec "${ctName}" -- grep -i "${ctMAC}" /sys/class/net/eth0/address ; then
+    echo "mac invalid"
+    false
+  fi
+
   # Test removing hot plugged device and check profile nic is restored.
-  lxc config device remove "${ct_name}" eth0
+  lxc config device remove "${ctName}" eth0
+
+  # Check routes are removed on hot-plug.
+  if ip -4 r list dev "${brName}" | grep "192.0.2.2${ipRand}" ; then
+    echo "ipv4.routes remain"
+    false
+  fi
+  if ip -6 r list dev "${brName}" | grep "2001:db8::2${ipRand}" ; then
+    echo "ipv4.routes remain"
+    false
+  fi
 
   # Check profile routes are applied on hot-removal.
-  if ! ip -4 r list dev "${veth_host_name}" | grep "192.0.2.1${ipRand}" ; then
+  if ! ip -4 r list dev "${brName}" | grep "192.0.2.1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}" | grep "2001:db8::1${ipRand}" ; then
+  if ! ip -6 r list dev "${brName}" | grep "2001:db8::1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
 
   # Check profile limits are applie on hot-removal.
-  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+  if ! tc class show dev "${vethHostName}" | grep "1Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
-  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+  if ! tc filter show dev "${vethHostName}" egress | grep "2Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
   # Check profile custom MTU is applied on hot-removal.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+  if ! lxc exec "${ctName}" -- grep "1400" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
   # Test hot plugging a container nic then updating it.
-  lxc config device add "${ct_name}" eth0 nic \
+  lxc config device add "${ctName}" eth0 nic \
     nictype=bridged \
     name=eth0 \
     parent=${brName} \
-    host_name="${veth_host_name}"
+    host_name="${vethHostName}" \
+    ipv4.routes="192.0.2.1${ipRand}/32" \
+    ipv6.routes="2001:db8::1${ipRand}/128"
 
-  lxc config device set "${ct_name}" eth0 ipv4.routes "192.0.2.2${ipRand}/32"
-  lxc config device set "${ct_name}" eth0 ipv6.routes "2001:db8::2${ipRand}/128"
-  lxc config device set "${ct_name}" eth0 limits.ingress 3Mbit
-  lxc config device set "${ct_name}" eth0 limits.egress 4Mbit
-  lxc config device set "${ct_name}" eth0 mtu 1402
+  lxc config device set "${ctName}" eth0 ipv4.routes "192.0.2.2${ipRand}/32"
+  lxc config device set "${ctName}" eth0 ipv6.routes "2001:db8::2${ipRand}/128"
+  lxc config device set "${ctName}" eth0 limits.ingress 3Mbit
+  lxc config device set "${ctName}" eth0 limits.egress 4Mbit
+  lxc config device set "${ctName}" eth0 mtu 1402
+  lxc config device set "${ctName}" eth0 hwaddr "${ctMAC}"
+
+  # Check original routes are removed on hot-plug.
+  if ip -4 r list dev "${brName}" | grep "192.0.2.1${ipRand}" ; then
+    echo "ipv4.routes remain"
+    false
+  fi
+  if ip -6 r list dev "${brName}" | grep "2001:db8::1${ipRand}" ; then
+    echo "ipv4.routes remain"
+    false
+  fi
 
   # Check routes are applied on update.
-  if ! ip -4 r list dev "${veth_host_name}" | grep "192.0.2.2${ipRand}" ; then
+  if ! ip -4 r list dev "${brName}" | grep "192.0.2.2${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}" | grep "2001:db8::2${ipRand}" ; then
+  if ! ip -6 r list dev "${brName}" | grep "2001:db8::2${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
 
   # Check limits are applied on update.
-  if ! tc class show dev "${veth_host_name}" | grep "3Mbit" ; then
+  if ! tc class show dev "${vethHostName}" | grep "3Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
-  if ! tc filter show dev "${veth_host_name}" egress | grep "4Mbit" ; then
+  if ! tc filter show dev "${vethHostName}" egress | grep "4Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
   # Check custom MTU is applied update.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1402" ; then
+  if ! lxc exec "${ctName}" -- grep "1402" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
+  # Check custom MAC is applied update.
+  if ! lxc exec "${ctName}" -- grep -i "${ctMAC}" /sys/class/net/eth0/address ; then
+    echo "mac invalid"
+    false
+  fi
+
   # Cleanup.
-  lxc config device remove "${ct_name}" eth0
-  lxc delete "${ct_name}" -f
+  lxc config device remove "${ctName}" eth0
+  lxc delete "${ctName}" -f
   lxc network delete "${brName}"
-  lxc profile delete "${ct_name}"
+  lxc profile delete "${ctName}"
 }
--- test/suites/container_devices_nic_p2p.sh
+++ test/suites/container_devices_nic_p2p.sh
@@ -2,160 +2,194 @@ test_container_devices_nic_p2p() {
   ensure_import_testimage
   ensure_has_localhost_remote "${LXD_ADDR}"
 
-  veth_host_name="veth$$"
-  ct_name="nt$$"
+  vethHostName="veth$$"
+  ctName="nt$$"
+  ctMAC="0A:92:a7:0d:b7:D9"
   ipRand=$(shuf -i 0-9 -n 1)
 
   # Test pre-launch profile config is applied at launch.
-  lxc profile copy default ${ct_name}
-  lxc profile device set ${ct_name} eth0 ipv4.routes "192.0.2.1${ipRand}/32"
-  lxc profile device set ${ct_name} eth0 ipv6.routes "2001:db8::1${ipRand}/128"
-  lxc profile device set ${ct_name} eth0 limits.ingress 1Mbit
-  lxc profile device set ${ct_name} eth0 limits.egress 2Mbit
-  lxc profile device set ${ct_name} eth0 host_name "${veth_host_name}"
-  lxc profile device set ${ct_name} eth0 mtu "1400"
-  lxc profile device set ${ct_name} eth0 nictype "p2p"
-  lxc launch testimage "${ct_name}" -p ${ct_name}
+  lxc profile copy default ${ctName}
+  lxc profile device set ${ctName} eth0 ipv4.routes "192.0.2.1${ipRand}/32"
+  lxc profile device set ${ctName} eth0 ipv6.routes "2001:db8::1${ipRand}/128"
+  lxc profile device set ${ctName} eth0 limits.ingress 1Mbit
+  lxc profile device set ${ctName} eth0 limits.egress 2Mbit
+  lxc profile device set ${ctName} eth0 host_name "${vethHostName}"
+  lxc profile device set ${ctName} eth0 mtu "1400"
+  lxc profile device set ${ctName} eth0 hwaddr "${ctMAC}"
+  lxc profile device set ${ctName} eth0 nictype "p2p"
+  lxc launch testimage "${ctName}" -p ${ctName}
 
   # Check profile routes are applied on boot.
-  if ! ip -4 r list dev "${veth_host_name}" | grep "192.0.2.1${ipRand}" ; then
+  if ! ip -4 r list dev "${vethHostName}" | grep "192.0.2.1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}" | grep "2001:db8::1${ipRand}" ; then
+  if ! ip -6 r list dev "${vethHostName}" | grep "2001:db8::1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
 
   # Check profile limits are applied on boot.
-  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+  if ! tc class show dev "${vethHostName}" | grep "1Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
-  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+  if ! tc filter show dev "${vethHostName}" egress | grep "2Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
-  # Check profile custom MTU is applied on boot.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+  # Check profile custom MTU is applied in container on boot.
+  if ! lxc exec "${ctName}" -- grep "1400" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
+  # Check profile custom MAC is applied in container on boot.
+  if ! lxc exec "${ctName}" -- grep -i "${ctMAC}" /sys/class/net/eth0/address ; then
+    echo "mac invalid"
+    false
+  fi
+
+  # Add IP alias to container and check routes actually work.
+  ip -4 addr add 192.0.2.1/32 dev "${vethHostName}"
+  lxc exec "${ctName}" -- ip -4 addr add "192.0.2.1${ipRand}/32" dev eth0
+  lxc exec "${ctName}" -- ip -4 route add default dev eth0
+  sleep 1
+  ping -c2 -W1 "192.0.2.1${ipRand}"
+  ip -6 addr add 2001:db8::1/128 dev "${vethHostName}"
+  lxc exec "${ctName}" -- ip -6 addr add "2001:db8::1${ipRand}/128" dev eth0
+  lxc exec "${ctName}" -- ip -6 route add default dev eth0
+  sleep 1
+  ping6 -c2 -W1 "2001:db8::1${ipRand}"
+
   # Test hot plugging a container nic with different settings to profile with the same name.
-  lxc config device add "${ct_name}" eth0 nic \
+  lxc config device add "${ctName}" eth0 nic \
     nictype=p2p \
     name=eth0 \
     ipv4.routes="192.0.2.3${ipRand}/32" \
     ipv6.routes="2001:db8::3${ipRand}/128" \
     limits.ingress=3Mbit \
     limits.egress=4Mbit \
-    host_name="${veth_host_name}p2p" \
+    host_name="${vethHostName}p2p" \
+    hwaddr="${ctMAC}" \
     mtu=1401
 
   # Check routes are applied on hot-plug.
-  if ! ip -4 r list dev "${veth_host_name}p2p" | grep "192.0.2.3${ipRand}" ; then
+  if ! ip -4 r list dev "${vethHostName}p2p" | grep "192.0.2.3${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}p2p" | grep "2001:db8::3${ipRand}" ; then
+  if ! ip -6 r list dev "${vethHostName}p2p" | grep "2001:db8::3${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
 
   # Check limits are applied on hot-plug.
-  if ! tc class show dev "${veth_host_name}p2p" | grep "3Mbit" ; then
+  if ! tc class show dev "${vethHostName}p2p" | grep "3Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
-  if ! tc filter show dev "${veth_host_name}p2p" egress | grep "4Mbit" ; then
+  if ! tc filter show dev "${vethHostName}p2p" egress | grep "4Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
-  # Check custom MTU is applied  on hot-plug.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1401" ; then
+  # Check custom MTU is applied on hot-plug.
+  if ! lxc exec "${ctName}" -- grep "1401" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
+  # Check custom MAC is applied on hot-plug.
+  if ! lxc exec "${ctName}" -- grep -i "${ctMAC}" /sys/class/net/eth0/address ; then
+    echo "mac invalid"
+    false
+  fi
+
   # Test removing hot plugged device and check profile nic is restored.
-  lxc config device remove "${ct_name}" eth0
+  lxc config device remove "${ctName}" eth0
 
   # Check profile routes are applied on hot-removal.
-  if ! ip -4 r list dev "${veth_host_name}" | grep "192.0.2.1${ipRand}" ; then
+  if ! ip -4 r list dev "${vethHostName}" | grep "192.0.2.1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}" | grep "2001:db8::1${ipRand}" ; then
+  if ! ip -6 r list dev "${vethHostName}" | grep "2001:db8::1${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! tc class show dev "${veth_host_name}" | grep "1Mbit" ; then
+  if ! tc class show dev "${vethHostName}" | grep "1Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
 
   # Check profile limits are applied on hot-removal.
-  if ! tc filter show dev "${veth_host_name}" egress | grep "2Mbit" ; then
+  if ! tc filter show dev "${vethHostName}" egress | grep "2Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
   # Check profile custom MTU is applied on hot-removal.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1400" ; then
+  if ! lxc exec "${ctName}" -- grep "1400" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
   # Test hot plugging a container nic then updating it.
-  lxc config device add "${ct_name}" eth0 nic \
+  lxc config device add "${ctName}" eth0 nic \
     nictype=p2p \
     name=eth0 \
-    host_name="${veth_host_name}"
+    host_name="${vethHostName}"
 
-  lxc config device set "${ct_name}" eth0 ipv4.routes "192.0.2.2${ipRand}/32"
-  lxc config device set "${ct_name}" eth0 ipv6.routes "2001:db8::2${ipRand}/128"
-  lxc config device set "${ct_name}" eth0 limits.ingress 3Mbit
-  lxc config device set "${ct_name}" eth0 limits.egress 4Mbit
-  lxc config device set "${ct_name}" eth0 mtu 1402
+  lxc config device set "${ctName}" eth0 ipv4.routes "192.0.2.2${ipRand}/32"
+  lxc config device set "${ctName}" eth0 ipv6.routes "2001:db8::2${ipRand}/128"
+  lxc config device set "${ctName}" eth0 limits.ingress 3Mbit
+  lxc config device set "${ctName}" eth0 limits.egress 4Mbit
+  lxc config device set "${ctName}" eth0 mtu 1402
+  lxc config device set "${ctName}" eth0 hwaddr "${ctMAC}"
 
   # Check routes are applied on update.
-  if ! ip -4 r list dev "${veth_host_name}" | grep "192.0.2.2${ipRand}" ; then
+  if ! ip -4 r list dev "${vethHostName}" | grep "192.0.2.2${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
-  if ! ip -6 r list dev "${veth_host_name}" | grep "2001:db8::2${ipRand}" ; then
+  if ! ip -6 r list dev "${vethHostName}" | grep "2001:db8::2${ipRand}" ; then
     echo "ipv4.routes invalid"
     false
   fi
 
   # Check limits are applied on update.
-  if ! tc class show dev "${veth_host_name}" | grep "3Mbit" ; then
+  if ! tc class show dev "${vethHostName}" | grep "3Mbit" ; then
     echo "limits.ingress invalid"
     false
   fi
-  if ! tc filter show dev "${veth_host_name}" egress | grep "4Mbit" ; then
+  if ! tc filter show dev "${vethHostName}" egress | grep "4Mbit" ; then
     echo "limits.egress invalid"
     false
   fi
 
   # Check custom MTU is applied update.
-  if ! lxc exec "${ct_name}" -- ip link show eth0 | grep "mtu 1402" ; then
+  if ! lxc exec "${ctName}" -- grep "1402" /sys/class/net/eth0/mtu ; then
     echo "mtu invalid"
     false
   fi
 
+  # Check custom MAC is applied update.
+  if ! lxc exec "${ctName}" -- grep -i "${ctMAC}" /sys/class/net/eth0/address ; then
+    echo "mac invalid"
+    false
+  fi
+
   # Cleanup.
-  lxc config device remove "${ct_name}" eth0
-  lxc delete "${ct_name}" -f
-  lxc profile delete "${ct_name}"
+  lxc config device remove "${ctName}" eth0
+  lxc delete "${ctName}" -f
+  lxc profile delete "${ctName}"
 
   # Test adding a p2p device to a running container without host_name and no limits/routes.
-  lxc launch testimage "${ct_name}"
-  lxc config device add "${ct_name}" eth0 nic \
+  lxc launch testimage "${ctName}"
+  lxc config device add "${ctName}" eth0 nic \
     nictype=p2p
-  lxc config device remove "${ct_name}" eth0
-  lxc delete "${ct_name}" -f
+  lxc config device remove "${ctName}" eth0
+  lxc delete "${ctName}" -f
 }
