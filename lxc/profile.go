4ef925058ecfb79751d9f035145db5330e26ed08
Thomas Hipp <thomas.hipp@canonical.com>
2019-06-03 19:47:42 +0200

    lxc: Transition to golang.org/x/sys
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxc/profile.go
+++ lxc/profile.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"sort"
 	"strings"
-	"syscall"
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
@@ -456,7 +455,7 @@ func (c *cmdProfileEdit) Run(cmd *cobra.Command, args []string) error {
 	}
 
 	// If stdin isn't a terminal, read text from it
-	if !termios.IsTerminal(int(syscall.Stdin)) {
+	if !termios.IsTerminal(getStdinFd()) {
 		contents, err := ioutil.ReadAll(os.Stdin)
 		if err != nil {
 			return err
@@ -832,7 +831,7 @@ func (c *cmdProfileSet) Run(cmd *cobra.Command, args []string) error {
 	key := args[1]
 	value := args[2]
 
-	if !termios.IsTerminal(int(syscall.Stdin)) && value == "-" {
+	if !termios.IsTerminal(getStdinFd()) && value == "-" {
 		buf, err := ioutil.ReadAll(os.Stdin)
 		if err != nil {
 			return fmt.Errorf(i18n.G("Can't read from stdin: %s"), err)
