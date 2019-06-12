canonical/lxd

commit 6813afb8bd2d5ab005017f536c93b9b95ab931a0

lxd/seccomp: Fix building on older kernels
Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

lxd/seccomp.go
@@ -42,6 +42,30 @@ import (
 #include <sys/types.h>
 #include <unistd.h>
 
+#ifndef SECCOMP_RET_USER_NOTIF
+#define SECCOMP_RET_USER_NOTIF 0x7fc00000U
+
+struct seccomp_notif_sizes {
+	__u16 seccomp_notif;
+	__u16 seccomp_notif_resp;
+	__u16 seccomp_data;
+};
+
+struct seccomp_notif {
+	__u64 id;
+	__u32 pid;
+	__u32 flags;
+	struct seccomp_data data;
+};
+
+struct seccomp_notif_resp {
+	__u64 id;
+	__s64 val;
+	__s32 error;
+	__u32 flags;
+}
+#endif
+
 struct seccomp_notify_proxy_msg {
 	uint32_t version;
 #ifdef SECCOMP_RET_USER_NOTIF

commit 0448ceeffd9740be14a354573986b918e8ce6e65

seccomp: fix missing ";"
Fixes: 6813afb8bd2 ("lxd/seccomp: Fix building on older kernels")
Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

lxd/seccomp.go
@@ -63,7 +63,7 @@ struct seccomp_notif_resp {
 	__s64 val;
 	__s32 error;
 	__u32 flags;
-}
+};
 #endif
 
 struct seccomp_notify_proxy_msg {