5d374966e4fa28a5abd0bc6bfee5bbf73121117a
Thomas Parrott <thomas.parrott@canonical.com>
2019-06-12 09:28:16 +0100

    main_forkdns: Removes unused includes
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/main_forkdns.go
+++ lxd/main_forkdns.go
@@ -25,14 +25,9 @@ import (
 #include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/epoll.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
