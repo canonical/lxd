f5c22fef70410f50f76ed868eb29739f3a8c093d
Thomas Parrott <thomas.parrott@canonical.com>
2019-06-17 15:08:16 +0100

    container/lxc: Adds error checking for calls to networkClearLease
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/container_lxc.go
+++ lxd/container_lxc.go
@@ -3800,7 +3800,10 @@ func (c *containerLXC) Delete() error {
 				continue
 			}
 
-			networkClearLease(c.state, c.name, m["parent"], m["hwaddr"])
+			err = networkClearLease(c.state, c.name, m["parent"], m["hwaddr"])
+			if err != nil {
+				logger.Error("Failed to delete DHCP lease", log.Ctx{"name": c.Name(), "err": err, "device": k, "hwaddr": m["hwaddr"]})
+			}
 		}
 	}
 
