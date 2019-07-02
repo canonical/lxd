d2d7de2e2d8f880c66e5f0bf145fa1fcc20f1e65
Stéphane Graber <stgraber@ubuntu.com>
2019-07-02 00:43:12 -0400

    lxd/resources: Add CPU information
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/resources/resources.go
+++ lxd/resources/resources.go
@@ -6,8 +6,16 @@ import (
 
 // GetResources returns a filled api.Resources struct ready for use by LXD
 func GetResources() (*api.Resources, error) {
+	// Get CPU information
+	cpu, err := GetCPU()
+	if err != nil {
+		return nil, err
+	}
+
 	// Build the final struct
-	resources := api.Resources{}
+	resources := api.Resources{
+		CPU: *cpu,
+	}
 
 	return &resources, nil
 }
