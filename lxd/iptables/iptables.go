52e817fdf572cd4ffe17a45acabeaccbaeaccebf
Thomas Parrott <thomas.parrott@canonical.com>
2019-07-16 18:30:58 +0100

    test: Updates static_analysis.sh
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/iptables/iptables.go
+++ lxd/iptables/iptables.go
@@ -103,29 +103,34 @@ func iptablesClear(protocol string, comment string, table string) error {
 	return nil
 }
 
+// NetworkAppend adds a network rule at end of ruleset.
 func NetworkAppend(protocol string, comment string, table string, chain string,
 	rule ...string) error {
 	return iptablesAppend(protocol, fmt.Sprintf("LXD network %s", comment),
 		table, chain, rule...)
 }
 
+// NetworkPrepend adds a network rule at start of ruleset.
 func NetworkPrepend(protocol string, comment string, table string, chain string,
 	rule ...string) error {
 	return iptablesPrepend(protocol, fmt.Sprintf("LXD network %s", comment),
 		table, chain, rule...)
 }
 
+// NetworkClear removes network rules.
 func NetworkClear(protocol string, comment string, table string) error {
 	return iptablesClear(protocol, fmt.Sprintf("LXD network %s", comment),
 		table)
 }
 
+// ContainerPrepend adds container rule at start of ruleset.
 func ContainerPrepend(protocol string, comment string, table string,
 	chain string, rule ...string) error {
 	return iptablesPrepend(protocol, fmt.Sprintf("LXD container %s", comment),
 		table, chain, rule...)
 }
 
+// ContainerClear removes container rules.
 func ContainerClear(protocol string, comment string, table string) error {
 	return iptablesClear(protocol, fmt.Sprintf("LXD container %s", comment),
 		table)
