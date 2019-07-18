ca8881de6e1ed2353c406f8385571148bd368a40
Stéphane Graber <stgraber@ubuntu.com>
2019-07-18 00:06:53 -0400

    DEBUG
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/clustering.sh
+++ test/suites/clustering.sh
@@ -506,10 +506,12 @@ test_clustering_storage() {
 
     # Shutdown node 3, and wait for it to be considered offline.
     LXD_DIR="${LXD_THREE_DIR}" lxc config set cluster.offline_threshold 12
+    LXD_DIR="${LXD_ONE_DIR}" lxc cluster list
     LXD_DIR="${LXD_THREE_DIR}" lxd shutdown
     sleep 30
 
     # Move the container back to node2, even if node3 is offline
+    LXD_DIR="${LXD_ONE_DIR}" lxc cluster list
     LXD_DIR="${LXD_ONE_DIR}" lxc move bar --target node2
     LXD_DIR="${LXD_ONE_DIR}" lxc info bar | grep -q "Location: node2"
     LXD_DIR="${LXD_TWO_DIR}" lxc info bar | grep -q "backup (taken at"
