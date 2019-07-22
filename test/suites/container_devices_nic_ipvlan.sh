5393a3cba07680e979325e1eb44dee39658a5001
Thomas Parrott <thomas.parrott@canonical.com>
2019-07-22 10:10:28 +0100

    device/nic/ipvlan: Adds IPVLAN NIC device implementation
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/suites/container_devices_nic_ipvlan.sh
+++ test/suites/container_devices_nic_ipvlan.sh
@@ -13,6 +13,9 @@ test_container_devices_nic_ipvlan() {
   # Test ipvlan support to offline container (hot plugging not supported).
   ip link add "${ct_name}" type dummy
 
+  # Record how many nics we started with.
+  startNicCount=$(find /sys/class/net | wc -l)
+
   # Check that starting IPVLAN container.
   sysctl net.ipv6.conf."${ct_name}".proxy_ndp=1
   sysctl net.ipv6.conf."${ct_name}".forwarding=1
@@ -48,6 +51,33 @@ test_container_devices_nic_ipvlan() {
   lxc exec "${ct_name}" -- ping6 -c2 -W1 "2001:db8::3${ipRand}"
   lxc exec "${ct_name}2" -- ping -c2 -W1 "192.0.2.1${ipRand}"
   lxc exec "${ct_name}2" -- ping6 -c2 -W1 "2001:db8::1${ipRand}"
+  lxc stop -f "${ct_name}2"
+
+  # Check IPVLAN ontop of VLAN parent.
+  lxc stop -f "${ct_name}"
+  lxc config device set "${ct_name}" eth0 vlan 1234
+  lxc start "${ct_name}"
+
+  # Check VLAN interface created
+  if ! grep "1" "/sys/class/net/${ct_name}.1234/carrier" ; then
+    echo "vlan interface not created"
+    false
+  fi
+
+  lxc stop -f "${ct_name}"
+
+  # Check parent device is still up.
+  if ! grep "1" "/sys/class/net/${ct_name}/carrier" ; then
+    echo "parent is down"
+    false
+  fi
+
+  # Check we haven't left any NICS lying around.
+  endNicCount=$(find /sys/class/net | wc -l)
+  if [ "$startNicCount" != "$endNicCount" ]; then
+    echo "leftover NICS detected"
+    false
+  fi
 
   # Cleanup ipvlan checks
   lxc delete "${ct_name}" -f
