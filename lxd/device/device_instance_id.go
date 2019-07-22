f026f842696c7b9b818f01e3c00a8ce93d77ecb9
Thomas Parrott <thomas.parrott@canonical.com>
2019-07-22 09:53:32 +0100

    device/instance/id: Adds functions to interface to expose config
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/device_instance_id.go
+++ lxd/device/device_instance_id.go
@@ -1,8 +1,16 @@
 package device
 
+import (
+	"github.com/lxc/lxd/lxd/device/config"
+)
+
 // InstanceIdentifier is an interface that allows us to identify an Instance and its properties.
+// It is intended that this interface be entirely comprised of functions which cannot be blocking
+// independent of when they're called in the instance lifecycle.
 type InstanceIdentifier interface {
 	Name() string
 	Type() string
 	Project() string
+	ExpandedConfig() map[string]string
+	ExpandedDevices() config.Devices
 }
