f47e144952e1ef4d3190ec61419c90819c996403
Thomas Parrott <thomas.parrott@canonical.com>
2019-07-29 14:26:04 +0100

    device/instance/id: Adds LogPath() to instance identifier interface
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/device_instance_id.go
+++ lxd/device/device_instance_id.go
@@ -12,6 +12,7 @@ type InstanceIdentifier interface {
 	Type() string
 	Project() string
 	DevicesPath() string
+	LogPath() string
 	ExpandedConfig() map[string]string
 	ExpandedDevices() config.Devices
 }
