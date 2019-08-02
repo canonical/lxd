732e902a355ed89c9ae42253b2da1eea83016e99
Thomas Parrott <thomas.parrott@canonical.com>
2019-08-02 11:45:02 +0100

    device/infiniband: Adds infiniband device loader
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/device/infiniband.go
@@ -0,0 +1,19 @@
+package device
+
+import (
+	"github.com/lxc/lxd/lxd/device/config"
+)
+
+// infinibandTypes defines the supported infiniband type devices and defines their creation functions.
+var infinibandTypes = map[string]func() device{
+	"physical": func() device { return &infinibandPhysical{} },
+	"sriov":    func() device { return &infinibandSRIOV{} },
+}
+
+// infinibandLoadByType returns an Infiniband device instantiated with supplied config.
+func infinibandLoadByType(c config.Device) device {
+	if f := infinibandTypes[c["nictype"]]; f != nil {
+		return f()
+	}
+	return nil
+}
