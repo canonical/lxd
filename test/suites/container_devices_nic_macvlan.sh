0347af7e07286b878118ea854f14f2e2f3309a15
Stéphane Graber <stgraber@ubuntu.com>
2019-08-09 13:27:46 -0400

    tests: Make shellcheck happy
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/container_devices_nic_macvlan.sh
+++ test/suites/container_devices_nic_macvlan.sh
@@ -16,7 +16,7 @@ test_container_devices_nic_macvlan() {
   lxc profile copy default "${ctName}"
 
   # Modifiy profile nictype and parent in atomic operation to ensure validation passes.
-  lxc profile show "${ctName}" | sed  "s/nictype: p2p/nictype: macvlan\n    parent: ${ctName}/" | lxc profile edit "${ctName}"
+  lxc profile show "${ctName}" | sed  "s/nictype: p2p/nictype: macvlan\\n    parent: ${ctName}/" | lxc profile edit "${ctName}"
   lxc profile device set "${ctName}" eth0 mtu "1400"
 
   lxc launch testimage "${ctName}" -p "${ctName}"
