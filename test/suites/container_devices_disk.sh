359f1db856c1c0319fb3c84cf0f6298139a551df
Stéphane Graber <stgraber@ubuntu.com>
2019-08-08 23:02:40 -0400

    tests: Add test for security.shifted
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/container_devices_disk.sh
+++ test/suites/container_devices_disk.sh
@@ -14,6 +14,7 @@ test_container_devices_disk_shift() {
     return
   fi
 
+  # Test basic shiftfs
   mkdir -p "${TEST_DIR}/shift-source"
   touch "${TEST_DIR}/shift-source/a"
   chown 123:456 "${TEST_DIR}/shift-source/a"
@@ -28,5 +29,33 @@ test_container_devices_disk_shift() {
   lxc stop foo -f
   lxc start foo
   [ "$(lxc exec foo -- stat /mnt/a -c '%u:%g')" = "123:456" ] || false
+  lxc config device remove foo shiftfs
+  lxc stop foo -f
+
+  # Test shifted custom volumes
+  POOL=$(lxc profile device get default root pool)
+  lxc storage volume create "${POOL}" foo-shift security.shifted=true
+
+  lxc start foo
+  lxc launch testimage foo-priv -c security.privileged=true
+  lxc launch testimage foo-isol1 -c security.idmap.isolated=true
+  lxc launch testimage foo-isol2 -c security.idmap.isolated=true
+
+  lxc config device add foo shifted disk pool="${POOL}" source=foo-shift path=/mnt
+  lxc config device add foo-priv shifted disk pool="${POOL}" source=foo-shift path=/mnt
+  lxc config device add foo-isol1 shifted disk pool="${POOL}" source=foo-shift path=/mnt
+  lxc config device add foo-isol2 shifted disk pool="${POOL}" source=foo-shift path=/mnt
+
+  lxc exec foo -- touch /mnt/a
+  lxc exec foo -- chown 123:456 /mnt/a
+
+  [ "$(lxc exec foo -- stat /mnt/a -c '%u:%g')" = "123:456" ] || false
+  [ "$(lxc exec foo-priv -- stat /mnt/a -c '%u:%g')" = "123:456" ] || false
+  [ "$(lxc exec foo-isol1 -- stat /mnt/a -c '%u:%g')" = "123:456" ] || false
+  [ "$(lxc exec foo-isol2 -- stat /mnt/a -c '%u:%g')" = "123:456" ] || false
+
+  lxc delete -f foo-priv foo-isol1 foo-isol2
+  lxc config device remove foo shifted
+  lxc storage volume delete "${POOL}" foo-shift
   lxc stop foo -f
 }
