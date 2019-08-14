1ef94219e4179f982bedb841c7f479127f1c046f
Stéphane Graber <stgraber@ubuntu.com>
2019-08-14 10:19:42 -0400

    tests: Split server tests
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/serverconfig.sh
+++ test/suites/serverconfig.sh
@@ -1,8 +1,15 @@
 test_server_config() {
   LXD_SERVERCONFIG_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   spawn_lxd "${LXD_SERVERCONFIG_DIR}" true
-
   ensure_has_localhost_remote "${LXD_ADDR}"
+
+  test_server_config_password
+  test_server_config_access
+
+  kill_lxd "${LXD_SERVERCONFIG_DIR}"
+}
+
+test_server_config_password() {
   lxc config set core.trust_password 123456
 
   config=$(lxc config show)
@@ -11,18 +18,20 @@ test_server_config() {
 
   lxc config unset core.trust_password
   lxc config show | grep -q -v "trust_password"
+}
 
+test_server_config_access() {
   # test untrusted server GET
   my_curl -X GET "https://$(cat "${LXD_SERVERCONFIG_DIR}/lxd.addr")/1.0" | grep -v -q environment
 
   # test authentication type
   curl --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0" | jq .metadata.auth_methods | grep tls
+
   # only tls is enabled by default
   ! curl --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0" | jq .metadata.auth_methods | grep candid || false
   lxc config set candid.api.url "https://localhost:8081"
+
   # macaroons are also enabled
   curl --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0" | jq .metadata.auth_methods | grep candid
   lxc config unset candid.api.url
-
-  kill_lxd "${LXD_SERVERCONFIG_DIR}"
 }
