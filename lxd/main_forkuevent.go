477427b9fe0515ff0730fe284f6e2d7488505736
Thomas Parrott <thomas.parrott@canonical.com>
2019-08-16 16:06:44 +0100

    main/forkuevent: Fixes error when >3 arguments used (normal case)
    In the past the exit code of this program was not checked or logged, but now it is it reveals that this program's argument validation is incorrect.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/main_forkuevent.go
+++ lxd/main_forkuevent.go
@@ -221,8 +221,8 @@ func (c *cmdForkuevent) Command() *cobra.Command {
 
 	// pull
 	cmdInject := &cobra.Command{}
-	cmdInject.Use = "inject <PID> <len> <uevent>"
-	cmdInject.Args = cobra.ExactArgs(3)
+	cmdInject.Use = "inject <PID> <len> <uevent parts>..."
+	cmdInject.Args = cobra.MinimumNArgs(3)
 	cmdInject.RunE = c.Run
 	cmd.AddCommand(cmdInject)
 
