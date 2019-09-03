5a81bab7a9e7ae778ae4e9729c65bbd20a49e394
Thomas Parrott <thomas.parrott@canonical.com>
2019-09-03 13:29:34 +0100

    device/config/devices/sort: Makes device set sorting exported
    Removes Names() as not used.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/config/devices_test.go
+++ lxd/device/config/devices_test.go
@@ -7,16 +7,33 @@ import (
 
 func TestSortableDevices(t *testing.T) {
 	devices := Devices{
-		"1": Device{"type": "nic"},
-		"3": Device{"type": "disk", "path": "/foo/bar"},
-		"4": Device{"type": "disk", "path": "/foo"},
-		"2": Device{"type": "nic"},
+		"dev1": Device{"type": "nic"},
+		"dev3": Device{"type": "disk", "path": "/foo/bar"},
+		"dev4": Device{"type": "disk", "path": "/foo"},
+		"dev2": Device{"type": "nic"},
 	}
 
-	expected := []string{"4", "3", "1", "2"}
+	expectedSorted := DevicesSortable{
+		DeviceNamed{Name: "dev4", Config: Device{"type": "disk", "path": "/foo"}},
+		DeviceNamed{Name: "dev3", Config: Device{"type": "disk", "path": "/foo/bar"}},
+		DeviceNamed{Name: "dev1", Config: Device{"type": "nic"}},
+		DeviceNamed{Name: "dev2", Config: Device{"type": "nic"}},
+	}
 
-	result := devices.DeviceNames()
-	if !reflect.DeepEqual(result, expected) {
+	result := devices.Sorted()
+	if !reflect.DeepEqual(result, expectedSorted) {
 		t.Error("devices sorted incorrectly")
+	}
+
+	expectedReversed := DevicesSortable{
+		DeviceNamed{Name: "dev2", Config: Device{"type": "nic"}},
+		DeviceNamed{Name: "dev1", Config: Device{"type": "nic"}},
+		DeviceNamed{Name: "dev3", Config: Device{"type": "disk", "path": "/foo/bar"}},
+		DeviceNamed{Name: "dev4", Config: Device{"type": "disk", "path": "/foo"}},
+	}
+
+	result = devices.Reversed()
+	if !reflect.DeepEqual(result, expectedReversed) {
+		t.Error("devices reverse sorted incorrectly")
 	}
 }
