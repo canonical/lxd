2eb29c1dd3d922a9f4980a983ca7f058e9a89483
Stéphane Graber <stgraber@ubuntu.com>
2019-09-03 16:09:08 -0400

    shared/simplestreams: Rename index structs
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/simplestreams/index.go
+++ shared/simplestreams/index.go
@@ -1,15 +1,17 @@
 package simplestreams
 
-type SimpleStreamsIndex struct {
-	Format  string                              `json:"format"`
-	Index   map[string]SimpleStreamsIndexStream `json:"index"`
-	Updated string                              `json:"updated,omitempty"`
+// Stream represents the base structure of index.json
+type Stream struct {
+	Index   map[string]StreamIndex `json:"index"`
+	Updated string                 `json:"updated,omitempty"`
+	Format  string                 `json:"format"`
 }
 
-type SimpleStreamsIndexStream struct {
-	Updated  string   `json:"updated,omitempty"`
+// StreamIndex represents the Index entry inside index.json
+type StreamIndex struct {
 	DataType string   `json:"datatype"`
 	Path     string   `json:"path"`
-	Format   string   `json:"format,omitempty"`
+	Updated  string   `json:"updated,omitempty"`
 	Products []string `json:"products"`
+	Format   string   `json:"format,omitempty"`
 }
