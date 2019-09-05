235b5afb6597f799aec0a8c0ef67133ca2337eff
leela <52065244+leela52452@users.noreply.github.com>
2019-09-05 16:11:41 +0000

    typo ellipsis, added

--- lxc/profile.go
+++ lxc/profile.go
@@ -756,7 +756,7 @@ type cmdProfileSet struct {
 
 func (c *cmdProfileSet) Command() *cobra.Command {
 	cmd := &cobra.Command{}
-	cmd.Use = i18n.G("set [<remote>:]<profile> <key><value>...")
+	cmd.Use = i18n.G("set [<remote>:]<profile> <key><value>…")
 	cmd.Short = i18n.G("Set profile configuration keys")
 	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
 		`Set profile configuration keys
