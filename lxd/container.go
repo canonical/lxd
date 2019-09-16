31e214f05da08a32c92128ac2ed6b4815f0d655f
Thomas Parrott <thomas.parrott@canonical.com>
2019-09-16 12:50:50 +0100

    lxd/container: Embeds the Instance interface into the container interface
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/container.go
+++ lxd/container.go
@@ -214,122 +214,23 @@ func containerValidDevices(state *state.State, cluster *db.Cluster, instanceName
 
 // The container interface
 type container interface {
-	// Container actions
-	Freeze() error
-	Shutdown(timeout time.Duration) error
-	Start(stateful bool) error
-	Stop(stateful bool) error
-	Unfreeze() error
+	Instance
 
-	// Snapshots & migration & backups
-	Restore(sourceContainer container, stateful bool) error
 	/* actionScript here is a script called action.sh in the stateDir, to
 	 * be passed to CRIU as --action-script
 	 */
 	Migrate(args *CriuMigrationArgs) error
-	Snapshots() ([]container, error)
-	Backups() ([]backup, error)
 
-	// Config handling
-	Rename(newName string) error
-	Update(newConfig db.ContainerArgs, userRequested bool) error
-
-	Delete() error
-	Export(w io.Writer, properties map[string]string) error
-
-	// Live configuration
-	CGroupGet(key string) (string, error)
-	CGroupSet(key string, value string) error
-	VolatileSet(changes map[string]string) error
-
-	// File handling
-	FileExists(path string) error
-	FilePull(srcpath string, dstpath string) (int64, int64, os.FileMode, string, []string, error)
-	FilePush(type_ string, srcpath string, dstpath string, uid int64, gid int64, mode int, write string) error
-	FileRemove(path string) error
-
-	// Console - Allocate and run a console tty.
-	//
-	// terminal  - Bidirectional file descriptor.
-	//
-	// This function will not return until the console has been exited by
-	// the user.
-	Console(terminal *os.File) *exec.Cmd
 	ConsoleLog(opts lxc.ConsoleLogOptions) (string, error)
-	/* Command execution:
-		 * 1. passing in false for wait
-		 *    - equivalent to calling cmd.Run()
-		 * 2. passing in true for wait
-	         *    - start the command and return its PID in the first return
-	         *      argument and the PID of the attached process in the second
-	         *      argument. It's the callers responsibility to wait on the
-	         *      command. (Note. The returned PID of the attached process can not
-	         *      be waited upon since it's a child of the lxd forkexec command
-	         *      (the PID returned in the first return argument). It can however
-	         *      be used to e.g. forward signals.)
-	*/
-	Exec(command []string, env map[string]string, stdin *os.File, stdout *os.File, stderr *os.File, wait bool, cwd string, uid uint32, gid uint32) (*exec.Cmd, int, int, error)
 
 	// Status
-	Render() (interface{}, interface{}, error)
-	RenderFull() (*api.InstanceFull, interface{}, error)
-	RenderState() (*api.InstanceState, error)
-	IsPrivileged() bool
-	IsRunning() bool
-	IsFrozen() bool
-	IsEphemeral() bool
-	IsSnapshot() bool
-	IsStateful() bool
 	IsNesting() bool
 
 	// Hooks
 	OnStart() error
 	OnStopNS(target string, netns string) error
 	OnStop(target string) error
-	DeviceEventHandler(*device.RunConfig) error
 
-	// Properties
-	Id() int
-	Location() string
-	Project() string
-	Name() string
-	Type() instance.Type
-	Description() string
-	Architecture() int
-	CreationDate() time.Time
-	LastUsedDate() time.Time
-	ExpandedConfig() map[string]string
-	ExpandedDevices() config.Devices
-	LocalConfig() map[string]string
-	LocalDevices() config.Devices
-	Profiles() []string
-	InitPID() int
-	State() string
-	ExpiryDate() time.Time
-
-	// Paths
-	Path() string
-	RootfsPath() string
-	TemplatesPath() string
-	StatePath() string
-	LogFilePath() string
-	ConsoleBufferLogPath() string
-	LogPath() string
-	DevicesPath() string
-
-	// Storage
-	StoragePool() (string, error)
-
-	// Progress reporting
-	SetOperation(op *operation)
-
-	// FIXME: Those should be internal functions
-	// Needed for migration for now.
-	StorageStart() (bool, error)
-	StorageStop() (bool, error)
-	Storage() storage
-	TemplateApply(trigger string) error
-	DaemonState() *state.State
 	InsertSeccompUnixDevice(prefix string, m config.Device, pid int) error
 
 	CurrentIdmap() (*idmap.IdmapSet, error)
