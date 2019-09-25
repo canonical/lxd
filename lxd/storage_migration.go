fc1ab45867cf00059cb1dd5e8d764604e91a712f
Thomas Parrott <thomas.parrott@canonical.com>
2019-09-25 11:38:36 +0100

    lxd/storage/migration: Removes MigrationStorageSourceDriver interface
    This has been moved to the instance package.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage_migration.go
+++ lxd/storage_migration.go
@@ -17,29 +17,6 @@ import (
 	"github.com/lxc/lxd/shared/logger"
 )
 
-// MigrationStorageSourceDriver defines the functions needed to implement a
-// migration source driver.
-type MigrationStorageSourceDriver interface {
-	/* send any bits of the container/snapshots that are possible while the
-	 * container is still running.
-	 */
-	SendWhileRunning(conn *websocket.Conn, op *operation, bwlimit string, containerOnly bool) error
-
-	/* send the final bits (e.g. a final delta snapshot for zfs, btrfs, or
-	 * do a final rsync) of the fs after the container has been
-	 * checkpointed. This will only be called when a container is actually
-	 * being live migrated.
-	 */
-	SendAfterCheckpoint(conn *websocket.Conn, bwlimit string) error
-
-	/* Called after either success or failure of a migration, can be used
-	 * to clean up any temporary snapshots, etc.
-	 */
-	Cleanup()
-
-	SendStorageVolume(conn *websocket.Conn, op *operation, bwlimit string, storage storage, volumeOnly bool) error
-}
-
 type rsyncStorageSourceDriver struct {
 	container     Instance
 	snapshots     []Instance
