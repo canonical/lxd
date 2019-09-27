ca071bba84f6a4a1a75ae1e843eda74b24ff549f
Thomas Hipp <thomas.hipp@canonical.com>
2019-09-27 17:13:20 +0200

    lxd-agent: Add basic structure
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

---
+++ lxd-agent/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/x509"
+	"flag"
+	"log"
+	"path/filepath"
+
+	"github.com/lxc/lxd/lxd/vsock"
+	"github.com/lxc/lxd/shared"
+	"github.com/pkg/errors"
+)
+
+var tlsClientCertFile = filepath.Join("/", "media", "lxd_config", "server.crt")
+var tlsServerCertFile = filepath.Join("/", "media", "lxd_config", "agent.crt")
+var tlsServerKeyFile = filepath.Join("/", "media", "lxd_config", "agent.key")
+
+var debug bool
+
+// cert is the only client certificate which is authorized.
+var cert *x509.Certificate
+
+func main() {
+	flag.BoolVar(&debug, "debug", false, "Enable debug mode")
+	flag.Parse()
+
+	l, err := vsock.Listen(8443)
+	if err != nil {
+		log.Fatalln(errors.Wrap(err, "Failed to listen on vsock"))
+	}
+
+	cert, err = shared.ReadCert(tlsClientCertFile)
+	if err != nil {
+		log.Fatalln(errors.Wrap(err, "Failed to read client certificate"))
+	}
+
+	tlsConfig, err := ServerTLSConfig()
+	if err != nil {
+		log.Fatalln(errors.Wrap(err, "Failed to get TLS config"))
+	}
+
+	httpServer := RestServer(tlsConfig)
+
+	log.Println(httpServer.ServeTLS(networkTLSListener(l, tlsConfig), tlsServerCertFile, tlsServerKeyFile))
+}
