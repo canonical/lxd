e34cad822fefd7f4bbde8a03c305609a0b9c62fa
Stéphane Graber <stgraber@ubuntu.com>
2019-09-30 15:57:16 -0400

    lxd/state: Carry event server instances
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/state/state.go
+++ lxd/state/state.go
@@ -3,6 +3,7 @@ package state
 import (
 	"github.com/lxc/lxd/lxd/db"
 	"github.com/lxc/lxd/lxd/endpoints"
+	"github.com/lxc/lxd/lxd/events"
 	"github.com/lxc/lxd/lxd/maas"
 	"github.com/lxc/lxd/lxd/sys"
 )
@@ -11,21 +12,34 @@ import (
 // and the operating system. It's typically used by model entities such as
 // containers, volumes, etc. in order to perform changes.
 type State struct {
-	Node      *db.Node
-	Cluster   *db.Cluster
-	MAAS      *maas.Controller
-	OS        *sys.OS
+	// Databases
+	Node    *db.Node
+	Cluster *db.Cluster
+
+	// MAAS server
+	MAAS *maas.Controller
+
+	// OS access
+	OS *sys.OS
+
+	// LXD server
 	Endpoints *endpoints.Endpoints
+
+	// Event server
+	DevlxdEvents *events.Server
+	Events       *events.Server
 }
 
 // NewState returns a new State object with the given database and operating
 // system components.
-func NewState(node *db.Node, cluster *db.Cluster, maas *maas.Controller, os *sys.OS, endpoints *endpoints.Endpoints) *State {
+func NewState(node *db.Node, cluster *db.Cluster, maas *maas.Controller, os *sys.OS, endpoints *endpoints.Endpoints, events *events.Server, devlxdEvents *events.Server) *State {
 	return &State{
-		Node:      node,
-		Cluster:   cluster,
-		MAAS:      maas,
-		OS:        os,
-		Endpoints: endpoints,
+		Node:         node,
+		Cluster:      cluster,
+		MAAS:         maas,
+		OS:           os,
+		Endpoints:    endpoints,
+		DevlxdEvents: devlxdEvents,
+		Events:       events,
 	}
 }
