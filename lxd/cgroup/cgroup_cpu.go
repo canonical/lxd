a7a34a6ef4c6df69dbcd612b7dbcec84c37e2a95
Thomas Parrott <thomas.parrott@canonical.com>
2019-10-07 11:22:49 +0100

    lxd/cgroup: Adds cgroup package with CPU task balancing functions
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/cgroup/cgroup_cpu.go
@@ -0,0 +1,83 @@
+package cgroup
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// DeviceSchedRebalance channel for scheduling a CPU rebalance.
+var DeviceSchedRebalance = make(chan []string, 2)
+
+// TaskSchedulerTrigger triggers a CPU rebalance.
+func TaskSchedulerTrigger(srcType string, srcName string, srcStatus string) {
+	// Spawn a go routine which then triggers the scheduler
+	select {
+	case DeviceSchedRebalance <- []string{srcType, srcName, srcStatus}:
+	default:
+		// Channel is full, drop the event
+	}
+}
+
+// ParseCPU parses CPU allowances.
+func ParseCPU(cpuAllowance string, cpuPriority string) (string, string, string, error) {
+	var err error
+
+	// Parse priority
+	cpuShares := 0
+	cpuPriorityInt := 10
+	if cpuPriority != "" {
+		cpuPriorityInt, err = strconv.Atoi(cpuPriority)
+		if err != nil {
+			return "", "", "", err
+		}
+	}
+	cpuShares -= 10 - cpuPriorityInt
+
+	// Parse allowance
+	cpuCfsQuota := "-1"
+	cpuCfsPeriod := "100000"
+
+	if cpuAllowance != "" {
+		if strings.HasSuffix(cpuAllowance, "%") {
+			// Percentage based allocation
+			percent, err := strconv.Atoi(strings.TrimSuffix(cpuAllowance, "%"))
+			if err != nil {
+				return "", "", "", err
+			}
+
+			cpuShares += (10 * percent) + 24
+		} else {
+			// Time based allocation
+			fields := strings.SplitN(cpuAllowance, "/", 2)
+			if len(fields) != 2 {
+				return "", "", "", fmt.Errorf("Invalid allowance: %s", cpuAllowance)
+			}
+
+			quota, err := strconv.Atoi(strings.TrimSuffix(fields[0], "ms"))
+			if err != nil {
+				return "", "", "", err
+			}
+
+			period, err := strconv.Atoi(strings.TrimSuffix(fields[1], "ms"))
+			if err != nil {
+				return "", "", "", err
+			}
+
+			// Set limit in ms
+			cpuCfsQuota = fmt.Sprintf("%d", quota*1000)
+			cpuCfsPeriod = fmt.Sprintf("%d", period*1000)
+			cpuShares += 1024
+		}
+	} else {
+		// Default is 100%
+		cpuShares += 1024
+	}
+
+	// Deal with a potential negative score
+	if cpuShares < 0 {
+		cpuShares = 0
+	}
+
+	return fmt.Sprintf("%d", cpuShares), cpuCfsQuota, cpuCfsPeriod, nil
+}
