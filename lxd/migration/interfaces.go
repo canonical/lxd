1250b363f8cc2cdca1da9eddbeb766f95fc15a91
Thomas Parrott <thomas.parrott@canonical.com>
2019-10-09 16:02:51 +0100

    lxd/storage: Adds interfaces
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/migration/interfaces.go
@@ -0,0 +1,12 @@
+package migration
+
+// FIXME: empty stubs until we're ready to move the migration code over
+
+type SourceArgs struct {
+}
+
+type SinkArgs struct {
+}
+
+type StorageSourceDriver interface {
+}
