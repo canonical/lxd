71b2aa7e7ff189dfe6a145bb38ae3a7b4ccfa24d
Christian Brauner <christian.brauner@ubuntu.com>
2019-10-21 19:43:35 +0200

    Revert "unixfd: hide symbols"
    This reverts commit 4f0f0e968e3456c21637107a8f571e70cca7aa3b.
    
    This will be replaced with a more generic hammer.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- shared/netutils/unixfd.h
+++ shared/netutils/unixfd.h
@@ -1,25 +1,20 @@
 // +build linux
 // +build cgo
-
+//
 #ifndef LXD_UNIXFD_H
 #define LXD_UNIXFD_H
 
 #include <sys/socket.h>
 #include <sys/types.h>
 
-#include "../../lxd/include/compiler.h"
+extern int lxc_abstract_unix_send_fds(int fd, int *sendfds, int num_sendfds,
+				      void *data, size_t size);
 
-__hidden extern int lxc_abstract_unix_send_fds(int fd, int *sendfds,
-					       int num_sendfds, void *data,
-					       size_t size);
+extern ssize_t lxc_abstract_unix_recv_fds_iov(int fd, int *recvfds,
+					      int num_recvfds,
+					      struct iovec *iov, size_t iovlen);
 
-__hidden extern ssize_t lxc_abstract_unix_recv_fds_iov(int fd, int *recvfds,
-						       int num_recvfds,
-						       struct iovec *iov,
-						       size_t iovlen);
-
-__hidden extern ssize_t lxc_abstract_unix_recv_fds(int fd, int *recvfds,
-						   int num_recvfds, void *data,
-						   size_t size);
+extern ssize_t lxc_abstract_unix_recv_fds(int fd, int *recvfds, int num_recvfds,
+					  void *data, size_t size);
 
 #endif // LXD_UNIXFD_H
