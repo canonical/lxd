f4dbc9ca4b468420b29b8d721751486f9ca6da06
Thomas Parrott <thomas.parrott@canonical.com>
2019-10-24 13:02:01 +0100

    lxd/storage/drivers/utils/test: Updates tests
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/utils_test.go
+++ lxd/storage/drivers/utils_test.go
@@ -6,32 +6,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Test GetVolumeMountPoint
-func TestGetVolumeMountPoint(t *testing.T) {
+// Test GetVolumeMountPath
+func TestGetVolumeMountPath(t *testing.T) {
 	poolName := "testpool"
 
 	// Test custom volume.
-	path := GetVolumeMountPoint(poolName, VolumeTypeCustom, "testvol")
-	expected := GetPoolMountPoint(poolName) + "/custom/testvol"
+	path := GetVolumeMountPath(poolName, VolumeTypeCustom, "testvol")
+	expected := GetPoolMountPath(poolName) + "/custom/testvol"
 	assert.Equal(t, expected, path)
 
 	// Test custom volume snapshot.
-	path = GetVolumeMountPoint(poolName, VolumeTypeCustom, "testvol/snap1")
-	expected = GetPoolMountPoint(poolName) + "/custom-snapshots/testvol/snap1"
+	path = GetVolumeMountPath(poolName, VolumeTypeCustom, "testvol/snap1")
+	expected = GetPoolMountPath(poolName) + "/custom-snapshots/testvol/snap1"
 	assert.Equal(t, expected, path)
 
 	// Test image volume.
-	path = GetVolumeMountPoint(poolName, VolumeTypeImage, "fingerprint")
-	expected = GetPoolMountPoint(poolName) + "/images/fingerprint"
+	path = GetVolumeMountPath(poolName, VolumeTypeImage, "fingerprint")
+	expected = GetPoolMountPath(poolName) + "/images/fingerprint"
 	assert.Equal(t, expected, path)
 
 	// Test container volume.
-	path = GetVolumeMountPoint(poolName, VolumeTypeContainer, "testvol")
-	expected = GetPoolMountPoint(poolName) + "/containers/testvol"
+	path = GetVolumeMountPath(poolName, VolumeTypeContainer, "testvol")
+	expected = GetPoolMountPath(poolName) + "/containers/testvol"
 	assert.Equal(t, expected, path)
 
 	// Test virtual-machine volume.
-	path = GetVolumeMountPoint(poolName, VolumeTypeVM, "testvol")
-	expected = GetPoolMountPoint(poolName) + "/virtual-machines/testvol"
+	path = GetVolumeMountPath(poolName, VolumeTypeVM, "testvol")
+	expected = GetPoolMountPath(poolName) + "/virtual-machines/testvol"
 	assert.Equal(t, expected, path)
 }
