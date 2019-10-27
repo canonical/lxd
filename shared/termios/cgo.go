8b1ee1253138e7d331e48bcb45e31cd91d5992e4
Stéphane Graber <stgraber@ubuntu.com>
2019-10-27 15:55:01 +0100

    global: Drop -Wcast-align (breaks armhf)
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/termios/cgo.go
+++ shared/termios/cgo.go
@@ -4,7 +4,7 @@ package termios
 
 // #cgo CFLAGS: -std=gnu11 -Wvla -Werror -fvisibility=hidden -Winit-self
 // #cgo CFLAGS: -Wformat=2 -Wshadow -Wendif-labels -fasynchronous-unwind-tables
-// #cgo CFLAGS: -pipe -Wcast-align --param=ssp-buffer-size=4 -g -Wunused
+// #cgo CFLAGS: -pipe --param=ssp-buffer-size=4 -g -Wunused
 // #cgo CFLAGS: -Werror=implicit-function-declaration
 // #cgo CFLAGS: -Werror=return-type -Wendif-labels -Werror=overflow
 // #cgo CFLAGS: -Wnested-externs -fexceptions
