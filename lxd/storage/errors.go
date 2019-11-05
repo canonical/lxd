bf83679a0f6a24601248ad3f8c1ef59420f83a35
Thomas Parrott <thomas.parrott@canonical.com>
2019-11-05 13:17:02 +0000

    lxd/storage/errors: Adds ErrRunningQuotaResizeNotSupported error
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/errors.go
+++ lxd/storage/errors.go
@@ -9,3 +9,6 @@ var ErrNilValue = fmt.Errorf("Nil value provided")
 
 // ErrNotImplemented is the "Not implemented" error
 var ErrNotImplemented = fmt.Errorf("Not implemented")
+
+// ErrRunningQuotaResizeNotSupported is the "Running quota resize not supported" error.
+var ErrRunningQuotaResizeNotSupported = fmt.Errorf("Running quota resize not supported")
