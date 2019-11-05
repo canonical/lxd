a88c9da3610c13f0a142b7fa47177a601d4eab39
Thomas Parrott <thomas.parrott@canonical.com>
2019-11-05 09:12:51 +0000

    lxd/storage/drivers/errors: Removes unused error
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/errors.go
+++ lxd/storage/drivers/errors.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 )
 
-// ErrNilValue is the "Nil value provided" error
-var ErrNilValue = fmt.Errorf("Nil value provided")
-
 // ErrNotImplemented is the "Not implemented" error
 var ErrNotImplemented = fmt.Errorf("Not implemented")
 
