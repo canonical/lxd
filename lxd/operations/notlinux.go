a72849611a8653fa1f299b36c2ad4ddf80f1d6cb
Stéphane Graber <stgraber@ubuntu.com>
2019-11-08 15:13:53 -0500

    lxd/operations: Disconnect from DB on non-LXD
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ lxd/operations/notlinux.go
@@ -0,0 +1,37 @@
+// +build !linux !cgo agent
+
+package operations
+
+import (
+	"fmt"
+
+	"github.com/lxc/lxd/lxd/db"
+)
+
+func registerDBOperation(op *Operation, opType db.OperationType) error {
+	if op.state != nil {
+		return fmt.Errorf("registerDBOperation not supported on this platform")
+	}
+
+	return nil
+}
+
+func removeDBOperation(op *Operation) error {
+	if op.state != nil {
+		return fmt.Errorf("registerDBOperation not supported on this platform")
+	}
+
+	return nil
+}
+
+func getServerName(op *Operation) (string, error) {
+	if op.state != nil {
+		return "", fmt.Errorf("registerDBOperation not supported on this platform")
+	}
+
+	return "", nil
+}
+
+func (op *Operation) sendEvent(eventMessage interface{}) {
+	return
+}
