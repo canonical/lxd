d704ae2b7ce7301fb5b85bbcf1ae023895d82db9
Thomas Parrott <thomas.parrott@canonical.com>
2019-11-25 15:17:50 +0000

    lxd/storage/drivers/load: Adds RunningSnapshotFreeze to driver Info struct
    - Also adds comments to other Info fields.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/load.go
+++ lxd/storage/drivers/load.go
@@ -29,14 +29,15 @@ func Load(state *state.State, driverName string, name string, config map[string]
 
 // Info represents information about a storage driver.
 type Info struct {
-	Name               string
-	Version            string
-	Remote             bool
-	OptimizedImages    bool
-	PreservesInodes    bool
-	VolumeTypes        []VolumeType
-	BlockBacking       bool
-	RunningQuotaResize bool
+	Name                  string
+	Version               string
+	VolumeTypes           []VolumeType // Supported volume types.
+	Remote                bool         // Whether the driver uses a remote backing store.
+	OptimizedImages       bool         // Whether driver stores images as separate volume.
+	PreservesInodes       bool         // Whether driver preserves inodes when volumes are moved hosts.
+	BlockBacking          bool         // Whether driver uses block devices as backing store.
+	RunningQuotaResize    bool         // Whether quota resize is supported whilst instance running.
+	RunningSnapshotFreeze bool         // Whether instance should be frozen during snapshot if running.
 }
 
 // SupportedDrivers returns a list of supported storage drivers.
