06844770ae2f4e05f80965debea111f56d2d857e
Stéphane Graber <stgraber@ubuntu.com>
2019-11-28 23:39:16 -0500

    lxd/vm: Add a virtio graphics card
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/vm_qemu.go
+++ lxd/vm_qemu.go
@@ -990,6 +990,10 @@ driver = "virtio-scsi-pci"
 bus = "qemu_pcie1"
 addr = "0x0"
 
+# Graphics card
+[device]
+driver = "virtio-gpu"
+
 # Balloon driver
 [device "qemu_pcie2"]
 driver = "pcie-root-port"
