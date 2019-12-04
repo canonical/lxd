1bbbfaf0f448169746324718f89334ca437cb3d0
Stéphane Graber <stgraber@ubuntu.com>
2019-12-04 13:53:05 -0500

    lxd/util: Add HasFilesystem
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/util/kernel.go
+++ lxd/util/kernel.go
@@ -1,7 +1,10 @@
 package util
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/lxc/lxd/shared"
 )
@@ -15,4 +18,27 @@ func LoadModule(module string) error {
 
 	_, err := shared.RunCommand("modprobe", module)
 	return err
+}
+
+// HasFilesystem checks whether a given filesystem is already supported
+// by the kernel. Note that if the filesystem is a module, you may need to
+// load it first.
+func HasFilesystem(filesystem string) bool {
+	file, err := os.Open("/proc/filesystems")
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fields := strings.Fields(strings.TrimSpace(scanner.Text()))
+		entry := fields[len(fields)-1]
+
+		if entry == filesystem {
+			return true
+		}
+	}
+
+	return false
 }
