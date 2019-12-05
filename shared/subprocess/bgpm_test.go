915ce27d749cc1eeb99aaee24fe52adaa3aa14de
Rishabh Thakkar <rishabh.thakkar@gmail.com>
2019-12-05 15:35:45 -0600

    shared: Implemented Background Process Manager tests
    Signed-off-by: Rishabh Thakkar <rishabh.thakkar@gmail.com>
    
    shared: Implemented Background Process Manager tests to use shell
    
    Signed-off-by: Rishabh Thakkar <rishabh.thakkar@gmail.com>
    
    shared: fixed spelling mistake
    
    Signed-off-by: ribsthakkar <rishabh.thakkar@gmail.com>

---
+++ shared/subprocess/bgpm_test.go
@@ -0,0 +1,185 @@
+package subprocess
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestSignalHandling(t *testing.T) {
+	var a []string
+	a = append(a, "testscript/signal.sh")
+	var file *os.File
+	p, err := NewProcess("sh", a, "", "testscript/signal_out.txt", "")
+
+	if err != nil {
+		t.Error("Failed process creation: ", err)
+	}
+
+	err = p.Start()
+	if err != nil {
+		t.Error("Failed to start process ", err)
+	}
+
+	time.Sleep(2 * time.Second)
+	err = p.Reload()
+	if err != nil {
+		t.Error("Unable to Reload process: ", err)
+	}
+
+	time.Sleep(2 * time.Second)
+	err = p.Signal(10)
+	if err != nil {
+		t.Error("Unable to Signal process: ", err)
+	}
+
+	ecode, err := p.Wait()
+	if err != nil {
+		t.Error("Failed to get exit code ", err)
+	} else if ecode != 1 {
+		t.Error("Exit code is not 1: ", ecode)
+	}
+
+	file, err = os.OpenFile("testscript/signal_out.txt", os.O_RDWR, 0644)
+	if err != nil {
+		t.Error("Could not open file ", err)
+	}
+	defer file.Close()
+
+	var text = make([]byte, 1024)
+	for {
+		_, err = file.Read(text)
+		// Break if finally arrived at end of file
+		if err == io.EOF {
+			break
+		}
+
+		// Break if error occurred
+		if err != nil && err != io.EOF {
+			t.Error("Error in reading file ", err)
+		}
+	}
+
+	if !strings.Contains(string(text), "Called with signal 1") {
+		t.Errorf("Reload failed. File output mismatch. Got %s", string(text))
+	}
+
+	if !strings.Contains(string(text), "Called with signal 10") {
+		t.Errorf("Signal failed. File output mismatch. Got %s", string(text))
+	}
+
+	err = os.Remove("testscript/signal_out.txt")
+	if err != nil {
+		t.Error("Could not delete file ", err)
+	}
+}
+
+//tests newprocess, start, stop, save, import, restart, wait
+func TestStopRestart(t *testing.T) {
+	var a []string
+	a = append(a, "testscript/stoprestart.sh")
+
+	p, err := NewProcess("sh", a, "", "", "")
+	if err != nil {
+		t.Error("Failed process creation: ", err)
+	}
+
+	err = p.Start()
+	if err != nil {
+		t.Error("Failed to start process: ", err)
+	}
+
+	err = p.Stop()
+	if err != nil {
+		t.Error("Failed to stop process: ", err)
+	}
+
+	err = p.Save("testscript/test2.yaml")
+	if err != nil {
+		t.Error("Failed to save process: ", err)
+	}
+
+	p, err = ImportProcess("testscript/test2.yaml")
+	if err != nil {
+		t.Error("Failed to import process: ", err)
+	}
+
+	err = p.Start()
+	if err != nil {
+		t.Error("Failed to start process: ", err)
+	}
+
+	err = p.Restart()
+	if err != nil {
+		t.Error("Failed to restart process: ", err)
+	}
+
+	exitcode, err := p.Wait()
+	if err != nil {
+		t.Error("Could not wait for process: ", err)
+	} else if exitcode != 0 {
+		t.Error("Exit code expected to be 0")
+	}
+
+	err = os.Remove("testscript/test2.yaml")
+	if err != nil {
+		t.Error("Could not delete file: ", err)
+	}
+}
+
+func TestProcessStartWaitExit(t *testing.T) {
+	var a []string
+	var file *os.File
+	var exp string
+	var text []byte
+	a = append(a, "testscript/exit1.sh")
+	p, err := NewProcess("sh", a, "", "testscript/out.txt", "")
+	if err != nil {
+		t.Error("Failed process creation: ", err)
+	}
+
+	err = p.Start()
+	if err != nil {
+		t.Error("Failed to start process: ", err)
+	}
+
+	ecode, err := p.Wait()
+	if err != nil {
+		t.Error("Failed to get exit code: ", err)
+	} else if ecode != 1 {
+		t.Error("Exit code is not 1: ", ecode)
+	}
+
+	file, err = os.OpenFile("testscript/out.txt", os.O_RDWR, 0644)
+	if err != nil {
+		t.Error("Could not open file: ", err)
+	}
+	defer file.Close()
+
+	exp = "hello again\nwaiting now\n"
+	// Read file, line by line
+	text = make([]byte, len(exp))
+	for {
+		_, err = file.Read(text)
+		// Break if finally arrived at end of file
+		if err == io.EOF {
+			break
+		}
+		// Break if error occurred
+		if err != nil && err != io.EOF {
+			t.Error("Error reading file: ", err)
+		}
+	}
+
+	if string(text) != exp {
+		t.Errorf("File output mismatch Expected %s got %s", "hello again\nwaiting now\n", string(text))
+	}
+
+	// Cleanup
+	err = os.Remove("testscript/out.txt")
+	if err != nil {
+		t.Error("Could not delete file: ", err)
+	}
+}
