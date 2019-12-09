34c1204b02aea705c9f6a01d8ed25a53b51dd7fe
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-12-09 11:40:49 +0000

    Add test_database_no_disk_space
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- test/suites/database.sh
+++ test/suites/database.sh
@@ -82,3 +82,51 @@ EOF
 
   kill_lxd "${LXD_RESTORE_DIR}"
 }
+
+test_database_no_disk_space(){
+  # shellcheck disable=2039
+  local LXD_DIR
+
+  LXD_NOSPACE_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
+
+  # Mount a tmpfs with limited space in the global database directory and create
+  # a very big file in it, which will eventually cause database transactions to
+  # fail.
+  GLOBAL_DB_DIR="${LXD_NOSPACE_DIR}/database/global"
+  BIG_FILE="${GLOBAL_DB_DIR}/bigfile"
+  mkdir -p "${GLOBAL_DB_DIR}"
+  mount -t tmpfs -o size=67108864 tmpfs "${GLOBAL_DB_DIR}"
+  dd bs=1024 count=51200 if=/dev/zero of="${BIG_FILE}"
+
+  spawn_lxd "${LXD_NOSPACE_DIR}" true
+
+  (
+    set -e
+    # shellcheck disable=SC2034,SC2030
+    LXD_DIR="${LXD_NOSPACE_DIR}"
+
+    ensure_import_testimage
+    lxc init testimage c
+
+    # Set a custom user property with a big value, so we eventually eat up all
+    # available disk space in the database directory.
+    DATA="${LXD_NOSPACE_DIR}/data"
+    head -c 262144 < /dev/zero | tr '\0' '\141' > "${DATA}"
+    for i in $(seq 20); do
+        if ! lxc config set c "user.prop${i}" - < "${DATA}"; then
+            break
+        fi
+    done
+
+    # Commands that involve writing to the database keep failing.
+    ! lxc config set c "user.propX" - < "${DATA}" || false
+    ! lxc config set c "user.propY" - < "${DATA}" || false
+
+    # Removing the big file makes the database happy again.
+    rm "${BIG_FILE}"
+    lxc config set c "user.propZ" - < "${DATA}"
+  )
+
+  shutdown_lxd "${LXD_NOSPACE_DIR}"
+  umount "${GLOBAL_DB_DIR}"
+}
