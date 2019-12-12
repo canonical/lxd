8921dacdd7330be8b427909198f932d5fd8b0164
Stéphane Graber <stgraber@ubuntu.com>
2019-12-11 22:17:31 -0500

    lxd/iptables: Fix ebtables handling regression
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/iptables/xtables.go
+++ lxd/iptables/xtables.go
@@ -332,7 +332,7 @@ func matchEbtablesRule(activeRule []string, matchRule []string, deleteMode bool)
 
 		// Check the match rule field matches the active rule field.
 		// If they don't match, then this isn't one of our rules.
-		if activeRule[i] != matchRule[i] {
+		if strings.Replace(activeRule[i], "/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff", "", -1) != strings.Replace(matchRule[i], "/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff", "", -1) {
 			return false
 		}
 	}
