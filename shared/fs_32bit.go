dfbdc1c0b2fe6361d15feacec513bcd1932e2e5c
Stéphane Graber <stgraber@ubuntu.com>
2019-12-16 18:54:04 -0500

    shared: Handle btrfs in IsMountPoint
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ shared/fs_32bit.go
@@ -0,0 +1,7 @@
+// +build 386 arm ppc s390
+
+package shared
+
+const (
+	filesystemSuperMagicBtrfs = -1859950530
+)
---
+++ shared/fs_64bit.go
@@ -0,0 +1,7 @@
+// +build amd64 ppc64 ppc64le arm64 s390x
+
+package shared
+
+const (
+	filesystemSuperMagicBtrfs = 0x9123683E
+)
