993886156574bdf1bb62530acc6bddbf3bb925ed
Stéphane Graber <stgraber@ubuntu.com>
2020-01-08 15:57:05 -0500

    lxd/storage: Support deleting snapshots during restore
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/storage/drivers/errors.go
+++ lxd/storage/drivers/errors.go
@@ -12,3 +12,12 @@ var ErrUnknownDriver = fmt.Errorf("Unknown driver")
 
 // ErrNotSupported is the "Not supported" error
 var ErrNotSupported = fmt.Errorf("Not supported")
+
+// ErrDeleteSnapshots is a special error used to tell the backend to delete more recent snapshots
+type ErrDeleteSnapshots struct {
+	Snapshots []string
+}
+
+func (e ErrDeleteSnapshots) Error() string {
+	return fmt.Sprintf("More recent snapshots must be deleted: %+v", e.Snapshots)
+}
