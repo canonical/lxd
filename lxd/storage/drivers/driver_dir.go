6efab601fb17da856f4991d185048241320f1f0a
Thomas Parrott <thomas.parrott@canonical.com>
2020-01-15 14:21:39 +0000

    lxd/storage/drivers: Call d.validatePool in Validate function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_dir.go
+++ lxd/storage/drivers/driver_dir.go
@@ -91,7 +91,7 @@ func (d *dir) Delete(op *operations.Operation) error {
 
 // Validate checks that all provide keys are supported and that no conflicting or missing configuration is present.
 func (d *dir) Validate(config map[string]string) error {
-	return nil
+	return d.validatePool(config, nil)
 }
 
 // Update applies any driver changes required from a configuration change.
