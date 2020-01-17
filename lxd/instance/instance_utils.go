86f170af428bb3ba95eee871e12abd0f09349f1b
Thomas Parrott <thomas.parrott@canonical.com>
2020-01-17 15:42:21 +0000

    lxd/instance/instance/utils: Load function comments
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/instance_utils.go
+++ lxd/instance/instance_utils.go
@@ -35,12 +35,10 @@ import (
 // ProfileValidationName is an indicator that the instance is just being used for profile validation.
 const ProfileValidationName = ""
 
-// ValidDevices is linked from main.instanceValidDevices to validate device config. Currently
-// main.instanceValidDevices uses containerLXC internally and so cannot be moved from main package.
+// ValidDevices is linked from instance/load.validDevices to validate device config.
 var ValidDevices func(state *state.State, cluster *db.Cluster, instanceType instancetype.Type, instanceName string, devices deviceConfig.Devices, expanded bool) error
 
-// Load is linked from main.instanceLoad to allow different instance types to be load,
-// including containerLXC which currently cannot be moved from main package.
+// Load is linked from instance/load.load to allow different instance types to be loaded.
 var Load func(s *state.State, args db.InstanceArgs, profiles []api.Profile) (Instance, error)
 
 // NetworkGetLeaseAddresses is linked from main.networkGetLeaseAddresses to limit scope of moving
