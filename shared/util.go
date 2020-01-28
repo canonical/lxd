d1e4f6aa39d594e7c8dee1ee80ed90e9d5554ecb
Thomas Parrott <thomas.parrott@canonical.com>
2020-01-28 09:25:43 +0000

    shared/util: Modifies ValidHostname to return specific error
    This makes the error messages more helpful to users.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/util.go
+++ shared/util.go
@@ -647,33 +647,34 @@ func RunningInUserNS() bool {
 	return true
 }
 
-func ValidHostname(name string) bool {
+// ValidHostname checks the string is valid DNS hostname.
+func ValidHostname(name string) error {
 	// Validate length
 	if len(name) < 1 || len(name) > 63 {
-		return false
+		return fmt.Errorf("Name must be 1-63 characters long")
 	}
 
 	// Validate first character
 	if strings.HasPrefix(name, "-") {
-		return false
+		return fmt.Errorf(`Name must not start with "-" character`)
 	}
 
 	if _, err := strconv.Atoi(string(name[0])); err == nil {
-		return false
+		return fmt.Errorf("Name must not be a number")
 	}
 
 	// Validate last character
 	if strings.HasSuffix(name, "-") {
-		return false
+		return fmt.Errorf(`Name must not end with "-" character`)
 	}
 
 	// Validate the character set
 	match, _ := regexp.MatchString("^[-a-zA-Z0-9]*$", name)
 	if !match {
-		return false
+		return fmt.Errorf("Name can only contain alphanumeric and hyphen characters")
 	}
 
-	return true
+	return nil
 }
 
 // Spawn the editor with a temporary YAML file for editing configs
