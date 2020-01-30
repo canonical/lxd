a4ee64787ffcab2b8e994767494c146fe2a992cf
Thomas Parrott <thomas.parrott@canonical.com>
2020-01-30 13:44:49 +0000

    lxd/device/device/utils/instance: Adds instanceSupported function
    Helper function to check instance type matches instancetype.Any or one of the instance types supplied.
    
    The instancetype.Any is always supported to allow mixed-instance type profile validation to be supported.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/device_utils_instance.go
+++ lxd/device/device_utils_instance.go
@@ -6,6 +6,7 @@ import (
 
 	deviceConfig "github.com/lxc/lxd/lxd/device/config"
 	"github.com/lxc/lxd/lxd/instance"
+	"github.com/lxc/lxd/lxd/instance/instancetype"
 	"github.com/lxc/lxd/lxd/state"
 )
 
@@ -53,4 +54,21 @@ func instanceGetReservedDevices(s *state.State, m deviceConfig.Device) (map[stri
 	}
 
 	return reservedDevices, nil
+}
+
+// instanceSupported is a helper function to check instance type is supported for validation.
+// Always returns true if supplied instance type is Any, to support profile validation.
+func instanceSupported(instType instancetype.Type, supportedTypes ...instancetype.Type) bool {
+	// If instance type is Any, then profile validation is occurring and we need to support this.
+	if instType == instancetype.Any {
+		return true
+	}
+
+	for _, supportedType := range supportedTypes {
+		if instType == supportedType {
+			return true
+		}
+	}
+
+	return false
 }
