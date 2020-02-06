d01d09942dfcb8e20e2dd3796f8b4789eadf54e6
Thomas Parrott <thomas.parrott@canonical.com>
2020-02-06 09:15:17 +0000

    lxd/storage/drivers/driver/lvm: Makes lvm.vg_name required for mounting
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_lvm.go
+++ lxd/storage/drivers/driver_lvm.go
@@ -479,6 +479,10 @@ func (d *lvm) Update(changedConfig map[string]string) error {
 // Mount mounts the storage pool (this does nothing for external LVM pools, but for loopback image
 // LVM pools this creates a loop device).
 func (d *lvm) Mount() (bool, error) {
+	if d.config["lvm.vg_name"] == "" {
+		return false, fmt.Errorf("Cannot mount pool as %q is not specified", "lvm.vg_name")
+	}
+
 	// Open the loop file if the LVM device doesn't exist yet and the source points to a file.
 	if !shared.IsDir(fmt.Sprintf("/dev/%s", d.config["lvm.vg_name"])) && filepath.IsAbs(d.config["source"]) && !shared.IsBlockdevPath(d.config["source"]) {
 		loopFile, err := d.openLoopFile(d.config["source"])
