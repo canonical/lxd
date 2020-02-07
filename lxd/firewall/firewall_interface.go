a7a10efbcb8933677256142fd0920358b4f24e82
Thomas Parrott <thomas.parrott@canonical.com>
2020-02-07 13:05:24 +0000

    lxd/firewall/firewall/interface: Reworks firewall interface
    Makes it less iptables specific ready for nftables driver.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/firewall/firewall_interface.go
+++ lxd/firewall/firewall_interface.go
@@ -4,28 +4,19 @@ import (
 	"net"
 
 	deviceConfig "github.com/lxc/lxd/lxd/device/config"
-	"github.com/lxc/lxd/lxd/firewall/drivers"
 )
 
 // Firewall represents an LXD firewall.
 type Firewall interface {
-	// Lower-level Functions
-	NetworkClear(family drivers.Family, table drivers.Table, comment string) error
-	InstanceClear(family drivers.Family, table drivers.Table, comment string) error
-	VerifyIPv6Module() error
+	NetworkSetupForwardingPolicy(networkName string, ipVersion uint, allow bool) error
+	NetworkSetupOutboundNAT(networkName string, subnet *net.IPNet, srcIP net.IP, append bool) error
+	NetworkSetupDHCPDNSAccess(networkName string, ipVersion uint) error
+	NetworkSetupDHCPv4Checksum(networkName string) error
+	NetworkClear(networkName string, ipVersion uint) error
 
-	// Proxy Functions
-	InstanceProxySetupNAT(family drivers.Family, connType, address, port string, destAddr net.IP, destPort string, comment string) error
+	InstanceSetupBridgeFilter(projectName, instanceName, deviceName, parentName, hostName, hwAddr string, IPv4, IPv6 net.IP) error
+	InstanceClearBridgeFilter(projectName, instanceName, deviceName, parentName, hostName, hwAddr string, IPv4, IPv6 net.IP) error
 
-	// NIC Bridged Functions
-	InstanceNicBridgedRemoveFilters(m deviceConfig.Device, ipv4 net.IP, ipv6 net.IP) error
-	InstanceNicBridgedSetFilters(m deviceConfig.Device, ipv4 net.IP, ipv6 net.IP, comment string) error
-
-	// Network Functions
-	NetworkSetupAllowForwarding(family drivers.Family, name string, actionType drivers.Action) error
-	NetworkSetupNAT(family drivers.Family, name string, location drivers.Location, args ...string) error
-	NetworkSetupIPv4DNSOverrides(name string) error
-	NetworkSetupIPv4DHCPWorkaround(name string) error
-	NetworkSetupIPv6DNSOverrides(name string) error
-	NetworkSetupTunnelNAT(name string, location drivers.Location, overlaySubnet net.IPNet) error
+	InstanceSetupProxyNAT(projectName, instanceName, deviceName string, listen, connect *deviceConfig.ProxyAddress) error
+	InstanceClearProxyNAT(projectName, instanceName, deviceName string) error
 }
