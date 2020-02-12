197caf6c26df4b29ba4d2a95ce36b17f5d8de1fd
Thomas Parrott <thomas.parrott@canonical.com>
2020-02-12 17:06:41 +0000

    lxd/firewall/firewall/load: Detect which firewall driver to use
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/firewall/firewall_load.go
+++ lxd/firewall/firewall_load.go
@@ -4,8 +4,35 @@ import (
 	"github.com/lxc/lxd/lxd/firewall/drivers"
 )
 
+const driverXtables = "xtables"
+const driverNftables = "nftables"
+
 // New returns an appropriate firewall implementation.
+// Uses xtables if nftables isn't compatible or isn't in use already, otherwise uses nftables.
 func New() Firewall {
-	// TODO: Issue #6223: add startup logic to choose xtables or nftables
-	return drivers.XTables{}
+	nftables := drivers.Nftables{}
+	xtables := drivers.Xtables{}
+
+	// If nftables is compatible and already in use, then we prefer to use the nftables driver irrespective of
+	// whether xtables is in use or not.
+	nftablesCompat, nftablesInUse := nftables.Compat()
+	if nftablesCompat && nftablesInUse {
+		return nftables
+	} else if !nftablesCompat {
+		// Note: If nftables isn't compatible, we fallback to xtables without considering whether xtables
+		// is itself compatible. This continues the existing behaviour of allowing LXD to start with
+		// potentially an incomplete firewall backend, so that only networks and instances using those
+		// features will fail to start later.
+		return xtables
+	}
+
+	// If xtables is compatible and already in use, then we prefer to stick with the xtables driver rather than
+	// mix the use of firewall drivers on the system.
+	xtablesCompat, xtablesInUse := xtables.Compat()
+	if xtablesCompat && xtablesInUse {
+		return xtables
+	}
+
+	// Otherwise prefer nftables as default.
+	return nftables
 }
