3e4e0ce70c223136e7e21d61c871841e6f6507c1
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-02-26 13:25:38 +0000

    lxd/project: Check that the project's "limits.memory" is honored when creating an instance
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/project/limits.go
+++ lxd/project/limits.go
@@ -5,7 +5,9 @@ import (
 	"strconv"
 
 	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/api"
+	"github.com/lxc/lxd/shared/units"
 	"github.com/pkg/errors"
 )
 
@@ -18,6 +20,17 @@ func CheckLimitsUponInstanceCreation(tx *db.ClusterTx, projectName string, req a
 	}
 
 	err = checkInstanceCountLimit(project, len(instances), req.Type)
+	if err != nil {
+		return err
+	}
+
+	// Add the instance being created.
+	instances = append(instances, db.Instance{
+		Profiles: req.Profiles,
+		Config:   req.Config,
+	})
+
+	err = checkAggregateInstanceLimits(tx, project, instances, profiles)
 	if err != nil {
 		return err
 	}
@@ -53,6 +66,44 @@ func checkInstanceCountLimit(project *api.Project, instanceCount int, instanceTy
 	return nil
 }
 
+// Check that we would not violate the project limits if we were to commit the
+// given instances and profiles.
+func checkAggregateInstanceLimits(tx *db.ClusterTx, project *api.Project, instances []db.Instance, profiles []db.Profile) error {
+	// List of config keys for which we need to check aggregate values
+	// across all project instances.
+	aggregateKeys := []string{}
+	for key := range project.Config {
+		if shared.StringInSlice(key, []string{"limits.memory"}) {
+			aggregateKeys = append(aggregateKeys, key)
+		}
+	}
+	if len(aggregateKeys) == 0 {
+		return nil
+	}
+
+	instances = expandInstancesConfig(instances, profiles)
+
+	totals, err := getTotalsAcrossInstances(instances, aggregateKeys)
+	if err != nil {
+		return err
+	}
+
+	for _, key := range aggregateKeys {
+		parser := aggregateLimitConfigValueParsers[key]
+		max, err := parser(project.Config[key])
+		if err != nil {
+			return err
+		}
+		if totals[key] > max {
+			return fmt.Errorf(
+				"Reached maximum aggregate value %s for %q in project %s",
+				project.Config[key], key, project.Name)
+		}
+	}
+
+	return nil
+}
+
 // Fetch the given project from the database along with its profiles and instances.
 func fetchProject(tx *db.ClusterTx, projectName string) (*api.Project, []db.Profile, []db.Instance, error) {
 	project, err := tx.ProjectGet(projectName)
@@ -82,4 +133,82 @@ func fetchProject(tx *db.ClusterTx, projectName string) (*api.Project, []db.Prof
 	}
 
 	return project, profiles, instances, nil
+}
+
+// Expand the configuration of the given instances, taking the give project
+// profiles into account.
+func expandInstancesConfig(instances []db.Instance, profiles []db.Profile) []db.Instance {
+	expandedInstances := make([]db.Instance, len(instances))
+
+	// Index of all profiles by name.
+	profilesByName := map[string]db.Profile{}
+	for _, profile := range profiles {
+		profilesByName[profile.Name] = profile
+	}
+
+	for i, instance := range instances {
+		profiles := make([]api.Profile, len(instance.Profiles))
+
+		for j, name := range instance.Profiles {
+			profile := profilesByName[name]
+			profiles[j] = *db.ProfileToAPI(&profile)
+		}
+
+		expandedInstances[i] = instance
+		expandedInstances[i].Config = db.ProfilesExpandConfig(instance.Config, profiles)
+	}
+
+	return expandedInstances
+}
+
+// Sum of the effective instance-level value for the given limits across all
+// project instances. If excludeInstance is not the empty string, exclude the
+// instance with that name.
+func getTotalsAcrossInstances(instances []db.Instance, keys []string) (map[string]int64, error) {
+	totals := map[string]int64{}
+
+	for _, key := range keys {
+		totals[key] = 0
+	}
+
+	for _, instance := range instances {
+		limits, err := getInstanceLimits(instance, keys)
+		if err != nil {
+			return nil, err
+		}
+		for _, key := range keys {
+			totals[key] += limits[key]
+		}
+	}
+
+	return totals, nil
+}
+
+// Return the effective instance-level values for the limits with the given
+// keys.
+func getInstanceLimits(instance db.Instance, keys []string) (map[string]int64, error) {
+	limits := map[string]int64{}
+
+	for _, key := range keys {
+		value, ok := instance.Config[key]
+		if !ok || value == "" {
+			return nil, fmt.Errorf(
+				"Instance %s in project %s has no '%s' config, either directly or via a profile",
+				instance.Name, instance.Project, key)
+		}
+		parser := aggregateLimitConfigValueParsers[key]
+		limit, err := parser(value)
+		if err != nil {
+			return nil, errors.Wrapf(
+				err, "Parse '%s' for instance %s in project %s",
+				key, instance.Name, instance.Project)
+		}
+		limits[key] = limit
+	}
+
+	return limits, nil
+}
+
+var aggregateLimitConfigValueParsers = map[string]func(string) (int64, error){
+	"limits.memory": units.ParseByteSizeString,
 }
