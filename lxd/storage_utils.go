dfc0eb6ba5b1e1eb89dcc3aa56f0bb7268b36d7c
Thomas Parrott <thomas.parrott@canonical.com>
2020-02-27 14:59:01 +0000

    lxd/storage/utils: Remove unused functions
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage_utils.go
+++ lxd/storage_utils.go
@@ -6,52 +6,8 @@ import (
 
 	"golang.org/x/sys/unix"
 
-	"github.com/lxc/lxd/lxd/instance"
-	driver "github.com/lxc/lxd/lxd/storage"
 	"github.com/lxc/lxd/shared"
-	"github.com/lxc/lxd/shared/logger"
 )
-
-func shrinkVolumeFilesystem(s storage, volumeType int, fsType string, devPath string, mntpoint string, byteSize int64, data interface{}) (func() (bool, error), error) {
-	var cleanupFunc func() (bool, error)
-	switch fsType {
-	case "xfs":
-		logger.Errorf("XFS filesystems cannot be shrunk: dump, mkfs, and restore are required")
-		return nil, fmt.Errorf("xfs filesystems cannot be shrunk: dump, mkfs, and restore are required")
-	case "btrfs":
-		fallthrough
-	case "": // if not specified, default to ext4
-		fallthrough
-	case "ext4":
-		switch volumeType {
-		case storagePoolVolumeTypeContainer:
-			c := data.(instance.Instance)
-			ourMount, err := c.StorageStop()
-			if err != nil {
-				return nil, err
-			}
-			if !ourMount {
-				cleanupFunc = c.StorageStart
-			}
-		case storagePoolVolumeTypeCustom:
-			ourMount, err := s.StoragePoolVolumeUmount()
-			if err != nil {
-				return nil, err
-			}
-			if !ourMount {
-				cleanupFunc = s.StoragePoolVolumeMount
-			}
-		default:
-			return nil, fmt.Errorf(`Resizing not implemented for storage volume type %d`, volumeType)
-		}
-
-	default:
-		return nil, fmt.Errorf(`Shrinking not supported for filesystem type "%s"`, fsType)
-	}
-
-	err := driver.ShrinkFileSystem(fsType, devPath, mntpoint, byteSize)
-	return cleanupFunc, err
-}
 
 // MkfsOptions represents options for filesystem creation.
 type mkfsOptions struct {
