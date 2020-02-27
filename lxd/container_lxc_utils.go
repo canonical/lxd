a6087ce3906287ad6cfeea6b51b92aea6135e658
Thomas Parrott <thomas.parrott@canonical.com>
2020-02-27 17:41:40 +0000

    lxd/container/lxc/utils: Removes idmapsetFromString
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/container_lxc_utils.go
+++ lxd/container_lxc_utils.go
@@ -6,20 +6,6 @@ import (
 	"github.com/lxc/lxd/shared/idmap"
 )
 
-func idmapsetFromString(idmapString string) (*idmap.IdmapSet, error) {
-	lastIdmap := new(idmap.IdmapSet)
-	err := json.Unmarshal([]byte(idmapString), &lastIdmap.Idmap)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(lastIdmap.Idmap) == 0 {
-		return nil, nil
-	}
-
-	return lastIdmap, nil
-}
-
 func idmapsetToJSON(idmapSet *idmap.IdmapSet) (string, error) {
 	idmapBytes, err := json.Marshal(idmapSet.Idmap)
 	if err != nil {
