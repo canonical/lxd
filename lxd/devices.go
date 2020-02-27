a8750234f8f37ac9b971e06b39a12da4bb663e30
Thomas Parrott <thomas.parrott@canonical.com>
2020-02-27 17:42:47 +0000

    lxd/devices: inst.RegisterDevices usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/devices.go
+++ lxd/devices.go
@@ -560,40 +560,16 @@ func deviceEventListener(s *state.State) {
 func devicesRegister(s *state.State) {
 	instances, err := instance.LoadNodeAll(s, instancetype.Container)
 	if err != nil {
-		logger.Error("Problem loading containers list", log.Ctx{"err": err})
+		logger.Error("Problem loading instances list", log.Ctx{"err": err})
 		return
 	}
 
-	for _, instanceIf := range instances {
-		c, ok := instanceIf.(*containerLXC)
-		if !ok {
-			logger.Errorf("Instance is not container type")
+	for _, inst := range instances {
+		if !inst.IsRunning() {
 			continue
 		}
 
-		if !c.IsRunning() {
-			continue
-		}
-
-		devices := c.ExpandedDevices()
-		for _, dev := range devices.Sorted() {
-			d, _, err := c.deviceLoad(dev.Name, dev.Config)
-			if err == device.ErrUnsupportedDevType {
-				continue
-			}
-
-			if err != nil {
-				logger.Error("Failed to load device to register", log.Ctx{"err": err, "container": c.Name(), "device": dev.Name})
-				continue
-			}
-
-			// Check whether device wants to register for any events.
-			err = d.Register()
-			if err != nil {
-				logger.Error("Failed to register device", log.Ctx{"err": err, "container": c.Name(), "device": dev.Name})
-				continue
-			}
-		}
+		inst.RegisterDevices()
 	}
 }
 
