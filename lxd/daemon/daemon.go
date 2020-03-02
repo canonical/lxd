49901fff78b8b8c78e97ba5d21b7177d37add977
Thomas Parrott <thomas.parrott@canonical.com>
2020-03-02 11:18:59 +0000

    lxd/daemon: Moves shared mount state to use daemon.SharedMountsSetup var
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/daemon/daemon.go
+++ lxd/daemon/daemon.go
@@ -5,3 +5,6 @@ var Debug bool
 
 // Verbose indicates if daemon is started with verbose mode.
 var Verbose bool
+
+// SharedMountsSetup indicates if daemon has setup shared mounts.
+var SharedMountsSetup bool
