ba0bc5e0b616bd3c4292725f92927628056aed65
Thomas Parrott <thomas.parrott@canonical.com>
2020-03-04 13:02:25 +0000

    lxd/project/project: Adds StorageVolumeProject function
    This function returns the project name that the storage volume should use, based on whether the project has the `features.storage.volumes` flag enabled.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/project/project.go
+++ lxd/project/project.go
@@ -3,6 +3,12 @@ package project
 import (
 	"fmt"
 	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/shared"
+	"github.com/lxc/lxd/shared/api"
 )
 
 // Default is the string used for a default project.
@@ -47,4 +53,39 @@ func StorageVolume(projectName string, storageVolumeName string) string {
 func StorageVolumeParts(projectStorageVolumeName string) (string, string) {
 	parts := strings.SplitN(projectStorageVolumeName, "_", 2)
 	return parts[0], parts[1]
+}
+
+// StorageVolumeProject returns the project name to use to for the volume based on the requested project.
+// For custom volume type, if the project specified has the "features.storage.volumes" flag enabled then the
+// project name is returned, otherwise the default project name is returned. For all other volume types the
+// supplied project name is returned.
+func StorageVolumeProject(c *db.Cluster, projectName string, volumeType int) (string, error) {
+	// Non-custom volumes always use the project specified.
+	if volumeType != db.StoragePoolVolumeTypeCustom {
+		return projectName, nil
+	}
+
+	var project *api.Project
+	var err error
+
+	err = c.Transaction(func(tx *db.ClusterTx) error {
+		project, err = tx.ProjectGet(projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return "", errors.Wrapf(err, "Failed to load project %q", projectName)
+	}
+
+	// Custom volumes only use the project specified if the project has the features.storage.volumes feature
+	// enabled, otherwise the legacy behaviour of using the default project for custom volumes is used.
+	if shared.IsTrue(project.Config["features.storage.volumes"]) {
+		return projectName, nil
+	}
+
+	return Default, nil
 }
