193d15431bc0d65e6a87ddcbfdcfba6184699cc3
Stéphane Graber <stgraber@ubuntu.com>
2020-03-05 00:00:37 +0100

    lxd/vm: Tweak to mount field names
    Rename to better line up with usual kernel names for those arguments.
    Also set JSON field names so the JSON file looks more like a normal LXD one.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/instance/instancetype/instance_vmagentmount.go
+++ lxd/instance/instancetype/instance_vmagentmount.go
@@ -2,8 +2,8 @@ package instancetype
 
 // VMAgentMount defines mounts to perform inside VM via agent.
 type VMAgentMount struct {
-	Source     string
-	TargetPath string
-	FSType     string
-	Opts       []string
+	Source  string   `json:"source"`
+	Target  string   `json:"target"`
+	FSType  string   `json:"fstype"`
+	Options []string `json:"options"`
 }
