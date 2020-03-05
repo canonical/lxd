fc19f32d7769bf13540a3bbc3d120d687bafc980
Thomas Parrott <thomas.parrott@canonical.com>
2020-03-05 17:03:20 +0000

    lxd/storage/backend/lxd/patches: Adds custom volume rename patch to add project prefix
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_btrfs.go
+++ lxd/storage/drivers/driver_btrfs.go
@@ -29,10 +29,11 @@ type btrfs struct {
 func (d *btrfs) load() error {
 	// Register the patches.
 	d.patches = map[string]func() error{
-		"storage_create_vm":       nil,
-		"storage_zfs_mount":       nil,
-		"storage_create_vm_again": nil,
-		"storage_zfs_volmode":     nil,
+		"storage_create_vm":                        nil,
+		"storage_zfs_mount":                        nil,
+		"storage_create_vm_again":                  nil,
+		"storage_zfs_volmode":                      nil,
+		"storage_rename_custom_volume_add_project": nil,
 	}
 
 	// Done if previously loaded.
