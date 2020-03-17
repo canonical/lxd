canonical/lxd

commit fc8b6c11b331000ba107c72e9bb656359ac87df9

lxd/storage/drivers/driver/lvm/volumes: Updates VolumeSnapshots to use lvs for snapshot list
Rather than listing the directories under /var/lib/lxd/storage/<pool>/<volume type>s-snapshots for snapshot lists, switches to using the lvs command to output logical volumes that match the prefix used for volume snapshots.

This is so if the volume has snapshots on the LVM storage layer, but the snapshot directory has been removed, they are still returned in the list.

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/storage/drivers/driver_lvm_volumes.go
@@ -1,10 +1,14 @@
 package drivers
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"math"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/pkg/errors"
 	"golang.org/x/sys/unix"
@@ -728,10 +732,50 @@ func (d *lvm) UnmountVolumeSnapshot(snapVol Volume, op *operations.Operation) (b
 
 // VolumeSnapshots returns a list of snapshots for the volume.
 func (d *lvm) VolumeSnapshots(vol Volume, op *operations.Operation) ([]string, error) {
-	// We use the vfsVolumeSnapshots rather than inspecting the logical volumes themselves because the origin
+	fullVolName := d.lvmFullVolumeName(vol.volType, vol.contentType, vol.name)
+
+	// We use the volume list rather than inspecting the logical volumes themselves because the origin
 	// property of an LVM snapshot can be removed/changed when restoring snapshots, such that they are no
-	// marked as origin of the parent volume.
-	return genericVFSVolumeSnapshots(d, vol, op)
+	// marked as origin of the parent volume. Instead we use prefix matching on the volume names to find the
+	// snapshot volumes.
+	cmd := exec.Command("lvs", "--noheadings", "-o", "lv_name", d.config["lvm.vg_name"])
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	snapshots := []string{}
+	scanner := bufio.NewScanner(stdout)
+	prefix := fmt.Sprintf("%s-", fullVolName)
+	for scanner.Scan() {
+		snapLine := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(snapLine, prefix) {
+			// Remove volume name prefix (including snapshot delimiter) and unescape snapshot name.
+			snapshots = append(snapshots, strings.Replace(strings.TrimPrefix(snapLine, prefix), "--", "-", -1))
+		}
+	}
+
+	errMsg, err := ioutil.ReadAll(stderr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to get snapshot list for volume %q: %v", fullVolName, strings.TrimSpace(string(errMsg)))
+	}
+
+	return snapshots, nil
 }
 
 // RestoreVolume restores a volume from a snapshot.

commit 96b028d5a6daad3926b5a972307b78876cbcb75c

lxd/storage/drivers/driver/lvm/volumes: Fixes LVM VM snapshot list
This bug was introduced in fc8b6c11b331000ba107c72e9bb656359ac87df9 due to the way that VM snapshot LVs are named differently than container snapshot LVs.

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/storage/drivers/driver_lvm_volumes.go
@@ -734,8 +734,6 @@ func (d *lvm) UnmountVolumeSnapshot(snapVol Volume, op *operations.Operation) (b
 
 // VolumeSnapshots returns a list of snapshots for the volume.
 func (d *lvm) VolumeSnapshots(vol Volume, op *operations.Operation) ([]string, error) {
-	fullVolName := d.lvmFullVolumeName(vol.volType, vol.contentType, vol.name)
-
 	// We use the volume list rather than inspecting the logical volumes themselves because the origin
 	// property of an LVM snapshot can be removed/changed when restoring snapshots, such that they are no
 	// marked as origin of the parent volume. Instead we use prefix matching on the volume names to find the
@@ -757,10 +755,29 @@ func (d *lvm) VolumeSnapshots(vol Volume, op *operations.Operation) ([]string, e
 	}
 
 	snapshots := []string{}
-	scanner := bufio.NewScanner(stdout)
+	fullVolName := d.lvmFullVolumeName(vol.volType, vol.contentType, vol.name)
+
+	// If block volume, remove the block suffix ready for comparison with LV list.
+	if vol.IsVMBlock() {
+		fullVolName = strings.TrimSuffix(fullVolName, lvmBlockVolSuffix)
+	}
+
 	prefix := fmt.Sprintf("%s-", fullVolName)
+
+	scanner := bufio.NewScanner(stdout)
 	for scanner.Scan() {
 		snapLine := strings.TrimSpace(scanner.Text())
+
+		// If block volume, skip any LVs that don't end with the block suffix, and then for those that do
+		// remove the block suffix so that they can be compared with the fullVolName prefix.
+		if vol.IsVMBlock() {
+			if !strings.HasSuffix(snapLine, lvmBlockVolSuffix) {
+				continue // Ignore non-block volumes.
+			}
+
+			snapLine = strings.TrimSuffix(snapLine, lvmBlockVolSuffix)
+		}
+
 		if strings.HasPrefix(snapLine, prefix) {
 			// Remove volume name prefix (including snapshot delimiter) and unescape snapshot name.
 			snapshots = append(snapshots, strings.Replace(strings.TrimPrefix(snapLine, prefix), "--", "-", -1))