97a1107276c63640e52116b09164668a0365ed2e
Thomas Parrott <thomas.parrott@canonical.com>
2020-03-19 13:29:22 +0000

    shared/instancewriter/instance/tar/writer: Modifies WriteFile to accept a file name arg
    Rather than an offset location from the srcPath name.
    
    This way files can be added to the tarball using an arbitrary name.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/instancewriter/instance_tar_writer.go
+++ shared/instancewriter/instance_tar_writer.go
@@ -28,8 +28,8 @@ func NewInstanceTarWriter(writer io.Writer, idmapSet *idmap.IdmapSet) *InstanceT
 	return ctw
 }
 
-// WriteFile adds a file to the tarball.
-func (ctw *InstanceTarWriter) WriteFile(offset int, path string, fi os.FileInfo) error {
+// WriteFile adds a file to the tarball with the specified name using the srcPath file as the contents of the file.
+func (ctw *InstanceTarWriter) WriteFile(name string, srcPath string, fi os.FileInfo) error {
 	var err error
 	var major, minor uint32
 	var nlink int
@@ -37,9 +37,9 @@ func (ctw *InstanceTarWriter) WriteFile(offset int, path string, fi os.FileInfo)
 
 	link := ""
 	if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
-		link, err = os.Readlink(path)
+		link, err = os.Readlink(srcPath)
 		if err != nil {
-			return errors.Wrap(err, "Failed to resolve symlink")
+			return errors.Wrapf(err, "Failed to resolve symlink for %q", srcPath)
 		}
 	}
 
@@ -53,16 +53,16 @@ func (ctw *InstanceTarWriter) WriteFile(offset int, path string, fi os.FileInfo)
 		return errors.Wrap(err, "Failed to create tar info header")
 	}
 
-	hdr.Name = path[offset:]
+	hdr.Name = name
 	if fi.IsDir() || fi.Mode()&os.ModeSymlink == os.ModeSymlink {
 		hdr.Size = 0
 	} else {
 		hdr.Size = fi.Size()
 	}
 
-	hdr.Uid, hdr.Gid, major, minor, ino, nlink, err = shared.GetFileStat(path)
+	hdr.Uid, hdr.Gid, major, minor, ino, nlink, err = shared.GetFileStat(srcPath)
 	if err != nil {
-		return errors.Wrap(err, "Failed to get file stat")
+		return errors.Wrapf(err, "Failed to get file stat %q", srcPath)
 	}
 
 	// Unshift the id under rootfs/ for unpriv containers.
@@ -91,9 +91,9 @@ func (ctw *InstanceTarWriter) WriteFile(offset int, path string, fi os.FileInfo)
 
 	// Handle xattrs (for real files only).
 	if link == "" {
-		hdr.Xattrs, err = shared.GetAllXattr(path)
+		hdr.Xattrs, err = shared.GetAllXattr(srcPath)
 		if err != nil {
-			return errors.Wrapf(err, "Failed to read xattr for %q", path)
+			return errors.Wrapf(err, "Failed to read xattr for %q", srcPath)
 		}
 	}
 
@@ -102,14 +102,14 @@ func (ctw *InstanceTarWriter) WriteFile(offset int, path string, fi os.FileInfo)
 	}
 
 	if hdr.Typeflag == tar.TypeReg {
-		f, err := os.Open(path)
+		f, err := os.Open(srcPath)
 		if err != nil {
-			return errors.Wrap(err, "Failed to open the file")
+			return errors.Wrapf(err, "Failed to open file %q", srcPath)
 		}
 		defer f.Close()
 
 		if _, err := io.Copy(ctw.tarWriter, f); err != nil {
-			return errors.Wrap(err, "Failed to copy file content")
+			return errors.Wrapf(err, "Failed to copy file content %q", srcPath)
 		}
 	}
 
