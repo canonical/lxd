a66fac91ab72a21ac6f72bd8456080f133cbdfce
Thomas Hipp <thomas.hipp@canonical.com>
2020-03-20 17:49:55 +0100

    test: Extend PKI test
    This extends the PKI tests by testing CA-trusted client certificates.
    
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- test/suites/pki.sh
+++ test/suites/pki.sh
@@ -16,15 +16,24 @@ test_pki() {
         ./pkitool --initca
         ./pkitool --server 127.0.0.1
         ./pkitool lxd-client
+        ./pkitool lxd-client-revoked
+        # This will revoke the certificate but fail in the end as it tries to then verify the
+        # revoked certificate.
+        ./revoke-full lxd-client-revoked || true
     else
         ./easyrsa init-pki
         echo "lxd" | ./easyrsa build-ca nopass
+        ./easyrsa gen-crl
         ./easyrsa build-server-full 127.0.0.1 nopass
         ./easyrsa build-client-full lxd-client nopass
+        ./easyrsa build-client-full lxd-client-revoked nopass
         mkdir keys
         cp pki/private/* keys/
         cp pki/issued/* keys/
         cp pki/ca.crt keys/
+        echo "yes" | ./easyrsa revoke lxd-client-revoked
+        ./easyrsa gen-crl
+        cp pki/crl.pem keys/
     fi
   )
 
@@ -34,6 +43,7 @@ test_pki() {
   cat "${TEST_DIR}/pki/keys/127.0.0.1.crt" "${TEST_DIR}/pki/keys/ca.crt" > "${LXD5_DIR}/server.crt"
   cp "${TEST_DIR}/pki/keys/127.0.0.1.key" "${LXD5_DIR}/server.key"
   cp "${TEST_DIR}/pki/keys/ca.crt" "${LXD5_DIR}/server.ca"
+  cp "${TEST_DIR}/pki/keys/crl.pem" "${LXD5_DIR}/ca.crl"
   spawn_lxd "${LXD5_DIR}" true
   LXD5_ADDR=$(cat "${LXD5_DIR}/lxd.addr")
 
@@ -47,9 +57,34 @@ test_pki() {
   (
     set -e
     export LXD_CONF=${LXC5_DIR}
+
+    # Try adding remote using an incorrect password
+    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=bar || false
+
+    # Add remote using the correct password
     lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=foo
     lxc_remote info pki-lxd:
     lxc_remote remote remove pki-lxd
+
+    # Add remote using a CA-signed client certificate, and not providing a password
+    LXD_DIR=${LXD5_DIR} lxc config set core.trust_ca_certificates true
+    lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate
+    lxc_remote info pki-lxd:
+    lxc_remote remote remove pki-lxd
+
+    # Add remote using a CA-signed client certificate, and providing an incorrect password
+    lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=bar
+    lxc_remote info pki-lxd:
+    lxc_remote remote remove pki-lxd
+
+    cp "${TEST_DIR}/pki/keys/lxd-client-revoked.crt" "${LXC5_DIR}/client.crt"
+    cp "${TEST_DIR}/pki/keys/lxd-client-revoked.key" "${LXC5_DIR}/client.key"
+
+    # Try adding a remote using a revoked client certificate, and the correct password
+    lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=foo
+
+    # Try adding a remote using a revoked client certificate, and an incorrect password
+    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=bar || false
   )
 
   # Confirm that a normal, non-PKI certificate doesn't
