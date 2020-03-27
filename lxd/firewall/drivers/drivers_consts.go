34d7df82bd24e92efb5e73633aaa07db9924d88a
Thomas Parrott <thomas.parrott@canonical.com>
2020-03-27 11:53:24 +0000

    lxd/firewall/drivers/drivers/consts: Adds FilterIPv4All constant
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/firewall/drivers/drivers_consts.go
+++ lxd/firewall/drivers/drivers_consts.go
@@ -2,3 +2,6 @@ package drivers
 
 // FilterIPv6All used to indicate to firewall package to filter all IPv6 traffic.
 const FilterIPv6All = "::"
+
+// FilterIPv4All used to indicate to firewall package to filter all IPv4 traffic.
+const FilterIPv4All = "0.0.0.0"
