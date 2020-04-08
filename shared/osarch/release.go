cf9e539a7fcb354e8e5ba4478ad9e7148a4bd129
Stéphane Graber <stgraber@ubuntu.com>
2020-04-07 21:02:02 -0400

    shared/osarch: Coding style
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/osarch/release.go
+++ shared/osarch/release.go
@@ -13,6 +13,7 @@ func GetLSBRelease() (map[string]string, error) {
 	if os.IsNotExist(err) {
 		return getLSBRelease("/usr/lib/os-release")
 	}
+
 	return osRelease, err
 }
 
@@ -23,10 +24,12 @@ func getLSBRelease(filename string) (map[string]string, error) {
 	if err != nil {
 		return osRelease, err
 	}
+
 	for i, line := range strings.Split(string(data), "\n") {
 		if len(line) == 0 {
 			continue
 		}
+
 		if strings.HasPrefix(line, "#") {
 			continue
 		}
@@ -35,6 +38,7 @@ func getLSBRelease(filename string) (map[string]string, error) {
 		if len(tokens) != 2 {
 			return osRelease, fmt.Errorf("%s: invalid format on line %d", filename, i+1)
 		}
+
 		osRelease[tokens[0]] = strings.Trim(tokens[1], `'"`)
 	}
 
