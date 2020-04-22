a641fd66daee754f5df2729acf3bf8d4bce0fd26
Thomas Parrott <thomas.parrott@canonical.com>
2020-04-22 11:54:12 +0100

    lxd/storage/drivers/errors: Adds ErrCannotBeShrunk error
    Used to indicate to the backend that the volume cannot be shrunk.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/errors.go
+++ lxd/storage/drivers/errors.go
@@ -13,6 +13,9 @@ var ErrUnknownDriver = fmt.Errorf("Unknown driver")
 // ErrNotSupported is the "Not supported" error
 var ErrNotSupported = fmt.Errorf("Not supported")
 
+// ErrCannotBeShrunk is the "Cannot be shrunk" error
+var ErrCannotBeShrunk = fmt.Errorf("Cannot be shrunk")
+
 // ErrDeleteSnapshots is a special error used to tell the backend to delete more recent snapshots
 type ErrDeleteSnapshots struct {
 	Snapshots []string
