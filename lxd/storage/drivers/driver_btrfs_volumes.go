43b1a64cf91afaed7ea21dcf6141347557c4be26
Thomas Parrott <thomas.parrott@canonical.com>
2020-04-30 15:18:57 +0100

    lxd/storage/drivers/driver/btrfs/volumes: No need to move GPT header if no filler used in CreateVolume
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_btrfs_volumes.go
+++ lxd/storage/drivers/driver_btrfs_volumes.go
@@ -65,8 +65,8 @@ func (d *btrfs) CreateVolume(vol Volume, filler *VolumeFiller, op *operations.Op
 			return err
 		}
 
-		// Move the GPT alt header to end of disk if needed.
-		if vol.IsVMBlock() {
+		// Move the GPT alt header to end of disk if needed and if filler specified.
+		if vol.IsVMBlock() && filler != nil && filler.Fill != nil {
 			err = d.moveGPTAltHeader(rootBlockPath)
 			if err != nil {
 				return err
