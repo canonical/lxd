dab2be684849255eeb8309898d127fc9fd15fa9c
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-04-30 14:57:33 +0100

    lxd/db: Drop unused ConfigValueSet
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db/config.go
+++ lxd/db/config.go
@@ -25,29 +25,3 @@ func (c *ClusterTx) Config() (map[string]string, error) {
 func (c *ClusterTx) UpdateConfig(values map[string]string) error {
 	return query.UpdateConfig(c.tx, "config", values)
 }
-
-// ConfigValueSet is a convenience to set a cluster-level key/value config pair
-// in a single transaction.
-func ConfigValueSet(c *Cluster, key string, value string) error {
-	err := c.Transaction(func(tx *ClusterTx) error {
-		_, err := tx.tx.Exec("DELETE FROM config WHERE key=?", key)
-		if err != nil {
-			return err
-		}
-
-		if value != "" {
-			str := `INSERT INTO config (key, value) VALUES (?, ?);`
-			stmt, err := tx.tx.Prepare(str)
-			if err != nil {
-				return err
-			}
-			defer stmt.Close()
-			_, err = stmt.Exec(key, value)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-	return err
-}
