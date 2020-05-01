2e3c5a23b4ed3124b8ba529b65bea0a707feb919
Thomas Parrott <thomas.parrott@canonical.com>
2020-05-01 11:13:01 +0100

    lxd/storage/drivers/driver/btrfs/volumes: Updates SetVolumeQuota to be byte oriented internally
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_btrfs_volumes.go
+++ lxd/storage/drivers/driver_btrfs_volumes.go
@@ -449,20 +449,27 @@ func (d *btrfs) GetVolumeUsage(vol Volume) (int64, error) {
 }
 
 // SetVolumeQuota sets the quota on the volume.
+// Does nothing if supplied with an empty/zero size for block volumes, and for filesystem volumes removes quota.
 func (d *btrfs) SetVolumeQuota(vol Volume, size string, op *operations.Operation) error {
+	// Convert to bytes.
+	sizeBytes, err := units.ParseByteSizeString(size)
+	if err != nil {
+		return err
+	}
+
 	// For VM block files, resize the file if needed.
 	if vol.contentType == ContentTypeBlock {
+		// Do nothing if size isn't specified.
+		if sizeBytes <= 0 {
+			return nil
+		}
+
 		rootBlockPath, err := d.GetVolumeDiskPath(vol)
 		if err != nil {
 			return err
 		}
 
-		// If size not specified in volume config, then use pool's default block size.
-		if size == "" || size == "0" {
-			size = defaultBlockSize
-		}
-
-		resized, err := genericVFSResizeBlockFile(rootBlockPath, size)
+		resized, err := genericVFSResizeBlockFile(rootBlockPath, sizeBytes)
 		if err != nil {
 			return err
 		}
@@ -482,12 +489,6 @@ func (d *btrfs) SetVolumeQuota(vol Volume, size string, op *operations.Operation
 
 	// For non-VM block volumes, set filesystem quota.
 	volPath := vol.MountPath()
-
-	// Convert to bytes.
-	sizeBytes, err := units.ParseByteSizeString(size)
-	if err != nil {
-		return err
-	}
 
 	// Try to locate an existing quota group.
 	qgroup, _, err := d.getQGroup(volPath)
