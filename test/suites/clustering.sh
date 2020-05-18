c5f436137987837a978dd18c36b51903085de4bf
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-05-18 12:06:56 +0100

    test/clustering: Make sure that a killed voter can't dsirupt current leader
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- test/suites/clustering.sh
+++ test/suites/clustering.sh
@@ -1755,7 +1755,16 @@ test_clustering_rebalance() {
   LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node4 | grep -q "status: Online"
   LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node4 | grep -q "database: true"
 
+  # Respawn the second node. It won't be able to disrupt the current leader,
+  # since dqlite uses pre-vote.
+  respawn_lxd_cluster_member "${ns2}" "${LXD_TWO_DIR}"
+  sleep 15
+
+  LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node2 | grep -q "status: Online"
+  LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node2 | grep -q "database: false"
+
   LXD_DIR="${LXD_ONE_DIR}" lxd shutdown
+  LXD_DIR="${LXD_TWO_DIR}" lxd shutdown
   LXD_DIR="${LXD_THREE_DIR}" lxd shutdown
   LXD_DIR="${LXD_FOUR_DIR}" lxd shutdown
   sleep 0.5
