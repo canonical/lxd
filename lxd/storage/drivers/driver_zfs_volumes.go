58f1881b8d65c8f8749a3ac8950dee904b38d178
Thomas Parrott <thomas.parrott@canonical.com>
2020-05-26 15:04:14 +0100

    lxd/storage/drivers/driver/zfs/volumes: d.runFiller usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_zfs_volumes.go
+++ lxd/storage/drivers/driver_zfs_volumes.go
@@ -174,31 +174,28 @@ func (d *zfs) CreateVolume(vol Volume, filler *VolumeFiller, op *operations.Oper
 	err := vol.MountTask(func(mountPath string, op *operations.Operation) error {
 		// Run the volume filler function if supplied.
 		if filler != nil && filler.Fill != nil {
-			if vol.contentType == ContentTypeFS {
-				// Run the filler.
-				err := filler.Fill(vol, "")
-				if err != nil {
-					return err
-				}
-			} else {
+			var err error
+			var devPath string
+
+			if vol.contentType == ContentTypeBlock {
 				// Get the device path.
-				devPath, err := d.GetVolumeDiskPath(vol)
+				devPath, err = d.GetVolumeDiskPath(vol)
 				if err != nil {
 					return err
 				}
+			}
 
-				// Run the filler.
-				err = filler.Fill(vol, devPath)
+			// Run the filler.
+			err = d.runFiller(vol, devPath, filler)
+			if err != nil {
+				return err
+			}
+
+			// Move the GPT alt header to end of disk if needed.
+			if vol.IsVMBlock() {
+				err = d.moveGPTAltHeader(devPath)
 				if err != nil {
 					return err
-				}
-
-				// Move the GPT alt header to end of disk if needed.
-				if vol.IsVMBlock() {
-					err = d.moveGPTAltHeader(devPath)
-					if err != nil {
-						return err
-					}
 				}
 			}
 		}
