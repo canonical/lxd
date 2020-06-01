9330eebb5b5bf1757997ad67a0104c5b6776bea6
Thomas Parrott <thomas.parrott@canonical.com>
2020-06-01 11:12:54 +0100

    lxd/firewall/drivers/drivers/nftables: Drops tagged vlan frames when using IP filtering
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/firewall/drivers/drivers_nftables.go
+++ lxd/firewall/drivers/drivers_nftables.go
@@ -299,6 +299,11 @@ func (d Nftables) InstanceSetupBridgeFilter(projectName string, instanceName str
 		"hwAddrHex":      fmt.Sprintf("0x%s", hex.EncodeToString(mac)),
 	}
 
+	// Filter VLAN tagged frames when using IP filtering.
+	if IPv4 != nil || IPv6 != nil {
+		tplFields["vlanFilter"] = true
+	}
+
 	if IPv4 != nil {
 		if IPv4.String() == FilterIPv4All {
 			tplFields["ipv4FilterAll"] = true
