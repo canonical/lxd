a64b928f0284ee9d225173607cbdf5e04bc9683c
Thomas Parrott <thomas.parrott@canonical.com>
2020-06-19 16:24:22 +0100

    test: Updates udhcpd args to ensure process quits one lease acquired
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/suites/container_devices_nic_bridged_filtering.sh
+++ test/suites/container_devices_nic_bridged_filtering.sh
@@ -172,7 +172,7 @@ test_container_devices_nic_bridged_filtering() {
 
   # Check DHCPv4 allocation still works.
   lxc exec "${ctPrefix}A" -- ip link set dev eth0 address "${ctAMAC}" up
-  lxc exec "${ctPrefix}A" -- udhcpc -i eth0 -n
+  lxc exec "${ctPrefix}A" -- udhcpc -i eth0 -n -q
   lxc exec "${ctPrefix}A" -- ip a flush dev eth0
   lxc exec "${ctPrefix}A" -- ip a add 192.0.2.2/24 dev eth0
 
@@ -348,7 +348,7 @@ test_container_devices_nic_bridged_filtering() {
   fi
 
   if [ "$busyboxUdhcpc6" = "1" ]; then
-      lxc exec "${ctPrefix}A" -- udhcpc6 -i eth0 -n
+      lxc exec "${ctPrefix}A" -- udhcpc6 -i eth0 -n -q
   fi
 
   lxc exec "${ctPrefix}A" -- ip -6 a flush dev eth0
