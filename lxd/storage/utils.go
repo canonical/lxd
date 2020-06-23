4d6721732a3c25cf635094bd43a5e7a7da8c5ad0
Thomas Hipp <thomas.hipp@canonical.com>
2020-06-23 18:42:00 +0200

    lxd/storage/utils: Add content type conversion functions
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/storage/utils.go
+++ lxd/storage/utils.go
@@ -136,6 +136,42 @@ func InstanceTypeToVolumeType(instType instancetype.Type) (drivers.VolumeType, e
 	return "", fmt.Errorf("Invalid instance type")
 }
 
+// VolumeContentTypeToDBContentType converts volume type to internal code.
+func VolumeContentTypeToDBContentType(contentType drivers.ContentType) (int, error) {
+	switch contentType {
+	case drivers.ContentTypeBlock:
+		return db.StoragePoolVolumeContentTypeBlock, nil
+	case drivers.ContentTypeFS:
+		return db.StoragePoolVolumeContentTypeFS, nil
+	}
+
+	return -1, fmt.Errorf("Invalid volume content type")
+}
+
+// VolumeDBContentTypeToContentType converts internal content type DB code to driver representation.
+func VolumeDBContentTypeToContentType(volDBType int) (drivers.ContentType, error) {
+	switch volDBType {
+	case db.StoragePoolVolumeContentTypeBlock:
+		return drivers.ContentTypeBlock, nil
+	case db.StoragePoolVolumeContentTypeFS:
+		return drivers.ContentTypeFS, nil
+	}
+
+	return "", fmt.Errorf("Invalid volume content type")
+}
+
+// VolumeContentTypeNameToContentType converts volume content type string internal code.
+func VolumeContentTypeNameToContentType(contentTypeName string) (int, error) {
+	switch contentTypeName {
+	case db.StoragePoolVolumeContentTypeNameFS:
+		return db.StoragePoolVolumeContentTypeFS, nil
+	case db.StoragePoolVolumeContentTypeNameBlock:
+		return db.StoragePoolVolumeContentTypeBlock, nil
+	}
+
+	return -1, fmt.Errorf("Invalid storage volume content type name")
+}
+
 // VolumeDBCreate creates a volume in the database.
 func VolumeDBCreate(s *state.State, project, poolName, volumeName, volumeDescription, volumeTypeName string, snapshot bool, volumeConfig map[string]string, expiryDate time.Time) error {
 	// Convert the volume type name to our internal integer representation.
