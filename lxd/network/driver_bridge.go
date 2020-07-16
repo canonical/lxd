07e8d179caaa606218f005ec222c4a6dfe987da8
Thomas Parrott <thomas.parrott@canonical.com>
2020-07-16 10:57:24 +0100

    lxd/network/driver/bridge: Updates to use contextual logger
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/driver_bridge.go
+++ lxd/network/driver_bridge.go
@@ -24,7 +24,6 @@ import (
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/api"
 	log "github.com/lxc/lxd/shared/log15"
-	"github.com/lxc/lxd/shared/logger"
 	"github.com/lxc/lxd/shared/subprocess"
 	"github.com/lxc/lxd/shared/version"
 )
@@ -328,6 +327,8 @@ func (n *bridge) setup(oldConfig map[string]string) error {
 		return nil
 	}
 
+	n.logger.Debug("Setting up network")
+
 	// Create directory
 	if !shared.PathExists(shared.VarPath("networks", n.name)) {
 		err := os.MkdirAll(shared.VarPath("networks", n.name), 0711)
@@ -439,13 +440,13 @@ func (n *bridge) setup(oldConfig map[string]string) error {
 	if n.config["bridge.driver"] != "openvswitch" {
 		err = BridgeVLANFilterSetStatus(n.name, "1")
 		if err != nil {
-			logger.Warnf("%v", err)
+			n.logger.Warn(fmt.Sprintf("%v", err))
 		}
 
 		// Set the default PVID for new ports to 1.
 		err = BridgeVLANSetDefaultPVID(n.name, "1")
 		if err != nil {
-			logger.Warnf("%v", err)
+			n.logger.Warn(fmt.Sprintf("%v", err))
 		}
 	}
 
@@ -461,6 +462,7 @@ func (n *bridge) setup(oldConfig map[string]string) error {
 			entry = strings.TrimSpace(entry)
 			iface, err := net.InterfaceByName(entry)
 			if err != nil {
+				n.logger.Warn("Skipping attaching missing external interface", log.Ctx{"interface": entry})
 				continue
 			}
 
@@ -711,7 +713,7 @@ func (n *bridge) setup(oldConfig map[string]string) error {
 		subnetSize, _ := subnet.Mask.Size()
 
 		if subnetSize > 64 {
-			logger.Warn("IPv6 networks with a prefix larger than 64 aren't properly supported by dnsmasq", log.Ctx{"network": n.name})
+			n.logger.Warn("IPv6 networks with a prefix larger than 64 aren't properly supported by dnsmasq")
 		}
 
 		// Update the dnsmasq config
@@ -1474,7 +1476,7 @@ func (n *bridge) HandleHeartbeat(heartbeatData *cluster.APIHeartbeat) error {
 		return err
 	}
 
-	logger.Infof("Refreshing forkdns peers for %v", n.name)
+	n.logger.Info("Refreshing forkdns peers")
 
 	cert := n.state.Endpoints.NetworkCert()
 	for _, node := range heartbeatData.Members {
@@ -1508,7 +1510,7 @@ func (n *bridge) HandleHeartbeat(heartbeatData *cluster.APIHeartbeat) error {
 	curList, err := ForkdnsServersList(n.name)
 	if err != nil {
 		// Only warn here, but continue on to regenerate the servers list from cluster info.
-		logger.Warnf("Failed to load existing forkdns server list: %v", err)
+		n.logger.Warn("Failed to load existing forkdns server list", log.Ctx{"err": err})
 	}
 
 	// If current list is same as cluster list, nothing to do.
@@ -1521,7 +1523,7 @@ func (n *bridge) HandleHeartbeat(heartbeatData *cluster.APIHeartbeat) error {
 		return err
 	}
 
-	logger.Infof("Updated forkdns server list for '%s': %v", n.name, addresses)
+	n.logger.Info("Updated forkdns server list", log.Ctx{"nodes": addresses})
 	return nil
 }
 
