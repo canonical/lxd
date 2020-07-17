2aead5eee92b5b8b9a69fcf46ce5c07cab1c1af8
Thomas Parrott <thomas.parrott@canonical.com>
2020-07-17 15:42:16 +0100

    lxd/device/nic/bridged: Validates network is type bridge
    Now that different NIC types support the "network" key.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/nic_bridged.go
+++ lxd/device/nic_bridged.go
@@ -81,6 +81,11 @@ func (d *nicBridged) validateConfig(instConf instance.ConfigReader) error {
 		if err != nil {
 			return errors.Wrapf(err, "Error loading network config for %q", d.config["network"])
 		}
+
+		if n.Type() != "bridge" {
+			return fmt.Errorf("Specified network must be of type bridge")
+		}
+
 		netConfig := n.Config()
 
 		if d.config["ipv4.address"] != "" {
@@ -127,6 +132,7 @@ func (d *nicBridged) validateConfig(instConf instance.ConfigReader) error {
 			d.config["mtu"] = netConfig["bridge.mtu"]
 		}
 
+		// Copy certain keys verbatim from the network's settings.
 		inheritKeys := []string{"maas.subnet.ipv4", "maas.subnet.ipv6"}
 		for _, inheritKey := range inheritKeys {
 			if _, found := netConfig[inheritKey]; found {
