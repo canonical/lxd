92608ca1586546954e6f89e228cfcaab97c6956b
Thomas Parrott <thomas.parrott@canonical.com>
2020-07-29 12:38:03 +0100

    lxd/network/network/utils: Adds OVN instance device port helpers
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/network_utils.go
+++ lxd/network/network_utils.go
@@ -866,3 +866,26 @@ func randomHwaddr(r *rand.Rand) string {
 
 	return ret.String()
 }
+
+// OVNInstanceDevicePortAdd adds a logical port to the OVN network's internal switch and returns the logical
+// port name for use linking an OVS port on the integration bridge to the logical switch port.
+func OVNInstanceDevicePortAdd(network Network, instanceID int, deviceName string, mac net.HardwareAddr, ips []net.IP) (openvswitch.OVNSwitchPort, error) {
+	// Check network is of type OVN.
+	n, ok := network.(*ovn)
+	if !ok {
+		return "", fmt.Errorf("Network is not OVN type")
+	}
+
+	return n.instanceDevicePortAdd(instanceID, deviceName, mac, ips)
+}
+
+// OVNInstanceDevicePortDelete deletes a logical port from the OVN network's internal switch.
+func OVNInstanceDevicePortDelete(network Network, instanceID int, deviceName string) error {
+	// Check network is of type OVN.
+	n, ok := network.(*ovn)
+	if !ok {
+		return fmt.Errorf("Network is not OVN type")
+	}
+
+	return n.instanceDevicePortDelete(instanceID, deviceName)
+}
