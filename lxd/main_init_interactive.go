a48ae9bbb2a23acc15a61e0eb1b9637d420a16a5
Thomas Parrott <thomas.parrott@canonical.com>
2020-08-03 18:00:18 +0100

    lxd/main/init/interactive: Switches to network.ValidateName for bridge validation
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/main_init_interactive.go
+++ lxd/main_init_interactive.go
@@ -351,7 +351,7 @@ func (c *cmdInit) askNetworking(config *cmdInitData, d lxd.InstanceServer) error
 		net.Config = map[string]string{}
 
 		// Network name
-		net.Name = cli.AskString("What should the new bridge be called? [default=lxdbr0]: ", "lxdbr0", network.ValidNetworkName)
+		net.Name = cli.AskString("What should the new bridge be called? [default=lxdbr0]: ", "lxdbr0", func(netName string) error { return network.ValidateName(netName, "bridge") })
 		_, _, err := d.GetNetwork(net.Name)
 		if err == nil {
 			fmt.Printf("The requested network bridge \"%s\" already exists. Please choose another name.\n", net.Name)
