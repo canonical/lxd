7d06080ff15642d7ecd2c89569a3a7c9a584c634
Thomas Parrott <thomas.parrott@canonical.com>
2020-08-06 14:57:48 +0100

    Revert "lxd/locking: Renames variables to make them generic"
    This reverts commit 5630989206e2e18dc2b33d1da1fcc7606e2217ed.

--- lxd/storage/locking/lock.go
+++ lxd/storage/locking/lock.go
@@ -1,38 +1,44 @@
 package locking
 
 import (
+	"fmt"
 	"sync"
 )
 
-// locks is a hashmap that allows functions to check whether the operation they are about to perform
-// is already in progress. If it is the channel can be used to wait for the operation to finish. If it is not, the
-// function that wants to perform the operation should store its code in the hashmap.
+// ongoingOperationMap is a hashmap that allows functions to check whether the
+// operation they are about to perform is already in progress. If it is the
+// channel can be used to wait for the operation to finish. If it is not, the
+// function that wants to perform the operation should store its code in the
+// hashmap.
 // Note that any access to this map must be done while holding a lock.
-var locks = map[string]chan struct{}{}
+var ongoingOperationMap = map[string]chan struct{}{}
 
-// locksMutex is used to access locks safely.
-var locksMutex sync.Mutex
+// ongoingOperationMapLock is used to access ongoingOperationMap.
+var ongoingOperationMapLock sync.Mutex
 
-// Lock creates a lock for a specific storage volume to allow activities that require exclusive access to occur.
-// Will block until the lock is established. On success, it returns an unlock function which needs to be called to
-// unlock the lock.
-func Lock(lockName string) func() {
+// Lock creates a lock for a specific storage volume to allow activities that
+// require exclusive access to take place. Will block until the lock is
+// established. On success, it returns an unlock function which needs to be
+// called to unlock the lock.
+func Lock(poolName string, volType string, volName string) func() {
+	lockID := fmt.Sprintf("%s/%s/%s", poolName, volType, volName)
+
 	for {
 		// Get exclusive access to the map and see if there is already an operation ongoing.
-		locksMutex.Lock()
-		waitCh, ok := locks[lockName]
+		ongoingOperationMapLock.Lock()
+		waitCh, ok := ongoingOperationMap[lockID]
 
 		if !ok {
 			// No ongoing operation, create a new channel to indicate our new operation.
 			waitCh = make(chan struct{})
-			locks[lockName] = waitCh
-			locksMutex.Unlock()
+			ongoingOperationMap[lockID] = waitCh
+			ongoingOperationMapLock.Unlock()
 
 			// Return a function that will complete the operation.
 			return func() {
 				// Get exclusive access to the map.
-				locksMutex.Lock()
-				doneCh, ok := locks[lockName]
+				ongoingOperationMapLock.Lock()
+				doneCh, ok := ongoingOperationMap[lockID]
 
 				// Load our existing operation.
 				if ok {
@@ -41,19 +47,19 @@ func Lock(lockName string) func() {
 					close(doneCh)
 
 					// Remove our existing operation entry from the map.
-					delete(locks, lockName)
+					delete(ongoingOperationMap, lockID)
 				}
 
 				// Release the lock now that the done channel is closed and the
 				// map entry has been deleted, this will allow any waiting users
 				// to try and get access to the map to create a new operation.
-				locksMutex.Unlock()
+				ongoingOperationMapLock.Unlock()
 			}
 		}
 
 		// An existing operation is ongoing, lets wait for that to finish and then try
 		// to get exlusive access to create a new operation again.
-		locksMutex.Unlock()
+		ongoingOperationMapLock.Unlock()
 		<-waitCh
 	}
 }
