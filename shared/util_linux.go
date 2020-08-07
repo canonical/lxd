a983ec749e32b3faadb1c4cb5edb38b07001a1be
Thomas Parrott <thomas.parrott@canonical.com>
2020-08-07 09:54:51 +0100

    Revert "exec: fix OpenPtyInDevpts()"
    This reverts commit a32272e33376a801165b7bacd56beac40156fc9d.

--- shared/util_linux.go
+++ shared/util_linux.go
@@ -439,11 +439,7 @@ func OpenPtyInDevpts(devpts_fd int, uid, gid int64) (*os.File, *os.File, error) 
 		}
 
 		pty = os.NewFile(ptyFd, fmt.Sprintf("/dev/pts/%d", id))
-	} else {
-		if devpts_fd >= 0 {
-			return nil, nil, fmt.Errorf("TIOCGPTPEER required but not available")
-		}
-
+	} else if devpts_fd < 0 {
 		// Get the pty side.
 		id := 0
 		_, _, errno = unix.Syscall(unix.SYS_IOCTL, uintptr(ptx.Fd()), unix.TIOCGPTN, uintptr(unsafe.Pointer(&id)))
