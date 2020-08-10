d13822a4e2ea03e643b9e8950c15ed4c87fee68e
Thomas Parrott <thomas.parrott@canonical.com>
2020-08-10 11:29:52 +0100

    lxd/network/network/load: Moves fillAuto logic into per-driver fillConfig function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/network_load.go
+++ lxd/network/network_load.go
@@ -80,11 +80,5 @@ func FillConfig(req *api.NetworksPost) error {
 		return err
 	}
 
-	// Replace "auto" by actual values.
-	err = fillAuto(req.Config)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
