ccfe2315af4ba2a3315c24f71f287c9489a90d34
Stéphane Graber <stgraber@ubuntu.com>
2020-08-20 14:34:49 -0400

    lxc/console: Support remote-viewer on Windows
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxc/console_windows.go
+++ lxc/console_windows.go
@@ -4,6 +4,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
@@ -20,4 +24,23 @@ func (c *cmdConsole) controlSocketHandler(control *websocket.Conn) {
 	if err != nil {
 		fmt.Printf("error setting term size %s\n", err)
 	}
+}
+
+func (c *cmdConsole) findCommand(name string) string {
+	path, _ := exec.LookPath(name)
+	if path == "" {
+		// Let's see if it's not in the usual location.
+		programs, err := ioutil.ReadDir("\\Program Files")
+		if err != nil {
+			return ""
+		}
+
+		for _, entry := range programs {
+			if strings.HasPrefix(entry.Name(), "VirtViewer") {
+				return filepath.Join("\\Program Files", entry.Name(), "bin", "remote-viewer.exe")
+			}
+		}
+	}
+
+	return path
 }
