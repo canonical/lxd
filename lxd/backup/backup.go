6b5b2a878ad46ef224e8f7e71cf46cc5775c227f
Thomas Parrott <thomas.parrott@canonical.com>
2020-09-18 16:05:50 +0100

    lxd/backup: Adds WorkingDirPrefix constant
    To link all variations that use temporary working directories in the codebase.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/backup/backup.go
+++ lxd/backup/backup.go
@@ -17,6 +17,9 @@ import (
 	"github.com/lxc/lxd/shared/api"
 )
 
+// WorkingDirPrefix is used when temporary working directories are needed.
+const WorkingDirPrefix = "lxd_backup"
+
 // Instance represents the backup relevant subset of a LXD instance.
 // This is used rather than instance.Instance to avoid import loops.
 type Instance interface {
