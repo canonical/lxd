85501289761d4af8b5230fbb2730e619631b68fd
Thomas Parrott <thomas.parrott@canonical.com>
2020-09-24 17:28:06 +0100

    lxd/backup/backup/config: Adds VolumeSnapshots to Config struct
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/backup/backup_config.go
+++ lxd/backup/backup_config.go
@@ -15,10 +15,11 @@ import (
 
 // Config represents the config of a backup that can be stored in a backup.yaml file (or embedded in index.yaml).
 type Config struct {
-	Container *api.Instance           `yaml:"container,omitempty"` // Used by VM backups too.
-	Snapshots []*api.InstanceSnapshot `yaml:"snapshots,omitempty"`
-	Pool      *api.StoragePool        `yaml:"pool,omitempty"`
-	Volume    *api.StorageVolume      `yaml:"volume,omitempty"`
+	Container       *api.Instance                `yaml:"container,omitempty"` // Used by VM backups too.
+	Snapshots       []*api.InstanceSnapshot      `yaml:"snapshots,omitempty"`
+	Pool            *api.StoragePool             `yaml:"pool,omitempty"`
+	Volume          *api.StorageVolume           `yaml:"volume,omitempty"`
+	VolumeSnapshots []*api.StorageVolumeSnapshot `yaml:"volume_snapshots,omitempty"`
 }
 
 // ParseConfigYamlFile decodes the YAML file at path specified into a Config.
