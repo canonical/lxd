62e83e4c08e8ef56405bce7d9bc05ff888bfa1c2
Thomas Parrott <thomas.parrott@canonical.com>
2020-09-24 17:13:53 +0100

    lxd/backup/backup/utils: Adds TarReader function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/backup/backup_utils.go
@@ -0,0 +1,30 @@
+package backup
+
+import (
+	"archive/tar"
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/lxc/lxd/shared"
+)
+
+// TarReader rewinds backup file handle r and returns new tar reader and process cleanup function.
+func TarReader(r io.ReadSeeker) (*tar.Reader, context.CancelFunc, error) {
+	r.Seek(0, 0)
+	_, _, unpacker, err := shared.DetectCompressionFile(r)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if unpacker == nil {
+		return nil, nil, fmt.Errorf("Unsupported backup compression")
+	}
+
+	tr, cancelFunc, err := shared.CompressedTarReader(context.Background(), r, unpacker)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return tr, cancelFunc, nil
+}
