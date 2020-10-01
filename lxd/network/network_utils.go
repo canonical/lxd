57eaae02b1fc56f96515467fdc033158582e35d3
Thomas Parrott <thomas.parrott@canonical.com>
2020-10-01 17:07:39 +0100

    lxd/network/network/utils: Adds GetNeighbourIPs function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/network_utils.go
+++ lxd/network/network_utils.go
@@ -749,6 +749,34 @@ func GetHostDevice(parent string, vlan string) string {
 	return defaultVlan
 }
 
+// GetNeighbourIPs returns the IP addresses in the neighbour cache for a particular interface and MAC.
+func GetNeighbourIPs(interfaceName string, hwaddr string) ([]net.IP, error) {
+	addresses := []net.IP{}
+
+	// Look for neighbour entries for IPv.
+	out, err := shared.RunCommand("ip", "neigh", "show", "dev", interfaceName)
+	if err == nil {
+		for _, line := range strings.Split(out, "\n") {
+			// Split fields and early validation.
+			fields := strings.Fields(line)
+			if len(fields) != 4 {
+				continue
+			}
+
+			if fields[2] != hwaddr {
+				continue
+			}
+
+			ip := net.ParseIP(fields[0])
+			if ip != nil {
+				addresses = append(addresses, ip)
+			}
+		}
+	}
+
+	return addresses, nil
+}
+
 // GetLeaseAddresses returns the lease addresses for a network and hwaddr.
 func GetLeaseAddresses(s *state.State, networkName string, hwaddr string) ([]api.InstanceStateNetworkAddress, error) {
 	addresses := []api.InstanceStateNetworkAddress{}
