142695828a9a8779a7914516ff9ff9967574f73b
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-10-08 16:23:23 +0200

    db: Retry failed rollbacks if they are due to transient errors
    This avoids leaving the connection in a bad state, where a transaction is
    already in progress and another one can't be started.
    
    Signed-off-by: Free Ekanayaka <free.ekanayaka@canonical.com>

--- lxd/db/query/transaction.go
+++ lxd/db/query/transaction.go
@@ -30,7 +30,7 @@ func Transaction(db *sql.DB, f func(*sql.Tx) error) error {
 // succeeds the given error is returned, otherwise a new error that wraps it
 // gets generated and returned.
 func rollback(tx *sql.Tx, reason error) error {
-	err := tx.Rollback()
+	err := Retry(tx.Rollback)
 	if err != nil {
 		logger.Warnf("Failed to rollback transaction after error (%v): %v", reason, err)
 	}
