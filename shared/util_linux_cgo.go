ef41ec6a2fc22af4a115f61e55f422341701102b
Stéphane Graber <stgraber@ubuntu.com>
2020-10-27 18:56:45 -0400

    shared: Drop GroupId and UserId
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/util_linux_cgo.go
+++ shared/util_linux_cgo.go
@@ -6,7 +6,6 @@ package shared
 import (
 	"fmt"
 	"os"
-	"unsafe"
 
 	// Used by cgo
 	_ "github.com/lxc/lxd/lxd/include"
@@ -57,102 +56,6 @@ again:
 import "C"
 
 const ABSTRACT_UNIX_SOCK_LEN int = C.ABSTRACT_UNIX_SOCK_LEN
-
-// UserId is an adaption from https://codereview.appspot.com/4589049.
-func UserId(name string) (int, error) {
-	var pw C.struct_passwd
-	var result *C.struct_passwd
-
-	bufSize := C.sysconf(C._SC_GETPW_R_SIZE_MAX)
-	if bufSize < 0 {
-		bufSize = 4096
-	}
-
-	buf := C.malloc(C.size_t(bufSize))
-	if buf == nil {
-		return -1, fmt.Errorf("allocation failed")
-	}
-	defer C.free(buf)
-
-	cname := C.CString(name)
-	defer C.free(unsafe.Pointer(cname))
-
-again:
-	rv, errno := C.getpwnam_r(cname,
-		&pw,
-		(*C.char)(buf),
-		C.size_t(bufSize),
-		&result)
-	if rv < 0 {
-		// OOM killer will take care of us if we end up doing this too
-		// often.
-		if errno == unix.ERANGE {
-			bufSize *= 2
-			tmp := C.realloc(buf, C.size_t(bufSize))
-			if tmp == nil {
-				return -1, fmt.Errorf("allocation failed")
-			}
-			buf = tmp
-			goto again
-		}
-		return -1, fmt.Errorf("failed user lookup: %s", unix.Errno(rv))
-	}
-
-	if result == nil {
-		return -1, fmt.Errorf("unknown user %s", name)
-	}
-
-	return int(C.int(result.pw_uid)), nil
-}
-
-// GroupId is an adaption from https://codereview.appspot.com/4589049.
-func GroupId(name string) (int, error) {
-	var grp C.struct_group
-	var result *C.struct_group
-
-	bufSize := C.sysconf(C._SC_GETGR_R_SIZE_MAX)
-	if bufSize < 0 {
-		bufSize = 4096
-	}
-
-	buf := C.malloc(C.size_t(bufSize))
-	if buf == nil {
-		return -1, fmt.Errorf("allocation failed")
-	}
-
-	cname := C.CString(name)
-	defer C.free(unsafe.Pointer(cname))
-
-again:
-	rv, errno := C.getgrnam_r(cname,
-		&grp,
-		(*C.char)(buf),
-		C.size_t(bufSize),
-		&result)
-	if rv != 0 {
-		// OOM killer will take care of us if we end up doing this too
-		// often.
-		if errno == unix.ERANGE {
-			bufSize *= 2
-			tmp := C.realloc(buf, C.size_t(bufSize))
-			if tmp == nil {
-				return -1, fmt.Errorf("allocation failed")
-			}
-			buf = tmp
-			goto again
-		}
-
-		C.free(buf)
-		return -1, fmt.Errorf("failed group lookup: %s", unix.Errno(rv))
-	}
-	C.free(buf)
-
-	if result == nil {
-		return -1, fmt.Errorf("unknown group %s", name)
-	}
-
-	return int(C.int(result.gr_gid)), nil
-}
 
 func ReadPid(r *os.File) int {
 	return int(C.read_pid(C.int(r.Fd())))
