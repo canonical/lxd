fcfb4cb55c7142965ebf049690b6cbcb5b8eaaac
Thomas Parrott <thomas.parrott@canonical.com>
2020-11-09 11:11:34 +0000

    lxd/device/disk: storagePools.VolumeUsedByInstanceDevices usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/disk.go
+++ lxd/device/disk.go
@@ -1046,7 +1046,7 @@ func (d *disk) storagePoolVolumeAttachShift(projectName, poolName, volumeName st
 
 		if !shared.IsTrue(poolVolumePut.Config["security.shifted"]) {
 			volumeUsedBy := []instance.Instance{}
-			err = storagePools.VolumeUsedByInstances(d.state, poolName, projectName, volume, true, func(dbInst db.Instance, project api.Project, profiles []api.Profile) error {
+			err = storagePools.VolumeUsedByInstanceDevices(d.state, poolName, projectName, volume, true, func(dbInst db.Instance, project api.Project, profiles []api.Profile, usedByDevices []string) error {
 				inst, err := instance.Load(d.state, db.InstanceToArgs(&dbInst), profiles)
 				if err != nil {
 					return err
