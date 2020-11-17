423afe7f71d777e5727ad28a0083180113a8cd87
Thomas Parrott <thomas.parrott@canonical.com>
2020-11-17 16:01:12 +0000

    lxd/device/errors: Adds ErrCannotUpdate error
    Return by the Update() function if the update is not supported by the device based on the instance type or run state.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/errors.go
+++ lxd/device/errors.go
@@ -6,3 +6,6 @@ import (
 
 // ErrUnsupportedDevType is the error that occurs when an unsupported device type is created.
 var ErrUnsupportedDevType = fmt.Errorf("Unsupported device type")
+
+// ErrCannotUpdate is the error that occurs when a device cannot be updated.
+var ErrCannotUpdate = fmt.Errorf("Device does not support updates")
