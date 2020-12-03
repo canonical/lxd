09ee0ec52f280a7e4740a83692b9137c8c81bd40
Thomas Parrott <thomas.parrott@canonical.com>
2020-12-03 16:43:07 +0000

    lxd/cluster/connect: Removes client type constants and helper
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/cluster/connect.go
+++ lxd/cluster/connect.go
@@ -20,38 +20,6 @@ import (
 	"github.com/lxc/lxd/shared/version"
 )
 
-// UserAgentNotifier used to distinguish between a regular client request and an internal cluster request when
-// notifying other nodes of a cluster change.
-const UserAgentNotifier = "lxd-cluster-notifier"
-
-// UserAgentJoiner used to distinguish between a regular client request and an internal cluster request when
-// joining a node to a cluster.
-const UserAgentJoiner = "lxd-cluster-joiner"
-
-// ClientType indicates which sort of client type is being used.
-type ClientType string
-
-// ClientTypeNotifier cluster notification client.
-const ClientTypeNotifier ClientType = "notifier"
-
-// ClientTypeJoiner cluster joiner client.
-const ClientTypeJoiner ClientType = "joiner"
-
-// ClientTypeNormal normal client.
-const ClientTypeNormal ClientType = "normal"
-
-// UserAgentClientType converts user agent to client type.
-func UserAgentClientType(userAgent string) ClientType {
-	switch userAgent {
-	case UserAgentNotifier:
-		return ClientTypeNotifier
-	case UserAgentJoiner:
-		return ClientTypeJoiner
-	}
-
-	return ClientTypeNormal
-}
-
 // Connect is a convenience around lxd.ConnectLXD that configures the client
 // with the correct parameters for node-to-node communication.
 //
