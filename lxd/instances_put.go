e5f35c2c27ff2a4a84c0d9e54c040274ff6bd674
Kevin Turner <kevinturner@utexas.edu>
2020-12-10 22:29:14 -0600

    lxd: Adds support for bulk instance state change.
    Signed-off-by: Kevin Turner <kevinturner@utexas.edu>

---
+++ lxd/instances_put.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/lxc/lxd/lxd/cgroup"
+	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/lxd/instance"
+	"github.com/lxc/lxd/lxd/operations"
+	"github.com/lxc/lxd/lxd/response"
+	"github.com/lxc/lxd/shared"
+	"github.com/lxc/lxd/shared/api"
+)
+
+func coalesceErrors(errors map[string]error) error {
+	if len(errors) == 0 {
+		return nil
+	}
+
+	errorMsg := "The following instances failed to update state:\n"
+	for instName, err := range errors {
+		errorMsg += fmt.Sprintf(" - Instance: %s: %v\n", instName, err)
+	}
+
+	return fmt.Errorf("%s", errorMsg)
+}
+
+// Update all instance states
+func containersPut(d *Daemon, r *http.Request) response.Response {
+	project := projectParam(r)
+
+	c, err := instance.LoadByProject(d.State(), project)
+	if err != nil {
+		return response.BadRequest(err)
+	}
+
+	raw := api.InstancesPut{}
+	raw.Timeout = -1
+	if err := json.NewDecoder(r.Body).Decode(&raw); err != nil {
+		return response.BadRequest(err)
+	}
+
+	action := shared.InstanceAction(raw.Action)
+
+	var names []string
+	var instances []instance.Instance
+	for _, inst := range c {
+		switch action {
+		case shared.Freeze:
+			if !inst.IsRunning() {
+				continue
+			}
+		case shared.Restart:
+			if !inst.IsRunning() {
+				continue
+			}
+		case shared.Start:
+			if inst.IsRunning() {
+				continue
+			}
+		case shared.Stop:
+			if !inst.IsRunning() {
+				continue
+			}
+		case shared.Unfreeze:
+			if inst.IsRunning() {
+				continue
+			}
+		}
+
+		instances = append(instances, inst)
+		names = append(names, inst.Name())
+	}
+
+	// Don't mess with containers while in setup mode
+	<-d.readyChan
+
+	var opType db.OperationType
+	var do func(*operations.Operation) error
+	switch action {
+	case shared.Start:
+		opType = db.OperationInstanceStart
+		do = func(op *operations.Operation) error {
+			failures := map[string]error{}
+			for _, inst := range instances {
+				inst.SetOperation(op)
+				if err = inst.Start(raw.Stateful); err != nil {
+					failures[inst.Name()] = err
+				}
+			}
+
+			return coalesceErrors(failures)
+		}
+	case shared.Stop:
+		opType = db.OperationInstanceStop
+		if raw.Stateful {
+			do = func(op *operations.Operation) error {
+				failures := map[string]error{}
+				for _, inst := range instances {
+					inst.SetOperation(op)
+					err := inst.Stop(raw.Stateful)
+					if err != nil {
+						failures[inst.Name()] = err
+					}
+				}
+
+				return coalesceErrors(failures)
+			}
+		} else if raw.Timeout == 0 || raw.Force {
+			do = func(op *operations.Operation) error {
+				failures := map[string]error{}
+				for _, inst := range instances {
+					inst.SetOperation(op)
+					err = inst.Stop(false)
+					if err != nil {
+						failures[inst.Name()] = err
+					}
+				}
+
+				return coalesceErrors(failures)
+			}
+		} else {
+			do = func(op *operations.Operation) error {
+				failures := map[string]error{}
+				for _, inst := range instances {
+					inst.SetOperation(op)
+					if inst.IsFrozen() {
+						err := inst.Unfreeze()
+						if err != nil {
+							failures[inst.Name()] = err
+							continue
+						}
+					}
+
+					err = inst.Shutdown(time.Duration(raw.Timeout) * time.Second)
+					if err != nil {
+						failures[inst.Name()] = err
+					}
+				}
+
+				return coalesceErrors(failures)
+			}
+		}
+	case shared.Restart:
+		opType = db.OperationInstanceRestart
+		do = func(op *operations.Operation) error {
+			failures := map[string]error{}
+			for _, inst := range instances {
+				inst.SetOperation(op)
+				ephemeral := inst.IsEphemeral()
+
+				if ephemeral {
+					// Unset ephemeral flag
+					args := db.InstanceArgs{
+						Architecture: inst.Architecture(),
+						Config:       inst.LocalConfig(),
+						Description:  inst.Description(),
+						Devices:      inst.LocalDevices(),
+						Ephemeral:    false,
+						Profiles:     inst.Profiles(),
+						Project:      inst.Project(),
+						Type:         inst.Type(),
+						Snapshot:     inst.IsSnapshot(),
+					}
+
+					err := inst.Update(args, false)
+					if err != nil {
+						failures[inst.Name()] = err
+					}
+
+					// On function return, set the flag back on
+					defer func() {
+						args.Ephemeral = ephemeral
+						inst.Update(args, false)
+					}()
+				}
+
+				timeout := raw.Timeout
+				if raw.Force {
+					timeout = 0
+				}
+
+				res := inst.Restart(time.Duration(timeout))
+				if res != nil {
+					failures[inst.Name()] = res
+				}
+
+			}
+
+			return coalesceErrors(failures)
+		}
+	case shared.Freeze:
+		if !d.os.CGInfo.Supports(cgroup.Freezer, nil) {
+			return response.BadRequest(fmt.Errorf("This system doesn't support freezing instances"))
+		}
+
+		opType = db.OperationInstanceFreeze
+		do = func(op *operations.Operation) error {
+			failures := map[string]error{}
+			for _, inst := range instances {
+				inst.SetOperation(op)
+				res := inst.Freeze()
+				if res != nil {
+					failures[inst.Name()] = res
+				}
+			}
+
+			return coalesceErrors(failures)
+		}
+	case shared.Unfreeze:
+		if !d.os.CGInfo.Supports(cgroup.Freezer, nil) {
+			return response.BadRequest(fmt.Errorf("This system doesn't support unfreezing instances"))
+		}
+
+		opType = db.OperationInstanceUnfreeze
+		do = func(op *operations.Operation) error {
+			failures := map[string]error{}
+			for _, inst := range instances {
+				inst.SetOperation(op)
+				res := inst.Unfreeze()
+				if res != nil {
+					failures[inst.Name()] = res
+				}
+			}
+
+			return coalesceErrors(failures)
+		}
+	default:
+		return response.BadRequest(fmt.Errorf("Unknown action %s", raw.Action))
+	}
+
+	resources := map[string][]string{}
+	resources["instances"] = names
+
+	op, err := operations.OperationCreate(d.State(), project, operations.OperationClassTask, opType, resources, nil, do, nil, nil)
+	if err != nil {
+		return response.InternalError(err)
+	}
+
+	return operations.OperationResponse(op)
+}
