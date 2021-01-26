4660df8cb4ff0b88c7d51a250b026e9847c4626b
Thomas Hipp <thomas.hipp@canonical.com>
2021-01-26 13:52:01 +0100

    lxd/device: Add function to validate PCI path
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/device/device_utils_generic.go
+++ lxd/device/device_utils_generic.go
@@ -1,10 +1,24 @@
 package device
 
 import (
+	"fmt"
 	"strings"
+
+	deviceConfig "github.com/lxc/lxd/lxd/device/config"
+	"github.com/lxc/lxd/shared"
 )
 
 // deviceJoinPath joins together prefix and text delimited by a "." for device path generation.
 func deviceJoinPath(parts ...string) string {
 	return strings.Join(parts, ".")
+}
+
+// validatePCIDevice returns whether a configured PCI device exists. It also returns true, if no device
+// has been specified.
+func validatePCIDevice(config deviceConfig.Device) error {
+	if config["pci"] != "" && !shared.PathExists(fmt.Sprintf("/sys/bus/pci/devices/%s", (config["pci"]))) {
+		return fmt.Errorf("Invalid PCI address (no device found): %s", config["pci"])
+	}
+
+	return nil
 }
