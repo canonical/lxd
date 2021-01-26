0b16801bad91b38fb18c5ca1f7a0d08463b34b84
Thomas Parrott <thomas.parrott@canonical.com>
2021-01-26 17:43:27 +0000

    lxd/network/acl: Adds ACL package for managing Network ACLs
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/network/acl/acl_interface.go
@@ -0,0 +1,28 @@
+package acl
+
+import (
+	"github.com/lxc/lxd/lxd/state"
+	"github.com/lxc/lxd/shared/api"
+)
+
+// NetworkACL represents a Network ACL.
+type NetworkACL interface {
+	// Initialise.
+	init(state *state.State, id int64, projectName string, aclInfo *api.NetworkACL)
+
+	// Info.
+	ID() int64
+	Project() string
+	Info() *api.NetworkACL
+	Etag() []interface{}
+	UsedBy() ([]string, error)
+
+	// Internal validation.
+	validateName(name string) error
+	validateConfig(config *api.NetworkACLPut) error
+
+	// Modifications.
+	Update(config *api.NetworkACLPut) error
+	Rename(newName string) error
+	Delete() error
+}
