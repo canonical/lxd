canonical/lxd

commit 28f7f6dc08d2b99ba5159dd497e323234375f3e6

lxd/forkproxy: Switch to using subprocess
Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

lxd/main_forkproxy.go
@@ -85,13 +85,12 @@ again:
 void forkproxy(void)
 {
 	unsigned int needs_mntns = 0;
-	int connect_pid, connect_pidfd, listen_pid, listen_pidfd, log_fd;
+	int connect_pid, connect_pidfd, listen_pid, listen_pidfd;
 	size_t unix_prefix_len = sizeof("unix:") - 1;
 	ssize_t ret;
 	pid_t pid;
-	char *connect_addr, *cur, *listen_addr, *log_path, *pid_path;
+	char *connect_addr, *cur, *listen_addr;
 	int sk_fds[2] = {-EBADF, -EBADF};
-	FILE *pid_file;
 
 	// Get the pid
 	cur = advance_arg(false);
@@ -106,29 +105,6 @@ void forkproxy(void)
 	connect_pid = atoi(advance_arg(true));
 	connect_pidfd = atoi(advance_arg(true));
 	connect_addr = advance_arg(true);
-	log_path = advance_arg(true);
-	pid_path = advance_arg(true);
-
-	close(STDIN_FILENO);
-	log_fd = open(log_path, O_WRONLY | O_CREAT | O_CLOEXEC | O_TRUNC, 0600);
-	if (log_fd < 0)
-		_exit(EXIT_FAILURE);
-
-	ret = dup3(log_fd, STDOUT_FILENO, O_CLOEXEC);
-	if (ret < 0)
-		_exit(EXIT_FAILURE);
-
-	ret = dup3(log_fd, STDERR_FILENO, O_CLOEXEC);
-	if (ret < 0)
-		_exit(EXIT_FAILURE);
-
-	pid_file = fopen(pid_path, "we+");
-	if (!pid_file) {
-		fprintf(stderr,
-			"%s - Failed to create pid file for proxy daemon\n",
-			strerror(errno));
-		_exit(EXIT_FAILURE);
-	}
 
 	if (strncmp(listen_addr, "udp:", sizeof("udp:") - 1) == 0 &&
 	    strncmp(connect_addr, "udp:", sizeof("udp:") - 1) != 0) {
@@ -166,7 +142,6 @@ void forkproxy(void)
 
 		whoami = FORKPROXY_CHILD;
 
-		fclose(pid_file);
 		ret = close(sk_fds[0]);
 		if (ret < 0)
 			fprintf(stderr, "%s - Failed to close fd %d\n",
@@ -269,41 +244,6 @@ void forkproxy(void)
 		// can just rely on init doing it's job and reaping the zombie
 		// process. So, technically unsatisfying but pragmatically
 		// correct.
-
-		// daemonize
-		pid = fork();
-		if (pid < 0)
-			_exit(EXIT_FAILURE);
-
-		if (pid != 0) {
-			ret = wait_for_pid(pid);
-			if (ret < 0)
-				_exit(EXIT_FAILURE);
-
-			_exit(EXIT_SUCCESS);
-		}
-
-		pid = fork();
-		if (pid < 0)
-			_exit(EXIT_FAILURE);
-
-		if (pid != 0) {
-			ret = fprintf(pid_file, "%d", pid);
-			fclose(pid_file);
-			if (ret < 0) {
-				fprintf(stderr, "Failed to write proxy daemon pid %d to \"%s\"\n",
-					pid, pid_path);
-				ret = EXIT_FAILURE;
-			}
-			close(STDOUT_FILENO);
-			close(STDERR_FILENO);
-			_exit(EXIT_SUCCESS);
-		}
-
-		ret = setsid();
-		if (ret < 0)
-			fprintf(stderr, "%s - Failed to setsid in proxy daemon\n",
-				strerror(errno));
 	}
 }
 */
@@ -338,7 +278,7 @@ func (c *cmdForkproxy) Command() *cobra.Command {
   container, connecting one side to the host and the other to the
   container.
 `
-	cmd.Args = cobra.ExactArgs(14)
+	cmd.Args = cobra.ExactArgs(12)
 	cmd.RunE = c.Run
 	cmd.Hidden = true
 
@@ -459,7 +399,7 @@ func (c *cmdForkproxy) Run(cmd *cobra.Command, args []string) error {
 	}
 
 	// Sanity checks
-	if len(args) != 14 {
+	if len(args) != 12 {
 		cmd.Help()
 
 		if len(args) == 0 {
@@ -529,16 +469,16 @@ func (c *cmdForkproxy) Run(cmd *cobra.Command, args []string) error {
 			var err error
 
 			listenAddrGID := -1
-			if args[8] != "" {
-				listenAddrGID, err = strconv.Atoi(args[8])
+			if args[6] != "" {
+				listenAddrGID, err = strconv.Atoi(args[6])
 				if err != nil {
 					return err
 				}
 			}
 
 			listenAddrUID := -1
-			if args[9] != "" {
-				listenAddrUID, err = strconv.Atoi(args[9])
+			if args[7] != "" {
+				listenAddrUID, err = strconv.Atoi(args[7])
 				if err != nil {
 					return err
 				}
@@ -552,8 +492,8 @@ func (c *cmdForkproxy) Run(cmd *cobra.Command, args []string) error {
 			}
 
 			var listenAddrMode os.FileMode
-			if args[10] != "" {
-				tmp, err := strconv.ParseUint(args[10], 8, 0)
+			if args[8] != "" {
+				tmp, err := strconv.ParseUint(args[8], 8, 0)
 				if err != nil {
 					return err
 				}
@@ -621,16 +561,16 @@ func (c *cmdForkproxy) Run(cmd *cobra.Command, args []string) error {
 
 	// Drop privilege if requested
 	gid := uint64(0)
-	if args[11] != "" {
-		gid, err = strconv.ParseUint(args[11], 10, 32)
+	if args[9] != "" {
+		gid, err = strconv.ParseUint(args[9], 10, 32)
 		if err != nil {
 			return err
 		}
 	}
 
 	uid := uint64(0)
-	if args[12] != "" {
-		uid, err = strconv.ParseUint(args[12], 10, 32)
+	if args[10] != "" {
+		uid, err = strconv.ParseUint(args[10], 10, 32)
 		if err != nil {
 			return err
 		}
@@ -710,7 +650,7 @@ func (c *cmdForkproxy) Run(cmd *cobra.Command, args []string) error {
 				continue
 			}
 
-			err := listenerInstance(epFd, lAddr, cAddr, curFd, srcConn, args[13] == "true")
+			err := listenerInstance(epFd, lAddr, cAddr, curFd, srcConn, args[11] == "true")
 			if err != nil {
 				fmt.Printf("Warning: Failed to prepare new listener instance: %s\n", err)
 			}

lxd/device/proxy.go
@@ -2,9 +2,7 @@ package device
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"net"
 	"os"
 	"path/filepath"
@@ -13,7 +11,6 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	"golang.org/x/sys/unix"
 	liblxc "gopkg.in/lxc/go-lxc.v2"
 
 	deviceConfig "github.com/lxc/lxd/lxd/device/config"
@@ -24,6 +21,7 @@ import (
 	"github.com/lxc/lxd/lxd/util"
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/logger"
+	"github.com/lxc/lxd/shared/subprocess"
 	"github.com/lxc/lxd/shared/validate"
 )
 
@@ -195,10 +193,9 @@ func (d *proxy) Start() (*deviceConfig.RunConfig, error) {
 			logFileName := fmt.Sprintf("proxy.%s.log", d.name)
 			logPath := filepath.Join(d.inst.LogPath(), logFileName)
 
-			_, err = shared.RunCommandInheritFds(
-				proxyValues.inheritFds,
-				d.state.OS.ExecPath,
-				"forkproxy",
+			// Spawn the daemon using subprocess
+			command := d.state.OS.ExecPath
+			forkproxyargs := []string{"forkproxy",
 				"--",
 				proxyValues.listenPid,
 				proxyValues.listenPidFd,
@@ -206,28 +203,43 @@ func (d *proxy) Start() (*deviceConfig.RunConfig, error) {
 				proxyValues.connectPid,
 				proxyValues.connectPidFd,
 				proxyValues.connectAddr,
-				logPath,
-				pidPath,
 				proxyValues.listenAddrGID,
 				proxyValues.listenAddrUID,
 				proxyValues.listenAddrMode,
 				proxyValues.securityGID,
 				proxyValues.securityUID,
 				proxyValues.proxyProtocol,
-			)
+			}
+
+			p, err := subprocess.NewProcess(command, forkproxyargs, logPath, logPath)
+			if err != nil {
+				return fmt.Errorf("Failed to create subprocess: %s", err)
+			}
+
+			err = p.StartWithFiles(proxyValues.inheritFds)
+			if err != nil {
+				return fmt.Errorf("Failed to run: %s %s: %v", command, strings.Join(forkproxyargs, " "), err)
+			}
 			for _, file := range proxyValues.inheritFds {
 				file.Close()
 			}
 
+			err = p.Save(pidPath)
 			if err != nil {
-				return fmt.Errorf("Error occurred when starting proxy device: %s", err)
+				// Kill Process if started, but could not save the file
+				err2 := p.Stop()
+				if err != nil {
+					return fmt.Errorf("Could not kill subprocess while handling saving error: %s: %s", err, err2)
+				}
+
+				return fmt.Errorf("Failed to save subprocess details: %s", err)
 			}
 
 			// Poll log file a few times until we see "Started" to indicate successful start.
 			for i := 0; i < 10; i++ {
 				started, err := d.checkProcStarted(logPath)
-
 				if err != nil {
+					p.Stop()
 					return fmt.Errorf("Error occurred when starting proxy device: %s", err)
 				}
 
@@ -238,6 +250,7 @@ func (d *proxy) Start() (*deviceConfig.RunConfig, error) {
 				time.Sleep(time.Second)
 			}
 
+			p.Stop()
 			return fmt.Errorf("Error occurred when starting proxy device, please look in %s", logPath)
 		},
 	}
@@ -514,45 +527,21 @@ func (d *proxy) setupProxyProcInfo() (*proxyProcInfo, error) {
 }
 
 func (d *proxy) killProxyProc(pidPath string) error {
-	// Get the contents of the pid file
-	contents, err := ioutil.ReadFile(pidPath)
-	if err != nil {
-		return err
-	}
-	pidString := strings.TrimSpace(string(contents))
-
-	// Check if the process still exists
-	if !shared.PathExists(fmt.Sprintf("/proc/%s", pidString)) {
-		os.Remove(pidPath)
+	// If the pid file doesn't exist, there is no process to kill.
+	if !shared.PathExists(pidPath) {
 		return nil
 	}
 
-	// Check if it's forkdns
-	cmdArgs, err := ioutil.ReadFile(fmt.Sprintf("/proc/%s/cmdline", pidString))
+	p, err := subprocess.ImportProcess(pidPath)
 	if err != nil {
-		os.Remove(pidPath)
-		return nil
+		return fmt.Errorf("Could not read pid file: %s", err)
 	}
 
-	cmdFields := strings.Split(string(bytes.TrimRight(cmdArgs, string("\x00"))), string(byte(0)))
-	if len(cmdFields) < 5 || cmdFields[1] != "forkproxy" {
-		os.Remove(pidPath)
-		return nil
+	err = p.Stop()
+	if err != nil && err != subprocess.ErrNotRunning {
+		return fmt.Errorf("Unable to kill forkproxy: %s", err)
 	}
 
-	// Parse the pid
-	pidInt, err := strconv.Atoi(pidString)
-	if err != nil {
-		return err
-	}
-
-	// Actually kill the process
-	err = unix.Kill(pidInt, unix.SIGKILL)
-	if err != nil {
-		return err
-	}
-
-	// Cleanup
 	os.Remove(pidPath)
 	return nil
 }

commit 7f14f0e3deed632ee7cbda2819561ba2f048a5ba

forkproxy: prevent zombies
When I originally introduced forkproxy we pretty quickly ran into a zombie
problem because of interactions between the C "runtime" and the Go runtime,
quote:

> Usually we should wait for the child process somewhere here.
> But we cannot really do this. The listener file descriptors
> are retrieved in the go runtime but at that point we have
> already double-fork()ed to daemonize ourselves and so we
> can't wait on the child anymore after we received the
> listener fds. On the other hand, if we wait on the child
> here we wait on the child before the receive. However, if we
> do this then we can end up in a situation where the socket
> send buffer is full and we need to retrieve some file
> descriptors first before we can go on sending more. But this
> won't be possible because we're waiting before the call to
> receive the file descriptor in the go runtime. Luckily, we
> can just rely on init doing it's job and reaping the zombie
> process. So, technically unsatisfying but pragmatically
> correct.

Removing the double-fork in 28f7f6dc08d2 ("lxd/forkproxy: Switch to using
subprocess") meant that the child process didn't get reparented to init which
means it doesn't get cleaned up and hangs around as a zombie.

There are multiple solutions to this:
1. Ignore SIGCHLD in some form e.g. via signal() or sigaction().
2. Create a go routine in the parent process that waits.
3. Create a detached pthread() that waits.

Solution number 1. likely won't work because I expected Go to mess with signals
once the runtime kicks in; especially with SIGCHLD. Even if it didn't I
wouldn't want to rely on it not changing in the future.
Solution number 2. is doable but means more code and we'd need to transfer the
child PID to the go runtime which is fine but seems conceptually odd since the
process got created in pure C.
So solutioon 3. it is.

Link: https://discuss.linuxcontainers.org/t/forkproxy-child-process-hangs-after-adding-proxy-device-to-a-container
Fixes: 28f7f6dc08d2 ("lxd/forkproxy: Switch to using subprocess")

lxd/main_forkproxy.go
@@ -31,6 +31,7 @@ import (
 #endif
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <sched.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -79,6 +80,14 @@ again:
 	if (ret < 0 && errno == EINTR)
 		goto again;
 	return ret;
+}
+
+static void *async_wait_kludge(void *args)
+{
+	pid_t pid = PTR_TO_INT(args);
+
+	wait_for_pid(pid);
+	return NULL;
 }
 
 #define LISTEN_NEEDS_MNTNS 1U
@@ -185,6 +194,7 @@ void forkproxy(void)
 			fprintf(stderr, "%s - Failed to close fd %d\n",
 				strerror(errno), sk_fds[1]);
 	} else {
+		pthread_t thread;
 		int connect_nsfd;
 
 		whoami = FORKPROXY_PARENT;
@@ -246,6 +256,15 @@ void forkproxy(void)
 		// can just rely on init doing it's job and reaping the zombie
 		// process. So, technically unsatisfying but pragmatically
 		// correct.
+
+		// Create detached waiting thread after all namespace
+		// interactions have concluded since some of them require
+		// single-threadedness.
+		if (pthread_create(&thread, NULL, async_wait_kludge, INT_TO_PTR(pid)) ||
+		    pthread_detach(thread)) {
+			fprintf(stderr, "%m - Failed to create detached thread\n");
+			_exit(EXIT_FAILURE);
+		}
 	}
 }
 */