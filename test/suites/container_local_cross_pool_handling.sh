9a487a87689fbb40d871c632955598346c2b0376
Stéphane Graber <stgraber@ubuntu.com>
2021-01-27 23:27:01 -0500

    tests: Fix for stable-4.0
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- test/suites/container_local_cross_pool_handling.sh
+++ test/suites/container_local_cross_pool_handling.sh
@@ -65,14 +65,7 @@ test_container_local_cross_pool_handling() {
         # Check volatile.apply_template is altered during copy.
         lxc config get c2 volatile.apply_template | grep copy
         lxc storage volume show "lxdtest-$(basename "${LXD_DIR}")-${driver}1" container/c2
-        lxc delete -f c2
-        lxc move c1 c2 -s "lxdtest-$(basename "${LXD_DIR}")-${driver}1"
-
-        # Check volatile.apply_template is not altered during move and rename.
-        lxc config get c2 volatile.apply_template | grep create
-        ! lxc info c1 || false
-        lxc storage volume show "lxdtest-$(basename "${LXD_DIR}")-${driver}1" container/c2
-        lxc delete -f c2
+        lxc delete -f c1 c2
 
         lxc init testimage c1
         lxc snapshot c1
