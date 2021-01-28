e10fde8408f43d02150f2a5c460a0c5224b29cdc
Thomas Parrott <thomas.parrott@canonical.com>
2021-01-28 10:41:06 +0000

    client/lxd/network/acls: Implements GetNetworkACLs function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- client/lxd_network_acls.go
+++ client/lxd_network_acls.go
@@ -6,7 +6,24 @@ import (
 	"github.com/lxc/lxd/shared/api"
 )
 
-// CreateNetworkACL defines a new network ACL using the provided struct.
+// GetNetworkACLs returns a list of Network ACL structs.
+func (r *ProtocolLXD) GetNetworkACLs() ([]api.NetworkACL, error) {
+	if !r.HasExtension("network_acl") {
+		return nil, fmt.Errorf(`The server is missing the required "network_acl" API extension`)
+	}
+
+	acls := []api.NetworkACL{}
+
+	// Fetch the raw value.
+	_, err := r.queryStruct("GET", "/network-acls?recursion=1", nil, "", &acls)
+	if err != nil {
+		return nil, err
+	}
+
+	return acls, nil
+}
+
+// CreateNetworkACL defines a new network using the provided Network struct
 func (r *ProtocolLXD) CreateNetworkACL(acl api.NetworkACLsPost) error {
 	if !r.HasExtension("network_acl") {
 		return fmt.Errorf(`The server is missing the required "network_acl" API extension`)
