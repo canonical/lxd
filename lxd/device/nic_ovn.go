50197a5d28bb2f39c99dd895552db76a12ede2a9
Thomas Parrott <thomas.parrott@canonical.com>
2021-02-02 15:24:55 +0000

    lxd/device/nic/ovn: d.network.InstanceDevicePortAdd and d.network.InstanceDevicePortDelete usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/nic_ovn.go
+++ lxd/device/nic_ovn.go
@@ -264,12 +264,30 @@ func (d *nicOVN) Start() (*deviceConfig.RunConfig, error) {
 
 	// Add new OVN logical switch port for instance.
 	instanceUUID := d.inst.LocalConfig()["volatile.uuid"]
-	logicalPortName, err := d.network.InstanceDevicePortAdd(uplink.Config, instanceUUID, d.inst.Name(), d.name, mac, ips, internalRoutes, externalRoutes)
+	logicalPortName, err := d.network.InstanceDevicePortAdd(&network.OVNInstanceNICSetupOpts{
+		OVNInstanceNICOpts: network.OVNInstanceNICOpts{
+			InstanceUUID:   instanceUUID,
+			DeviceName:     d.name,
+			InternalRoutes: internalRoutes,
+			ExternalRoutes: externalRoutes,
+		},
+		UplinkConfig: uplink.Config,
+		InstanceName: d.inst.Name(),
+		MAC:          mac,
+		IPs:          ips,
+	})
 	if err != nil {
 		return nil, errors.Wrapf(err, "Failed adding OVN port")
 	}
 
-	revert.Add(func() { d.network.InstanceDevicePortDelete(instanceUUID, d.name, "", internalRoutes, externalRoutes) })
+	revert.Add(func() {
+		d.network.InstanceDevicePortDelete("", &network.OVNInstanceNICOpts{
+			InstanceUUID:   instanceUUID,
+			DeviceName:     d.name,
+			InternalRoutes: internalRoutes,
+			ExternalRoutes: externalRoutes,
+		})
+	})
 
 	// Attach host side veth interface to bridge.
 	integrationBridge, err := d.getIntegrationBridgeName()
@@ -410,7 +428,12 @@ func (d *nicOVN) Stop() (*deviceConfig.RunConfig, error) {
 	}
 
 	instanceUUID := d.inst.LocalConfig()["volatile.uuid"]
-	err = d.network.InstanceDevicePortDelete(instanceUUID, d.name, ovsExternalOVNPort, internalRoutes, externalRoutes)
+	err = d.network.InstanceDevicePortDelete(ovsExternalOVNPort, &network.OVNInstanceNICOpts{
+		InstanceUUID:   instanceUUID,
+		DeviceName:     d.name,
+		InternalRoutes: internalRoutes,
+		ExternalRoutes: externalRoutes,
+	})
 	if err != nil {
 		// Don't fail here as we still want the postStop hook to run to clean up the local veth pair.
 		d.logger.Error("Failed to remove OVN device port", log.Ctx{"err": err})
