6dc02735bc02529da4cfdf110fca25eac06dff86
Thomas Parrott <thomas.parrott@canonical.com>
2021-02-02 14:22:01 +0000

    lxd/network/network/utils: Removes whitespace trimming from SubnetParseAppend
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/network_utils.go
+++ lxd/network/network_utils.go
@@ -1145,11 +1145,9 @@ func SubnetIterate(subnet *net.IPNet, ipFunc func(ip net.IP) error) error {
 	return nil
 }
 
-// SubnetParseAppend parses one or more string CIDR subnets. Trims any white space before parsing and appends to
-// the supplied slice. Returns subnets slice.
+// SubnetParseAppend parses one or more string CIDR subnets. Appends to the supplied slice. Returns subnets slice.
 func SubnetParseAppend(subnets []*net.IPNet, parseSubnet ...string) ([]*net.IPNet, error) {
 	for _, subnetStr := range parseSubnet {
-		subnetStr = strings.TrimSpace(subnetStr)
 		_, subnet, err := net.ParseCIDR(subnetStr)
 		if err != nil {
 			return nil, errors.Wrapf(err, "Invalid subnet %q", subnetStr)
