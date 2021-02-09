5cdfbb6a90b5064fa455ed52115007038819e9d8
Thomas Parrott <thomas.parrott@canonical.com>
2021-02-09 11:40:18 +0000

    lxd/device/nic/bridged: Switches to use NIC type agnostic route helper functions
    Also ensures that during Update, that old device config is enriched with network info (now that the enrichment isn't happening as a side effect of updatable field comparison).
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/nic_bridged.go
+++ lxd/device/nic_bridged.go
@@ -271,8 +271,8 @@ func (d *nicBridged) Start() (*deviceConfig.RunConfig, error) {
 	// Populate device config with volatile fields if needed.
 	networkVethFillFromVolatile(d.config, saveData)
 
-	// Apply host-side routes.
-	err = networkSetupHostVethRoutes(d.state, d.config, nil, saveData)
+	// Apply host-side routes to bridge interface.
+	err = networkNICRouteAdd(d.config["parent"], append(util.SplitNTrimSpace(d.config["ipv4.routes"], ",", -1, true), util.SplitNTrimSpace(d.config["ipv6.routes"], ",", -1, true)...)...)
 	if err != nil {
 		return nil, err
 	}
@@ -357,6 +357,11 @@ func (d *nicBridged) Start() (*deviceConfig.RunConfig, error) {
 // Update applies configuration changes to a started device.
 func (d *nicBridged) Update(oldDevices deviceConfig.Devices, isRunning bool) error {
 	oldConfig := oldDevices[d.name]
+	v := d.volatileGet()
+
+	// Populate device config with volatile fields if needed.
+	networkVethFillFromVolatile(d.config, v)
+	networkVethFillFromVolatile(oldConfig, v)
 
 	// If an IPv6 address has changed, flush all existing IPv6 leases for instance so instance
 	// isn't allocated old IP. This is important with IPv6 because DHCPv6 supports multiple IP
@@ -368,11 +373,6 @@ func (d *nicBridged) Update(oldDevices deviceConfig.Devices, isRunning bool) err
 		}
 	}
 
-	v := d.volatileGet()
-
-	// Populate device config with volatile fields if needed.
-	networkVethFillFromVolatile(d.config, v)
-
 	// If instance is running, apply host side limits and filters first before rebuilding
 	// dnsmasq config below so that existing config can be used as part of the filter removal.
 	if isRunning {
@@ -381,8 +381,17 @@ func (d *nicBridged) Update(oldDevices deviceConfig.Devices, isRunning bool) err
 			return err
 		}
 
-		// Apply host-side routes.
-		err = networkSetupHostVethRoutes(d.state, d.config, oldConfig, v)
+		// Validate old config so that it is enriched with network parent config needed for route removal.
+		err = Validate(d.inst, d.state, d.name, oldConfig)
+		if err != nil {
+			return err
+		}
+
+		// Remove old host-side routes from bridge interface.
+		networkNICRouteDelete(oldConfig["parent"], append(util.SplitNTrimSpace(oldConfig["ipv4.routes"], ",", -1, true), util.SplitNTrimSpace(oldConfig["ipv6.routes"], ",", -1, true)...)...)
+
+		// Apply host-side routes to bridge interface.
+		err = networkNICRouteAdd(d.config["parent"], append(util.SplitNTrimSpace(d.config["ipv4.routes"], ",", -1, true), util.SplitNTrimSpace(d.config["ipv6.routes"], ",", -1, true)...)...)
 		if err != nil {
 			return err
 		}
@@ -442,7 +451,7 @@ func (d *nicBridged) postStop() error {
 
 	networkVethFillFromVolatile(d.config, v)
 
-	if d.config["host_name"] != "" && shared.PathExists(fmt.Sprintf("/sys/class/net/%s", d.config["host_name"])) {
+	if d.config["host_name"] != "" && network.InterfaceExists(d.config["host_name"]) {
 		// Detach host-side end of veth pair from bridge (required for openvswitch particularly).
 		err := network.DetachInterface(d.config["parent"], d.config["host_name"])
 		if err != nil {
@@ -456,7 +465,8 @@ func (d *nicBridged) postStop() error {
 		}
 	}
 
-	networkRemoveVethRoutes(d.state, d.config)
+	// Remove host-side routes from bridge interface.
+	networkNICRouteDelete(d.config["parent"], append(util.SplitNTrimSpace(d.config["ipv4.routes"], ",", -1, true), util.SplitNTrimSpace(d.config["ipv6.routes"], ",", -1, true)...)...)
 	d.removeFilters(d.config)
 
 	return nil
