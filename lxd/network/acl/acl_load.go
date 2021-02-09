d61ce457a30c4b131f01ee1e3736db8d83f3a443
Thomas Parrott <thomas.parrott@canonical.com>
2021-02-09 17:58:12 +0000

    lxd/network/acl/acl/load: Adds UsedBy function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/acl/acl_load.go
+++ lxd/network/acl/acl_load.go
@@ -3,7 +3,11 @@ package acl
 import (
 	"fmt"
 
+	"github.com/lxc/lxd/lxd/db"
+	deviceConfig "github.com/lxc/lxd/lxd/device/config"
+	"github.com/lxc/lxd/lxd/project"
 	"github.com/lxc/lxd/lxd/state"
+	"github.com/lxc/lxd/lxd/util"
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/api"
 )
@@ -68,4 +72,39 @@ func Exists(s *state.State, projectName string, name ...string) error {
 	}
 
 	return nil
+}
+
+// UsedBy finds all networks and instance NICs that use an ACL and executes usageFunc with info about the usage.
+func UsedBy(s *state.State, projectName string, aclName string, usageFunc func(usageType string, projectName string, name string, config map[string]string) error) error {
+	return s.Cluster.InstanceList(nil, func(inst db.Instance, p api.Project, profiles []api.Profile) error {
+		// Get the instance's effective network project name.
+		instNetworkProject := project.NetworkProjectFromRecord(&p)
+
+		// Skip instances who's effective network project doesn't match this Network ACL's project.
+		if instNetworkProject != projectName {
+			return nil
+		}
+
+		devices := db.ExpandInstanceDevices(deviceConfig.NewDevices(inst.Devices), profiles).CloneNative()
+
+		// Iterate through each of the instance's devices, looking for NICs that are linked this ACL.
+		for _, devConfig := range devices {
+			// Only NICs linked to managed networks can use network ACLs.
+			if devConfig["type"] != "nic" || devConfig["network"] == "" {
+				continue
+			}
+
+			nicACLNames := util.SplitNTrimSpace(devConfig["security.acls"], ",", -1, true)
+			for _, nicACLName := range nicACLNames {
+				if nicACLName == aclName {
+					err := usageFunc("instance", inst.Project, inst.Name, devConfig)
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+
+		return nil
+	})
 }
