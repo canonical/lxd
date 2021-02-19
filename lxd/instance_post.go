9826f057dc46429c8650ec4493b71bfac8ba6542
Thomas Parrott <thomas.parrott@canonical.com>
2021-02-19 14:14:44 +0000

    lxd/instance/post: Update instancePostClusteringMigrate to respect instance's project
    Fixes #8484
    
    Also renames instance argument to inst.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance_post.go
+++ lxd/instance_post.go
@@ -375,7 +375,7 @@ func instancePostPoolMigration(d *Daemon, inst instance.Instance, newName string
 }
 
 // Move a non-ceph container to another cluster node.
-func instancePostClusteringMigrate(d *Daemon, c instance.Instance, oldName, newName, newNode string) response.Response {
+func instancePostClusteringMigrate(d *Daemon, inst instance.Instance, oldName, newName, newNode string) response.Response {
 	cert := d.endpoints.NetworkCert()
 
 	var sourceAddress string
@@ -383,7 +383,7 @@ func instancePostClusteringMigrate(d *Daemon, c instance.Instance, oldName, newN
 
 	// Save the original value of the "volatile.apply_template" config key,
 	// since we'll want to preserve it in the copied container.
-	origVolatileApplyTemplate := c.LocalConfig()["volatile.apply_template"]
+	origVolatileApplyTemplate := inst.LocalConfig()["volatile.apply_template"]
 
 	err := d.cluster.Transaction(func(tx *db.ClusterTx) error {
 		var err error
@@ -411,13 +411,14 @@ func instancePostClusteringMigrate(d *Daemon, c instance.Instance, oldName, newN
 		if err != nil {
 			return errors.Wrap(err, "Failed to connect to source server")
 		}
+		source = source.UseProject(inst.Project())
 
 		// Connect to the destination host, i.e. the node to migrate the container to.
 		dest, err := cluster.Connect(targetAddress, cert, false)
 		if err != nil {
 			return errors.Wrap(err, "Failed to connect to destination server")
 		}
-		dest = dest.UseTarget(newNode)
+		dest = dest.UseTarget(newNode).UseProject(inst.Project())
 
 		destName := newName
 		isSameName := false
@@ -482,7 +483,7 @@ func instancePostClusteringMigrate(d *Daemon, c instance.Instance, oldName, newN
 		}
 
 		// Restore the original value of "volatile.apply_template"
-		project := c.Project()
+		project := inst.Project()
 		err = d.cluster.Transaction(func(tx *db.ClusterTx) error {
 			id, err := tx.GetInstanceID(project, destName)
 			if err != nil {
@@ -513,8 +514,13 @@ func instancePostClusteringMigrate(d *Daemon, c instance.Instance, oldName, newN
 	}
 
 	resources := map[string][]string{}
-	resources["containers"] = []string{oldName}
-	op, err := operations.OperationCreate(d.State(), c.Project(), operations.OperationClassTask, db.OperationInstanceMigrate, resources, nil, run, nil, nil)
+	resources["instances"] = []string{oldName}
+
+	if inst.Type() == instancetype.Container {
+		resources["containers"] = resources["instances"]
+	}
+
+	op, err := operations.OperationCreate(d.State(), inst.Project(), operations.OperationClassTask, db.OperationInstanceMigrate, resources, nil, run, nil, nil)
 	if err != nil {
 		return response.InternalError(err)
 	}
