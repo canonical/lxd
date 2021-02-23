47b64c27ea87e965b9a669970108e43136515658
Stéphane Graber <stgraber@ubuntu.com>
2021-02-23 15:18:16 -0500

    shared/api: Add comments to certificate fields
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/api/certificate.go
+++ shared/api/certificate.go
@@ -6,9 +6,11 @@ package api
 type CertificatesPost struct {
 	CertificatePut `yaml:",inline"`
 
+	// The certificate itself, as PEM encoded X509
 	// Example: X509 PEM certificate
 	Certificate string `json:"certificate" yaml:"certificate"`
 
+	// Server trust password (used to add an untrusted client)
 	// Example: blah
 	Password string `json:"password" yaml:"password"`
 }
@@ -19,20 +21,24 @@ type CertificatesPost struct {
 //
 // swagger:model
 type CertificatePut struct {
+	// Name associated with the certificate
 	// Example: castiana
 	Name string `json:"name" yaml:"name"`
 
+	// Usage type for the certificate (only client currently)
 	// Example: client
 	Type string `json:"type" yaml:"type"`
 
-	// API extension: certificate_project
-	//
+	// Whether to limit the certificate to listed projects
 	// Example: true
+	//
+	// API extension: certificate_project
 	Restricted bool `json:"restricted" yaml:"restricted"`
 
-	// API extension: certificate_project
-	//
+	// List of allowed projects (applies when restricted)
 	// Example: ["default", "foo", "bar"]
+	//
+	// API extension: certificate_project
 	Projects []string `json:"projects" yaml:"projects"`
 }
 
@@ -42,9 +48,13 @@ type CertificatePut struct {
 type Certificate struct {
 	CertificatePut `yaml:",inline"`
 
+	// The certificate itself, as PEM encoded X509
+	// Read only: true
 	// Example: X509 PEM certificate
 	Certificate string `json:"certificate" yaml:"certificate"`
 
+	// SHA256 fingerprint of the certificate
+	// Read only: true
 	// Example: fd200419b271f1dc2a5591b693cc5774b7f234e1ff8c6b78ad703b6888fe2b69
 	Fingerprint string `json:"fingerprint" yaml:"fingerprint"`
 }
