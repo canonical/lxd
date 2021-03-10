eb6e630baf3729e0d22ce0d0aaa9b0c625277d34
Stéphane Graber <stgraber@ubuntu.com>
2021-03-09 22:33:52 -0500

    shared/osarch: Remove dependency on shared package
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/osarch/architectures_linux.go
+++ shared/osarch/architectures_linux.go
@@ -4,15 +4,18 @@
 package osarch
 
 import (
-	"github.com/lxc/lxd/shared"
+	"bytes"
+
+	"golang.org/x/sys/unix"
 )
 
 // ArchitectureGetLocal returns the local hardware architecture
 func ArchitectureGetLocal() (string, error) {
-	uname, err := shared.Uname()
+	uname := unix.Utsname{}
+	err := unix.Uname(&uname)
 	if err != nil {
 		return ArchitectureDefault, err
 	}
 
-	return uname.Machine, nil
+	return string(uname.Machine[:bytes.IndexByte(uname.Machine[:], 0)]), nil
 }
