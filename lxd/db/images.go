canonical/lxd

commit 9c1bc785d8adbe29a98a8b9fcb05b7601b8a1a72

lxd/db/images: Changes GetExpiredImages to GetExpiredImagesInProject
Signed-off-by: Piotr Resztak <piotr.resztak@gmail.com>

lxd/db/images.go
@@ -263,25 +263,19 @@ SELECT fingerprint
 	return fingerprints, nil
 }
 
-// ExpiredImage used to store expired image info.
-type ExpiredImage struct {
-	Fingerprint string
-	ProjectName string
-}
-
-// GetExpiredImages returns the names and project name of all images that have expired since the given time.
-func (c *Cluster) GetExpiredImages(expiry int64) ([]ExpiredImage, error) {
+// GetExpiredImagesInProject returns the names of all images that have expired since the given time.
+func (c *Cluster) GetExpiredImagesInProject(expiry int64, project string) ([]string, error) {
 	var images []Image
 	err := c.Transaction(func(tx *ClusterTx) error {
 		var err error
-		images, err = tx.GetImages(ImageFilter{Cached: true})
+		images, err = tx.GetImages(ImageFilter{Cached: true, Project: project})
 		return err
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	results := []ExpiredImage{}
+	results := []string{}
 	for _, r := range images {
 		// Figure out the expiry
 		timestamp := r.UploadDate
@@ -297,12 +291,7 @@ func (c *Cluster) GetExpiredImages(expiry int64) ([]ExpiredImage, error) {
 			continue
 		}
 
-		result := ExpiredImage{
-			Fingerprint: r.Fingerprint,
-			ProjectName: r.Project,
-		}
-
-		results = append(results, result)
+		results = append(results, r.Fingerprint)
 	}
 
 	return results, nil

commit dd4d9348563455298e55d2f790d981aa26ae1a2b

lxd/db/images: Fix incorrect cached attribute handling
This fixed a very nasty regression introduced in 9c1bc785d8 which
switched the function fetching expired images to filter on a per-project
basis.

That part made total sense but unfortunately what we missed is that we
had no code-generated DB code for that particular filter
(Project-Cached) so instead only the project filter was used, causing
the Cached field to be ignored entirely.

This then resulted in all expired images to be returned to the image
pruner EVEN if an image wasn't cached, causing the deletion of manually
created images that weren't used recently...

Reported at: https://discuss.linuxcontainers.org/t/images-vanished/10498/

Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

lxd/db/images.go
@@ -24,6 +24,7 @@ import (
 //
 //go:generate mapper stmt -p db -e image objects
 //go:generate mapper stmt -p db -e image objects-by-Project
+//go:generate mapper stmt -p db -e image objects-by-Project-and-Cached
 //go:generate mapper stmt -p db -e image objects-by-Project-and-Public
 //go:generate mapper stmt -p db -e image objects-by-Project-and-Fingerprint
 //go:generate mapper stmt -p db -e image objects-by-Project-and-Fingerprint-and-Public

lxd/db/images.mapper.go
@@ -28,6 +28,12 @@ SELECT images.id, projects.name AS project, images.fingerprint, images.type, ima
   WHERE project = ? ORDER BY projects.id, images.fingerprint
 `)
 
+var imageObjectsByProjectAndCached = cluster.RegisterStmt(`
+SELECT images.id, projects.name AS project, images.fingerprint, images.type, images.filename, images.size, images.public, images.architecture, images.creation_date, images.expiry_date, images.upload_date, images.cached, images.last_use_date, images.auto_update
+  FROM images JOIN projects ON images.project_id = projects.id
+  WHERE project = ? AND images.cached = ? ORDER BY projects.id, images.fingerprint
+`)
+
 var imageObjectsByProjectAndPublic = cluster.RegisterStmt(`
 SELECT images.id, projects.name AS project, images.fingerprint, images.type, images.filename, images.size, images.public, images.architecture, images.creation_date, images.expiry_date, images.upload_date, images.cached, images.last_use_date, images.auto_update
   FROM images JOIN projects ON images.project_id = projects.id
@@ -109,6 +115,12 @@ func (c *ClusterTx) GetImages(filter ImageFilter) ([]Image, error) {
 		args = []interface{}{
 			filter.Project,
 			filter.Fingerprint,
+		}
+	} else if criteria["Project"] != nil && criteria["Cached"] != nil {
+		stmt = c.stmt(imageObjectsByProjectAndCached)
+		args = []interface{}{
+			filter.Project,
+			filter.Cached,
 		}
 	} else if criteria["Project"] != nil {
 		stmt = c.stmt(imageObjectsByProject)