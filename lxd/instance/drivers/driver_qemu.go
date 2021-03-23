4f9634b98a3d5ab68491070656a679d9e68da725
Thomas Parrott <thomas.parrott@canonical.com>
2021-03-23 12:02:26 +0000

    lxd/instance/drivers/driver/qemu: Adds renderState and updates RenderState to use it
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -4597,12 +4597,11 @@ func (d *qemu) RenderFull() (*api.InstanceFull, interface{}, error) {
 	return &vmState, etag, nil
 }
 
-// RenderState returns just state info about the instance.
-func (d *qemu) RenderState() (*api.InstanceState, error) {
+// renderState returns just state info about the instance.
+func (d *qemu) renderState(statusCode api.StatusCode) (*api.InstanceState, error) {
 	var err error
 
 	status := &api.InstanceState{}
-	statusCode := d.statusCode()
 	pid, _ := d.pid()
 
 	if d.isRunningStatusCode(statusCode) {
@@ -4682,6 +4681,11 @@ func (d *qemu) RenderState() (*api.InstanceState, error) {
 	}
 
 	return status, nil
+}
+
+// RenderState returns just state info about the instance.
+func (d *qemu) RenderState() (*api.InstanceState, error) {
+	return d.renderState(d.statusCode())
 }
 
 // diskState gets disk usage info.
