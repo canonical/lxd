d00f4464ac3d9f04a5d4da76f1828bc38f309348
Thomas Parrott <thomas.parrott@canonical.com>
2021-03-24 16:13:41 +0000

    lxd/firewall/drivers/drivers/consts: Adds ErrNotSupported var
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/firewall/drivers/drivers_consts.go
+++ lxd/firewall/drivers/drivers_consts.go
@@ -1,7 +1,12 @@
 package drivers
 
+import "fmt"
+
 // FilterIPv6All used to indicate to firewall package to filter all IPv6 traffic.
 const FilterIPv6All = "::"
 
 // FilterIPv4All used to indicate to firewall package to filter all IPv4 traffic.
 const FilterIPv4All = "0.0.0.0"
+
+// ErrNotSupported is returned when the firewall driver doesn't support a feature.
+var ErrNotSupported error = fmt.Errorf("Not supported")
