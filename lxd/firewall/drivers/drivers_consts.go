a639ebe87beec7d71984898ca4e0c8d64d14e8be
Thomas Parrott <thomas.parrott@canonical.com>
2021-04-01 16:53:51 +0100

    lxd/firewall/drivers/driver/consts: Add network firewall Opts
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/firewall/drivers/drivers_consts.go
+++ lxd/firewall/drivers/drivers_consts.go
@@ -1,7 +1,30 @@
 package drivers
 
+import "net"
+
 // FilterIPv6All used to indicate to firewall package to filter all IPv6 traffic.
 const FilterIPv6All = "::"
 
 // FilterIPv4All used to indicate to firewall package to filter all IPv4 traffic.
 const FilterIPv4All = "0.0.0.0"
+
+// FeatureOpts specify how firewall features are setup.
+type FeatureOpts struct {
+	DHCPDNSAccess   bool // Add rules to allow DHCP and DNS access.
+	ForwardingAllow bool // Add rules to allow IP forwarding. Blocked if false.
+}
+
+// SNATOpts specify how SNAT rules are setup.
+type SNATOpts struct {
+	Append      bool       // Append rules (has no effect if driver doesn't support it).
+	Subnet      *net.IPNet // Subnet of source network used to identify candidate traffic.
+	SNATAddress net.IP     // SNAT IP address to use. If nil then MASQUERADE is used.
+}
+
+// Opts for setting up the firewall.
+type Opts struct {
+	FeaturesV4 *FeatureOpts // Enable IPv4 firewall with specified options. Off if not provided.
+	FeaturesV6 *FeatureOpts // Enable IPv6 firewall with specified options. Off if not provided.
+	SNATV4     *SNATOpts    // Enable IPv4 SNAT with specified options. Off if not provided.
+	SNATV6     *SNATOpts    // Enable IPv6 SNAT with specified options. Off if not provided.
+}
