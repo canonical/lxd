56e850dfd77796a9069f1379504265c16996f69d
Sascha Bast <sash@mischkonsum.org>
2021-04-07 13:40:17 -0400

    lxd/snapshot: Add snapshot aliases
    Signed-off-by: Sascha Bast <sash@mischkonsum.org>

---
+++ lxd/snapshot_common_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/lxd/instance"
+	"github.com/lxc/lxd/lxd/instance/instancetype"
+)
+
+func (suite *containerTestSuite) TestSnapshotScheduling() {
+	args := db.InstanceArgs{
+		Type:      instancetype.Container,
+		Ephemeral: false,
+		Name:      "hal9000",
+	}
+
+	c, err := instance.CreateInternal(suite.d.State(), args)
+	suite.Req.Nil(err)
+	suite.Equal(true, snapshotIsScheduledNow("* * * * *",
+		int64(c.ID())),
+		"snapshot.schedule config '* * * * *' should have matched now")
+	suite.Equal(true, snapshotIsScheduledNow("@daily,"+
+		"@hourly,"+
+		"@midnight,"+
+		"@weekly,"+
+		"@monthly,"+
+		"@annually,"+
+		"@yearly,"+
+		" * * * * *",
+		int64(c.ID())),
+		"snapshot.schedule config '* * * * *' should have matched now")
+}
+
+func TestSnapshotCommon(t *testing.T) {
+	suite.Run(t, new(containerTestSuite))
+}
