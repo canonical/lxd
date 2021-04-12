d7586b95f1281769722214d5f427d049d1e0b237
Thomas Hipp <thomas.hipp@canonical.com>
2021-04-12 21:28:53 +0200

    lxd/db: Add operation type OperationWarningsPruneResolved
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/db/operations_types.go
+++ lxd/db/operations_types.go
@@ -60,6 +60,7 @@ const (
 	OperationCustomVolumeBackupRemove
 	OperationCustomVolumeBackupRename
 	OperationCustomVolumeBackupRestore
+	OperationWarningsPruneResolved
 )
 
 // Description return a human-readable description of the operation type.
@@ -165,6 +166,8 @@ func (t OperationType) Description() string {
 		return "Renaming custom volume backup"
 	case OperationCustomVolumeBackupRestore:
 		return "Restoring custom volume backup"
+	case OperationWarningsPruneResolved:
+		return "Pruning resolved warnings"
 	default:
 		return "Executing operation"
 	}
