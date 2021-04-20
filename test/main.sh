31a9886b65ce549d971cecf506e2d7f4fad8a4cd
Thomas Hipp <thomas.hipp@canonical.com>
2021-04-20 13:20:17 +0200

    test: Add warnings test
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -276,6 +276,7 @@ run_test test_profiles_project_images_profiles "profiles in project with images 
 run_test test_profiles_project_images "profiles in project with images enabled and profiles disabled"
 run_test test_profiles_project_profiles "profiles in project with images disabled and profiles enabled"
 run_test test_filtering "API filtering"
+run_test test_warnings "Warnings"
 
 # shellcheck disable=SC2034
 TEST_RESULT=success
