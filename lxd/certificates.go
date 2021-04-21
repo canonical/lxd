fc64cca7b217b04baf7f7ba09f2ddc3be0bfa02f
Thomas Parrott <thomas.parrott@canonical.com>
2021-04-21 16:36:03 +0100

    lxd/certificates: Updates updateCertificateCache to handle per-certificate upgrade
     - Detects no local trusted server certificates and switches to trusting network certificate.
     - Populates local certificates table.
     - Improves logging.
     - Change debug to warn when failing to load trusted certs.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/certificates.go
+++ lxd/certificates.go
@@ -164,17 +164,20 @@ func certificatesGet(d *Daemon, r *http.Request) response.Response {
 }
 
 func updateCertificateCache(d *Daemon) {
+	logger.Debug("Refreshing trusted certificate cache")
+
 	newCerts := map[int]map[string]x509.Certificate{}
 	newProjects := map[string][]string{}
 
 	var dbCerts []db.Certificate
+	var localCerts []db.Certificate
 	var err error
 	err = d.cluster.Transaction(func(tx *db.ClusterTx) error {
 		dbCerts, err = tx.GetCertificates(db.CertificateFilter{})
 		return err
 	})
 	if err != nil {
-		logger.Infof("Error reading certificates from database: %v", err)
+		logger.Warn("Failed reading certificates from global database", log.Ctx{"err": err})
 		return
 	}
 
@@ -185,20 +188,36 @@ func updateCertificateCache(d *Daemon) {
 
 		certBlock, _ := pem.Decode([]byte(dbCert.Certificate))
 		if certBlock == nil {
-			logger.Infof("Error decoding certificate for %q: %v", dbCert.Name, err)
+			logger.Warn("Failed decoding certificate", log.Ctx{"name": dbCert.Name, "err": err})
 			continue
 		}
 
 		cert, err := x509.ParseCertificate(certBlock.Bytes)
 		if err != nil {
-			logger.Infof("Error reading certificate for %q: %v", dbCert.Name, err)
+			logger.Warn("Failed parsing certificate", log.Ctx{"name": dbCert.Name, "err": err})
 			continue
 		}
 
 		newCerts[dbCert.Type][shared.CertFingerprint(cert)] = *cert
+
 		if dbCert.Restricted {
 			newProjects[shared.CertFingerprint(cert)] = dbCert.Projects
 		}
+
+		// Add server certs to list of certificates to store in local database to allow cluster restart.
+		if dbCert.Type == db.CertificateTypeServer {
+			localCerts = append(localCerts, dbCert)
+		}
+	}
+
+	// Write out the server certs to the local database to allow the cluster to restart.
+	err = d.db.Transaction(func(tx *db.NodeTx) error {
+		return tx.ReplaceCertificates(localCerts)
+	})
+	if err != nil {
+		logger.Warn("Failed writing certificates to local database", log.Ctx{"err": err})
+		// Don't return here, as we still should update the in-memory cache to allow the cluster to
+		// continue functioning, and hopefully the write will succeed on next update.
 	}
 
 	d.clientCerts.Lock.Lock()
