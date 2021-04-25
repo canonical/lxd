4966562d3d19ad3ddfbffea9f9745c3805b99971
Piotr Resztak <piotr.resztak@gmail.com>
2021-04-25 16:35:29 +0200

    lxd/ip: Add ip package
    Signed-off-by: Piotr Resztak <piotr.resztak@gmail.com>

---
+++ lxd/ip/link_vxlan.go
@@ -0,0 +1,47 @@
+package ip
+
+// Vxlan represents arguments for link of type vxlan
+type Vxlan struct {
+	Link
+	VxlanID string
+	DevName string
+	Local   string
+	Remote  string
+	Group   string
+	DstPort string
+	TTL     string
+	FanMap  string
+}
+
+// additionalArgs generates vxlan specific arguments
+func (vxlan *Vxlan) additionalArgs() []string {
+	args := []string{}
+	args = append(args, "id", vxlan.VxlanID)
+	if vxlan.DevName != "" {
+		args = append(args, "dev", vxlan.DevName)
+	}
+	if vxlan.Group != "" {
+		args = append(args, "group", vxlan.Group)
+	}
+	if vxlan.Remote != "" {
+		args = append(args, "remote", vxlan.Remote)
+	}
+	if vxlan.Local != "" {
+		args = append(args, "local", vxlan.Local)
+	}
+	if vxlan.TTL != "" {
+		args = append(args, "ttl", vxlan.TTL)
+	}
+	if vxlan.DstPort != "" {
+		args = append(args, "dstport", vxlan.DstPort)
+	}
+	if vxlan.FanMap != "" {
+		args = append(args, "fan-map", vxlan.FanMap)
+	}
+	return args
+}
+
+// Add adds new virtual link
+func (vxlan *Vxlan) Add() error {
+	return vxlan.Link.add("vxlan", vxlan.additionalArgs())
+}
---
+++ lxd/ip/tuntap.go
@@ -0,0 +1,25 @@
+package ip
+
+import (
+	"github.com/lxc/lxd/shared"
+)
+
+// Tuntap represents arguments for tuntap manipulation
+type Tuntap struct {
+	Name       string
+	Mode       string
+	MultiQueue bool
+}
+
+// Add adds new tuntap interface
+func (t *Tuntap) Add() error {
+	cmd := []string{"tuntap", "add", "name", t.Name, "mode", t.Mode}
+	if t.MultiQueue {
+		cmd = append(cmd, "multi_queue")
+	}
+	_, err := shared.RunCommand("ip", cmd...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
