59ad1d4cc19ac8b124aa55de64927e22a939725a
Christian Brauner <christian.brauner@ubuntu.com>
2021-04-28 15:44:20 +0200

    include: add open_tree() and mount_setattr()
    Idmapped mounts require the open_tree() and mount_setattr() syscalls
    which are part of the new mount API. Add syscall number definitions as
    well as syscall- and type wrappers.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/include/syscall_wrappers.h
+++ lxd/include/syscall_wrappers.h
@@ -21,4 +21,25 @@ static inline int close_range(unsigned int fd, unsigned int max_fd, unsigned int
 	return syscall(__NR_close_range, fd, max_fd, flags);
 }
 
+static inline int open_tree(int dfd, const char *filename, unsigned int flags)
+{
+	return syscall(__NR_open_tree, dfd, filename, flags);
+}
+
+/*
+ * mount_setattr()
+ */
+struct lxc_mount_attr {
+	__u64 attr_set;
+	__u64 attr_clr;
+	__u64 propagation;
+	__u64 userns_fd;
+};
+
+static inline int mount_setattr(int dfd, const char *path, unsigned int flags,
+				struct lxc_mount_attr *attr, size_t size)
+{
+	return syscall(__NR_mount_setattr, dfd, path, flags, attr, size);
+}
+
 #endif /* __LXD_SYSCALL_WRAPPER_H */
