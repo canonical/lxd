9468097af14f398367644893719fcd70bef73215
Stéphane Graber <stgraber@ubuntu.com>
2021-05-05 18:35:03 -0400

    shared/api: Fix snapshot structs
    Read-only fields were incorrectly part of the writable struct.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/api/instance_snapshot.go
+++ shared/api/instance_snapshot.go
@@ -29,12 +29,7 @@ type InstanceSnapshotPost struct {
 //
 // API extension: instances
 type InstanceSnapshotPut struct {
-	Architecture string                       `json:"architecture" yaml:"architecture"`
-	Config       map[string]string            `json:"config" yaml:"config"`
-	Devices      map[string]map[string]string `json:"devices" yaml:"devices"`
-	Ephemeral    bool                         `json:"ephemeral" yaml:"ephemeral"`
-	Profiles     []string                     `json:"profiles" yaml:"profiles"`
-	ExpiresAt    time.Time                    `json:"expires_at" yaml:"expires_at"`
+	ExpiresAt time.Time `json:"expires_at" yaml:"expires_at"`
 }
 
 // InstanceSnapshot represents a LXD instance snapshot.
@@ -43,11 +38,16 @@ type InstanceSnapshotPut struct {
 type InstanceSnapshot struct {
 	InstanceSnapshotPut `yaml:",inline"`
 
+	Architecture    string                       `json:"architecture" yaml:"architecture"`
+	Config          map[string]string            `json:"config" yaml:"config"`
 	CreatedAt       time.Time                    `json:"created_at" yaml:"created_at"`
+	Devices         map[string]map[string]string `json:"devices" yaml:"devices"`
+	Ephemeral       bool                         `json:"ephemeral" yaml:"ephemeral"`
 	ExpandedConfig  map[string]string            `json:"expanded_config" yaml:"expanded_config"`
 	ExpandedDevices map[string]map[string]string `json:"expanded_devices" yaml:"expanded_devices"`
 	LastUsedAt      time.Time                    `json:"last_used_at" yaml:"last_used_at"`
 	Name            string                       `json:"name" yaml:"name"`
+	Profiles        []string                     `json:"profiles" yaml:"profiles"`
 	Stateful        bool                         `json:"stateful" yaml:"stateful"`
 
 	// API extension: snapshot_disk_usage
