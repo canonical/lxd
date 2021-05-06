e8e858e1c4071730b8c4c866c7b9eeb63b1e4a71
Thomas Parrott <thomas.parrott@canonical.com>
2021-05-06 18:03:39 +0100

    lxd/db/query/retry: Use errors.Cause in Retry
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/db/query/retry.go
+++ lxd/db/query/retry.go
@@ -26,7 +26,7 @@ func Retry(f func() error) error {
 		err = f()
 		if err != nil {
 			// No point in re-trying or logging a no-row error.
-			if err == sql.ErrNoRows {
+			if errors.Cause(err) == sql.ErrNoRows {
 				break
 			}
 
