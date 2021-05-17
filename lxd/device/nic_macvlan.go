c60195cc6f92bf31feb82eb9c3204db53c3ff31a
Thomas Parrott <thomas.parrott@canonical.com>
2021-05-17 14:04:51 +0100

    lxd/device/nic/macvlan: SetMTU usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/nic_macvlan.go
+++ lxd/device/nic_macvlan.go
@@ -3,6 +3,8 @@ package device
 import (
 	"fmt"
 
+	"github.com/pkg/errors"
+
 	deviceConfig "github.com/lxc/lxd/lxd/device/config"
 	"github.com/lxc/lxd/lxd/instance"
 	"github.com/lxc/lxd/lxd/instance/instancetype"
@@ -134,9 +136,10 @@ func (d *nicMACVLAN) Start() (*deviceConfig.RunConfig, error) {
 
 	// Set the MTU.
 	if d.config["mtu"] != "" {
-		err = network.InterfaceSetMTU(saveData["host_name"], d.config["mtu"])
+		link := &ip.Link{Name: saveData["host_name"]}
+		err := link.SetMTU(d.config["mtu"])
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrapf(err, "Failed setting MTU %q on %q", d.config["mtu"], saveData["host_name"])
 		}
 	}
 
