365830d9646f51cbcc768f02495ea215230e5edb
Thomas Parrott <thomas.parrott@canonical.com>
2021-05-20 17:41:03 +0100

    lxd/instance/drivers/driver/qemu: Allocate 4 additional PCI slots for hotplugging in generateQemuConfigFile
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -2335,6 +2335,11 @@ func (d *qemu) generateQemuConfigFile(mountInfo *storagePools.MountInfo, busName
 		}
 	}
 
+	// Allocate 4 PCI slots for hotplug devices.
+	for i := 0; i < 4; i++ {
+		bus.allocate(busFunctionGroupNone)
+	}
+
 	// Write the agent mount config.
 	agentMountJSON, err := json.Marshal(agentMounts)
 	if err != nil {
