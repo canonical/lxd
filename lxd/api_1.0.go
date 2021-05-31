4a21c9f4ccb3621cac9995a81576b9831948f670
Piotr Resztak <piotr.resztak@gmail.com>
2021-05-31 23:08:14 +0200

    lxd: Support HAProxy protocol header
    Signed-off-by: Piotr Resztak <piotr.resztak@gmail.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -649,6 +649,8 @@ func doApi10UpdateTriggers(d *Daemon, nodeChanged, clusterChanged map[string]str
 
 	for key := range clusterChanged {
 		switch key {
+		case "core.https_trusted_proxy":
+			d.endpoints.NetworkUpdateTrustedProxy(clusterChanged[key])
 		case "core.proxy_http":
 			fallthrough
 		case "core.proxy_https":
@@ -710,6 +712,7 @@ func doApi10UpdateTriggers(d *Daemon, nodeChanged, clusterChanged map[string]str
 		if err != nil {
 			return err
 		}
+		d.endpoints.NetworkUpdateTrustedProxy(clusterConfig.HTTPSTrustedProxy())
 	}
 
 	value, ok = nodeChanged["cluster.https_address"]
@@ -718,6 +721,7 @@ func doApi10UpdateTriggers(d *Daemon, nodeChanged, clusterChanged map[string]str
 		if err != nil {
 			return err
 		}
+		d.endpoints.NetworkUpdateTrustedProxy(clusterConfig.HTTPSTrustedProxy())
 	}
 
 	value, ok = nodeChanged["core.debug_address"]
