36112821c1d62a6e0111707a03ea74fd890e1d6d
Stéphane Graber <stgraber@ubuntu.com>
2021-06-01 21:40:19 -0400

    shared/api: Add swagger metadata for storage volume state
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/api/storage_pool_volume_state.go
+++ shared/api/storage_pool_volume_state.go
@@ -1,15 +1,22 @@
 package api
 
-// StorageVolumeState represents the live state of the volume.
+// StorageVolumeState represents the live state of the volume
+//
+// swagger:model
 //
 // API extension: storage_volume_state
 type StorageVolumeState struct {
+	// Volume usage
 	Usage *StorageVolumeStateUsage `json:"usage" yaml:"usage"`
 }
 
-// StorageVolumeStateUsage represents the disk usage of a volume.
+// StorageVolumeStateUsage represents the disk usage of a volume
+//
+// swagger:model
 //
 // API extension: storage_volume_state
 type StorageVolumeStateUsage struct {
+	// Used space in bytes
+	// Example: 1693552640
 	Used uint64 `json:"used,omitempty" yaml:"used,omitempty"`
 }
