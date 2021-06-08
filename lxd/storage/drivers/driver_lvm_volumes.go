87420d583d949f096fe9fbeae9f74a61a31d463c
Thomas Parrott <thomas.parrott@canonical.com>
2021-06-08 17:03:22 +0100

    lxd/storage/drivers/driver/lvm/volumes: shrinkFileSystem force arg usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_lvm_volumes.go
+++ lxd/storage/drivers/driver_lvm_volumes.go
@@ -445,7 +445,12 @@ func (d *lvm) SetVolumeQuota(vol Volume, size string, op *operations.Operation) 
 			}
 
 			// Shrink filesystem first.
-			err = shrinkFileSystem(fsType, volDevPath, vol, sizeBytes)
+			// Pass vol.allowUnsafeResize to allow disabling of filesystem resize safety checks.
+			// We do this as a separate step rather than passing -r to lvresize in resizeLogicalVolume
+			// so that we can have more control over when we trigger unsafe filesystem resize mode,
+			// otherwise by passing -f to lvresize (required for other reasons) this would then pass
+			// -f onto resize2fs as well.
+			err = shrinkFileSystem(fsType, volDevPath, vol, sizeBytes, vol.allowUnsafeResize)
 			if err != nil {
 				return err
 			}
