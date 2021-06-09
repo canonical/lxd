a8ef0dd570c23b6194992fa264393e7423db4003
Max Asnaashari <max.asnaashari@canonical.com>
2021-06-09 18:22:56 +0000

    shared/api/cluster shared/version/api client/interfaces client/lxd_cluster lxd/api_1.0 lxd/api_cluster lxd/certificates lxd/cluster/gateway lxd/cluster/heartbeat lxd/daemon lxd/endpoints/cluster lxd/endpoints/endpoints: cluster api things
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -271,6 +271,7 @@ var APIExtensions = []string{
 	"projects_restricted_backups_and_snapshots",
 	"clustering_join_token",
 	"clustering_description",
+	"clustering_update_certs",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
