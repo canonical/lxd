e0a080b06993bbaea22763331a92352726a381d1
Thomas Parrott <thomas.parrott@canonical.com>
2021-06-09 14:24:11 +0100

    Revert "lxd/storage/drivers/driver/ceph/volumes: Updates CreateVolumeFromBackup to use createVolumeFromBackupInstancePostHookResize in generic post hook"
    This reverts commit 4ea9e933e32a6104cf01378dedf8627f3214c220.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_ceph_volumes.go
+++ lxd/storage/drivers/driver_ceph_volumes.go
@@ -288,34 +288,7 @@ func (d *ceph) getVolumeSize(volumeName string) (int64, error) {
 
 // CreateVolumeFromBackup re-creates a volume from its exported state.
 func (d *ceph) CreateVolumeFromBackup(vol Volume, srcBackup backup.Info, srcData io.ReadSeeker, op *operations.Operation) (func(vol Volume) error, func(), error) {
-	postHook, revertHook, err := genericVFSBackupUnpack(d, vol, srcBackup.Snapshots, srcData, op)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// genericVFSBackupUnpack returns a nil postHook when volume's type is VolumeTypeCustom which
-	// doesn't need any post hook processing after DB record creation.
-	if postHook != nil {
-		// Define a post hook function that can be run once the backup config has been restored.
-		// This will setup the quota using the restored config.
-		postHookWrapper := func(vol Volume) error {
-			err := postHook(vol)
-			if err != nil {
-				return err
-			}
-
-			err = d.createVolumeFromBackupInstancePostHookResize(d, vol, op)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-		return postHookWrapper, revertHook, nil
-	}
-
-	return nil, revertHook, nil
+	return genericVFSBackupUnpack(d, vol, srcBackup.Snapshots, srcData, op)
 }
 
 // CreateVolumeFromCopy provides same-pool volume copying functionality.
