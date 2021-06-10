fcea2c3a43fbe25e60194f00011c46079a729411
Stéphane Graber <stgraber@ubuntu.com>
2021-06-10 09:47:52 -0400

    lxd/request: Introduce new package
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ lxd/request/const.go
@@ -0,0 +1,33 @@
+package request
+
+const (
+	// CtxAccess is the access field in request context.
+	CtxAccess = "access"
+
+	// CtxAddress is the address field in request context.
+	CtxAddress = "address"
+
+	// CtxUsername is the username field in request context.
+	CtxUsername = "username"
+
+	// CtxProtocol is the protocol field in request context.
+	CtxProtocol = "protocol"
+
+	// CtxForwardedAddress is the forwarded address field in request context.
+	CtxForwardedAddress = "forwarded_address"
+
+	// CtxForwardedUsername is the forwarded username field in request context.
+	CtxForwardedUsername = "forwarded_username"
+
+	// CtxForwardedProtocol is the forwarded protocol field in request context.
+	CtxForwardedProtocol = "forwarded_protocol"
+
+	// HeaderForwardedAddress is the forwarded address field in request header.
+	HeaderForwardedAddress = "X-LXD-forwarded-address"
+
+	// HeaderForwardedUsername is the forwarded username field in request header.
+	HeaderForwardedUsername = "X-LXD-forwarded-username"
+
+	// HeaderForwardedProtocol is the forwarded protocol field in request header.
+	HeaderForwardedProtocol = "X-LXD-forwarded-protocol"
+)
