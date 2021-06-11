36f97532482bd2a05a9aa3f9ee5a82949603ed3e
Thomas Parrott <thomas.parrott@canonical.com>
2021-06-11 16:03:04 +0100

    test: Add test for unmanaged bridge IP filtering
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/suites/container_devices_nic_bridged_filtering.sh
+++ test/suites/container_devices_nic_bridged_filtering.sh
@@ -545,6 +545,9 @@ test_container_devices_nic_bridged_filtering() {
 
   # Test MAC filtering on unmanaged bridge.
   ip link add "${brName}2" type bridge
+  ip a add 192.0.2.1/24 dev "${brName}2"
+  ip a add 2001:db8::1/64 dev "${brName}2"
+  ip link set "${brName}2" up
 
   lxc init testimage "${ctPrefix}A" -p "${ctPrefix}"
   lxc config device add "${ctPrefix}A" eth0 nic \
@@ -602,6 +605,28 @@ test_container_devices_nic_bridged_filtering() {
       fi
     done
   fi
+
+  # Enable IP filtering and check container won't start without manual IP assigned in LXD config.
+  lxc config device set "${ctPrefix}A" eth0 security.ipv4_filtering=true security.ipv6_filtering=true
+  ! lxc start "${ctPrefix}A" || false
+  lxc config device set "${ctPrefix}A" eth0 ipv4.address=192.0.2.2
+  ! lxc start "${ctPrefix}A" || false
+  lxc config device set "${ctPrefix}A" eth0 ipv6.address=2001:db8::2
+  lxc start "${ctPrefix}A"
+  lxc exec "${ctPrefix}A" -- ip a add 192.0.2.2/24 dev eth0
+  lxc exec "${ctPrefix}A" -- ip a add 2001:db8::2/64 dev eth0
+
+  # Check basic connectivity without any filtering.
+  lxc exec "${ctPrefix}A" -- ping -c2 -W1 192.0.2.1
+  lxc exec "${ctPrefix}A" -- ping -c2 -W1 2001:db8::1
+
+  # Check fraudulent IPs are blocked.
+  lxc exec "${ctPrefix}A" -- ip a flush dev eth0
+  lxc exec "${ctPrefix}A" -- ip a add 192.0.2.3/24 dev eth0
+  lxc exec "${ctPrefix}A" -- ip a add 2001:db8::3/64 dev eth0
+
+  ! lxc exec "${ctPrefix}A" -- ping -c2 -W1 192.0.2.1 || false
+  ! lxc exec "${ctPrefix}A" -- ping -c2 -W1 2001:db8::1 || false
 
   lxc delete -f "${ctPrefix}A"
   ip link delete "${brName}2"
