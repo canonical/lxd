05e1b16543f56f38f4547a195e2e5b5d83dca2a1
Thomas Parrott <thomas.parrott@canonical.com>
2021-06-15 11:36:29 +0100

    lxd/main/init/auto: Updates RunAuto to use poolType and associated constants
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/main_init_auto.go
+++ lxd/main_init_auto.go
@@ -20,7 +20,7 @@ func (c *cmdInit) RunAuto(cmd *cobra.Command, args []string, d lxd.InstanceServe
 		return nil, fmt.Errorf("The requested backend '%s' isn't supported by lxd init", c.flagStorageBackend)
 	}
 
-	if c.flagStorageBackend != "" && !shared.StringInSlice(c.flagStorageBackend, c.availableStorageDrivers("all")) {
+	if c.flagStorageBackend != "" && !shared.StringInSlice(c.flagStorageBackend, c.availableStorageDrivers(poolTypeAny)) {
 		return nil, fmt.Errorf("The requested backend '%s' isn't available on your system (missing tools)", c.flagStorageBackend)
 	}
 
