7db011ee0bd06b76ace7fa5b6a0ad7405de5e2f3
Thomas Parrott <thomas.parrott@canonical.com>
2021-06-16 11:38:28 +0100

    lxd/device/nic: Return -1 for Mtu in State() for bridged and ovn NICs if host interface not available
    Rather than failing the entire State() call.
    
    This ensures that if a client is polling the /state endpoint to find when a VM has stopped that it won't sometimes return an error during the VM shutdown process, if the NIC has been cleaned up but the VM is not yet recorded as stopped.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/nic_bridged.go
+++ lxd/device/nic_bridged.go
@@ -1051,10 +1051,15 @@ func (d *nicBridged) State() (*api.InstanceStateNetwork, error) {
 		addresses = append(addresses, addr)
 	}
 
-	// Get MTU.
+	// Get MTU of host interface if exists.
 	iface, err := net.InterfaceByName(d.config["host_name"])
 	if err != nil {
-		return nil, errors.Wrapf(err, "Failed getting host interface state for %q", d.config["host_name"])
+		d.logger.Warn("Failed getting host interface state for MTU", log.Ctx{"host_name": d.config["host_name"], "err": err})
+	}
+
+	mtu := -1
+	if iface != nil {
+		mtu = iface.MTU
 	}
 
 	// Retrieve the host counters, as we report the values from the instance's point of view,
@@ -1074,7 +1079,7 @@ func (d *nicBridged) State() (*api.InstanceStateNetwork, error) {
 		},
 		Hwaddr:   d.config["hwaddr"],
 		HostName: d.config["host_name"],
-		Mtu:      iface.MTU,
+		Mtu:      mtu,
 		State:    "up",
 		Type:     "broadcast",
 	}
