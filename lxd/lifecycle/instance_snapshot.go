a3408c08b43e19dbcb9f9ddc8fbb4c9132274a3d
Max Asnaashari <max.asnaashari@canonical.com>
2021-06-22 00:27:50 +0000

    lxd/lifecycle/instance/snapshot: add instance_snapshot
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/lifecycle/instance_snapshot.go
@@ -0,0 +1,44 @@
+package lifecycle
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/lxc/lxd/lxd/project"
+	"github.com/lxc/lxd/shared"
+	"github.com/lxc/lxd/shared/api"
+)
+
+// InstanceSnapshotAction represents a lifecycle event action for instance snapshots.
+type InstanceSnapshotAction string
+
+// All supported lifecycle events for instance snapshots.
+const (
+	InstanceSnapshotCreated = InstanceSnapshotAction("created")
+	InstanceSnapshotDeleted = InstanceSnapshotAction("deleted")
+	InstanceSnapshotRenamed = InstanceSnapshotAction("renamed")
+	InstanceSnapshotUpdated = InstanceSnapshotAction("updated")
+)
+
+// Event creates the lifecycle event for an action on an instance snapshot.
+func (a InstanceSnapshotAction) Event(inst instance, ctx map[string]interface{}) api.EventLifecycle {
+	parentName, instanceName, _ := shared.InstanceGetParentAndSnapshotName(inst.Name())
+	u := fmt.Sprintf("/1.0/instances/%s/snapshots/%s", url.PathEscape(parentName), url.PathEscape(instanceName))
+	eventType := fmt.Sprintf("instance-snapshot-%s", a)
+
+	if inst.Project() != project.Default {
+		u = fmt.Sprintf("%s?project=%s", u, url.QueryEscape(inst.Project()))
+	}
+
+	var requestor *api.EventLifecycleRequestor
+	if inst.Operation() != nil {
+		requestor = inst.Operation().Requestor()
+	}
+
+	return api.EventLifecycle{
+		Action:    eventType,
+		Source:    u,
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
