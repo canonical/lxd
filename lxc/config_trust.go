935f3c0ddc25144e594520e4751cef240b17a275
Max Asnaashari <max.asnaashari@canonical.com>
2021-06-22 19:55:10 +0000

    lxc/config/trust: workaround for subcommand errors
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxc/config_trust.go
+++ lxc/config_trust.go
@@ -40,6 +40,9 @@ func (c *cmdConfigTrust) Command() *cobra.Command {
 	configTrustRemoveCmd := cmdConfigTrustRemove{global: c.global, config: c.config, configTrust: c}
 	cmd.AddCommand(configTrustRemoveCmd.Command())
 
+	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
+	cmd.Args = cobra.NoArgs
+	cmd.Run = func(cmd *cobra.Command, args []string) { cmd.Usage() }
 	return cmd
 }
 
