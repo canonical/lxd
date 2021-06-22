532650b58b8348a0a0d648f2c294cc13adc75fc6
Max Asnaashari <max.asnaashari@canonical.com>
2021-06-22 19:55:10 +0000

    lxc/storage: workaround for subcommand errors
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxc/storage.go
+++ lxc/storage.go
@@ -73,6 +73,9 @@ func (c *cmdStorage) Command() *cobra.Command {
 	storageVolumeCmd := cmdStorageVolume{global: c.global, storage: c}
 	cmd.AddCommand(storageVolumeCmd.Command())
 
+	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
+	cmd.Args = cobra.NoArgs
+	cmd.Run = func(cmd *cobra.Command, args []string) { cmd.Usage() }
 	return cmd
 }
 
