5db0bd24bd7319307343d822427675a9deebb0a5
Max Asnaashari <max.asnaashari@canonical.com>
2021-06-22 19:56:04 +0000

    lxd/main/forkmount: workaround for subcommand errors
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/main_forkmount.go
+++ lxd/main_forkmount.go
@@ -490,6 +490,9 @@ func (c *cmdForkmount) Command() *cobra.Command {
 	cmdLXDUmount.RunE = c.Run
 	cmd.AddCommand(cmdLXDUmount)
 
+	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
+	cmd.Args = cobra.NoArgs
+	cmd.Run = func(cmd *cobra.Command, args []string) { cmd.Usage() }
 	return cmd
 }
 
