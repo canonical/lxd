64ea71f66e6af157534211277086ca08056aa234
Max Asnaashari <max.asnaashari@canonical.com>
2021-06-25 01:11:14 +0000

    lxd/api/cluster: handle ClusterCertificateUpdated lifecycle event
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/api_cluster.go
+++ lxd/api_cluster.go
@@ -1641,6 +1641,9 @@ func clusterCertificatePut(d *Daemon, r *http.Request) response.Response {
 	d.endpoints.NetworkUpdateCert(cert)
 	d.gateway.NetworkUpdateCert(cert)
 
+	requestor := request.CreateRequestor(r)
+	d.State().Events.SendLifecycle(projectParam(r), lifecycle.ClusterCertificateUpdated.Event("certificate", requestor, nil))
+
 	return response.EmptySyncResponse
 }
 
