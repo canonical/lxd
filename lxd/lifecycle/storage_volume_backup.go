820d0cc38b625af330537efb4f98b2345bd4db9b
Max Asnaashari <max.asnaashari@canonical.com>
2021-06-28 23:24:13 +0000

    lxd/lifecycle/storage/volume/backup: add storage_volume_backup
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/lifecycle/storage_volume_backup.go
@@ -0,0 +1,42 @@
+package lifecycle
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/lxc/lxd/lxd/project"
+	"github.com/lxc/lxd/shared"
+	"github.com/lxc/lxd/shared/api"
+)
+
+// StorageVolumeBackupAction represents a lifecycle event action for storage volume backups.
+type StorageVolumeBackupAction string
+
+// All supported lifecycle events for storage volume backups.
+const (
+	StorageVolumeBackupCreated   = StorageVolumeBackupAction("created")
+	StorageVolumeBackupDeleted   = StorageVolumeBackupAction("deleted")
+	StorageVolumeBackupRetrieved = StorageVolumeBackupAction("retrieved")
+	StorageVolumeBackupRenamed   = StorageVolumeBackupAction("renamed")
+)
+
+// Event creates the lifecycle event for an action on a storage volume backup.
+func (a StorageVolumeBackupAction) Event(poolName string, volumeType string, volumeName string, projectName string, requestor *api.EventLifecycleRequestor, ctx map[string]interface{}) api.EventLifecycle {
+	eventType := fmt.Sprintf("storage-volume-backup-%s", a)
+	parentName, backupName, _ := shared.InstanceGetParentAndSnapshotName(volumeName)
+	u := fmt.Sprintf("/1.0/storage-pools/%s/volumes/%s/%s/backups", url.PathEscape(poolName), url.PathEscape(volumeType), url.PathEscape(parentName))
+	if backupName != "" {
+		u = fmt.Sprintf("%s/%s", u, backupName)
+	}
+
+	if projectName != project.Default {
+		u = fmt.Sprintf("%s?project=%s", u, url.QueryEscape(projectName))
+	}
+
+	return api.EventLifecycle{
+		Action:    eventType,
+		Source:    u,
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
