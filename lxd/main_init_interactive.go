8a42ea90efee8e521ca781bc398c601fd8f4d198
MarsssBlkm <mars.blackman.23@gmail.com>
2021-06-30 15:35:54 +1000

    lxd/main_init_interactive: only ask for server host name when no joining token was provided
    Signed-off-by: Jianming Ma <ma.jianming@hotmail.com>

--- lxd/main_init_interactive.go
+++ lxd/main_init_interactive.go
@@ -112,8 +112,10 @@ func (c *cmdInit) askClustering(config *cmdInitData, d lxd.InstanceServer) error
 			serverName = "lxd"
 		}
 
-		config.Cluster.ServerName = cli.AskString(
-			fmt.Sprintf("What name should be used to identify this node in the cluster? [default=%s]: ", serverName), serverName, nil)
+		askForServerName := func() {
+			config.Cluster.ServerName = cli.AskString(
+				fmt.Sprintf("What name should be used to identify this node in the cluster? [default=%s]: ", serverName), serverName, nil)
+		}
 
 		// Cluster server address
 		address := util.NetworkInterfaceAddress()
@@ -168,9 +170,8 @@ func (c *cmdInit) askClustering(config *cmdInitData, d lxd.InstanceServer) error
 
 				rawJoinToken := cli.AskString("Please provide join token: ", "", validJoinToken)
 
-				if joinToken.ServerName != config.Cluster.ServerName {
-					return fmt.Errorf("Server name does not match the one specified in join token")
-				}
+				// Set server name from join token
+				config.Cluster.ServerName = joinToken.ServerName
 
 				// Attempt to find a working cluster member to use for joining by retrieving the
 				// cluster certificate from each address in the join token until we succeed.
@@ -206,6 +207,9 @@ func (c *cmdInit) askClustering(config *cmdInitData, d lxd.InstanceServer) error
 				// Raw join token used as cluster password so it can be validated.
 				config.Cluster.ClusterPassword = rawJoinToken
 			} else {
+				// Ask for server name since no token is provided
+				askForServerName()
+
 				for {
 					// Cluster URL
 					clusterAddress := cli.AskString("IP address or FQDN of an existing cluster node: ", "", nil)
@@ -284,6 +288,9 @@ func (c *cmdInit) askClustering(config *cmdInitData, d lxd.InstanceServer) error
 
 			config.Cluster.MemberConfig = cluster.MemberConfig
 		} else {
+			// Ask for server name since no token is provided
+			askForServerName()
+
 			// Password authentication
 			if cli.AskBool("Setup password authentication on the cluster? (yes/no) [default=no]: ", "no") {
 				config.Node.Config["core.trust_password"] = cli.AskPassword("Trust password for new clients: ")
