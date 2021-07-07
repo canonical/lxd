38e9eb99f716ceb916fead7a14853783ace144f1
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-07 22:44:47 +0000

    lxd/lifecycle/config: add config
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/lifecycle/config.go
@@ -0,0 +1,28 @@
+package lifecycle
+
+import (
+	"fmt"
+
+	"github.com/lxc/lxd/shared/api"
+)
+
+// ConfigAction represents a lifecycle event action for the server configuration
+type ConfigAction string
+
+// All supported lifecycle events for the server configuration
+const (
+	ConfigUpdated = ConfigAction("updated")
+)
+
+// Event creates the lifecycle event for an action on the server configuration
+func (a ConfigAction) Event(requestor *api.EventLifecycleRequestor, ctx map[string]interface{}) api.EventLifecycle {
+	eventType := fmt.Sprintf("config-%s", a)
+	u := fmt.Sprintf("/1.0")
+
+	return api.EventLifecycle{
+		Action:    eventType,
+		Source:    u,
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
