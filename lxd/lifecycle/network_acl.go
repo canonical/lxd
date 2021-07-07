89ce1b98c272ef475f0a61f710e26c25e7215e83
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-07 20:32:08 +0000

    lxd/lifecycle/network/acl: add network_acl
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/lifecycle/network_acl.go
@@ -0,0 +1,46 @@
+package lifecycle
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/lxc/lxd/lxd/project"
+	"github.com/lxc/lxd/shared/api"
+)
+
+// Internal copy of the network acl interface.
+type networkACL interface {
+	Info() *api.NetworkACL
+	Project() string
+}
+
+// NetworkACLAction represents a lifecycle event action for network acls.
+type NetworkACLAction string
+
+// All supported lifecycle events for network acls.
+const (
+	NetworkACLCreated = NetworkACLAction("created")
+	NetworkACLDeleted = NetworkACLAction("deleted")
+	NetworkACLUpdated = NetworkACLAction("updated")
+	NetworkACLRenamed = NetworkACLAction("renamed")
+)
+
+// Event creates the lifecycle event for an action on a network acl.
+func (a NetworkACLAction) Event(n networkACL, requestor *api.EventLifecycleRequestor, ctx map[string]interface{}) api.EventLifecycle {
+	eventType := fmt.Sprintf("network-acl-%s", a)
+
+	u := fmt.Sprintf("/1.0/network-acls")
+
+	if a != NetworkACLCreated {
+		u = fmt.Sprintf("%s/%s", u, url.PathEscape(n.Info().Name))
+	}
+	if n.Project() != project.Default {
+		u = fmt.Sprintf("%s?project=%s", u, url.QueryEscape(n.Project()))
+	}
+	return api.EventLifecycle{
+		Action:    eventType,
+		Source:    u,
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
