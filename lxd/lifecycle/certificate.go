d1f5424d0e4f4283b01ce66fd97f8b87a397f376
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-07 22:20:56 +0000

    lxd/lifecycle/certificate: add certificate
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/lifecycle/certificate.go
@@ -0,0 +1,35 @@
+package lifecycle
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/lxc/lxd/shared/api"
+)
+
+// CertificateAction represents a lifecycle event action for Certificates.
+type CertificateAction string
+
+// All supported lifecycle events for Certificates.
+const (
+	CertificateCreated = CertificateAction("created")
+	CertificateDeleted = CertificateAction("deleted")
+	CertificateUpdated = CertificateAction("updated")
+)
+
+// Event creates the lifecycle event for an action on a Certificate.
+func (a CertificateAction) Event(fingerprint string, requestor *api.EventLifecycleRequestor, ctx map[string]interface{}) api.EventLifecycle {
+	eventType := fmt.Sprintf("certificate-%s", a)
+
+	u := fmt.Sprintf("/1.0/certificates")
+	if a != CertificateCreated {
+		u = fmt.Sprintf("%s/%s", u, url.PathEscape(fingerprint))
+	}
+
+	return api.EventLifecycle{
+		Action:    eventType,
+		Source:    u,
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
