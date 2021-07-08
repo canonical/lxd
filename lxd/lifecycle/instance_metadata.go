062fedd06ae4030578c665e95f471d05182cf0cd
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-08 01:00:49 +0000

    lxd/lifecycle/instance/metadata: add instance_metadata
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/lifecycle/instance_metadata.go
@@ -0,0 +1,35 @@
+package lifecycle
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/lxc/lxd/lxd/project"
+	"github.com/lxc/lxd/shared/api"
+)
+
+// InstanceMetadataAction represents a lifecycle event action for instance metadata.
+type InstanceMetadataAction string
+
+// All supported lifecycle events for instance metadata.
+const (
+	InstanceMetadataUpdated   = InstanceMetadataAction("updated")
+	InstanceMetadataRetrieved = InstanceMetadataAction("retrieved")
+)
+
+// Event creates the lifecycle event for an action on instance metadata.
+func (a InstanceMetadataAction) Event(inst instance, requestor *api.EventLifecycleRequestor, ctx map[string]interface{}) api.EventLifecycle {
+	eventType := fmt.Sprintf("instance-metadata-%s", a)
+	u := fmt.Sprintf("/1.0/instances/%s/metadata", url.PathEscape(inst.Name()))
+
+	if inst.Project() != project.Default {
+		u = fmt.Sprintf("%s?project=%s", u, url.QueryEscape(inst.Project()))
+	}
+
+	return api.EventLifecycle{
+		Action:    eventType,
+		Source:    u,
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
