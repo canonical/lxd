641464aeebdb83d267ed92ccb17868d1868025db
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-08 00:28:10 +0000

    lxd/lifecycle/instance/log: add instance_log
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/lifecycle/instance_log.go
@@ -0,0 +1,35 @@
+package lifecycle
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/lxc/lxd/lxd/project"
+	"github.com/lxc/lxd/shared/api"
+)
+
+// InstanceLogAction represents a lifecycle event action for instance logs.
+type InstanceLogAction string
+
+// All supported lifecycle events for instance logs.
+const (
+	InstanceLogRetrieved = InstanceLogAction("retrieved")
+	InstanceLogDeleted   = InstanceLogAction("deleted")
+)
+
+// Event creates the lifecycle event for an action on an instance log.
+func (a InstanceLogAction) Event(file string, inst instance, requestor *api.EventLifecycleRequestor, ctx map[string]interface{}) api.EventLifecycle {
+	eventType := fmt.Sprintf("instance-log-%s", a)
+	u := fmt.Sprintf("/1.0/instance/%s/logs/%s", url.PathEscape(inst.Name()), file)
+
+	if inst.Project() != project.Default {
+		u = fmt.Sprintf("%s?project=%s", u, url.QueryEscape(inst.Project()))
+	}
+
+	return api.EventLifecycle{
+		Action:    eventType,
+		Source:    u,
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
