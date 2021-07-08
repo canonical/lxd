5cc3cf03e77c51f067e446be9090ac1b9d352d51
Thomas Parrott <thomas.parrott@canonical.com>
2021-07-08 17:29:43 +0100

    lxd/storage/backend/mock: Adds ImportInstance placeholder function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/backend_mock.go
+++ lxd/storage/backend_mock.go
@@ -137,6 +137,10 @@ func (b *mockBackend) ListUnknownVolumes(op *operations.Operation) (map[string][
 	return nil, nil
 }
 
+func (b *mockBackend) ImportInstance(inst instance.Instance, op *operations.Operation) error {
+	return nil
+}
+
 func (b *mockBackend) MigrateInstance(inst instance.Instance, conn io.ReadWriteCloser, args *migration.VolumeSourceArgs, op *operations.Operation) error {
 	return nil
 }
