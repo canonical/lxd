c4b3d693ce223e6baa363b0a13acccf77a88a257
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-13 18:56:53 +0000

    lxd/db/operations: remove hard-coded functions
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/operations.go
+++ lxd/db/operations.go
@@ -4,10 +4,6 @@
 package db
 
 import (
-	"fmt"
-
-	"github.com/pkg/errors"
-
 	"github.com/lxc/lxd/lxd/db/query"
 )
 
@@ -42,18 +38,6 @@ type OperationFilter struct {
 	ID     int64
 	NodeID int64
 	UUID   string
-}
-
-// GetLocalOperations returns all operations associated with this node.
-func (c *ClusterTx) GetLocalOperations() ([]Operation, error) {
-	return c.operations("node_id=?", c.nodeID)
-}
-
-// GetLocalOperationsUUIDs returns the UUIDs of all operations associated with this
-// node.
-func (c *ClusterTx) GetLocalOperationsUUIDs() ([]string, error) {
-	stmt := "SELECT uuid FROM operations WHERE node_id=?"
-	return query.SelectStrings(c.tx, stmt, c.nodeID)
 }
 
 // GetNodesWithRunningOperations returns a list of nodes that have running operations
@@ -99,113 +83,4 @@ WHERE (projects.name = ? OR operations.project_id IS NULL) and operations.type =
 	}
 
 	return ops, nil
-}
-
-// GetOperationWithID returns the operation with the given ID.
-func (c *ClusterTx) GetOperationWithID(opID int) (Operation, error) {
-	null := Operation{}
-	operations, err := c.operations("id=?", opID)
-	if err != nil {
-		return null, err
-	}
-	switch len(operations) {
-	case 0:
-		return null, ErrNoSuchObject
-	case 1:
-		return operations[0], nil
-	default:
-		return null, fmt.Errorf("More than one operation matches")
-	}
-}
-
-// GetOperationByUUID returns the operation with the given UUID.
-func (c *ClusterTx) GetOperationByUUID(uuid string) (Operation, error) {
-	null := Operation{}
-	operations, err := c.operations("uuid=?", uuid)
-	if err != nil {
-		return null, err
-	}
-	switch len(operations) {
-	case 0:
-		return null, ErrNoSuchObject
-	case 1:
-		return operations[0], nil
-	default:
-		return null, fmt.Errorf("More than one operation matches")
-	}
-}
-
-// CreateOperation adds a new operations to the table.
-func (c *ClusterTx) CreateOperation(project, uuid string, typ OperationType) (int64, error) {
-	var projectID interface{}
-
-	if project != "" {
-		var err error
-		projectID, err = c.GetProjectID(project)
-		if err != nil {
-			return -1, errors.Wrap(err, "Fetch project ID")
-		}
-	} else {
-		projectID = nil
-	}
-
-	columns := []string{"uuid", "node_id", "type", "project_id"}
-	values := []interface{}{uuid, c.nodeID, typ, projectID}
-	return query.UpsertObject(c.tx, "operations", columns, values)
-}
-
-// RemoveOperation removes the operation with the given UUID.
-func (c *ClusterTx) RemoveOperation(uuid string) error {
-	result, err := c.tx.Exec("DELETE FROM operations WHERE uuid=?", uuid)
-	if err != nil {
-		return err
-	}
-	n, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if n != 1 {
-		return fmt.Errorf("query deleted %d rows instead of 1", n)
-	}
-	return nil
-}
-
-// Remove all operations for the given node.
-func (c *ClusterTx) removeNodeOperations(nodeID int64) error {
-	_, err := c.tx.Exec("DELETE FROM operations WHERE node_id=?", nodeID)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Operations returns all operations in the cluster, filtered by the given clause.
-func (c *ClusterTx) operations(where string, args ...interface{}) ([]Operation, error) {
-	operations := []Operation{}
-	dest := func(i int) []interface{} {
-		operations = append(operations, Operation{})
-		return []interface{}{
-			&operations[i].ID,
-			&operations[i].UUID,
-			&operations[i].NodeAddress,
-			&operations[i].Type,
-		}
-	}
-	sql := `
-SELECT operations.id, uuid, nodes.address, type FROM operations JOIN nodes ON nodes.id = node_id `
-	if where != "" {
-		sql += fmt.Sprintf("WHERE %s ", where)
-	}
-	sql += "ORDER BY operations.id"
-	stmt, err := c.tx.Prepare(sql)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-	err = query.SelectObjects(stmt, dest, args...)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to fetch operations")
-	}
-	return operations, nil
 }
