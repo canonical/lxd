791ff43fb3343e9de78c9e3be340c85e741e8019
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-13 04:01:38 +0000

    lxd/db/certificates: use Filter as parameter for delete
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/certificates.go
+++ lxd/db/certificates.go
@@ -162,7 +162,7 @@ func (c *Cluster) CreateCertificate(cert Certificate) (int64, error) {
 // DeleteCertificate deletes a certificate from the db.
 func (c *Cluster) DeleteCertificate(fingerprint string) error {
 	err := c.Transaction(func(tx *ClusterTx) error {
-		return tx.DeleteCertificate(fingerprint)
+		return tx.DeleteCertificate(CertificateFilter{Fingerprint: fingerprint})
 	})
 	return err
 }
