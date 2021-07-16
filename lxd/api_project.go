0e57135ca956ebd307652c57ebbde53eb3ba67c8
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-16 21:56:49 +0000

    lxd/api: use db.Project instead of api.Project
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/api_project.go
+++ lxd/api_project.go
@@ -159,7 +159,7 @@ func projectsGet(d *Daemon, r *http.Request) response.Response {
 					continue
 				}
 
-				filtered = append(filtered, project)
+				filtered = append(filtered, project.ToAPI())
 			}
 
 			result = filtered
@@ -221,7 +221,7 @@ func projectsGet(d *Daemon, r *http.Request) response.Response {
 //     $ref: "#/responses/InternalServerError"
 func projectsPost(d *Daemon, r *http.Request) response.Response {
 	// Parse the request.
-	project := api.ProjectsPost{}
+	project := db.Project{}
 
 	// Set default features.
 	if project.Config == nil {
@@ -359,7 +359,7 @@ func projectGet(d *Daemon, r *http.Request) response.Response {
 		project.Config,
 	}
 
-	return response.SyncResponseETag(true, project, etag)
+	return response.SyncResponseETag(true, project.ToAPI(), etag)
 }
 
 // swagger:operation PUT /1.0/projects/{name} projects project_put
@@ -525,7 +525,7 @@ func projectPatch(d *Daemon, r *http.Request) response.Response {
 }
 
 // Common logic between PUT and PATCH.
-func projectChange(d *Daemon, project *api.Project, req api.ProjectPut) response.Response {
+func projectChange(d *Daemon, project *db.Project, req api.ProjectPut) response.Response {
 	// Make a list of config keys that have changed.
 	configChanged := []string{}
 	for key := range project.Config {
@@ -829,7 +829,7 @@ func projectStateGet(d *Daemon, r *http.Request) response.Response {
 }
 
 // Check if a project is empty.
-func projectIsEmpty(project *api.Project) bool {
+func projectIsEmpty(project *db.Project) bool {
 	if len(project.UsedBy) > 0 {
 		// Check if the only entity is the default profile.
 		if len(project.UsedBy) == 1 && strings.Contains(project.UsedBy[0], "/profiles/default") {
