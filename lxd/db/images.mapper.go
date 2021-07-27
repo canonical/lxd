db33c49680924ed861e643abc68507882efb967e
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-27 20:28:13 +0000

    lxd/db/mapper: generated code
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/images.mapper.go
+++ lxd/db/images.mapper.go
@@ -71,6 +71,7 @@ SELECT images.id, projects.name AS project, images.fingerprint, images.type, ima
 `)
 
 // GetImages returns all available images.
+// generator: image List
 func (c *ClusterTx) GetImages(filter ImageFilter) ([]Image, error) {
 	// Result slice.
 	objects := make([]Image, 0)
@@ -178,6 +179,7 @@ func (c *ClusterTx) GetImages(filter ImageFilter) ([]Image, error) {
 }
 
 // GetImage returns the image with the given key.
+// generator: image Get
 func (c *ClusterTx) GetImage(project string, fingerprint string) (*Image, error) {
 	filter := ImageFilter{}
 	filter.Project = project
