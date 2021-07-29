ab3369566e964db282947a216095036bfc831183
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-29 21:54:51 +0000

    lxd/db/interface/mapper: add generated interface files
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/db/certificates.interface.mapper.go
@@ -0,0 +1,43 @@
+//go:build linux && cgo && !agent
+// +build linux,cgo,!agent
+
+package db
+
+// CertificateGenerated is an interface of generated methods for Certificate
+type CertificateGenerated interface {
+	// GetCertificates returns all available certificates.
+	// generator: certificate List
+	GetCertificates(filter CertificateFilter) ([]Certificate, error)
+
+	// GetCertificate returns the certificate with the given key.
+	// generator: certificate Get
+	GetCertificate(fingerprint string) (*Certificate, error)
+
+	// GetCertificateID return the ID of the certificate with the given key.
+	// generator: certificate ID
+	GetCertificateID(fingerprint string) (int64, error)
+
+	// CertificateExists checks if a certificate with the given key exists.
+	// generator: certificate Exists
+	CertificateExists(fingerprint string) (bool, error)
+
+	// CreateCertificate adds a new certificate to the database.
+	// generator: certificate Create
+	CreateCertificate(object Certificate) (int64, error)
+
+	// CertificateProjectsRef returns entities used by certificates.
+	// generator: certificate ProjectsRef
+	CertificateProjectsRef(filter CertificateFilter) (map[string][]string, error)
+
+	// DeleteCertificate deletes the certificate matching the given key parameters.
+	// generator: certificate DeleteOne
+	DeleteCertificate(filter CertificateFilter) error
+
+	// DeleteCertificates deletes the certificate matching the given key parameters.
+	// generator: certificate DeleteMany
+	DeleteCertificates(filter CertificateFilter) error
+
+	// UpdateCertificate updates the certificate matching the given key parameters.
+	// generator: certificate Update
+	UpdateCertificate(fingerprint string, object Certificate) error
+}
---
+++ lxd/db/profiles.interface.mapper.go
@@ -0,0 +1,55 @@
+//go:build linux && cgo && !agent
+// +build linux,cgo,!agent
+
+package db
+
+// ProfileGenerated is an interface of generated methods for Profile
+type ProfileGenerated interface {
+	// GetProfileURIs returns all available profile URIs.
+	// generator: profile URIs
+	GetProfileURIs(filter ProfileFilter) ([]string, error)
+
+	// GetProfiles returns all available profiles.
+	// generator: profile List
+	GetProfiles(filter ProfileFilter) ([]Profile, error)
+
+	// GetProfile returns the profile with the given key.
+	// generator: profile Get
+	GetProfile(project string, name string) (*Profile, error)
+
+	// ProfileExists checks if a profile with the given key exists.
+	// generator: profile Exists
+	ProfileExists(project string, name string) (bool, error)
+
+	// GetProfileID return the ID of the profile with the given key.
+	// generator: profile ID
+	GetProfileID(project string, name string) (int64, error)
+
+	// ProfileConfigRef returns entities used by profiles.
+	// generator: profile ConfigRef
+	ProfileConfigRef(filter ProfileFilter) (map[string]map[string]map[string]string, error)
+
+	// ProfileDevicesRef returns entities used by profiles.
+	// generator: profile DevicesRef
+	ProfileDevicesRef(filter ProfileFilter) (map[string]map[string]map[string]map[string]string, error)
+
+	// ProfileUsedByRef returns entities used by profiles.
+	// generator: profile UsedByRef
+	ProfileUsedByRef(filter ProfileFilter) (map[string]map[string][]string, error)
+
+	// CreateProfile adds a new profile to the database.
+	// generator: profile Create
+	CreateProfile(object Profile) (int64, error)
+
+	// RenameProfile renames the profile matching the given key parameters.
+	// generator: profile Rename
+	RenameProfile(project string, name string, to string) error
+
+	// DeleteProfile deletes the profile matching the given key parameters.
+	// generator: profile DeleteOne
+	DeleteProfile(filter ProfileFilter) error
+
+	// UpdateProfile updates the profile matching the given key parameters.
+	// generator: profile Update
+	UpdateProfile(project string, name string, object Profile) error
+}
