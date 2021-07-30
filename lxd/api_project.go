3f9a6323d3d58ca440e86e4a8849df3eae69425e
Max Asnaashari <max.asnaashari@canonical.com>
2021-07-30 06:26:46 +0000

    lxd/api/project: use explicit arguments for delete
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/api_project.go
+++ lxd/api_project.go
@@ -570,8 +570,7 @@ func projectChange(d *Daemon, project *db.Project, req api.ProjectPut) response.
 				}
 			} else {
 				// Delete the project-specific default profile.
-				filter := db.ProfileFilter{Project: project.Name, Name: projecthelpers.Default}
-				err = tx.DeleteProfile(filter)
+				err = tx.DeleteProfile(project.Name, projecthelpers.Default)
 				if err != nil {
 					return errors.Wrap(err, "Delete project default profile")
 				}
@@ -731,7 +730,7 @@ func projectDelete(d *Daemon, r *http.Request) response.Response {
 			return errors.Wrapf(err, "Fetch project id %q", name)
 		}
 
-		return tx.DeleteProject(db.ProjectFilter{Name: name})
+		return tx.DeleteProject(name)
 	})
 
 	if err != nil {
