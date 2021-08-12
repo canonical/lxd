efa20a82b128c4616e0d28ac8e67a0ed30f9a09d
Thomas Parrott <thomas.parrott@canonical.com>
2021-08-12 15:43:51 +0100

    lxd/network/errors: Adds ErrNotImplemented error
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/errors.go
+++ lxd/network/errors.go
@@ -6,3 +6,6 @@ import (
 
 // ErrUnknownDriver is the "Unknown driver" error.
 var ErrUnknownDriver = fmt.Errorf("Unknown driver")
+
+// ErrNotImplemented is the "Not implemented" error.
+var ErrNotImplemented = fmt.Errorf("Not implemented")
