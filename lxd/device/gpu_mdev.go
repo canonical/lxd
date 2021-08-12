cad8d2fa541f97d174eab81411ea3c4195dacba8
Stéphane Graber <stgraber@ubuntu.com>
2021-08-12 11:12:39 -0400

    lxd/device/gpu_mdev: Switch to common UUID package
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/device/gpu_mdev.go
+++ lxd/device/gpu_mdev.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/google/uuid"
+	"github.com/pborman/uuid"
 	"github.com/pkg/errors"
 
 	deviceConfig "github.com/lxc/lxd/lxd/device/config"
@@ -117,11 +117,7 @@ func (d *gpuMdev) startVM() (*deviceConfig.RunConfig, error) {
 
 		// Create the vGPU.
 		if mdevUUID == "" || !shared.PathExists(fmt.Sprintf("/sys/bus/pci/devices/%s/%s", pciAddress, mdevUUID)) {
-			devUUID, err := uuid.NewUUID()
-			if err != nil {
-				return nil, errors.Wrap(err, "Failed to generate UUID")
-			}
-			mdevUUID = devUUID.String()
+			mdevUUID := uuid.NewRandom().String()
 
 			err = ioutil.WriteFile(filepath.Join(fmt.Sprintf("/sys/bus/pci/devices/%s/mdev_supported_types/%s/create", pciAddress, d.config["mdev"])), []byte(mdevUUID), 200)
 			if err != nil {
