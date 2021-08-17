8ea734b55a61c15301c5af8ecfc831886bc5518b
Thomas Parrott <thomas.parrott@canonical.com>
2021-08-17 15:00:51 +0100

    lxd/storage/drivers/driver/btrfs/volumes: Consistently apply referenced limit only and remove exclusive limits in SetVolumeQuota
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_btrfs_volumes.go
+++ lxd/storage/drivers/driver_btrfs_volumes.go
@@ -765,14 +765,25 @@ func (d *btrfs) SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, 
 			d.logger.Debug("Accounting for VM image file size", "sizeBytes", sizeBytes)
 		}
 
-		// Apply the limit.
-		_, err := shared.RunCommand("btrfs", "qgroup", "limit", fmt.Sprintf("%d", sizeBytes), volPath)
+		// Apply the limit to referenced data in qgroup.
+		_, err = shared.RunCommand("btrfs", "qgroup", "limit", fmt.Sprintf("%d", sizeBytes), qgroup, volPath)
+		if err != nil {
+			return err
+		}
+
+		// Remove any former exclusive data limit.
+		_, err = shared.RunCommand("btrfs", "qgroup", "limit", "-e", "none", qgroup, volPath)
 		if err != nil {
 			return err
 		}
 	} else if qgroup != "" {
-		// Remove the limit.
-		_, err := shared.RunCommand("btrfs", "qgroup", "limit", "none", qgroup, volPath)
+		// Remove all limits.
+		_, err = shared.RunCommand("btrfs", "qgroup", "limit", "none", qgroup, volPath)
+		if err != nil {
+			return err
+		}
+
+		_, err = shared.RunCommand("btrfs", "qgroup", "limit", "none", "-e", qgroup, volPath)
 		if err != nil {
 			return err
 		}
