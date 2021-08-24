c35f049d7afcf15bce9b593f774f8e6f84701bde
Thomas Parrott <thomas.parrott@canonical.com>
2021-08-24 16:26:41 +0100

    lxd/firewall/firewall/interface: Updates InstanceSetupProxyNAT to accept AddressForward
    Decouples from proxy device.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/firewall/firewall_interface.go
+++ lxd/firewall/firewall_interface.go
@@ -3,7 +3,6 @@ package firewall
 import (
 	"net"
 
-	deviceConfig "github.com/lxc/lxd/lxd/device/config"
 	drivers "github.com/lxc/lxd/lxd/firewall/drivers"
 )
 
@@ -18,7 +17,7 @@ type Firewall interface {
 	InstanceSetupBridgeFilter(projectName string, instanceName string, deviceName string, parentName string, hostName string, hwAddr string, IPv4 net.IP, IPv6 net.IP, parentManaged bool) error
 	InstanceClearBridgeFilter(projectName string, instanceName string, deviceName string, parentName string, hostName string, hwAddr string, IPv4 net.IP, IPv6 net.IP) error
 
-	InstanceSetupProxyNAT(projectName string, instanceName string, deviceName string, listen *deviceConfig.ProxyAddress, connect *deviceConfig.ProxyAddress) error
+	InstanceSetupProxyNAT(projectName string, instanceName string, deviceName string, forward *drivers.AddressForward) error
 	InstanceClearProxyNAT(projectName string, instanceName string, deviceName string) error
 
 	InstanceSetupRPFilter(projectName string, instanceName string, deviceName string, hostName string) error
