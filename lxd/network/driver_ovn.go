8f2ffaaf52439aa91ec6d86eb455b6a824c1c974
Thomas Parrott <thomas.parrott@canonical.com>
2021-08-31 17:58:25 +0100

    lxd/network/driver/ovn: n.state.Cluster.GetNetworkForwards usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/driver_ovn.go
+++ lxd/network/driver_ovn.go
@@ -469,7 +469,8 @@ func (n *ovn) Validate(config map[string]string) error {
 	}
 
 	// Check any existing network forward target addresses are suitable for this network's subnet.
-	forwards, err := n.state.Cluster.GetNetworkForwards(n.ID())
+	memberSpecific := false // OVN doesn't support per-member forwards.
+	forwards, err := n.state.Cluster.GetNetworkForwards(n.ID(), memberSpecific)
 	if err != nil {
 		return fmt.Errorf("Failed loading network forwards: %w", err)
 	}
