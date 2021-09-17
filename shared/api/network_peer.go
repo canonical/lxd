da43129c5daaa2e577c6e2d5fc0718b795624462
Thomas Parrott <thomas.parrott@canonical.com>
2021-09-17 13:32:51 +0100

    shared/api/network/peer: Adds network peering API types
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ shared/api/network_peer.go
@@ -0,0 +1,76 @@
+package api
+
+// NetworkPeersPost represents the fields of a new LXD network peering
+//
+// swagger:model
+//
+// API extension: network_peer
+type NetworkPeersPost struct {
+	NetworkPeerPut `yaml:",inline"`
+
+	// Name of the peer
+	// Example: project1-network1
+	Name string `json:"name" yaml:"name"`
+
+	// Name of the target project
+	// Example: project1
+	TargetProject string `json:"target_project" yaml:"target_project"`
+
+	// Name of the target network
+	// Example: network1
+	TargetNetwork string `json:"target_network" yaml:"target_network"`
+}
+
+// NetworkPeerPut represents the modifiable fields of a LXD network peering
+//
+// swagger:model
+//
+// API extension: network_peer
+type NetworkPeerPut struct {
+	// Description of the peer
+	// Example: Peering with network1 in project1
+	Description string `json:"description" yaml:"description"`
+
+	// Peer configuration map (refer to doc/network-peers.md)
+	// Example: {"user.mykey": "foo"}
+	Config map[string]string `json:"config" yaml:"config"`
+}
+
+// NetworkPeer used for displaying a LXD network peering.
+//
+// swagger:model
+//
+// API extension: network_forward
+type NetworkPeer struct {
+	NetworkPeerPut `yaml:",inline"`
+
+	// Name of the peer
+	// Read only: true
+	// Example: project1-network1
+	Name string `json:"name" yaml:"name"`
+
+	// Name of the target project
+	// Read only: true
+	// Example: project1
+	TargetProject string `json:"target_project" yaml:"target_project"`
+
+	// Name of the target network
+	// Read only: true
+	// Example: network1
+	TargetNetwork string `json:"target_network" yaml:"target_network"`
+
+	// The state of the peering
+	// Read only: true
+	// Example: Pending
+	Status string `json:"status" yaml:"status"`
+}
+
+// Etag returns the values used for etag generation.
+func (p *NetworkPeer) Etag() []interface{} {
+	return []interface{}{p.Name, p.Description, p.Config}
+}
+
+// Writable converts a full NetworkPeer struct into a NetworkPeerPut struct (filters read-only fields).
+func (p *NetworkPeer) Writable() NetworkPeerPut {
+	return p.NetworkPeerPut
+}
