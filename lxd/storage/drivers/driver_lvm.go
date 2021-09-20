06301dfd7dd47a01de0b612b22ea596c8d56f80e
Thomas Parrott <thomas.parrott@canonical.com>
2021-09-20 16:47:19 +0100

    lxd/storage/drivers/driver/lvm: Fix Mount to be more reliable
    Ensure that volume group and thin pool exist (if needed).
    
    This allows time for the volume group to activate before considering the pool loaded.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_lvm.go
+++ lxd/storage/drivers/driver_lvm.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 
@@ -483,24 +484,70 @@ func (d *lvm) Update(changedConfig map[string]string) error {
 	return nil
 }
 
-// Mount mounts the storage pool (this does nothing for external LVM pools, but for loopback image
-// LVM pools this creates a loop device).
+// Mount mounts the storage pool (for loopback image pools this creates a loop device), and checks the volume group
+// and thin pool volume (if used) exists.
 func (d *lvm) Mount() (bool, error) {
 	if d.config["lvm.vg_name"] == "" {
 		return false, fmt.Errorf("Cannot mount pool as %q is not specified", "lvm.vg_name")
 	}
 
-	// Open the loop file if the LVM device doesn't exist yet and the source points to a file.
-	if !shared.IsDir(fmt.Sprintf("/dev/%s", d.config["lvm.vg_name"])) && filepath.IsAbs(d.config["source"]) && !shared.IsBlockdevPath(d.config["source"]) {
+	// Check if VG exists before we do anthing, this will indicate if its our mount or not.
+	vgExists, _, _ := d.volumeGroupExists(d.config["lvm.vg_name"])
+	ourMount := !vgExists
+
+	waitDuration := time.Second * time.Duration(5)
+
+	// Open the loop file if the source points to a non-block device file.
+	// This ensures that auto clear isn't enabled on the loop file.
+	if filepath.IsAbs(d.config["source"]) && !shared.IsBlockdevPath(d.config["source"]) {
 		loopFile, err := d.openLoopFile(d.config["source"])
 		if err != nil {
 			return false, err
 		}
-		defer loopFile.Close()
-		return true, nil
+
+		err = loopFile.Close()
+		if err != nil {
+			return false, err
+		}
+
+		// Wait for volume group to be detected if wasn't detected before.
+		if !vgExists {
+			waitUntil := time.Now().Add(waitDuration)
+			for {
+				vgExists, _, _ = d.volumeGroupExists(d.config["lvm.vg_name"])
+				if vgExists {
+					break
+				}
+
+				if time.Now().After(waitUntil) {
+					return false, fmt.Errorf("Volume group %q not found", d.config["lvm.vg_name"])
+				}
+
+				time.Sleep(1 * time.Second)
+			}
+		}
+	} else if !vgExists {
+		return false, fmt.Errorf("Volume group %s not found", d.config["lvm.vg_name"])
 	}
 
-	return false, nil
+	// Ensure thinpool exists if needed for storage pool.
+	if d.usesThinpool() {
+		waitUntil := time.Now().Add(waitDuration)
+		for {
+			thinpoolExists, _ := d.thinpoolExists(d.config["lvm.vg_name"], d.thinpoolName())
+			if thinpoolExists {
+				break
+			}
+
+			if time.Now().After(waitUntil) {
+				return false, fmt.Errorf("Thin pool not found %q in volume group %q", d.thinpoolName(), d.config["lvm.vg_name"])
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	return ourMount, nil
 }
 
 // Unmount unmounts the storage pool (this does nothing for external LVM pools, but for loopback
