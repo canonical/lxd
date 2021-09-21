b5761b0b2dca2d2f43ffc523470a7e77a84fceb1
Thomas Parrott <thomas.parrott@canonical.com>
2021-09-21 16:44:07 +0100

    lxd/cluster/gateway: Updates HandlerFuncs to accept a HeartbeatMemberTask
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/cluster/gateway.go
+++ lxd/cluster/gateway.go
@@ -152,7 +152,7 @@ func setDqliteVersionHeader(request *http.Request) {
 // These handlers might return 404, either because this LXD node is a
 // non-clustered node not available over the network or because it is not a
 // database node part of the dqlite cluster.
-func (g *Gateway) HandlerFuncs(nodeRefreshTask func(*APIHeartbeat), trustedCerts func() map[db.CertificateType]map[string]x509.Certificate) map[string]http.HandlerFunc {
+func (g *Gateway) HandlerFuncs(heartbeatMemberHook HeartbeatMemberTask, trustedCerts func() map[db.CertificateType]map[string]x509.Certificate) map[string]http.HandlerFunc {
 	database := func(w http.ResponseWriter, r *http.Request) {
 		g.lock.RLock()
 		defer g.lock.RUnlock()
@@ -274,8 +274,8 @@ func (g *Gateway) HandlerFuncs(nodeRefreshTask func(*APIHeartbeat), trustedCerts
 			}
 
 			// If node refresh task is specified, run it async.
-			if nodeRefreshTask != nil {
-				go nodeRefreshTask(&heartbeatData)
+			if heartbeatMemberHook != nil {
+				go heartbeatMemberHook(&heartbeatData, false)
 			}
 
 			return
