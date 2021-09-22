0344f74381d1114879bbd00e17d7d0908497761f
Max Asnaashari <max.asnaashari@canonical.com>
2021-09-22 22:49:29 +0000

    lxd/db/config: add Config struct and generator comments
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/config.go
+++ lxd/db/config.go
@@ -5,6 +5,28 @@ package db
 
 import "github.com/lxc/lxd/lxd/db/query"
 
+// Code generation directives.
+//
+//go:generate -command mapper lxd-generate db mapper -t config.mapper.go
+//go:generate mapper reset
+//
+//go:generate mapper stmt -p db -e config objects
+//go:generate mapper stmt -p db -e config create struct=Config
+//go:generate mapper stmt -p db -e config delete
+//
+//go:generate mapper method -p db -e config GetMany
+//go:generate mapper method -p db -e config Create struct=Config
+//go:generate mapper method -p db -e config Update struct=Config
+//go:generate mapper method -p db -e config DeleteMany
+
+// Config is a reference struct representing one configuration entry of another entity.
+type Config struct {
+	ID          int `db:"primary=yes"`
+	ReferenceID int
+	Key         string
+	Value       string
+}
+
 // Config fetches all LXD node-level config keys.
 func (n *NodeTx) Config() (map[string]string, error) {
 	return query.SelectConfig(n.tx, "config", "")
