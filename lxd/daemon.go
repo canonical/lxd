aa0f26572c20abb3be0d5400b8b4e76e3b6385f9
Thomas Parrott <thomas.parrott@canonical.com>
2021-09-23 16:52:07 +0100

    lxd/daemon: Clearer logging of API requests in createCmd
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -480,10 +480,10 @@ func (d *Daemon) createCmd(restAPI *mux.Router, version string, c APIEndpoint) {
 			}
 		}
 
+		logCtx := log.Ctx{"method": r.Method, "url": r.URL.RequestURI(), "ip": r.RemoteAddr, "username": username, "protocol": protocol}
 		untrustedOk := (r.Method == "GET" && c.Get.AllowUntrusted) || (r.Method == "POST" && c.Post.AllowUntrusted)
 		if trusted {
-			logCtx := log.Ctx{"method": r.Method, "url": r.URL.RequestURI(), "ip": r.RemoteAddr, "username": username, "protocol": protocol}
-			logger.Debug("Handling", logCtx)
+			logger.Debug("Handling API request", logCtx)
 
 			// Get user access data.
 			userAccess, err := func() (*rbac.UserAccess, error) {
