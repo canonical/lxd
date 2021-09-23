310989918c32e4b4c8afc60ffb96e5532b508ddd
Thomas Parrott <thomas.parrott@canonical.com>
2021-09-23 09:59:31 +0100

    lxd/storage/filesystem: Removes duplicated constants from unix package
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/filesystem/fs_32bit.go
+++
@@ -1,9 +0,0 @@
-//go:build 386 || arm || ppc || s390 || mips || mipsle
-// +build 386 arm ppc s390 mips mipsle
-
-package filesystem
-
-const (
-	// FilesystemSuperMagicBtrfs is the 32bit magic for Btrfs (as signed constant)
-	FilesystemSuperMagicBtrfs = -1859950530
-)
