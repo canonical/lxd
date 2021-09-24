5016ed16e35ecb7af28cef96e58ecfdb79ccada2
Thomas Parrott <thomas.parrott@canonical.com>
2021-09-24 17:58:09 +0100

    lxd/network/acl/driver/common: ValidName usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/acl/driver_common.go
+++ lxd/network/acl/driver_common.go
@@ -189,23 +189,7 @@ func (d *common) Etag() []interface{} {
 
 // validateName checks name is valid.
 func (d *common) validateName(name string) error {
-	if name == "" {
-		return fmt.Errorf("Name is required")
-	}
-
-	// Don't allow ACL names to start with special port selector characters to allow LXD to define special port
-	// selectors without risking conflict with user defined ACL names.
-	if shared.StringHasPrefix(name, "@", "%", "#") {
-		return fmt.Errorf("Name cannot start with reserved character %q", name[0])
-	}
-
-	// Ensures we can differentiate an ACL name from an IP in rules that reference this ACL.
-	err := shared.ValidHostname(name)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return ValidName(name)
 }
 
 // validateConfig checks the config and rules are valid.
