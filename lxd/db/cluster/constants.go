efcb3dbef23bf8c0abb77d48480fb35952af4b58
Max Asnaashari <max.asnaashari@canonical.com>
2021-09-30 00:23:16 +0000

    lxd/db/cluster/constants: format used-by uris to drop default project
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/cluster/constants.go
+++ lxd/db/cluster/constants.go
@@ -75,6 +75,14 @@ var EntityFormatURIs = map[int]func(a ...interface{}) string{
 
 		return uri
 	},
+	TypeInstance: func(a ...interface{}) string {
+		uri := fmt.Sprintf(EntityURIs[TypeInstance], a[1], a[0])
+		if a[0] == "default" {
+			return strings.Split(uri, fmt.Sprintf("?project=%s", a[0]))[0]
+		}
+
+		return uri
+	},
 	TypeProfile: func(a ...interface{}) string {
 		uri := fmt.Sprintf(EntityURIs[TypeProfile], a[1], a[0])
 		if a[0] == "default" {
@@ -85,6 +93,22 @@ var EntityFormatURIs = map[int]func(a ...interface{}) string{
 	},
 	TypeProject: func(a ...interface{}) string {
 		uri := fmt.Sprintf(EntityURIs[TypeProject], a[0])
+		return uri
+	},
+	TypeNetwork: func(a ...interface{}) string {
+		uri := fmt.Sprintf(EntityURIs[TypeNetwork], a[1], a[0])
+		if a[0] == "default" {
+			return strings.Split(uri, fmt.Sprintf("?project=%s", a[0]))[0]
+		}
+
+		return uri
+	},
+	TypeImage: func(a ...interface{}) string {
+		uri := fmt.Sprintf(EntityURIs[TypeImage], a[1], a[0])
+		if a[0] == "default" {
+			return strings.Split(uri, fmt.Sprintf("?project=%s", a[0]))[0]
+		}
+
 		return uri
 	},
 }
