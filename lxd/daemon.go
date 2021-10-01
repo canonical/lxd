da7ca771d9ac399c82429622f75b472a459b42df
Christian Brauner <christian.brauner@ubuntu.com>
2021-10-01 10:27:58 +0200

    lxd/checkfeature: check whether the kernel supports core scheduling
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -788,7 +788,7 @@ func (d *Daemon) init() error {
 		logger.Info(" - pidfds: no")
 	}
 
-	if d.os.LXCFeatures["core_scheduling"] {
+	if canUseCoreScheduling() && d.os.LXCFeatures["core_scheduling"] {
 		d.os.CoreScheduling = true
 		logger.Info(" - core scheduling: yes")
 	} else {
