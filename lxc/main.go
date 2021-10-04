eda46055e907f88fb24f752e63e6056ff8528938
Simon Deziel <simon.deziel@canonical.com>
2021-10-04 12:15:13 -0400

    lxc: suggest 20.04 as the first container to launch instead of 18.04
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxc/main.go
+++ lxc/main.go
@@ -339,7 +339,7 @@ func (c *cmdGlobal) PreRun(cmd *cobra.Command, args []string) error {
 		}
 
 		if !shared.StringInSlice(cmd.Name(), []string{"init", "launch"}) {
-			fmt.Fprintf(os.Stderr, i18n.G("To start your first instance, try: lxc launch ubuntu:18.04")+"\n")
+			fmt.Fprintf(os.Stderr, i18n.G("To start your first instance, try: lxc launch ubuntu:20.04")+"\n")
 			flush = true
 		}
 
