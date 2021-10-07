b1e7314873c594071b74456cfef7776e84c9b23f
Thomas Hipp <thomas.hipp@canonical.com>
2021-10-07 15:00:18 +0200

    lxd: Add fsmonitor package
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

---
+++ lxd/fsmonitor/drivers/events.go
@@ -0,0 +1,18 @@
+package drivers
+
+// Event is a numeric code identifying the event.
+type Event int
+
+const (
+	// Add represents the add event
+	Add Event = iota
+	// Remove represents the remove event
+	Remove
+)
+
+func (e Event) String() string {
+	return map[Event]string{
+		Add:    "add",
+		Remove: "remove",
+	}[e]
+}
---
+++ lxd/fsmonitor/drivers/interface.go
@@ -0,0 +1,22 @@
+package drivers
+
+import (
+	"context"
+
+	"github.com/lxc/lxd/shared/logger"
+)
+
+// driver is the extended internal interface.
+type driver interface {
+	Driver
+
+	init(logger logger.Logger, path string)
+	load(ctx context.Context) error
+}
+
+// Driver represents a low-level fs notification driver.
+type Driver interface {
+	PrefixPath() string
+	Watch(path string, identifier string, f func(path string, event string) bool) error
+	Unwatch(path string, identifier string) error
+}
