7e90030f15d4f3a099d24711be4da723c3fc2270
Thomas Hipp <thomas.hipp@canonical.com>
2021-10-07 15:00:18 +0200

    lxd: Add fsmonitor package
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

---
+++ lxd/fsmonitor/drivers/load.go
@@ -0,0 +1,31 @@
+package drivers
+
+import (
+	"context"
+
+	"github.com/lxc/lxd/shared/logger"
+)
+
+var drivers = map[string]func() driver{
+	"fsnotify": func() driver { return &fsnotify{} },
+	"fanotify": func() driver { return &fanotify{} },
+}
+
+// Load returns a Driver for an existing low-level FS monitor.
+func Load(ctx context.Context, logger logger.Logger, driverName string, path string) (Driver, error) {
+	df, ok := drivers[driverName]
+	if !ok {
+		return nil, ErrUnknownDriver
+	}
+
+	d := df()
+
+	d.init(logger, path)
+
+	err := d.load(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return d, nil
+}
