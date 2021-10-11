5ebfbf23c1bae855c24ef024ff6bd4ad09b47de9
Thomas Parrott <thomas.parrott@canonical.com>
2021-10-11 14:37:29 +0100

    lxd/network/network/utils: Updates usedByInstanceDevices to use updated isInUseByDevice
    Back port of 3cdca5022f2a42b7ee07f52cb707e6ed7157b501
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/network_utils.go
+++ lxd/network/network_utils.go
@@ -66,6 +66,24 @@ func RandomDevName(prefix string) string {
 	return iface
 }
 
+// usedByInstanceDevices looks for instance NIC devices using the network and runs the supplied usageFunc for each.
+func usedByInstanceDevices(s *state.State, networkName string, usageFunc func(inst db.Instance, nicName string, nicConfig map[string]string) error) error {
+	return s.Cluster.InstanceList(nil, func(inst db.Instance, p db.Project, profiles []api.Profile) error {
+		// Look for NIC devices using this network.
+		devices := db.ExpandInstanceDevices(deviceConfig.NewDevices(inst.Devices), profiles)
+		for devName, devConfig := range devices {
+			if isInUseByDevice(networkName, devConfig) {
+				err := usageFunc(inst, devName, devConfig)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+}
+
 // UsedBy returns list of API resources using network. Accepts firstOnly argument to indicate that only the first
 // resource using network should be returned. This can help to quickly check if the network is in use.
 func UsedBy(s *state.State, networkName string, firstOnly bool) ([]string, error) {
