c63f1a58773f867468322288159ade4ccbbf888d
Thomas Hipp <thomas.hipp@canonical.com>
2021-10-13 16:22:10 +0200

    lxd/state: Add DevMonitor to state
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/state/state.go
+++ lxd/state/state.go
@@ -12,6 +12,7 @@ import (
 	"github.com/lxc/lxd/lxd/endpoints"
 	"github.com/lxc/lxd/lxd/events"
 	"github.com/lxc/lxd/lxd/firewall"
+	"github.com/lxc/lxd/lxd/fsmonitor"
 	"github.com/lxc/lxd/lxd/instance/instancetype"
 	"github.com/lxc/lxd/lxd/maas"
 	"github.com/lxc/lxd/lxd/sys"
@@ -52,4 +53,7 @@ type State struct {
 
 	// Available instance types based on operational drivers.
 	InstanceTypes map[instancetype.Type]struct{}
+
+	// Filesystem monitor
+	DevMonitor fsmonitor.FSMonitor
 }
