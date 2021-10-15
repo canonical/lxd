10c2995836df90222c1cdbf31c06d95d0b9988d6
Christian Brauner <christian.brauner@ubuntu.com>
2021-10-15 17:28:43 +0200

    process_utils: move wait_for_pid_status_nointr() to common code
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/main_forkexec.go
+++ lxd/main_forkexec.go
@@ -50,25 +50,6 @@ static bool write_nointr(int fd, const void *buf, size_t count)
 
 define_cleanup_function(struct lxc_container *, lxc_container_put);
 
-static int wait_for_pid_status_nointr(pid_t pid)
-{
-	int status, ret;
-
-again:
-	ret = waitpid(pid, &status, 0);
-	if (ret == -1) {
-		if (errno == EINTR)
-			goto again;
-
-		return -1;
-	}
-
-	if (ret != pid)
-		goto again;
-
-	return status;
-}
-
 static int append_null_to_list(void ***list)
 {
 	int newentry = 0;
