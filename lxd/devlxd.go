9d86d262a58bab6540055e34be129f437edf3614
Simon Deziel <simon.deziel@canonical.com>
2021-11-10 20:22:06 -0500

    lxd/devlxd: stricter regexp
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/devlxd.go
+++ lxd/devlxd.go
@@ -347,7 +347,7 @@ func findContainerForPid(pid int32, s *state.State) (instance.Container, error) 
 			return nil, err
 		}
 
-		re := regexp.MustCompile("PPid:\\s*([0-9]*)")
+		re := regexp.MustCompile(`^PPid:\s+([0-9]+)`)
 		for _, line := range strings.Split(string(status), "\n") {
 			m := re.FindStringSubmatch(line)
 			if m != nil && len(m) > 1 {
