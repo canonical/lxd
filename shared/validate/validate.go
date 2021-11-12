1f3c52a511b40f4e3fd3b44d8afc9c85e0fb5a5b
Thomas Parrott <thomas.parrott@canonical.com>
2021-11-12 10:23:55 +0000

    shared/validate/validate: Fixes comment on IsListOf
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/validate/validate.go
+++ shared/validate/validate.go
@@ -139,7 +139,7 @@ func IsAny(value string) error {
 	return nil
 }
 
-// IsList returns a validator for a list of values.
+// IsListOf returns a validator for a list of values.
 func IsListOf(validator func(value string) error) func(value string) error {
 	return func(value string) error {
 		for _, v := range strings.Split(value, ",") {
