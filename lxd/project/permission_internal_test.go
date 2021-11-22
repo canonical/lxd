d821cd89e7c5cb33f8a663b2cd11e32d7c7c919d
Thomas Parrott <thomas.parrott@canonical.com>
2021-11-22 11:09:16 +0000

    lxd/project/permission/internal/test: Adds tests for parseHostIDMapRange
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/project/permission_internal_test.go
@@ -0,0 +1,74 @@
+package project
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lxc/lxd/shared/idmap"
+)
+
+func TestParseHostIDMapRange(t *testing.T) {
+	for _, mode := range []string{"uid", "gid", "both"} {
+		var isUID, isGID bool
+		switch mode {
+		case "uid":
+			isUID = true
+		case "gid":
+			isGID = true
+		case "both":
+			isUID = true
+			isGID = true
+		}
+
+		idmaps, err := parseHostIDMapRange(isUID, isGID, "foo")
+		assert.NotErrorIs(t, err, nil)
+		assert.Nil(t, idmaps)
+
+		idmaps, err = parseHostIDMapRange(isUID, isGID, "1000")
+		expected := []idmap.IdmapEntry{
+			{
+				Isuid:    isUID,
+				Isgid:    isGID,
+				Hostid:   1000,
+				Maprange: 1,
+				Nsid:     -1,
+			},
+		}
+		assert.ErrorIs(t, err, nil)
+		assert.Equal(t, idmaps, expected)
+
+		idmaps, err = parseHostIDMapRange(isUID, isGID, "1000-1001")
+		expected = []idmap.IdmapEntry{
+			{
+				Isuid:    isUID,
+				Isgid:    isGID,
+				Hostid:   1000,
+				Maprange: 2,
+				Nsid:     -1,
+			},
+		}
+		assert.ErrorIs(t, err, nil)
+		assert.Equal(t, idmaps, expected)
+
+		idmaps, err = parseHostIDMapRange(isUID, isGID, "1000-1001,1002")
+		expected = []idmap.IdmapEntry{
+			{
+				Isuid:    isUID,
+				Isgid:    isGID,
+				Hostid:   1000,
+				Maprange: 2,
+				Nsid:     -1,
+			},
+			{
+				Isuid:    isUID,
+				Isgid:    isGID,
+				Hostid:   1002,
+				Maprange: 1,
+				Nsid:     -1,
+			},
+		}
+		assert.ErrorIs(t, err, nil)
+		assert.Equal(t, idmaps, expected)
+	}
+}
