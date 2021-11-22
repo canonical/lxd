293c065976f19067ee4d784f9822389f2bc1e1df
Thomas Parrott <thomas.parrott@canonical.com>
2021-11-22 10:46:21 +0000

    lxd/device/device/utils/disk/test: Adds tests for diskAddRootUserNSEntry
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/device/device_utils_disk_test.go
@@ -0,0 +1,89 @@
+package device
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lxc/lxd/shared/idmap"
+)
+
+func TestDiskAddRootUserNSEntry(t *testing.T) {
+	// Check adds a combined uid/gid root entry to an empty list.
+	var idmaps []idmap.IdmapEntry
+	idmaps = diskAddRootUserNSEntry(idmaps, 65534)
+	expected := []idmap.IdmapEntry{
+		{
+			Isuid:    true,
+			Isgid:    true,
+			Hostid:   65534,
+			Maprange: 1,
+			Nsid:     0,
+		},
+	}
+	assert.Equal(t, idmaps, expected)
+
+	// Check doesn't add another one if an existing combined entry exists.
+	idmaps = diskAddRootUserNSEntry(idmaps, 65534)
+	assert.Equal(t, idmaps, expected)
+
+	// Check adds a root gid entry if root uid entry already exists.
+	idmaps = []idmap.IdmapEntry{
+		{
+			Isuid:    true,
+			Isgid:    false,
+			Hostid:   65534,
+			Maprange: 1,
+			Nsid:     0,
+		},
+	}
+
+	idmaps = diskAddRootUserNSEntry(idmaps, 65534)
+	expected = []idmap.IdmapEntry{
+		{
+			Isuid:    true,
+			Isgid:    false,
+			Hostid:   65534,
+			Maprange: 1,
+			Nsid:     0,
+		},
+		{
+			Isuid:    false,
+			Isgid:    true,
+			Hostid:   65534,
+			Maprange: 1,
+			Nsid:     0,
+		},
+	}
+	assert.Equal(t, idmaps, expected)
+
+	// Check adds a root uid entry if root gid entry already exists.
+	idmaps = []idmap.IdmapEntry{
+		{
+			Isuid:    false,
+			Isgid:    true,
+			Hostid:   65534,
+			Maprange: 1,
+			Nsid:     0,
+		},
+	}
+
+	idmaps = diskAddRootUserNSEntry(idmaps, 65534)
+	expected = []idmap.IdmapEntry{
+		{
+			Isuid:    false,
+			Isgid:    true,
+			Hostid:   65534,
+			Maprange: 1,
+			Nsid:     0,
+		},
+		{
+			Isuid:    true,
+			Isgid:    false,
+			Hostid:   65534,
+			Maprange: 1,
+			Nsid:     0,
+		},
+	}
+	assert.Equal(t, idmaps, expected)
+}
