6ce5adde6bb4c778275f04afcd09e2cea8aa71e6
Thomas Parrott <thomas.parrott@canonical.com>
2021-11-26 15:54:06 +0000

    test: Add non-concurrent exec test
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -238,6 +238,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_image_auto_update "image auto-update"
     run_test test_image_prefer_cached "image prefer cached"
     run_test test_image_import_dir "import image from directory"
+    run_test test_exec "exec"
     run_test test_concurrent_exec "concurrent exec"
     run_test test_concurrent "concurrent startup"
     run_test test_snapshots "container snapshots"
