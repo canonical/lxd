e7ba1acabfade680920f8b79d3865a4ed8f262f8
Thomas Parrott <thomas.parrott@canonical.com>
2021-11-26 16:56:40 +0000

    Revert "shared/termios: manually copy termios settings between Go and C"
    This reverts commit a918841cf49cab2b5b9027201d9d22b9a253098a.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/termios/termios_unix.go
+++ shared/termios/termios_unix.go
@@ -4,6 +4,8 @@
 package termios
 
 import (
+	"unsafe"
+
 	"golang.org/x/sys/unix"
 
 	"github.com/lxc/lxd/shared"
@@ -46,20 +48,6 @@ func GetSize(fd int) (int, int, error) {
 	return int(winsize.Col), int(winsize.Row), nil
 }
 
-func copyTermios(state *State, cTermios *C.struct_termios) {
-	cTermios.c_iflag = C.tcflag_t(state.Termios.Iflag)
-	cTermios.c_oflag = C.tcflag_t(state.Termios.Oflag)
-	cTermios.c_cflag = C.tcflag_t(state.Termios.Cflag)
-	cTermios.c_lflag = C.tcflag_t(state.Termios.Lflag)
-	cTermios.c_line = C.cc_t(state.Termios.Line)
-	cTermios.c_ispeed = C.speed_t(state.Termios.Ispeed)
-	cTermios.c_ospeed = C.speed_t(state.Termios.Ospeed)
-
-	for i := 0; i < len(state.Termios.Cc) && i < C.NCCS; i++ {
-		cTermios.c_cc[i] = C.uchar(i)
-	}
-}
-
 // MakeRaw put the terminal connected to the given file descriptor into raw mode and returns the previous state of the terminal so that it can be restored.
 func MakeRaw(fd int) (*State, error) {
 	var err error
@@ -75,9 +63,7 @@ func MakeRaw(fd int) (*State, error) {
 		return nil, err
 	}
 
-	var cTermios C.struct_termios
-	copyTermios(newState, &cTermios)
-	C.cfmakeraw(&cTermios)
+	C.cfmakeraw((*C.struct_termios)(unsafe.Pointer(&newState.Termios)))
 
 	err = Restore(fd, newState)
 	if err != nil {
@@ -89,9 +75,7 @@ func MakeRaw(fd int) (*State, error) {
 
 // Restore restores the terminal connected to the given file descriptor to a previous state.
 func Restore(fd int, state *State) error {
-	var cTermios C.struct_termios
-	copyTermios(state, &cTermios)
-	ret, err := C.tcsetattr(C.int(fd), C.TCSANOW, &cTermios)
+	ret, err := C.tcsetattr(C.int(fd), C.TCSANOW, (*C.struct_termios)(unsafe.Pointer(&state.Termios)))
 	if ret != 0 {
 		return err.(unix.Errno)
 	}
