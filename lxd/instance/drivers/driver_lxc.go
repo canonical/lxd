9dfb11e650177c935721ec26d8f7b5b2c779eff5
Thomas Parrott <thomas.parrott@canonical.com>
2021-11-26 12:07:29 +0000

    lxd/instance/drivers/driver/lxc: Close forkexec log file
    We only need this file for the initial launch of forkexec, the actual output from the requested command is via extra files.
    
    Tested we still get launch failures in the forkexec.log file by modifying it to run as non-root to trigger the non-root check.
    
    ```
    cmd.SysProcAttr = &syscall.SysProcAttr{
    	Credential: &syscall.Credential{Uid: 1000},
    }
    ```
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_lxc.go
+++ lxd/instance/drivers/driver_lxc.go
@@ -5726,6 +5726,7 @@ func (d *lxc) Exec(req api.InstanceExecPost, stdin *os.File, stdout *os.File, st
 	if err != nil {
 		return nil, err
 	}
+	defer logFile.Close()
 
 	// Prepare the subcommand
 	cname := project.Instance(d.Project(), d.Name())
