c66bcbbb3fd9396b3c5907867674c0b508ef2951
Thomas Parrott <thomas.parrott@canonical.com>
2021-11-26 15:54:06 +0000

    test: Add non-concurrent exec test
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/suites/exec.sh
+++ test/suites/exec.sh
@@ -1,3 +1,30 @@
+test_exec() {
+  ensure_import_testimage
+
+  name=x1
+  lxc launch testimage x1
+  lxc list ${name} | grep RUNNING
+
+  exec_container_noninteractive() {
+    echo "abc${1}" | lxc exec "${name}" --force-noninteractive -- cat | grep abc
+  }
+
+  exec_container_interactive() {
+    echo "abc${1}" | lxc exec "${name}" -- cat | grep abc
+  }
+
+  for i in $(seq 1 25); do
+    exec_container_interactive "${i}" > "${LXD_DIR}/exec-${i}.out" 2>&1
+  done
+
+  for i in $(seq 1 25); do
+    exec_container_noninteractive "${i}" > "${LXD_DIR}/exec-${i}.out" 2>&1
+  done
+
+  lxc stop "${name}" --force
+  lxc delete "${name}"
+}
+
 test_concurrent_exec() {
   if [ -z "${LXD_CONCURRENT:-}" ]; then
     echo "==> SKIP: LXD_CONCURRENT isn't set"
