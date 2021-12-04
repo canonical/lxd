d8049133d3ab3e3373f8be133245d8b76afb4b64
Stéphane Graber <stgraber@ubuntu.com>
2021-12-03 23:50:19 -0500

    lxd-user: Initial implementation
    This introduces a new command `lxd-user` which acts as a proxy to the
    main LXD API. It listens on its own unix socket and handles:
    
     - Initial configuration of the LXD daemon, creating a default storage
       pool, default network and default profile on first connection.
     - On initial connection from a user, it will generate a new certificate
       keypair, it then generates a dedicated project for the user and ties
       that certificate to the project.
     - The user interacts with that LXD normally through the CLI or API but
       will only see their own project and instances.
    
    This effectively allows running both the `lxd` and `lxd-user` daemon on
    a system. The main `lxd` daemon's socket is restricted to those who
    administer the entire system (members of the `lxd` group usually)
    whereas the `lxd-user` daemon's socket is accessible to all local users
    (`users` group or similar).
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ lxd-user/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/lxc/lxd/shared/version"
+)
+
+type cmdGlobal struct {
+	flagHelp    bool
+	flagVersion bool
+}
+
+func main() {
+	// daemon command (main)
+	daemonCmd := cmdDaemon{}
+	app := daemonCmd.Command()
+	app.Use = "lxd-user"
+	app.Short = "LXD user project daemon"
+	app.Long = `Description:
+  LXD user project daemon
+
+  This daemon is used to allow users that aren't considered to be LXD
+  administrators access to a personal LXD project with suitable
+  restrictions.
+`
+	app.SilenceUsage = true
+	app.CompletionOptions = cobra.CompletionOptions{DisableDefaultCmd: true}
+
+	// Global flags
+	globalCmd := cmdGlobal{}
+	app.PersistentFlags().BoolVar(&globalCmd.flagVersion, "version", false, "Print version number")
+	app.PersistentFlags().BoolVarP(&globalCmd.flagHelp, "help", "h", false, "Print help")
+
+	// Version handling
+	app.SetVersionTemplate("{{.Version}}\n")
+	app.Version = version.Version
+
+	// Run the main command and handle errors
+	err := app.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
---
+++ lxd-user/proxy.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/lxc/lxd/lxd/ucred"
+	"github.com/lxc/lxd/shared"
+)
+
+func tlsConfig(uid uint32) (*tls.Config, error) {
+	// Load the client certificate.
+	content, err := ioutil.ReadFile(filepath.Join("users", fmt.Sprintf("%d", uid), "client.crt"))
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open client certificate: %w", err)
+	}
+	tlsClientCert := string(content)
+
+	// Load the client key.
+	content, err = ioutil.ReadFile(filepath.Join("users", fmt.Sprintf("%d", uid), "client.key"))
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open client key: %w", err)
+	}
+	tlsClientKey := string(content)
+
+	// Load the server certificate.
+	content, err = ioutil.ReadFile(shared.VarPath("server.crt"))
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open server certificate: %w", err)
+	}
+	tlsServerCert := string(content)
+
+	return shared.GetTLSConfigMem(tlsClientCert, tlsClientKey, "", tlsServerCert, false)
+}
+
+func proxyConnection(conn *net.UnixConn) {
+	// Setup connection counter.
+	connectionsLock.Lock()
+	connections++
+	connectionsLock.Unlock()
+
+	defer func() {
+		connectionsLock.Lock()
+		connections--
+		connectionsLock.Unlock()
+	}()
+
+	// Close on exit.
+	defer conn.Close()
+
+	// Get credentials.
+	creds, err := ucred.GetCred(conn)
+	if err != nil {
+		log.Errorf("Unable to get user credentials: %s", err)
+		return
+	}
+
+	// Setup logging context.
+	logger := log.WithFields(log.Fields{
+		"uid": creds.Uid,
+		"gid": creds.Gid,
+		"pid": creds.Pid,
+	})
+
+	logger.Info("Connected")
+	defer logger.Info("Disconnected")
+
+	// Check if the user was setup.
+	if !shared.PathExists(filepath.Join("users", fmt.Sprintf("%d", creds.Uid))) {
+		log.Infof("Setting up LXD for uid %d", creds.Uid)
+		err := lxdSetupUser(creds.Uid)
+		if err != nil {
+			log.Errorf("Failed to setup new user: %v", err)
+			return
+		}
+	}
+
+	// Connect to LXD.
+	unixAddr, err := net.ResolveUnixAddr("unix", shared.VarPath("unix.socket"))
+	if err != nil {
+		log.Errorf("Unable to resolve the target server: %v", err)
+		return
+	}
+
+	client, err := net.DialUnix("unix", nil, unixAddr)
+	if err != nil {
+		log.Errorf("Unable to connect to target server: %v", err)
+		return
+	}
+	defer client.Close()
+
+	// Get the TLS configuration
+	tlsConfig, err := tlsConfig(creds.Uid)
+	if err != nil {
+		log.Errorf("Failed to load TLS connection settings: %v", err)
+		return
+	}
+
+	// Setup TLS.
+	_, err = client.Write([]byte("STARTTLS\n"))
+	if err != nil {
+		log.Errorf("Failed to setup TLS connection to target server: %v", err)
+		return
+	}
+
+	tlsClient := tls.Client(client, tlsConfig)
+
+	// Establish the TLS handshake.
+	err = tlsClient.Handshake()
+	if err != nil {
+		conn.Close()
+		log.Errorf("Failed TLS handshake with target server: %v", err)
+		return
+	}
+
+	// Start proxying.
+	go io.Copy(conn, tlsClient)
+	io.Copy(tlsClient, conn)
+}
