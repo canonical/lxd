ee235e2253c071884bad8125e2ded7ac21f0912d
Mark Laing <mark.laing@canonical.com>
2021-12-06 16:25:44 +0000

    lxd/firewall/drivers: Adds util to calculate DNAT rule optimisations.
    Both nftables and xtables are able to optimise DNAT rules in the same
    way. This util function implements the logic for when these rules can be
    applied. Tests have been added to validate the logic.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/firewall/drivers/drivers_util.go
+++ lxd/firewall/drivers/drivers_util.go
@@ -35,3 +35,73 @@ func portRangeStr(portRange [2]uint64, delimiter string) string {
 	}
 	return fmt.Sprintf("%d%s%d", portRange[0], delimiter, portRange[0]+portRange[1]-1)
 }
+
+// getOptimisedDNATRanges returns a map of listen port ranges to target port ranges that can be
+// applied in any order.
+//
+// Both Xtables and Nftables are able to apply rules for multiple listen ports at a time when a
+// listen port range exactly matches the corresponding target port range (e.g. "80-85" to "80-85")
+// or when there is a single target port (e.g. "80-85" to "80"). This function checks when these
+// conditions are met and returns a map of listen and target port ranges to be applied by the loaded
+// driver.
+func getOptimisedDNATRanges(forward *AddressForward) map[[2]uint64][2]uint64 {
+	targetPortsLen := len(forward.TargetPorts)
+	listenPortsLen := len(forward.ListenPorts)
+
+	snatRules := make(map[[2]uint64][2]uint64, listenPortsLen)
+	listenPortRanges := portRangesFromSlice(forward.ListenPorts)
+
+	// If there is only one target port, DNAT rules can be optimised for all listen ranges.
+	if targetPortsLen == 1 {
+		targetPort := forward.TargetPorts[0]
+		for _, listenPortRange := range listenPortRanges {
+			snatRules[listenPortRange] = [2]uint64{targetPort, 1}
+		}
+
+		return snatRules
+	}
+
+	// For a given listen range, the corresponding target range may not simply be targetRange[i] (where "i"
+	// is the index of the listen range). For example, "100-101,300" to "100-102" would be valid config
+	// because the number of listen and target ports are equal, but there are two listen ranges and one
+	// target range. Instead, to check if there is a target range we create a map of port range starting
+	// values and check if the current target port is in the map.
+	targetPortRanges := portRangesFromSlice(forward.TargetPorts)
+	targetPortRangeMap := make(map[uint64]uint64, len(targetPortRanges))
+	for _, targetPortRange := range targetPortRanges {
+		targetPortRangeMap[targetPortRange[0]] = targetPortRange[1]
+	}
+
+	nProcessedPorts := 0
+	for _, listenPortRange := range listenPortRanges {
+		rangeEndIdx := nProcessedPorts + int(listenPortRange[1])
+
+		currentTargetPort := forward.TargetPorts[nProcessedPorts]
+		targetPortRangeSize, ok := targetPortRangeMap[currentTargetPort]
+
+		// Check that we have a target port range and that the listen and target port ranges start
+		// at the same value.
+		if ok && listenPortRange[0] == currentTargetPort {
+			targetPortRange := [2]uint64{currentTargetPort, targetPortRangeSize}
+
+			// Check if the listen and target ranges are the same size.
+			if listenPortRange[1] == targetPortRangeSize {
+				// Port ranges are identical. One to one mapping.
+				snatRules[listenPortRange] = targetPortRange
+				nProcessedPorts += int(listenPortRange[1])
+			} else {
+				// Port ranges are identical until the end of the target range.
+				// Rules can be optimised for a portion of ports in the listen range.
+				snatRules[[2]uint64{listenPortRange[0], targetPortRangeSize}] = targetPortRange
+				nProcessedPorts += int(targetPortRangeSize)
+			}
+		}
+
+		// Remaining ports in the listen range cannot be optimised.
+		for ; nProcessedPorts < rangeEndIdx; nProcessedPorts++ {
+			snatRules[[2]uint64{forward.ListenPorts[nProcessedPorts], 1}] = [2]uint64{forward.TargetPorts[nProcessedPorts], 1}
+		}
+	}
+
+	return snatRules
+}
