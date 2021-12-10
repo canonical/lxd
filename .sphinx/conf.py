7993fb21347397ca20ff7a92da69dbda80955adc
Ruth Fuchss <ruth.fuchss@canonical.com>
2021-12-10 17:39:19 +0100

    doc: use customized Furo theme
    Switch to a customized version of the Furo theme.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- .sphinx/conf.py
+++ .sphinx/conf.py
@@ -28,8 +28,7 @@ if os.path.exists("doc/substitutions.yaml"):
         myst_substitutions = yaml.load(fd.read())
 
 # Setup theme.
-html_theme_path = ["themes"]
-html_theme = "vanilla"
+html_theme = "furo"
 html_show_sphinx = False
 html_last_updated_fmt = ""
 html_favicon = "https://linuxcontainers.org/static/img/favicon.ico"
@@ -38,12 +37,59 @@ html_static_path = ['_static']
 html_css_files = ['custom.css']
 html_extra_path = ['_extra']
 
-
-# Uses global TOC for side nav instead of default local TOC.
-html_sidebars = {
-    "**": [
-        "globaltoc.html",
-    ]
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_css_variables": {
+        "font-stack": "Ubuntu, -apple-system, Segoe UI, Roboto, Oxygen, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+        "font-stack--monospace": "Ubuntu Mono, Consolas, Monaco, Courier, monospace",
+        "color-foreground-primary": "#111",
+        "color-foreground-secondary": "var(--color-foreground-primary)",
+        "color-foreground-muted": "#333",
+        "color-background-secondary": "#FFF",
+        "color-background-hover": "#f2f2f2",
+        "color-brand-primary": "#111",
+        "color-brand-content": "#06C",
+        "color-api-background": "#cdcdcd",
+        "color-inline-code-background": "rgba(0,0,0,.03)",
+        "color-sidebar-link-text": "#111",
+        "color-sidebar-item-background--current": "#ebebeb",
+        "color-sidebar-item-background--hover": "#f2f2f2",
+        "toc-font-size": "var(--font-size--small)",
+        "color-admonition-title-background--note": "var(--color-background-primary)",
+        "color-admonition-title-background--tip": "var(--color-background-primary)",
+        "color-admonition-title-background--important": "var(--color-background-primary)",
+        "color-admonition-title-background--caution": "var(--color-background-primary)",
+        "color-admonition-title--note": "#24598F",
+        "color-admonition-title--tip": "#24598F",
+        "color-admonition-title--important": "#C7162B",
+        "color-admonition-title--caution": "#F99B11",
+        "color-highlighted-background": "#EbEbEb",
+        "color-link-underline": "var(--color-background-primary)",
+        "color-link-underline--hover": "var(--color-background-primary)",
+    },
+    "dark_css_variables": {
+        "color-foreground-secondary": "var(--color-foreground-primary)",
+        "color-foreground-muted": "#CDCDCD",
+        "color-background-secondary": "var(--color-background-primary)",
+        "color-background-hover": "#666",
+        "color-brand-primary": "#fff",
+        "color-brand-content": "#06C",
+        "color-sidebar-link-text": "#f7f7f7",
+        "color-sidebar-item-background--current": "#666",
+        "color-sidebar-item-background--hover": "#333",
+        "color-admonition-background": "transparent",
+        "color-admonition-title-background--note": "var(--color-background-primary)",
+        "color-admonition-title-background--tip": "var(--color-background-primary)",
+        "color-admonition-title-background--important": "var(--color-background-primary)",
+        "color-admonition-title-background--caution": "var(--color-background-primary)",
+        "color-admonition-title--note": "#24598F",
+        "color-admonition-title--tip": "#24598F",
+        "color-admonition-title--important": "#C7162B",
+        "color-admonition-title--caution": "#F99B11",
+        "color-highlighted-background": "#666",
+        "color-link-underline": "var(--color-background-primary)",
+        "color-link-underline--hover": "var(--color-background-primary)",
+    },
 }
 
 source_suffix = ".md"
