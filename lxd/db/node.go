cd117d7074e1f636d3748c89761bb159cfb2cb46
Stéphane Graber <stgraber@ubuntu.com>
2021-12-13 15:43:06 -0500

    lxd/db/node: Fix handling of groups
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/db/node.go
+++ lxd/db/node.go
@@ -1122,28 +1122,27 @@ func (c *ClusterTx) GetNodeWithLeastInstances(archs []int, defaultArch int, grou
 	containers := -1
 	isDefaultArchChosen := false
 	for _, node := range nodes {
-		if node.Config["scheduler.instance"] == "manual" {
-			continue
-		}
-
-		// If scheduler.instance is "all", skip the node if group is set and the node doesn't belong to it.
-		if node.Config["scheduler.instance"] == "all" && group != "" && !shared.StringInSlice(group, node.Groups) {
-			continue
-		}
-
-		// If scheduler.instance is "group", skip the node if it doesn't belong to the group.
-		// The node will also be skipped if the group is empty.
-		if node.Config["scheduler.instance"] == "group" {
-			if !shared.StringInSlice(group, node.Groups) {
-				continue
-			}
-		}
-
+		// Skip evacuated members.
 		if node.State == ClusterMemberStateEvacuated || node.IsOffline(threshold) {
 			continue
 		}
 
-		// Get personalities too.
+		// Skip manually targeted members.
+		if node.Config["scheduler.instance"] == "manual" {
+			continue
+		}
+
+		// Skip group-only members if targeted group doesn't match.
+		if node.Config["scheduler.instance"] == "group" && !shared.StringInSlice(group, node.Groups) {
+			continue
+		}
+
+		// Skip if a group is requested and member isn't part of it.
+		if group != "" && !shared.StringInSlice(group, node.Groups) {
+			continue
+		}
+
+		// Get member personalities too.
 		personalities, err := osarch.ArchitecturePersonalities(node.Architecture)
 		if err != nil {
 			return "", err
@@ -1169,13 +1168,13 @@ func (c *ClusterTx) GetNodeWithLeastInstances(archs []int, defaultArch int, grou
 			continue
 		}
 
-		// Fetch the number of containers already created on this node.
+		// Fetch the number of instances already created on this node.
 		created, err := query.Count(c.tx, "instances", "node_id=?", node.ID)
 		if err != nil {
 			return "", errors.Wrap(err, "Failed to get instances count")
 		}
 
-		// Fetch the number of containers currently being created on this node.
+		// Fetch the number of instances currently being created on this node.
 		pending, err := query.Count(
 			c.tx, "operations", "node_id=? AND type=?", node.ID, OperationInstanceCreate)
 		if err != nil {
