381dbcff997d57367726745ddadf4922ea63fcf1
Thomas Parrott <thomas.parrott@canonical.com>
2021-12-16 15:21:46 +0000

    lxd/migrate: Change allConnected channel in migrationSink to be struct{}
    As it does not send any data, just closing it is sufficient to indicate that all channels are connected.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/migrate_storage_volumes.go
+++ lxd/migrate_storage_volumes.go
@@ -180,7 +180,7 @@ func newStorageMigrationSink(args *MigrationSinkArgs) (*migrationSink, error) {
 	}
 
 	if sink.push {
-		sink.allConnected = make(chan bool, 1)
+		sink.allConnected = make(chan struct{})
 	}
 
 	var ok bool
