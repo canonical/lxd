7737a14622827c266c3f8165386306c2c4536494
Stéphane Graber <stgraber@ubuntu.com>
2022-01-20 23:23:33 -0500

    client: Add network zone records
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- client/lxd_network_zones.go
+++ client/lxd_network_zones.go
@@ -103,3 +103,100 @@ func (r *ProtocolLXD) DeleteNetworkZone(name string) error {
 
 	return nil
 }
+
+// GetNetworkZoneRecordNames returns a list of network zone record names.
+func (r *ProtocolLXD) GetNetworkZoneRecordNames(zone string) ([]string, error) {
+	if !r.HasExtension("network_dns_records") {
+		return nil, fmt.Errorf(`The server is missing the required "network_dns_records" API extension`)
+	}
+
+	// Fetch the raw URL values.
+	urls := []string{}
+	baseURL := fmt.Sprintf("/network-zones/%s/records", url.PathEscape(zone))
+	_, err := r.queryStruct("GET", baseURL, nil, "", &urls)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse it.
+	return urlsToResourceNames(baseURL, urls...)
+}
+
+// GetNetworkZoneRecords returns a list of Network zone record structs.
+func (r *ProtocolLXD) GetNetworkZoneRecords(zone string) ([]api.NetworkZoneRecord, error) {
+	if !r.HasExtension("network_dns_records") {
+		return nil, fmt.Errorf(`The server is missing the required "network_dns_records" API extension`)
+	}
+
+	records := []api.NetworkZoneRecord{}
+
+	// Fetch the raw value.
+	_, err := r.queryStruct("GET", fmt.Sprintf("/network-zones/%s/records?recursion=1", url.PathEscape(zone)), nil, "", &records)
+	if err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
+
+// GetNetworkZoneRecord returns a Network zone record entry for the provided zone and name.
+func (r *ProtocolLXD) GetNetworkZoneRecord(zone string, name string) (*api.NetworkZoneRecord, string, error) {
+	if !r.HasExtension("network_dns_records") {
+		return nil, "", fmt.Errorf(`The server is missing the required "network_dns_records" API extension`)
+	}
+
+	record := api.NetworkZoneRecord{}
+
+	// Fetch the raw value.
+	etag, err := r.queryStruct("GET", fmt.Sprintf("/network-zones/%s/records/%s", url.PathEscape(zone), url.PathEscape(name)), nil, "", &record)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return &record, etag, nil
+}
+
+// CreateNetworkZoneRecord defines a new Network zone record using the provided struct.
+func (r *ProtocolLXD) CreateNetworkZoneRecord(zone string, record api.NetworkZoneRecordsPost) error {
+	if !r.HasExtension("network_dns_records") {
+		return fmt.Errorf(`The server is missing the required "network_dns_records" API extension`)
+	}
+
+	// Send the request.
+	_, _, err := r.query("POST", fmt.Sprintf("/network-zones/%s/records", url.PathEscape(zone)), record, "")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateNetworkZoneRecord updates the network zone record to match the provided struct.
+func (r *ProtocolLXD) UpdateNetworkZoneRecord(zone string, name string, record api.NetworkZoneRecordPut, ETag string) error {
+	if !r.HasExtension("network_dns_records") {
+		return fmt.Errorf(`The server is missing the required "network_dns_records" API extension`)
+	}
+
+	// Send the request.
+	_, _, err := r.query("PUT", fmt.Sprintf("/network-zones/%s/records/%s", url.PathEscape(zone), url.PathEscape(name)), record, ETag)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteNetworkZoneRecord deletes an existing network zone record.
+func (r *ProtocolLXD) DeleteNetworkZoneRecord(zone string, name string) error {
+	if !r.HasExtension("network_dns_records") {
+		return fmt.Errorf(`The server is missing the required "network_dns_records" API extension`)
+	}
+
+	// Send the request.
+	_, _, err := r.query("DELETE", fmt.Sprintf("/network-zones/%s/records/%s", url.PathEscape(zone), url.PathEscape(name)), nil, "")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
