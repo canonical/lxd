c9a4b28aa74e06b3d2a45098611757497ef5c966
Max Asnaashari <max.asnaashari@canonical.com>
2022-01-24 21:56:31 +0000

    lxd/endpoints/starttls: Remove TLS methods from endpoints package
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/endpoints/network.go
+++ lxd/endpoints/network.go
@@ -1,15 +1,12 @@
 package endpoints
 
 import (
-	"crypto/tls"
 	"fmt"
 	"net"
 	"strings"
-	"sync"
 	"time"
 
-	"github.com/armon/go-proxyproto"
-
+	"github.com/lxc/lxd/lxd/endpoints/listeners"
 	"github.com/lxc/lxd/lxd/util"
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/logger"
@@ -194,71 +191,4 @@ func networkCreateListener(address string, cert *shared.CertInfo) (net.Listener,
 		return nil, errors.Wrap(err, "Bind network address")
 	}
 	return networkTLSListener(listener, cert), nil
-}
-
-// A variation of the standard tls.Listener that supports atomically swapping
-// the underlying TLS configuration. Requests served before the swap will
-// continue using the old configuration.
-type networkListener struct {
-	net.Listener
-	mu           sync.RWMutex
-	config       *tls.Config
-	trustedProxy []net.IP
-}
-
-func networkTLSListener(inner net.Listener, cert *shared.CertInfo) *networkListener {
-	listener := &networkListener{
-		Listener: inner,
-	}
-	listener.Config(cert)
-	return listener
-}
-
-// Accept waits for and returns the next incoming TLS connection then use the
-// current TLS configuration to handle it.
-func (l *networkListener) Accept() (net.Conn, error) {
-	c, err := l.Listener.Accept()
-	if err != nil {
-		return nil, err
-	}
-	l.mu.RLock()
-	defer l.mu.RUnlock()
-	config := l.config
-	if isProxy(c.RemoteAddr().String(), l.trustedProxy) {
-		c = proxyproto.NewConn(c, 0)
-	}
-	return tls.Server(c, config), nil
-}
-
-// Config safely swaps the underlying TLS configuration.
-func (l *networkListener) Config(cert *shared.CertInfo) {
-	config := util.ServerTLSConfig(cert)
-
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	l.config = config
-}
-
-// TrustedProxy sets new the https trusted proxy configuration
-func (l *networkListener) TrustedProxy(trustedProxy []net.IP) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	l.trustedProxy = trustedProxy
-}
-
-func isProxy(addr string, proxies []net.IP) bool {
-	host, _, err := net.SplitHostPort(addr)
-	if err != nil {
-		return false
-	}
-	hostIP := net.ParseIP(host)
-
-	for _, p := range proxies {
-		if hostIP.Equal(p) {
-			return true
-		}
-	}
-	return false
 }
