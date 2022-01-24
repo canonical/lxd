e438253d897cd0f15265efb0b677349d3500ab98
Mark Laing <mark.laing@canonical.com>
2022-01-24 17:12:51 +0000

    lxd/storage/drivers: Uses fast snapshot as source of migration.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/storage/drivers/driver_zfs_volumes.go
+++ lxd/storage/drivers/driver_zfs_volumes.go
@@ -1382,6 +1382,19 @@ func (d *zfs) RenameVolume(vol Volume, newVolName string, op *operations.Operati
 func (d *zfs) MigrateVolume(vol Volume, conn io.ReadWriteCloser, volSrcArgs *migration.VolumeSourceArgs, op *operations.Operation) error {
 	// Handle simple rsync and block_and_rsync through generic.
 	if volSrcArgs.MigrationType.FSType == migration.MigrationFSType_RSYNC || volSrcArgs.MigrationType.FSType == migration.MigrationFSType_BLOCK_AND_RSYNC {
+		// If volume is filesystem type, create a fast snapshot to ensure migration is consistent.
+		if vol.contentType == ContentTypeFS && !vol.IsSnapshot() {
+			snapshotPath, reverter, err := d.readonlySnapshot(vol)
+			if err != nil {
+				return err
+			}
+
+			// Clean up the snapshot.
+			defer reverter.Fail()
+
+			// Set the path of the volume to the path of the fast snapshot so the migration reads from there instead.
+			vol.mountCustomPath = snapshotPath
+		}
 		return genericVFSMigrateVolume(d, d.state, vol, conn, volSrcArgs, op)
 	} else if volSrcArgs.MigrationType.FSType != migration.MigrationFSType_ZFS {
 		return ErrNotSupported
