89496a666271aa9ed7b5a255b2e9de55751a2bd8
Thomas Parrott <thomas.parrott@canonical.com>
2022-02-11 10:44:19 +0000

    lxd-agent/events: d.events.AddListener excludeSources usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd-agent/events.go
+++ lxd-agent/events.go
@@ -45,7 +45,7 @@ func eventsSocket(d *Daemon, r *http.Request, w http.ResponseWriter) error {
 	defer c.Close() // This ensures the go routine below is ended when this function ends.
 
 	// As we don't know which project we are in, subscribe to events from all projects.
-	listener, err := d.events.AddListener("", true, c, strings.Split(typeStr, ","), nil, nil)
+	listener, err := d.events.AddListener("", true, c, strings.Split(typeStr, ","), nil, nil, nil)
 	if err != nil {
 		return err
 	}
