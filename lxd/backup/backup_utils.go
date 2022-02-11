e5a3dbc97617cf07e65b79d5e1e00ffa01492a53
Piotr Resztak <piotr.resztak@gmail.com>
2022-02-11 15:43:55 +0100

    lxd/backup: AppArmor support for extractors
    Signed-off-by: Piotr Resztak <piotr.resztak@gmail.com>

--- lxd/backup/backup_utils.go
+++ lxd/backup/backup_utils.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/lxc/lxd/lxd/archive"
+	"github.com/lxc/lxd/lxd/sys"
 	"github.com/lxc/lxd/shared"
 )
 
 // TarReader rewinds backup file handle r and returns new tar reader and process cleanup function.
-func TarReader(r io.ReadSeeker) (*tar.Reader, context.CancelFunc, error) {
+func TarReader(r io.ReadSeeker, sysOS *sys.OS, outputPath string) (*tar.Reader, context.CancelFunc, error) {
 	r.Seek(0, 0)
 	_, _, unpacker, err := shared.DetectCompressionFile(r)
 	if err != nil {
@@ -21,7 +23,7 @@ func TarReader(r io.ReadSeeker) (*tar.Reader, context.CancelFunc, error) {
 		return nil, nil, fmt.Errorf("Unsupported backup compression")
 	}
 
-	tr, cancelFunc, err := shared.CompressedTarReader(context.Background(), r, unpacker)
+	tr, cancelFunc, err := archive.CompressedTarReader(context.Background(), r, unpacker, sysOS, outputPath)
 	if err != nil {
 		return nil, nil, err
 	}
