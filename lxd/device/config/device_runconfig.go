caa59fdb94ceec6592e10e6da4fae12e2fdde92f
Thomas Parrott <thomas.parrott@canonical.com>
2022-02-24 11:17:27 +0000

    lxd/device/config/device/runconfig: Long form import
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/config/device_runconfig.go
+++ lxd/device/config/device_runconfig.go
@@ -1,6 +1,8 @@
 package config
 
-import "github.com/lxc/lxd/lxd/revert"
+import (
+	"github.com/lxc/lxd/lxd/revert"
+)
 
 // MountOwnerShiftNone do not use owner shifting.
 const MountOwnerShiftNone = ""
