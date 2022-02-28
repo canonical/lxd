7fe365d0cb6f428bcaae0972e979c5423d69b4b2
Thomas Parrott <thomas.parrott@canonical.com>
2022-02-28 16:18:29 +0000

    lxd/instance/sftp: Adds /1.0/instances/<name>/sftp handler
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instances.go
+++ lxd/instances.go
@@ -64,6 +64,17 @@ var instanceStateCmd = APIEndpoint{
 	Put: APIEndpointAction{Handler: instanceStatePut, AccessHandler: allowProjectPermission("containers", "operate-containers")},
 }
 
+var instanceSFTPCmd = APIEndpoint{
+	Name: "instanceFile",
+	Path: "instances/{name}/sftp",
+	Aliases: []APIEndpointAlias{
+		{Name: "containerFile", Path: "containers/{name}/files"},
+		{Name: "vmFile", Path: "virtual-machines/{name}/files"},
+	},
+
+	Get: APIEndpointAction{Handler: instanceSFTPHandler, AccessHandler: allowProjectPermission("containers", "operate-containers")},
+}
+
 var instanceFileCmd = APIEndpoint{
 	Name: "instanceFile",
 	Path: "instances/{name}/files",
