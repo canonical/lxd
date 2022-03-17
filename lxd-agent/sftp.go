87fc947446e32657c8da5958bf8d990bb0c59691
Thomas Parrott <thomas.parrott@canonical.com>
2022-03-17 13:59:02 +0000

    lxd-agent/sftp: response.Upgrade usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd-agent/sftp.go
+++ lxd-agent/sftp.go
@@ -39,19 +39,22 @@ func (r *sftpServe) Render(w http.ResponseWriter) error {
 	hijacker, ok := w.(http.Hijacker)
 	if !ok {
 		http.Error(w, "Webserver doesn't support hijacking", http.StatusInternalServerError)
+
 		return nil
 	}
 
 	conn, _, err := hijacker.Hijack()
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to hijack connection: %v", err), http.StatusInternalServerError)
+		http.Error(w, fmt.Errorf("Failed to hijack connection: %w", err).Error(), http.StatusInternalServerError)
+
 		return nil
 	}
 	defer conn.Close()
 
-	data := []byte("HTTP/1.1 101 Switching Protocols\r\nUpgrade: sftp\r\n\r\n")
-	n, err := conn.Write(data)
-	if err != nil || n != len(data) {
+	err = response.Upgrade(conn, "sftp")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
 		return nil
 	}
 
