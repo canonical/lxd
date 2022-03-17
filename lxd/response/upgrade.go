9ec233ffdbcd07b6c640398dbc51380df1655e81
Thomas Parrott <thomas.parrott@canonical.com>
2022-03-17 13:58:30 +0000

    lxd/response/upgrade: Adds Upgrade function to upgrade an HTTP connection
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ lxd/response/upgrade.go
@@ -0,0 +1,31 @@
+package response
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+// Upgrade takes a hijacked HTTP connection and sends the HTTP 101 Switching Protocols headers for protocolName.
+func Upgrade(hijackedConn net.Conn, protocolName string) error {
+	// Write the status line and upgrade header by hand since w.WriteHeader() would fail after Hijack().
+	sb := strings.Builder{}
+	sb.WriteString("HTTP/1.1 101 Switching Protocols\r\n")
+	sb.WriteString(fmt.Sprintf("Upgrade: %s\r\n", protocolName))
+	sb.WriteString("Connection: Upgrade\r\n\r\n")
+
+	hijackedConn.SetWriteDeadline(time.Now().Add(time.Second * 5))
+	n, err := hijackedConn.Write([]byte(sb.String()))
+	hijackedConn.SetWriteDeadline(time.Time{}) // Cancel deadline.
+
+	if err != nil {
+		return fmt.Errorf("Failed writing upgrade headers: %w", err)
+	}
+
+	if n != sb.Len() {
+		return fmt.Errorf("Failed writing upgrade headers")
+	}
+
+	return nil
+}
