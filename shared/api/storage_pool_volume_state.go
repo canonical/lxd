c4a153e4ceb0d54382692df372cb38263f098a0e
Piotr Resztak <piotr.resztak@gmail.com>
2022-04-01 14:00:14 +0200

    shared/api: Add Total field
    Signed-off-by: Piotr Resztak <piotr.resztak@gmail.com>

--- shared/api/storage_pool_volume_state.go
+++ shared/api/storage_pool_volume_state.go
@@ -19,4 +19,10 @@ type StorageVolumeStateUsage struct {
 	// Used space in bytes
 	// Example: 1693552640
 	Used uint64 `json:"used,omitempty" yaml:"used,omitempty"`
+
+	// Storage volume size in bytes
+	// Example: 5189222192
+	//
+	// API extension: storage_volume_state_total
+	Total int64 `json:"total" yaml:"total"`
 }
