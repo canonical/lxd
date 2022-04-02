5b077062c759bbb6da7b6ef3d8900e3deaf6d972
Stéphane Graber <stgraber@ubuntu.com>
2022-04-01 20:19:39 -0400

    lxd/util: Remove SplitNTrimSpace
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/util/config.go
+++ lxd/util/config.go
@@ -57,19 +57,3 @@ func CopyConfig(config map[string]string) map[string]string {
 
 	return copy
 }
-
-// SplitNTrimSpace returns result of strings.SplitN() and then strings.TrimSpace() on each element.
-// Accepts nilIfEmpty argument which if true, will return nil slice if s is empty (after trimming space).
-func SplitNTrimSpace(s string, sep string, n int, nilIfEmpty bool) []string {
-	if nilIfEmpty && strings.TrimSpace(s) == "" {
-		return nil
-	}
-
-	parts := strings.SplitN(s, sep, n)
-
-	for i, v := range parts {
-		parts[i] = strings.TrimSpace(v)
-	}
-
-	return parts
-}
