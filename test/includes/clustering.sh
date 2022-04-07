4e80624109bf62fda0b2b12cba96df729fc92681
Simon Deziel <simon.deziel@canonical.com>
2022-04-07 17:26:52 -0400

    test/includes: remove unnessary subshells
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/clustering.sh
+++ test/includes/clustering.sh
@@ -33,8 +33,7 @@ setup_clustering_netns() {
 
   echo "==> Setup clustering netns ${ns}"
 
-  (
-    cat << EOF
+  cat << EOF | unshare -m -n /bin/sh
 set -e
 mkdir -p "${TEST_DIR}/ns/${ns}"
 touch "${TEST_DIR}/ns/${ns}/net"
@@ -54,12 +53,10 @@ touch /run/netns/hostns
 mount --bind /proc/1/ns/net /run/netns/hostns
 
 mount -t tmpfs tmpfs /usr/local/bin
-(
-cat << EOE
+cat << EOE > /usr/local/bin/in-hostnetns
 #!/bin/sh
 exec ip netns exec hostns /usr/bin/\\\$(basename \\\$0) "\\\$@"
 EOE
-) > /usr/local/bin/in-hostnetns
 chmod +x /usr/local/bin/in-hostnetns
 # Setup ceph
 ln -s in-hostnetns /usr/local/bin/ceph
@@ -68,7 +65,6 @@ ln -s in-hostnetns /usr/local/bin/rbd
 sleep 300&
 echo \$! > "${TEST_DIR}/ns/${ns}/PID"
 EOF
-  ) | unshare -m -n /bin/sh
 
   veth1="v${ns}1"
   veth2="v${ns}2"
@@ -79,8 +75,7 @@ EOF
 
   nsbridge="br$$"
   ip link set dev "${veth1}" master "${nsbridge}" up
-  (
-    cat <<EOF
+  cat << EOF | nsenter -n -m -t "${nspid}" /bin/sh
 set -e
 
 ip link set dev lo up
@@ -89,7 +84,6 @@ ip link set eth0 up
 ip addr add "10.1.1.10${id}/16" dev eth0
 ip route add default via 10.1.1.1
 EOF
-  ) | nsenter -n -m -t "${nspid}" /bin/sh
 }
 
 teardown_clustering_netns() {
