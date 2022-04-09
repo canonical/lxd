63fb0e29884225ac8b51658455ae86f2013cbfe0
Piotr Resztak <piotr.resztak@gmail.com>
2022-04-09 15:30:13 +0200

    lxd/network: Support for instances.nic.host_name config key
    Signed-off-by: Piotr Resztak <piotr.resztak@gmail.com>

--- lxd/network/network_utils.go
+++ lxd/network/network_utils.go
@@ -64,6 +64,12 @@ func RandomDevName(prefix string) string {
 	return iface
 }
 
+// MACDevName returns interface name with prefix 'lxd' and MAC without leading 2 digits.
+func MACDevName(mac net.HardwareAddr) string {
+	devName := strings.Join(strings.Split(mac.String(), ":"), "")
+	return fmt.Sprintf("lxd%s", devName[2:])
+}
+
 // usedByInstanceDevices looks for instance NIC devices using the network and runs the supplied usageFunc for each.
 func usedByInstanceDevices(s *state.State, networkProjectName string, networkName string, usageFunc func(inst db.Instance, nicName string, nicConfig map[string]string) error) error {
 	return s.Cluster.InstanceList(nil, func(inst db.Instance, p db.Project, profiles []api.Profile) error {
