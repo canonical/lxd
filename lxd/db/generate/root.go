d5bc046a9a3de0152d7a30521e663df81e67d871
Stéphane Graber <stgraber@ubuntu.com>
2022-04-14 02:33:56 -0400

    lxd/db/generate: Don't list bash completion
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/db/generate/root.go
+++ lxd/db/generate/root.go
@@ -16,6 +16,7 @@ used in LXD's source code.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return fmt.Errorf("Not implemented")
 		},
+		CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
 	}
 	cmd.AddCommand(newDb())
 
