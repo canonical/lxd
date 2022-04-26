bdf429060518930409c4fa3e01f20a5072468a42
Thomas Parrott <thomas.parrott@canonical.com>
2022-04-26 14:06:25 +0100

    test: Adds syscall sysinfo interception test
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

---
+++ test/suites/container_syscall_interception.sh
@@ -0,0 +1,23 @@
+test_container_syscall_interception() {
+  ensure_import_testimage
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  (
+    cd syscall/sysinfo || return
+    # Use -buildvcs=false here to prevent git complaining about untrusted directory when tests are run as root.
+    go build -v -buildvcs=false ./...
+  )
+
+  lxc init testimage c1
+  lxc config set c1 limits.memory=123MiB
+  lxc start c1
+  lxc file push syscall/sysinfo/sysinfo c1/root/sysinfo
+  lxc exec c1 -- /root/sysinfo
+  ! lxc exec c1 -- /root/sysinfo | grep "Totalram:128974848 " || false
+  lxc stop -f c1
+  lxc config set c1 security.syscalls.intercept.sysinfo=true
+  lxc start c1
+  lxc exec c1 -- /root/sysinfo
+  lxc exec c1 -- /root/sysinfo | grep "Totalram:128974848 "
+  lxc delete -f c1
+}
