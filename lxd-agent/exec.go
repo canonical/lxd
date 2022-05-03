0b2d58cb2eaf062a8f984c9ce6f415239e79daad
Thomas Parrott <thomas.parrott@canonical.com>
2022-05-03 15:42:55 +0200

    lxd-agent/exec: Removes unused imports
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd-agent/exec.go
+++ lxd-agent/exec.go
@@ -23,8 +23,6 @@ import (
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/api"
 	"github.com/lxc/lxd/shared/logger"
-	"github.com/lxc/lxd/shared/logging"
-	"github.com/lxc/lxd/shared/netutils"
 	"github.com/lxc/lxd/shared/ws"
 )
 
