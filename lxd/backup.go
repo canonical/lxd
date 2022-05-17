d06b09297515da92557c90767374106d12600b3a
Thomas Parrott <thomas.parrott@canonical.com>
2022-05-17 09:40:00 +0100

    lxd/backup: Updates backupWriteIndex to use pool.GenerateInstanceBackupConfig
    This removes duplication and has the side effect of including the contents of
    the instance's on disk backup.yaml in index.yaml.
    
    This is desirable as it will mean that in the future we can potentially create
    the storage volume DB records and on-disk storage volumes at the correct size
    and config before unpacking a backup into it.
    
    This is because for optimized backups, the instance's backup.yaml file is not
    accessible in the tarball as its part of the binary blob from the storage driver.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/backup.go
+++ lxd/backup.go
@@ -232,71 +232,25 @@ func backupWriteIndex(sourceInst instance.Instance, pool storagePools.Pool, opti
 		return fmt.Errorf("Unrecognised instance type for backup type conversion")
 	}
 
-	volType, err := storagePools.InstanceTypeToVolumeType(sourceInst.Type())
+	config, err := pool.GenerateInstanceBackupConfig(sourceInst, snapshots, nil)
 	if err != nil {
-		return err
-	}
-
-	vol, err := storagePools.VolumeDBGet(pool, sourceInst.Project(), sourceInst.Name(), volType)
-	if err != nil {
-		return fmt.Errorf("Failed loading instance volume record: %w", err)
+		return fmt.Errorf("Failed generating instance backup config: %w", err)
 	}
 
 	indexInfo := backup.Info{
 		Name:             sourceInst.Name(),
 		Pool:             pool.Name(),
-		Snapshots:        []string{},
 		Backend:          pool.Driver().Info().Name,
 		Type:             backupType,
 		OptimizedStorage: &optimized,
 		OptimizedHeader:  &poolDriverOptimizedHeader,
-		Config: &backup.Config{
-			Volume: vol,
-		},
+		Config:           config,
 	}
 
 	if snapshots {
-		snaps, err := sourceInst.Snapshots()
-		if err != nil {
-			return err
-		}
-
-		volSnaps, err := storagePools.VolumeDBSnapshotsGet(pool, sourceInst.Project(), sourceInst.Name(), volType)
-		if err != nil {
-			return fmt.Errorf("Failed loading instance volume snapshot records: %w", err)
-		}
-
-		if len(snaps) != len(volSnaps) {
-			return fmt.Errorf("Instance snapshot record count doesn't match instance snapshot volume record count")
-		}
-
-		for i := range volSnaps {
-			foundInstanceSnapshot := false
-			for _, snap := range snaps {
-				if snap.Name() == volSnaps[i].Name {
-					foundInstanceSnapshot = true
-					break
-				}
-			}
-
-			if !foundInstanceSnapshot {
-				return fmt.Errorf("Instance snapshot record missing for %q", volSnaps[i].Name)
-			}
-
-			_, snapName, _ := shared.InstanceGetParentAndSnapshotName(volSnaps[i].Name)
-			indexInfo.Snapshots = append(indexInfo.Snapshots, snapName)
-
-			snapshot := api.StorageVolumeSnapshot{
-				StorageVolumeSnapshotPut: api.StorageVolumeSnapshotPut{
-					Description: volSnaps[i].Description,
-					ExpiresAt:   &volSnaps[i].ExpiryDate,
-				},
-				Name:        snapName, // Snapshot only name, not full name.
-				Config:      volSnaps[i].Config,
-				ContentType: volSnaps[i].ContentType,
-			}
-
-			indexInfo.Config.VolumeSnapshots = append(indexInfo.Config.VolumeSnapshots, &snapshot)
+		indexInfo.Snapshots = make([]string, 0, len(config.Snapshots))
+		for _, s := range config.Snapshots {
+			indexInfo.Snapshots = append(indexInfo.Snapshots, s.Name)
 		}
 	}
 
