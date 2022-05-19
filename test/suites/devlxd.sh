40aedfd1038757f4c458ba3416a956b99c297bb9
Thomas Hipp <thomas.hipp@canonical.com>
2022-05-19 21:19:13 +0200

    test: Add devlxd http stream
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- test/suites/devlxd.sh
+++ test/suites/devlxd.sh
@@ -25,8 +25,11 @@ test_devlxd() {
   lxc config set devlxd security.nesting true
   ! lxc exec devlxd devlxd-client security.nesting | grep true || false
 
-  lxc exec devlxd devlxd-client monitor > "${TEST_DIR}/devlxd.log" &
-  client=$!
+  lxc exec devlxd devlxd-client monitor-websocket > "${TEST_DIR}/devlxd-websocket.log" &
+  client_websocket=$!
+
+  lxc exec devlxd devlxd-client monitor-stream > "${TEST_DIR}/devlxd-stream.log" &
+  client_stream=$!
 
   (
     cat << EOF
@@ -67,14 +70,15 @@ EOF
     lxc config set devlxd user.foo bar
     lxc config set devlxd security.nesting true
 
-    true > "${TEST_DIR}/devlxd.log"
+    true > "${TEST_DIR}/devlxd-websocket.log"
+    true > "${TEST_DIR}/devlxd-stream.log"
 
     lxc config set devlxd user.foo baz
     lxc config set devlxd security.nesting false
     lxc config device add devlxd mnt disk source="${TEST_DIR}" path=/mnt
     lxc config device remove devlxd mnt
 
-    if [ "$(tr -d '\0' < "${TEST_DIR}/devlxd.log" | md5sum | cut -d' ' -f1)" != "$(md5sum "${TEST_DIR}/devlxd.expected" | cut -d' ' -f1)" ]; then
+    if [ "$(tr -d '\0' < "${TEST_DIR}/devlxd-websocket.log" | md5sum | cut -d' ' -f1)" != "$(md5sum "${TEST_DIR}/devlxd.expected" | cut -d' ' -f1)" ] || [ "$(tr -d '\0' < "${TEST_DIR}/devlxd-stream.log" | md5sum | cut -d' ' -f1)" != "$(md5sum "${TEST_DIR}/devlxd.expected" | cut -d' ' -f1)" ]; then
       sleep 0.5
       continue
     fi
@@ -83,7 +87,8 @@ EOF
     break
   done
 
-  kill -9 "${client}"
+  kill -9 "${client_websocket}"
+  kill -9 "${client_stream}"
   lxc delete devlxd --force
 
   [ "${MATCH}" = "1" ] || false
