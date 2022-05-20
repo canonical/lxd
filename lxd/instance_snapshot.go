b2342771f4730caeb04de5a3be139d65de6022ed
Mark Laing <mark.laing@canonical.com>
2022-05-20 17:39:43 +0100

    lxd: Default allowInconsistent to false for snapshots.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/instance_snapshot.go
+++ lxd/instance_snapshot.go
@@ -654,7 +654,7 @@ func snapshotPost(d *Daemon, r *http.Request, snapInst instance.Instance, contai
 			}
 		}
 
-		ws, err := newMigrationSource(snapInst, reqNew.Live, true)
+		ws, err := newMigrationSource(snapInst, reqNew.Live, true, false)
 		if err != nil {
 			return response.SmartError(err)
 		}
