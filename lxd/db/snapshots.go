37cdf0f307a20f7504a97c0e73d71a0b7e1fbd96
Stéphane Graber <stgraber@ubuntu.com>
2022-05-25 17:27:37 -0400

    lxd/db/snapshots: Add GetLocalExpiredInstanceSnapshots
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/db/snapshots.go
+++ lxd/db/snapshots.go
@@ -108,6 +108,61 @@ func (c *ClusterTx) UpdateInstanceSnapshot(id int, description string, expiryDat
 	return nil
 }
 
+// GetLocalExpiredInstanceSnapshots returns a list of expired snapshots.
+func (c *ClusterTx) GetLocalExpiredInstanceSnapshots() ([]InstanceSnapshot, error) {
+	q := `
+	SELECT
+		instances_snapshots.id,
+		instances_snapshots.expiry_date
+	FROM instances_snapshots
+	JOIN instances ON instances.id=instances_snapshots.instance_id
+	WHERE instances.node_id=? AND instances_snapshots.expiry_date != '0001-01-01T00:00:00Z'
+	`
+
+	snapshotIDs := []int{}
+	err := c.QueryScan(q, func(scan func(dest ...any) error) error {
+		var id int
+		var expiry sql.NullTime
+
+		// Read the row.
+		err := scan(&id, &expiry)
+		if err != nil {
+			return err
+		}
+
+		// Skip if not expired.
+		if !expiry.Valid || expiry.Time.Unix() <= 0 {
+			return nil
+		}
+
+		if time.Now().Unix()-expiry.Time.Unix() < 0 {
+			return nil
+		}
+
+		// Add the snapshot.
+		snapshotIDs = append(snapshotIDs, id)
+
+		return nil
+	}, c.nodeID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Fetch all the expired snapshot details.
+	snapshots := make([]InstanceSnapshot, len(snapshotIDs))
+
+	for i, id := range snapshotIDs {
+		snap, err := c.GetInstanceSnapshots(InstanceSnapshotFilter{ID: &id})
+		if err != nil {
+			return nil, err
+		}
+
+		snapshots[i] = snap[0]
+	}
+
+	return snapshots, nil
+}
+
 // GetInstanceSnapshotID returns the ID of the snapshot with the given name.
 func (c *Cluster) GetInstanceSnapshotID(project, instance, name string) (int, error) {
 	var id int64
