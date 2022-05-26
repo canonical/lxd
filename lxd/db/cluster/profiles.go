2bb83c59da577e15ee8e359801954f7db7b9d630
Max Asnaashari <max.asnaashari@canonical.com>
2022-05-26 01:21:28 +0000

    lxd/db/cluster/profiles.mapper: Add cluster.Profile and generated methods
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/cluster/profiles.go
+++ lxd/db/cluster/profiles.go
@@ -7,6 +7,37 @@ package cluster
 //go:generate -command mapper lxd-generate db mapper -t profiles.mapper.go
 //go:generate mapper reset -i -b "//go:build linux && cgo && !agent"
 //
-//go:generate mapper stmt -p "github.com/lxc/lxd/lxd/db" -e profile id version=2
+//go:generate mapper stmt -e profile objects version=2
+//go:generate mapper stmt -e profile objects-by-ID version=2
+//go:generate mapper stmt -e profile objects-by-Project version=2
+//go:generate mapper stmt -e profile objects-by-Project-and-Name version=2
+//go:generate mapper stmt -e profile id version=2
+//go:generate mapper stmt -e profile create version=2
+//go:generate mapper stmt -e profile rename version=2
+//go:generate mapper stmt -e profile update version=2
+//go:generate mapper stmt -e profile delete-by-Project-and-Name version=2
 //
-//go:generate mapper method -i -p "github.com/lxc/lxd/lxd/db" -e profile ID struct=Profile version=2
+//go:generate mapper method -i -e profile ID version=2
+//go:generate mapper method -i -e profile Exists version=2
+//go:generate mapper method -i -e profile GetMany references=Config,Device version=2
+//go:generate mapper method -i -e profile GetOne version=2
+//go:generate mapper method -i -e profile Create references=Config,Device version=2
+//go:generate mapper method -i -e profile Rename version=2
+//go:generate mapper method -i -e profile Update references=Config,Device version=2
+//go:generate mapper method -i -e profile DeleteOne-by-Project-and-Name version=2
+
+// Profile is a value object holding db-related details about a profile.
+type Profile struct {
+	ID          int
+	ProjectID   int    `db:"omit=create,update"`
+	Project     string `db:"primary=yes&join=projects.name"`
+	Name        string `db:"primary=yes"`
+	Description string `db:"coalesce=''"`
+}
+
+// ProfileFilter specifies potential query parameter fields.
+type ProfileFilter struct {
+	ID      *int
+	Project *string
+	Name    *string
+}
