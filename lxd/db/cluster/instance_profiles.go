0f299deaf8204d1e7e705cf4fc2266c9702a9a55
Max Asnaashari <max.asnaashari@canonical.com>
2022-05-31 02:23:21 +0000

    lxd/db/cluster/instances.mapper: Update generated code
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/cluster/instance_profiles.go
+++ lxd/db/cluster/instance_profiles.go
@@ -1,13 +1,25 @@
 package cluster
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
 // Code generation directives.
 //
 //go:generate -command mapper lxd-generate db mapper -t instance_profiles.mapper.go
 //go:generate mapper reset -i -b "//go:build linux && cgo && !agent"
 //
 //go:generate mapper stmt -e instance_profile objects-by-ProfileID version=2
+//go:generate mapper stmt -e instance_profile objects-by-InstanceID version=2
+//go:generate mapper stmt -e instance_profile create version=2
+//go:generate mapper stmt -e instance_profile delete-by-InstanceID version=2
 //
 //go:generate mapper method -i -e instance_profile GetMany struct=Profile version=2
+//go:generate mapper method -i -e instance_profile GetMany struct=Instance version=2
+//go:generate mapper method -i -e instance_profile Create struct=Instance version=2
+//go:generate mapper method -i -e instance_profile DeleteMany struct=Instance version=2
 
 // InstanceProfile is an association table struct that associates Instances
 // to Profiles.
@@ -21,4 +33,41 @@ type InstanceProfile struct {
 type InstanceProfileFilter struct {
 	InstanceID *int
 	ProfileID  *int
+}
+
+// UpdateInstanceProfiles updates the profiles of an instance in the order they are given.
+func UpdateInstanceProfiles(ctx context.Context, tx *sql.Tx, instance Instance, profiles []string) error {
+	err := DeleteInstanceProfiles(ctx, tx, instance.ID)
+	if err != nil {
+		return err
+	}
+
+	project := instance.Project
+	enabled, err := ProjectHasProfiles(ctx, tx, project)
+	if err != nil {
+		return fmt.Errorf("Check if project has profiles: %w", err)
+	}
+
+	if !enabled {
+		project = "default"
+	}
+
+	applyOrder := 1
+	stmt := stmt(tx, instanceProfileCreate)
+
+	for _, name := range profiles {
+		profileID, err := GetProfileID(ctx, tx, project, name)
+		if err != nil {
+			return err
+		}
+
+		_, err = stmt.Exec(instance.ID, profileID, applyOrder)
+		if err != nil {
+			return err
+		}
+
+		applyOrder = applyOrder + 1
+	}
+
+	return nil
 }
