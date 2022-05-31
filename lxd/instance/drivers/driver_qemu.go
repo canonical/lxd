3b1596e8f59e63cac382532fba505497333f44a2
Thomas Parrott <thomas.parrott@canonical.com>
2022-05-31 12:58:07 +0100

    lxd/instance/drivers/driver/qemu: Whitespace in Restore
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -4038,7 +4038,6 @@ func (d *qemu) Restore(source instance.Instance, stateful bool) error {
 			return fmt.Errorf("Failed to create instance restore operation: %w", err)
 		}
 		defer op.Done(nil)
-
 	}
 
 	ctxMap = logger.Ctx{
