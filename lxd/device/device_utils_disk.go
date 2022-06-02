dcf8928bd9b0e1396737831f526bc1e37118c3c2
Thomas Parrott <thomas.parrott@canonical.com>
2022-06-02 21:05:06 +0100

    lxd/device/device/utils/disk: Return revert.Hook after file handle in DiskVMVirtfsProxyStart
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/device_utils_disk.go
+++ lxd/device/device_utils_disk.go
@@ -298,9 +298,9 @@ func diskAddRootUserNSEntry(idmaps []idmap.IdmapEntry, hostRootID int64) []idmap
 
 // DiskVMVirtfsProxyStart starts a new virtfs-proxy-helper process.
 // If the idmaps slice is supplied then the proxy process is run inside a user namespace using the supplied maps.
-// Returns a revert fail function that can be used to undo this function if a subsequent step fails,
-// and a file handle to the proxy process.
-func DiskVMVirtfsProxyStart(execPath string, pidPath string, sharePath string, idmaps []idmap.IdmapEntry) (revert.Hook, *os.File, error) {
+// Returns a file handle to the proxy process and a revert fail function that can be used to undo this function if
+// a subsequent step fails,
+func DiskVMVirtfsProxyStart(execPath string, pidPath string, sharePath string, idmaps []idmap.IdmapEntry) (*os.File, revert.Hook, error) {
 	revert := revert.New()
 	defer revert.Fail()
 
@@ -384,9 +384,9 @@ func DiskVMVirtfsProxyStart(execPath string, pidPath string, sharePath string, i
 		return nil, nil, fmt.Errorf("Failed to save virtfs-proxy-helper state: %w", err)
 	}
 
-	revertExternal := revert.Clone()
+	cleanup := revert.Clone().Fail
 	revert.Success()
-	return revertExternal.Fail, cDialUnixFile, err
+	return cDialUnixFile, cleanup, err
 }
 
 // DiskVMVirtfsProxyStop stops the virtfs-proxy-helper process.
