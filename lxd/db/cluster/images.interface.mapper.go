a8d183fa70435f9e72e9b3489ec51c5338e09e45
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-12 08:34:01 +0000

    lxd/db/cluster/images.mapper: Update generated code
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/db/cluster/images.interface.mapper.go
@@ -0,0 +1,19 @@
+//go:build linux && cgo && !agent
+
+package cluster
+
+import (
+	"context"
+	"database/sql"
+)
+
+// ImageGenerated is an interface of generated methods for Image
+type ImageGenerated interface {
+	// GetImages returns all available images.
+	// generator: image GetMany
+	GetImages(ctx context.Context, tx *sql.Tx, filter ImageFilter) ([]Image, error)
+
+	// GetImage returns the image with the given key.
+	// generator: image GetOne
+	GetImage(ctx context.Context, tx *sql.Tx, project string, fingerprint string) (*Image, error)
+}
