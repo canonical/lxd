907385cff9b122c49a334b5fc42bde22e33bc28b
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-12 06:47:01 +0000

    lxd/db/snapshots: Remove db.Snapshot and generator comments
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/snapshots.go
+++ lxd/db/snapshots.go
@@ -9,71 +9,7 @@ import (
 	"time"
 
 	"github.com/lxc/lxd/lxd/db/cluster"
-	"github.com/lxc/lxd/shared"
 )
-
-// Code generation directives.
-//
-//go:generate -command mapper lxd-generate db mapper -t snapshots.mapper.go
-//go:generate mapper reset -i -b "//go:build linux && cgo && !agent"
-//
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot objects
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot objects-by-ID
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot objects-by-Project-and-Instance
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot objects-by-Project-and-Instance-and-Name
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot id
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot create struct=InstanceSnapshot
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot rename
-//go:generate mapper stmt -d cluster -p db -e instance_snapshot delete-by-Project-and-Instance-and-Name
-//
-//go:generate mapper method -i -d cluster -p db -e instance_snapshot GetMany
-//go:generate mapper method -i -d cluster -p db -e instance_snapshot GetOne
-//go:generate mapper method -i -d cluster -p db -e instance_snapshot ID struct=InstanceSnapshot
-//go:generate mapper method -i -d cluster -p db -e instance_snapshot Exists struct=InstanceSnapshot
-//go:generate mapper method -i -d cluster -p db -e instance_snapshot Create struct=InstanceSnapshot
-//go:generate mapper method -i -d cluster -p db -e instance_snapshot Rename
-//go:generate mapper method -i -d cluster -p db -e instance_snapshot DeleteOne-by-Project-and-Instance-and-Name
-
-// InstanceSnapshot is a value object holding db-related details about a snapshot.
-type InstanceSnapshot struct {
-	ID           int
-	Project      string `db:"primary=yes&join=projects.name&via=instance"`
-	Instance     string `db:"primary=yes&join=instances.name"`
-	Name         string `db:"primary=yes"`
-	CreationDate time.Time
-	Stateful     bool
-	Description  string `db:"coalesce=''"`
-	Config       map[string]string
-	Devices      map[string]Device
-	ExpiryDate   sql.NullTime
-}
-
-// InstanceSnapshotFilter specifies potential query parameter fields.
-type InstanceSnapshotFilter struct {
-	ID       *int
-	Project  *string
-	Instance *string
-	Name     *string
-}
-
-// ToInstance converts an instance snapshot to a database Instance, filling in extra fields from the parent instance.
-func (s *InstanceSnapshot) ToInstance(instance *cluster.Instance) cluster.Instance {
-	return cluster.Instance{
-		ID:           s.ID,
-		Project:      s.Project,
-		Name:         instance.Name + shared.SnapshotDelimiter + s.Name,
-		Node:         instance.Node,
-		Type:         instance.Type,
-		Snapshot:     true,
-		Architecture: instance.Architecture,
-		Ephemeral:    false,
-		CreationDate: s.CreationDate,
-		Stateful:     s.Stateful,
-		LastUseDate:  sql.NullTime{},
-		Description:  s.Description,
-		ExpiryDate:   s.ExpiryDate,
-	}
-}
 
 // UpdateInstanceSnapshotConfig inserts/updates/deletes the provided config keys.
 func (c *ClusterTx) UpdateInstanceSnapshotConfig(id int, values map[string]string) error {
