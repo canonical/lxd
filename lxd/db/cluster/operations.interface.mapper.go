9bfd65349f674424db0a7e2805db3745e677eda2
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-14 18:15:50 +0000

    lxd/db/cluster/operations.mapper: Update generated code
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/db/cluster/operations.interface.mapper.go
@@ -0,0 +1,27 @@
+//go:build linux && cgo && !agent
+
+package cluster
+
+import (
+	"context"
+	"database/sql"
+)
+
+// OperationGenerated is an interface of generated methods for Operation
+type OperationGenerated interface {
+	// GetOperations returns all available operations.
+	// generator: operation GetMany
+	GetOperations(ctx context.Context, tx *sql.Tx, filter OperationFilter) ([]Operation, error)
+
+	// CreateOrReplaceOperation adds a new operation to the database.
+	// generator: operation CreateOrReplace
+	CreateOrReplaceOperation(ctx context.Context, tx *sql.Tx, object Operation) (int64, error)
+
+	// DeleteOperation deletes the operation matching the given key parameters.
+	// generator: operation DeleteOne-by-UUID
+	DeleteOperation(ctx context.Context, tx *sql.Tx, uuid string) error
+
+	// DeleteOperations deletes the operation matching the given key parameters.
+	// generator: operation DeleteMany-by-NodeID
+	DeleteOperations(ctx context.Context, tx *sql.Tx, nodeID int64) error
+}
