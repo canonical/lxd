4138d269b57d486ce6c1fc447d6efc40ee01cc4e
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-14 21:32:45 +0000

    lxd/db/operationtype/operation/type: Create operationtype package
    OperationType is used by lxd-agent, but represents the type of an
    Operation defined in the lxd/db/cluster package. Moving this to its own
    package prevents from having to import the db and cluster packages into
    lxd-agent.
    
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/db/operationtype/operation_type.go
@@ -0,0 +1,267 @@
+package operationtype
+
+// Type is a numeric code indentifying the type of an Operation.
+type Type int64
+
+// Possible values for Type
+//
+// WARNING: The type codes are stored in the database, so this list of
+//          definitions should be normally append-only. Any other change
+//          requires a database update.
+const (
+	Unknown Type = iota
+	ClusterBootstrap
+	ClusterJoin
+	BackupCreate
+	BackupRename
+	BackupRestore
+	BackupRemove
+	ConsoleShow
+	InstanceCreate
+	InstanceUpdate
+	InstanceRename
+	InstanceMigrate
+	InstanceLiveMigrate
+	InstanceFreeze
+	InstanceUnfreeze
+	InstanceDelete
+	InstanceStart
+	InstanceStop
+	InstanceRestart
+	CommandExec
+	SnapshotCreate
+	SnapshotRename
+	SnapshotRestore
+	SnapshotTransfer
+	SnapshotUpdate
+	SnapshotDelete
+	ImageDownload
+	ImageDelete
+	ImageToken
+	ImageRefresh
+	VolumeCopy
+	VolumeCreate
+	VolumeMigrate
+	VolumeMove
+	VolumeSnapshotCreate
+	VolumeSnapshotDelete
+	VolumeSnapshotUpdate
+	ProjectRename
+	ImagesExpire
+	ImagesPruneLeftover
+	ImagesUpdate
+	ImagesSynchronize
+	LogsExpire
+	InstanceTypesUpdate
+	BackupsExpire
+	SnapshotsExpire
+	CustomVolumeSnapshotsExpire
+	CustomVolumeBackupCreate
+	CustomVolumeBackupRemove
+	CustomVolumeBackupRename
+	CustomVolumeBackupRestore
+	WarningsPruneResolved
+	ClusterJoinToken
+	VolumeSnapshotRename
+	ClusterMemberEvacuate
+	ClusterMemberRestore
+	CertificateAddToken
+	RemoveOrphanedOperations
+)
+
+// Description return a human-readable description of the operation type.
+func (t Type) Description() string {
+	switch t {
+	case ClusterBootstrap:
+		return "Creating bootstrap node"
+	case ClusterJoin:
+		return "Joining cluster"
+	case BackupCreate:
+		return "Backing up instance"
+	case BackupRename:
+		return "Renaming instance backup"
+	case BackupRestore:
+		return "Restoring backup"
+	case BackupRemove:
+		return "Removing instance backup"
+	case ConsoleShow:
+		return "Showing console"
+	case InstanceCreate:
+		return "Creating instance"
+	case InstanceUpdate:
+		return "Updating instance"
+	case InstanceRename:
+		return "Renaming instance"
+	case InstanceMigrate:
+		return "Migrating instance"
+	case InstanceLiveMigrate:
+		return "Live-migrating instance"
+	case InstanceFreeze:
+		return "Freezing instance"
+	case InstanceUnfreeze:
+		return "Unfreezing instance"
+	case InstanceDelete:
+		return "Deleting instance"
+	case InstanceStart:
+		return "Starting instance"
+	case InstanceStop:
+		return "Stopping instance"
+	case InstanceRestart:
+		return "Restarting instance"
+	case CommandExec:
+		return "Executing command"
+	case SnapshotCreate:
+		return "Snapshotting instance"
+	case SnapshotRename:
+		return "Renaming snapshot"
+	case SnapshotRestore:
+		return "Restoring snapshot"
+	case SnapshotTransfer:
+		return "Transferring snapshot"
+	case SnapshotUpdate:
+		return "Updating snapshot"
+	case SnapshotDelete:
+		return "Deleting snapshot"
+	case ImageDownload:
+		return "Downloading image"
+	case ImageDelete:
+		return "Deleting image"
+	case ImageToken:
+		return "Image download token"
+	case ImageRefresh:
+		return "Refreshing image"
+	case VolumeCopy:
+		return "Copying storage volume"
+	case VolumeCreate:
+		return "Creating storage volume"
+	case VolumeMigrate:
+		return "Migrating storage volume"
+	case VolumeMove:
+		return "Moving storage volume"
+	case VolumeSnapshotCreate:
+		return "Creating storage volume snapshot"
+	case VolumeSnapshotDelete:
+		return "Deleting storage volume snapshot"
+	case VolumeSnapshotUpdate:
+		return "Updating storage volume snapshot"
+	case VolumeSnapshotRename:
+		return "Renaming storage volume snapshot"
+	case ProjectRename:
+		return "Renaming project"
+	case ImagesExpire:
+		return "Cleaning up expired images"
+	case ImagesPruneLeftover:
+		return "Pruning leftover image files"
+	case ImagesUpdate:
+		return "Updating images"
+	case ImagesSynchronize:
+		return "Synchronizing images"
+	case LogsExpire:
+		return "Expiring log files"
+	case InstanceTypesUpdate:
+		return "Updating instance types"
+	case BackupsExpire:
+		return "Cleaning up expired instance backups"
+	case SnapshotsExpire:
+		return "Cleaning up expired instance snapshots"
+	case CustomVolumeSnapshotsExpire:
+		return "Cleaning up expired volume snapshots"
+	case CustomVolumeBackupCreate:
+		return "Creating custom volume backup"
+	case CustomVolumeBackupRemove:
+		return "Deleting custom volume backup"
+	case CustomVolumeBackupRename:
+		return "Renaming custom volume backup"
+	case CustomVolumeBackupRestore:
+		return "Restoring custom volume backup"
+	case WarningsPruneResolved:
+		return "Pruning resolved warnings"
+	case ClusterMemberEvacuate:
+		return "Evacuating cluster member"
+	case ClusterMemberRestore:
+		return "Restoring cluster member"
+	case RemoveOrphanedOperations:
+		return "Remove orphaned operations"
+	default:
+		return "Executing operation"
+	}
+}
+
+// Permission returns the needed RBAC permission to cancel the oepration
+func (t Type) Permission() string {
+	switch t {
+	case BackupCreate:
+		return "operate-containers"
+	case BackupRename:
+		return "operate-containers"
+	case BackupRestore:
+		return "operate-containers"
+	case BackupRemove:
+		return "operate-containers"
+	case ConsoleShow:
+		return "operate-containers"
+	case InstanceFreeze:
+		return "operate-containers"
+	case InstanceUnfreeze:
+		return "operate-containers"
+	case InstanceStart:
+		return "operate-containers"
+	case InstanceStop:
+		return "operate-containers"
+	case InstanceRestart:
+		return "operate-containers"
+	case CommandExec:
+		return "operate-containers"
+	case SnapshotCreate:
+		return "operate-containers"
+	case SnapshotRename:
+		return "operate-containers"
+	case SnapshotTransfer:
+		return "operate-containers"
+	case SnapshotUpdate:
+		return "operate-containers"
+	case SnapshotDelete:
+		return "operate-containers"
+
+	case InstanceCreate:
+		return "manage-containers"
+	case InstanceUpdate:
+		return "manage-containers"
+	case InstanceRename:
+		return "manage-containers"
+	case InstanceMigrate:
+		return "manage-containers"
+	case InstanceLiveMigrate:
+		return "manage-containers"
+	case InstanceDelete:
+		return "manage-containers"
+	case SnapshotRestore:
+		return "manage-containers"
+
+	case ImageDownload:
+		return "manage-images"
+	case ImageDelete:
+		return "manage-images"
+	case ImageToken:
+		return "manage-images"
+	case ImageRefresh:
+		return "manage-images"
+	case ImagesUpdate:
+		return "manage-images"
+	case ImagesSynchronize:
+		return "manage-images"
+
+	case CustomVolumeSnapshotsExpire:
+		return "operate-volumes"
+	case CustomVolumeBackupCreate:
+		return "manage-storage-volumes"
+	case CustomVolumeBackupRemove:
+		return "manage-storage-volumes"
+	case CustomVolumeBackupRename:
+		return "manage-storage-volumes"
+	case CustomVolumeBackupRestore:
+		return "manage-storage-volumes"
+	}
+
+	return ""
+}
