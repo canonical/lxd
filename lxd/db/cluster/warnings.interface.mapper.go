a285528ca9bdfd841a53040f6a3844b7146f255e
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-14 22:46:44 +0000

    lxd/db/cluster/warnings.mapper: Update generated code
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ lxd/db/cluster/warnings.interface.mapper.go
@@ -0,0 +1,35 @@
+//go:build linux && cgo && !agent
+
+package cluster
+
+import (
+	"context"
+	"database/sql"
+)
+
+// WarningGenerated is an interface of generated methods for Warning
+type WarningGenerated interface {
+	// GetWarnings returns all available warnings.
+	// generator: warning GetMany
+	GetWarnings(ctx context.Context, tx *sql.Tx, filter WarningFilter) ([]Warning, error)
+
+	// GetWarning returns the warning with the given key.
+	// generator: warning GetOne-by-UUID
+	GetWarning(ctx context.Context, tx *sql.Tx, uuid string) (*Warning, error)
+
+	// DeleteWarning deletes the warning matching the given key parameters.
+	// generator: warning DeleteOne-by-UUID
+	DeleteWarning(ctx context.Context, tx *sql.Tx, uuid string) error
+
+	// DeleteWarnings deletes the warning matching the given key parameters.
+	// generator: warning DeleteMany-by-EntityTypeCode-and-EntityID
+	DeleteWarnings(ctx context.Context, tx *sql.Tx, entityTypeCode int, entityID int) error
+
+	// GetWarningID return the ID of the warning with the given key.
+	// generator: warning ID
+	GetWarningID(ctx context.Context, tx *sql.Tx, uuid string) (int64, error)
+
+	// WarningExists checks if a warning with the given key exists.
+	// generator: warning Exists
+	WarningExists(ctx context.Context, tx *sql.Tx, uuid string) (bool, error)
+}
