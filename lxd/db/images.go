10b18da33b57a61cd3ce656579d6074c19734c10
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-15 17:38:00 +0000

    lxd/profiles/utils: Add missing profiles slice check
    GetProfilesIfEnabled can return an empty slice with no error, so it's
    not safe to check the first index without checking if the slice is
    empty.
    
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/images.go
+++ lxd/db/images.go
@@ -1066,6 +1066,10 @@ func (c *Cluster) CreateImage(project, fp string, fname string, sz int64, public
 				return err
 			}
 
+			if len(dbProfiles) != 1 {
+				return fmt.Errorf("Failed to find default profile in project %q", project)
+			}
+
 			_, err = tx.tx.Exec("INSERT INTO images_profiles(image_id, profile_id) VALUES(?, ?)", id, dbProfiles[0].ID)
 			if err != nil {
 				return err
