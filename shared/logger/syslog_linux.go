0b1ab1b53495e1c2709cadb7334cb33c0f0c2448
Stéphane Graber <stgraber@ubuntu.com>
2022-06-20 20:10:39 -0500

    shared/logger: Filter event levels for syslog
    Closes #10554
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- shared/logger/syslog_linux.go
+++ shared/logger/syslog_linux.go
@@ -9,12 +9,30 @@ import (
 	lSyslog "github.com/sirupsen/logrus/hooks/syslog"
 )
 
+type syslogHandler struct {
+	handler logrus.Hook
+}
+
+func (h syslogHandler) Fire(entry *logrus.Entry) error {
+	return h.handler.Fire(entry)
+}
+
+func (h syslogHandler) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+	}
+}
+
 func setupSyslog(logger *logrus.Logger, syslogName string) error {
 	syslogHook, err := lSyslog.NewSyslogHook("", "", syslog.LOG_INFO, syslogName)
 	if err != nil {
 		return err
 	}
 
-	logger.AddHook(syslogHook)
+	logger.AddHook(syslogHandler{syslogHook})
 	return nil
 }
