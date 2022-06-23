91a54e5d3b55f284a83d78dad86be757073d06ee
Mark Laing <mark.laing@canonical.com>
2022-06-23 15:33:03 +0100

    lxd/config: Fixes imports.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/config/map_test.go
+++ lxd/config/map_test.go
@@ -5,9 +5,10 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/lxc/lxd/lxd/config"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/lxc/lxd/lxd/config"
 )
 
 // Loading a config Map initializes it with the given values.
