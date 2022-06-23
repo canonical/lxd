f38c1c04f60a23075ef68545110a2ccc58e5cdac
Mark Laing <mark.laing@canonical.com>
2022-06-23 18:43:26 +0100

    lxd/operations: Ends all comments with a full-stop.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/operations/operations.go
+++ lxd/operations/operations.go
@@ -26,15 +26,15 @@ var debug bool
 var operationsLock sync.Mutex
 var operations = make(map[string]*Operation)
 
-// OperationClass represents the OperationClass type
+// OperationClass represents the OperationClass type.
 type OperationClass int
 
 const (
-	// OperationClassTask represents the Task OperationClass
+	// OperationClassTask represents the Task OperationClass.
 	OperationClassTask OperationClass = 1
-	// OperationClassWebsocket represents the Websocket OperationClass
+	// OperationClassWebsocket represents the Websocket OperationClass.
 	OperationClassWebsocket OperationClass = 2
-	// OperationClassToken represents the Token OperationClass
+	// OperationClassToken represents the Token OperationClass.
 	OperationClassToken OperationClass = 3
 )
 
