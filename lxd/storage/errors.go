65751fea8da2ba97f71d5b4958eb52972deb49d2
Mark Laing <mark.laing@canonical.com>
2022-06-23 18:48:08 +0100

    lxd/storage: Ends all comments with a full-stop.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/storage/errors.go
+++ lxd/storage/errors.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// ErrNilValue is the "Nil value provided" error
+// ErrNilValue is the "Nil value provided" error.
 var ErrNilValue = fmt.Errorf("Nil value provided")
 
 // ErrBackupSnapshotsMismatch is the "Backup snapshots mismatch" error.
