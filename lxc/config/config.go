ff3f373c570afab7bd68f683698a1157cec6fdfb
Mark Laing <mark.laing@canonical.com>
2022-06-23 15:20:32 +0100

    lxc/config: End comments with a full-stop.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxc/config/config.go
+++ lxc/config/config.go
@@ -8,7 +8,7 @@ import (
 	"github.com/juju/persistent-cookiejar"
 )
 
-// Config holds settings to be used by a client or daemon
+// Config holds settings to be used by a client or daemon.
 type Config struct {
 	// DefaultRemote holds the remote daemon name from the Remotes map
 	// that the client should communicate with by default
@@ -37,7 +37,7 @@ type Config struct {
 	cookieJars map[string]*cookiejar.Jar
 }
 
-// GlobalConfigPath returns a joined path of the global configuration directory and passed arguments
+// GlobalConfigPath returns a joined path of the global configuration directory and passed arguments.
 func (c *Config) GlobalConfigPath(paths ...string) string {
 	configDir := "/etc/lxd"
 	if os.Getenv("LXD_GLOBAL_CONF") != "" {
@@ -49,7 +49,7 @@ func (c *Config) GlobalConfigPath(paths ...string) string {
 	return filepath.Join(path...)
 }
 
-// ConfigPath returns a joined path of the configuration directory and passed arguments
+// ConfigPath returns a joined path of the configuration directory and passed arguments.
 func (c *Config) ConfigPath(paths ...string) string {
 	path := []string{c.ConfigDir}
 	path = append(path, paths...)
@@ -57,12 +57,12 @@ func (c *Config) ConfigPath(paths ...string) string {
 	return filepath.Join(path...)
 }
 
-// CookiesPath returns the path for the remote's cookie jar
+// CookiesPath returns the path for the remote's cookie jar.
 func (c *Config) CookiesPath(remote string) string {
 	return c.ConfigPath("jars", remote)
 }
 
-// ServerCertPath returns the path for the remote's server certificate
+// ServerCertPath returns the path for the remote's server certificate.
 func (c *Config) ServerCertPath(remote string) string {
 	if c.Remotes[remote].Global {
 		return c.GlobalConfigPath("servercerts", fmt.Sprintf("%s.crt", remote))
@@ -70,7 +70,7 @@ func (c *Config) ServerCertPath(remote string) string {
 	return c.ConfigPath("servercerts", fmt.Sprintf("%s.crt", remote))
 }
 
-// SaveCookies saves cookies to file
+// SaveCookies saves cookies to file.
 func (c *Config) SaveCookies() {
 	for _, jar := range c.cookieJars {
 		_ = jar.Save()
