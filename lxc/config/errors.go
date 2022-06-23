c354cfe527cda5476754b6cef46fac48c2301b8f
Mark Laing <mark.laing@canonical.com>
2022-06-23 15:20:32 +0100

    lxc/config: End comments with a full-stop.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxc/config/errors.go
+++ lxc/config/errors.go
@@ -4,5 +4,5 @@ import (
 	"fmt"
 )
 
-// ErrNotLinux is returned when attemping to access the "local" remote on non-Linux systems
+// ErrNotLinux is returned when attemping to access the "local" remote on non-Linux systems.
 var ErrNotLinux = fmt.Errorf("Can't connect to a local LXD server on a non-Linux system")
