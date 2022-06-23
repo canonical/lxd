cd7ddf7fe1d3eafa09e512692e949743b3d42af4
Mark Laing <mark.laing@canonical.com>
2022-06-23 15:20:32 +0100

    lxc/config: End comments with a full-stop.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxc/config/cert.go
+++ lxc/config/cert.go
@@ -8,7 +8,7 @@ import (
 	"github.com/lxc/lxd/shared"
 )
 
-// HasClientCertificate will return true if a client certificate has already been generated
+// HasClientCertificate will return true if a client certificate has already been generated.
 func (c *Config) HasClientCertificate() bool {
 	certf := c.ConfigPath("client.crt")
 	keyf := c.ConfigPath("client.key")
@@ -19,7 +19,7 @@ func (c *Config) HasClientCertificate() bool {
 	return true
 }
 
-// GenerateClientCertificate will generate the needed client.crt and client.key if needed
+// GenerateClientCertificate will generate the needed client.crt and client.key if needed.
 func (c *Config) GenerateClientCertificate() error {
 	if c.HasClientCertificate() {
 		return nil
@@ -31,7 +31,7 @@ func (c *Config) GenerateClientCertificate() error {
 	return shared.FindOrGenCert(certf, keyf, true, false)
 }
 
-// CopyGlobalCert will copy global (system-wide) certificate to the user config path
+// CopyGlobalCert will copy global (system-wide) certificate to the user config path.
 func (c *Config) CopyGlobalCert(src string, dst string) error {
 	oldPath := c.GlobalConfigPath("servercerts", fmt.Sprintf("%s.crt", src))
 	newPath := c.ConfigPath("servercerts", fmt.Sprintf("%s.crt", dst))
