fce2ec4f171ae0b36c7b0b5552e171ca43818cce
Mark Laing <mark.laing@canonical.com>
2022-06-29 09:26:41 +0100

    lxd/db: Use multiline import syntax.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/schema/errors.go
+++ lxd/db/schema/errors.go
@@ -1,6 +1,8 @@
 package schema
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // ErrGracefulAbort is a special error that can be returned by a Check function
 // to force Schema.Ensure to abort gracefully.
