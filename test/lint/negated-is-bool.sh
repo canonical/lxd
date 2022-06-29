7f01b7d9ddd62b905cf0c628d956031c459c8d9e
Simon Deziel <simon.deziel@canonical.com>
2022-06-29 14:10:36 -0400

    test/lint/negated-is-bool: use git grep and only check .go files
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/lint/negated-is-bool.sh
+++ test/lint/negated-is-bool.sh
@@ -2,7 +2,7 @@
 
 echo "Checking usage of negated shared.Is(True|False)*() functions..."
 
-OUT=$(grep -Pr --exclude-dir=.git '!(shared\.)?Is(True|False).*\(' . 2>/dev/null || true)
+OUT=$(git grep -P '!(shared\.)?Is(True|False).*\(' '*.go' || true)
 if [ -n "${OUT}" ]; then
   echo "ERROR: negated shared.Is(True|False)*() function in script: ${OUT}"
   exit 1
