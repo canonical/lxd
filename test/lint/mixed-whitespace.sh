4872f1f5ee0a00eb1787b829e47f07640f5190ed
Simon Deziel <simon.deziel@canonical.com>
2022-06-29 14:06:53 -0400

    test/lint/mixed-whitespace: only process files with .sh extention
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/lint/mixed-whitespace.sh
+++ test/lint/mixed-whitespace.sh
@@ -2,8 +2,8 @@
 
 echo "Checking for mixed tabs and spaces in shell scripts..."
 
-OUT=$(git grep -P '\t' . 2>/dev/null | grep '\.sh:' || true)
+OUT=$(git grep -lP '\t' '*.sh' || true)
 if [ -n "${OUT}" ]; then
-  echo "ERROR: mixed tabs and spaces in script: ${OUT}"
+  echo "ERROR: mixed tabs and spaces in script(s): ${OUT}"
   exit 1
 fi
