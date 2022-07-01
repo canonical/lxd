92837f9fc930f3a525fbacd0c4c4547dcc7b5db1
Thomas Hipp <thomas.hipp@canonical.com>
2022-07-01 12:24:48 +0200

    lxd-agent: Introduce CONNECTED state
    This sets the state to CONNECTED if something is received on
    chConnected.
    
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd-agent/daemon.go
+++ lxd-agent/daemon.go
@@ -8,6 +8,9 @@ import (
 type Daemon struct {
 	// Event servers
 	events *events.Server
+
+	// The channel which is used to indicate that the lxd-agent was able to connect to LXD.
+	chConnected chan struct{}
 }
 
 // newDaemon returns a new Daemon object with the given configuration.
@@ -15,6 +18,7 @@ func newDaemon(debug, verbose bool) *Daemon {
 	lxdEvents := events.NewServer(debug, verbose, nil)
 
 	return &Daemon{
-		events: lxdEvents,
+		events:      lxdEvents,
+		chConnected: make(chan struct{}),
 	}
 }
