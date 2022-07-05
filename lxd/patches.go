5ea0ae26e785c043068e1f687760430544818ee9
Thomas Parrott <thomas.parrott@canonical.com>
2022-07-05 17:03:43 +0100

    lxd: InstanceList updated usage
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/patches.go
+++ lxd/patches.go
@@ -401,7 +401,7 @@ func patchVMRenameUUIDKey(name string, d *Daemon) error {
 	oldUUIDKey := "volatile.vm.uuid"
 	newUUIDKey := "volatile.uuid"
 
-	return d.State().DB.Cluster.InstanceList(nil, func(inst db.InstanceArgs, p api.Project, profiles []api.Profile) error {
+	return d.State().DB.Cluster.InstanceList(nil, func(inst db.InstanceArgs, p api.Project) error {
 		if inst.Type != instancetype.VM {
 			return nil
 		}
