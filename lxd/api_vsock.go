9b8b62062af39cebe8dc97152a528bee6dfc1f01
Thomas Hipp <thomas.hipp@canonical.com>
2022-07-12 22:36:58 +0200

    lxd: Add vsock server
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

---
+++ lxd/api_vsock.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"crypto/x509"
+	"fmt"
+	"net/http"
+
+	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/lxd/db/cluster"
+	"github.com/lxc/lxd/lxd/instance"
+	"github.com/lxc/lxd/lxd/util"
+)
+
+func authenticateAgentCert(d *Daemon, r *http.Request) (bool, instance.Instance, error) {
+	var vsockID int
+	trusted := false
+
+	_, err := fmt.Sscanf(r.RemoteAddr, "vm(%d)", &vsockID)
+	if err != nil {
+		return false, nil, err
+	}
+
+	var clusterInst *cluster.Instance
+
+	err = d.db.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		var err error
+
+		clusterInst, err = tx.GetLocalInstanceWithVsockID(vsockID)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return false, nil, err
+	}
+
+	inst, err := instance.LoadByProjectAndName(d.State(), clusterInst.Project, clusterInst.Name)
+	if err != nil {
+		return false, nil, err
+	}
+
+	agentCert := inst.(instance.VM).AgentCertificate()
+
+	for _, cert := range r.TLS.PeerCertificates {
+		trusted, _ = util.CheckTrustState(*cert, map[string]x509.Certificate{"0": *agentCert}, nil, false)
+		if trusted {
+			return true, inst, nil
+		}
+	}
+
+	return false, nil, nil
+}
