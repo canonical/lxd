aae354a4473ed75218e3b697e7628e18cc1babde
Thomas Parrott <thomas.parrott@canonical.com>
2022-08-01 14:41:03 +0100

    lxd/storage/drivers/errors: Removes little used ErrNotImplemented
    There is a similar ErrNotSupported error we can use instead.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/errors.go
+++ lxd/storage/drivers/errors.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 )
 
-// ErrNotImplemented is the "Not implemented" error.
-var ErrNotImplemented = fmt.Errorf("Not implemented")
-
 // ErrUnknownDriver is the "Unknown driver" error.
 var ErrUnknownDriver = fmt.Errorf("Unknown driver")
 
