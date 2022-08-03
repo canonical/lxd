canonical/lxd

commit 9cda9c2135601e223a462f2fbd8a4e0f8dc0e017

client: Use DialContext.
Dial is deprecated.

Signed-off-by: Mark Laing <mark.laing@canonical.com>

client/util.go
@@ -1,6 +1,7 @@
 package lxd
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"net"
@@ -22,7 +23,7 @@ func tlsHTTPClient(client *http.Client, tlsClientCert string, tlsClientKey strin
 	// Define the http transport
 	transport := &http.Transport{
 		TLSClientConfig:       tlsConfig,
-		Dial:                  shared.RFC3493Dialer,
+		DialContext:           shared.RFC3493Dialer,
 		Proxy:                 shared.ProxyFromEnvironment,
 		DisableKeepAlives:     true,
 		ExpectContinueTimeout: time.Second * 30,
@@ -41,7 +42,7 @@ func tlsHTTPClient(client *http.Client, tlsClientCert string, tlsClientKey strin
 		tlsDial := func(network string, addr string, config *tls.Config, resetName bool) (net.Conn, error) {
 			// TCP connection
 			//lint:ignore SA1019 DialContext doesn't exist in Go 1.13
-			conn, err := transport.Dial(network, addr)
+			conn, err := transport.DialContext(context.Background(), network, addr)
 			if err != nil {
 				return nil, err
 			}
@@ -104,7 +105,7 @@ func tlsHTTPClient(client *http.Client, tlsClientCert string, tlsClientKey strin
 
 func unixHTTPClient(client *http.Client, path string) (*http.Client, error) {
 	// Setup a Unix socket dialer
-	unixDial := func(network, addr string) (net.Conn, error) {
+	unixDial := func(_ context.Context, network, addr string) (net.Conn, error) {
 		raddr, err := net.ResolveUnixAddr("unix", path)
 		if err != nil {
 			return nil, err
@@ -115,7 +116,7 @@ func unixHTTPClient(client *http.Client, path string) (*http.Client, error) {
 
 	// Define the http transport
 	transport := &http.Transport{
-		Dial:                  unixDial,
+		DialContext:           unixDial,
 		DisableKeepAlives:     true,
 		ExpectContinueTimeout: time.Second * 30,
 		ResponseHeaderTimeout: time.Second * 3600,

client/lxd.go
@@ -409,8 +409,7 @@ func (r *ProtocolLXD) rawWebsocket(url string) (*websocket.Conn, error) {
 
 	// Setup a new websocket dialer based on it
 	dialer := websocket.Dialer{
-		//lint:ignore SA1019 DialContext doesn't exist in Go 1.13
-		NetDial:          httpTransport.Dial,
+		NetDialContext:   httpTransport.DialContext,
 		TLSClientConfig:  httpTransport.TLSClientConfig,
 		Proxy:            httpTransport.Proxy,
 		HandshakeTimeout: time.Second * 5,

client/lxd_instances.go
@@ -2,6 +2,7 @@ package lxd
 
 import (
 	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -1370,9 +1371,9 @@ func (r *ProtocolLXD) rawSFTPConn(apiURL *url.URL) (net.Conn, error) {
 	var err error
 
 	if httpTransport.TLSClientConfig != nil {
-		conn, err = httpTransport.DialTLS("tcp", apiURL.Host)
+		conn, err = httpTransport.DialTLSContext(context.Background(), "tcp", apiURL.Host)
 	} else {
-		conn, err = httpTransport.Dial("tcp", apiURL.Host)
+		conn, err = httpTransport.DialContext(context.Background(), "tcp", apiURL.Host)
 	}
 	if err != nil {
 		return nil, err

commit 13f765dd41dbff8acf2c078b650f4fd265f4c7cd

client/util: Set transport.DialTLSContext in tlsHTTPClient
Fixes regression caused by 9cda9c2135601e223a462f2fbd8a4e0f8dc0e017

Fixes #10734

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

client/util.go
@@ -23,7 +23,6 @@ func tlsHTTPClient(client *http.Client, tlsClientCert string, tlsClientKey strin
 	// Define the http transport
 	transport := &http.Transport{
 		TLSClientConfig:       tlsConfig,
-		DialContext:           shared.RFC3493Dialer,
 		Proxy:                 shared.ProxyFromEnvironment,
 		DisableKeepAlives:     true,
 		ExpectContinueTimeout: time.Second * 30,
@@ -37,12 +36,9 @@ func tlsHTTPClient(client *http.Client, tlsClientCert string, tlsClientKey strin
 	}
 
 	// Special TLS handling
-	//lint:ignore SA1019 DialContext doesn't exist in Go 1.13
-	transport.DialTLS = func(network string, addr string) (net.Conn, error) {
+	transport.DialTLSContext = func(ctx context.Context, network string, addr string) (net.Conn, error) {
 		tlsDial := func(network string, addr string, config *tls.Config, resetName bool) (net.Conn, error) {
-			// TCP connection
-			//lint:ignore SA1019 DialContext doesn't exist in Go 1.13
-			conn, err := transport.DialContext(context.Background(), network, addr)
+			conn, err := shared.RFC3493Dialer(ctx, network, addr)
 			if err != nil {
 				return nil, err
 			}