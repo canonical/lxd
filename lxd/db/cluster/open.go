canonical/lxd

commit c6d0928b0f5ad63d8eb835d0ff454e14b4931b16

lxd/db/cluster/open: Update EnsureSchema to use ProjectFeaturesDefaults for default project features
Rather than duplicating list of default enabled features.

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/db/cluster/open.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"path/filepath"
+	"strings"
 	"sync/atomic"
 
 	driver "github.com/canonical/go-dqlite/driver"
@@ -186,14 +187,14 @@ INSERT INTO nodes(id, name, address, schema, api_extensions, arch, description) 
 			}
 
 			// Default project
-			stmt = `
-INSERT INTO projects (name, description) VALUES ('default', 'Default LXD project');
-INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.images', 'true');
-INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.profiles', 'true');
-INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.storage.volumes', 'true');
-INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.networks', 'true');
-`
-			_, err = tx.Exec(stmt)
+			var defaultProjectStmt strings.Builder
+			_, _ = defaultProjectStmt.WriteString("INSERT INTO projects (name, description) VALUES ('default', 'Default LXD project');")
+
+			for _, key := range ProjectFeaturesDefaults {
+				_, _ = defaultProjectStmt.WriteString(fmt.Sprintf("INSERT INTO projects_config (project_id, key, value) VALUES (1, '%s', 'true');", key))
+			}
+
+			_, err = tx.Exec(defaultProjectStmt.String())
 			if err != nil {
 				return err
 			}

commit 55440132c2ff97d9dcf79af1d7ee4959c37405de

lxd/projects: Ensure default project has features.networks
Fixes a regression introduced by c6d0928b0f5ad63d8eb835d0ff454e14b4931b16

Closes #10776

Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

lxd/db/cluster/open.go
@@ -190,7 +190,7 @@ INSERT INTO nodes(id, name, address, schema, api_extensions, arch, description) 
 			var defaultProjectStmt strings.Builder
 			_, _ = defaultProjectStmt.WriteString("INSERT INTO projects (name, description) VALUES ('default', 'Default LXD project');")
 
-			for _, key := range ProjectFeaturesDefaults {
+			for _, key := range ProjectFeatures {
 				_, _ = defaultProjectStmt.WriteString(fmt.Sprintf("INSERT INTO projects_config (project_id, key, value) VALUES (1, '%s', 'true');", key))
 			}
 

lxd/db/migration.go
@@ -129,6 +129,7 @@ INSERT INTO nodes(id, name, address, schema, api_extensions) VALUES(1, 'none', '
 	stmt = `
 INSERT INTO projects (name, description) VALUES ('default', 'Default LXD project');
 INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.images', 'true');
+INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.networks', 'true');
 INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.profiles', 'true');
 INSERT INTO projects_config (project_id, key, value) VALUES (1, 'features.storage.volumes', 'true');
 `