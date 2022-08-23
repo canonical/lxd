8287dd3304948fa3eec882962a89ec511f61c332
Thomas Parrott <thomas.parrott@canonical.com>
2022-08-23 11:09:17 +0100

    api: Adds storage_buckets_create_credentials extension
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -343,6 +343,7 @@ var APIExtensions = []string{
 	"storage_volumes_all_projects",
 	"metrics_memory_oom_total",
 	"storage_buckets",
+	"storage_buckets_create_credentials",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
