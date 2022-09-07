582a72b3b511c98a5ea8a6dfb07d98ccd5149a07
Max Asnaashari <max.asnaashari@canonical.com>
2022-09-07 19:54:10 +0000

    lxd/db/generate/db/mapping: Override joined column name with 'joinon' tag
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/generate/db/mapping.go
+++ lxd/db/generate/db/mapping.go
@@ -430,11 +430,12 @@ func (f *Field) InsertColumn(pkg *ast.Package, dbPkg *ast.Package, mapping *Mapp
 		// If there is a 'joinon' tag present without this table in the condition, then assume there is no column for this field.
 		joinOn := f.Config.Get("joinon")
 		if joinOn != "" {
-			before, _, ok := strings.Cut(joinOn, ".")
+			before, after, ok := strings.Cut(joinOn, ".")
 			if !ok {
 				return "", "", fmt.Errorf("'joinon' tag of field %q of struct %q must be of form '<table>.<column>'", f.Name, mapping.Name)
 			}
 
+			columnName = after
 			if tableName != before {
 				return "", "", nil
 			}
