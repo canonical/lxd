canonical/lxd

commit f041bb099b16983a09b3efb20052e4275e71d8d8

lxd/warnings/warnings: Use cluster package helpers for Warnings
Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

lxd/warnings.go
@@ -168,39 +168,34 @@ func warningsGet(d *Daemon, r *http.Request) response.Response {
 	// Parse the project field
 	projectName := queryParam(r, "project")
 
-	var dbWarnings []db.Warning
-
+	var warnings []api.Warning
 	err = d.db.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		filter := db.WarningFilter{}
+		filter := cluster.WarningFilter{}
 
 		if projectName != "" {
 			filter.Project = &projectName
 		}
 
-		dbWarnings, err = tx.GetWarnings(filter)
-		if err != nil {
-			return err
-		}
-
+		dbWarnings, err := cluster.GetWarnings(ctx, tx.Tx(), filter)
 		if err != nil {
 			return fmt.Errorf("Failed to get warnings: %w", err)
+		}
+
+		warnings = make([]api.Warning, len(dbWarnings))
+		for i, w := range dbWarnings {
+			warning := w.ToAPI()
+			warning.EntityURL, err = getWarningEntityURL(ctx, tx.Tx(), &w)
+			if err != nil {
+				return err
+			}
+
+			warnings[i] = warning
 		}
 
 		return nil
 	})
 	if err != nil {
 		return response.SmartError(err)
-	}
-
-	warnings := make([]api.Warning, len(dbWarnings))
-
-	for i, w := range dbWarnings {
-		warning, err := w.ToAPI(d.db.Cluster)
-		if err != nil {
-			return response.SmartError(err)
-		}
-
-		warnings[i] = warning
 	}
 
 	if recursion == 0 {
@@ -258,21 +253,21 @@ func warningGet(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(err)
 	}
 
-	var dbWarning *db.Warning
-
+	var resp api.Warning
 	err = d.db.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		dbWarning, err = tx.GetWarning(id)
+		dbWarning, err := cluster.GetWarning(ctx, tx.Tx(), id)
+		if err != nil {
+			return err
+		}
+
+		resp := dbWarning.ToAPI()
+		resp.EntityURL, err = getWarningEntityURL(ctx, tx.Tx(), dbWarning)
 		if err != nil {
 			return err
 		}
 
 		return nil
 	})
-	if err != nil {
-		return response.SmartError(err)
-	}
-
-	resp, err := dbWarning.ToAPI(d.db.Cluster)
 	if err != nil {
 		return response.SmartError(err)
 	}
@@ -404,7 +399,7 @@ func warningDelete(d *Daemon, r *http.Request) response.Response {
 	}
 
 	err = d.db.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		err := tx.DeleteWarning(id)
+		err := cluster.DeleteWarning(ctx, tx.Tx(), id)
 		if err != nil {
 			return err
 		}
@@ -449,11 +444,11 @@ func pruneResolvedWarnings(ctx context.Context, d *Daemon) error {
 	err := d.db.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		// Retrieve warnings by resolved status.
 		statusResolved := warningtype.StatusResolved
-		filter := db.WarningFilter{
+		filter := cluster.WarningFilter{
 			Status: &statusResolved,
 		}
 
-		warnings, err := tx.GetWarnings(filter)
+		warnings, err := cluster.GetWarnings(ctx, tx.Tx(), filter)
 		if err != nil {
 			return fmt.Errorf("Failed to get resolved warnings: %w", err)
 		}
@@ -461,7 +456,7 @@ func pruneResolvedWarnings(ctx context.Context, d *Daemon) error {
 		for _, w := range warnings {
 			// Delete the warning if it has been resolved for at least 24 hours
 			if time.Since(w.UpdatedDate) >= 24*time.Hour {
-				err = tx.DeleteWarning(w.UUID)
+				err = cluster.DeleteWarning(ctx, tx.Tx(), w.UUID)
 				if err != nil {
 					return err
 				}

lxd/db/warnings.go
@@ -12,8 +12,6 @@ import (
 	"github.com/lxc/lxd/lxd/db/cluster"
 	"github.com/lxc/lxd/lxd/db/warningtype"
 	"github.com/lxc/lxd/shared"
-	"github.com/lxc/lxd/shared/api"
-	"github.com/lxc/lxd/shared/logger"
 )
 
 var warningCreate = cluster.RegisterStmt(`
@@ -61,7 +59,7 @@ func (c *Cluster) UpsertWarning(nodeName string, projectName string, entityTypeC
 	now := time.Now()
 
 	err = c.Transaction(context.TODO(), func(ctx context.Context, tx *ClusterTx) error {
-		filter := WarningFilter{
+		filter := cluster.WarningFilter{
 			TypeCode:       &typeCode,
 			Node:           &nodeName,
 			Project:        &projectName,
@@ -69,7 +67,7 @@ func (c *Cluster) UpsertWarning(nodeName string, projectName string, entityTypeC
 			EntityID:       &entityID,
 		}
 
-		warnings, err := tx.GetWarnings(filter)
+		warnings, err := cluster.GetWarnings(ctx, tx.tx, filter)
 		if err != nil {
 			return fmt.Errorf("Failed to retrieve warnings: %w", err)
 		}
@@ -88,7 +86,7 @@ func (c *Cluster) UpsertWarning(nodeName string, projectName string, entityTypeC
 
 			err = tx.UpdateWarningState(warnings[0].UUID, message, newStatus)
 		} else {
-			warning := Warning{
+			warning := cluster.Warning{
 				Node:           nodeName,
 				Project:        projectName,
 				EntityTypeCode: entityTypeCode,
@@ -103,7 +101,7 @@ func (c *Cluster) UpsertWarning(nodeName string, projectName string, entityTypeC
 				Count:          1,
 			}
 
-			_, err = tx.createWarning(warning)
+			_, err = tx.createWarning(ctx, warning)
 		}
 
 		if err != nil {
@@ -158,9 +156,9 @@ func (c *ClusterTx) UpdateWarningState(UUID string, message string, status warni
 }
 
 // createWarning adds a new warning to the database.
-func (c *ClusterTx) createWarning(object Warning) (int64, error) {
+func (c *ClusterTx) createWarning(ctx context.Context, object cluster.Warning) (int64, error) {
 	// Check if a warning with the same key exists.
-	exists, err := c.WarningExists(object.UUID)
+	exists, err := cluster.WarningExists(ctx, c.tx, object.UUID)
 	if err != nil {
 		return -1, fmt.Errorf("Failed to check for duplicates: %w", err)
 	}
@@ -228,30 +226,4 @@ func (c *ClusterTx) createWarning(object Warning) (int64, error) {
 	}
 
 	return id, nil
-}
-
-// ToAPI returns a LXD API entry.
-func (w Warning) ToAPI(c *Cluster) (api.Warning, error) {
-	typeCode := warningtype.Type(w.TypeCode)
-
-	entity, err := c.GetURIFromEntity(w.EntityTypeCode, w.EntityID)
-	if err != nil {
-		logger.Warn("Failed to get entity URI for warning", logger.Ctx{"ID": w.UUID, "entityID": w.EntityID, "entityTypeCode": w.EntityTypeCode, "err": err})
-	}
-
-	return api.Warning{
-		WarningPut: api.WarningPut{
-			Status: warningtype.Statuses[warningtype.Status(w.Status)],
-		},
-		UUID:        w.UUID,
-		Location:    w.Node,
-		Project:     w.Project,
-		Type:        warningtype.TypeNames[typeCode],
-		Count:       w.Count,
-		FirstSeenAt: w.FirstSeenDate,
-		LastSeenAt:  w.LastSeenDate,
-		LastMessage: w.LastMessage,
-		Severity:    warningtype.Severities[typeCode.Severity()],
-		EntityURL:   entity,
-	}, nil
 }

lxd/warnings/warnings.go
@@ -6,15 +6,16 @@ import (
 	"time"
 
 	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/lxd/db/cluster"
 	"github.com/lxc/lxd/lxd/db/warningtype"
 )
 
 // ResolveWarningsByLocalNodeOlderThan resolves all warnings which are older than the provided time.
-func ResolveWarningsByLocalNodeOlderThan(cluster *db.Cluster, date time.Time) error {
+func ResolveWarningsByLocalNodeOlderThan(dbCluster *db.Cluster, date time.Time) error {
 	var err error
 	var localName string
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		localName, err = tx.GetLocalNodeName()
 		if err != nil {
 			return err
@@ -30,8 +31,8 @@ func ResolveWarningsByLocalNodeOlderThan(cluster *db.Cluster, date time.Time) er
 		return fmt.Errorf("Local member name not available")
 	}
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		warnings, err := tx.GetWarnings(db.WarningFilter{})
+	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		warnings, err := cluster.GetWarnings(ctx, tx.Tx(), cluster.WarningFilter{})
 		if err != nil {
 			return err
 		}
@@ -60,11 +61,11 @@ func ResolveWarningsByLocalNodeOlderThan(cluster *db.Cluster, date time.Time) er
 
 // ResolveWarningsByLocalNodeAndType resolves warnings with the local member and type code.
 // Returns error if no local member name.
-func ResolveWarningsByLocalNodeAndType(cluster *db.Cluster, typeCode warningtype.Type) error {
+func ResolveWarningsByLocalNodeAndType(dbCluster *db.Cluster, typeCode warningtype.Type) error {
 	var err error
 	var localName string
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		localName, err = tx.GetLocalNodeName()
 		if err != nil {
 			return err
@@ -80,18 +81,18 @@ func ResolveWarningsByLocalNodeAndType(cluster *db.Cluster, typeCode warningtype
 		return fmt.Errorf("Local member name not available")
 	}
 
-	return ResolveWarningsByNodeAndType(cluster, localName, typeCode)
+	return ResolveWarningsByNodeAndType(dbCluster, localName, typeCode)
 }
 
 // ResolveWarningsByNodeAndType resolves warnings with the given node and type code.
-func ResolveWarningsByNodeAndType(cluster *db.Cluster, nodeName string, typeCode warningtype.Type) error {
-	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		filter := db.WarningFilter{
+func ResolveWarningsByNodeAndType(dbCluster *db.Cluster, nodeName string, typeCode warningtype.Type) error {
+	err := dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		filter := cluster.WarningFilter{
 			TypeCode: &typeCode,
 			Node:     &nodeName,
 		}
 
-		warnings, err := tx.GetWarnings(filter)
+		warnings, err := cluster.GetWarnings(ctx, tx.Tx(), filter)
 		if err != nil {
 			return err
 		}
@@ -113,15 +114,15 @@ func ResolveWarningsByNodeAndType(cluster *db.Cluster, nodeName string, typeCode
 }
 
 // ResolveWarningsByNodeAndProjectAndType resolves warnings with the given node, project and type code.
-func ResolveWarningsByNodeAndProjectAndType(cluster *db.Cluster, nodeName string, projectName string, typeCode warningtype.Type) error {
-	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		filter := db.WarningFilter{
+func ResolveWarningsByNodeAndProjectAndType(dbCluster *db.Cluster, nodeName string, projectName string, typeCode warningtype.Type) error {
+	err := dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		filter := cluster.WarningFilter{
 			TypeCode: &typeCode,
 			Node:     &nodeName,
 			Project:  &projectName,
 		}
 
-		warnings, err := tx.GetWarnings(filter)
+		warnings, err := cluster.GetWarnings(ctx, tx.Tx(), filter)
 		if err != nil {
 			return err
 		}
@@ -143,11 +144,11 @@ func ResolveWarningsByNodeAndProjectAndType(cluster *db.Cluster, nodeName string
 }
 
 // ResolveWarningsByLocalNodeAndProjectAndType resolves warnings with the given project and type code.
-func ResolveWarningsByLocalNodeAndProjectAndType(cluster *db.Cluster, projectName string, typeCode warningtype.Type) error {
+func ResolveWarningsByLocalNodeAndProjectAndType(dbCluster *db.Cluster, projectName string, typeCode warningtype.Type) error {
 	var err error
 	var localName string
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		localName, err = tx.GetLocalNodeName()
 		if err != nil {
 			return err
@@ -163,13 +164,13 @@ func ResolveWarningsByLocalNodeAndProjectAndType(cluster *db.Cluster, projectNam
 		return fmt.Errorf("Local member name not available")
 	}
 
-	return ResolveWarningsByNodeAndProjectAndType(cluster, localName, projectName, typeCode)
+	return ResolveWarningsByNodeAndProjectAndType(dbCluster, localName, projectName, typeCode)
 }
 
 // ResolveWarningsByNodeAndProjectAndTypeAndEntity resolves warnings with the given node, project, type code, and entity.
-func ResolveWarningsByNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, nodeName string, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
-	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		filter := db.WarningFilter{
+func ResolveWarningsByNodeAndProjectAndTypeAndEntity(dbCluster *db.Cluster, nodeName string, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
+	err := dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		filter := cluster.WarningFilter{
 			TypeCode:       &typeCode,
 			Node:           &nodeName,
 			Project:        &projectName,
@@ -177,7 +178,7 @@ func ResolveWarningsByNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, nodeNa
 			EntityID:       &entityID,
 		}
 
-		warnings, err := tx.GetWarnings(filter)
+		warnings, err := cluster.GetWarnings(ctx, tx.Tx(), filter)
 		if err != nil {
 			return err
 		}
@@ -199,11 +200,11 @@ func ResolveWarningsByNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, nodeNa
 }
 
 // ResolveWarningsByLocalNodeAndProjectAndTypeAndEntity resolves warnings with the given project, type code, and entity.
-func ResolveWarningsByLocalNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
+func ResolveWarningsByLocalNodeAndProjectAndTypeAndEntity(dbCluster *db.Cluster, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
 	var err error
 	var localName string
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		localName, err = tx.GetLocalNodeName()
 		if err != nil {
 			return err
@@ -219,13 +220,13 @@ func ResolveWarningsByLocalNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, p
 		return fmt.Errorf("Local member name not available")
 	}
 
-	return ResolveWarningsByNodeAndProjectAndTypeAndEntity(cluster, localName, projectName, typeCode, entityTypeCode, entityID)
+	return ResolveWarningsByNodeAndProjectAndTypeAndEntity(dbCluster, localName, projectName, typeCode, entityTypeCode, entityID)
 }
 
 // DeleteWarningsByNodeAndProjectAndTypeAndEntity deletes warnings with the given node, project, type code, and entity.
-func DeleteWarningsByNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, nodeName string, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
-	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		filter := db.WarningFilter{
+func DeleteWarningsByNodeAndProjectAndTypeAndEntity(dbCluster *db.Cluster, nodeName string, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
+	err := dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		filter := cluster.WarningFilter{
 			TypeCode:       &typeCode,
 			Node:           &nodeName,
 			Project:        &projectName,
@@ -233,13 +234,13 @@ func DeleteWarningsByNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, nodeNam
 			EntityID:       &entityID,
 		}
 
-		warnings, err := tx.GetWarnings(filter)
+		warnings, err := cluster.GetWarnings(ctx, tx.Tx(), filter)
 		if err != nil {
 			return err
 		}
 
 		for _, w := range warnings {
-			err = tx.DeleteWarning(w.UUID)
+			err = cluster.DeleteWarning(ctx, tx.Tx(), w.UUID)
 			if err != nil {
 				return err
 			}
@@ -255,11 +256,11 @@ func DeleteWarningsByNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, nodeNam
 }
 
 // DeleteWarningsByLocalNodeAndProjectAndTypeAndEntity resolves warnings with the given project, type code, and entity.
-func DeleteWarningsByLocalNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
+func DeleteWarningsByLocalNodeAndProjectAndTypeAndEntity(dbCluster *db.Cluster, projectName string, typeCode warningtype.Type, entityTypeCode int, entityID int) error {
 	var err error
 	var localName string
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		localName, err = tx.GetLocalNodeName()
 		if err != nil {
 			return err
@@ -275,5 +276,5 @@ func DeleteWarningsByLocalNodeAndProjectAndTypeAndEntity(cluster *db.Cluster, pr
 		return fmt.Errorf("Local member name not available")
 	}
 
-	return DeleteWarningsByNodeAndProjectAndTypeAndEntity(cluster, localName, projectName, typeCode, entityTypeCode, entityID)
+	return DeleteWarningsByNodeAndProjectAndTypeAndEntity(dbCluster, localName, projectName, typeCode, entityTypeCode, entityID)
 }

lxd/cgroup/init.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/lxd/db/cluster"
 	"github.com/lxc/lxd/lxd/db/warningtype"
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/logger"
@@ -244,88 +244,88 @@ func (info *Info) Supports(resource Resource, cgroup *CGroup) bool {
 }
 
 // Warnings returns a list of CGroup warnings.
-func (info *Info) Warnings() []db.Warning {
-	warnings := []db.Warning{}
+func (info *Info) Warnings() []cluster.Warning {
+	warnings := []cluster.Warning{}
 
 	if !info.Supports(Blkio, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupBlkio,
 			LastMessage: "disk I/O limits will be ignored",
 		})
 	}
 
 	if !info.Supports(BlkioWeight, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupBlkioWeight,
 			LastMessage: "disk priority will be ignored",
 		})
 	}
 
 	if !info.Supports(CPU, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupCPUController,
 			LastMessage: "CPU time limits will be ignored",
 		})
 	}
 
 	if !info.Supports(CPUAcct, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupCPUacctController,
 			LastMessage: "CPU accounting will not be available",
 		})
 	}
 
 	if !info.Supports(CPUSet, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupCPUController,
 			LastMessage: "CPU pinning will be ignored",
 		})
 	}
 
 	if !info.Supports(Devices, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupDevicesController,
 			LastMessage: "device access control won't work",
 		})
 	}
 
 	if !info.Supports(Freezer, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupFreezerController,
 			LastMessage: "pausing/resuming containers won't work",
 		})
 	}
 
 	if !info.Supports(Hugetlb, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupHugetlbController,
 			LastMessage: "hugepage limits will be ignored",
 		})
 	}
 
 	if !info.Supports(Memory, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupMemoryController,
 			LastMessage: "memory limits will be ignored",
 		})
 	}
 
 	if !info.Supports(NetPrio, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupNetworkPriorityController,
 			LastMessage: "network priority will be ignored",
 		})
 	}
 
 	if !info.Supports(Pids, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupPidsController,
 			LastMessage: "process limits will be ignored",
 		})
 	}
 
 	if !info.Supports(MemorySwap, nil) {
-		warnings = append(warnings, db.Warning{
+		warnings = append(warnings, cluster.Warning{
 			TypeCode:    warningtype.MissingCGroupMemorySwapAccounting,
 			LastMessage: "swap limits will be ignored",
 		})

lxd/daemon.go
@@ -770,7 +770,7 @@ func (d *Daemon) Init() error {
 }
 
 func (d *Daemon) init() error {
-	var dbWarnings []db.Warning
+	var dbWarnings []clusterDB.Warning
 
 	// Setup logger
 	events.LoggingServer = d.events

lxd/instance/drivers/driver_common.go
@@ -897,7 +897,7 @@ func (d *common) onStopOperationSetup(target string) (*operationlock.InstanceOpe
 // warningsDelete deletes any persistent warnings for the instance.
 func (d *common) warningsDelete() error {
 	err := d.state.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		return tx.DeleteWarnings(dbCluster.TypeInstance, d.ID())
+		return dbCluster.DeleteWarnings(ctx, tx.Tx(), dbCluster.TypeInstance, d.ID())
 	})
 	if err != nil {
 		return fmt.Errorf("Failed deleting persistent warnings: %w", err)

lxd/instance/drivers/load.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/lxc/lxd/lxd/db"
+	"github.com/lxc/lxd/lxd/db/cluster"
 	"github.com/lxc/lxd/lxd/db/warningtype"
 	"github.com/lxc/lxd/lxd/device"
 	deviceConfig "github.com/lxc/lxd/lxd/device/config"
@@ -26,7 +27,7 @@ var instanceDrivers = map[string]func() instance.Instance{
 // DriverStatus definition.
 type DriverStatus struct {
 	Info      instance.Info
-	Warning   *db.Warning
+	Warning   *cluster.Warning
 	Supported bool
 }
 
@@ -144,7 +145,7 @@ func DriverStatuses() map[instancetype.Type]*DriverStatus {
 			logger.Warn("Instance type not operational", logger.Ctx{"type": driverInfo.Type, "driver": driverInfo.Name, "err": driverInfo.Error})
 
 			driverStatus.Supported = false
-			driverStatus.Warning = &db.Warning{
+			driverStatus.Warning = &cluster.Warning{
 				TypeCode:    warningtype.InstanceTypeNotOperational,
 				LastMessage: fmt.Sprintf("%v", driverInfo.Error),
 			}

lxd/network/driver_common.go
@@ -429,7 +429,7 @@ func (n *common) rename(newName string) error {
 // warningsDelete deletes any persistent warnings for the network.
 func (n *common) warningsDelete() error {
 	err := n.state.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		return tx.DeleteWarnings(dbCluster.TypeNetwork, int(n.ID()))
+		return dbCluster.DeleteWarnings(ctx, tx.Tx(), dbCluster.TypeNetwork, int(n.ID()))
 	})
 	if err != nil {
 		return fmt.Errorf("Failed deleting persistent warnings: %w", err)

commit 39a9d7bcba2035763473f86ec21090fcdf8b3f28

lxd/warnings: Fix `lxc warning show`
Broken since f041bb099b16983a09b3efb20052e4275e71d8d8

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/warnings.go
@@ -260,7 +260,8 @@ func warningGet(d *Daemon, r *http.Request) response.Response {
 			return err
 		}
 
-		resp := dbWarning.ToAPI()
+		resp = dbWarning.ToAPI()
+
 		resp.EntityURL, err = getWarningEntityURL(ctx, tx.Tx(), dbWarning)
 		if err != nil {
 			return err