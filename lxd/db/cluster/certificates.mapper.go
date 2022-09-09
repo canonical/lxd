beaa8929b3ded08e8cc6de0335921d8f781ac88c
Max Asnaashari <max.asnaashari@canonical.com>
2022-09-09 07:39:41 +0000

    lxd/db/cluster/mapper: Update generated code
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/db/cluster/certificates.mapper.go
+++ lxd/db/cluster/certificates.mapper.go
@@ -7,6 +7,7 @@ package cluster
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -194,31 +195,20 @@ func GetCertificateID(ctx context.Context, tx *sql.Tx, fingerprint string) (int6
 		return -1, fmt.Errorf("Failed to get \"certificateID\" prepared statement: %w", err)
 	}
 
-	rows, err := stmt.Query(fingerprint)
+	row := stmt.QueryRowContext(ctx, fingerprint)
+	err = row.Err()
 	if err != nil {
 		return -1, fmt.Errorf("Failed to get \"certificates\" ID: %w", err)
 	}
 
-	defer func() { _ = rows.Close() }()
-
-	// Ensure we read one and only one row.
-	if !rows.Next() {
+	var id int64
+	err = row.Scan(&id)
+	if errors.Is(err, sql.ErrNoRows) {
 		return -1, api.StatusErrorf(http.StatusNotFound, "Certificate not found")
 	}
 
-	var id int64
-	err = rows.Scan(&id)
 	if err != nil {
 		return -1, fmt.Errorf("Failed to scan ID: %w", err)
-	}
-
-	if rows.Next() {
-		return -1, fmt.Errorf("More than one row returned")
-	}
-
-	err = rows.Err()
-	if err != nil {
-		return -1, fmt.Errorf("Result set failure: %w", err)
 	}
 
 	return id, nil
