b01f8e6a1bf398c48bee67b16e7883683bbd56bf
Thomas Parrott <thomas.parrott@canonical.com>
2022-10-18 16:12:36 +0100

    lxd/db/storage/volumes: Updates GetCustomVolumesInProject to populate creation date
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/db/storage_volumes.go
+++ lxd/db/storage_volumes.go
@@ -899,7 +899,12 @@ func storagePoolVolumeContentTypeToName(contentType int) (string, error) {
 // GetCustomVolumesInProject returns all custom volumes in the given project.
 func (c *ClusterTx) GetCustomVolumesInProject(ctx context.Context, project string) ([]StorageVolumeArgs, error) {
 	sql := `
-SELECT storage_volumes.id, storage_volumes.name, storage_pools.name, IFNULL(storage_volumes.node_id, -1)
+SELECT
+	storage_volumes.id,
+	storage_volumes.name,
+	storage_volumes.creation_date,
+	storage_pools.name,
+	IFNULL(storage_volumes.node_id, -1)
 FROM storage_volumes
 JOIN storage_pools ON storage_pools.id = storage_volumes.storage_pool_id
 JOIN projects ON projects.id = storage_volumes.project_id
@@ -909,7 +914,7 @@ WHERE storage_volumes.type = ? AND projects.name = ?
 	volumes := []StorageVolumeArgs{}
 	err := query.Scan(ctx, c.tx, sql, func(scan func(dest ...any) error) error {
 		volume := StorageVolumeArgs{}
-		err := scan(&volume.ID, &volume.Name, &volume.PoolName, &volume.NodeID)
+		err := scan(&volume.ID, &volume.Name, &volume.CreationDate, &volume.PoolName, &volume.NodeID)
 		if err != nil {
 			return err
 		}
