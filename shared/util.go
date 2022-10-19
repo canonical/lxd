abf02da723bc40e7da2173927e711bd0efaff18e
Thomas Hipp <thomas.hipp@canonical.com>
2022-10-19 17:21:07 +0200

    shared/util: Allow seconds in GetExpiry
    This adds support for seconds in GetExpiry. Although this most likely
    won't be used very often, it is benefitial for testing as we won't have
    to wait for 60 seconds.
    
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- shared/util.go
+++ shared/util.go
@@ -1232,7 +1232,7 @@ func RenderTemplate(template string, ctx pongo2.Context) (string, error) {
 }
 
 // GetExpiry returns the expiry date based on the reference date and a length of time.
-// The length of time format is "<integer>(M|H|d|w|m|y)", and can contain multiple such fields, e.g.
+// The length of time format is "<integer>(S|M|H|d|w|m|y)", and can contain multiple such fields, e.g.
 // "1d 3H" (1 day and 3 hours).
 func GetExpiry(refDate time.Time, s string) (time.Time, error) {
 	expr := strings.TrimSpace(s)
@@ -1241,8 +1241,9 @@ func GetExpiry(refDate time.Time, s string) (time.Time, error) {
 		return time.Time{}, nil
 	}
 
-	re := regexp.MustCompile(`^(\d+)(M|H|d|w|m|y)$`)
+	re := regexp.MustCompile(`^(\d+)(S|M|H|d|w|m|y)$`)
 	expiry := map[string]int{
+		"S": 0,
 		"M": 0,
 		"H": 0,
 		"d": 0,
@@ -1277,7 +1278,7 @@ func GetExpiry(refDate time.Time, s string) (time.Time, error) {
 	}
 
 	t := refDate.AddDate(expiry["y"], expiry["m"], expiry["d"]+expiry["w"]*7).Add(
-		time.Hour*time.Duration(expiry["H"]) + time.Minute*time.Duration(expiry["M"]))
+		time.Hour*time.Duration(expiry["H"]) + time.Minute*time.Duration(expiry["M"]) + time.Second*time.Duration(expiry["S"]))
 
 	return t, nil
 }
