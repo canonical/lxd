f608deace695f14c92ecb73034758bd771be995f
Thomas Hipp <thomas.hipp@canonical.com>
2022-10-20 13:36:23 +0200

    lxd: Remove auto removal of cluster join tokens
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -1575,9 +1575,6 @@ func (d *Daemon) startClusterTasks() {
 	// Remove orphaned operations
 	d.clusterTasks.Add(autoRemoveOrphanedOperationsTask(d))
 
-	// Remove expired cluster join tokens
-	d.clusterTasks.Add(autoRemoveExpiredClusterJoinTokensTask(d))
-
 	// Start all background tasks
 	d.clusterTasks.Start(d.shutdownCtx)
 }
