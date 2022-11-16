canonical/lxd

commit 897a481be9738ccc1e3a0ab3c736ed02e4de05f6

lxd/device/disk: Use rbd syntax for rbd volumes
This uses the rbd syntax instead of mounting the disk, which can then be
used to add the disk using the QEMU rbd driver.

Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

lxd/device/disk.go
@@ -712,21 +712,10 @@ func (d *disk) startVM() (*deviceConfig.RunConfig, error) {
 			// Get the pool and volume names.
 			fields := strings.SplitN(d.config["source"], ":", 2)
 			fields = strings.SplitN(fields[1], "/", 2)
-			poolName := fields[0]
-			volumeName := fields[1]
 			clusterName, userName := d.cephCreds()
-
-			// Configuration values containing :, @, or = can be escaped with a leading \ character.
-			// According to https://docs.ceph.com/docs/hammer/rbd/qemu-rbd/#usage
-			optEscaper := strings.NewReplacer(":", `\:`, "@", `\@`, "=", `\=`)
-			opts := []string{
-				fmt.Sprintf("id=%s", optEscaper.Replace(userName)),
-				fmt.Sprintf("conf=/etc/ceph/%s.conf", optEscaper.Replace(clusterName)),
-			}
-
 			runConf.Mounts = []deviceConfig.MountEntryItem{
 				{
-					DevPath: fmt.Sprintf("rbd:%s/%s:%s", optEscaper.Replace(poolName), optEscaper.Replace(volumeName), strings.Join(opts, ":")),
+					DevPath: DiskGetRBDFormat(clusterName, userName, fields[0], fields[1]),
 					DevName: d.name,
 				},
 			}
@@ -744,6 +733,20 @@ func (d *disk) startVM() (*deviceConfig.RunConfig, error) {
 			// be returned as the path to the block device).
 			if d.config["pool"] != "" {
 				var revertFunc func()
+
+				// If the pool is ceph backed, don't mount it, instead pass config to QEMU instance
+				// to use the built in RBD support.
+				if d.pool.Driver().Info().Remote {
+					clusterName, userName := d.cephCreds()
+					runConf.Mounts = []deviceConfig.MountEntryItem{
+						{
+							DevPath: DiskGetRBDFormat(clusterName, userName, d.pool.ToAPI().Config["ceph.osd.pool_name"], d.config["source"]),
+							DevName: d.name,
+						},
+					}
+
+					return &runConf, nil
+				}
 
 				revertFunc, mount.DevPath, err = d.mountPoolVolume()
 				if err != nil {

commit 5f4160bb017283ae71539c15b93f2dcd406aac55

lxd/device/disk: Fix attaching cephfs volumes to VMs
Fixes regression caused by 897a481be9738ccc1e3a0ab3c736ed02e4de05f6

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/device/disk.go
@@ -728,7 +728,7 @@ func (d *disk) startVM() (*deviceConfig.RunConfig, error) {
 
 				// If the pool is ceph backed, don't mount it, instead pass config to QEMU instance
 				// to use the built in RBD support.
-				if d.pool.Driver().Info().Remote {
+				if d.pool.Driver().Info().Name == "ceph" {
 					config := d.pool.ToAPI().Config
 					poolName := config["ceph.osd.pool_name"]
 