41c0c0dee57c75eef7fc1236d7342be0779e7995
Thomas Parrott <thomas.parrott@canonical.com>
2022-11-24 15:19:34 +0000

    lxd/device/device/utils/unix/events: Only call filepath.Clean once in unixRegisterHandler
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/device_utils_unix_events.go
+++ lxd/device/device_utils_unix_events.go
@@ -49,14 +49,16 @@ func unixRegisterHandler(s *state.State, inst instance.Instance, deviceName, pat
 
 	identifier := fmt.Sprintf("%d_%s", inst.ID(), deviceName)
 
+	path = filepath.Clean(path)
+
 	// Add inotify watcher to its nearest existing ancestor.
-	err := s.DevMonitor.Watch(filepath.Clean(path), identifier, func(path, event string) bool {
+	err := s.DevMonitor.Watch(path, identifier, func(path, event string) bool {
 		e := unixNewEvent(event, path)
 		unixRunHandlers(s, &e)
 		return true
 	})
 	if err != nil {
-		return fmt.Errorf("Failed to add %q to watch targets: %w", filepath.Clean(path), err)
+		return fmt.Errorf("Failed to add %q to watch targets: %w", path, err)
 	}
 
 	logger.Debugf("Added %q to watch targets", filepath.Clean(path))
