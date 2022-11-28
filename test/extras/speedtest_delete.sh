f0b2dc0a7b101cd81a57cb3858c9abccf8fc871b
Simon Deziel <simon.deziel@canonical.com>
2022-11-28 15:15:43 -0500

    test/extras: shellcheck fixes
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/extras/speedtest_delete.sh
+++ test/extras/speedtest_delete.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-MYDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-CIMAGE="testimage"
 CNAME="speedtest"
 
 count=${1}
-if [ "x${count}" == "x" ]; then
+if [ -z "${count}" ]; then
   echo "USAGE: ${0} 10"
   echo "This deletes 10 busybox containers"
   exit 1
 fi
 
-if [ "x${2}" != "xnotime" ]; then
-  time ${0} ${count} notime
+if [ "${2}" != "notime" ]; then
+  time ${0} "${count}" notime
   exit 0
 fi
 
 PIDS=""
-for c in $(seq 1 $count); do
+for c in $(seq "$count"); do
   lxc delete "${CNAME}${c}" 2>&1 &
   PIDS="$PIDS $!"
 done
 
 for pid in $PIDS; do
-  wait $pid
+  wait "$pid"
 done
 
 echo -e "\nRun completed"
