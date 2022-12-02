canonical/lxd

commit 55230a54dfbfc169d5c5d544b6b9a8242d600822

lxc/query: Add support for non-JSON endpoints
This should handle the file and image endpoints that don't return JSON
by attempting to perform a direct http query when a LXD API query fails.

Closes: #4452

Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

lxc/query.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/spf13/cobra"
 
@@ -80,7 +83,47 @@ func (c *cmdQuery) Run(cmd *cobra.Command, args []string) error {
 	// Perform the query
 	resp, _, err := d.RawQuery(c.flagAction, path, data, "")
 	if err != nil {
-		return err
+		cleanErr := err
+
+		// Lets assume the endpoint is raw output
+		// Get a raw http client
+		httpClient, err := d.GetHTTPClient()
+		if err != nil {
+			return err
+		}
+
+		// Get the URL prefix
+		httpInfo, err := d.GetConnectionInfo()
+		if err != nil {
+			return err
+		}
+
+		// Setup the request
+		req, err := http.NewRequest(c.flagAction, fmt.Sprintf("%s%s", httpInfo.URL, path), bytes.NewReader([]byte(c.flagData)))
+		if err != nil {
+			return err
+		}
+
+		// Set the encoding accordingly
+		req.Header.Set("Content-Type", "plain/text")
+
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			return err
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return cleanErr
+		}
+
+		content, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+
+		fmt.Print(string(content))
+
+		return nil
 	}
 
 	if c.flagRespWait && resp.Operation != "" {

commit be6b5390dce50ab3bfb6d8ce9eb7ef3f96c8fd44

lxd/query: Only repeat query as plain request for JSON decode errors in cmdQuery
Otherwise any failed request is tried again.

Issue was introduced with 55230a54dfbfc169d5c5d544b6b9a8242d600822 in an attempt to
handle non-JSON endpoints.

Now only retry if the error was specifically related to a failure to decode JSON.

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxc/query.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -98,6 +99,14 @@ func (c *cmdQuery) Run(cmd *cobra.Command, args []string) error {
 	// Perform the query
 	resp, _, err := d.RawQuery(c.flagAction, path, data, "")
 	if err != nil {
+		var jsonSyntaxError *json.SyntaxError
+
+		// If not JSON decoding error then fail immediately.
+		if !errors.As(err, &jsonSyntaxError) {
+			return err
+		}
+
+		// If JSON decoding error then try a plain request.
 		cleanErr := err
 
 		// Get the URL prefix