032bb45ea9c0017f8f8c1a6774b6234cf4fcffbf
Simon Richardson <stickupkid@gmail.com>
2023-02-17 16:54:39 +0000

    Move socket to linux
    To better describe the package, rename socket to linux. This is because
    the package contained more than just socket operations.
    
    Signed-off-by: Simon Richardson <stickupkid@gmail.com>

--- lxd/rsync/rsync.go
+++ lxd/rsync/rsync.go
@@ -13,8 +13,8 @@ import (
 
 	"github.com/lxc/lxd/shared"
 	"github.com/lxc/lxd/shared/ioprogress"
+	"github.com/lxc/lxd/shared/linux"
 	"github.com/lxc/lxd/shared/logger"
-	"github.com/lxc/lxd/shared/socket"
 	"github.com/lxc/lxd/shared/version"
 )
 
@@ -99,8 +99,8 @@ func sendSetup(name string, path string, bwlimit string, execPath string, featur
 	auds := fmt.Sprintf("@lxd/%s", uuid.New())
 	// We simply copy a part of the uuid if it's longer than the allowed
 	// maximum. That should be safe enough for our purposes.
-	if len(auds) > socket.ABSTRACT_UNIX_SOCK_LEN-1 {
-		auds = auds[:socket.ABSTRACT_UNIX_SOCK_LEN-1]
+	if len(auds) > linux.ABSTRACT_UNIX_SOCK_LEN-1 {
+		auds = auds[:linux.ABSTRACT_UNIX_SOCK_LEN-1]
 	}
 
 	l, err := net.Listen("unix", auds)
