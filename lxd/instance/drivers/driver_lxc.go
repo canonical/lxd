2e85f5b98fdce9205e2b629c29e83737ef188122
Thomas Parrott <thomas.parrott@canonical.com>
2023-02-20 13:06:38 +0000

    lxd/instance/drivers/driver/lxc: Only intialise cgroup reader/writer when needed
    Relies on liblxc reference already being initialised.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_lxc.go
+++ lxd/instance/drivers/driver_lxc.go
@@ -4176,11 +4176,6 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 		}
 	}
 
-	cg, err := d.cgroup(nil)
-	if err != nil {
-		return err
-	}
-
 	// If raw.lxc changed, re-validate the config.
 	if shared.StringInSlice("raw.lxc", changedConfig) && d.expandedConfig["raw.lxc"] != "" {
 		// Get a new liblxc instance.
@@ -4271,6 +4266,11 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 
 	// Apply the live changes
 	if isRunning {
+		cg, err := d.cgroup(nil)
+		if err != nil {
+			return err
+		}
+
 		// Live update the container config
 		for _, key := range changedConfig {
 			value := d.expandedConfig[key]
