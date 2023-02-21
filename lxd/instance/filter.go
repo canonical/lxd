849d280f563fc83bc8e34acaa64a1ce278dc5585
Thomas Parrott <thomas.parrott@canonical.com>
2023-02-21 13:52:16 +0000

    lxd/instance/filter: Removes unused function Filter
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/filter.go
+++ lxd/instance/filter.go
@@ -5,20 +5,6 @@ import (
 	"github.com/lxc/lxd/shared/api"
 )
 
-// Filter returns a filtered list of instances that match the given clauses.
-func Filter(instances []*api.Instance, clauses []filter.Clause) []*api.Instance {
-	filtered := []*api.Instance{}
-	for _, instance := range instances {
-		if !filter.Match(*instance, clauses) {
-			continue
-		}
-
-		filtered = append(filtered, instance)
-	}
-
-	return filtered
-}
-
 // FilterFull returns a filtered list of full instances that match the given clauses.
 func FilterFull(instances []*api.InstanceFull, clauses []filter.Clause) []*api.InstanceFull {
 	filtered := []*api.InstanceFull{}
