bc4a7403325812a361f286fea108d4609a78c133
Thomas Parrott <thomas.parrott@canonical.com>
2023-02-24 09:18:11 +0000

    lxd/ip/link/veth: Restructure Veth type to replace PeerName with Peer Link field
    This allows setting multiple peer link properties atomically at Add time.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/ip/link_veth.go
+++ lxd/ip/link_veth.go
@@ -3,20 +3,10 @@ package ip
 // Veth represents arguments for link of type veth.
 type Veth struct {
 	Link
-	PeerName string
-}
-
-// additionalArgs generates veth specific arguments.
-func (veth *Veth) additionalArgs() []string {
-	args := []string{}
-	if veth.PeerName != "" {
-		args = append(args, "peer", "name", veth.PeerName)
-	}
-
-	return args
+	Peer Link
 }
 
 // Add adds new virtual link.
 func (veth *Veth) Add() error {
-	return veth.Link.add("veth", veth.additionalArgs())
+	return veth.Link.add("veth", append([]string{"peer"}, veth.Peer.args()...))
 }
