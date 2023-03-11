fe83e34c8c6e152761d98b1f2c53159ecf41ff70
Thomas Parrott <thomas.parrott@canonical.com>
2023-03-11 14:31:47 +0000

    lxd/instance/drivers: Update comments for accuracy
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -5119,14 +5119,13 @@ func (d *qemu) delete(force bool) error {
 				return err
 			}
 		} else {
-			// Remove all snapshots by initialising each snapshot as an Instance and
-			// calling its Delete function.
+			// Remove all snapshots.
 			err := instance.DeleteSnapshots(d)
 			if err != nil {
-				return err
+				return fmt.Errorf("Failed deleting instance snapshots; %w", err)
 			}
 
-			// Remove the storage volume, snapshot volumes and database records.
+			// Remove the storage volume and database records.
 			err = pool.DeleteInstance(d, nil)
 			if err != nil {
 				return err
