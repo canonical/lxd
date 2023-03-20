canonical/lxd

commit 0fa1252f20ac12065afe8c68a7719b7448504b41

lxd/storage/drivers/driver/ceph/volumes: Updates CreateVolumeSnapshot to use filesystemFreeze
This is more targetted than syncing the whole system.

Also fail if filesystem syncing/freezing doesn't succeed.

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/storage/drivers/driver_ceph_volumes.go
@@ -1469,12 +1469,12 @@ func (d *ceph) CreateVolumeSnapshot(snapVol Volume, op *operations.Operation) er
 		// been committed to disk. If we don't then the rbd snapshot of
 		// the underlying filesystem can be inconsistent or - worst case
 		// - empty.
-		unix.Sync()
-
-		_, err := shared.TryRunCommand("fsfreeze", "--freeze", sourcePath)
-		if err == nil {
-			defer func() { _, _ = shared.TryRunCommand("fsfreeze", "--unfreeze", sourcePath) }()
+		unfreezeFS, err := d.filesystemFreeze(sourcePath)
+		if err != nil {
+			return err
 		}
+
+		defer func() { _ = unfreezeFS() }()
 	}
 
 	// Create the parent directory.

commit 156a8656be3ab114e4630e529ed7c69cc8c6ac55

lxd/storage/drivers/driver/ceph/volumes: Don't fail CreateVolumeSnapshot when freezing filesystem not possible
Still attempt it, as this will also try doing a `sync` call, but if the filesystem is busy, go on with he snapshot.

Fixes VM snapshots on ceph caused by 0fa1252f20ac12065afe8c68a7719b7448504b41.

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/storage/drivers/driver_ceph_volumes.go
@@ -1465,16 +1465,14 @@ func (d *ceph) CreateVolumeSnapshot(snapVol Volume, op *operations.Operation) er
 	snapshotName := fmt.Sprintf("snapshot_%s", snapshotOnlyName)
 
 	if filesystem.IsMountPoint(sourcePath) {
-		// This is costly but we need to ensure that all cached data has
-		// been committed to disk. If we don't then the rbd snapshot of
-		// the underlying filesystem can be inconsistent or - worst case
-		// - empty.
+		// Attempt to sync and freeze filesystem, but do not error if not able to freeze (as filesystem
+		// could still be busy), as we do not guarantee the consistency of a snapshot. This is costly but
+		// try to ensure that all cached data has been committed to disk. If we don't then the rbd snapshot
+		// of the underlying filesystem can be inconsistent or, in the worst case, empty.
 		unfreezeFS, err := d.filesystemFreeze(sourcePath)
-		if err != nil {
-			return err
+		if err == nil {
+			defer func() { _ = unfreezeFS() }()
 		}
-
-		defer func() { _ = unfreezeFS() }()
 	}
 
 	// Create the parent directory.