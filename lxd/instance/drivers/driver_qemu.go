canonical/lxd

commit 384114482c8807dcf19b3394277f6050b627401f

lxd/instance/qemu: add VM Generation ID to QEMU config
Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

lxd/instance/drivers/driver_qemu.go
@@ -1302,11 +1302,6 @@ func (d *qemu) start(stateful bool, op *operationlock.InstanceOperation) error {
 		}
 	}
 
-	// VM generation ID is only available on x86.
-	if d.architecture == osarch.ARCH_64BIT_INTEL_X86 {
-		qemuCmd = append(qemuCmd, "-device", fmt.Sprintf("vmgenid,guid=%s", d.localConfig["volatile.uuid.generation"]))
-	}
-
 	// SMBIOS only on x86_64 and aarch64.
 	if d.architectureSupportsUEFI(d.architecture) {
 		qemuCmd = append(qemuCmd, "-smbios", "type=2,manufacturer=Canonical Ltd.,product=LXD")
@@ -2854,6 +2849,14 @@ func (d *qemu) generateQemuConfigFile(mountInfo *storagePools.MountInfo, busName
 				return "", nil, err
 			}
 		}
+
+		// VM generation ID is only available on x86.
+		if d.architecture == osarch.ARCH_64BIT_INTEL_X86 {
+			err = d.addVmgenDeviceConfig(&cfg, d.localConfig["volatile.uuid.generation"])
+			if err != nil {
+				return "", nil, err
+			}
+		}
 	}
 
 	// Allocate 4 PCI slots for hotplug devices.
@@ -3905,6 +3908,16 @@ func (d *qemu) addTPMDeviceConfig(cfg *[]cfgSection, tpmConfig []deviceConfig.Ru
 		path:    socketPath,
 	}
 	*cfg = append(*cfg, qemuTPM(&tpmOpts)...)
+
+	return nil
+}
+
+func (d *qemu) addVmgenDeviceConfig(cfg *[]cfgSection, guid string) error {
+	qemuVmgenOpts := qemuVmgenOpts{
+		driver: "vmgenid",
+		guid:   guid,
+	}
+	*cfg = append(*cfg, qemuVmgen(&qemuVmgenOpts)...)
 
 	return nil
 }

commit 7ca0b08f8618fb0ca0fafffa3cf5f8f50d202952

lxd/instance/drivers/driver/qemu: Fix VM start with vmgenid
Fixes issue caused by 384114482c8807dcf19b3394277f6050b627401f

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/instance/drivers/driver_qemu.go
@@ -2849,13 +2849,13 @@ func (d *qemu) generateQemuConfigFile(mountInfo *storagePools.MountInfo, busName
 				return "", nil, err
 			}
 		}
+	}
 
-		// VM generation ID is only available on x86.
-		if d.architecture == osarch.ARCH_64BIT_INTEL_X86 {
-			err = d.addVmgenDeviceConfig(&cfg, d.localConfig["volatile.uuid.generation"])
-			if err != nil {
-				return "", nil, err
-			}
+	// VM generation ID is only available on x86.
+	if d.architecture == osarch.ARCH_64BIT_INTEL_X86 {
+		err = d.addVmgenDeviceConfig(&cfg, d.localConfig["volatile.uuid.generation"])
+		if err != nil {
+			return "", nil, err
 		}
 	}
 