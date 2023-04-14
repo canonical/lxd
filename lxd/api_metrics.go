b04323250ad1b5e6cf2a573007cdde1408cd71c5
Stéphane Graber <stgraber@ubuntu.com>
2023-04-14 00:08:58 -0400

    lxd/metrics: Add compression support
    Closes #11441
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd/api_metrics.go
+++ lxd/api_metrics.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"net/http"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 
@@ -81,6 +82,7 @@ func metricsGet(d *Daemon, r *http.Request) response.Response {
 	s := d.State()
 
 	projectName := queryParam(r, "project")
+	compress := strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
 
 	// Forward if requested.
 	resp := forwardedResponseIfTargetIsRemote(s, r)
@@ -154,7 +156,7 @@ func metricsGet(d *Daemon, r *http.Request) response.Response {
 
 	// If all valid, return immediately.
 	if len(projectsToFetch) == 0 {
-		return response.SyncResponsePlain(true, metricSet.String())
+		return response.SyncResponsePlain(true, compress, metricSet.String())
 	}
 
 	cacheDuration := time.Duration(8) * time.Second
@@ -176,7 +178,7 @@ func metricsGet(d *Daemon, r *http.Request) response.Response {
 
 	// If all valid, return immediately.
 	if len(projectsToFetch) == 0 {
-		return response.SyncResponsePlain(true, metricSet.String())
+		return response.SyncResponsePlain(true, compress, metricSet.String())
 	}
 
 	// Gather information about host interfaces once.
@@ -267,7 +269,7 @@ func metricsGet(d *Daemon, r *http.Request) response.Response {
 
 	metricsCacheLock.Unlock()
 
-	return response.SyncResponsePlain(true, metricSet.String())
+	return response.SyncResponsePlain(true, compress, metricSet.String())
 }
 
 func internalMetrics(ctx context.Context, daemonStartTime time.Time, tx *db.ClusterTx) *metrics.MetricSet {
