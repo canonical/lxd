canonical/lxd

commit b2936739b8e0a18761cdfb2c5edbdcab074d39e4

lxd/device/device/utils/network: Reworks networkCreateVethPair to use ip.Veth settings
To atomically apply settings to both ends of veth pair.
This reduces the calls to `ip` command and should avoid MAC address races with systemd-udevd
when the host system has the default MACAddressPolicy=persistent for all new interfaces.

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/device/device_utils_network.go
@@ -206,67 +206,48 @@ func networkRestorePhysicalNIC(hostName string, volatile map[string]string) erro
 // is supplied in config, then the MTU of the new peer interface will inherit the parent MTU.
 // Accepts the name of the host side interface as a parameter and returns the peer interface name and MTU used.
 func networkCreateVethPair(hostName string, m deviceConfig.Device) (string, uint32, error) {
-	peerName := network.RandomDevName("veth")
+	var err error
 
 	veth := &ip.Veth{
 		Link: ip.Link{
 			Name: hostName,
+			Up:   true,
 		},
-		PeerName: peerName,
+		Peer: ip.Link{
+			Name: network.RandomDevName("veth"),
+		},
 	}
 
-	err := veth.Add()
-	if err != nil {
-		return "", 0, fmt.Errorf("Failed to create the veth interfaces %q and %q: %w", hostName, peerName, err)
-	}
-
-	err = veth.SetUp()
-	if err != nil {
-		_ = network.InterfaceRemove(hostName)
-		return "", 0, fmt.Errorf("Failed to bring up the veth interface %q: %w", hostName, err)
-	}
-
-	// Set the MAC address on peer.
-	if m["hwaddr"] != "" {
-		link := &ip.Link{Name: peerName}
-		err := link.SetAddress(m["hwaddr"])
-		if err != nil {
-			_ = network.InterfaceRemove(peerName)
-			return "", 0, fmt.Errorf("Failed to set the MAC address: %w", err)
-		}
-	}
-
-	// Set the MTU on peer. If not specified and has parent, will inherit MTU from parent.
-	var mtu uint32
+	// Set the MTU on both ends. If not specified and has parent, will inherit MTU from parent.
 	if m["mtu"] != "" {
-		nicMTU, err := strconv.ParseUint(m["mtu"], 10, 32)
+		mtu, err := strconv.ParseUint(m["mtu"], 10, 32)
 		if err != nil {
 			return "", 0, fmt.Errorf("Invalid MTU specified: %w", err)
 		}
 
-		mtu = uint32(nicMTU)
+		veth.MTU = uint32(mtu)
 	} else if m["parent"] != "" {
-		mtu, err = network.GetDevMTU(m["parent"])
+		mtu, err := network.GetDevMTU(m["parent"])
 		if err != nil {
 			return "", 0, fmt.Errorf("Failed to get the parent MTU: %w", err)
 		}
+
+		veth.MTU = mtu
 	}
 
-	if mtu > 0 {
-		err = NetworkSetDevMTU(peerName, mtu)
+	veth.Peer.MTU = veth.MTU
+
+	// Set the MAC address on peer.
+	if m["hwaddr"] != "" {
+		hwaddr, err := net.ParseMAC(m["hwaddr"])
 		if err != nil {
-			_ = network.InterfaceRemove(peerName)
-			return "", 0, fmt.Errorf("Failed to set the MTU %d: %w", mtu, err)
+			return "", 0, fmt.Errorf("Failed parsing MAC address %q: %w", m["hwaddr"], err)
 		}
 
-		err = NetworkSetDevMTU(hostName, mtu)
-		if err != nil {
-			_ = network.InterfaceRemove(peerName)
-			return "", 0, fmt.Errorf("Failed to set the MTU %d: %w", mtu, err)
-		}
+		veth.Peer.Address = hwaddr
 	}
 
-	return peerName, mtu, nil
+	return veth.Peer.Name, veth.MTU, nil
 }
 
 // networkCreateTap creates and configures a TAP device.

commit 7877961190e89875b808f840f1c49e466b3e47df

lxd/device/device/utils/network: Fix networkCreateVethPair backport
Fixes regression caused by b2936739b8e0a18761cdfb2c5edbdcab074d39e4

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/device/device_utils_network.go
@@ -211,6 +211,8 @@ func networkRestorePhysicalNIC(hostName string, volatile map[string]string) erro
 // is supplied in config, then the MTU of the new peer interface will inherit the parent MTU.
 // Accepts the name of the host side interface as a parameter and returns the peer interface name and MTU used.
 func networkCreateVethPair(hostName string, m deviceConfig.Device) (string, uint32, error) {
+	var err error
+
 	veth := &ip.Veth{
 		Link: ip.Link{
 			Name: hostName,
@@ -248,6 +250,30 @@ func networkCreateVethPair(hostName string, m deviceConfig.Device) (string, uint
 		}
 
 		veth.Peer.Address = hwaddr
+	}
+
+	// Set TX queue length on both ends.
+	if m["queue.tx.length"] != "" {
+		nicTXqlen, err := strconv.ParseUint(m["queue.tx.length"], 10, 32)
+		if err != nil {
+			return "", 0, fmt.Errorf("Invalid txqueuelen specified: %w", err)
+		}
+
+		veth.TXQueueLength = uint32(nicTXqlen)
+	} else if m["parent"] != "" {
+		veth.TXQueueLength, err = network.GetTXQueueLength(m["parent"])
+		if err != nil {
+			return "", 0, fmt.Errorf("Failed to get the parent txqueuelen: %w", err)
+		}
+	}
+
+	veth.Peer.TXQueueLength = veth.TXQueueLength
+
+	// Add and configure the interface in one operation to reduce the number of executions and to avoid
+	// systemd-udevd from applying the default MACAddressPolicy=persistent policy.
+	err = veth.Add()
+	if err != nil {
+		return "", 0, fmt.Errorf("Failed to create the veth interfaces %q and %q: %w", hostName, veth.Peer.Name, err)
 	}
 
 	return veth.Peer.Name, veth.MTU, nil