724215c0f74feefa73cea3e93be7f8b478318833
Thomas Parrott <thomas.parrott@canonical.com>
2023-04-17 12:06:27 +0100

    lxd/network/driver/ovn: Set instance switch port Location in InstanceDevicePortStart
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/driver_ovn.go
+++ lxd/network/driver_ovn.go
@@ -3383,6 +3383,7 @@ func (n *ovn) InstanceDevicePortStart(opts *OVNInstanceNICSetupOpts, securityACL
 		IPs:          staticIPs,
 		Parent:       nestedPortParentName,
 		VLAN:         nestedPortVLAN,
+		Location:     n.state.ServerName,
 	}, true)
 	if err != nil {
 		return "", err
