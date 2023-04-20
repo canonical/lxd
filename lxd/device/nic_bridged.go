b08a91412111ff9ba2a6f292085077c0837e2334
Jeffrey Xie <jeffreyxie0615@gmail.com>
2023-04-20 14:22:08 -0500

    Add required=false option for nic devices with an unavailable parent

--- lxd/device/nic_bridged.go
+++ lxd/device/nic_bridged.go
@@ -480,7 +480,6 @@ func (d *nicBridged) PreStartCheck() error {
 	}
 
 	// If managed network is not available, don't try and start instance.
-	
 	if d.network.Required() && d.network.LocalStatus() == api.NetworkStatusUnavailable {
 		return api.StatusErrorf(http.StatusServiceUnavailable, "Network %q unavailable on this server", d.network.Name())
 	}
