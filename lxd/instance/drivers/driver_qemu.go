5554afae6bfc8297296dd767e0c5ad9a43941241
Thomas Parrott <thomas.parrott@canonical.com>
2023-04-21 08:46:44 +0100

    lxd/instance/drivers/driver/qemu: Only update instance backup file when handling a request to delete a snapshot
    Don't update the backup file for each snapshot being deleted as part of an overall instance delete.
    As this is inefficient.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -5237,7 +5237,29 @@ func (d *qemu) Delete(force bool) error {
 		return api.StatusErrorf(http.StatusBadRequest, "Instance is running")
 	}
 
-	return d.delete(force)
+	err = d.delete(force)
+	if err != nil {
+		return err
+	}
+
+	// If dealing with a snapshot, refresh the backup file on the parent.
+	if d.IsSnapshot() {
+		parentName, _, _ := api.GetParentAndSnapshotName(d.name)
+
+		// Load the parent.
+		parent, err := instance.LoadByProjectAndName(d.state, d.project.Name, parentName)
+		if err != nil {
+			return fmt.Errorf("Invalid parent: %w", err)
+		}
+
+		// Update the backup file.
+		err = parent.UpdateBackupFile()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // Delete the instance without creating an operation lock.
@@ -5326,23 +5348,6 @@ func (d *qemu) delete(force bool) error {
 	if err != nil {
 		d.logger.Error("Failed deleting instance entry", logger.Ctx{"project": d.Project().Name})
 		return err
-	}
-
-	// If dealing with a snapshot, refresh the backup file on the parent.
-	if d.IsSnapshot() {
-		parentName, _, _ := api.GetParentAndSnapshotName(d.name)
-
-		// Load the parent.
-		parent, err := instance.LoadByProjectAndName(d.state, d.project.Name, parentName)
-		if err != nil {
-			return fmt.Errorf("Invalid parent: %w", err)
-		}
-
-		// Update the backup file.
-		err = parent.UpdateBackupFile()
-		if err != nil {
-			return err
-		}
 	}
 
 	if d.isSnapshot {
