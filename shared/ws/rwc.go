c02c5c5c5d9f79d97a4ef65038396f77774c736a
Thomas Parrott <thomas.parrott@canonical.com>
2023-04-26 13:36:54 +0100

    shared/ws/rwc: Merge duplicate implementations from shared
    The resulting implementations take the best bits of both:
    
    * Retains debug logging for errors not passed to callers.
    * Adds reader locking required by gorilla package.
    * Allows Close to be called multiple times (as required by migration subsystem to reuse connection for multiple uses).
    * Uses w.conn.WriteMessage to benefit from possible fastpath in gorilla package.
    * Don't return -1 as n as this isn't compliant with ReadWriteCloser interface.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/ws/rwc.go
+++ shared/ws/rwc.go
@@ -11,104 +11,81 @@ import (
 
 // NewWrapper returns a new ReadWriteCloser wrapper for a websocket connection.
 func NewWrapper(conn *websocket.Conn) io.ReadWriteCloser {
-	return &Wrapper{conn: conn}
+	return &wrapper{conn: conn}
 }
 
-// Wrapper is a wrapper implementing ReadWriteCloser on top of a websocket connection.
-type Wrapper struct {
+// wrapper implements ReadWriteCloser on top of a websocket connection.
+type wrapper struct {
 	conn   *websocket.Conn
 	reader io.Reader
-	mu     sync.Mutex
-	closed bool
+	mur    sync.Mutex
+	muw    sync.Mutex
 }
 
-func (w *Wrapper) Read(p []byte) (n int, err error) {
-	// Processing new message.
+func (w *wrapper) Read(p []byte) (n int, err error) {
+	w.mur.Lock()
+	defer w.mur.Unlock()
+
+	// Get new message if no active one.
 	if w.reader == nil {
-		mt, wr, err := w.conn.NextReader()
+		var mt int
+
+		mt, w.reader, err = w.conn.NextReader()
 		if err != nil {
 			if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				// Websocket was cleanly disconnected.
-				logger.Debug("Websocket: Got disconnected", logger.Ctx{"address": w.conn.RemoteAddr().String()})
+				logger.Debug("Websocket: Got normal closure", logger.Ctx{"address": w.conn.RemoteAddr().String()})
 				return 0, io.EOF
 			}
 
-			// Failed to get a reader.
-			logger.Debug("Websocket: Failed to get reader", logger.Ctx{"err": err, "address": w.conn.RemoteAddr().String()})
-			return -1, err
+			return 0, err
 		}
 
-		w.mu.Lock()
-		w.reader = wr
-		w.mu.Unlock()
+		if mt == websocket.CloseMessage || mt == websocket.TextMessage {
+			switch mt {
+			case websocket.CloseMessage:
+				logger.Debug("Websocket: Got close message", logger.Ctx{"address": w.conn.RemoteAddr().String()})
+			case websocket.TextMessage:
+				logger.Debug("Websocket: Got barrier message", logger.Ctx{"address": w.conn.RemoteAddr().String()})
+			}
 
-		if mt == websocket.CloseMessage {
-			// Websocket closed by remote.
-			logger.Debug("Websocket: Got close message", logger.Ctx{"address": w.conn.RemoteAddr().String()})
-			return 0, io.EOF
-		}
+			w.reader = nil // At the end of the message, reset reader.
 
-		if mt == websocket.TextMessage {
-			// Barrier message, done with this stream.
-			logger.Debug("Websocket: Got barrier message", logger.Ctx{"address": w.conn.RemoteAddr().String()})
 			return 0, io.EOF
 		}
 	}
 
 	// Perform the read itself.
 	n, err = w.reader.Read(p)
-	if err == io.EOF {
-		// At the end of the message, reset for next one.
-		w.mu.Lock()
-		w.reader = nil
-		w.mu.Unlock()
-
-		return n, nil
-	}
-
 	if err != nil {
-		// Failed to read the message.
-		logger.Debug("Websocket: Failed to read message", logger.Ctx{"err": err, "address": w.conn.RemoteAddr().String()})
-		return -1, err
+		w.reader = nil // At the end of the message, reset reader.
+
+		if err == io.EOF {
+			return n, nil // Don't return EOF error at end of message.
+		}
+
+		return n, err
 	}
 
 	return n, nil
 }
 
-func (w *Wrapper) Write(p []byte) (n int, err error) {
-	// Locking.
-	w.mu.Lock()
-	defer w.mu.Unlock()
+func (w *wrapper) Write(p []byte) (int, error) {
+	w.muw.Lock()
+	defer w.muw.Unlock()
 
 	// Send the data as a binary message.
-	wr, err := w.conn.NextWriter(websocket.BinaryMessage)
+	err := w.conn.WriteMessage(websocket.BinaryMessage, p)
 	if err != nil {
-		return -1, err
+		return 0, err
 	}
 
-	defer wr.Close()
-
-	n, err = wr.Write(p)
-	if err != nil {
-		return -1, err
-	}
-
-	return n, nil
+	return len(p), nil
 }
 
 // Close sends a control message indicating the stream is finished, but it does not actually close the socket.
-func (w *Wrapper) Close() error {
-	// Locking.
-	w.mu.Lock()
-	defer w.mu.Unlock()
-
-	// Don't send the barrier multiple times.
-	if w.closed {
-		return io.ErrClosedPipe
-	}
-
-	// Mark as closed.
-	w.closed = true
+func (w *wrapper) Close() error {
+	w.muw.Lock()
+	defer w.muw.Unlock()
 
 	// Send the barrier message (don't actually close the socket so we can use it for another stream).
 	logger.Debug("Websocket: Sending barrier message", logger.Ctx{"address": w.conn.RemoteAddr().String()})
