5347c1ce270c73584aaabf5a75139b36faf9e917
Thomas Parrott <thomas.parrott@canonical.com>
2023-05-04 11:08:26 +0100

    lxd/vsock/vsock: Removes unused Listen function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/vsock/vsock.go
+++ lxd/vsock/vsock.go
@@ -22,11 +22,6 @@ func Dial(cid, port uint32) (net.Conn, error) {
 	return vsock.Dial(cid, port, nil)
 }
 
-// Listen listens for a connection.
-func Listen(port uint32) (net.Listener, error) {
-	return vsock.Listen(port, nil)
-}
-
 // HTTPClient provides an HTTP client for using over vsock.
 func HTTPClient(vsockID int, port int, tlsClientCert string, tlsClientKey string, tlsServerCert string) (*http.Client, error) {
 	client := &http.Client{}
