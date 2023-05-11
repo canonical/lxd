canonical/lxd

commit 123f4c81509bd6aca41ee98cefc47a329e09f6aa

lxd/storage/drivers/driver/zfs/volumes: Pass true to allowUnsafeResize when setting volume size in CreateVolumeFromCopy
This disables pre-resize safety checks in resize2fs as this increases the minumum size that the volume
can be shrunk to. However because if the shrink fails we will delete the volume it doesn't matter
if it leaves the volume in an inconsistent state.

Fixes #11506

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/storage/drivers/driver_zfs_volumes.go
@@ -826,7 +826,9 @@ func (d *zfs) CreateVolumeFromCopy(vol Volume, srcVol Volume, copySnapshots bool
 
 	// Resize volume to the size specified. Only uses volume "size" property and does not use pool/defaults
 	// to give the caller more control over the size being used.
-	err = d.SetVolumeQuota(vol, vol.config["size"], false, op)
+	// Pass allowUnsafeResize as true because if the resize fails we will delete the volume anyway so don't
+	// have to worry about it being left in an inconsistent state.
+	err = d.SetVolumeQuota(vol, vol.config["size"], true, op)
 	if err != nil {
 		return err
 	}

commit 0d136eb0a0fbee2ec1e2a5585b08c54aa1e9a57c

lxd/storage/drivers/driver/zfs/volumes: Fix block volume shrink regression
Only use unsafe resize when operating on block backed filesystem volumes.

Caused by 123f4c81509bd6aca41ee98cefc47a329e09f6aa

Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/storage/drivers/driver_zfs_volumes.go
@@ -823,11 +823,18 @@ func (d *zfs) CreateVolumeFromCopy(vol Volume, srcVol Volume, copySnapshots bool
 		}
 	}
 
+	// Pass allowUnsafeResize as true when resizing block backed filesystem volumes because we want to allow
+	// the filesystem to be shrunk as small as possible without needing the safety checks that would prevent
+	// leaving the filesystem in an inconsistent state if the resize couldn't be completed. This is because if
+	// the resize fails we will delete the volume anyway so don't have to worry about it being inconsistent.
+	var allowUnsafeResize bool
+	if d.isBlockBacked(vol) && vol.contentType == ContentTypeFS {
+		allowUnsafeResize = true
+	}
+
 	// Resize volume to the size specified. Only uses volume "size" property and does not use pool/defaults
 	// to give the caller more control over the size being used.
-	// Pass allowUnsafeResize as true because if the resize fails we will delete the volume anyway so don't
-	// have to worry about it being left in an inconsistent state.
-	err = d.SetVolumeQuota(vol, vol.config["size"], true, op)
+	err = d.SetVolumeQuota(vol, vol.config["size"], allowUnsafeResize, op)
 	if err != nil {
 		return err
 	}