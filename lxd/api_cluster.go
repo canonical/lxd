8cf30520e3e10c64a3b406085ae523cbcba2f0f0
Thomas Parrott <thomas.parrott@canonical.com>
2023-05-22 10:30:43 +0100

    lxd/api/cluster: Fix cluster healing so instances are started in internalClusterHeal
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/api_cluster.go
+++ lxd/api_cluster.go
@@ -3057,6 +3057,21 @@ func internalClusterHeal(d *Daemon, r *http.Request) response.Response {
 			return err
 		}
 
+		if !startInstance || live {
+			return nil
+		}
+
+		// Start it back up on target.
+		startOp, err := dest.UpdateInstanceState(inst.Name(), api.InstanceStatePut{Action: "start"}, "")
+		if err != nil {
+			return err
+		}
+
+		err = startOp.Wait()
+		if err != nil {
+			return err
+		}
+
 		return nil
 	}
 
