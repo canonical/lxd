0f14d0837d511c77f4406e6ce5930168b627d006
Thomas Parrott <thomas.parrott@canonical.com>
2023-06-19 08:19:52 +0100

    lxd/instance/drivers/driver/qemu: Load vhost_vsock kernel module if /dev/kvm is available
    Otherwise /dev/vsock may not be present and VM support detection will fail.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -7774,14 +7774,14 @@ func (d *qemu) Info() instance.Info {
 		return data
 	}
 
-	if !shared.PathExists("/dev/vsock") {
-		data.Error = fmt.Errorf("Vsock support is missing (no /dev/vsock)")
-		return data
-	}
-
 	err := util.LoadModule("vhost_vsock")
 	if err != nil {
 		data.Error = fmt.Errorf("vhost_vsock kernel module not loaded")
+		return data
+	}
+
+	if !shared.PathExists("/dev/vsock") {
+		data.Error = fmt.Errorf("Vsock support is missing (no /dev/vsock)")
 		return data
 	}
 
