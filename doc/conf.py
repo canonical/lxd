914b477bcb7caa40e140489d82ca09a55b3f55d3
Ruth Fuchss <ruth.fuchss@canonical.com>
2023-06-19 12:54:28 +0200

    doc: move requirement setup from the Makefile to conf.py
    RTD doesn't use Makefiles.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- doc/conf.py
+++ doc/conf.py
@@ -2,6 +2,31 @@ import datetime
 import os
 import sys
 import yaml
+from git import Repo
+import wget
+
+# Download and link swagger-ui files
+if not os.path.isdir('.sphinx/deps/swagger-ui'):
+    Repo.clone_from('https://github.com/swagger-api/swagger-ui', '.sphinx/deps/swagger-ui', depth=1)
+
+os.makedirs('.sphinx/_static/swagger-ui/', exist_ok=True)
+
+if not os.path.islink('.sphinx/_static/swagger-ui/swagger-ui-bundle.js'):
+    os.symlink('../../deps/swagger-ui/dist/swagger-ui-bundle.js', '.sphinx/_static/swagger-ui/swagger-ui-bundle.js')
+if not os.path.islink('.sphinx/_static/swagger-ui/swagger-ui-standalone-preset.js'):
+    os.symlink('../../deps/swagger-ui/dist/swagger-ui-standalone-preset.js', '.sphinx/_static/swagger-ui/swagger-ui-standalone-preset.js')
+if not os.path.islink('.sphinx/_static/swagger-ui/swagger-ui.css'):
+    os.symlink('../../deps/swagger-ui/dist/swagger-ui.css', '.sphinx/_static/swagger-ui/swagger-ui.css')
+
+# Download and link images
+os.makedirs('.sphinx/_static/download/', exist_ok=True)
+
+if not os.path.isfile('.sphinx/_static/download/favicon.ico'):
+    wget.download("https://linuxcontainers.org/static/img/favicon.ico", ".sphinx/_static/download/favicon.ico")
+if not os.path.isfile('.sphinx/_static/download/containers.png'):
+    wget.download("https://linuxcontainers.org/static/img/containers.png", ".sphinx/_static/download/containers.png")
+if not os.path.isfile('doc/.sphinx/_static/download/containers.small.png'):
+    wget.download("https://linuxcontainers.org/static/img/containers.small.png", ".sphinx/_static/download/containers.small.png")
 
 # Project config.
 project = "LXD"
