25874f34112c8d1106b155718ffbd9ef54369f9c
Thomas Hipp <thomas.hipp@canonical.com>
2023-06-20 12:16:53 +0200

    lxd/storage/drivers/btrfs: Handle ContentTypeISO
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/storage/drivers/driver_btrfs.go
+++ lxd/storage/drivers/driver_btrfs.go
@@ -491,7 +491,7 @@ func (d *btrfs) MigrationTypes(contentType ContentType, refresh bool, copySnapsh
 	if d.state.OS.RunningInUserNS {
 		var transportType migration.MigrationFSType
 
-		if contentType == ContentTypeBlock {
+		if IsContentBlock(contentType) {
 			transportType = migration.MigrationFSType_BLOCK_AND_RSYNC
 		} else {
 			transportType = migration.MigrationFSType_RSYNC
@@ -505,7 +505,7 @@ func (d *btrfs) MigrationTypes(contentType ContentType, refresh bool, copySnapsh
 		}
 	}
 
-	if contentType == ContentTypeBlock {
+	if IsContentBlock(contentType) {
 		return []migration.Type{
 			{
 				FSType:   migration.MigrationFSType_BTRFS,
