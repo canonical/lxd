f73bd302163681a276abd0eb4dde70ab0e92de11
Weii Wang <weii.wang@canonical.com>
2023-06-21 12:20:29 +0800

    Disable direct IO for QEMU drives on tmpfs

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -3530,7 +3530,8 @@ func (d *qemu) addDriveConfig(bootIndexes map[string]int, driveConf deviceConfig
 
 			// If backing FS is ZFS or BTRFS, avoid using direct I/O and use host page cache only.
 			// We've seen ZFS lock up and BTRFS checksum issues when using direct I/O on image files.
-			if fsType == "zfs" || fsType == "btrfs" {
+			// tmpfs doesn't support direct I/O
+			if fsType == "zfs" || fsType == "btrfs" || fsType == "tmpfs" {
 				if driveConf.FSType != "iso9660" {
 					// Only warn about using writeback cache if the drive image is writable.
 					d.logger.Warn("Using writeback cache I/O", logger.Ctx{"device": driveConf.DevName, "devPath": srcDevPath, "fsType": fsType})
