b870f3f033b11c5787a25a74553c2b8595a39967
Simon Deziel <simon.deziel@canonical.com>
2023-06-22 16:51:24 -0400

    lxc/launch: use aws:t2.micro as t1.micro is no longer available
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxc/launch.go
+++ lxc/launch.go
@@ -29,8 +29,8 @@ func (c *cmdLaunch) Command() *cobra.Command {
 lxc launch ubuntu:22.04 u1 < config.yaml
     Create and start a container with configuration from config.yaml
 
-lxc launch ubuntu:22.04 u2 -t aws:t1.micro
-    Create and start a container using the same size as an AWS t1.micro (1 vCPU, 627MB of RAM)
+lxc launch ubuntu:22.04 u2 -t aws:t2.micro
+    Create and start a container using the same size as an AWS t2.micro (1 vCPU, 1GiB of RAM)
 
 lxc launch ubuntu:22.04 v1 --vm -c limits.cpu=4 -c limits.memory=4GiB
     Create and start a virtual machine with 4 vCPUs and 4GiB of RAM`))
