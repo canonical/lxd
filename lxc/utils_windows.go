76cb48b55913f5e5ea0cfe82c47efdfa5c767d42
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-07-16 14:20:00 +0500

    Added the headings in multiple functions in the LXC/utils_windows.go file
    Signed-off-by: Bilal Khan <bilalkhanrecovered@gmail.com>

--- lxc/utils_windows.go
+++ lxc/utils_windows.go
@@ -11,22 +11,27 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+// Retrieves a wrapped io.WriteCloser object representing the standard output with Windows color support.
 func getStdout() io.WriteCloser {
 	return &WrappedWriteCloser{os.Stdout, colorable.NewColorableStdout()}
 }
 
+// Obtains the file descriptor for the standard output on Windows.
 func getStdoutFd() int {
 	return int(windows.Stdout)
 }
 
+// Fetches the file descriptor for the standard input on Windows.
 func getStdinFd() int {
 	return int(windows.Stdin)
 }
 
+// Retrieves the environment variables specific to Windows as a slice of strings.
 func getEnviron() []string {
 	return windows.Environ()
 }
 
+// Throws an error indicating that the exec operation is not supported on Windows.
 func doExec(argv0 string, argv []string, envv []string) error {
 	return errors.New("not supported by windows")
 }
