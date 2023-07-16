def39305d98685c7a6d069290684ce1dfe406f3a
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-07-16 14:19:21 +0500

    Added the headings in multiple functions in the LXC/utils_test.go file
    Signed-off-by: Bilal Khan <bilalkhanrecovered@gmail.com>

--- lxc/utils_test.go
+++ lxc/utils_test.go
@@ -13,10 +13,12 @@ type utilsTestSuite struct {
 	suite.Suite
 }
 
+// Runs a test suite for utility functions, using the provided testing.T instance.
 func TestUtilsTestSuite(t *testing.T) {
 	suite.Run(t, new(utilsTestSuite))
 }
 
+// Checks if one array of image aliases is a subset of another, expecting the result to be true.
 func (s *utilsTestSuite) TestIsAliasesSubsetTrue() {
 	a1 := []api.ImageAlias{
 		{Name: "foo"},
@@ -31,6 +33,7 @@ func (s *utilsTestSuite) TestIsAliasesSubsetTrue() {
 	s.Exactly(IsAliasesSubset(a1, a2), true)
 }
 
+// Checks if one array of image aliases is a subset of another, expecting the result to be false.
 func (s *utilsTestSuite) TestIsAliasesSubsetFalse() {
 	a1 := []api.ImageAlias{
 		{Name: "foo"},
@@ -45,6 +48,7 @@ func (s *utilsTestSuite) TestIsAliasesSubsetFalse() {
 	s.Exactly(IsAliasesSubset(a1, a2), false)
 }
 
+// Retrieves the retrieval of existing image aliases based on a list of provided aliases.
 func (s *utilsTestSuite) TestGetExistingAliases() {
 	images := []api.ImageAliasesEntry{
 		{Name: "foo"},
@@ -56,6 +60,7 @@ func (s *utilsTestSuite) TestGetExistingAliases() {
 	s.Exactly([]api.ImageAliasesEntry{images[0], images[1]}, aliases)
 }
 
+// Retrieves the retrieval of existing image aliases when no matches are found.
 func (s *utilsTestSuite) TestGetExistingAliasesEmpty() {
 	images := []api.ImageAliasesEntry{
 		{Name: "foo"},
@@ -67,12 +72,14 @@ func (s *utilsTestSuite) TestGetExistingAliasesEmpty() {
 	s.Exactly([]api.ImageAliasesEntry{}, aliases)
 }
 
+// Determines the presence of specific fields in a given struct type, expecting true if the fields exist and false if they don't.
 func (s *utilsTestSuite) TestStructHasFields() {
 	s.Equal(structHasField(reflect.TypeOf(api.Image{}), "type"), true)
 	s.Equal(structHasField(reflect.TypeOf(api.Image{}), "public"), true)
 	s.Equal(structHasField(reflect.TypeOf(api.Image{}), "foo"), false)
 }
 
+// Filters the filtering of supported and unsupported filters based on a provided list of filters and an instance of the API structure.
 func (s *utilsTestSuite) TestGetServerSupportedFilters() {
 	filters := []string{
 		"foo", "type=container", "user.blah=a", "status=running,stopped",
