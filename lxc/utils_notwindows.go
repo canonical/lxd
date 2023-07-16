5e2b8482b35d7d18c3460c5c4c940ac473882072
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-07-16 14:18:41 +0500

    Added the headings in multiple functions in the LXC/utils_notwindows.go file
    Signed-off-by: Bilal Khan <bilalkhanrecovered@gmail.com>

--- lxc/utils_notwindows.go
+++ lxc/utils_notwindows.go
@@ -9,22 +9,27 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// Returns the standard output stream as an io.WriteCloser.
 func getStdout() io.WriteCloser {
 	return os.Stdout
 }
 
+// Returns the file descriptor for the standard output.
 func getStdoutFd() int {
 	return unix.Stdout
 }
 
+// Returns the file descriptor for the standard input.
 func getStdinFd() int {
 	return unix.Stdin
 }
 
+// Returns the environment variables as a slice of strings.
 func getEnviron() []string {
 	return unix.Environ()
 }
 
+// Executes a new process with the given command-line arguments and environment variables.
 func doExec(argv0 string, argv []string, envv []string) error {
 	return unix.Exec(argv0, argv, envv)
 }
