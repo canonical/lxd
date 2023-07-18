fec375c5b9a7ccdff4d1b4c5831af7cecc977d00
Thomas Hipp <thomas.hipp@canonical.com>
2023-07-18 14:53:59 +0200

    operationtype: Change description of BackupsExpire
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/db/operationtype/operation_type.go
+++ lxd/db/operationtype/operation_type.go
@@ -167,7 +167,7 @@ func (t Type) Description() string {
 	case InstanceTypesUpdate:
 		return "Updating instance types"
 	case BackupsExpire:
-		return "Cleaning up expired instance backups"
+		return "Cleaning up expired backups"
 	case SnapshotsExpire:
 		return "Cleaning up expired instance snapshots"
 	case CustomVolumeSnapshotsExpire:
