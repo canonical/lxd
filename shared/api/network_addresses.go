98f2a9f154672d4ddc3169072a36ea81940a99bd
Thomas Parrott <thomas.parrott@canonical.com>
2023-07-21 08:46:26 +0100

    shared/api/network/addresses: Reworks NetworkAllocations struct to allow for the concept of NAT being enabled/disabled on a per-IP family basis
    The previous layout was not supporting the possibility of having NAT disabled for one or other of IPv4 vs IPv6.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/api/network_addresses.go
+++ shared/api/network_addresses.go
@@ -7,14 +7,14 @@ package api
 //
 // API extension: network_allocations.
 type NetworkAllocations struct {
-	// The CIDR subnet of the forward
+	// The network address of the allocation (in CIDR format)
 	// Example: 192.0.2.1/24
-	Addresses []string `json:"addresses" yaml:"addresses"`
+	Address string `json:"addresses" yaml:"addresses"`
 	// Name of the entity consuming the network address
 	UsedBy string `json:"used_by" yaml:"used_by"`
 	// Type of the entity consuming the network address
 	Type string `json:"type" yaml:"type"`
-	// Whether the entity comes from a network that LXD performs NAT on from those that are directly routed from the external network
+	// Whether the entity comes from a network that LXD performs egress source NAT on
 	NAT bool `json:"nat" yaml:"nat"`
 	// Hwaddr is the MAC address of the entity consuming the network address
 	Hwaddr string `json:"hwaddr" yaml:"hwaddr"`
