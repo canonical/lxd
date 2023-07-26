2bd0a5d2dc56846b58c13afeb23c98e2c295d8da
Stéphane Graber <stgraber@stgraber.org>
2023-07-26 11:41:46 -0400

    lxd/storage: Add DelegateVolume to PostHooks
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Sponsored-by: Buddy (https://buddy.works)

--- lxd/storage/backend_lxd.go
+++ lxd/storage/backend_lxd.go
@@ -2674,6 +2674,21 @@ func (b *lxdBackend) MountInstance(inst instance.Instance, op *operations.Operat
 	}
 
 	revert.Success() // From here on it is up to caller to call UnmountInstance() when done.
+
+	// Handle delegation.
+	if b.driver.CanDelegateVolume(vol) {
+		mountInfo.PostHooks = append(mountInfo.PostHooks, func(inst instance.Instance) error {
+			pid := inst.InitPID()
+
+			// Only apply to running instances.
+			if pid < 1 {
+				return nil
+			}
+
+			return b.driver.DelegateVolume(vol, pid)
+		})
+	}
+
 	return mountInfo, nil
 }
 
@@ -5267,6 +5282,20 @@ func (b *lxdBackend) MountCustomVolume(projectName, volName string, op *operatio
 	err = b.driver.MountVolume(vol, op)
 	if err != nil {
 		return nil, err
+	}
+
+	// Handle delegation.
+	if b.driver.CanDelegateVolume(vol) {
+		mountInfo.PostHooks = append(mountInfo.PostHooks, func(inst instance.Instance) error {
+			pid := inst.InitPID()
+
+			// Only apply to running instances.
+			if pid < 1 {
+				return nil
+			}
+
+			return b.driver.DelegateVolume(vol, pid)
+		})
 	}
 
 	return mountInfo, nil
