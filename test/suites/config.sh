4869c09a9e0becf683d2b4a1e2af04bb2f3cf98d
Simon Deziel <simon.deziel@canonical.com>
2023-07-31 14:26:17 -0400

    test/suites/config.sh: LXC 2.0 is EOL
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/config.sh
+++ test/suites/config.sh
@@ -164,16 +164,7 @@ test_config_profiles() {
   lxc profile assign foo onenic
   lxc profile create unconfined
 
-  # Look at the LXC version to decide whether to use the new
-  # or the new config key for apparmor.
-  lxc_version=$(lxc info | awk '/driver_version:/ {print $NF}')
-  lxc_major=$(echo "${lxc_version}" | cut -d. -f1)
-  lxc_minor=$(echo "${lxc_version}" | cut -d. -f2)
-  if [ "${lxc_major}" -lt 2 ] || { [ "${lxc_major}" = "2" ] && [ "${lxc_minor}" -lt "1" ]; }; then
-      lxc profile set unconfined raw.lxc "lxc.aa_profile=unconfined"
-  else
-      lxc profile set unconfined raw.lxc "lxc.apparmor.profile=unconfined"
-  fi
+  lxc profile set unconfined raw.lxc "lxc.apparmor.profile=unconfined"
 
   lxc profile assign foo onenic,unconfined
 
