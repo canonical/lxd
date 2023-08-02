065f59f40ebc8a8b2003bac8ffad7296187e1cc2
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-08-02 08:58:23 +0500

    Added the headings in multiple functions in lxd-benchmark/benchmark/util.go file
    Signed-off-by: Bilal Khan <bilalkhanrecovered@gmail.com>

--- lxd-benchmark/benchmark/util.go
+++ lxd-benchmark/benchmark/util.go
@@ -5,15 +5,18 @@ import (
 	"time"
 )
 
+// Formats a container name using the provided count and index.
 func getContainerName(count int, index int) string {
 	nameFormat := "benchmark-%." + fmt.Sprintf("%d", len(fmt.Sprintf("%d", count))) + "d"
 	return fmt.Sprintf(nameFormat, index+1)
 }
 
+// Logs a formatted message with the current timestamp.
 func logf(format string, args ...any) {
 	fmt.Printf(fmt.Sprintf("[%s] %s\n", time.Now().Format(time.StampMilli), format), args...)
 }
 
+// Displays the current test configuration in a human-readable format.
 func printTestConfig(count int, batchSize int, image string, privileged bool, freeze bool) {
 	privilegedStr := "unprivileged"
 	if privileged {
