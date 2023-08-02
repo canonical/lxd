d55602774e5a11e172f1b5ff83af9189c93e06b7
Simon Deziel <simon.deziel@canonical.com>
2023-08-02 09:44:27 -0400

    test/container_syscall_interception: supported releases have seccomp_notify support
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/container_syscall_interception.sh
+++ test/suites/container_syscall_interception.sh
@@ -2,11 +2,6 @@ test_container_syscall_interception() {
   ensure_import_testimage
   ensure_has_localhost_remote "${LXD_ADDR}"
 
-  if [ "$(lxc query /1.0 | jq -r .environment.lxc_features.seccomp_notify)" != "true" ]; then
-    echo "==> SKIP: Seccomp notify not supported"
-    return
-  fi
-
   if [ "$(awk '/^Seccomp:/ {print $2}' "/proc/self/status")" -eq "0" ]; then
     echo "==> SKIP: syscall interception (seccomp filtering is externally enabled)"
     return
