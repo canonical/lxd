51bbda3c5d29621fb7f9907eefb38a66cd741877
Thomas Hipp <thomas.hipp@canonical.com>
2023-08-04 12:56:33 +0200

    lxd/auth: Update commonAuthorizer
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/auth/driver_common.go
+++ lxd/auth/driver_common.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"net/http"
+
 	"github.com/canonical/lxd/shared/logger"
 )
 
@@ -16,4 +18,22 @@ func (c *commonAuthorizer) init(name string, config map[string]any, l logger.Log
 	c.config = config
 	c.logger = l
 	c.projectsGetFunc = projectsGetFunc
+}
+
+func (c *commonAuthorizer) AddTuple(user string, relation Relation, object string) error {
+	return ErrNotSupported
+}
+
+func (c *commonAuthorizer) DeleteTuple(user string, relation Relation, object string) error {
+	return ErrNotSupported
+}
+
+func (c *commonAuthorizer) ListObjects(r *http.Request, relation Relation, objectType ObjectType) ([]string, error) {
+	return nil, ErrNotSupported
+}
+
+func (c *commonAuthorizer) GetPermissionChecker(r *http.Request, relation Relation, objectType ObjectType) (func(object string) bool, error) {
+	return func(object string) bool {
+		return true
+	}, nil
 }
