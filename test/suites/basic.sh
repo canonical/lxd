c30300dd8799cfb8a6093444c2ca475ae68e32af
Simon Deziel <simon.deziel@canonical.com>
2023-08-07 17:03:54 -0400

    test/basic: s/sss/ss/
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/basic.sh
+++ test/suites/basic.sh
@@ -566,7 +566,7 @@ test_basic_usage() {
         REBOOTED="true"
         break
       else
-        lxc exec foo reboot || true  # Signal to running old init processs to reboot if not rebooted yet.
+        lxc exec foo reboot || true  # Signal to running old init process to reboot if not rebooted yet.
       fi
     fi
 
