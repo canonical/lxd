9214e9c45dd58eca3aa3d1b59068fe3d163ee420
Simon Deziel <simon.deziel@canonical.com>
2023-08-09 17:19:54 -0400

    shared/util: fix RandomCryptoString()'s description
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- shared/util.go
+++ shared/util.go
@@ -329,7 +329,7 @@ func ReaderToChannel(r io.Reader, bufferSize int) <-chan []byte {
 	return ch
 }
 
-// Returns a random base64 encoded string from crypto/rand.
+// Returns a random hex encoded string from crypto/rand.
 func RandomCryptoString() (string, error) {
 	buf := make([]byte, 32)
 	n, err := rand.Read(buf)
