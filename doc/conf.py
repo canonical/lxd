7da8009896c2e32b3ef97aacff13afc43a2f74e7
Ruth Fuchss <ruth.fuchss@canonical.com>
2023-08-25 17:10:26 +0200

    doc: update Contribution section
    Clean up the "How to contribute to LXD" page (and included content)
    and add a section about contributing to the documentation.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- doc/conf.py
+++ doc/conf.py
@@ -167,7 +167,8 @@ ogp_image = "https://documentation.ubuntu.com/lxd/en/latest/_static/tag.png"
 
 linkcheck_ignore = [
     'https://127.0.0.1:8443/1.0',
-    'https://web.libera.chat/#lxd'
+    'https://web.libera.chat/#lxd',
+    'http://localhost:8001'
 ]
 
 # Setup redirects (https://documatt.gitlab.io/sphinx-reredirects/usage.html)
