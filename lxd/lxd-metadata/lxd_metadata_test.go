0ff6badfee98d27633c43f22fc65f038925e40bd
Gabriel Mougard <gabriel.mougard@canonical.com>
2023-08-31 17:19:41 +0200

    lxd-metadata: Update unit test with the new JSON structure
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- lxd/lxd-metadata/lxd_metadata_test.go
+++ lxd/lxd-metadata/lxd_metadata_test.go
@@ -8,91 +8,108 @@ import (
 
 // Test the alphabetical sorting of a `lxd-metadata` JSON structure.
 func TestJSONSorted(t *testing.T) {
-	projectEntries := map[string][]any{
-		"groupKey1": {
-			map[string]any{
-				"b.core.server.test.a": map[string]string{
-					"todo1": "stuff",
-					"todo2": "stuff",
+	projectEntries := make(map[string]any)
+	projectEntries["entityKey1"] = map[string]any{
+		"groupKey1": map[string]any{
+			"keys": []any{
+				map[string]any{
+					"a.core.server.test.b": map[string]string{
+						"todo5": "stuff",
+						"todo6": "stuff",
+					},
 				},
-			},
-			map[string]any{
-				"a.core.server.test.c": map[string]string{
-					"todo3": "stuff",
-					"todo4": "stuff",
+				map[string]any{
+					"a.core.server.test.c": map[string]string{
+						"todo3": "stuff",
+						"todo4": "stuff",
+					},
 				},
-			},
-			map[string]any{
-				"a.core.server.test.b": map[string]string{
-					"todo5": "stuff",
-					"todo6": "stuff",
-				},
-			},
-		},
-		"groupKey2": {
-			map[string]any{
-				"aaa.ccc.bbb": map[string]string{
-					"todo7": "stuff",
-					"todo8": "stuff",
-				},
-			},
-			map[string]any{
-				"000.111.222": map[string]string{
-					"todo9":  "stuff",
-					"todo10": "stuff",
-				},
-			},
-			map[string]any{
-				"zzz.*": map[string]string{
-					"todo11": "stuff",
-					"todo12": "stuff",
+				map[string]any{
+					"b.core.server.test.a": map[string]string{
+						"todo1": "stuff",
+						"todo2": "stuff",
+					},
 				},
 			},
 		},
 	}
 
-	sortedProjectEntries := map[string][]any{
-		"groupKey1": {
-			map[string]any{
-				"a.core.server.test.b": map[string]string{
-					"todo5": "stuff",
-					"todo6": "stuff",
+	projectEntries["entityKey2"] = map[string]any{
+		"groupKey2": map[string]any{
+			"keys": []any{
+				map[string]any{
+					"000.111.222": map[string]string{
+						"todo9":  "stuff",
+						"todo10": "stuff",
+					},
 				},
-			},
-			map[string]any{
-				"a.core.server.test.c": map[string]string{
-					"todo3": "stuff",
-					"todo4": "stuff",
+				map[string]any{
+					"aaa.ccc.bbb": map[string]string{
+						"todo7": "stuff",
+						"todo8": "stuff",
+					},
 				},
-			},
-			map[string]any{
-				"b.core.server.test.a": map[string]string{
-					"todo1": "stuff",
-					"todo2": "stuff",
-				},
-			},
-		},
-		"groupKey2": {
-			map[string]any{
-				"000.111.222": map[string]string{
-					"todo9":  "stuff",
-					"todo10": "stuff",
-				},
-			},
-			map[string]any{
-				"aaa.ccc.bbb": map[string]string{
-					"todo7": "stuff",
-					"todo8": "stuff",
-				},
-			},
-			map[string]any{
-				"zzz.*": map[string]string{
-					"todo11": "stuff",
-					"todo12": "stuff",
+				map[string]any{
+					"zzz.*": map[string]string{
+						"todo11": "stuff",
+						"todo12": "stuff",
+					},
 				},
 			},
 		},
 	}
 
-	assert.Equal(t, sortedProjectEntries, sortConfigKeys(projectEntries))
+	sortedProjectEntries := make(map[string]any)
+	sortedProjectEntries["entityKey1"] = map[string]any{
+		"groupKey1": map[string]any{
+			"keys": []any{
+				map[string]any{
+					"a.core.server.test.b": map[string]string{
+						"todo5": "stuff",
+						"todo6": "stuff",
+					},
+				},
+				map[string]any{
+					"a.core.server.test.c": map[string]string{
+						"todo3": "stuff",
+						"todo4": "stuff",
+					},
+				},
+				map[string]any{
+					"b.core.server.test.a": map[string]string{
+						"todo1": "stuff",
+						"todo2": "stuff",
+					},
+				},
+			},
+		},
+	}
+
+	sortedProjectEntries["entityKey2"] = map[string]any{
+		"groupKey2": map[string]any{
+			"keys": []any{
+				map[string]any{
+					"000.111.222": map[string]string{
+						"todo9":  "stuff",
+						"todo10": "stuff",
+					},
+				},
+				map[string]any{
+					"aaa.ccc.bbb": map[string]string{
+						"todo7": "stuff",
+						"todo8": "stuff",
+					},
+				},
+				map[string]any{
+					"zzz.*": map[string]string{
+						"todo11": "stuff",
+						"todo12": "stuff",
+					},
+				},
+			},
+		},
+	}
+
+	sortConfigKeys(projectEntries)
+	assert.Equal(t, sortedProjectEntries, projectEntries)
 }
