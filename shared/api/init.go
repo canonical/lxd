014e417f3dfb8ecc67b3e35245738bc86776a52d
Stéphane Graber <stgraber@stgraber.org>
2023-09-01 23:49:53 -0400

    shared/api: Add TrustToken support in CertificatesPost and ClusterPut
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    (cherry picked from commit 1687b0ee25c89f9a0579eaa6d227d36a17958052)
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    License: Apache-2.0

--- shared/api/init.go
+++ shared/api/init.go
@@ -78,8 +78,4 @@ type InitClusterPreseed struct {
 	// The path to the cluster certificate
 	// Example: /tmp/cluster.crt
 	ClusterCertificatePath string `json:"cluster_certificate_path" yaml:"cluster_certificate_path"`
-
-	// A cluster join token
-	// Example: BASE64-TOKEN
-	ClusterToken string `json:"cluster_token" yaml:"cluster_token"`
 }
