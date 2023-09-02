b9a84c923bbd2c06fca22b46b6f995ee20b52c8e
Simon Deziel <simon.deziel@canonical.com>
2023-09-01 20:29:22 -0400

    lxd/cgroup/abstraction: skip loop devices from cgroup1's blkio.throttle.{io_service_bytes_recursive,io_serviced_recursive}
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/cgroup/abstraction.go
+++ lxd/cgroup/abstraction.go
@@ -876,6 +876,11 @@ func (cg *CGroup) GetIOStats() (map[string]*IOStats, error) {
 				continue
 			}
 
+			// Skip loop devices (major dev ID 7) as they are irrelevant.
+			if strings.HasPrefix(fields[0], "7:") {
+				continue
+			}
+
 			if ioMap[partMap[fields[0]]] == nil {
 				ioMap[partMap[fields[0]]] = &IOStats{}
 			}
@@ -903,6 +908,11 @@ func (cg *CGroup) GetIOStats() (map[string]*IOStats, error) {
 			fields := strings.Fields(scanner.Text())
 
 			if len(fields) != 3 {
+				continue
+			}
+
+			// Skip loop devices (major dev ID 7) as they are irrelevant.
+			if strings.HasPrefix(fields[0], "7:") {
 				continue
 			}
 
