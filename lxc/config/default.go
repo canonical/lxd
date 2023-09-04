f7ec5c248553d273ce99aa93f499500c46d3cb31
Din Music <din.music@canonical.com>
2023-09-04 10:27:26 +0200

    lxd/config: Add ubuntu-minimal and ubuntu-minimal-daily remotes
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxc/config/default.go
+++ lxc/config/default.go
@@ -30,19 +30,39 @@ var UbuntuDailyRemote = Remote{
 	Protocol: "simplestreams",
 }
 
+// UbuntuMinimalRemote is the Ubuntu minimal image server (over simplestreams).
+var UbuntuMinimalRemote = Remote{
+	Addr:     "https://cloud-images.ubuntu.com/minimal/releases/",
+	Static:   true,
+	Public:   true,
+	Protocol: "simplestreams",
+}
+
+// UbuntuMinimalDailyRemote is the Ubuntu daily minimal image server (over simplestreams).
+var UbuntuMinimalDailyRemote = Remote{
+	Addr:     "https://cloud-images.ubuntu.com/minimal/daily/",
+	Static:   true,
+	Public:   true,
+	Protocol: "simplestreams",
+}
+
 // StaticRemotes is the list of remotes which can't be removed.
 var StaticRemotes = map[string]Remote{
-	"local":        LocalRemote,
-	"ubuntu":       UbuntuRemote,
-	"ubuntu-daily": UbuntuDailyRemote,
+	"local":                LocalRemote,
+	"ubuntu":               UbuntuRemote,
+	"ubuntu-daily":         UbuntuDailyRemote,
+	"ubuntu-minimal":       UbuntuMinimalRemote,
+	"ubuntu-minimal-daily": UbuntuMinimalDailyRemote,
 }
 
 // DefaultRemotes is the list of default remotes.
 var DefaultRemotes = map[string]Remote{
-	"images":       ImagesRemote,
-	"local":        LocalRemote,
-	"ubuntu":       UbuntuRemote,
-	"ubuntu-daily": UbuntuDailyRemote,
+	"images":               ImagesRemote,
+	"local":                LocalRemote,
+	"ubuntu":               UbuntuRemote,
+	"ubuntu-daily":         UbuntuDailyRemote,
+	"ubuntu-minimal":       UbuntuMinimalRemote,
+	"ubuntu-minimal-daily": UbuntuMinimalDailyRemote,
 }
 
 // DefaultConfig returns the default configuration.
