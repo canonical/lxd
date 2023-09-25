6352c1bc6d4e6f793e36ce1b37ecb5e5874b8a8a
Mark Laing <mark.laing@canonical.com>
2023-09-25 18:02:51 +0100

    lxd/auth: Adds Resources type and load option.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/auth/authorization.go
+++ lxd/auth/authorization.go
@@ -98,6 +98,23 @@ type Authorizer interface {
 type Opts struct {
 	config          map[string]any
 	projectsGetFunc func(ctx context.Context) (map[int64]string, error)
+	resources       *Resources
+}
+
+// Resources represents a set of current API resources as Object slices for use when loading an Authorizer.
+type Resources struct {
+	CertificateObjects       []Object
+	StoragePoolObjects       []Object
+	ProjectObjects           []Object
+	ImageObjects             []Object
+	ImageAliasObjects        []Object
+	InstanceObjects          []Object
+	NetworkObjects           []Object
+	NetworkACLObjects        []Object
+	NetworkZoneObjects       []Object
+	ProfileObjects           []Object
+	StoragePoolVolumeObjects []Object
+	StorageBucketObjects     []Object
 }
 
 // WithConfig can be passed into LoadAuthorizer to pass in driver specific configuration.
@@ -111,6 +128,13 @@ func WithConfig(c map[string]any) func(*Opts) {
 func WithProjectsGetFunc(f func(ctx context.Context) (map[int64]string, error)) func(*Opts) {
 	return func(o *Opts) {
 		o.projectsGetFunc = f
+	}
+}
+
+// WithResources should be passed into LoadAuthorizer when DriverOpenFGA is used.
+func WithResources(r Resources) func(*Opts) {
+	return func(o *Opts) {
+		o.resources = &r
 	}
 }
 
