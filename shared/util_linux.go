14b4b931b9aaba2e7195733e6852ef5145e4cb6d
Thomas Parrott <thomas.parrott@canonical.com>
2023-10-16 08:47:21 +0100

    shared/util/linux: Partially reverts 81cd686c7aff398c02dae804d7159b8e04208598 and reintroduces GetPollRevents
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/util_linux.go
+++ shared/util_linux.go
@@ -396,3 +396,26 @@ func ExitStatus(err error) (int, error) {
 
 	return -1, err // Not able to extract an exit status.
 }
+
+// GetPollRevents poll for events on provided fd.
+func GetPollRevents(fd int, timeout int, flags int) (int, int, error) {
+	pollFd := unix.PollFd{
+		Fd:      int32(fd),
+		Events:  int16(flags),
+		Revents: 0,
+	}
+
+	pollFds := []unix.PollFd{pollFd}
+
+again:
+	n, err := unix.Poll(pollFds, timeout)
+	if err != nil {
+		if err == unix.EAGAIN || err == unix.EINTR {
+			goto again
+		}
+
+		return -1, -1, err
+	}
+
+	return n, int(pollFds[0].Revents), err
+}
