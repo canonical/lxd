71b4a9a58e637826d4d4b12ebfdf66f50fe6b6d9
Stephane Graber <stgraber@stgraber.org>
2023-10-17 16:22:50 -0400

    lxd/apparmor/rsync: Replace ioutil.WriteFile() by os.WriteFile()
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/apparmor/rsync.go
+++ lxd/apparmor/rsync.go
@@ -2,7 +2,6 @@ package apparmor
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -141,7 +140,7 @@ func rsyncProfileLoad(sysOS *sys.OS, sourcePath string, dstPath string) (string,
 	}
 
 	// Write it to disk.
-	err = ioutil.WriteFile(profilePath, []byte(content), 0600)
+	err = os.WriteFile(profilePath, []byte(content), 0600)
 	if err != nil {
 		return "", err
 	}
