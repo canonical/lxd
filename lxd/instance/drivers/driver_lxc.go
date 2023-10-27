ffb0f1bce2470e7c0fd82a4cff25b37027a35df4
Thomas Hipp <thomas.hipp@canonical.com>
2023-10-27 12:36:32 +0200

    driver_lxc: Include running state in metrics
    This adds the running state to container metrics. The value will be
    either "RUNNING" or "STOPPED.
    
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- lxd/instance/drivers/driver_lxc.go
+++ lxd/instance/drivers/driver_lxc.go
@@ -8320,11 +8320,14 @@ func (d *lxc) Info() instance.Info {
 }
 
 func (d *lxc) Metrics(hostInterfaces []net.Interface) (*metrics.MetricSet, error) {
-	out := metrics.NewMetricSet(map[string]string{"project": d.project.Name, "name": d.name, "type": instancetype.Container.String()})
+	state := instance.PowerStateStopped
+	isRunning := d.IsRunning()
 
-	if !d.IsRunning() {
-		return nil, ErrInstanceIsStopped
+	if isRunning {
+		state = instance.PowerStateRunning
 	}
+
+	out := metrics.NewMetricSet(map[string]string{"project": d.project.Name, "name": d.name, "type": instancetype.Container.String(), "state": state})
 
 	cc, err := d.initLXC(false)
 	if err != nil {
