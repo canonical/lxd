6d413e6d73ef6eb83081d96134bfc65a1f5a7804
Stéphane Graber <stgraber@stgraber.org>
2023-10-31 01:22:31 -0400

    tests/lint: Ignore test/mini-oidc
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- test/lint/no-oneline-assign-and-test.sh
+++ test/lint/no-oneline-assign-and-test.sh
@@ -3,4 +3,4 @@
 echo "Checking for oneline assign & test..."
 
 # Recursively grep go files for if statements that contain assignments.
-! git grep --untracked -P -n '^\s+if.*:=.*;.*{\s*$' -- '*.go'
+! git grep --untracked -P -n '^\s+if.*:=.*;.*{\s*$' -- '*.go' ':!:test/mini-oidc/storage/*.go'
