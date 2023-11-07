lxc/incus

tests: Add mini-oidc
Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

test/mini-oidc/go.mod
@@ -1,0 +1,33 @@
+module github.com/lxc/incus/test/mini-oidc
+
+go 1.20
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-jose/go-jose/v3 v3.0.0
+	github.com/google/uuid v1.4.0
+	github.com/zitadel/oidc/v3 v3.1.1
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/text v0.13.0
+)
+
+require (
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/muhlemmer/gu v0.3.1 // indirect
+	github.com/muhlemmer/httpforwarded v0.1.0 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/zitadel/logging v0.5.0 // indirect
+	github.com/zitadel/schema v1.3.0 // indirect
+	go.opentelemetry.io/otel v1.19.0 // indirect
+	go.opentelemetry.io/otel/metric v1.19.0 // indirect
+	go.opentelemetry.io/otel/trace v1.19.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)

test/mini-oidc/main.go
@@ -1,0 +1,104 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/zitadel/oidc/v3/pkg/op"
+
+	"github.com/lxc/incus/test/mini-oidc/storage"
+)
+
+func init() {
+	storage.RegisterClients(
+		storage.IncusDeviceClient("device"),
+	)
+}
+
+func main() {
+	port := os.Args[1]
+	issuer := fmt.Sprintf("http://127.0.0.1:%s/", port)
+
+	// Setup the OIDC provider.
+	key := sha256.Sum256([]byte("test"))
+	router := chi.NewRouter()
+	storage := storage.NewStorage(storage.NewUserStore(issuer))
+
+	// Create the provider.
+	config := &op.Config{
+		CryptoKey:               key,
+		CodeMethodS256:          true,
+		AuthMethodPost:          true,
+		AuthMethodPrivateKeyJWT: true,
+		GrantTypeRefreshToken:   true,
+		RequestObjectSupported:  true,
+		DeviceAuthorization: op.DeviceAuthorizationConfig{
+			Lifetime:     5 * time.Minute,
+			PollInterval: 5 * time.Second,
+			UserFormPath: "/device",
+			UserCode:     op.UserCodeBase20,
+		},
+	}
+
+	provider, err := op.NewOpenIDProvider(issuer, config, storage, op.WithAllowInsecure())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Only configure device code authentication.
+	router.HandleFunc("/device", func(w http.ResponseWriter, r *http.Request) {
+		userCodeHandler(storage, w, r)
+	})
+
+	// Register the root to handle discovery.
+	router.Mount("/", http.Handler(provider))
+
+	// Start listening.
+	server := &http.Server{
+		Addr:    "127.0.0.1:" + port,
+		Handler: router,
+	}
+
+	err = server.ListenAndServe()
+	if err != http.ErrServerClosed {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func userCodeHandler(storage *storage.Storage, w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		return
+	}
+
+	userCode := r.Form.Get("user_code")
+	if userCode == "" {
+		return
+	}
+
+	userName := "unknown"
+
+	content, err := os.ReadFile(os.Args[2])
+	if err == nil {
+		userName = strings.TrimSpace(string(content))
+	} else if !os.IsNotExist(err) {
+		return
+	}
+
+	err = storage.CompleteDeviceAuthorization(r.Context(), userCode, userName)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("%s => %s\n", userCode, userName)
+
+	return
+}

test/mini-oidc/storage/local_client.go
@@ -1,0 +1,19 @@
+package storage
+
+import (
+	"github.com/zitadel/oidc/v3/pkg/oidc"
+	"github.com/zitadel/oidc/v3/pkg/op"
+)
+
+// IncusDeviceClient creates a device client suitable for Incus.
+func IncusDeviceClient(id string) *Client {
+	return &Client{
+		id:              id,
+		redirectURIs:    nil,
+		applicationType: op.ApplicationTypeNative,
+		authMethod:      oidc.AuthMethodNone,
+		responseTypes:   []oidc.ResponseType{oidc.ResponseTypeCode},
+		grantTypes:      []oidc.GrantType{oidc.GrantTypeDeviceCode, oidc.GrantTypeRefreshToken},
+		accessTokenType: op.AccessTokenTypeJWT,
+	}
+}

canonical/lxd

Signed-off-by: Mark Laing <mark.laing@canonical.com>

test/mini-oidc/go.mod
@@ -1,0 +1,33 @@
+module github.com/canonical/lxd/test/mini-oidc
+
+go 1.20
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-jose/go-jose/v3 v3.0.0
+	github.com/google/uuid v1.4.0
+	github.com/zitadel/oidc/v3 v3.1.1
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/text v0.13.0
+)
+
+require (
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/muhlemmer/gu v0.3.1 // indirect
+	github.com/muhlemmer/httpforwarded v0.1.0 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/zitadel/logging v0.5.0 // indirect
+	github.com/zitadel/schema v1.3.0 // indirect
+	go.opentelemetry.io/otel v1.19.0 // indirect
+	go.opentelemetry.io/otel/metric v1.19.0 // indirect
+	go.opentelemetry.io/otel/trace v1.19.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)

test/mini-oidc/main.go
@@ -1,0 +1,103 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/zitadel/oidc/v3/pkg/op"
+
+	"github.com/canonical/lxd/test/mini-oidc/storage"
+)
+
+func init() {
+	storage.RegisterClients(
+		storage.LXDDeviceClient("device"),
+	)
+}
+
+func main() {
+	port := os.Args[1]
+	issuer := fmt.Sprintf("http://127.0.0.1:%s/", port)
+
+	// Setup the OIDC provider.
+	key := sha256.Sum256([]byte("test"))
+	router := chi.NewRouter()
+	storage := storage.NewStorage(storage.NewUserStore(issuer))
+
+	// Create the provider.
+	config := &op.Config{
+		CryptoKey:               key,
+		CodeMethodS256:          true,
+		AuthMethodPost:          true,
+		AuthMethodPrivateKeyJWT: true,
+		GrantTypeRefreshToken:   true,
+		RequestObjectSupported:  true,
+		DeviceAuthorization: op.DeviceAuthorizationConfig{
+			Lifetime:     5 * time.Minute,
+			PollInterval: 5 * time.Second,
+			UserFormPath: "/device",
+			UserCode:     op.UserCodeBase20,
+		},
+	}
+
+	provider, err := op.NewOpenIDProvider(issuer, config, storage, op.WithAllowInsecure())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Only configure device code authentication.
+	router.HandleFunc("/device", func(w http.ResponseWriter, r *http.Request) {
+		userCodeHandler(storage, w, r)
+	})
+
+	// Register the root to handle discovery.
+	router.Mount("/", http.Handler(provider))
+
+	// Start listening.
+	server := &http.Server{
+		Addr:    "127.0.0.1:" + port,
+		Handler: router,
+	}
+
+	err = server.ListenAndServe()
+	if err != http.ErrServerClosed {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func userCodeHandler(storage *storage.Storage, w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		return
+	}
+
+	userCode := r.Form.Get("user_code")
+	if userCode == "" {
+		return
+	}
+
+	userName := "unknown"
+
+	content, err := os.ReadFile(os.Args[2])
+	if err == nil {
+		userName = strings.TrimSpace(string(content))
+	} else if !os.IsNotExist(err) {
+		return
+	}
+
+	err = storage.CompleteDeviceAuthorization(r.Context(), userCode, userName)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("%s => %s\n", userCode, userName)
+
+	return
+}

test/mini-oidc/storage/local_client.go
@@ -1,0 +1,19 @@
+package storage
+
+import (
+	"github.com/zitadel/oidc/v3/pkg/oidc"
+	"github.com/zitadel/oidc/v3/pkg/op"
+)
+
+// LXDDeviceClient creates a device client suitable for LXD.
+func LXDDeviceClient(id string) *Client {
+	return &Client{
+		id:              id,
+		redirectURIs:    nil,
+		applicationType: op.ApplicationTypeNative,
+		authMethod:      oidc.AuthMethodNone,
+		responseTypes:   []oidc.ResponseType{oidc.ResponseTypeCode},
+		grantTypes:      []oidc.GrantType{oidc.GrantTypeDeviceCode, oidc.GrantTypeRefreshToken},
+		accessTokenType: op.AccessTokenTypeJWT,
+	}
+}