41ff925944334d7702832c9abd44f813b0b1e51e
Max Asnaashari <max.asnaashari@canonical.com>
2023-11-20 22:45:42 +0000

    lxd/storage/drivers: Update cephfs entity helpers
    Adds a helper to find osd pools, and updates the fs lookup helper to
    differentiate between exit codes from ceph.
    
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/storage/drivers/driver_cephfs_utils.go
+++ lxd/storage/drivers/driver_cephfs_utils.go
@@ -7,9 +7,41 @@ import (
 )
 
 // fsExists checks that the Ceph FS instance indeed exists.
-func (d *cephfs) fsExists(clusterName string, userName string, fsName string) bool {
+func (d *cephfs) fsExists(clusterName string, userName string, fsName string) (bool, error) {
 	_, err := shared.RunCommand("ceph", "--name", fmt.Sprintf("client.%s", userName), "--cluster", clusterName, "fs", "get", fsName)
-	return err == nil
+	if err != nil {
+		status, _ := shared.ExitStatus(err)
+		// If the error status code is 2, the fs definitely doesn't exist.
+		if status == 2 {
+			return false, nil
+		}
+
+		// Else, the error status is not 0 or 2,
+		// so we can't be sure if the fs exists or not
+		// as it might be a network issue, an internal ceph issue, etc.
+		return false, err
+	}
+
+	return true, nil
+}
+
+// osdPoolExists checks that the Ceph OSD Pool indeed exists.
+func (d *cephfs) osdPoolExists(clusterName string, userName string, osdPoolName string) (bool, error) {
+	_, err := shared.RunCommand("ceph", "--name", fmt.Sprintf("client.%s", userName), "--cluster", clusterName, "osd", "pool", "get", osdPoolName, "size")
+	if err != nil {
+		status, _ := shared.ExitStatus(err)
+		// If the error status code is 2, the pool definitely doesn't exist.
+		if status == 2 {
+			return false, nil
+		}
+
+		// Else, the error status is not 0 or 2,
+		// so we can't be sure if the pool exists or not
+		// as it might be a network issue, an internal ceph issue, etc.
+		return false, err
+	}
+
+	return true, nil
 }
 
 // getConfig parses the Ceph configuration file and returns the list of monitors and secret key.
