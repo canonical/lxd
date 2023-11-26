21c0d4f5f19c5c895a803620356c1bd511b94547
Stéphane Graber <stgraber@stgraber.org>
2023-11-25 23:06:48 -0500

    lxd/images: Properly detect unfiltered
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/images.go
+++ lxd/images.go
@@ -1242,7 +1242,7 @@ func getImageMetadata(fname string) (*api.ImageMetadata, string, error) {
 }
 
 func doImagesGet(ctx context.Context, tx *db.ClusterTx, recursion bool, projectName string, public bool, clauses *filter.ClauseSet) (any, error) {
-	mustLoadObjects := recursion || clauses != nil
+	mustLoadObjects := recursion || (clauses != nil && len(clauses.Clauses) > 0)
 
 	fingerprints, err := tx.GetImagesFingerprints(ctx, projectName, public)
 	if err != nil {
@@ -1259,15 +1259,15 @@ func doImagesGet(ctx context.Context, tx *db.ClusterTx, recursion bool, projectN
 	}
 
 	for _, fingerprint := range fingerprints {
+		image, err := doImageGet(ctx, tx, projectName, fingerprint, public)
+		if err != nil {
+			continue
+		}
+
 		if !mustLoadObjects {
 			resultString = append(resultString, api.NewURL().Path(version.APIVersion, "images", fingerprint).String())
 		} else {
-			image, err := doImageGet(ctx, tx, projectName, fingerprint, public)
-			if err != nil {
-				continue
-			}
-
-			if clauses != nil {
+			if clauses != nil && len(clauses.Clauses) > 0 {
 				match, err := filter.Match(*image, *clauses)
 				if err != nil {
 					return nil, err
