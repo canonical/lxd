951f961db2fd106fae0e1e956d895aa203b7ed8b
Din Music <din.music@canonical.com>
2023-11-28 10:16:51 +0100

    shared/api/instance: Expand InstancePost structure
    Signed-off-by: Din Music <din.music@canonical.com>

--- shared/api/instance.go
+++ shared/api/instance.go
@@ -107,6 +107,24 @@ type InstancePost struct {
 	//
 	// API extension: instance_allow_inconsistent_copy
 	AllowInconsistent bool `json:"allow_inconsistent" yaml:"allow_inconsistent"`
+
+	// Instance configuration file.
+	// Example: {"security.nesting": "true"}
+	//
+	// API extension: instance_move_config
+	Config map[string]string
+
+	// Instance devices.
+	// Example: {"root": {"type": "disk", "pool": "default", "path": "/"}}
+	//
+	// API extension: instance_move_config
+	Devices map[string]map[string]string
+
+	// List of profiles applied to the instance.
+	// Example: ["default"]
+	//
+	// API extension: instance_move_config
+	Profiles []string
 }
 
 // InstancePostTarget represents the migration target host and operation.
