bd481337495884c014d15a02fedd65cfb5f1ecb6
Julian Pelizäus <julian.pelizaeus@canonical.com>
2023-11-29 09:42:52 +0100

    lxd/device/config: Add DefaultVMPowerFlexBlockFilesystemSize constant
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/device/config/consts.go
+++ lxd/device/config/consts.go
@@ -2,3 +2,6 @@ package config
 
 // DefaultVMBlockFilesystemSize is the size of a VM root device block volume's associated filesystem volume.
 const DefaultVMBlockFilesystemSize = "100MiB"
+
+// DefaultVMPowerFlexBlockFilesystemSize is the size of a VM PowerFlex root device block volume's associated filesystem volume.
+const DefaultVMPowerFlexBlockFilesystemSize = "8GiB"
