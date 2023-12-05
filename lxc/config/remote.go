lxc/incus

shared/cliconfig: Nicer error on missing socket
Closes #184

Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

shared/cliconfig/remote.go
@@ -4,7 +4,9 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"encoding/pem"
+	"errors"
 	"fmt"
+	"net"
 	"os"
 	"runtime"
 	"strings"
@@ -74,6 +76,12 @@ func (c *Config) GetInstanceServer(name string) (incus.InstanceServer, error) {
 	if strings.HasPrefix(remote.Addr, "unix:") {
 		d, err := incus.ConnectIncusUnix(strings.TrimPrefix(strings.TrimPrefix(remote.Addr, "unix:"), "//"), args)
 		if err != nil {
+			var netErr *net.OpError
+
+			if errors.As(err, &netErr) {
+				return nil, fmt.Errorf("The incus daemon doesn't appear to be started (socket path: %s)", netErr.Addr)
+			}
+
 			return nil, err
 		}
 

canonical/lxd

lxc/config/remote.go
@@ -3,7 +3,9 @@ package config
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
+	"net"
 	"net/url"
 	"os"
 	"runtime"
@@ -78,6 +80,12 @@ func (c *Config) GetInstanceServer(name string) (lxd.InstanceServer, error) {
 	if strings.HasPrefix(remote.Addr, "unix:") {
 		d, err := lxd.ConnectLXDUnix(strings.TrimPrefix(strings.TrimPrefix(remote.Addr, "unix:"), "//"), args)
 		if err != nil {
+			var netErr *net.OpError
+
+			if errors.As(err, &netErr) {
+				return nil, fmt.Errorf("The LXD daemon doesn't appear to be started (socket path: %s)", netErr.Addr)
+			}
+
 			return nil, err
 		}
 