ca4b661ee9b88a97183ae0680972d0326f78ba4f
Stéphane Graber <stgraber@stgraber.org>
2023-12-14 22:51:06 -0500

    test/golangci: Handle some common upstream branch names
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    Licence: Apache-2.0

--- test/lint/golangci.sh
+++ test/lint/golangci.sh
@@ -1,13 +1,26 @@
 #!/bin/sh -eu
 
-# Default target branch.
-target_branch="main"
+target_branch=""
 if [ -n "${GITHUB_ACTIONS:-}" ]; then
   # Target branch when running in github actions (see https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables).
   target_branch="${GITHUB_BASE_REF}"
 elif [ -n "${1:-}" ]; then
   # Allow a target branch parameter.
   target_branch="${1}"
+else
+  # Default target branch.
+  for branch in main origin; do
+    if git show-ref --quiet "refs/heads/${branch}" >/dev/null 2>&1; then
+        target_branch="${branch}"
+        break
+    fi
+  done
+fi
+
+# Check if we found a target branch.
+if [ -z "${target_branch}" ]; then
+  echo "The target branch for golangci couldn't be found, skipping."
+  return
 fi
 
 # Gets the most recent commit hash from the target branch.
