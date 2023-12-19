ccdca81031b06bf14f184229a03ed95d9510108b
Stéphane Graber <stgraber@stgraber.org>
2023-12-19 13:57:45 -0500

    lxd/network/ovn: Add new Southbound client
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Sponsored-by: Luizalabs (https://luizalabs.com)
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    License: Apache-2.0

---
+++ lxd/network/ovn/ovn_sb.go
@@ -0,0 +1,198 @@
+package ovn
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+
+	"github.com/go-logr/logr"
+	ovsdbClient "github.com/ovn-org/libovsdb/client"
+
+	ovnSB "github.com/canonical/lxd/lxd/network/ovn/schema/ovn-sb"
+	"github.com/canonical/lxd/lxd/network/ovs"
+	"github.com/canonical/lxd/lxd/state"
+)
+
+// SB client.
+type SB struct {
+	client ovsdbClient.Client
+	cookie ovsdbClient.MonitorCookie
+}
+
+// NewSB initialises new OVN client for Southbound operations.
+func NewSB(s *state.State) (*SB, error) {
+	// Get the database connection string.
+	dbAddr, err := ovs.NewVSwitch().OVNSouthboundDBRemoteAddress()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get OVN southbound connection string: %w", err)
+	}
+
+	// Prepare the OVSDB client.
+	dbSchema, err := ovnSB.FullDatabaseModel()
+	if err != nil {
+		return nil, err
+	}
+
+	discard := logr.Discard()
+
+	options := []ovsdbClient.Option{ovsdbClient.WithLogger(&discard)}
+	for _, entry := range strings.Split(dbAddr, ",") {
+		options = append(options, ovsdbClient.WithEndpoint(entry))
+	}
+
+	// Handle SSL.
+	if strings.Contains(dbAddr, "ssl:") {
+		// Get the OVN SSL keys from the daemon config.
+		sslCACert, sslClientCert, sslClientKey := s.GlobalConfig.NetworkOVNSSL()
+
+		// Fallback to filesystem keys.
+		if sslCACert == "" {
+			content, err := os.ReadFile("/etc/ovn/ovn-central.crt")
+			if err != nil {
+				if os.IsNotExist(err) {
+					return nil, fmt.Errorf("OVN configured to use SSL but no SSL CA certificate defined")
+				}
+
+				return nil, err
+			}
+
+			sslCACert = string(content)
+		}
+
+		if sslClientCert == "" {
+			content, err := os.ReadFile("/etc/ovn/cert_host")
+			if err != nil {
+				if os.IsNotExist(err) {
+					return nil, fmt.Errorf("OVN configured to use SSL but no SSL client certificate defined")
+				}
+
+				return nil, err
+			}
+
+			sslClientCert = string(content)
+		}
+
+		if sslClientKey == "" {
+			content, err := os.ReadFile("/etc/ovn/key_host")
+			if err != nil {
+				if os.IsNotExist(err) {
+					return nil, fmt.Errorf("OVN configured to use SSL but no SSL client key defined")
+				}
+
+				return nil, err
+			}
+
+			sslClientKey = string(content)
+		}
+
+		// Validation.
+		if sslClientCert == "" {
+			return nil, fmt.Errorf("OVN is configured to use SSL but no client certificate was found")
+		}
+
+		if sslClientKey == "" {
+			return nil, fmt.Errorf("OVN is configured to use SSL but no client key was found")
+		}
+
+		// Prepare the client.
+		clientCert, err := tls.X509KeyPair([]byte(sslClientCert), []byte(sslClientKey))
+		if err != nil {
+			return nil, err
+		}
+
+		tlsConfig := &tls.Config{
+			Certificates:       []tls.Certificate{clientCert},
+			InsecureSkipVerify: true,
+		}
+
+		// Add CA check if provided.
+		if sslCACert != "" {
+			tlsCAder, _ := pem.Decode([]byte(sslCACert))
+			if tlsCAder == nil {
+				return nil, fmt.Errorf("Couldn't parse CA certificate")
+			}
+
+			tlsCAcert, err := x509.ParseCertificate(tlsCAder.Bytes)
+			if err != nil {
+				return nil, err
+			}
+
+			tlsCAcert.IsCA = true
+			tlsCAcert.KeyUsage = x509.KeyUsageCertSign
+
+			clientCAPool := x509.NewCertPool()
+			clientCAPool.AddCert(tlsCAcert)
+
+			tlsConfig.VerifyPeerCertificate = func(rawCerts [][]byte, chains [][]*x509.Certificate) error {
+				if len(rawCerts) < 1 {
+					return fmt.Errorf("Missing server certificate")
+				}
+
+				// Load the chain.
+				roots := x509.NewCertPool()
+				for _, rawCert := range rawCerts {
+					cert, _ := x509.ParseCertificate(rawCert)
+					if cert != nil {
+						roots.AddCert(cert)
+					}
+				}
+
+				// Load the main server certificate.
+				cert, _ := x509.ParseCertificate(rawCerts[0])
+				if cert == nil {
+					return fmt.Errorf("Bad server certificate")
+				}
+
+				// Validate.
+				opts := x509.VerifyOptions{
+					Roots: roots,
+				}
+
+				_, err := cert.Verify(opts)
+				return err
+			}
+		}
+
+		// Add the TLS config to the client.
+		options = append(options, ovsdbClient.WithTLSConfig(tlsConfig))
+	}
+
+	// Connect to OVSDB.
+	ovn, err := ovsdbClient.NewOVSDBClient(dbSchema, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ovn.Connect(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	err = ovn.Echo(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	monitorCookie, err := ovn.MonitorAll(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the SB struct.
+	client := &SB{
+		client: ovn,
+		cookie: monitorCookie,
+	}
+
+	// Set finalizer to stop the monitor.
+	runtime.SetFinalizer(client, func(o *SB) {
+		_ = ovn.MonitorCancel(context.Background(), o.cookie)
+	})
+
+	return client, nil
+}
