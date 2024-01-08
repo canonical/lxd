02729524728b7e06dc0a92a746300e98e7396b99
Fabian Mettler <dev@maveonair.com>
2024-01-09 00:03:05 +0100

    api: Add storage_bucket_backup extension
    Signed-off-by: Fabian Mettler <dev@maveonair.com>
    (cherry picked from commit 21ed02ae159abd398ea623406101f877d4092b59)
    Signed-off-by: Mark Bolton <mark.bolton@canonical.com>
    License: Apache-2.0

--- shared/version/api.go
+++ shared/version/api.go
@@ -418,6 +418,7 @@ var APIExtensions = []string{
 	"projects_limits_disk_pool",
 	"ubuntu_pro_guest_attach",
 	"metadata_configuration_entity_types",
+	"storage_bucket_backup",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
