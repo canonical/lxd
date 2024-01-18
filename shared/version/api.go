23436b3b6c24a7659bc32ebd9857b207c6646304
Mahesh Punjabi <mahesh.punjabi86@gmail.com>
2024-01-17 21:52:09 -0500

    api: Add `images_all_projects` extension
    Signed-off-by: Mahesh Punjabi <mahesh.punjabi86@gmail.com>
    (cherry picked from commit 09576c289f57dc657ad99ef4a9a2fc5630d78007)
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    License: Apache-2.0
    (cherry picked from commit 60f0c5c8231716fe84d2356f7bc39e2ae4b9f3c9)

--- shared/version/api.go
+++ shared/version/api.go
@@ -423,6 +423,7 @@ var APIExtensions = []string{
 	"state_logical_cpus",
 	"vm_limits_cpu_pin_strategy",
 	"gpu_cdi",
+	"images_all_projects",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
