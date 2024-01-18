4d3a4c081f3e4f19391db5f708ccb51fca4eaee4
Mark Laing <mark.laing@canonical.com>
2024-01-18 13:26:22 +0000

    test/lint: Always fetch the target branch if it cannot be found.
    Previously this was only performed when the GITHUB_BEFORE environment
    variable was set.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- test/lint/golangci.sh
+++ test/lint/golangci.sh
@@ -7,9 +7,6 @@ if [ -n "${GITHUB_BASE_REF:-}" ]; then
 elif [ -n "${GITHUB_BEFORE:-}" ]; then
   # Target branch when scanning a Github merge
   target_branch="${GITHUB_BEFORE}"
-
-  # Attempt to fetch the reference (Github uses a shallow clone).
-  git fetch -q origin "${GITHUB_BEFORE}" || true
 elif [ -n "${1:-}" ]; then
   # Allow a target branch parameter.
   target_branch="${1}"
@@ -27,6 +24,11 @@ fi
 if [ -z "${target_branch}" ]; then
   echo "The target branch for golangci couldn't be found, skipping."
   return
+fi
+
+if ! git show-ref --quiet "refs/heads/${target_branch}" --quiet >/dev/null 2>&1; then
+    # Attempt to fetch the reference (Github uses a shallow clone).
+    git fetch -q origin "${target_branch}"
 fi
 
 # Gets the most recent commit hash from the target branch.
