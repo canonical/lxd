20af046998d7c3ba83e6f26dca05652b14d0196d
Stéphane Graber <stgraber@stgraber.org>
2024-01-18 14:56:58 -0500

    lxd/loki: Add support for overriding instance name
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    (cherry picked from commit 291882e0de6ca7bb760009ce6c5caedda3c4d538)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    License: Apache-2.0

--- lxd/loki/loki.go
+++ lxd/loki/loki.go
@@ -9,7 +9,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"os"
 	"reflect"
 	"sort"
 	"strconv"
@@ -37,6 +36,7 @@ type config struct {
 	username  string
 	password  string
 	labels    []string
+	instance  string
 	logLevel  string
 	timeout   time.Duration
 	types     []string
@@ -60,7 +60,7 @@ type Client struct {
 }
 
 // NewClient returns a Client.
-func NewClient(ctx context.Context, url *url.URL, username string, password string, caCert string, labels []string, logLevel string, types []string) *Client {
+func NewClient(ctx context.Context, url *url.URL, username string, password string, caCert string, instance string, logLevel string, labels []string, types []string) *Client {
 	client := Client{
 		cfg: config{
 			batchSize: 10 * 1024,
@@ -68,6 +68,7 @@ func NewClient(ctx context.Context, url *url.URL, username string, password stri
 			caCert:    caCert,
 			username:  username,
 			password:  password,
+			instance:  instance,
 			labels:    labels,
 			logLevel:  logLevel,
 			timeout:   10 * time.Second,
@@ -226,17 +227,12 @@ func (c *Client) HandleEvent(event api.Event) {
 		return
 	}
 
-	hostname, err := os.Hostname()
-	if err != nil {
-		hostname = "none"
-	}
-
 	entry := entry{
 		labels: LabelSet{
 			"app":      "lxd",
 			"type":     event.Type,
 			"location": event.Location,
-			"instance": hostname,
+			"instance": c.cfg.instance,
 		},
 		Entry: Entry{
 			Timestamp: event.Timestamp,
