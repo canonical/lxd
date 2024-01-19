11c4ae7d5e345ddea419a032fc70be4e6ec405dd
Simon Deziel <simon.deziel@canonical.com>
2024-01-19 16:27:48 -0500

    test/lint/golangci: undo default branch guessing logic
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/lint/golangci.sh
+++ test/lint/golangci.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -eu
 
-target_branch=""
+# Default target branch.
+target_branch="main"
 if [ -n "${GITHUB_BASE_REF:-}" ]; then
   # Target branch when scanning a Github pull request
   target_branch="${GITHUB_BASE_REF}"
@@ -10,30 +11,17 @@ elif [ -n "${GITHUB_BEFORE:-}" ]; then
 elif [ -n "${1:-}" ]; then
   # Allow a target branch parameter.
   target_branch="${1}"
-else
-  # Default target branch.
-  for branch in main origin; do
-    if git show-ref --quiet "refs/heads/${branch}" >/dev/null 2>&1; then
-        target_branch="${branch}"
-        break
-    fi
-  done
-fi
-
-# Check if we found a target branch.
-if [ -z "${target_branch}" ]; then
-  echo "The target branch for golangci couldn't be found, aborting."
-  false
-fi
-
-# Fetch the reference if it doesn't exist (Github uses a shallow clone).
-if ! git show-ref --quiet "refs/heads/${target_branch}" >/dev/null 2>&1; then
-    echo "Convert shallow clone into treeless clone"
-    git fetch --filter=tree:0 origin "${target_branch}"
 fi
 
 # Gets the most recent commit hash from the target branch.
-rev="$(git log --max-count=1 --format=%H "origin/${target_branch}")"
+rev="$(git log --max-count=1 --format=%H "origin/${target_branch}" || true)"
+if [ -z "${rev}" ]; then
+    # actions/checkout creates shallow clones by default
+    echo "Convert shallow clone into treeless clone"
+    git fetch --filter=tree:0 origin "${target_branch}"
+    rev="$(git log --max-count=1 --format=%H "origin/${target_branch}")"
+fi
+
 if [ -z "${rev}" ]; then
   echo "No revision found for the tip of the target branch, aborting."
   false
