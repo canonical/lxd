64f3223cafe6cd5f55021ecd6c9a5c3518a98b49
Thomas Parrott <thomas.parrott@canonical.com>
2024-01-30 13:45:44 +0000

    test/lint: Bring linters inline with main
    There have been various iterations to get the golangci linter working properly.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/lint/no-short-form-imports.sh
+++ test/lint/no-short-form-imports.sh
@@ -2,7 +2,7 @@
 
 echo "Checking for short form imports..."
 
-OUT=$(git grep --untracked -n -P '^\s*import\s+"' '*.go' | grep -v ':import "C"$' || true)
+OUT=$(git grep --untracked -n -P '^\s*import\s+"' '*.go' ':!:test/mini-oidc/storage/*.go' | grep -v ':import "C"$' || true)
 if [ -n "${OUT}" ]; then
   echo "ERROR: found short form imports: ${OUT}"
   exit 1
