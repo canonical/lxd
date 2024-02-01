7f198676a2fecab116c5d06ccd52a6eaa813fbb2
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-02-01 10:46:14 +0100

    lxd/storage/drivers/ceph: Disable filesystem config keys on block volumes
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/storage/drivers/driver_ceph_volumes.go
+++ lxd/storage/drivers/driver_ceph_volumes.go
@@ -855,7 +855,18 @@ func (d *ceph) commonVolumeRules() map[string]func(value string) error {
 
 // ValidateVolume validates the supplied volume config.
 func (d *ceph) ValidateVolume(vol Volume, removeUnknownKeys bool) error {
-	return d.validateVolume(vol, d.commonVolumeRules(), removeUnknownKeys)
+	commonRules := d.commonVolumeRules()
+
+	// Disallow block.* settings for regular custom block volumes. These settings only make sense
+	// when using custom filesystem volumes. LXD will create the filesystem
+	// for these volumes, and use the mount options. When attaching a regular block volume to a VM,
+	// these are not mounted by LXD and therefore don't need these config keys.
+	if vol.IsVMBlock() || vol.volType == VolumeTypeCustom && vol.contentType == ContentTypeBlock {
+		delete(commonRules, "block.filesystem")
+		delete(commonRules, "block.mount_options")
+	}
+
+	return d.validateVolume(vol, commonRules, removeUnknownKeys)
 }
 
 // UpdateVolume applies config changes to the volume.
