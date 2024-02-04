070f17385811b1a7164911fd97aea59670355d02
Thomas Hipp <thomashipp@gmail.com>
2024-02-04 21:54:41 +0100

    lxd/warnings: Update context
    Signed-off-by: Thomas Hipp <thomashipp@gmail.com>
    (cherry picked from commit 3be19c71862b82ed319fe28a2845c98035eb07ef)
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    License: Apache-2.0
    (cherry picked from commit 089200418bf2f65306fd01d463170acd12e23742)

--- lxd/warnings.go
+++ lxd/warnings.go
@@ -170,7 +170,7 @@ func warningsGet(d *Daemon, r *http.Request) response.Response {
 	projectName := request.QueryParam(r, "project")
 
 	var warnings []api.Warning
-	err = d.State().DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = d.State().DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
 		filters := []cluster.WarningFilter{}
 		if projectName != "" {
 			filter := cluster.WarningFilter{Project: &projectName}
@@ -266,7 +266,7 @@ func warningGet(d *Daemon, r *http.Request) response.Response {
 	}
 
 	var resp api.Warning
-	err = d.State().DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = d.State().DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
 		dbWarning, err := cluster.GetWarning(ctx, tx.Tx(), id)
 		if err != nil {
 			return err
@@ -372,7 +372,7 @@ func warningPut(d *Daemon, r *http.Request) response.Response {
 		return response.Forbidden(errors.New(`Status may only be set to "acknowledge" or "new"`))
 	}
 
-	err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
 		err := tx.UpdateWarningStatus(id, status)
 		if err != nil {
 			return err
@@ -415,7 +415,7 @@ func warningDelete(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(err)
 	}
 
-	err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
 		err := cluster.DeleteWarning(ctx, tx.Tx(), id)
 		if err != nil {
 			return err
@@ -466,7 +466,7 @@ func pruneResolvedWarningsTask(stateFunc func() *state.State) (task.Func, task.S
 }
 
 func pruneResolvedWarnings(ctx context.Context, s *state.State) error {
-	err := s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err := s.DB.Cluster.Transaction(ctx, func(ctx context.Context, tx *db.ClusterTx) error {
 		// Retrieve warnings by resolved status.
 		statusResolved := warningtype.StatusResolved
 		filter := cluster.WarningFilter{
