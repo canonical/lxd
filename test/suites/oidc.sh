5a4a2f432e87e945d747d5c4692206d0619d4ec4
Mark Laing <mark.laing@canonical.com>
2024-02-06 18:54:08 +0000

    test/suites: Test that OIDC identities are being added to the database.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- test/suites/oidc.sh
+++ test/suites/oidc.sh
@@ -11,12 +11,14 @@ test_oidc() {
 
   BROWSER=curl lxc remote add --accept-certificate oidc "${LXD_ADDR}" --auth-type oidc
   [ "$(lxc info oidc: | grep ^auth_user_name | sed "s/.*: //g")" = "unknown" ]
+  [ "$(lxd sql global "SELECT identifier, name, auth_method, type FROM identities WHERE type = 5 AND identifier = 'unknown' AND auth_method = 2" | wc -l)" = 5 ]
   lxc remote remove oidc
 
   set_oidc test-user
 
   BROWSER=curl lxc remote add --accept-certificate oidc "${LXD_ADDR}" --auth-type oidc
   [ "$(lxc info oidc: | grep ^auth_user_name | sed "s/.*: //g")" = "test-user" ]
+  [ "$(lxd sql global "SELECT identifier, name, auth_method, type FROM identities WHERE type = 5 AND identifier = 'test-user' AND auth_method = 2" | wc -l)" = 5 ]
   lxc remote remove oidc
 
   # Cleanup OIDC
