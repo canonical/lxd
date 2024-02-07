706e40ac66afc4769a6a3e052f6651cd806a1eee
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-02-07 15:06:18 +0100

    lxd: Move `ExpandInstanceConfig` and `ExpandInstanceDevices` from `lxd/db` to `lxd/instance/instancetype` package
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/network/driver_bridge.go
+++ lxd/network/driver_bridge.go
@@ -28,6 +28,7 @@ import (
 	"github.com/canonical/lxd/lxd/dnsmasq"
 	"github.com/canonical/lxd/lxd/dnsmasq/dhcpalloc"
 	firewallDrivers "github.com/canonical/lxd/lxd/firewall/drivers"
+	"github.com/canonical/lxd/lxd/instance/instancetype"
 	"github.com/canonical/lxd/lxd/ip"
 	"github.com/canonical/lxd/lxd/network/acl"
 	"github.com/canonical/lxd/lxd/network/openvswitch"
@@ -2482,7 +2483,7 @@ func (n *bridge) bridgedNICExternalRoutes(bridgeProjectNetworks map[string][]*ap
 				return nil // Managed bridge networks can only exist in default project.
 			}
 
-			devices := db.ExpandInstanceDevices(inst.Devices, inst.Profiles)
+			devices := instancetype.ExpandInstanceDevices(inst.Devices, inst.Profiles)
 
 			// Iterate through each of the instance's devices, looking for bridged NICs that are linked to
 			// networks specified.
@@ -2582,7 +2583,7 @@ func (n *bridge) getExternalSubnetInUse() ([]externalSubnetUsage, error) {
 	err = n.state.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		// Detect if there are any conflicting proxy devices on all instances with the to be created network forward
 		return tx.InstanceList(ctx, func(inst db.InstanceArgs, p api.Project) error {
-			devices := db.ExpandInstanceDevices(inst.Devices, inst.Profiles)
+			devices := instancetype.ExpandInstanceDevices(inst.Devices, inst.Profiles)
 
 			for devName, devConfig := range devices {
 				if devConfig["type"] != "proxy" {
@@ -2756,7 +2757,7 @@ func (n *bridge) ForwardCreate(forward api.NetworkForwardsPost, clientType reque
 							return nil // Managed bridge networks can only exist in default project.
 						}
 
-						devices := db.ExpandInstanceDevices(inst.Devices.Clone(), inst.Profiles)
+						devices := instancetype.ExpandInstanceDevices(inst.Devices.Clone(), inst.Profiles)
 
 						// Iterate through each of the instance's devices, looking for bridged NICs
 						// that are linked to this network.
