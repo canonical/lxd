b06108bc0b1fadaa7dcb84e31358ccd601470379
Mark Laing <mark.laing@canonical.com>
2024-02-07 08:56:16 +0000

    lxd: Remove Candid from list of supported authentication methods.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -211,11 +211,6 @@ func api10Get(d *Daemon, r *http.Request) response.Response {
 
 	// Get the authentication methods.
 	authMethods := []string{api.AuthenticationMethodTLS}
-	candidURL, _, _, _ := s.GlobalConfig.CandidServer()
-	rbacURL, _, _, _, _, _, _ := s.GlobalConfig.RBACServer()
-	if candidURL != "" || rbacURL != "" {
-		authMethods = append(authMethods, api.AuthenticationMethodCandid)
-	}
 
 	oidcIssuer, oidcClientID, _ := s.GlobalConfig.OIDCServer()
 	if oidcIssuer != "" && oidcClientID != "" {
