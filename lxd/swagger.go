a064c05b96c0ce99fb452d0c2a5e7723f0718ffd
Mark Laing <mark.laing@canonical.com>
2024-02-07 09:59:08 +0000

    lxd: Remove candid from swagger doc.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/swagger.go
+++ lxd/swagger.go
@@ -5,9 +5,7 @@
 //
 // The LXD API is available over both a local unix+http and remote https API.
 // Authentication for local users relies on group membership and access to the unix socket.
-// For remote users, the default authentication method is TLS client
-// certificates with a macaroon based (candid) authentication method also
-// supported.
+// For remote users, the default authentication method is TLS client.
 //
 //	Version: 1.0
 //	License: AGPL-3.0-only https://www.gnu.org/licenses/agpl-3.0.en.html
