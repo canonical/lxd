bb6c35e723bb06915bd2c687da26973e51285b50
Adam Stephens <adam@valkor.net>
2024-02-09 22:56:01 -0500

    completion: support returning non-lxd remotes
    Signed-off-by: Adam Stephens <adam@valkor.net>
    (cherry picked from commit 6fa9c4459e96da7bf024d9b6b02f300a8fe6c5f8)
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    License: Apache-2.0

--- lxc/network.go
+++ lxc/network.go
@@ -332,7 +332,7 @@ lxc network create bar network=baz --type ovn
 			return nil, cobra.ShellCompDirectiveNoFileComp
 		}
 
-		return c.global.cmpRemotes()
+		return c.global.cmpRemotes(false)
 	}
 
 	return cmd
@@ -1013,7 +1013,7 @@ func (c *cmdNetworkList) command() *cobra.Command {
 			return nil, cobra.ShellCompDirectiveNoFileComp
 		}
 
-		return c.global.cmpRemotes()
+		return c.global.cmpRemotes(false)
 	}
 
 	return cmd
