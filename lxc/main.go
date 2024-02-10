585e8a3d40d7b9cfaae99001402c6cc6d250fd88
Mark Laing <mark.laing@canonical.com>
2024-02-10 09:44:25 +0000

    Fix linter errors (revive: exported).
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxc/main.go
+++ lxc/main.go
@@ -309,6 +309,8 @@ To easily setup a local LXD server in a virtual machine, consider using: https:/
 	}
 }
 
+// PreRun is set as the (*cobra.Command).PersistentPreRunE for the top level lxc command. It loads configuration and
+// performs additional checks if it detects that LXD has not been configured yet.
 func (c *cmdGlobal) PreRun(cmd *cobra.Command, args []string) error {
 	var err error
 
@@ -423,6 +425,8 @@ Or for a virtual machine: lxc launch ubuntu:22.04 --vm`)+"\n")
 	return nil
 }
 
+// PostRun is set as the (*cobra.Command).PersistentPostRunE hook on the top level lxc command.
+// It saves any configuration that must persist between runs.
 func (c *cmdGlobal) PostRun(cmd *cobra.Command, args []string) error {
 	if c.conf != nil && shared.PathExists(c.confPath) {
 		// Save OIDC tokens on exit
@@ -438,6 +442,8 @@ type remoteResource struct {
 	name   string
 }
 
+// ParseServers parses a list of remotes (`<remote>:<resource>...`) and calls (*config.Config).GetInstanceServer
+// for each remote to configure a new connection.
 func (c *cmdGlobal) ParseServers(remotes ...string) ([]remoteResource, error) {
 	servers := map[string]lxd.InstanceServer{}
 	resources := []remoteResource{}
@@ -477,6 +483,7 @@ func (c *cmdGlobal) ParseServers(remotes ...string) ([]remoteResource, error) {
 	return resources, nil
 }
 
+// CheckArgs checks that the given list of arguments has length between minArgs and maxArgs.
 func (c *cmdGlobal) CheckArgs(cmd *cobra.Command, args []string, minArgs int, maxArgs int) (bool, error) {
 	if len(args) < minArgs || (maxArgs != -1 && len(args) > maxArgs) {
 		_ = cmd.Help()
