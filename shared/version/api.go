fac0b8017d5a2159b62496af94a96086978401dd
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-02-14 12:08:53 +0100

    shared/version: Adds a `resources_v3` API extension.
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -394,6 +394,7 @@ var APIExtensions = []string{
 	"server_version_lts",
 	"oidc_groups_claim",
 	"loki_config_instance",
+	"resources_v3",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
