5aa123bfe559c99f3691d748c6ed588cb74bd734
Stéphane Graber <stgraber@stgraber.org>
2024-02-16 15:35:45 -0500

    lxd/device: Bump base VM filesystem volume to 500MiB
    This avoids an issue on XFS where the base filesystem size is now 300MiB.
    
    Closes #498
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    (cherry picked from commit 1f54dbb29b56cfdbe7ab4fcbf9e5bb96fa632ba8)
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>
    License: Apache-2.0

--- lxd/device/config/consts.go
+++ lxd/device/config/consts.go
@@ -1,4 +1,4 @@
 package config
 
 // DefaultVMBlockFilesystemSize is the size of a VM root device block volume's associated filesystem volume.
-const DefaultVMBlockFilesystemSize = "100MiB"
+const DefaultVMBlockFilesystemSize = "500MiB"
