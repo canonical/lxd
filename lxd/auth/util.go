c0a548fd5dd9c43a16a5ddc66d3fb4d3cbf15556
Mark Laing <mark.laing@canonical.com>
2024-02-19 15:25:31 +0000

    lxd/auth: Add function to validate an authentication method.
    The identities API accepts the authentication method as a path argument,
    so this should be validate before allowing the request to continue.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/auth/util.go
+++ lxd/auth/util.go
@@ -2,10 +2,22 @@ package auth
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/canonical/lxd/shared"
+	"github.com/canonical/lxd/shared/api"
 	"github.com/canonical/lxd/shared/entity"
 )
+
+// ValidateAuthenticationMethod returns an api.StatusError with http.StatusBadRequest if the given authentication
+// method is not recognised.
+func ValidateAuthenticationMethod(authenticationMethod string) error {
+	if !shared.ValueInSlice(authenticationMethod, []string{api.AuthenticationMethodTLS, api.AuthenticationMethodOIDC}) {
+		return api.StatusErrorf(http.StatusBadRequest, "Unrecognized authentication method %q", authenticationMethod)
+	}
+
+	return nil
+}
 
 // ValidateEntitlement returns an error if the given Entitlement does not apply to the entity.Type.
 func ValidateEntitlement(entityType entity.Type, entitlement Entitlement) error {
