40925ddd3054e0c746a6113c8bfb0114a3dcd48a
Mark Laing <mark.laing@canonical.com>
2024-02-19 14:52:38 +0000

    lxd/db/cluster: Adds methods for populating api.IdentityProviderGroup data.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/identity_provider_groups.go
+++ lxd/db/cluster/identity_provider_groups.go
@@ -1,5 +1,15 @@
 package cluster
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/canonical/lxd/lxd/db/query"
+	"github.com/canonical/lxd/shared/api"
+)
+
 // Code generation directives.
 //
 //go:generate -command mapper lxd-generate db mapper -t identity_provider_groups.mapper.go
@@ -33,4 +43,101 @@ type IdentityProviderGroup struct {
 type IdentityProviderGroupFilter struct {
 	ID   *int
 	Name *string
+}
+
+// ToAPI converts the IdentityProviderGroup to an api.IdentityProviderGroup, making more database calls as necessary.
+func (i *IdentityProviderGroup) ToAPI(ctx context.Context, tx *sql.Tx) (*api.IdentityProviderGroup, error) {
+	idpGroup := &api.IdentityProviderGroup{
+		IdentityProviderGroupPost: api.IdentityProviderGroupPost{Name: i.Name},
+	}
+
+	groups, err := GetAuthGroupsByIdentityProviderGroupID(ctx, tx, i.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	groupNames := make([]string, 0, len(groups))
+	for _, group := range groups {
+		groupNames = append(groupNames, group.Name)
+	}
+
+	idpGroup.Groups = groupNames
+	return idpGroup, nil
+}
+
+// GetAuthGroupsByIdentityProviderGroupID returns a list of a groups that the identity provider group with the given ID.
+func GetAuthGroupsByIdentityProviderGroupID(ctx context.Context, tx *sql.Tx, idpGroupID int) ([]AuthGroup, error) {
+	stmt := `
+SELECT auth_groups.id, auth_groups.name, auth_groups.description 
+FROM auth_groups_identity_provider_groups 
+JOIN auth_groups ON auth_groups_identity_provider_groups.auth_group_id = auth_groups.id 
+WHERE auth_groups_identity_provider_groups.identity_provider_group_id = ?`
+
+	var result []AuthGroup
+	dest := func(scan func(dest ...any) error) error {
+		g := AuthGroup{}
+		err := scan(&g.ID, &g.Name, &g.Description)
+		if err != nil {
+			return err
+		}
+
+		result = append(result, g)
+
+		return nil
+	}
+
+	err := query.Scan(ctx, tx, stmt, dest, idpGroupID)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get group mappings for identity provider group with ID `%d`: %w", idpGroupID, err)
+	}
+
+	return result, nil
+}
+
+// SetIdentityProviderGroupMapping deletes all auth_group -> identity_provider_group mappings from the `ath_groups_identity_provider_groups` table
+// where the identity provider group ID is equal to the given value. Then it inserts new assocations into the table where the
+// group IDs correspond to the given group names.
+func SetIdentityProviderGroupMapping(ctx context.Context, tx *sql.Tx, identityProviderGroupID int, groupNames []string) error {
+	_, err := tx.ExecContext(ctx, `DELETE FROM auth_groups_identity_provider_groups WHERE identity_provider_group_id = ?`, identityProviderGroupID)
+	if err != nil {
+		return fmt.Errorf("Failed to delete existing identity provider group mappings: %w", err)
+	}
+
+	if len(groupNames) == 0 {
+		return nil
+	}
+
+	args := []any{identityProviderGroupID}
+	var builder strings.Builder
+	builder.WriteString(`
+INSERT INTO auth_groups_identity_provider_groups (auth_group_id, identity_provider_group_id)
+SELECT ?, auth_groups.id
+FROM auth_groups
+WHERE auth_groups.name IN (
+`)
+	for i, groupName := range groupNames {
+		if i == len(groupNames)-1 {
+			builder.WriteString(`?)`)
+		} else {
+			builder.WriteString(`?, `)
+		}
+
+		args = append(args, groupName)
+	}
+
+	res, err := tx.ExecContext(ctx, builder.String(), args...)
+	if err != nil {
+		return fmt.Errorf("Failed to write identity provider group mappings: %w", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Failed to check validity of identity provider group mapping creation: %w", err)
+	}
+
+	if int(rowsAffected) != len(groupNames) {
+		return fmt.Errorf("Failed to write expected number of rows to identity provider group association table (expected %d, got %d)", len(groupNames), rowsAffected)
+	}
+
+	return nil
 }
