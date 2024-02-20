lxc/incus

Move db storage volume functions to ClusterTx
Signed-off-by: Thomas Hipp <thomashipp@gmail.com>

internal/server/db/instances_test.go
@@ -441,20 +441,17 @@ func TestGetInstancePool(t *testing.T) {
 	dbCluster, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	var poolID int64
-
 	err := dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		var err error
+		poolID, err := tx.CreateStoragePool(ctx, "default", "", "dir", nil)
+		if err != nil {
+			return err
+		}
 
-		poolID, err = tx.CreateStoragePool(ctx, "default", "", "dir", nil)
+		_, err = tx.CreateStoragePoolVolume(ctx, "default", "c1", "", db.StoragePoolVolumeTypeContainer, poolID, nil, db.StoragePoolVolumeContentTypeFS, time.Now())
+		if err != nil {
+			return err
+		}
 
-		return err
-	})
-	require.NoError(t, err)
-	_, err = dbCluster.CreateStoragePoolVolume("default", "c1", "", db.StoragePoolVolumeTypeContainer, poolID, nil, db.StoragePoolVolumeContentTypeFS, time.Now())
-	require.NoError(t, err)
-
-	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		container := cluster.Instance{
 			Project: "default",
 			Name:    "c1",

internal/server/db/storage_pools_test.go
@@ -186,7 +186,13 @@ func TestStoragePoolVolume_Ceph(t *testing.T) {
 	})
 
 	config := map[string]string{"k": "v"}
-	volumeID, err := cluster.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, db.StoragePoolVolumeContentTypeFS, time.Now())
+	var volumeID int64
+
+	_ = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		volumeID, err = tx.CreateStoragePoolVolume(ctx, "default", "v1", "", 1, poolID, config, db.StoragePoolVolumeContentTypeFS, time.Now())
+
+		return err
+	})
 	require.NoError(t, err)
 
 	getStoragePoolVolume := func(volumeProjectName string, volumeName string, volumeType int, poolID int64) (*db.StorageVolume, error) {
@@ -212,21 +218,27 @@ func TestStoragePoolVolume_Ceph(t *testing.T) {
 
 	// Update the volume
 	config["k"] = "v2"
-	err = cluster.UpdateStoragePoolVolume("default", "v1", 1, poolID, "volume 1", config)
+	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		return tx.UpdateStoragePoolVolume(ctx, "default", "v1", 1, poolID, "volume 1", config)
+	})
 	require.NoError(t, err)
 	volume, err := getStoragePoolVolume("default", "v1", 1, poolID)
 	require.NoError(t, err)
 	assert.Equal(t, "volume 1", volume.Description)
 	assert.Equal(t, config, volume.Config)
 
-	err = cluster.RenameStoragePoolVolume("default", "v1", "v1-new", 1, poolID)
+	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		return tx.RenameStoragePoolVolume(ctx, "default", "v1", "v1-new", 1, poolID)
+	})
 	require.NoError(t, err)
 	volume, err = getStoragePoolVolume("default", "v1-new", 1, poolID)
 	require.NoError(t, err)
 	assert.NotNil(t, volume)
 
 	// Delete the volume
-	err = cluster.RemoveStoragePoolVolume("default", "v1-new", 1, poolID)
+	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		return tx.RemoveStoragePoolVolume(ctx, "default", "v1-new", 1, poolID)
+	})
 	require.NoError(t, err)
 	volume, err = getStoragePoolVolume("default", "v1-new", 1, poolID)
 	assert.True(t, response.IsNotFoundError(err))
@@ -250,27 +262,24 @@ func TestCreateStoragePoolVolume_Snapshot(t *testing.T) {
 		poolID1, err = tx.CreateStoragePool(ctx, "p2", "", "dir", nil)
 		require.NoError(t, err)
 
+		config := map[string]string{"k": "v"}
+
+		_, err = tx.CreateStoragePoolVolume(ctx, "default", "v1", "", 1, poolID, config, db.StoragePoolVolumeContentTypeFS, time.Now())
+		require.NoError(t, err)
+
+		_, err = tx.CreateStoragePoolVolume(ctx, "default", "v1", "", 1, poolID1, config, db.StoragePoolVolumeContentTypeFS, time.Now())
+		require.NoError(t, err)
+
+		config = map[string]string{"k": "v"}
+		_, err = tx.CreateStorageVolumeSnapshot(ctx, "default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
+		require.NoError(t, err)
+
+		n := tx.GetNextStorageVolumeSnapshotIndex(ctx, "p1", "v1", 1, "snap%d")
+		assert.Equal(t, n, 1)
+
+		n = tx.GetNextStorageVolumeSnapshotIndex(ctx, "p2", "v1", 1, "snap%d")
+		assert.Equal(t, n, 0)
+
 		return nil
 	})
-
-	config := map[string]string{"k": "v"}
-	_, err := cluster.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, db.StoragePoolVolumeContentTypeFS, time.Now())
-	require.NoError(t, err)
-
-	_, err = cluster.CreateStoragePoolVolume("default", "v1", "", 1, poolID1, config, db.StoragePoolVolumeContentTypeFS, time.Now())
-	require.NoError(t, err)
-
-	config = map[string]string{"k": "v"}
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		_, err = tx.CreateStorageVolumeSnapshot(ctx, "default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
-
-		return err
-	})
-	require.NoError(t, err)
-
-	n := cluster.GetNextStorageVolumeSnapshotIndex("p1", "v1", 1, "snap%d")
-	assert.Equal(t, n, 1)
-
-	n = cluster.GetNextStorageVolumeSnapshotIndex("p2", "v1", 1, "snap%d")
-	assert.Equal(t, n, 0)
 }

cmd/incusd/instance_test.go
@@ -180,7 +180,11 @@ func (suite *containerTestSuite) TestContainer_LoadFromDB() {
 	pool, err := storagePools.LoadByName(state, poolName)
 	suite.Req.Nil(err)
 
-	_, err = state.DB.Cluster.CreateStoragePoolVolume(c.Project().Name, c.Name(), "", db.StoragePoolVolumeContentTypeFS, pool.ID(), nil, db.StoragePoolVolumeContentTypeFS, time.Now())
+	err = state.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		_, err = tx.CreateStoragePoolVolume(ctx, c.Project().Name, c.Name(), "", db.StoragePoolVolumeContentTypeFS, pool.ID(), nil, db.StoragePoolVolumeContentTypeFS, time.Now())
+
+		return err
+	})
 	suite.Req.Nil(err)
 
 	// Load the container and trigger initLXC()

cmd/incusd/storage_volumes_snapshot.go
@@ -169,7 +169,14 @@ func storagePoolVolumeSnapshotsTypePost(d *Daemon, r *http.Request) response.Res
 
 	// Get a snapshot name.
 	if req.Name == "" {
-		i := s.DB.Cluster.GetNextStorageVolumeSnapshotIndex(poolName, volumeName, volumeType, "snap%d")
+		var i int
+
+		_ = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+			i = tx.GetNextStorageVolumeSnapshotIndex(ctx, poolName, volumeName, volumeType, "snap%d")
+
+			return nil
+		})
+
 		req.Name = fmt.Sprintf("snap%d", i)
 	}
 
@@ -387,21 +394,25 @@ func storagePoolVolumeSnapshotsTypeGet(d *Daemon, r *http.Request) response.Resp
 	}
 
 	var poolID int64
+	var volumes []db.StorageVolumeArgs
 
 	err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		var err error
 
 		// Retrieve ID of the storage pool (and check if the storage pool exists).
 		poolID, err = tx.GetStoragePoolID(ctx, poolName)
+		if err != nil {
+			return err
+		}
 
-		return err
+		// Get the names of all storage volume snapshots of a given volume.
+		volumes, err = tx.GetLocalStoragePoolVolumeSnapshotsWithType(ctx, projectName, volumeName, volumeType, poolID)
+		if err != nil {
+			return err
+		}
+
+		return nil
 	})
-	if err != nil {
-		return response.SmartError(err)
-	}
-
-	// Get the names of all storage volume snapshots of a given volume.
-	volumes, err := s.DB.Cluster.GetLocalStoragePoolVolumeSnapshotsWithType(projectName, volumeName, volumeType, poolID)
 	if err != nil {
 		return response.SmartError(err)
 	}
@@ -1466,7 +1477,14 @@ func volumeDetermineNextSnapshotName(s *state.State, volume db.StorageVolumeArgs
 	if count > 1 {
 		return "", fmt.Errorf("Snapshot pattern may contain '%%d' only once")
 	} else if count == 1 {
-		i := s.DB.Cluster.GetNextStorageVolumeSnapshotIndex(volume.PoolName, volume.Name, db.StoragePoolVolumeTypeCustom, pattern)
+		var i int
+
+		_ = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+			i = tx.GetNextStorageVolumeSnapshotIndex(ctx, volume.PoolName, volume.Name, db.StoragePoolVolumeTypeCustom, pattern)
+
+			return nil
+		})
+
 		return strings.Replace(pattern, "%d", strconv.Itoa(i), 1), nil
 	}
 
@@ -1506,13 +1524,20 @@ func volumeDetermineNextSnapshotName(s *state.State, volume db.StorageVolumeArgs
 			return "", err
 		}
 
-		for _, project := range projects {
-			snaps, err := s.DB.Cluster.GetLocalStoragePoolVolumeSnapshotsWithType(project, volume.Name, db.StoragePoolVolumeTypeCustom, poolID)
-			if err != nil {
-				return "", err
+		err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+			for _, project := range projects {
+				snaps, err := tx.GetLocalStoragePoolVolumeSnapshotsWithType(ctx, project, volume.Name, db.StoragePoolVolumeTypeCustom, poolID)
+				if err != nil {
+					return err
+				}
+
+				snapshots = append(snapshots, snaps...)
 			}
 
-			snapshots = append(snapshots, snaps...)
+			return nil
+		})
+		if err != nil {
+			return "", err
 		}
 	}
 
@@ -1526,7 +1551,14 @@ func volumeDetermineNextSnapshotName(s *state.State, volume db.StorageVolumeArgs
 	}
 
 	if snapshotExists {
-		i := s.DB.Cluster.GetNextStorageVolumeSnapshotIndex(volume.PoolName, volume.Name, db.StoragePoolVolumeTypeCustom, pattern)
+		var i int
+
+		_ = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+			i = tx.GetNextStorageVolumeSnapshotIndex(ctx, volume.PoolName, volume.Name, db.StoragePoolVolumeTypeCustom, pattern)
+
+			return nil
+		})
+
 		return strings.Replace(pattern, "%d", strconv.Itoa(i), 1), nil
 	}
 

canonical/lxd

(cherry picked from commit 66adc717447664555cfe30b11a8e3e8fa63c21a8)
Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>
License: Apache-2.0

lxd/db/instances_test.go
@@ -441,20 +441,17 @@ func TestGetInstancePool(t *testing.T) {
 	dbCluster, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	var poolID int64
-
 	err := dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		var err error
+		poolID, err := tx.CreateStoragePool(ctx, "default", "", "dir", nil)
+		if err != nil {
+			return err
+		}
 
-		poolID, err = tx.CreateStoragePool(ctx, "default", "", "dir", nil)
+		_, err = tx.CreateStoragePoolVolume(ctx, "default", "c1", "", cluster.StoragePoolVolumeTypeContainer, poolID, nil, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+		if err != nil {
+			return err
+		}
 
-		return err
-	})
-	require.NoError(t, err)
-	_, err = dbCluster.CreateStoragePoolVolume("default", "c1", "", cluster.StoragePoolVolumeTypeContainer, poolID, nil, cluster.StoragePoolVolumeContentTypeFS, time.Now())
-	require.NoError(t, err)
-
-	err = dbCluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		container := cluster.Instance{
 			Project: "default",
 			Name:    "c1",

lxd/db/storage_pools_test.go
@@ -187,7 +187,13 @@ func TestStoragePoolVolume_Ceph(t *testing.T) {
 	})
 
 	config := map[string]string{"k": "v"}
-	volumeID, err := clusterDB.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+	var volumeID int64
+
+	_ = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		volumeID, err = tx.CreateStoragePoolVolume(ctx, "default", "v1", "", 1, poolID, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+
+		return err
+	})
 	require.NoError(t, err)
 
 	getStoragePoolVolume := func(volumeProjectName string, volumeName string, volumeType int, poolID int64) (*db.StorageVolume, error) {
@@ -213,21 +219,27 @@ func TestStoragePoolVolume_Ceph(t *testing.T) {
 
 	// Update the volume
 	config["k"] = "v2"
-	err = clusterDB.UpdateStoragePoolVolume("default", "v1", 1, poolID, "volume 1", config)
+	err = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		return tx.UpdateStoragePoolVolume(ctx, "default", "v1", 1, poolID, "volume 1", config)
+	})
 	require.NoError(t, err)
 	volume, err := getStoragePoolVolume("default", "v1", 1, poolID)
 	require.NoError(t, err)
 	assert.Equal(t, "volume 1", volume.Description)
 	assert.Equal(t, config, volume.Config)
 
-	err = clusterDB.RenameStoragePoolVolume("default", "v1", "v1-new", 1, poolID)
+	err = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		return tx.RenameStoragePoolVolume(ctx, "default", "v1", "v1-new", 1, poolID)
+	})
 	require.NoError(t, err)
 	volume, err = getStoragePoolVolume("default", "v1-new", 1, poolID)
 	require.NoError(t, err)
 	assert.NotNil(t, volume)
 
 	// Delete the volume
-	err = clusterDB.RemoveStoragePoolVolume("default", "v1-new", 1, poolID)
+	err = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		return tx.RemoveStoragePoolVolume(ctx, "default", "v1-new", 1, poolID)
+	})
 	require.NoError(t, err)
 	volume, err = getStoragePoolVolume("default", "v1-new", 1, poolID)
 	assert.True(t, response.IsNotFoundError(err))
@@ -251,27 +263,24 @@ func TestCreateStoragePoolVolume_Snapshot(t *testing.T) {
 		poolID1, err = tx.CreateStoragePool(ctx, "p2", "", "dir", nil)
 		require.NoError(t, err)
 
+		config := map[string]string{"k": "v"}
+
+		_, err = tx.CreateStoragePoolVolume(ctx, "default", "v1", "", 1, poolID, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+		require.NoError(t, err)
+
+		_, err = tx.CreateStoragePoolVolume(ctx, "default", "v1", "", 1, poolID1, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+		require.NoError(t, err)
+
+		config = map[string]string{"k": "v"}
+		_, err = tx.CreateStorageVolumeSnapshot(ctx, "default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
+		require.NoError(t, err)
+
+		n := tx.GetNextStorageVolumeSnapshotIndex(ctx, "p1", "v1", 1, "snap%d")
+		assert.Equal(t, n, 1)
+
+		n = tx.GetNextStorageVolumeSnapshotIndex(ctx, "p2", "v1", 1, "snap%d")
+		assert.Equal(t, n, 0)
+
 		return nil
 	})
-
-	config := map[string]string{"k": "v"}
-	_, err := clusterDB.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
-	require.NoError(t, err)
-
-	_, err = clusterDB.CreateStoragePoolVolume("default", "v1", "", 1, poolID1, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
-	require.NoError(t, err)
-
-	config = map[string]string{"k": "v"}
-	err = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		_, err = tx.CreateStorageVolumeSnapshot(ctx, "default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
-
-		return err
-	})
-	require.NoError(t, err)
-
-	n := clusterDB.GetNextStorageVolumeSnapshotIndex("p1", "v1", 1, "snap%d")
-	assert.Equal(t, n, 1)
-
-	n = clusterDB.GetNextStorageVolumeSnapshotIndex("p2", "v1", 1, "snap%d")
-	assert.Equal(t, n, 0)
 }

lxd/instance_test.go
@@ -180,7 +180,11 @@ func (suite *containerTestSuite) TestContainer_LoadFromDB() {
 	pool, err := storagePools.LoadByName(state, poolName)
 	suite.Req.Nil(err)
 
-	_, err = state.DB.Cluster.CreateStoragePoolVolume(c.Project().Name, c.Name(), "", cluster.StoragePoolVolumeContentTypeFS, pool.ID(), nil, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+	err = state.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		_, err = tx.CreateStoragePoolVolume(ctx, c.Project().Name, c.Name(), "", cluster.StoragePoolVolumeContentTypeFS, pool.ID(), nil, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+
+		return err
+	})
 	suite.Req.Nil(err)
 
 	// Load the container and trigger initLXC()

lxd/storage_volumes_snapshot.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/canonical/lxd/lxd/auth"
 	"github.com/canonical/lxd/lxd/db"
+	"github.com/canonical/lxd/lxd/db/cluster"
 	dbCluster "github.com/canonical/lxd/lxd/db/cluster"
 	"github.com/canonical/lxd/lxd/db/operationtype"
 	"github.com/canonical/lxd/lxd/instance"
@@ -405,21 +406,25 @@ func storagePoolVolumeSnapshotsTypeGet(d *Daemon, r *http.Request) response.Resp
 	}
 
 	var poolID int64
+	var volumes []db.StorageVolumeArgs
 
 	err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		var err error
 
 		// Retrieve ID of the storage pool (and check if the storage pool exists).
 		poolID, err = tx.GetStoragePoolID(ctx, poolName)
+		if err != nil {
+			return err
+		}
 
-		return err
+		// Get the names of all storage volume snapshots of a given volume.
+		volumes, err = tx.GetLocalStoragePoolVolumeSnapshotsWithType(ctx, projectName, volumeName, volumeType, poolID)
+		if err != nil {
+			return err
+		}
+
+		return nil
 	})
-	if err != nil {
-		return response.SmartError(err)
-	}
-
-	// Get the names of all storage volume snapshots of a given volume.
-	volumes, err := s.DB.Cluster.GetLocalStoragePoolVolumeSnapshotsWithType(projectName, volumeName, volumeType, poolID)
 	if err != nil {
 		return response.SmartError(err)
 	}
@@ -1484,7 +1489,13 @@ func volumeDetermineNextSnapshotName(s *state.State, volume db.StorageVolumeArgs
 	if count > 1 {
 		return "", fmt.Errorf("Snapshot pattern may contain '%%d' only once")
 	} else if count == 1 {
-		i := s.DB.Cluster.GetNextStorageVolumeSnapshotIndex(volume.PoolName, volume.Name, dbCluster.StoragePoolVolumeTypeCustom, pattern)
+		var i int
+		_ = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+			i = tx.GetNextStorageVolumeSnapshotIndex(ctx, volume.PoolName, volume.Name, cluster.StoragePoolVolumeTypeCustom, pattern)
+
+			return nil
+		})
+
 		return strings.Replace(pattern, "%d", strconv.Itoa(i), 1), nil
 	}
 
@@ -1524,13 +1535,20 @@ func volumeDetermineNextSnapshotName(s *state.State, volume db.StorageVolumeArgs
 			return "", err
 		}
 
-		for _, project := range projects {
-			snaps, err := s.DB.Cluster.GetLocalStoragePoolVolumeSnapshotsWithType(project, volume.Name, dbCluster.StoragePoolVolumeTypeCustom, poolID)
-			if err != nil {
-				return "", err
+		err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+			for _, project := range projects {
+				snaps, err := tx.GetLocalStoragePoolVolumeSnapshotsWithType(ctx, project, volume.Name, cluster.StoragePoolVolumeTypeCustom, poolID)
+				if err != nil {
+					return err
+				}
+
+				snapshots = append(snapshots, snaps...)
 			}
 
-			snapshots = append(snapshots, snaps...)
+			return nil
+		})
+		if err != nil {
+			return "", err
 		}
 	}
 
@@ -1544,7 +1562,14 @@ func volumeDetermineNextSnapshotName(s *state.State, volume db.StorageVolumeArgs
 	}
 
 	if snapshotExists {
-		i := s.DB.Cluster.GetNextStorageVolumeSnapshotIndex(volume.PoolName, volume.Name, dbCluster.StoragePoolVolumeTypeCustom, pattern)
+		var i int
+
+		_ = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+			i = tx.GetNextStorageVolumeSnapshotIndex(ctx, volume.PoolName, volume.Name, cluster.StoragePoolVolumeTypeCustom, pattern)
+
+			return nil
+		})
+
 		return strings.Replace(pattern, "%d", strconv.Itoa(i), 1), nil
 	}
 