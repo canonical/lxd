lxc/incus

Move db storage pool functions to ClusterTx
Signed-off-by: Thomas Hipp <thomashipp@gmail.com>

internal/server/db/storage_pools_test.go
@@ -25,27 +25,31 @@ func TestGetStoragePoolsLocalConfigs(t *testing.T) {
 	cluster, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	// Create a storage pool named "local" (like the default clustering one), then delete it and create another one.
-	_, err := cluster.CreateStoragePool("local", "", "dir", map[string]string{
-		"rsync.bwlimit": "1",
-		"source":        "/foo/bar",
-	})
-	require.NoError(t, err)
+	_ = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		// Create a storage pool named "local" (like the default clustering one), then delete it and create another one.
+		_, err := tx.CreateStoragePool(ctx, "local", "", "dir", map[string]string{
+			"rsync.bwlimit": "1",
+			"source":        "/foo/bar",
+		})
+		require.NoError(t, err)
 
-	_, err = cluster.RemoveStoragePool("local")
-	require.NoError(t, err)
+		_, err = tx.RemoveStoragePool(ctx, "local")
+		require.NoError(t, err)
 
-	_, err = cluster.CreateStoragePool("BTRFS", "", "dir", map[string]string{
-		"rsync.bwlimit": "1",
-		"source":        "/egg/baz",
+		_, err = tx.CreateStoragePool(ctx, "BTRFS", "", "dir", map[string]string{
+			"rsync.bwlimit": "1",
+			"source":        "/egg/baz",
+		})
+		require.NoError(t, err)
+
+		return nil
 	})
-	require.NoError(t, err)
 
 	// Check that the config map returned by StoragePoolsConfigs actually
 	// contains the value of the "BTRFS" storage pool.
 	var config map[string]map[string]string
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		var err error
 		config, err = tx.GetStoragePoolsLocalConfig(ctx)
 		return err
@@ -172,8 +176,14 @@ func TestStoragePoolVolume_Ceph(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	poolID, err := cluster.CreateStoragePool("p1", "", "ceph", nil)
-	require.NoError(t, err)
+	var poolID int64
+
+	_ = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		poolID, err = tx.CreateStoragePool(ctx, "p1", "", "ceph", nil)
+		require.NoError(t, err)
+
+		return nil
+	})
 
 	config := map[string]string{"k": "v"}
 	volumeID, err := cluster.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, db.StoragePoolVolumeContentTypeFS, time.Now())
@@ -228,14 +238,23 @@ func TestCreateStoragePoolVolume_Snapshot(t *testing.T) {
 	cluster, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	poolID, err := cluster.CreateStoragePool("p1", "", "dir", nil)
-	require.NoError(t, err)
+	var poolID int64
+	var poolID1 int64
 
-	poolID1, err := cluster.CreateStoragePool("p2", "", "dir", nil)
-	require.NoError(t, err)
+	_ = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		var err error
+
+		poolID, err = tx.CreateStoragePool(ctx, "p1", "", "dir", nil)
+		require.NoError(t, err)
+
+		poolID1, err = tx.CreateStoragePool(ctx, "p2", "", "dir", nil)
+		require.NoError(t, err)
+
+		return nil
+	})
 
 	config := map[string]string{"k": "v"}
-	_, err = cluster.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, db.StoragePoolVolumeContentTypeFS, time.Now())
+	_, err := cluster.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, db.StoragePoolVolumeContentTypeFS, time.Now())
 	require.NoError(t, err)
 
 	_, err = cluster.CreateStoragePoolVolume("default", "v1", "", 1, poolID1, config, db.StoragePoolVolumeContentTypeFS, time.Now())

canonical/lxd

(cherry picked from commit cf5c42d2b6127d2c7da4706a009ce05d223cc268)
Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>
License: Apache-2.0

lxd/db/storage_pools_test.go
@@ -26,28 +26,31 @@ func TestGetStoragePoolsLocalConfigs(t *testing.T) {
 	cluster, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	// Create a storage pool named "local" (like the default LXD clustering
-	// one), then delete it and create another one.
-	_, err := cluster.CreateStoragePool("local", "", "dir", map[string]string{
-		"rsync.bwlimit": "1",
-		"source":        "/foo/bar",
-	})
-	require.NoError(t, err)
+	_ = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		// Create a storage pool named "local" (like the default clustering one), then delete it and create another one.
+		_, err := tx.CreateStoragePool(ctx, "local", "", "dir", map[string]string{
+			"rsync.bwlimit": "1",
+			"source":        "/foo/bar",
+		})
+		require.NoError(t, err)
 
-	_, err = cluster.RemoveStoragePool("local")
-	require.NoError(t, err)
+		_, err = tx.RemoveStoragePool(ctx, "local")
+		require.NoError(t, err)
 
-	_, err = cluster.CreateStoragePool("BTRFS", "", "dir", map[string]string{
-		"rsync.bwlimit": "1",
-		"source":        "/egg/baz",
+		_, err = tx.CreateStoragePool(ctx, "BTRFS", "", "dir", map[string]string{
+			"rsync.bwlimit": "1",
+			"source":        "/egg/baz",
+		})
+		require.NoError(t, err)
+
+		return nil
 	})
-	require.NoError(t, err)
 
 	// Check that the config map returned by StoragePoolsConfigs actually
 	// contains the value of the "BTRFS" storage pool.
 	var config map[string]map[string]string
 
-	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		var err error
 		config, err = tx.GetStoragePoolsLocalConfig(ctx)
 		return err
@@ -174,8 +177,14 @@ func TestStoragePoolVolume_Ceph(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	poolID, err := clusterDB.CreateStoragePool("p1", "", "ceph", nil)
-	require.NoError(t, err)
+	var poolID int64
+
+	_ = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		poolID, err = tx.CreateStoragePool(ctx, "p1", "", "ceph", nil)
+		require.NoError(t, err)
+
+		return nil
+	})
 
 	config := map[string]string{"k": "v"}
 	volumeID, err := clusterDB.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
@@ -230,14 +239,23 @@ func TestCreateStoragePoolVolume_Snapshot(t *testing.T) {
 	clusterDB, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	poolID, err := clusterDB.CreateStoragePool("p1", "", "dir", nil)
-	require.NoError(t, err)
+	var poolID int64
+	var poolID1 int64
 
-	poolID1, err := clusterDB.CreateStoragePool("p2", "", "dir", nil)
-	require.NoError(t, err)
+	_ = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		var err error
+
+		poolID, err = tx.CreateStoragePool(ctx, "p1", "", "dir", nil)
+		require.NoError(t, err)
+
+		poolID1, err = tx.CreateStoragePool(ctx, "p2", "", "dir", nil)
+		require.NoError(t, err)
+
+		return nil
+	})
 
 	config := map[string]string{"k": "v"}
-	_, err = clusterDB.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
+	_, err := clusterDB.CreateStoragePoolVolume("default", "v1", "", 1, poolID, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())
 	require.NoError(t, err)
 
 	_, err = clusterDB.CreateStoragePoolVolume("default", "v1", "", 1, poolID1, config, cluster.StoragePoolVolumeContentTypeFS, time.Now())