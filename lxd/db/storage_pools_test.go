lxc/incus

Move db volume snapshot functions to ClusterTx
Signed-off-by: Thomas Hipp <thomashipp@gmail.com>

internal/server/db/storage_pools_test.go
@@ -261,7 +261,11 @@ func TestCreateStoragePoolVolume_Snapshot(t *testing.T) {
 	require.NoError(t, err)
 
 	config = map[string]string{"k": "v"}
-	_, err = cluster.CreateStorageVolumeSnapshot("default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
+	err = cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		_, err = tx.CreateStorageVolumeSnapshot(ctx, "default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
+
+		return err
+	})
 	require.NoError(t, err)
 
 	n := cluster.GetNextStorageVolumeSnapshotIndex("p1", "v1", 1, "snap%d")

canonical/lxd

(cherry picked from commit 45d76977e5ddff0c894a174c532aaaf8878981ea)
Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>
License: Apache-2.0

lxd/db/storage_pools_test.go
@@ -262,7 +262,11 @@ func TestCreateStoragePoolVolume_Snapshot(t *testing.T) {
 	require.NoError(t, err)
 
 	config = map[string]string{"k": "v"}
-	_, err = clusterDB.CreateStorageVolumeSnapshot("default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
+	err = clusterDB.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		_, err = tx.CreateStorageVolumeSnapshot(ctx, "default", "v1/snap0", "", 1, poolID, config, time.Now(), time.Time{})
+
+		return err
+	})
 	require.NoError(t, err)
 
 	n := clusterDB.GetNextStorageVolumeSnapshotIndex("p1", "v1", 1, "snap%d")