lxc/incus

Move db network functions to ClusterTx
Signed-off-by: Thomas Hipp <thomashipp@gmail.com>

internal/server/db/networks_test.go
@@ -19,9 +19,13 @@ func TestGetNetworksLocalConfigs(t *testing.T) {
 	cluster, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	_, err := cluster.CreateNetwork(api.ProjectDefaultName, "incusbr0", "", db.NetworkTypeBridge, map[string]string{
-		"dns.mode":                   "none",
-		"bridge.external_interfaces": "vlan0",
+	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		_, err := tx.CreateNetwork(ctx, api.ProjectDefaultName, "incusbr0", "", db.NetworkTypeBridge, map[string]string{
+			"dns.mode":                   "none",
+			"bridge.external_interfaces": "vlan0",
+		})
+
+		return err
 	})
 	require.NoError(t, err)
 

internal/server/device/nictype/nictype.go
@@ -3,11 +3,14 @@
 package nictype
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/lxc/incus/internal/server/db"
 	deviceConfig "github.com/lxc/incus/internal/server/device/config"
 	"github.com/lxc/incus/internal/server/project"
 	"github.com/lxc/incus/internal/server/state"
+	"github.com/lxc/incus/shared/api"
 )
 
 // NICType resolves the NIC Type for the supplied NIC device config.
@@ -24,7 +27,13 @@ func NICType(s *state.State, deviceProjectName string, d deviceConfig.Device) (s
 				return "", fmt.Errorf("Failed to translate device project %q into network project: %w", deviceProjectName, err)
 			}
 
-			_, netInfo, _, err := s.DB.Cluster.GetNetworkInAnyState(networkProjectName, d["network"])
+			var netInfo *api.Network
+
+			err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+				_, netInfo, _, err = tx.GetNetworkInAnyState(ctx, networkProjectName, d["network"])
+
+				return err
+			})
 			if err != nil {
 				return "", fmt.Errorf("Failed to load network %q for project %q: %w", d["network"], networkProjectName, err)
 			}

canonical/lxd

(cherry picked from commit 6b689bb160d4079c54de44dc330c10583e76f76f)
Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>
License: Apache-2.0

lxd/db/networks_test.go
@@ -19,9 +19,13 @@ func TestGetNetworksLocalConfigs(t *testing.T) {
 	cluster, cleanup := db.NewTestCluster(t)
 	defer cleanup()
 
-	_, err := cluster.CreateNetwork(api.ProjectDefaultName, "lxdbr0", "", db.NetworkTypeBridge, map[string]string{
-		"dns.mode":                   "none",
-		"bridge.external_interfaces": "vlan0",
+	err := cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+		_, err := tx.CreateNetwork(ctx, api.ProjectDefaultName, "lxdbr0", "", db.NetworkTypeBridge, map[string]string{
+			"dns.mode":                   "none",
+			"bridge.external_interfaces": "vlan0",
+		})
+
+		return err
 	})
 	require.NoError(t, err)
 

lxd/device/nictype/nictype.go
@@ -3,11 +3,14 @@
 package nictype
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/canonical/lxd/lxd/db"
 	deviceConfig "github.com/canonical/lxd/lxd/device/config"
 	"github.com/canonical/lxd/lxd/project"
 	"github.com/canonical/lxd/lxd/state"
+	"github.com/canonical/lxd/shared/api"
 )
 
 // NICType resolves the NIC Type for the supplied NIC device config.
@@ -24,7 +27,13 @@ func NICType(s *state.State, deviceProjectName string, d deviceConfig.Device) (s
 				return "", fmt.Errorf("Failed to translate device project %q into network project: %w", deviceProjectName, err)
 			}
 
-			_, netInfo, _, err := s.DB.Cluster.GetNetworkInAnyState(networkProjectName, d["network"])
+			var netInfo *api.Network
+
+			err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+				_, netInfo, _, err = tx.GetNetworkInAnyState(ctx, networkProjectName, d["network"])
+
+				return err
+			})
 			if err != nil {
 				return "", fmt.Errorf("Failed to load network %q for project %q: %w", d["network"], networkProjectName, err)
 			}