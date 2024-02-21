75704ebf4224e4752ee593a01c381ef5b484e7b4
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-02-21 11:02:06 +0100

    lxd/instance/drivers/qemu: Revert linter fix
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -1359,7 +1359,6 @@ func (d *qemu) start(stateful bool, op *operationlock.InstanceOperation) error {
 		d.logger.Warn("Unable to use virtio-fs for config drive, using 9p as a fallback", logger.Ctx{"err": errUnsupported})
 
 		if errUnsupported == device.ErrMissingVirtiofsd {
-			// Create a warning if virtiofsd is missing.
 			_ = d.state.DB.Cluster.UpsertWarning(d.node, d.project.Name, dbCluster.TypeInstance, d.ID(), warningtype.MissingVirtiofsd, "Using 9p as a fallback")
 		} else {
 			// Resolve previous warning.
