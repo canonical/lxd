5ea54a50e31b0bbffad86c65833103cad932f3e3
Mark Laing <mark.laing@canonical.com>
2024-03-01 10:39:19 +0000

    lxd/db/cluster: Add GroupID field to Permission.
    Additionally, remove code generation directives.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/permissions.go
+++ lxd/db/cluster/permissions.go
@@ -11,34 +11,13 @@ import (
 	"github.com/canonical/lxd/shared/entity"
 )
 
-// Code generation directives.
-//
-//go:generate -command mapper lxd-generate db mapper -t permissions.mapper.go
-//go:generate mapper reset -i -b "//go:build linux && cgo && !agent"
-//
-//go:generate mapper stmt -e permission objects
-//go:generate mapper stmt -e permission objects-by-ID
-//go:generate mapper stmt -e permission objects-by-EntityType
-//go:generate mapper stmt -e permission objects-by-EntityType-and-EntityID
-//go:generate mapper stmt -e permission objects-by-EntityType-and-EntityID-and-Entitlement
-//
-//go:generate mapper method -i -e permission GetMany
-//go:generate mapper method -i -e permission GetOne
-
 // Permission is the database representation of an api.Permission.
 type Permission struct {
 	ID          int
-	Entitlement auth.Entitlement `db:"primary=true"`
-	EntityType  EntityType       `db:"primary=true"`
-	EntityID    int              `db:"primary=true"`
-}
-
-// PermissionFilter contains the fields upon which a Permission may be filtered.
-type PermissionFilter struct {
-	ID          *int
-	Entitlement *auth.Entitlement
-	EntityType  *EntityType
-	EntityID    *int
+	GroupID     int
+	Entitlement auth.Entitlement
+	EntityType  EntityType
+	EntityID    int
 }
 
 // GetPermissionEntityURLs accepts a slice of Permission and returns a map of entity.Type, to entity ID, to api.URL.
