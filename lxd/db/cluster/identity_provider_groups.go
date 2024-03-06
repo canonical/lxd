a696aa6d3d33e286e7c648f175f1125b1a2aece0
Din Music <din.music@canonical.com>
2024-03-06 18:01:01 +0100

    lxd/db/cluster/identity_provider_groups: Remove trailing spaces from SQL queries
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/db/cluster/identity_provider_groups.go
+++ lxd/db/cluster/identity_provider_groups.go
@@ -71,9 +71,9 @@ func (i *IdentityProviderGroup) ToAPI(ctx context.Context, tx *sql.Tx, canViewGr
 // GetAuthGroupsByIdentityProviderGroupID returns a list of a groups that the identity provider group with the given ID.
 func GetAuthGroupsByIdentityProviderGroupID(ctx context.Context, tx *sql.Tx, idpGroupID int) ([]AuthGroup, error) {
 	stmt := `
-SELECT auth_groups.id, auth_groups.name, auth_groups.description 
-FROM auth_groups_identity_provider_groups 
-JOIN auth_groups ON auth_groups_identity_provider_groups.auth_group_id = auth_groups.id 
+SELECT auth_groups.id, auth_groups.name, auth_groups.description
+FROM auth_groups_identity_provider_groups
+JOIN auth_groups ON auth_groups_identity_provider_groups.auth_group_id = auth_groups.id
 WHERE auth_groups_identity_provider_groups.identity_provider_group_id = ?`
 
 	var result []AuthGroup
