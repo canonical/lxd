6fd968e85f5a7b725026cc3c94ab915c70128d5a
Ruth Fuchss <ruth.fuchss@canonical.com>
2024-03-20 11:59:06 +0100

    doc/explanation: reorder pages and update the landing page
    Also combine the two different explanation topics about instance
    types into one.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- doc/custom_conf.py
+++ doc/custom_conf.py
@@ -134,6 +134,7 @@ slug = "lxd"
 redirects = {
     'howto/instances_snapshots/index': '../instances_backup/',
     'reference/network_external/index': '../networks/',
+    'explanation/containers_and_vms/index': '../instances/'
 }
 
 ############################################################
