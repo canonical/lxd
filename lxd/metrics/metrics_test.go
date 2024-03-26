d3b5ab7b66a7313da508e9876c0ec9d65e7770f9
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-03-26 11:51:18 +0100

    lxd/metrics: Use label aware permission check when filtering samples
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/metrics/metrics_test.go
+++ lxd/metrics/metrics_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/canonical/lxd/shared/api"
 	"github.com/canonical/lxd/shared/entity"
 )
 
@@ -20,21 +19,21 @@ func TestMetricSet_FilterSamples(t *testing.T) {
 	}
 
 	m := newMetricSet()
-	permissionChecker := func(u *api.URL) bool {
-		return u.String() == entity.InstanceURL("default", "jammy").String()
+	filter := func(labels map[string]string) bool {
+		return entity.InstanceURL(labels["project"], labels["name"]).String() == entity.InstanceURL("default", "jammy").String()
 	}
 
-	m.FilterSamples(permissionChecker)
+	m.FilterSamples(filter)
 
 	// Should still contain the sample
 	require.Equal(t, []Sample{{Value: 10, Labels: labels}}, m.set[CPUSecondsTotal])
 
 	m = newMetricSet()
-	permissionChecker = func(u *api.URL) bool {
-		return u.String() == entity.InstanceURL("not-default", "jammy").String()
+	filter = func(labels map[string]string) bool {
+		return entity.InstanceURL(labels["project"], labels["name"]).String() == entity.InstanceURL("not-default", "jammy").String()
 	}
 
-	m.FilterSamples(permissionChecker)
+	m.FilterSamples(filter)
 
 	// Should no longer contain the sample.
 	require.Equal(t, []Sample{}, m.set[CPUSecondsTotal])
