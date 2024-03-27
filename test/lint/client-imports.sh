9fc35bfd021b9690dc870fa19a0f1b957a8378cb
Simon Deziel <simon.deziel@canonical.com>
2024-03-27 17:42:27 -0400

    test/lint/client-imports: export LC_ALL for predictable sorting
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/lint/client-imports.sh
+++ test/lint/client-imports.sh
@@ -3,6 +3,9 @@ set -eu
 
 echo "Checking for imports that have been added to client or shared..."
 
+# Ensure predictable sorting
+export LC_ALL=C.UTF-8
+
 OUT=$(go list -f '{{ join .Imports "\n" }}' ./client ./shared/api ./lxc/config | grep -F . | sort -u | diff -u test/client-godeps.list - || true)
 if [ -n "${OUT}" ]; then
   echo "ERROR: you added a new dependency to the client or shared; please make sure this is what you want"
