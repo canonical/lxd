75d910956b6e87876f2aebfbc5809dbaaeac3f93
Stéphane Graber <stgraber@stgraber.org>
2024-03-28 21:09:11 -0400

    lxd: respect cluster groups in evacuateClusterSelectTarget()
    Closes https://github.com/lxc/incus/issues/483
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    (cherry picked from commit 7fbb7bc18adb35bc1a34a226aa9af92efb195a5e)
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    License: Apache-2.0

--- lxd/api_cluster.go
+++ lxd/api_cluster.go
@@ -4471,9 +4471,25 @@ func evacuateClusterSelectTarget(ctx context.Context, s *state.State, inst insta
 			return fmt.Errorf("Failed getting cluster members: %w", err)
 		}
 
+		// Filter candidates by group if needed.
+		_, group := shared.TargetDetect(inst.LocalConfig()["volatile.cluster.target"])
+		if group != "" {
+			newMembers := make([]db.NodeInfo, 0, len(allMembers))
+			for _, member := range allMembers {
+				if !slices.Contains(member.Groups, group) {
+					continue
+				}
+
+				newMembers = append(newMembers, member)
+			}
+
+			allMembers = newMembers
+		}
+
 		instProject := inst.Project()
 		clusterGroupsAllowed := limits.GetRestrictedClusterGroups(&instProject)
 
+		// Filter offline servers.
 		candidateMembers, err = tx.GetCandidateMembers(ctx, allMembers, []int{inst.Architecture()}, "", clusterGroupsAllowed, s.GlobalConfig.OfflineThreshold())
 		if err != nil {
 			return err
