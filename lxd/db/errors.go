f69e2c35e09bbb870b88728c5b020f335b086aa8
Mark Laing <mark.laing@canonical.com>
2024-04-03 10:20:37 +0100

    lxd/db: Remove `ErrAlreadyDefined` sentinel error.
    This error is imported by the `lxd/request` package so that it can
    convert it to an `http.StatusConflict` on a call to
    `response.SmartError`. Other applications are using the `lxd/request`
    package. Removing this error prevents the import of `lxd/db` into
    `lxd/request` and therefore prevents the import of `lxd/db` into
    downstream repositories.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/errors.go
+++ lxd/db/errors.go
@@ -5,10 +5,6 @@ import (
 )
 
 var (
-	// ErrAlreadyDefined hapens when the given entry already exists,
-	// for example a container.
-	ErrAlreadyDefined = fmt.Errorf("The record already exists")
-
 	// ErrNoClusterMember is used to indicate no cluster member has been found for a resource.
 	ErrNoClusterMember = fmt.Errorf("No cluster member found")
 )
