11799091a37053a4f1c44d12439e37464af90882
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2024-04-04 16:28:08 +0200

    instance/driver_lxc: get rid of redunant calls to cgroup.TaskSchedulerTrigger
    Calls to cgroup.TaskSchedulerTrigger can be very costly, let's avoid
    calling it without real reason. We only have to call it when CPU limits
    were changed for the instance.
    
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>

--- lxd/instance/drivers/driver_lxc.go
+++ lxd/instance/drivers/driver_lxc.go
@@ -4194,7 +4194,6 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 			d.release()
 			d.cConfig = false
 			_, _ = d.initLXC(true)
-			cgroup.TaskSchedulerTrigger("container", d.name, "changed")
 		}
 	}()
 
@@ -4392,6 +4391,8 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 			return err
 		}
 	}
+
+	cpuLimitWasChanged := false
 
 	// Apply the live changes
 	if isRunning {
@@ -4645,8 +4646,7 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 					return err
 				}
 			} else if key == "limits.cpu" || key == "limits.cpu.nodes" {
-				// Trigger a scheduler re-run
-				cgroup.TaskSchedulerTrigger("container", d.name, "changed")
+				cpuLimitWasChanged = true
 			} else if key == "limits.cpu.priority" || key == "limits.cpu.allowance" {
 				// Skip if no cpu CGroup
 				if !d.state.OS.CGInfo.Supports(cgroup.CPU, cg) {
@@ -4847,6 +4847,11 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 
 	// Success, update the closure to mark that the changes should be kept.
 	undoChanges = false
+
+	if cpuLimitWasChanged {
+		// Trigger a scheduler re-run
+		cgroup.TaskSchedulerTrigger("container", d.name, "changed")
+	}
 
 	if userRequested {
 		if d.isSnapshot {
