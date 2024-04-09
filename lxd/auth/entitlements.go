9444a399ff21dcb30deef8399a95a0e50ff9e6c7
Mark Laing <mark.laing@canonical.com>
2024-04-09 20:00:08 +0100

    lxd/auth: Updates entitlement methods to use an `entity.TypeName` argument.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/auth/entitlements.go
+++ lxd/auth/entitlements.go
@@ -10,7 +10,7 @@ import (
 )
 
 // ValidateEntitlement returns an error if the given Entitlement does not apply to the entity.Type.
-func ValidateEntitlement(entityType entity.Type, entitlement Entitlement) error {
+func ValidateEntitlement(entityType entity.TypeName, entitlement Entitlement) error {
 	entitlements := EntitlementsByEntityType(entityType)
 	if len(entitlements) == 0 {
 		return fmt.Errorf("No entitlements can be granted against entities of type %q", entityType)
@@ -23,7 +23,7 @@ func ValidateEntitlement(entityType entity.Type, entitlement Entitlement) error 
 	return nil
 }
 
-// EntitlementsByEntityType returns a list of available Entitlement for the entity.Type.
-func EntitlementsByEntityType(entityType entity.Type) []Entitlement {
-	return EntityTypeToEntitlements[entityType]
+// EntitlementsByEntityType returns a list of available Entitlement for the entity.TypeName.
+func EntitlementsByEntityType(entityType entity.TypeName) []Entitlement {
+	return entityTypeToEntitlements[entityType]
 }
