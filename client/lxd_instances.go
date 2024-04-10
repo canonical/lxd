ed8ea463271b033bea7e431a26a16edc36996524
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-04-10 15:49:56 -0500

    client: Fix permissions extension typo
    Fixes #12724
    
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- client/lxd_instances.go
+++ client/lxd_instances.go
@@ -1567,7 +1567,7 @@ func (r *ProtocolLXD) CreateInstanceFile(instanceName string, filePath string, a
 		modifyPerm = append(modifyPerm, "mode")
 	}
 
-	if len(modifyPerm) != 0 && r.CheckExtension("instance_files_modify_permissions") == nil {
+	if len(modifyPerm) != 0 && r.CheckExtension("instances_files_modify_permissions") == nil {
 		req.Header.Set("X-LXD-modify-perm", strings.Join(modifyPerm, ","))
 	}
 
