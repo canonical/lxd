77a4aa71c5414a4740d19f2d3721577b2935bfcd
Thomas Parrott <thomas.parrott@canonical.com>
2024-04-12 13:29:28 +0100

    lxd/api/metrics: Avoid repeated calls to wg.Add in  metricsGet
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/api_metrics.go
+++ lxd/api_metrics.go
@@ -275,8 +275,8 @@ func metricsGet(d *Daemon, r *http.Request) response.Response {
 	}
 
 	// Fetch what's missing.
+	wg.Add(len(instances))
 	for _, inst := range instances {
-		wg.Add(1)
 		instMetricsCh <- inst
 	}
 
