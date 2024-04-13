5562284d96e186864c530de1c415772ef06e9f51
Mohamed Ilaiwi <burkmoh@gmail.com>
2024-04-13 12:10:06 -0500

    client: Add `GetNetworkZonesAllProjects`
    Signed-off-by: Mohamed Ilaiwi <burkmoh@gmail.com>
    (cherry picked from commit 48f3de4b84402faef1741e595292d6d284fd830b)
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    License: Apache-2.0

--- client/lxd_network_zones.go
+++ client/lxd_network_zones.go
@@ -44,6 +44,23 @@ func (r *ProtocolLXD) GetNetworkZones() ([]api.NetworkZone, error) {
 	return zones, nil
 }
 
+// GetNetworkZonesAllProjects returns a list of network zones across all projects as NetworkZone structs.
+func (r *ProtocolLXD) GetNetworkZonesAllProjects() ([]api.NetworkZone, error) {
+	err := r.CheckExtension("network_zones_all_projects")
+	if err != nil {
+		return nil, err
+	}
+
+	zones := []api.NetworkZone{}
+	uri := "/network-zones?all-projects=true&recursion=1"
+	_, err = r.queryStruct("GET", uri, nil, "", &zones)
+	if err != nil {
+		return nil, err
+	}
+
+	return zones, nil
+}
+
 // GetNetworkZone returns a Network zone entry for the provided name.
 func (r *ProtocolLXD) GetNetworkZone(name string) (*api.NetworkZone, string, error) {
 	err := r.CheckExtension("network_dns")
