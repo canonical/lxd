3c47bd1024acb463a8d815b79933ddd666cb466a
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-04-19 12:47:27 +0200

    lxd_metadata: Annotate network peering properties
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- shared/api/network_peer.go
+++ shared/api/network_peer.go
@@ -8,13 +8,34 @@ package api
 type NetworkPeersPost struct {
 	NetworkPeerPut `yaml:",inline"`
 
+	// lxdmeta:generate(entities=network-peering; group=peering-properties; key=name)
+	//
+	// ---
+	//  type: string
+	//  required: yes
+	//  shortdesc: Name of the network peering on the local network
+
 	// Name of the peer
 	// Example: project1-network1
 	Name string `json:"name" yaml:"name"`
 
+	// lxdmeta:generate(entities=network-peering; group=peering-properties; key=target_project)
+	// This option must be set at create time.
+	// ---
+	//  type: string
+	//  required: yes
+	//  shortdesc: Which project the target network exists in
+
 	// Name of the target project
 	// Example: project1
 	TargetProject string `json:"target_project" yaml:"target_project"`
+
+	// lxdmeta:generate(entities=network-peering; group=peering-properties; key=target_network)
+	// This option must be set at create time.
+	// ---
+	//  type: string
+	//  required: yes
+	//  shortdesc: Which network to create a peering with
 
 	// Name of the target network
 	// Example: network1
@@ -27,9 +48,23 @@ type NetworkPeersPost struct {
 //
 // API extension: network_peer.
 type NetworkPeerPut struct {
+	// lxdmeta:generate(entities=network-peering; group=peering-properties; key=description)
+	//
+	// ---
+	//  type: string
+	//  required: no
+	//  shortdesc: Description of the network peering
+
 	// Description of the peer
 	// Example: Peering with network1 in project1
 	Description string `json:"description" yaml:"description"`
+
+	// lxdmeta:generate(entities=network-peering; group=peering-properties; key=config)
+	// The only supported keys are `user.*` custom keys.
+	// ---
+	//  type: string set
+	//  required: no
+	//  shortdesc: User-provided free-form key/value pairs
 
 	// Peer configuration map (refer to doc/network-peers.md)
 	// Example: {"user.mykey": "foo"}
@@ -60,6 +95,14 @@ type NetworkPeer struct {
 	// Read only: true
 	// Example: network1
 	TargetNetwork string `json:"target_network" yaml:"target_network"`
+
+	// lxdmeta:generate(entities=network-peering; group=peering-properties; key=status)
+	// Indicates if mutual peering exists with the target network.
+	// This property is read-only and cannot be updated.
+	// ---
+	//  type: string
+	//  required: --
+	//  shortdesc: Status indicating if pending or created
 
 	// The state of the peering
 	// Read only: true
