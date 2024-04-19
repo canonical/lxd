92b56fa340b9a1fd44dd78fc479ed1bd9c4bf1ea
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-04-19 12:47:01 +0200

    lxd_metadadta: Annotate network load balancer properties
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>
    Co-authored-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- shared/api/network_load_balancer.go
+++ shared/api/network_load_balancer.go
@@ -11,17 +11,46 @@ import (
 //
 // API extension: network_load_balancer.
 type NetworkLoadBalancerBackend struct {
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-backend-properties; key=name)
+	//
+	// ---
+	//  type: string
+	//  required: yes
+	//  shortdesc: Name of the backend
+
 	// Name of the load balancer backend
 	// Example: c1-http
 	Name string `json:"name" yaml:"name"`
+
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-backend-properties; key=description)
+	//
+	// ---
+	//  type: string
+	//  required: no
+	//  shortdesc: Description of the backend
 
 	// Description of the load balancer backend
 	// Example: C1 webserver
 	Description string `json:"description" yaml:"description"`
 
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-backend-properties; key=target_port)
+	// For example: `70,80-90` or `90`
+	// ---
+	//  type: string
+	//  required: no
+	//  defaultdesc: same as {config:option}`network-load-balancer-load-balancer-port-properties:listen_port`
+	//  shortdesc: Target port or ports
+
 	// TargetPort(s) to forward ListenPorts to (allows for many-to-one)
 	// Example: 80,81,8080-8090
 	TargetPort string `json:"target_port" yaml:"target_port"`
+
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-backend-properties; key=target_address)
+	//
+	// ---
+	//  type: string
+	//  required: yes
+	//  shortdesc: IP address to forward to
 
 	// TargetAddress to forward ListenPorts to
 	// Example: 198.51.100.2
@@ -53,17 +82,45 @@ func (b *NetworkLoadBalancerBackend) Normalise() {
 //
 // API extension: network_load_balancer.
 type NetworkLoadBalancerPort struct {
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-port-properties; key=description)
+	//
+	// ---
+	//  type: string
+	//  required: no
+	//  shortdesc: Description of the port or ports
+
 	// Description of the load balancer port
 	// Example: My web server load balancer
 	Description string `json:"description" yaml:"description"`
+
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-port-properties; key=protocol)
+	// Possible values are `tcp` and `udp`.
+	// ---
+	//  type: string
+	//  required: yes
+	//  shortdesc: Protocol for the port or ports
 
 	// Protocol for load balancer port (either tcp or udp)
 	// Example: tcp
 	Protocol string `json:"protocol" yaml:"protocol"`
 
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-port-properties; key=listen_port)
+	// For example: `80,90-100`
+	// ---
+	//  type: string
+	//  required: yes
+	//  shortdesc: Listen port or ports
+
 	// ListenPort(s) of load balancer (comma delimited ranges)
 	// Example: 80,81,8080-8090
 	ListenPort string `json:"listen_port" yaml:"listen_port"`
+
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-port-properties; key=target_backend)
+	//
+	// ---
+	//  type: backend list
+	//  required: yes
+	//  shortdesc: Backend name or names to forward to
 
 	// TargetBackend backend names to load balance ListenPorts to
 	// Example: ["c1-http","c2-http"]
@@ -92,6 +149,13 @@ func (p *NetworkLoadBalancerPort) Normalise() {
 type NetworkLoadBalancersPost struct {
 	NetworkLoadBalancerPut `yaml:",inline"`
 
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-properties; key=listen_address)
+	//
+	// ---
+	//  type: string
+	//  required: no
+	//  shortdesc: IP address to listen on
+
 	// The listen address of the load balancer
 	// Example: 192.0.2.1
 	ListenAddress string `json:"listen_address" yaml:"listen_address"`
@@ -103,16 +167,44 @@ type NetworkLoadBalancersPost struct {
 //
 // API extension: network_load_balancer.
 type NetworkLoadBalancerPut struct {
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-properties; key=description)
+	//
+	// ---
+	//  type: string
+	//  required: no
+	//  shortdesc: Description of the network load balancer
+
 	// Description of the load balancer listen IP
 	// Example: My public IP load balancer
 	Description string `json:"description" yaml:"description"`
+
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-properties; key=config)
+	// The only supported keys are `user.*` custom keys.
+	// ---
+	//  type: string set
+	//  required: no
+	//  shortdesc: User-provided free-form key/value pairs
 
 	// Load balancer configuration map (refer to doc/network-load-balancers.md)
 	// Example: {"user.mykey": "foo"}
 	Config map[string]string `json:"config" yaml:"config"`
 
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-properties; key=backends)
+	// See {ref}`network-load-balancers-backend-specifications`.
+	// ---
+	//  type: backend list
+	//  required: no
+	//  shortdesc: List of backend specifications
+
 	// Backends (optional)
 	Backends []NetworkLoadBalancerBackend `json:"backends" yaml:"backends"`
+
+	// lxdmeta:generate(entities=network-load-balancer; group=load-balancer-properties; key=ports)
+	// See {ref}`network-load-balancers-port-specifications`.
+	// ---
+	//  type: port list
+	//  required: no
+	//  shortdesc: List of port specifications
 
 	// Port forwards (optional)
 	Ports []NetworkLoadBalancerPort `json:"ports" yaml:"ports"`
