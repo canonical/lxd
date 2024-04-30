c54ddf14933b2dabc5476aa3f5ef3cb1f5f46add
hamistao <pedro.ribeiro@canonical.com>
2024-04-30 11:07:44 -0300

    lxd/storage: Add comments for metadata generation
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>
    (cherry picked from commit 362734e5ae886b43310b9a42fa01cec4345cbe73)

--- lxd/storage/utils.go
+++ lxd/storage/utils.go
@@ -539,6 +539,14 @@ func poolAndVolumeCommonRules(vol *drivers.Volume) map[string]func(string) error
 
 	// security.shared is only relevant for custom block volumes.
 	if (vol == nil) || (vol != nil && vol.Type() == drivers.VolumeTypeCustom && vol.ContentType() == drivers.ContentTypeBlock) {
+		// lxdmeta:generate(entities=storage-btrfs,storage-cephfs,storage-ceph,storage-dir,storage-lvm,storage-zfs,storage-powerflex; group=volume-conf; key=security.shared)
+		// Enabling this option allows sharing the volume across multiple instances despite the possibility of data loss.
+		//
+		// ---
+		//  type: bool
+		//  condition: custom block volume
+		//  defaultdesc: same as `volume.security.shared` or `false`
+		//  shortdesc: Enable volume sharing
 		rules["security.shared"] = validate.Optional(validate.IsBool)
 	}
 
