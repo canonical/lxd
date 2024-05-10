642f05da8e2b9c5b5eaf6730caadc8e0ee066198
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-05-10 16:52:29 -0500

    lxd/storage/drivers: Refactor volume size rounding logic
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- lxd/storage/drivers/utils.go
+++ lxd/storage/drivers/utils.go
@@ -876,3 +876,19 @@ func wipeBlockHeaders(path string) error {
 func IsContentBlock(contentType ContentType) bool {
 	return contentType == ContentTypeBlock || contentType == ContentTypeISO
 }
+
+// roundAbove returns the next multiple of `above` greater than `val`.
+func roundAbove(above, val int64) int64 {
+	if val < above {
+		val = above
+	}
+
+	rounded := int64(val/above) * above
+
+	// Ensure the rounded size is at least x.
+	if rounded < val {
+		rounded += above
+	}
+
+	return rounded
+}
