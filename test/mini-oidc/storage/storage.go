467959a5e68ad3a7bf995122c4cd8808c93bc3dc
Thomas Parrott <thomas.parrott@canonical.com>
2024-06-06 13:30:23 +0100

    test/mini-oidc: Comment should end in a period (godot)
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/mini-oidc/storage/storage.go
+++ test/mini-oidc/storage/storage.go
@@ -19,7 +19,7 @@ import (
 )
 
 // serviceKey1 is a public key which will be used for the JWT Profile Authorization Grant
-// the corresponding private key is in the service-key1.json (for demonstration purposes)
+// the corresponding private key is in the service-key1.json (for demonstration purposes).
 var serviceKey1 = &rsa.PublicKey{
 	N: func() *big.Int {
 		n, _ := new(big.Int).SetString("00f6d44fb5f34ac2033a75e73cb65ff24e6181edc58845e75a560ac21378284977bb055b1a75b714874e2a2641806205681c09abec76efd52cf40984edcf4c8ca09717355d11ac338f280d3e4c905b00543bdb8ee5a417496cb50cb0e29afc5a0d0471fd5a2fa625bd5281f61e6b02067d4fe7a5349eeae6d6a4300bcd86eef331", 16)
@@ -33,9 +33,9 @@ var (
 	_ op.ClientCredentialsStorage = &Storage{}
 )
 
-// storage implements the op.Storage interface
+// Storage implements the op.Storage interface
 // typically you would implement this as a layer on top of your database
-// for simplicity this example keeps everything in-memory
+// for simplicity this example keeps everything in-memory.
 type Storage struct {
 	lock          sync.Mutex
 	authRequests  map[string]*AuthRequest
@@ -125,7 +125,7 @@ func NewStorage(userStore UserStore) *Storage {
 	}
 }
 
-// CheckUsernamePassword implements the `authenticate` interface of the login
+// CheckUsernamePassword implements the `authenticate` interface of the login.
 func (s *Storage) CheckUsernamePassword(username, password, id string) error {
 	s.lock.Lock()
 	defer s.lock.Unlock()
