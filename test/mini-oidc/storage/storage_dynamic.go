722e21358c3a8001102d1746825472b77e870012
Thomas Parrott <thomas.parrott@canonical.com>
2024-06-06 13:55:39 +0100

    test/mini-oidc/storage/storage: linter fixes
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/mini-oidc/storage/storage_dynamic.go
+++ test/mini-oidc/storage/storage_dynamic.go
@@ -15,7 +15,7 @@ type multiStorage struct {
 }
 
 // NewMultiStorage implements the op.Storage interface by wrapping multiple storage structs
-// and selecting them by the calling issuer
+// and selecting them by the calling issuer.
 func NewMultiStorage(issuers []string) *multiStorage { //nolint:revive
 	s := make(map[string]*Storage)
 	for _, issuer := range issuers {
@@ -34,7 +34,7 @@ func (s *multiStorage) CheckUsernamePassword(ctx context.Context, username, pass
 }
 
 // CreateAuthRequest implements the op.Storage interface
-// it will be called after parsing and validation of the authentication request
+// it will be called after parsing and validation of the authentication request.
 func (s *multiStorage) CreateAuthRequest(ctx context.Context, authReq *oidc.AuthRequest, userID string) (op.AuthRequest, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -44,7 +44,7 @@ func (s *multiStorage) CreateAuthRequest(ctx context.Context, authReq *oidc.Auth
 }
 
 // AuthRequestByID implements the op.Storage interface
-// it will be called after the Login UI redirects back to the OIDC endpoint
+// it will be called after the Login UI redirects back to the OIDC endpoint.
 func (s *multiStorage) AuthRequestByID(ctx context.Context, id string) (op.AuthRequest, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -54,7 +54,7 @@ func (s *multiStorage) AuthRequestByID(ctx context.Context, id string) (op.AuthR
 }
 
 // AuthRequestByCode implements the op.Storage interface
-// it will be called after parsing and validation of the token request (in an authorization code flow)
+// it will be called after parsing and validation of the token request (in an authorization code flow).
 func (s *multiStorage) AuthRequestByCode(ctx context.Context, code string) (op.AuthRequest, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -65,7 +65,7 @@ func (s *multiStorage) AuthRequestByCode(ctx context.Context, code string) (op.A
 
 // SaveAuthCode implements the op.Storage interface
 // it will be called after the authentication has been successful and before redirecting the user agent to the redirect_uri
-// (in an authorization code flow)
+// (in an authorization code flow).
 func (s *multiStorage) SaveAuthCode(ctx context.Context, id string, code string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -77,7 +77,7 @@ func (s *multiStorage) SaveAuthCode(ctx context.Context, id string, code string)
 // DeleteAuthRequest implements the op.Storage interface
 // it will be called after creating the token response (id and access tokens) for a valid
 // - authentication request (in an implicit flow)
-// - token request (in an authorization code flow)
+// - token request (in an authorization code flow).
 func (s *multiStorage) DeleteAuthRequest(ctx context.Context, id string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -87,7 +87,7 @@ func (s *multiStorage) DeleteAuthRequest(ctx context.Context, id string) error {
 }
 
 // CreateAccessToken implements the op.Storage interface
-// it will be called for all requests able to return an access token (Authorization Code Flow, Implicit Flow, JWT Profile, ...)
+// it will be called for all requests able to return an access token (Authorization Code Flow, Implicit Flow, JWT Profile, ...).
 func (s *multiStorage) CreateAccessToken(ctx context.Context, request op.TokenRequest) (string, time.Time, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -97,7 +97,7 @@ func (s *multiStorage) CreateAccessToken(ctx context.Context, request op.TokenRe
 }
 
 // CreateAccessAndRefreshTokens implements the op.Storage interface
-// it will be called for all requests able to return an access and refresh token (Authorization Code Flow, Refresh Token Request)
+// it will be called for all requests able to return an access and refresh token (Authorization Code Flow, Refresh Token Request).
 func (s *multiStorage) CreateAccessAndRefreshTokens(ctx context.Context, request op.TokenRequest, currentRefreshToken string) (accessTokenID string, newRefreshToken string, expiration time.Time, err error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -107,7 +107,7 @@ func (s *multiStorage) CreateAccessAndRefreshTokens(ctx context.Context, request
 }
 
 // TokenRequestByRefreshToken implements the op.Storage interface
-// it will be called after parsing and validation of the refresh token request
+// it will be called after parsing and validation of the refresh token request.
 func (s *multiStorage) TokenRequestByRefreshToken(ctx context.Context, refreshToken string) (op.RefreshTokenRequest, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -116,8 +116,8 @@ func (s *multiStorage) TokenRequestByRefreshToken(ctx context.Context, refreshTo
 	return storage.TokenRequestByRefreshToken(ctx, refreshToken)
 }
 
-// TerminateSession implements the op.Storage interface
-// it will be called after the user signed out, therefore the access and refresh token of the user of this client must be removed
+// TerminateSession implements the op.Storage interface.
+// it will be called after the user signed out, therefore the access and refresh token of the user of this client must be removed.
 func (s *multiStorage) TerminateSession(ctx context.Context, userID string, clientID string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -137,7 +137,7 @@ func (s *multiStorage) GetRefreshTokenInfo(ctx context.Context, clientID string,
 }
 
 // RevokeToken implements the op.Storage interface
-// it will be called after parsing and validation of the token revocation request
+// it will be called after parsing and validation of the token revocation request.
 func (s *multiStorage) RevokeToken(ctx context.Context, token string, userID string, clientID string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -147,7 +147,7 @@ func (s *multiStorage) RevokeToken(ctx context.Context, token string, userID str
 }
 
 // SigningKey implements the op.Storage interface
-// it will be called when creating the OpenID Provider
+// it will be called when creating the OpenID Provider.
 func (s *multiStorage) SigningKey(ctx context.Context) (op.SigningKey, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -157,7 +157,7 @@ func (s *multiStorage) SigningKey(ctx context.Context) (op.SigningKey, error) {
 }
 
 // SignatureAlgorithms implements the op.Storage interface
-// it will be called to get the sign
+// it will be called to get the sign.
 func (s *multiStorage) SignatureAlgorithms(ctx context.Context) ([]jose.SignatureAlgorithm, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -177,7 +177,7 @@ func (s *multiStorage) KeySet(ctx context.Context) ([]op.Key, error) {
 }
 
 // GetClientByClientID implements the op.Storage interface
-// it will be called whenever information (type, redirect_uris, ...) about the client behind the client_id is needed
+// it will be called whenever information (type, redirect_uris, ...) about the client behind the client_id is needed.
 func (s *multiStorage) GetClientByClientID(ctx context.Context, clientID string) (op.Client, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -187,7 +187,7 @@ func (s *multiStorage) GetClientByClientID(ctx context.Context, clientID string)
 }
 
 // AuthorizeClientIDSecret implements the op.Storage interface
-// it will be called for validating the client_id, client_secret on token or introspection requests
+// it will be called for validating the client_id, client_secret on token or introspection requests.
 func (s *multiStorage) AuthorizeClientIDSecret(ctx context.Context, clientID, clientSecret string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -206,9 +206,9 @@ func (s *multiStorage) SetUserinfoFromScopes(ctx context.Context, userinfo *oidc
 	return storage.SetUserinfoFromScopes(ctx, userinfo, userID, clientID, scopes)
 }
 
-// SetUserinfoFromRequests implements the op.CanSetUserinfoFromRequest interface.  In the
+// SetUserinfoFromRequest implements the op.CanSetUserinfoFromRequest interface.  In the
 // next major release, it will be required for op.Storage.
-// It will be called for the creation of an id_token, so we'll just pass it to the private function without any further check
+// It will be called for the creation of an id_token, so we'll just pass it to the private function without any further check.
 func (s *multiStorage) SetUserinfoFromRequest(ctx context.Context, userinfo *oidc.UserInfo, token op.IDTokenRequest, scopes []string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -218,7 +218,7 @@ func (s *multiStorage) SetUserinfoFromRequest(ctx context.Context, userinfo *oid
 }
 
 // SetUserinfoFromToken implements the op.Storage interface
-// it will be called for the userinfo endpoint, so we read the token and pass the information from that to the private function
+// it will be called for the userinfo endpoint, so we read the token and pass the information from that to the private function.
 func (s *multiStorage) SetUserinfoFromToken(ctx context.Context, userinfo *oidc.UserInfo, tokenID, subject, origin string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -228,7 +228,7 @@ func (s *multiStorage) SetUserinfoFromToken(ctx context.Context, userinfo *oidc.
 }
 
 // SetIntrospectionFromToken implements the op.Storage interface
-// it will be called for the introspection endpoint, so we read the token and pass the information from that to the private function
+// it will be called for the introspection endpoint, so we read the token and pass the information from that to the private function.
 func (s *multiStorage) SetIntrospectionFromToken(ctx context.Context, introspection *oidc.IntrospectionResponse, tokenID, subject, clientID string) error {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -238,7 +238,7 @@ func (s *multiStorage) SetIntrospectionFromToken(ctx context.Context, introspect
 }
 
 // GetPrivateClaimsFromScopes implements the op.Storage interface
-// it will be called for the creation of a JWT access token to assert claims for custom scopes
+// it will be called for the creation of a JWT access token to assert claims for custom scopes.
 func (s *multiStorage) GetPrivateClaimsFromScopes(ctx context.Context, userID, clientID string, scopes []string) (claims map[string]any, err error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -248,7 +248,7 @@ func (s *multiStorage) GetPrivateClaimsFromScopes(ctx context.Context, userID, c
 }
 
 // GetKeyByIDAndClientID implements the op.Storage interface
-// it will be called to validate the signatures of a JWT (JWT Profile Grant and Authentication)
+// it will be called to validate the signatures of a JWT (JWT Profile Grant and Authentication).
 func (s *multiStorage) GetKeyByIDAndClientID(ctx context.Context, keyID, userID string) (*jose.JSONWebKey, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -258,7 +258,7 @@ func (s *multiStorage) GetKeyByIDAndClientID(ctx context.Context, keyID, userID 
 }
 
 // ValidateJWTProfileScopes implements the op.Storage interface
-// it will be called to validate the scopes of a JWT Profile Authorization Grant request
+// it will be called to validate the scopes of a JWT Profile Authorization Grant request.
 func (s *multiStorage) ValidateJWTProfileScopes(ctx context.Context, userID string, scopes []string) ([]string, error) {
 	storage, err := s.storageFromContext(ctx)
 	if err != nil {
@@ -267,7 +267,7 @@ func (s *multiStorage) ValidateJWTProfileScopes(ctx context.Context, userID stri
 	return storage.ValidateJWTProfileScopes(ctx, userID, scopes)
 }
 
-// Health implements the op.Storage interface
+// Health implements the op.Storage interface.
 func (s *multiStorage) Health(ctx context.Context) error {
 	return nil
 }
