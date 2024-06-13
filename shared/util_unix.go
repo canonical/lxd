2da4f2731d7f6d35f5480201016090b23d1ca9a8
Din Music <din.music@canonical.com>
2024-06-13 10:19:49 +0000

    shared/util: Add comments to exported functions
    Signed-off-by: Din Music <din.music@canonical.com>

--- shared/util_unix.go
+++ shared/util_unix.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// GetOwnerMode retrieves the file mode, user ID, and group ID for the given file.
 func GetOwnerMode(fInfo os.FileInfo) (os.FileMode, int, int) {
 	mode := fInfo.Mode()
 	uid := int(fInfo.Sys().(*syscall.Stat_t).Uid)
@@ -16,6 +17,7 @@ func GetOwnerMode(fInfo os.FileInfo) (os.FileMode, int, int) {
 	return mode, uid, gid
 }
 
+// PathIsWritable returns true if the given path is writable and false otherwise.
 func PathIsWritable(path string) bool {
 	return unix.Access(path, unix.W_OK) == nil
 }
