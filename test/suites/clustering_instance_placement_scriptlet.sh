lxc/incus

tests: Remove trust password
Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

test/suites/clustering_instance_placement_scriptlet.sh
@@ -28,14 +28,14 @@ test_clustering_instance_placement_scriptlet() {
   INCUS_TWO_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   chmod +x "${INCUS_TWO_DIR}"
   ns2="${prefix}2"
-  spawn_incus_and_join_cluster "${ns2}" "${bridge}" "${cert}" 2 1 "${INCUS_TWO_DIR}" "${poolDriver}"
+  spawn_incus_and_join_cluster "${ns2}" "${bridge}" "${cert}" 2 1 "${INCUS_TWO_DIR}" "${INCUS_ONE_DIR}" "${poolDriver}"
 
   # Spawn a third node
   setup_clustering_netns 3
   INCUS_THREE_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   chmod +x "${INCUS_THREE_DIR}"
   ns3="${prefix}3"
-  spawn_incus_and_join_cluster "${ns3}" "${bridge}" "${cert}" 3 1 "${INCUS_THREE_DIR}" "${poolDriver}"
+  spawn_incus_and_join_cluster "${ns3}" "${bridge}" "${cert}" 3 1 "${INCUS_THREE_DIR}" "${INCUS_ONE_DIR}" "${poolDriver}"
 
   INCUS_DIR="${INCUS_ONE_DIR}" ensure_import_testimage
 

test/suites/init_auto.sh
@@ -57,12 +57,12 @@ test_init_auto() {
     kill_incus "${INCUS_INIT_DIR}"
   fi
 
-  # incusd init --trust-password test --network-address 127.0.0.1 --network-port LOCAL --auto
+  # incusd init --network-address 127.0.0.1 --network-port LOCAL --auto
   INCUS_INIT_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   chmod +x "${INCUS_INIT_DIR}"
   spawn_incus "${INCUS_INIT_DIR}" false
 
-  INCUS_DIR=${INCUS_INIT_DIR} incusd init --trust-password test --network-address 127.0.0.1 --network-port "$(local_tcp_port)" --auto
+  INCUS_DIR=${INCUS_INIT_DIR} incusd init --network-address 127.0.0.1 --network-port "$(local_tcp_port)" --auto
 
   kill_incus "${INCUS_INIT_DIR}"
 }

test/suites/migration.sh
@@ -13,9 +13,12 @@ test_migration() {
   # workaround for kernel/criu
   umount /sys/kernel/debug >/dev/null 2>&1 || true
 
+  token="$(incus config trust add --name foo -q)"
   # shellcheck disable=2153
-  incus_remote remote add l1 "${INCUS_ADDR}" --accept-certificate --password foo
-  incus_remote remote add l2 "${INCUS2_ADDR}" --accept-certificate --password foo
+  incus_remote remote add l1 "${INCUS_ADDR}" --accept-certificate --token "${token}"
+
+  token="$(INCUS_DIR=${INCUS2_DIR} incus config trust add --name foo -q)"
+  incus_remote remote add l2 "${INCUS2_ADDR}" --accept-certificate --token "${token}"
 
   migration "$INCUS2_DIR"
 

test/suites/pki.sh
@@ -55,27 +55,28 @@ test_pki() {
     set -e
     export INCUS_CONF="${INC5_DIR}"
 
-    # Try adding remote using an incorrect password.
-    # This should fail, as if the certificate is unknown and password is wrong then no access should be allowed.
-    ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --password=bar || false
+    # Try adding remote using an incorrect token.
+    # This should fail, as if the certificate is unknown and token is wrong then no access should be allowed.
+    ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --token=bar || false
 
-    # Add remote using the correct password.
+    # Add remote using the correct token.
     # This should work because the client certificate is signed by the CA.
-    incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --password=foo
+    token="$(INCUS_DIR=${INCUS5_DIR} incus config trust add --name foo -q)"
+    incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --token "${token}"
     incus_remote config trust ls pki-incus: | grep incus-client
     incus_remote remote remove pki-incus
 
-    # Add remote using a CA-signed client certificate, and not providing a password.
+    # Add remote using a CA-signed client certificate, and not providing a token.
     # This should succeed and tests that the CA trust is working, as adding the client certificate to the trust
-    # store without a trust password would normally fail.
+    # store without a token would normally fail.
     INCUS_DIR=${INCUS5_DIR} incus config set core.trust_ca_certificates true
     incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate
     incus_remote config trust ls pki-incus: | grep incus-client
     incus_remote remote remove pki-incus
 
-    # Add remote using a CA-signed client certificate, and providing an incorrect password.
-    # This should succeed as is the same as the test above but with an incorrect password rather than no password.
-    incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --password=bar
+    # Add remote using a CA-signed client certificate, and providing an incorrect token.
+    # This should succeed as is the same as the test above but with an incorrect token rather than no token.
+    incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --token=bar
     incus_remote config trust ls pki-incus: | grep incus-client
 
     # Try removing the fingerprint.
@@ -89,21 +90,23 @@ test_pki() {
     cp "${TEST_DIR}/pki/keys/incus-client-revoked.crt" "${INC5_DIR}/client.crt"
     cp "${TEST_DIR}/pki/keys/incus-client-revoked.key" "${INC5_DIR}/client.key"
 
-    # Try adding a remote using a revoked client certificate, and the correct password.
+    # Try adding a remote using a revoked client certificate, and the correct token.
     # This should fail, as although revoked certificates can be added to the trust store, they will not be usable.
-    ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --password=foo || false
+    token="$(INCUS_DIR=${INCUS5_DIR} incus config trust add --name foo -q)"
+    ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --token "${token}" || false
 
-    # Try adding a remote using a revoked client certificate, and an incorrect password.
-    # This should fail, as if the certificate is revoked and password is wrong then no access should be allowed.
-    ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --password=incorrect || false
+    # Try adding a remote using a revoked client certificate, and an incorrect token.
+    # This should fail, as if the certificate is revoked and token is wrong then no access should be allowed.
+    ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --token=incorrect || false
   )
 
   # Confirm that a normal, non-PKI certificate doesn't.
   # As INCUS_CONF is not set to INC5_DIR where the CA signed client certs are, this will cause the incus command to
   # generate a new certificate that isn't trusted by the CA certificate and thus will not be allowed, even with a
-  # correct trust password. This is because the Incus TLS listener in CA mode will not consider a client cert that
+  # correct token. This is because the Incus TLS listener in CA mode will not consider a client cert that
   # is not signed by the CA as valid.
-  ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --password=foo || false
+  token="$(INCUS_DIR=${INCUS5_DIR} incus config trust add --name foo -q)"
+  ! incus_remote remote add pki-incus "${INCUS5_ADDR}" --accept-certificate --token "${token}" || false
 
   kill_incus "${INCUS5_DIR}"
 }

test/suites/projects.sh
@@ -521,7 +521,7 @@ test_projects_network() {
   # Create a container in the project
   incus init -n "${network}" testimage c1
 
-  incus network show "${network}" |grep -q "/1.0/instances/c1?project=foo"
+  incus network show "${network}" | grep -q "/1.0/instances/c1?project=foo"
 
   # Delete the container
   incus delete c1
@@ -754,7 +754,8 @@ test_projects_limits() {
     INCUS_REMOTE_ADDR=$(cat "${INCUS_REMOTE_DIR}/incus.addr")
     (INCUS_DIR=${INCUS_REMOTE_DIR} deps/import-busybox --alias remoteimage --template start --public)
 
-    incus remote add l2 "${INCUS_REMOTE_ADDR}" --accept-certificate --password foo
+    token="$(INCUS_DIR=${INCUS_REMOTE_DIR} incus config trust add --name foo -q)"
+    incus remote add l2 "${INCUS_REMOTE_ADDR}" --accept-certificate --token "${token}"
 
     # Relax all constraints except the disk limits, which won't be enough for the
     # image to be downloaded.

test/suites/tls_restrictions.sh
@@ -81,7 +81,7 @@ test_certificate_edit() {
 
   curl -k -s --cert "${INCUS_CONF}/client.crt" --key "${INCUS_CONF}/client.key" -X PATCH -d "{\"restricted\": false}" "https://${INCUS_ADDR}/1.0/certificates/${FINGERPRINT}"
 
-  ! incus_remote config trust show "${FINGERPRINT}" | sed -e "s/name:.*/name: foo/" | incus_remote config trust edit localhost:"${FINGERPRINT}" || false
+  ! incus_remote config trust show "${FINGERPRINT}" | sed -e "s/name:.*/name: bar/" | incus_remote config trust edit localhost:"${FINGERPRINT}" || false
 
   curl -k -s --cert "${INCUS_CONF}/client.crt" --key "${INCUS_CONF}/client.key" -X PATCH -d "{\"name\": \"bar\"}" "https://${INCUS_ADDR}/1.0/certificates/${FINGERPRINT}"
 

canonical/lxd

(cherry picked from commit a47d14fdeac0d2fb4544553b7a4973e62816a68d)
Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
License: Apache-2.0

test/suites/clustering_instance_placement_scriptlet.sh
@@ -28,14 +28,14 @@ test_clustering_instance_placement_scriptlet() {
   LXD_TWO_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   chmod +x "${LXD_TWO_DIR}"
   ns2="${prefix}2"
-  spawn_lxd_and_join_cluster "${ns2}" "${bridge}" "${cert}" 2 1 "${LXD_TWO_DIR}" "${poolDriver}"
+  spawn_lxd_and_join_cluster "${ns2}" "${bridge}" "${cert}" 2 1 "${LXD_TWO_DIR}" "${LXD_ONE_DIR}" "${poolDriver}"
 
   # Spawn a third node
   setup_clustering_netns 3
   LXD_THREE_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   chmod +x "${LXD_THREE_DIR}"
   ns3="${prefix}3"
-  spawn_lxd_and_join_cluster "${ns3}" "${bridge}" "${cert}" 3 1 "${LXD_THREE_DIR}" "${poolDriver}"
+  spawn_lxd_and_join_cluster "${ns3}" "${bridge}" "${cert}" 3 1 "${LXD_THREE_DIR}" "${LXD_ONE_DIR}" "${poolDriver}"
 
   LXD_DIR="${LXD_ONE_DIR}" ensure_import_testimage
 

test/suites/init_auto.sh
@@ -57,12 +57,12 @@ test_init_auto() {
     kill_lxd "${LXD_INIT_DIR}"
   fi
 
-  # lxd init --trust-password test --network-address 127.0.0.1 --network-port LOCAL --auto
+  # lxd init --network-address 127.0.0.1 --network-port LOCAL --auto
   LXD_INIT_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   chmod +x "${LXD_INIT_DIR}"
   spawn_lxd "${LXD_INIT_DIR}" false
 
-  LXD_DIR=${LXD_INIT_DIR} lxd init --trust-password test --network-address 127.0.0.1 --network-port "$(local_tcp_port)" --auto
+  LXD_DIR=${LXD_INIT_DIR} lxd init --network-address 127.0.0.1 --network-port "$(local_tcp_port)" --auto
 
   kill_lxd "${LXD_INIT_DIR}"
 }

test/suites/migration.sh
@@ -13,9 +13,12 @@ test_migration() {
   # workaround for kernel/criu
   umount /sys/kernel/debug >/dev/null 2>&1 || true
 
+  token="$(lxc config trust add --name foo -q)"
   # shellcheck disable=2153
-  lxc_remote remote add l1 "${LXD_ADDR}" --accept-certificate --password foo
-  lxc_remote remote add l2 "${LXD2_ADDR}" --accept-certificate --password foo
+  lxc_remote remote add l1 "${LXD_ADDR}" --accept-certificate --token "${token}"
+
+  token="$(LXD_DIR=${LXD2_DIR} lxc config trust add --name foo -q)"
+  lxc_remote remote add l2 "${LXD2_ADDR}" --accept-certificate --token "${token}"
 
   migration "$LXD2_DIR"
 

test/suites/pki.sh
@@ -55,20 +55,20 @@ test_pki() {
     set -e
     export LXD_CONF="${LXC5_DIR}"
 
-    # Try adding remote using an incorrect password.
-    # This should fail, as if the certificate is unknown and password is wrong then no access should be allowed.
-    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=bar || false
+    # Try adding remote using an incorrect token.
+    # This should fail, as if the certificate is unknown and token is wrong then no access should be allowed.
+    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --token=bar || false
 
-    # Add remote using the correct password.
+    # Add remote using the correct token.
     # This should work because the client certificate is signed by the CA.
     token="$(LXD_DIR=${LXD5_DIR} lxc config trust add --name foo -q)"
-    lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password "${token}"
+    lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --token "${token}"
     lxc_remote config trust ls pki-lxd: | grep lxd-client
     fingerprint="$(lxc_remote config trust ls pki-lxd: --format csv | cut -d, -f4)"
     lxc_remote config trust remove pki-lxd:"${fingerprint}"
     lxc_remote remote remove pki-lxd
 
-    # Add remote using a CA-signed client certificate, and not providing a password.
+    # Add remote using a CA-signed client certificate, and not providing a token.
     # This should succeed and tests that the CA trust is working, as adding the client certificate to the trust
     # store without a token would normally fail.
     LXD_DIR=${LXD5_DIR} lxc config set core.trust_ca_certificates true
@@ -78,7 +78,7 @@ test_pki() {
 
     # Add remote using a CA-signed client certificate, and providing an incorrect token.
     # This should succeed as is the same as the test above but with an incorrect token rather than no token.
-    lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=bar
+    lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --token=bar
     ! lxc_remote config trust ls pki-lxd: | grep lxd-client || false
     lxc_remote remote remove pki-lxd
 
@@ -86,21 +86,23 @@ test_pki() {
     cp "${TEST_DIR}/pki/keys/lxd-client-revoked.crt" "${LXC5_DIR}/client.crt"
     cp "${TEST_DIR}/pki/keys/lxd-client-revoked.key" "${LXC5_DIR}/client.key"
 
-    # Try adding a remote using a revoked client certificate, and the correct password.
+    # Try adding a remote using a revoked client certificate, and the correct token.
     # This should fail, as although revoked certificates can be added to the trust store, they will not be usable.
-    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=foo || false
+    token="$(LXD_DIR=${LXD5_DIR} lxc config trust add --name foo -q)"
+    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --token "${token}" || false
 
-    # Try adding a remote using a revoked client certificate, and an incorrect password.
-    # This should fail, as if the certificate is revoked and password is wrong then no access should be allowed.
-    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=incorrect || false
+    # Try adding a remote using a revoked client certificate, and an incorrect token.
+    # This should fail, as if the certificate is revoked and token is wrong then no access should be allowed.
+    ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --token=incorrect || false
   )
 
   # Confirm that a normal, non-PKI certificate doesn't.
   # As LXD_CONF is not set to LXC5_DIR where the CA signed client certs are, this will cause the lxc command to
   # generate a new certificate that isn't trusted by the CA certificate and thus will not be allowed, even with a
-  # correct trust password. This is because the LXD TLS listener in CA mode will not consider a client cert that
+  # correct token. This is because the LXD TLS listener in CA mode will not consider a client cert that
   # is not signed by the CA as valid.
-  ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --password=foo || false
+  token="$(LXD_DIR=${LXD5_DIR} lxc config trust add --name foo -q)"
+  ! lxc_remote remote add pki-lxd "${LXD5_ADDR}" --accept-certificate --token "${token}" || false
 
   kill_lxd "${LXD5_DIR}"
 }

test/suites/projects.sh
@@ -521,7 +521,7 @@ test_projects_network() {
   # Create a container in the project
   lxc init -n "${network}" testimage c1
 
-  lxc network show "${network}" |grep -q "/1.0/instances/c1?project=foo"
+  lxc network show "${network}" | grep -q "/1.0/instances/c1?project=foo"
 
   # Delete the container
   lxc delete c1
@@ -754,7 +754,8 @@ test_projects_limits() {
     LXD_REMOTE_ADDR=$(cat "${LXD_REMOTE_DIR}/lxd.addr")
     (LXD_DIR=${LXD_REMOTE_DIR} deps/import-busybox --alias remoteimage --template start --public)
 
-    lxc remote add l2 "${LXD_REMOTE_ADDR}" --accept-certificate --password foo
+    token="$(LXD_DIR=${LXD_REMOTE_DIR} lxc config trust add --name foo -q)"
+    lxc remote add l2 "${LXD_REMOTE_ADDR}" --accept-certificate --token "${token}"
 
     # Relax all constraints except the disk limits, which won't be enough for the
     # image to be downloaded.

test/suites/tls_restrictions.sh
@@ -154,7 +154,7 @@ test_certificate_edit() {
 
   curl -k -s --cert "${LXD_CONF}/client.crt" --key "${LXD_CONF}/client.key" -X PATCH -d "{\"restricted\": false}" "https://${LXD_ADDR}/1.0/certificates/${FINGERPRINT}" | grep -F '"error_code":403'
 
-  ! lxc_remote config trust show "${FINGERPRINT}" | sed -e "s/name:.*/name: foo/" | lxc_remote config trust edit localhost:"${FINGERPRINT}" || false
+  ! lxc_remote config trust show "${FINGERPRINT}" | sed -e "s/name:.*/name: bar/" | lxc_remote config trust edit localhost:"${FINGERPRINT}" || false
 
   curl -k -s --cert "${LXD_CONF}/client.crt" --key "${LXD_CONF}/client.key" -X PATCH -d "{\"name\": \"bar\"}" "https://${LXD_ADDR}/1.0/certificates/${FINGERPRINT}" | grep -F '"error_code":403'
 