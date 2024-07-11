d95bf7afa966c033cfdd7d17bce57b323dd0fe56
Mark Laing <mark.laing@canonical.com>
2024-07-11 10:05:09 +0100

    test: Include cluster trust add test in clustering suite.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -257,6 +257,7 @@ if [ "${1:-"all"}" != "standalone" ]; then
     run_test test_clustering_groups "clustering groups"
     run_test test_clustering_events "clustering events"
     run_test test_clustering_uuid "clustering uuid"
+    run_test test_clustering_trust_add "clustering trust add"
 fi
 
 if [ "${1:-"all"}" != "cluster" ]; then
