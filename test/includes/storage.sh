df582a39c4985b5503378e780eaa4800deb3d88e
Simon Deziel <simon.deziel@canonical.com>
2024-07-12 16:29:13 -0400

    test/includes/storage: add storage_backend_optimized_backup() helper
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/storage.sh
+++ test/includes/storage.sh
@@ -16,6 +16,13 @@ storage_backend_available() {
     false
 }
 
+# Returns 0 if --optimized-storage works for backups (export/import)
+storage_backend_optimized_backup() {
+    [ "${1}" = "btrfs" ] && return 0
+    [ "${1}" = "zfs" ]   && return 0
+    return 1
+}
+
 # Choose a random available backend, excluding LXD_BACKEND
 random_storage_backend() {
     # shellcheck disable=2046
