4d78aa8db7d156dbe509b9f45491b56793cce67c
Stéphane Graber <stgraber@stgraber.org>
2024-07-19 14:38:03 -0400

    lxd/storage/lvm: Properly handle activation during resize
    LVM resize requires the volume be fully deactivated.
    
    For some reason our LVM logic would always fully activate the volume,
    which is needed for shrinking but was still doing it during growth.
    
    Note that this is only needed when not using thin pools so this is
    likely why this slipped through the cracks.
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    (cherry picked from commit 64966418de3372ac2c563c58fe56852987b5ad75)
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>
    License: Apache-2.0

--- lxd/storage/drivers/driver_lvm_volumes.go
+++ lxd/storage/drivers/driver_lvm_volumes.go
@@ -449,16 +449,6 @@ func (d *lvm) SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, op
 
 	l := d.logger.AddContext(logger.Ctx{"dev": volDevPath, "size": fmt.Sprintf("%db", sizeBytes)})
 
-	// Activate volume if needed.
-	activated, err := d.activateVolume(vol)
-	if err != nil {
-		return err
-	}
-
-	if activated {
-		defer func() { _, _ = d.deactivateVolume(vol) }()
-	}
-
 	inUse := vol.MountInUse()
 
 	// Resize filesystem if needed.
@@ -474,6 +464,12 @@ func (d *lvm) SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, op
 				return ErrInUse // We don't allow online shrinking of filesytem volumes.
 			}
 
+			// Activate volume if needed.
+			_, err := d.activateVolume(vol)
+			if err != nil {
+				return err
+			}
+
 			// Shrink filesystem first.
 			// Pass allowUnsafeResize to allow disabling of filesystem resize safety checks.
 			// We do this as a separate step rather than passing -r to lvresize in resizeLogicalVolume
@@ -481,6 +477,13 @@ func (d *lvm) SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, op
 			// otherwise by passing -f to lvresize (required for other reasons) this would then pass
 			// -f onto resize2fs as well.
 			err = shrinkFileSystem(fsType, volDevPath, vol, sizeBytes, allowUnsafeResize)
+			if err != nil {
+				_, _ = d.deactivateVolume(vol)
+				return err
+			}
+
+			// Deactivate the volume if needed.
+			_, err = d.deactivateVolume(vol)
 			if err != nil {
 				return err
 			}
@@ -498,6 +501,16 @@ func (d *lvm) SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, op
 			if err != nil {
 				return err
 			}
+
+			// Activate the volume if needed.
+			_, err := d.activateVolume(vol)
+			if err != nil {
+				return err
+			}
+
+			defer func() {
+				_, _ = d.deactivateVolume(vol)
+			}()
 
 			// Grow the filesystem to fill block device.
 			err = growFileSystem(fsType, volDevPath, vol)
@@ -524,6 +537,16 @@ func (d *lvm) SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, op
 		if err != nil {
 			return err
 		}
+
+		// Activate the volume if needed.
+		_, err := d.activateVolume(vol)
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			_, _ = d.deactivateVolume(vol)
+		}()
 
 		// Move the VM GPT alt header to end of disk if needed (not needed in unsafe resize mode as it is
 		// expected the caller will do all necessary post resize actions themselves).
