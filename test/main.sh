a7fbba518a16c875d6715a72745d628b48df567b
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-07-25 16:16:06 -0500

    test: Snapshot creation failure
    This at least runs through the reverter that gets built in case snapshot
    creation fails.
    
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -317,6 +317,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_snap_expiry "snapshot expiry"
     run_test test_snap_schedule "snapshot scheduling"
     run_test test_snap_volume_db_recovery "snapshot volume database record recovery"
+    run_test test_snap_fail "snapshot creation failure"
     run_test test_config_profiles "profiles and configuration"
     run_test test_config_edit "container configuration edit"
     run_test test_property "container property"
