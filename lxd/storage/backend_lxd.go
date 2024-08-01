791b96412b0f0f6f2eea178b1acec30c2c9ec9ac
Din Music <din.music@canonical.com>
2024-08-01 14:43:59 +0000

    lxd/storage/backend_lxd: Show image conversion progress
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/backend_lxd.go
+++ lxd/storage/backend_lxd.go
@@ -1813,7 +1813,7 @@ func (b *lxdBackend) isoFiller(data io.Reader) func(vol drivers.Volume, rootBloc
 
 // imageConversionFiller returns a function that converts an image from the given path to the instance's volume.
 // Function returns the unpacked image size on success. Otherwise, it returns -1 for size and an error.
-func (b *lxdBackend) imageConversionFiller(imgPath string, imgFormat string) func(vol drivers.Volume, rootBlockPath string, allowUnsafeResize bool) (sizeInBytes int64, err error) {
+func (b *lxdBackend) imageConversionFiller(imgPath string, imgFormat string, op *operations.Operation) func(vol drivers.Volume, rootBlockPath string, allowUnsafeResize bool) (sizeInBytes int64, err error) {
 	return func(vol drivers.Volume, rootBlockPath string, allowUnsafeResize bool) (int64, error) {
 		diskPath, err := b.driver.GetVolumeDiskPath(vol)
 		if err != nil {
@@ -1826,17 +1826,30 @@ func (b *lxdBackend) imageConversionFiller(imgPath string, imgFormat string) fun
 			return -1, fmt.Errorf("Unsupported image format %q, allowed formats are [%s]", imgFormat, strings.Join(supportedImageFormats, ", "))
 		}
 
+		// Setup the progress tracker.
+		var tracker *ioprogress.ProgressTracker
+		if op != nil {
+			metadata := make(map[string]any)
+			tracker = &ioprogress.ProgressTracker{
+				Handler: func(percent, speed int64) {
+					displayPrefix := fmt.Sprintf("Converting image format from %s to raw", imgFormat)
+					shared.SetProgressMetadata(metadata, "format_progress", displayPrefix, percent, 0, speed)
+					_ = op.UpdateMetadata(metadata)
+				},
+			}
+		}
+
 		// Convert uploaded image from backups directory into RAW format on the instance volume.
 		cmd := []string{
 			// Run with low priority to reduce CPU impact on other processes.
 			"nice", "-n19",
-			"qemu-img", "convert", "-f", imgFormat, "-O", "raw", imgPath, diskPath, "-t", "writeback",
+			"qemu-img", "convert", "-p", "-f", imgFormat, "-O", "raw", imgPath, diskPath, "-t", "writeback",
 		}
 
 		b.logger.Debug("Image conversion started", logger.Ctx{"from": imgFormat, "to": "raw"})
 		defer b.logger.Debug("Image conversion finished", logger.Ctx{"from": imgFormat, "to": "raw"})
 
-		out, err := apparmor.QemuImg(b.state.OS, cmd, imgPath, diskPath, nil)
+		out, err := apparmor.QemuImg(b.state.OS, cmd, imgPath, diskPath, tracker)
 		if err != nil {
 			b.logger.Debug("Image conversion failed", logger.Ctx{"error": out})
 			return -1, fmt.Errorf("qemu-img convert: failed to convert image from %q to %q format: %v", imgFormat, "raw", err)
@@ -2488,7 +2501,7 @@ func (b *lxdBackend) CreateInstanceFromConversion(inst instance.Instance, conn i
 		}
 
 		// Convert received image into intance volume.
-		volFiller.Fill = b.imageConversionFiller(imgPath, imgFormat)
+		volFiller.Fill = b.imageConversionFiller(imgPath, imgFormat, op)
 	} else {
 		// If volume size is provided, then use that as block volume size instead of pool default.
 		// This way if the volume being received is larger than the pool default size, the created
