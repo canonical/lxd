bdcbad33ddca9dd3db2f154ad784f15cbd44478d
Mark Bolton <mark.bolton@canonical.com>
2024-08-12 11:53:50 -0700

    lxd/db: Add back ErrAlreadyDefined
    Signed-off-by: Mark Bolton <mark.bolton@canonical.com>

--- lxd/db/errors.go
+++ lxd/db/errors.go
@@ -11,6 +11,10 @@ import (
 var (
 	// ErrNoClusterMember is used to indicate no cluster member has been found for a resource.
 	ErrNoClusterMember = fmt.Errorf("No cluster member found")
+
+	// ErrAlreadyDefined hapens when the given entry already exists,
+	// for example a container.
+	ErrAlreadyDefined = fmt.Errorf("The record already exists")
 )
 
 // SmartErrors are used to return more appropriate errors to the caller.
