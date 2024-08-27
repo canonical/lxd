071728526b95920a837b59b4a97f154fd008b199
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-08-27 17:14:26 -0500

    lxd/device/config: Remove DefaultVMBlockFilesystemSize consts
    These should be grabbed via the device driver
    
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>
    (cherry picked from commit 73ab9e73468486ac4c808ca9810d54583f3cb6ea)

--- lxd/device/config/consts.go
+++
@@ -1,7 +0,0 @@
-package config
-
-// DefaultVMBlockFilesystemSize is the size of a VM root device block volume's associated filesystem volume.
-const DefaultVMBlockFilesystemSize = "100MiB"
-
-// DefaultVMPowerFlexBlockFilesystemSize is the size of a VM PowerFlex root device block volume's associated filesystem volume.
-const DefaultVMPowerFlexBlockFilesystemSize = "8GiB"
