daaf398f0edb848a1cd7452a856c8539bbef64e4
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-08-28 15:29:15 +0200

    lxd/device/cdi: no NVIDIA with CDI for `armhf`
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>
    (cherry picked from commit 8740fa542e52fb289b1c658082ef9b169a70162e)

---
+++ lxd/device/cdi/spec_unsupported.go
@@ -0,0 +1,14 @@
+//go:build armhf || arm || arm32
+
+package cdi
+
+import (
+	"fmt"
+
+	"github.com/canonical/lxd/lxd/instance"
+	"tags.cncf.io/container-device-interface/specs-go"
+)
+
+func generateSpec(cdiID ID, inst instance.Instance) (*specs.Spec, error) {
+	return nil, fmt.Errorf("NVIDIA CDI operations not supported on this platform")
+}
