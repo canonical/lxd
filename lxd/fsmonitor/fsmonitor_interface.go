e988dd7f96eda6ec623a04035c8c366a00fd77c3
Mark Laing <mark.laing@canonical.com>
2024-08-29 16:21:00 +0100

    lxd/fsmonitor: Add driver name to interface.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/fsmonitor/fsmonitor_interface.go
+++ lxd/fsmonitor/fsmonitor_interface.go
@@ -2,6 +2,7 @@ package fsmonitor
 
 // FSMonitor represents a filesystem monitor.
 type FSMonitor interface {
+	DriverName() string
 	PrefixPath() string
 	Watch(path string, identifier string, f func(path string, event string) bool) error
 	Unwatch(path string, identifier string) error
