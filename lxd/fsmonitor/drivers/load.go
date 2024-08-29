148afffd4b3334e19ff3eff2cccc3cf2920ff5b1
Mark Laing <mark.laing@canonical.com>
2024-08-29 16:12:08 +0100

    lxd/fsmonitor/drivers: Remove New function and use drivers.Load.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 9642cf048751ef8f80aa652fd87ac5e92d3cf7b2)

--- lxd/fsmonitor/drivers/load.go
+++ lxd/fsmonitor/drivers/load.go
@@ -2,7 +2,11 @@ package drivers
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"github.com/canonical/lxd/lxd/fsmonitor"
+	"github.com/canonical/lxd/lxd/storage/filesystem"
 	"github.com/canonical/lxd/shared/logger"
 )
 
@@ -11,21 +15,42 @@ var drivers = map[string]func() driver{
 	"fanotify": func() driver { return &fanotify{} },
 }
 
-// Load returns a Driver for an existing low-level FS monitor.
-func Load(ctx context.Context, logger logger.Logger, driverName string, path string) (Driver, error) {
-	df, ok := drivers[driverName]
-	if !ok {
-		return nil, ErrUnknownDriver
+// Load returns a new fsmonitor.FSMonitor with an applicable Driver.
+func Load(ctx context.Context, path string) (fsmonitor.FSMonitor, error) {
+	startMonitor := func(driverName string) (Driver, error) {
+		logger := logger.AddContext(logger.Ctx{"driver": driverName})
+
+		df, ok := drivers[driverName]
+		if !ok {
+			return nil, ErrUnknownDriver
+		}
+
+		d := df()
+
+		d.init(logger, path)
+
+		err := d.load(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to load fsmonitor driver %q: %w", driverName, err)
+		}
+
+		return d, nil
 	}
 
-	d := df()
+	if !filesystem.IsMountPoint(path) {
+		return nil, errors.New("Path needs to be a mountpoint")
+	}
 
-	d.init(logger, path)
-
-	err := d.load(ctx)
+	driver, err := startMonitor("fanotify")
 	if err != nil {
-		return nil, err
+		logger.Warn("Failed to initialize fanotify, falling back on inotify", logger.Ctx{"err": err})
+		driver, err = startMonitor("inotify")
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	return d, nil
+	logger.Info("Initialized filesystem monitor", logger.Ctx{"path": path, "driver": driver.Name()})
+
+	return driver, nil
 }
