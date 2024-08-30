4a98a875da13cf3ad7b5860b931ceba2d27781ce
Simon Deziel <simon.deziel@canonical.com>
2024-08-30 13:13:25 -0400

    test/lint/godeps: don't abort on first error
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 282f82a0d11dbdcbec9d1f1f3b0bf66f2ea861fc)

--- test/lint/godeps.sh
+++ test/lint/godeps.sh
@@ -5,13 +5,17 @@ set -o pipefail
 # Ensure predictable sorting
 export LC_ALL=C.UTF-8
 
+rc=0
 for pkg in client lxc/config lxd-agent shared/api; do
+  echo ""
   echo "==> Checking for imports/deps that have been added to ${pkg}..."
   DEP_FILE="test/godeps/$(echo "${pkg}" | sed 's/\//-/g').list"
   OUT="$(go list -f '{{ join .Deps "\n" }}' ./${pkg} | grep -F . | sort -u | diff --new-file -u "${DEP_FILE}" - || true)"
   if [ -n "${OUT}" ]; then
     echo "ERROR: you added a new dependency to ${pkg}; please make sure this is what you want"
     echo "${OUT}"
-    exit 1
+    rc=1
   fi
 done
+
+exit "${rc}"
