bfebfafe9341d550d2c43d6d46d80b1946cf2290
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-08-30 11:49:39 -0500

    lxd/db: Implement GetStoragePoolDrivers
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- lxd/db/storage_pools.go
+++ lxd/db/storage_pools.go
@@ -548,6 +548,32 @@ WHERE storage_pools.id = ? AND storage_pools.state = ?
 	return configs, nil
 }
 
+// GetStoragePoolDrivers maps all storage pool names to driver name.
+func (c *ClusterTx) GetStoragePoolDrivers(ctx context.Context) (map[string]string, error) {
+	sql := "SELECT name, driver FROM storage_pools"
+	drivers := make(map[string]string)
+
+	err := query.Scan(ctx, c.tx, sql, func(scan func(dest ...any) error) error {
+		poolName := ""
+		driver := ""
+
+		err := scan(&poolName, &driver)
+		if err != nil {
+			return err
+		}
+
+		drivers[poolName] = driver
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return drivers, nil
+}
+
 // GetStoragePoolNames returns the names of all storage pools.
 func (c *ClusterTx) GetStoragePoolNames(ctx context.Context) ([]string, error) {
 	return c.storagePools(ctx, "")
