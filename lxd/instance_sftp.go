bd43ea4f75dd004f86d253e6fc33c8f35da0b85e
hamistao <pedro.ribeiro@canonical.com>
2024-08-29 23:30:11 -0300

    lxd: Track completed requests on `Render`
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>
    (cherry picked from commit 2fe07bec65c8cd8d1930daeecb4acbbc81ab5e63)

--- lxd/instance_sftp.go
+++ lxd/instance_sftp.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/canonical/lxd/lxd/cluster"
 	"github.com/canonical/lxd/lxd/instance"
+	"github.com/canonical/lxd/lxd/metrics"
 	"github.com/canonical/lxd/lxd/request"
 	"github.com/canonical/lxd/lxd/response"
 	"github.com/canonical/lxd/shared"
@@ -173,6 +174,8 @@ func (r *sftpServeResponse) Render(w http.ResponseWriter, req *http.Request) err
 	}
 
 	wg.Wait() // Wait for copy go routine to finish.
+
+	request.MetricsCallback(req, metrics.Success)
 
 	return nil
 }
--- lxd/operations/websocket.go
+++ lxd/operations/websocket.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gorilla/websocket"
 
+	"github.com/canonical/lxd/lxd/metrics"
+	"github.com/canonical/lxd/lxd/request"
 	"github.com/canonical/lxd/lxd/response"
 	"github.com/canonical/lxd/shared/ws"
 )
@@ -27,6 +29,12 @@ func (r *operationWebSocket) Render(w http.ResponseWriter, req *http.Request) er
 	}
 
 	err = <-chanErr
+
+	if err == nil {
+		// If there was an error on Render, the callback function will be called during the error handling.
+		request.MetricsCallback(req, metrics.Success)
+	}
+
 	return err
 }
 
@@ -63,6 +71,9 @@ func (r *forwardedOperationWebSocket) Render(w http.ResponseWriter, req *http.Re
 	// Make sure both sides are closed.
 	_ = r.source.Close()
 	_ = target.Close()
+
+	// If there was an error on Render, the callback function will be called during the error handling.
+	request.MetricsCallback(req, metrics.Success)
 
 	return nil
 }
