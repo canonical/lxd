377914908482cb5b18e2181f5a201681e5e8c090
Mark Laing <mark.laing@canonical.com>
2024-09-05 11:25:29 +0100

    lxd/fsmonitor: Update interface to use fsmonitor.Event rather than string.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit a7015270f8c709665401fdf109ceb14264732439)

--- lxd/fsmonitor/drivers/common.go
+++ lxd/fsmonitor/drivers/common.go
@@ -5,19 +5,20 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/canonical/lxd/lxd/fsmonitor"
 	"github.com/canonical/lxd/shared/logger"
 )
 
 type common struct {
 	logger     logger.Logger
 	mu         sync.Mutex
-	watches    map[string]map[string]func(string, string) bool
+	watches    map[string]map[string]func(string, fsmonitor.Event) bool
 	prefixPath string
 }
 
 func (d *common) init(logger logger.Logger, path string) {
 	d.logger = logger
-	d.watches = make(map[string]map[string]func(string, string) bool)
+	d.watches = make(map[string]map[string]func(string, fsmonitor.Event) bool)
 	d.prefixPath = path
 }
 
@@ -30,7 +31,7 @@ func (d *common) PrefixPath() string {
 // inotify event, f() is called. If there already is a watch on the provided path, the callback
 // function will simply be replaced without returning an error.
 // Note: If f() returns false, the watch is removed.
-func (d *common) Watch(path string, identifier string, f func(path string, event string) bool) error {
+func (d *common) Watch(path string, identifier string, f func(path string, event fsmonitor.Event) bool) error {
 	d.logger.Info("Watching path", logger.Ctx{"path": path})
 
 	if f == nil {
@@ -48,7 +49,7 @@ func (d *common) Watch(path string, identifier string, f func(path string, event
 
 	_, ok := d.watches[path]
 	if !ok {
-		d.watches[path] = make(map[string]func(string, string) bool)
+		d.watches[path] = make(map[string]func(string, fsmonitor.Event) bool)
 	}
 
 	_, ok = d.watches[path][identifier]
