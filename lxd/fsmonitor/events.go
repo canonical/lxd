db629c8e51109fb53faa75cff25e5c6d0a5fab2a
Mark Laing <mark.laing@canonical.com>
2024-09-05 11:09:38 +0100

    lxd/fsmonitor: Add EventWrite and EventRename.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/fsmonitor/events.go
+++ lxd/fsmonitor/events.go
@@ -9,11 +9,21 @@ const (
 
 	// EventRemove represents the remove event.
 	EventRemove
+
+	// EventWrite represents the write event (a file that was opened for writing was closed).
+	EventWrite
+
+	// EventRename represents the rename event. This event will fire when a file is renamed to the watched file name,
+	// e.g. If watching `/some-dir`, and a file is renamed from `/some-dir/file.txt.tmp` to `/some-dir/file.txt`, the
+	// event fires for `/some-dir/file.txt`, and not for `/some-dir/file.txt.tmp`.
+	EventRename
 )
 
 func (e Event) String() string {
 	return map[Event]string{
 		EventAdd:    "add",
 		EventRemove: "remove",
+		EventWrite:  "write",
+		EventRename: "rename",
 	}[e]
 }
