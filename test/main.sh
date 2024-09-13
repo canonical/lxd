3c2a79da52f1bf62a860d0174c257b03a346d1b4
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-09-13 13:24:40 +0200

    test/suites/project: Check project operations before init
    This ensures projects can be both created and modified
    even if LXD isn't yet initialized.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit 39ea5dd61a0fcb59e557ce91ba3b4c4be2f5aa95)

--- test/main.sh
+++ test/main.sh
@@ -294,6 +294,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_projects_limits "projects limits"
     run_test test_projects_usage "projects usage"
     run_test test_projects_yaml "projects with yaml initialization"
+    run_test test_projects_before_init "project operations before init"
     run_test test_projects_restrictions "projects restrictions"
     run_test test_container_devices_disk "container devices - disk"
     run_test test_container_devices_disk_restricted "container devices - disk - restricted"
