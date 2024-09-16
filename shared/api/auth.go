d5428c8a9260b8e2d41990ed82fa9dcf404ae992
Mark Laing <mark.laing@canonical.com>
2024-09-16 09:05:23 +0100

    shared/api: Add API structs for identity creation.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- shared/api/auth.go
+++ shared/api/auth.go
@@ -105,6 +105,41 @@ type IdentityPut struct {
 	Groups []string `json:"groups" yaml:"groups"`
 }
 
+// IdentitiesPostTLS contains required information for the creation of a TLS identity.
+//
+// swagger:model
+//
+// API extension: access_management_tls.
+type IdentitiesPostTLS struct {
+	// Name associated with the identity
+	// Example: foo
+	Name string `json:"name" yaml:"name"`
+
+	// Trust token (used to add an untrusted client)
+	// Example: blah
+	TrustToken string `json:"trust_token" yaml:"trust_token"`
+
+	// Whether to create a certificate add token
+	// Example: true
+	Token bool `json:"token" yaml:"token"`
+
+	// The PEM encoded and base64 encoded public certificate of the identity
+	Certificate string `json:"certificate" yaml:"certificate"`
+
+	// Groups is the list of groups for which the identity is a member.
+	// Example: ["foo", "bar"]
+	Groups []string `json:"groups" yaml:"groups"`
+}
+
+// IdentityTokenTLS contains a token that can be used by an untrusted client to gain trust with a LXD system.
+//
+// swagger:model
+//
+// API extension: access_management_tls.
+type IdentityTokenTLS struct {
+	TrustToken string `json:"trust_token" yaml:"trust_token"`
+}
+
 // AuthGroup is the type for a LXD group.
 //
 // swagger:model
