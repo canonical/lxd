84237031dde52c794b4f010ba84046b6fce08ccf
Mark Laing <mark.laing@canonical.com>
2024-09-16 08:48:32 +0100

    lxd: Move CA check into `certificateValidate` method.
    Whether a new client is being trusted via secret, or if an
    administrator is adding or updating the certificate directly,
    the certificate must be signed by the CA if set.
    
    Note that this is *not* a security issue, because we validate
    that client certificates have been signed by the CA when authenticating.
    This just meant that it was possible for an admin to create a certificate
    that would be invalid.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit b1108094f0391fe1dcd55d547ebc9eef30236fd6)

--- lxd/certificates.go
+++ lxd/certificates.go
@@ -662,20 +662,13 @@ func certificatesPost(d *Daemon, r *http.Request) response.Response {
 		}
 
 		cert = r.TLS.PeerCertificates[len(r.TLS.PeerCertificates)-1]
-		networkCert := s.Endpoints.NetworkCert()
-		if networkCert.CA() != nil {
-			// If we are in CA mode, we only allow adding certificates that are signed by the CA.
-			trusted, _, _ := util.CheckCASignature(*cert, networkCert)
-			if !trusted {
-				return response.Forbidden(fmt.Errorf("The certificate is not trusted by the CA or has been revoked"))
-			}
-		}
 	} else {
 		return response.BadRequest(fmt.Errorf("Can't use TLS data on non-TLS link"))
 	}
 
 	// Check validity.
-	err = certificateValidate(cert)
+	networkCert := d.endpoints.NetworkCert()
+	err = certificateValidate(networkCert, cert)
 	if err != nil {
 		return response.BadRequest(err)
 	}
@@ -723,7 +716,7 @@ func certificatesPost(d *Daemon, r *http.Request) response.Response {
 	}
 
 	// Send a notification to other cluster members to refresh their identity cache.
-	notifier, err := cluster.NewNotifier(s, s.Endpoints.NetworkCert(), s.ServerCert(), cluster.NotifyAlive)
+	notifier, err := cluster.NewNotifier(s, networkCert, s.ServerCert(), cluster.NotifyAlive)
 	if err != nil {
 		return response.SmartError(err)
 	}
@@ -1031,6 +1024,7 @@ func doCertificateUpdate(ctx context.Context, d *Daemon, dbInfo api.Certificate,
 		}
 	}
 
+	networkCert := d.endpoints.NetworkCert()
 	if req.Certificate != "" && dbInfo.Certificate != req.Certificate {
 		// Add supplied certificate.
 		block, _ := pem.Decode([]byte(req.Certificate))
@@ -1044,7 +1038,7 @@ func doCertificateUpdate(ctx context.Context, d *Daemon, dbInfo api.Certificate,
 		dbCert.Fingerprint = shared.CertFingerprint(cert)
 
 		// Check validity.
-		err = certificateValidate(cert)
+		err = certificateValidate(networkCert, cert)
 		if err != nil {
 			return response.BadRequest(err)
 		}
@@ -1069,7 +1063,7 @@ func doCertificateUpdate(ctx context.Context, d *Daemon, dbInfo api.Certificate,
 	}
 
 	// Notify other cluster members to update their identity cache.
-	notifier, err := cluster.NewNotifier(s, s.Endpoints.NetworkCert(), s.ServerCert(), cluster.NotifyAlive)
+	notifier, err := cluster.NewNotifier(s, networkCert, s.ServerCert(), cluster.NotifyAlive)
 	if err != nil {
 		return response.SmartError(err)
 	}
@@ -1201,13 +1195,21 @@ func certificateDelete(d *Daemon, r *http.Request) response.Response {
 	return response.EmptySyncResponse
 }
 
-func certificateValidate(cert *x509.Certificate) error {
+func certificateValidate(networkCert *shared.CertInfo, cert *x509.Certificate) error {
 	if time.Now().Before(cert.NotBefore) {
 		return fmt.Errorf("The provided certificate isn't valid yet")
 	}
 
 	if time.Now().After(cert.NotAfter) {
 		return fmt.Errorf("The provided certificate is expired")
+	}
+
+	if networkCert != nil && networkCert.CA() != nil {
+		// If we are in CA mode, we only allow adding certificates that are signed by the CA.
+		trusted, _, _ := util.CheckCASignature(*cert, networkCert)
+		if !trusted {
+			return api.NewStatusError(http.StatusForbidden, "The certificate is not trusted by the CA or has been revoked")
+		}
 	}
 
 	if cert.PublicKeyAlgorithm == x509.RSA {
