2c59134e017bcc26ac62d228bde690b946883b71
Din Music <din.music@canonical.com>
2024-09-30 19:59:07 +0000

    lxd/storage/drivers/pure: Add login and request wrapper
    Signed-off-by: Din Music <din.music@canonical.com>
    (cherry picked from commit 332f95598fe825c3fb584b6035426a3d4cf0fbd4)

--- lxd/storage/drivers/driver_pure_util.go
+++ lxd/storage/drivers/driver_pure_util.go
@@ -1,5 +1,49 @@
 package drivers
 
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"slices"
+	"strings"
+
+	"github.com/canonical/lxd/shared"
+	"github.com/canonical/lxd/shared/api"
+)
+
+// pureAPIVersion is the Pure Storage API version used by LXD.
+// The 2.21 version is the first version that supports NVMe/TCP.
+const pureAPIVersion = "2.21"
+
+// pureError represents an error responses from Pure Storage API.
+type pureError struct {
+	// List of errors returned by the Pure Storage API.
+	Errors []struct {
+		Context string `json:"context"`
+		Message string `json:"message"`
+	} `json:"errors"`
+
+	// StatusCode is not part of the response body but is used
+	// to store the HTTP status code.
+	StatusCode int `json:"-"`
+}
+
+// Error returns the first error message from the Pure Storage API error.
+func (p *pureError) Error() string {
+	if p == nil || len(p.Errors) == 0 {
+		return ""
+	}
+
+	// Return the first error message without the trailing dot.
+	return strings.TrimSuffix(p.Errors[0].Message, ".")
+}
+
 // pureClient holds the Pure Storage HTTP client and an access token.
 type pureClient struct {
 	driver      *pure
@@ -11,4 +55,207 @@ func newPureClient(driver *pure) *pureClient {
 	return &pureClient{
 		driver: driver,
 	}
+}
+
+// createBodyReader creates a reader for the given request body contents.
+func (p *pureClient) createBodyReader(contents map[string]any) (io.Reader, error) {
+	body := &bytes.Buffer{}
+
+	err := json.NewEncoder(body).Encode(contents)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to write request body: %w", err)
+	}
+
+	return body, nil
+}
+
+// request issues a HTTP request against the Pure Storage gateway.
+func (p *pureClient) request(method string, url url.URL, reqBody io.Reader, reqHeaders map[string]string, respBody any, respHeaders map[string]string) error {
+	// Extract scheme and host from the gateway URL.
+	scheme, host, found := strings.Cut(p.driver.config["pure.gateway"], "://")
+	if !found {
+		return fmt.Errorf("Invalid Pure Storage gateway URL: %q", p.driver.config["pure.gateway"])
+	}
+
+	// Set request URL scheme and host.
+	url.Scheme = scheme
+	url.Host = host
+
+	// Prefixes the given path with the API version in the format "/api/<version>/<path>".
+	// If the path is "/api/api_version", the API version is not included as this path
+	// is used to retrieve supported API versions.
+	if url.Path != "/api/api_version" {
+		// If API version is not known yet, retrieve and cache it first.
+		if p.driver.apiVersion == "" {
+			apiVersions, err := p.getAPIVersions()
+			if err != nil {
+				return fmt.Errorf("Failed to retrieve supported Pure Storage API versions: %w", err)
+			}
+
+			// Ensure the required API version is supported by Pure Storage array.
+			if !slices.Contains(apiVersions, pureAPIVersion) {
+				return fmt.Errorf("Required API version %q is not supported by Pure Storage array", pureAPIVersion)
+			}
+
+			// Set API version to the driver to avoid checking the API version
+			// for each subsequent request.
+			p.driver.apiVersion = pureAPIVersion
+		}
+
+		// Prefix current path with the API version.
+		url.Path = path.Join("api", p.driver.apiVersion, url.Path)
+	}
+
+	req, err := http.NewRequest(method, url.String(), reqBody)
+	if err != nil {
+		return fmt.Errorf("Failed to create request: %w", err)
+	}
+
+	// Set custom request headers.
+	for k, v := range reqHeaders {
+		req.Header.Add(k, v)
+	}
+
+	req.Header.Add("Accept", "application/json")
+	if reqBody != nil {
+		req.Header.Add("Content-Type", "application/json")
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: shared.IsFalse(p.driver.config["pure.gateway.verify"]),
+			},
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Failed to send request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	// The unauthorized error is reported when an invalid (or expired) access token is provided.
+	// Wrap unauthorized requests into an API status error to allow easier checking for expired
+	// token in the requestAuthenticated function.
+	if resp.StatusCode == http.StatusUnauthorized {
+		return api.StatusErrorf(http.StatusUnauthorized, "Unauthorized request")
+	}
+
+	// Overwrite the response data type if an error is detected.
+	if resp.StatusCode != http.StatusOK {
+		respBody = &pureError{}
+	}
+
+	// Extract the response body if requested.
+	if respBody != nil {
+		err = json.NewDecoder(resp.Body).Decode(respBody)
+		if err != nil {
+			return fmt.Errorf("Failed to read response body from %q: %w", url.String(), err)
+		}
+	}
+
+	// Extract the response headers if requested.
+	if respHeaders != nil {
+		for k, v := range resp.Header {
+			respHeaders[k] = strings.Join(v, ",")
+		}
+	}
+
+	// Return the formatted error from the body
+	pureErr, ok := respBody.(*pureError)
+	if ok {
+		pureErr.StatusCode = resp.StatusCode
+		return pureErr
+	}
+
+	return nil
+}
+
+// requestAuthenticated issues an authenticated HTTP request against the Pure Storage gateway.
+// In case the access token is expired, the function will try to obtain a new one.
+func (p *pureClient) requestAuthenticated(method string, url url.URL, reqBody io.Reader, respBody any) error {
+	// If request fails with an unauthorized error, the request will be retried after
+	// requesting a new access token.
+	retries := 1
+
+	for {
+		// Ensure we are logged into the Pure Storage.
+		err := p.login()
+		if err != nil {
+			return err
+		}
+
+		// Set access token as request header.
+		reqHeaders := map[string]string{
+			"X-Auth-Token": p.accessToken,
+		}
+
+		// Initiate request.
+		err = p.request(method, url, reqBody, reqHeaders, respBody, nil)
+		if err != nil {
+			if api.StatusErrorCheck(err, http.StatusUnauthorized) && retries > 0 {
+				// Access token seems to be expired.
+				// Reset the token and try one more time.
+				p.accessToken = ""
+				retries--
+				continue
+			}
+
+			// Either the error is not of type unauthorized or the maximum number of
+			// retries has been exceeded.
+			return err
+		}
+
+		return nil
+	}
+}
+
+// getAPIVersion returns the list of API versions that are supported by the Pure Storage.
+func (p *pureClient) getAPIVersions() ([]string, error) {
+	var resp struct {
+		APIVersions []string `json:"version"`
+	}
+
+	url := api.NewURL().Path("api", "api_version")
+	err := p.request(http.MethodGet, url.URL, nil, nil, &resp, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to retrieve available API versions from Pure Storage: %w", err)
+	}
+
+	if len(resp.APIVersions) == 0 {
+		return nil, fmt.Errorf("Pure Storage does not support any API versions")
+	}
+
+	return resp.APIVersions, nil
+}
+
+// login initiates an authentication request against the Pure Storage using the API token. If successful,
+// an access token is retrieved and stored within a client. The access token is then used for further
+// authentication.
+func (p *pureClient) login() error {
+	if p.accessToken != "" {
+		// Token has been already obtained.
+		return nil
+	}
+
+	reqHeaders := map[string]string{
+		"api-token": p.driver.config["pure.api.token"],
+	}
+
+	respHeaders := make(map[string]string)
+
+	url := api.NewURL().Path("login")
+	err := p.request(http.MethodPost, url.URL, nil, reqHeaders, nil, respHeaders)
+	if err != nil {
+		return fmt.Errorf("Failed to login: %w", err)
+	}
+
+	p.accessToken = respHeaders["X-Auth-Token"]
+	if p.accessToken == "" {
+		return errors.New("Failed to obtain access token")
+	}
+
+	return nil
 }
