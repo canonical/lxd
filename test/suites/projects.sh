lxc/incus

tests: Don't over-provision test volume
Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

test/suites/projects.sh
@@ -1067,7 +1067,7 @@ test_projects_usage() {
     limits.cpu=1 \
     limits.memory=512MiB \
     limits.processes=20
-  incus profile device set default root size=3GiB --project test-usage
+  incus profile device set default root size=300MiB --project test-usage
 
   # Spin up a container
   deps/import-busybox --project test-usage --alias testimage
@@ -1076,7 +1076,7 @@ test_projects_usage() {
 
   incus project info test-usage --format csv | grep -q "CONTAINERS,UNLIMITED,1"
   incus project info test-usage --format csv | grep -q "CPU,5,1"
-  incus project info test-usage --format csv | grep -q "DISK,10.00GiB,3.00GiB"
+  incus project info test-usage --format csv | grep -q "DISK,10.00GiB,300.00MiB"
   incus project info test-usage --format csv | grep -q "INSTANCES,UNLIMITED,1"
   incus project info test-usage --format csv | grep -q "MEMORY,1.00GiB,512.00MiB"
   incus project info test-usage --format csv | grep -q "NETWORKS,3,0"

canonical/lxd

(cherry picked from commit a91b193660f4c8c5d298a49e6a889654ed35194a)
Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>
License: Apache-2.0
(cherry picked from commit 849b1dea006029ed2feff5f63ca9914c5fcbf838)

test/suites/projects.sh
@@ -1040,7 +1040,7 @@ test_projects_usage() {
     limits.cpu=1 \
     limits.memory=512MiB \
     limits.processes=20
-  lxc profile device set default root size=3GiB --project test-usage
+  lxc profile device set default root size=300MiB --project test-usage
 
   # Spin up a container
   deps/import-busybox --project test-usage --alias testimage
@@ -1049,7 +1049,7 @@ test_projects_usage() {
 
   lxc project info test-usage --format csv | grep -q "CONTAINERS,UNLIMITED,1"
   lxc project info test-usage --format csv | grep -q "CPU,5,1"
-  lxc project info test-usage --format csv | grep -q "DISK,10.00GiB,3.00GiB"
+  lxc project info test-usage --format csv | grep -q "DISK,10.00GiB,300.00MiB"
   lxc project info test-usage --format csv | grep -q "INSTANCES,UNLIMITED,1"
   lxc project info test-usage --format csv | grep -q "MEMORY,1.00GiB,512.00MiB"
   lxc project info test-usage --format csv | grep -q "NETWORKS,3,0"