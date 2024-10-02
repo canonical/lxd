4a85c129994fc69e20797a64ad35183b79506562
Kadin Sayani <kadin.sayani@canonical.com>
2024-10-02 10:55:54 -0600

    lxc/completion: Add instance device completions to `lxc config device add`
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxc/completion.go
+++ lxc/completion.go
@@ -339,6 +339,33 @@ func (g *cmdGlobal) cmpInstanceDeviceNames(instanceName string) ([]string, cobra
 	return results, cobra.ShellCompDirectiveNoFileComp
 }
 
+func (g *cmdGlobal) cmpInstanceAllDevices(instanceName string) ([]string, cobra.ShellCompDirective) {
+	resources, err := g.ParseServers(instanceName)
+	if err != nil || len(resources) == 0 {
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	resource := resources[0]
+	client := resource.server
+
+	metadataConfiguration, err := client.GetMetadataConfiguration()
+	if err != nil {
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	devices := make([]string, 0, len(metadataConfiguration.Configs))
+
+	for key := range metadataConfiguration.Configs {
+		if strings.HasPrefix(key, "device-") {
+			parts := strings.Split(key, "-")
+			deviceName := parts[1]
+			devices = append(devices, deviceName)
+		}
+	}
+
+	return devices, cobra.ShellCompDirectiveNoFileComp
+}
+
 func (g *cmdGlobal) cmpInstances(toComplete string) ([]string, cobra.ShellCompDirective) {
 	var results []string
 	cmpDirectives := cobra.ShellCompDirectiveNoFileComp
