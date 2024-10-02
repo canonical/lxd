11d00aca4a77a5a8f41a28eb4b9e1ebfbcd4fef8
Mark Laing <mark.laing@canonical.com>
2024-10-02 14:57:26 +0100

    client: Add client methods to delete an identity.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 81837b73e84f17de26fd114805ca3c06daff8529)

--- client/interfaces.go
+++ client/interfaces.go
@@ -445,6 +445,7 @@ type InstanceServer interface {
 	GetIdentity(authenticationMethod string, nameOrIdentifier string) (identity *api.Identity, ETag string, err error)
 	GetCurrentIdentityInfo() (identityInfo *api.IdentityInfo, ETag string, err error)
 	UpdateIdentity(authenticationMethod string, nameOrIdentifier string, identityPut api.IdentityPut, ETag string) error
+	DeleteIdentity(authenticationMethod string, nameOrIdentifier string) error
 	GetIdentityProviderGroupNames() (identityProviderGroupNames []string, err error)
 	GetIdentityProviderGroups() (identityProviderGroups []api.IdentityProviderGroup, err error)
 	GetIdentityProviderGroup(identityProviderGroupName string) (identityProviderGroup *api.IdentityProviderGroup, ETag string, err error)
