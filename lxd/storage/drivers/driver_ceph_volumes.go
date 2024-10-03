ed1b59eb0d99371976d4d430ae781ffd798c9701
cuishuang <imcusg@gmail.com>
2024-10-04 01:44:34 +0800

    fix: fix slice init length
    Signed-off-by: cuishuang <imcusg@gmail.com>
    (cherry picked from commit d9fdfc543dd2e14154fbe4a06f648b050f2657ba)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    License: Apache-2.0

--- lxd/storage/drivers/driver_ceph_volumes.go
+++ lxd/storage/drivers/driver_ceph_volumes.go
@@ -1520,7 +1520,7 @@ func (d *ceph) ListVolumes() ([]Volume, error) {
 		return nil, fmt.Errorf("Failed getting volume list: %v: %w", strings.TrimSpace(string(errMsg)), err)
 	}
 
-	volList := make([]Volume, len(vols))
+	volList := make([]Volume, 0, len(vols))
 	for _, v := range vols {
 		volList = append(volList, v)
 	}
