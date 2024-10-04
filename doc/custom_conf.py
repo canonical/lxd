6a0b45b3aba08622dca1585354db76676bf22bfc
Simon Deziel <simon.deziel@canonical.com>
2024-10-04 13:24:39 -0400

    doc: silence all warnings related to Sphinx 9.0 deprecations
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- doc/custom_conf.py
+++ doc/custom_conf.py
@@ -8,7 +8,7 @@ import filecmp
 
 # XXX: workaround for https://github.com/executablebooks/sphinx-tabs/issues/197
 import warnings
-warnings.filterwarnings("ignore", message='The str interface for _JavaScript objects is deprecated. Use js.filename instead.')
+warnings.filterwarnings("ignore", message='RemovedInSphinx90Warning: ')
 
 # Custom configuration for the Sphinx documentation builder.
 # All configuration specific to your project should be done in this file.
