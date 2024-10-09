0202557cf063b75ddaf7d3279a488ba70a4bedbf
Din Music <din.music@canonical.com>
2024-10-09 15:56:02 +0000

    lxd/storage/drivers/pure: Create and delete volume snapshots
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/drivers/driver_pure_util.go
+++ lxd/storage/drivers/driver_pure_util.go
@@ -592,6 +592,96 @@ func (p *pureClient) deleteVolume(poolName string, volName string) error {
 	return nil
 }
 
+// getVolumeSnapshots retrieves all existing snapshot for the given storage volume.
+func (p *pureClient) getVolumeSnapshots(poolName string, volName string) ([]pureVolume, error) {
+	var resp pureResponse[pureVolume]
+
+	url := api.NewURL().Path("volume-snapshots").WithQuery("source_names", poolName+"::"+volName)
+	err := p.requestAuthenticated(http.MethodGet, url.URL, nil, &resp)
+	if err != nil {
+		if isPureErrorNotFound(err) {
+			return nil, api.StatusErrorf(http.StatusNotFound, "Volume %q not found", volName)
+		}
+
+		return nil, fmt.Errorf("Failed to retrieve snapshots for volume %q in storage pool %q: %w", volName, poolName, err)
+	}
+
+	return resp.Items, nil
+}
+
+// getVolumeSnapshot retrieves an existing snapshot for the given storage volume.
+func (p *pureClient) getVolumeSnapshot(poolName string, volName string, snapshotName string) (*pureVolume, error) {
+	var resp pureResponse[pureVolume]
+
+	url := api.NewURL().Path("volume-snapshots").WithQuery("names", poolName+"::"+volName+"."+snapshotName)
+	err := p.requestAuthenticated(http.MethodGet, url.URL, nil, &resp)
+	if err != nil {
+		if isPureErrorNotFound(err) {
+			return nil, api.StatusErrorf(http.StatusNotFound, "Snapshot %q not found", snapshotName)
+		}
+
+		return nil, fmt.Errorf("Failed to retrieve snapshot %q for volume %q in storage pool %q: %w", snapshotName, volName, poolName, err)
+	}
+
+	if len(resp.Items) == 0 {
+		return nil, api.StatusErrorf(http.StatusNotFound, "Snapshot %q not found", snapshotName)
+	}
+
+	return &resp.Items[0], nil
+}
+
+// createVolumeSnapshot creates a new snapshot for the given storage volume.
+func (p *pureClient) createVolumeSnapshot(poolName string, volName string, snapshotName string) error {
+	req, err := p.createBodyReader(map[string]any{
+		"suffix": snapshotName,
+	})
+	if err != nil {
+		return err
+	}
+
+	url := api.NewURL().Path("volume-snapshots").WithQuery("source_names", poolName+"::"+volName)
+	err = p.requestAuthenticated(http.MethodPost, url.URL, req, nil)
+	if err != nil {
+		return fmt.Errorf("Failed to create snapshot %q for volume %q in storage pool %q: %w", snapshotName, volName, poolName, err)
+	}
+
+	return nil
+}
+
+// deleteVolumeSnapshot deletes an existing snapshot for the given storage volume.
+func (p *pureClient) deleteVolumeSnapshot(poolName string, volName string, snapshotName string) error {
+	snapshot, err := p.getVolumeSnapshot(poolName, volName, snapshotName)
+	if err != nil {
+		return err
+	}
+
+	if !snapshot.IsDestroyed {
+		// First destroy the snapshot.
+		req, err := p.createBodyReader(map[string]any{
+			"destroyed": true,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Destroy snapshot.
+		url := api.NewURL().Path("volume-snapshots").WithQuery("names", poolName+"::"+volName+"."+snapshotName)
+		err = p.requestAuthenticated(http.MethodPatch, url.URL, req, nil)
+		if err != nil {
+			return fmt.Errorf("Failed to destroy snapshot %q for volume %q in storage pool %q: %w", snapshotName, volName, poolName, err)
+		}
+	}
+
+	// Delete (eradicate) snapshot.
+	url := api.NewURL().Path("volume-snapshots").WithQuery("names", poolName+"::"+volName+"."+snapshotName)
+	err = p.requestAuthenticated(http.MethodDelete, url.URL, nil, nil)
+	if err != nil {
+		return fmt.Errorf("Failed to delete snapshot %q for volume %q in storage pool %q: %w", snapshotName, volName, poolName, err)
+	}
+
+	return nil
+}
+
 // getHosts retrieves an existing Pure Storage host.
 func (p *pureClient) getHosts() ([]pureHost, error) {
 	var resp pureResponse[pureHost]
