d1a826671096456c2a4cab8ea6157217e907ad06
hamistao <pedro.ribeiro@canonical.com>
2024-10-14 15:08:11 -0300

    lxd/response: Don't consider writing errors
    Errors while sending the response back to the client shouldn't be considered here since the handler was excecuted successfully and the faulty part could be either the server, network or the client. So here we consider this cases as successes for the metrics.
    
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- lxd/response/response.go
+++ lxd/response/response.go
@@ -148,7 +148,7 @@ func SyncResponsePlain(success bool, compress bool, metadata string) Response {
 }
 
 // Render renders a synchronous response.
-func (r *syncResponse) Render(w http.ResponseWriter, req *http.Request) (err error) {
+func (r *syncResponse) Render(w http.ResponseWriter, req *http.Request) error {
 	// Set an appropriate ETag header
 	if r.etag != nil {
 		etag, err := util.EtagHash(r.etag)
@@ -206,13 +206,10 @@ func (r *syncResponse) Render(w http.ResponseWriter, req *http.Request) (err err
 
 	// defer calling the callback function after possibly considering the response a SmartError.
 	defer func() {
-		// If there was an error on Render, the callback function will be called during the error handling.
-		if err == nil {
-			if r.success {
-				metrics.UseMetricsCallback(req, metrics.Success)
-			} else {
-				metrics.UseMetricsCallback(req, metrics.ErrorServer)
-			}
+		if r.success {
+			metrics.UseMetricsCallback(req, metrics.Success)
+		} else {
+			metrics.UseMetricsCallback(req, metrics.ErrorServer)
 		}
 	}()
 
@@ -223,12 +220,12 @@ func (r *syncResponse) Render(w http.ResponseWriter, req *http.Request) (err err
 				comp := gzip.NewWriter(w)
 				defer comp.Close()
 
-				_, err = comp.Write([]byte(r.metadata.(string)))
+				_, err := comp.Write([]byte(r.metadata.(string)))
 				if err != nil {
 					return err
 				}
 			} else {
-				_, err = w.Write([]byte(r.metadata.(string)))
+				_, err := w.Write([]byte(r.metadata.(string)))
 				if err != nil {
 					return err
 				}
@@ -251,9 +248,7 @@ func (r *syncResponse) Render(w http.ResponseWriter, req *http.Request) (err err
 		debugLogger = logger.AddContext(logger.Ctx{"http_code": code})
 	}
 
-	err = util.WriteJSON(w, resp, debugLogger)
-
-	return err
+	return util.WriteJSON(w, resp, debugLogger)
 }
 
 func (r *syncResponse) String() string {
@@ -426,13 +421,14 @@ func FileResponse(files []FileResponseEntry, headers map[string]string) Response
 }
 
 // Render renders a file response.
-func (r *fileResponse) Render(w http.ResponseWriter, req *http.Request) (err error) {
+func (r *fileResponse) Render(w http.ResponseWriter, req *http.Request) error {
 	if r.headers != nil {
 		for k, v := range r.headers {
 			w.Header().Set(k, v)
 		}
 	}
 
+	var err error
 	defer func() {
 		if err == nil {
 			// If there was an error on Render, the callback function will be called during the error handling.
@@ -527,7 +523,7 @@ func (r *fileResponse) Render(w http.ResponseWriter, req *http.Request) (err err
 			return err
 		}
 
-		_, err = io.Copy(fw, rd)
+		_, err := io.Copy(fw, rd)
 		if err != nil {
 			return err
 		}
