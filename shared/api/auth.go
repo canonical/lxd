6429d084514819ae41ae56cb06fa7560430f0849
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-11-05 16:08:35 +0100

    shared/api: Add `IsFineGrained` field to `IdentityInfo`
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- shared/api/auth.go
+++ shared/api/auth.go
@@ -92,6 +92,10 @@ type IdentityInfo struct {
 	// Effective permissions is the combined and deduplicated list of permissions that the identity has by virtue of
 	// direct membership to a LXD group, or effective membership of a LXD group via identity provider group mappings.
 	EffectivePermissions []Permission `json:"effective_permissions" yaml:"effective_permissions"`
+
+	// IsFineGrained is a boolean indicating whether the identity is fine-grained,
+	// meaning that permissions are managed via group membership.
+	IsFineGrained bool `json:"is_fine_grained" yaml:"is_fine_grained"`
 }
 
 // IdentityPut contains the editable fields of an IdentityInfo.
