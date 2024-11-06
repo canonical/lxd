d5dd293b1a3964e0b39c1c3d1d250043e198b50b
Mark Laing <mark.laing@canonical.com>
2024-11-06 09:21:44 +0000

    lxd/db: Define database specific smart errors.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/errors.go
+++ lxd/db/errors.go
@@ -2,9 +2,19 @@ package db
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/canonical/go-dqlite/v3/driver"
+	"github.com/mattn/go-sqlite3"
 )
 
 var (
 	// ErrNoClusterMember is used to indicate no cluster member has been found for a resource.
 	ErrNoClusterMember = fmt.Errorf("No cluster member found")
 )
+
+// SmartErrors are used to return more appropriate errors to the caller.
+var SmartErrors = map[int][]error{
+	http.StatusConflict:           {sqlite3.ErrConstraintUnique},
+	http.StatusServiceUnavailable: {driver.ErrNoAvailableLeader},
+}
