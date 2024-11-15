fe1e7070df1d63ea7ae257e2ca06c7d4338bd615
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-11-15 17:22:03 -0600

    lxd: Rename UnmountCustomVolume -> UnmountVolume
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- lxd/daemon_storage.go
+++ lxd/daemon_storage.go
@@ -51,7 +51,7 @@ func daemonStorageVolumesUnmount(s *state.State) error {
 		}
 
 		// Mount volume.
-		_, err = pool.UnmountCustomVolume(api.ProjectDefaultName, volumeName, nil)
+		_, err = pool.UnmountVolume(api.ProjectDefaultName, volumeName, storageDrivers.VolumeTypeCustom, nil)
 		if err != nil {
 			return fmt.Errorf("Failed to unmount storage volume %q: %w", source, err)
 		}
@@ -201,7 +201,9 @@ func daemonStorageValidate(s *state.State, target string) error {
 		return fmt.Errorf("Failed to mount storage volume %q: %w", target, err)
 	}
 
-	defer func() { _, _ = pool.UnmountCustomVolume(api.ProjectDefaultName, volumeName, nil) }()
+	defer func() {
+		_, _ = pool.UnmountVolume(api.ProjectDefaultName, volumeName, storageDrivers.VolumeTypeCustom, nil)
+	}()
 
 	// Validate volume is empty (ignore lost+found).
 	volStorageName := project.StorageVolume(api.ProjectDefaultName, volumeName)
@@ -302,7 +304,7 @@ func daemonStorageMove(s *state.State, storageType string, target string) error 
 
 		// Unmount old volume.
 		projectName, sourceVolumeName := project.StorageVolumeParts(sourceVolume)
-		_, err = pool.UnmountCustomVolume(projectName, sourceVolumeName, nil)
+		_, err = pool.UnmountVolume(projectName, sourceVolumeName, storageDrivers.VolumeTypeCustom, nil)
 		if err != nil {
 			return fmt.Errorf(`Failed to umount storage volume "%s/%s": %w`, sourcePool, sourceVolumeName, err)
 		}
@@ -369,7 +371,7 @@ func daemonStorageMove(s *state.State, storageType string, target string) error 
 
 		// Unmount old volume.
 		projectName, sourceVolumeName := project.StorageVolumeParts(sourceVolume)
-		_, err = pool.UnmountCustomVolume(projectName, sourceVolumeName, nil)
+		_, err = pool.UnmountVolume(projectName, sourceVolumeName, storageDrivers.VolumeTypeCustom, nil)
 		if err != nil {
 			return fmt.Errorf(`Failed to umount storage volume "%s/%s": %w`, sourcePool, sourceVolumeName, err)
 		}
