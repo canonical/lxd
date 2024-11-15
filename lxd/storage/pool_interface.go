55c2e2c189cad54cba590ff85f917f895d2861b6
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-11-15 17:20:20 -0600

    lxd/storage: Rename UnmountCustomVolume -> UnmountVolume
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- lxd/storage/pool_interface.go
+++ lxd/storage/pool_interface.go
@@ -62,6 +62,7 @@ type Pool interface {
 
 	GetVolume(volumeType drivers.VolumeType, contentType drivers.ContentType, name string, config map[string]string) drivers.Volume
 	MountVolume(projectName string, volName string, volType drivers.VolumeType, op *operations.Operation) (*MountInfo, error)
+	UnmountVolume(projectName string, volName string, volType drivers.VolumeType, op *operations.Operation) (bool, error)
 
 	// Instances.
 	CreateInstance(inst instance.Instance, op *operations.Operation) error
@@ -121,7 +122,6 @@ type Pool interface {
 	RenameCustomVolume(projectName string, volName string, newVolName string, op *operations.Operation) error
 	DeleteCustomVolume(projectName string, volName string, op *operations.Operation) error
 	GetCustomVolumeUsage(projectName string, volName string) (*VolumeUsage, error)
-	UnmountCustomVolume(projectName string, volName string, op *operations.Operation) (bool, error)
 	ImportCustomVolume(projectName string, poolVol *backupConfig.Config, op *operations.Operation) (revert.Hook, error)
 	RefreshCustomVolume(projectName string, srcProjectName string, volName, desc string, config map[string]string, srcPoolName, srcVolName string, snapshots bool, op *operations.Operation) error
 	GenerateCustomVolumeBackupConfig(projectName string, volName string, snapshots bool, op *operations.Operation) (*backupConfig.Config, error)
