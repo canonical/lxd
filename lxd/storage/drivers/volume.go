cf9c283b2469d1f13f38e1f3874933c1d23b3b7b
hamistao <pedro.ribeiro@canonical.com>
2024-11-26 10:11:08 -0300

    lxd/storage/drivers: Use pool-specific `DefaultBlockSize`
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- lxd/storage/drivers/volume.go
+++ lxd/storage/drivers/volume.go
@@ -475,7 +475,7 @@ func (v Volume) ConfigSize() string {
 	// If volume size isn't defined in either volume or pool config, then for block volumes or block-backed
 	// volumes return the defaultBlockSize.
 	if (size == "" || size == "0") && (v.contentType == ContentTypeBlock || v.IsBlockBacked()) {
-		return DefaultBlockSize
+		return v.driver.Info().DefaultBlockSize
 	}
 
 	// Return defined size or empty string if not defined.
