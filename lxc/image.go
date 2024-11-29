b330da9307edf1f532b282c2d5693c9e72d14685
Kadin Sayani <kadin.sayani@canonical.com>
2024-11-29 16:25:48 -0700

    lxc/image: Add `GetImagesAllProjects` and `GetImagesAllProjectsWithFilter` client functions and interfaces to `simplestreams_images.go`
    This commit fixes a regression introduced with support for fetching
    images across all projects by adding `GetImagesAllProjects` and
    `GetImagesAllProjectsWithFilter` functions to
    `client/simplestreams_images.go`, and moving their method signatures to
    the `ImageServer` interface.
    
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    (cherry picked from commit 003e92879e1a9590985b5518e900b30041bbb916)

--- lxc/image.go
+++ lxc/image.go
@@ -1353,11 +1353,6 @@ func (c *cmdImageList) run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	d, err := c.global.conf.GetInstanceServer(remoteName)
-	if err != nil {
-		return err
-	}
-
 	// Process the filters
 	filters := []string{}
 	if name != "" {
@@ -1378,9 +1373,9 @@ func (c *cmdImageList) run(cmd *cobra.Command, args []string) error {
 
 	var allImages, images []api.Image
 	if c.flagAllProjects {
-		allImages, err = d.GetImagesAllProjectsWithFilter(serverFilters)
+		allImages, err = remoteServer.GetImagesAllProjectsWithFilter(serverFilters)
 		if err != nil {
-			allImages, err = d.GetImagesAllProjects()
+			allImages, err = remoteServer.GetImagesAllProjects()
 			if err != nil {
 				return err
 			}
