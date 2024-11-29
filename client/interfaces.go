003e92879e1a9590985b5518e900b30041bbb916
Kadin Sayani <kadin.sayani@canonical.com>
2024-11-29 16:25:48 -0700

    lxc/image: Add `GetImagesAllProjects` and `GetImagesAllProjectsWithFilter` client functions and interfaces to `simplestreams_images.go`
    This commit fixes a regression introduced with support for fetching
    images across all projects by adding `GetImagesAllProjects` and
    `GetImagesAllProjectsWithFilter` functions to
    `client/simplestreams_images.go`, and moving their method signatures to
    the `ImageServer` interface.
    
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -50,6 +50,8 @@ type ImageServer interface {
 	GetImages() (images []api.Image, err error)
 	GetImageFingerprints() (fingerprints []string, err error)
 	GetImagesWithFilter(filters []string) (images []api.Image, err error)
+	GetImagesAllProjects() (images []api.Image, err error)
+	GetImagesAllProjectsWithFilter(filters []string) (images []api.Image, err error)
 
 	GetImage(fingerprint string) (image *api.Image, ETag string, err error)
 	GetImageFile(fingerprint string, req ImageFileRequest) (resp *ImageFileResponse, err error)
@@ -243,8 +245,6 @@ type InstanceServer interface {
 	UpdateImageAlias(name string, alias api.ImageAliasesEntryPut, ETag string) (err error)
 	RenameImageAlias(name string, alias api.ImageAliasesEntryPost) (err error)
 	DeleteImageAlias(name string) (err error)
-	GetImagesAllProjects() (images []api.Image, err error)
-	GetImagesAllProjectsWithFilter(filters []string) (images []api.Image, err error)
 
 	// Network functions ("network" API extension)
 	GetNetworkNames() (names []string, err error)
