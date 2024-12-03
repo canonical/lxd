0055193526fdc249fb5900245e9b06dd0c469ac5
Simon Deziel <simon.deziel@canonical.com>
2024-12-03 16:15:50 -0500

    lxc/completion: use strings.Cut() where appropriate
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxc/completion.go
+++ lxc/completion.go
@@ -189,7 +189,7 @@ func (g *cmdGlobal) cmpImages(toComplete string) ([]string, cobra.ShellCompDirec
 	cmpDirectives := cobra.ShellCompDirectiveNoFileComp
 
 	if strings.Contains(toComplete, ":") {
-		remote = strings.Split(toComplete, ":")[0]
+		remote, _, _ = strings.Cut(toComplete, ":")
 	} else {
 		remote = g.conf.DefaultRemote
 	}
@@ -432,9 +432,9 @@ func (g *cmdGlobal) cmpInstanceConfigTemplates(instanceName string) ([]string, c
 	resource := resources[0]
 	client := resource.server
 
-	var instanceNameOnly = instanceName
-	if strings.Contains(instanceName, ":") {
-		instanceNameOnly = strings.Split(instanceName, ":")[1]
+	_, instanceNameOnly, _ := strings.Cut(instanceName, ":")
+	if instanceNameOnly == "" {
+		instanceNameOnly = instanceName
 	}
 
 	results, err := client.GetInstanceTemplateFiles(instanceNameOnly)
