ae3ddeeae88593207fbb46672b13197294482247
Din Music <din.music@canonical.com>
2024-12-05 16:01:34 +0000

    client/lxd: Fix non-constant format string
    Signed-off-by: Din Music <din.music@canonical.com>
    (cherry picked from commit ec7a109073af149583bdd5cc3c81d2e58e7f6653)

--- client/lxd.go
+++ client/lxd.go
@@ -221,7 +221,7 @@ func lxdParseResponse(resp *http.Response) (*api.Response, string, error) {
 
 	// Handle errors
 	if response.Type == api.ErrorResponse {
-		return nil, "", api.StatusErrorf(resp.StatusCode, response.Error)
+		return nil, "", api.StatusErrorf(resp.StatusCode, "%s", response.Error)
 	}
 
 	return &response, etag, nil
@@ -354,7 +354,7 @@ func (r *ProtocolLXD) queryStruct(method string, path string, data any, ETag str
 
 	// Log the data
 	logger.Debugf("Got response struct from LXD")
-	logger.Debugf(logger.Pretty(target))
+	logger.Debug(logger.Pretty(target))
 
 	return etag, nil
 }
@@ -401,7 +401,7 @@ func (r *ProtocolLXD) queryOperation(method string, path string, data any, ETag 
 
 	// Log the data
 	logger.Debugf("Got operation from LXD")
-	logger.Debugf(logger.Pretty(op.Operation))
+	logger.Debug(logger.Pretty(op.Operation))
 
 	return &op, etag, nil
 }
