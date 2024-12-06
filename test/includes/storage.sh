fd0b2ecfe41665d81fa040eaf49124377c369761
Din Music <din.music@canonical.com>
2024-12-06 15:24:37 +0000

    test/includes/storage: Include pure storage driver if gateway is set
    Signed-off-by: Din Music <din.music@canonical.com>

--- test/includes/storage.sh
+++ test/includes/storage.sh
@@ -39,6 +39,10 @@ available_storage_backends() {
 
     backends="dir" # always available
 
+    if [ -n "${PURESTORAGE_GATEWAY:-}" ]; then
+        backends="$backends pure"
+    fi
+
     storage_backends="btrfs lvm zfs"
     if [ -n "${LXD_CEPH_CLUSTER:-}" ]; then
         storage_backends="${storage_backends} ceph"
