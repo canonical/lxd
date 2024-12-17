44925f71a677de587e84b50213a787d60ff5fb32
Simon Deziel <simon.deziel@canonical.com>
2024-12-17 17:00:26 -0500

    test/suites/filtering: remove `|| false` on tests that should succeed
    Also simplify output comparison.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/filtering.sh
+++ test/suites/filtering.sh
@@ -16,20 +16,15 @@ test_filtering() {
     lxc init testimage c1
     lxc init testimage c2
 
-    count=$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=0" --data-urlencode "filter=name eq c1" | jq ".metadata | length")
-    [ "${count}" = "1" ] || false
+    [ "$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=0" --data-urlencode "filter=name eq c1" | jq ".metadata | length")" = "1" ]
 
-    count=$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=1" --data-urlencode "filter=name eq c1" | jq ".metadata | length")
-    [ "${count}" = "1" ] || false
+    [ "$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=1" --data-urlencode "filter=name eq c1" | jq ".metadata | length")" = "1" ]
 
-    count=$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=2" --data-urlencode "filter=name eq c1" | jq ".metadata | length")
-    [ "${count}" = "1" ] || false
+    [ "$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=2" --data-urlencode "filter=name eq c1" | jq ".metadata | length")" = "1" ]
 
-    count=$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/images" --data-urlencode "recursion=0" --data-urlencode "filter=properties.os eq BusyBox" | jq ".metadata | length")
-    [ "${count}" = "1" ] || false
+    [ "$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/images" --data-urlencode "recursion=0" --data-urlencode "filter=properties.os eq BusyBox" | jq ".metadata | length")" = "1" ]
 
-    count=$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/images" --data-urlencode "recursion=1" --data-urlencode "filter=properties.os eq Ubuntu" | jq ".metadata | length")
-    [ "${count}" = "0" ] || false
+    [ "$(curl -G --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/images" --data-urlencode "recursion=1" --data-urlencode "filter=properties.os eq Ubuntu" | jq ".metadata | length")" = "0" ]
 
     lxc delete c1
     lxc delete c2
