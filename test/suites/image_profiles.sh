6a085670593bbd662106b136cf03b21048be16b9
Simon Deziel <simon.deziel@canonical.com>
2024-12-17 17:22:51 -0500

    test/suites/image_profiles: remove `|| false` on tests that should succeed
    Use shell comparison where applicable.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/image_profiles.sh
+++ test/suites/image_profiles.sh
@@ -2,7 +2,7 @@ _image_nil_profile_list() {
   # Launch container with default profile list and check its profiles
   ensure_import_testimage
   lxc launch testimage c1
-  lxc list -f json c1 | jq -r '.[0].profiles | join(" ")' | grep -q "default" || false
+  [ "$(lxc list -f json c1 | jq -r '.[0].profiles | join(" ")')" = "default" ]
 
   # Cleanup
   lxc delete c1 -f
@@ -15,13 +15,13 @@ _image_empty_profile_list() {
   lxc image show testimage | sed "s/profiles.*/profiles: []/; s/- default//" | lxc image edit testimage
 
   # Check that the profile list is correct
-  lxc image show testimage | grep -q 'profiles: \[\]' || false
+  lxc image show testimage | grep -qF 'profiles: []'
   ! lxc image show testimage | grep -q -- '- default' || false
 
   # Launch the container and check its profiles
   storage=$(lxc storage list | grep "^| " | tail -n 1 | cut -d' ' -f2)
   lxc launch testimage c1 -s "$storage"
-  lxc list -f json c1 | jq -r '.[0].profiles | join(" ")' | grep -q "^$" || false
+  [ "$(lxc list -f json c1 | jq -r '.[0].profiles | join(" ")')" = "" ]
 
   # Cleanup
   lxc delete c1 -f
@@ -37,16 +37,16 @@ _image_alternate_profile_list() {
   lxc image show testimage | sed "s/profiles.*/profiles: ['p1','p2','p3']/; s/- default//" | lxc image edit testimage
 
   # Check that the profile list is correct
-  lxc image show testimage | grep -q -- '- p1' || false
-  lxc image show testimage | grep -q -- '- p2' || false
-  lxc image show testimage | grep -q -- '- p3' || false
+  lxc image show testimage | grep -q -- '- p1'
+  lxc image show testimage | grep -q -- '- p2'
+  lxc image show testimage | grep -q -- '- p3'
   ! lxc image show testimage | grep -q -- '- default' || false
 
   # Launch the container and check its profiles
   storage=$(lxc storage list | grep "^| " | tail -n 1 | cut -d' ' -f2)
   lxc profile device add p1 root disk path=/ pool="$storage"
   lxc launch testimage c1
-  lxc list -f json c1 | jq -r '.[0].profiles | join(" ")' | grep -q "p1 p2 p3" || false
+  [ "$(lxc list -f json c1 | jq -r '.[0].profiles | join(" ")')" = "p1 p2 p3" ]
 
   # Cleanup
   lxc delete c1 -f
