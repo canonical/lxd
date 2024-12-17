a6235200670e5bceaa40e2827e00f680bf06a6b9
Mark Laing <mark.laing@canonical.com>
2024-12-17 17:23:48 +0000

    test/suites: Update `lxd sql` usage in oidc test.
    Using `count` and `--format csv` we can assert the user is present in the
    database without `wc -l`.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 900c87e19eb5c2644d60f3227c396d565252c70e)

--- test/suites/oidc.sh
+++ test/suites/oidc.sh
@@ -30,7 +30,7 @@ test_oidc() {
   [ "$(lxc query oidc:/1.0 | jq -r '.auth_user_name')" = "test-user@example.com" ]
 
   # OIDC user should be added to identities table.
-  [ "$(lxd sql global "SELECT identifier, name, auth_method, type FROM identities WHERE type = 5 AND identifier = 'test-user@example.com' AND auth_method = 2" | wc -l)" = 5 ]
+  [ "$(lxd sql global --format csv "SELECT count(*) FROM identities WHERE type = 5 AND identifier = 'test-user@example.com' AND auth_method = 2")" = 1 ]
 
   # Cleanup OIDC
   lxc auth identity delete oidc/test-user@example.com
