fde613adfa0a1c1956c87e525041fdcd4c75a8cb
Mark Laing <mark.laing@canonical.com>
2024-12-20 12:14:37 +0000

    lxd/db: Add freshschema package.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 8b52bd1c6f6d504cd5cefee631f0bcd63b514054)

---
+++ lxd/db/freshschema/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/canonical/lxd/lxd/db/cluster"
+	"github.com/canonical/lxd/lxd/db/node"
+)
+
+func main() {
+	err := freshSchema(os.Args[1:])
+	if err != nil {
+		_, _ = os.Stderr.Write([]byte(err.Error()))
+		os.Exit(1)
+	}
+}
+
+// UpdateSchema updates the schema.go file of the cluster and node databases.
+func freshSchema(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf(`Schema kind must be provided (must be "node", or "cluster")`)
+	}
+
+	kind := args[0]
+	var err error
+	switch kind {
+	case "node":
+		err = node.SchemaDotGo()
+	case "cluster":
+		err = cluster.SchemaDotGo()
+	default:
+		return fmt.Errorf(`No such schema kind %q (must be "node", or "cluster")`, kind)
+	}
+
+	if err != nil {
+		return fmt.Errorf("Update node database schema: %w", err)
+	}
+
+	return nil
+}
