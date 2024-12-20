20c25eac2126030702fb44eef3c8a9660cfc9ae3
Mark Laing <mark.laing@canonical.com>
2024-12-20 10:37:11 +0000

    lxd/db/generate/db: Expect database kind as argument.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit e55e06beca407ee54a7f6f64df4f687b1ea3e811)

--- lxd/db/generate/db/schema.go
+++ lxd/db/generate/db/schema.go
@@ -8,13 +8,17 @@ import (
 )
 
 // UpdateSchema updates the schema.go file of the cluster and node databases.
-func UpdateSchema() error {
-	err := cluster.SchemaDotGo()
-	if err != nil {
-		return fmt.Errorf("Update cluster database schema: %w", err)
+func UpdateSchema(kind string) error {
+	var err error
+	switch kind {
+	case "node":
+		err = node.SchemaDotGo()
+	case "cluster":
+		err = cluster.SchemaDotGo()
+	default:
+		return fmt.Errorf(`No such schema kind %q (must be "node", or "cluster")`, kind)
 	}
 
-	err = node.SchemaDotGo()
 	if err != nil {
 		return fmt.Errorf("Update node database schema: %w", err)
 	}
