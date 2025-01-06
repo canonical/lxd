92a475f8a778b82399b2f0f14959df5cdda145d3
hamistao <pedro.ribeiro@canonical.com>
2025-01-06 07:47:47 -0300

    lxd/device/disk: Update `createDevice` call for VMs
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- lxd/device/disk.go
+++ lxd/device/disk.go
@@ -1156,16 +1156,20 @@ func (d *disk) startVM() (*deviceConfig.RunConfig, error) {
 					return nil, fmt.Errorf(`Missing mount "path" setting`)
 				}
 
+				// Escaping to avoid invalid source errors.
+				devPath := d.virtiofsdSourceEncode(d.getDevicePath(d.name, d.config))
+
 				// Mount the source in the instance devices directory.
 				// This will ensure that if the exported directory configured as readonly that this
 				// takes effect event if using virtio-fs (which doesn't support read only mode) by
 				// having the underlying mount setup as readonly.
 				var revertFunc func()
-				revertFunc, mount.DevPath, _, err = d.createDevice(mount.DevPath)
+				revertFunc, _, err = d.createDevice(mount.DevPath, devPath)
 				if err != nil {
 					return nil, err
 				}
 
+				mount.DevPath = devPath
 				revert.Add(revertFunc)
 
 				mount.TargetPath = d.config["path"]
