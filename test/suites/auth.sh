887447b4f2ccb9b31da7a633662c08fdcfa8578b
Gabriel Mougard <gabriel.mougard@canonical.com>
2025-01-08 15:44:46 +0100

    test/auth: Add check for entitlements
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- test/suites/auth.sh
+++ test/suites/auth.sh
@@ -138,7 +138,8 @@ test_authorization() {
   lxc auth identity list --format csv | grep -Fq "tls,Client certificate,test-user,${tls_identity_fingerprint},test-group"
 
   # Test `lxc auth identity info`
-  expectedOIDCInfo='authentication_method: oidc
+  expectedOIDCInfo='access_entitlements: []
+authentication_method: oidc
 type: OIDC client
 id: test-user@example.com
 name: '"'"' '"'"'
@@ -152,7 +153,8 @@ fine_grained: true'
 
   [ "$(lxc auth identity info oidc:)" = "${expectedOIDCInfo}" ]
 
-  expectedTLSInfo="authentication_method: tls
+  expectedTLSInfo="access_entitlements: []
+authentication_method: tls
 type: Client certificate
 id: ${tls_identity_fingerprint}
 name: test-user
