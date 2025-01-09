58e780c5e9399953e6debeef352e3bda68756776
Wesley Hershberger <wesley.hershberger@canonical.com>
2025-01-09 16:51:45 -0600

    lxd/db/cluster: Add StoragePoolVolumeType newtype
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- lxd/db/cluster/storage_volume_type.go
+++ lxd/db/cluster/storage_volume_type.go
@@ -1,9 +1,21 @@
 package cluster
 
+import (
+	"errors"
+)
+
+// StoragePoolVolumeType records a volume's type in the database.
+//
+// # Type Safety
+// Funtions using this type should assume that a StoragePoolVolumeType is always
+// a valid value; i.e. that it is type safe. Use the parsing methods below when
+// converting to StoragePoolVolumeType.
+type StoragePoolVolumeType int
+
 // XXX: this was extracted from lxd/storage_volume_utils.go, we find a way to
 // factor it independently from both the db and main packages.
 const (
-	StoragePoolVolumeTypeContainer = iota
+	StoragePoolVolumeTypeContainer StoragePoolVolumeType = iota
 	StoragePoolVolumeTypeImage
 	StoragePoolVolumeTypeCustom
 	StoragePoolVolumeTypeVM
@@ -19,12 +31,50 @@ const (
 	StoragePoolVolumeTypeNameCustom    string = "custom"
 )
 
-// StoragePoolVolumeTypeNames represents a map of storage volume types and their names.
-var StoragePoolVolumeTypeNames = map[int]string{
-	StoragePoolVolumeTypeContainer: "container",
-	StoragePoolVolumeTypeImage:     "image",
-	StoragePoolVolumeTypeCustom:    "custom",
-	StoragePoolVolumeTypeVM:        "virtual-machine",
+// StoragePoolVolumeTypeFromInt is a checked conversion to StoragePoolVolumeType.
+func StoragePoolVolumeTypeFromInt(volType int) (StoragePoolVolumeType, error) {
+	switch StoragePoolVolumeType(volType) {
+	case StoragePoolVolumeTypeContainer, StoragePoolVolumeTypeVM, StoragePoolVolumeTypeCustom, StoragePoolVolumeTypeImage:
+		return StoragePoolVolumeType(volType), nil
+	default:
+		return StoragePoolVolumeType(volType), errors.New("Invalid storage volume type")
+	}
+}
+
+// StoragePoolVolumeTypeFromName is a checked conversion to StoragePoolVolumeType.
+func StoragePoolVolumeTypeFromName(volTypeName string) (StoragePoolVolumeType, error) {
+	switch volTypeName {
+	case StoragePoolVolumeTypeNameContainer:
+		return StoragePoolVolumeTypeContainer, nil
+	case StoragePoolVolumeTypeNameVM:
+		return StoragePoolVolumeTypeVM, nil
+	case StoragePoolVolumeTypeNameImage:
+		return StoragePoolVolumeTypeImage, nil
+	case StoragePoolVolumeTypeNameCustom:
+		return StoragePoolVolumeTypeCustom, nil
+	}
+
+	return StoragePoolVolumeTypeCustom, errors.New("Invalid storage volume type")
+}
+
+// Name gives the name of a StoragePoolVolumeType.
+//
+// # Safety
+// This function assumes that `t` is one of the StoragePoolVolumeType enums
+// defined above.
+func (t StoragePoolVolumeType) Name() string {
+	switch t {
+	case StoragePoolVolumeTypeContainer:
+		return StoragePoolVolumeTypeNameContainer
+	case StoragePoolVolumeTypeVM:
+		return StoragePoolVolumeTypeNameVM
+	case StoragePoolVolumeTypeImage:
+		return StoragePoolVolumeTypeNameImage
+	case StoragePoolVolumeTypeCustom:
+		return StoragePoolVolumeTypeNameCustom
+	}
+
+	return StoragePoolVolumeTypeNameCustom
 }
 
 // Content types.
