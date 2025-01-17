3007dbd835ee709ab588bfe3dcfb76c73a167e3c
Kadin Sayani <kadin.sayani@canonical.com>
2025-01-17 13:05:57 -0700

    api: Add `cluster_links` extension
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -448,6 +448,7 @@ var APIExtensions = []string{
 	"backup_metadata_version",
 	"storage_buckets_all_projects",
 	"network_acls_all_projects",
+	"cluster_links",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
