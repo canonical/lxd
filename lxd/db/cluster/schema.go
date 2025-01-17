353c38300dc5998a226d5fab31ae256cda8fb5fc
Kadin Sayani <kadin.sayani@canonical.com>
2025-01-17 13:24:22 -0700

    lxd/db/cluster: Run `make update-schema`
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/db/cluster/schema.go
+++ lxd/db/cluster/schema.go
@@ -481,6 +481,25 @@ CREATE TABLE "projects_config" (
     FOREIGN KEY (project_id) REFERENCES "projects" (id) ON DELETE CASCADE,
     UNIQUE (project_id, key)
 );
+CREATE TABLE sites (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	identity_id INTEGER NOT NULL,
+	name TEXT NOT NULL,
+	addresses TEXT NOT NULL,
+	type INTEGER NOT NULL,
+	delegated INTEGER NOT NULL,
+	description TEXT NOT NULL,
+	UNIQUE(name),
+	FOREIGN KEY (identity_id) REFERENCES identities (id) ON DELETE CASCADE
+);
+CREATE TABLE sites_config(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	site_id INTEGER NOT NULL,
+	key TEXT NOT NULL,
+	value TEXT NOT NULL,
+	FOREIGN KEY (site_id) REFERENCES sites (id) ON DELETE CASCADE,
+	UNIQUE(site_id)
+);
 CREATE TABLE "storage_buckets" (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	name TEXT NOT NULL,
@@ -662,5 +681,5 @@ CREATE TABLE "warnings" (
 );
 CREATE UNIQUE INDEX warnings_unique_node_id_project_id_entity_type_code_entity_id_type_code ON warnings(IFNULL(node_id, -1), IFNULL(project_id, -1), entity_type_code, entity_id, type_code);
 
-INSERT INTO schema (version, updated_at) VALUES (73, strftime("%s"))
+INSERT INTO schema (version, updated_at) VALUES (74, strftime("%s"))
 `
