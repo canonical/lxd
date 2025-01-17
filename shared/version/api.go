39529008adf933e2e68004416670097d92051a5b
Kadin Sayani <kadin.sayani@canonical.com>
2025-01-17 13:05:57 -0700

    api: Add `services` extension
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -439,6 +439,7 @@ var APIExtensions = []string{
 	"cloud_init_ssh_keys",
 	"oidc_scopes",
 	"project_default_network_and_storage",
+	"services",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
