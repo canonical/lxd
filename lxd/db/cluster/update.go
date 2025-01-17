1f4abeaca8dcae81a66db1da09403f2093771d42
Kadin Sayani <kadin.sayani@canonical.com>
2025-01-17 13:14:07 -0700

    lxd/db/cluster: Add cluster links tables
    Add `cluster_links` and `cluster_links_config`, and update the schema.
    
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/db/cluster/update.go
+++ lxd/db/cluster/update.go
@@ -123,6 +123,33 @@ var updates = map[int]schema.Update{
 	78: updateFromV77,
 	79: updateFromV78,
 	80: updateFromV79,
+	81: updateFromV80,
+}
+
+func updateFromV80(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.ExecContext(ctx, `
+CREATE TABLE cluster_links (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	identity_id INTEGER NOT NULL,
+	description TEXT NOT NULL,
+	name TEXT NOT NULL,
+	type INTEGER NOT NULL DEFAULT 0,
+	UNIQUE(identity_id),
+	UNIQUE(name),
+	FOREIGN KEY (identity_id) REFERENCES identities (id) ON DELETE CASCADE
+);
+
+CREATE TABLE cluster_links_config (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	cluster_link_id INTEGER NOT NULL,
+	key TEXT NOT NULL,
+	value TEXT NOT NULL,
+	UNIQUE (cluster_link_id, key),
+	FOREIGN KEY (cluster_link_id) REFERENCES cluster_links (id) ON DELETE CASCADE
+);
+`)
+
+	return err
 }
 
 func updateFromV79(ctx context.Context, tx *sql.Tx) error {
