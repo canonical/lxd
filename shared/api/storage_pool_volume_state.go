55fca9607bf73a4bfe709d8560fea9ad3a7b18cb
hamistao <pedro.ribeiro@canonical.com>
2025-01-28 17:17:29 -0300

    shared/api: Explicitly report null usage
    This omitempty is ineffective here since 0 is not a valid value for usage and any error in retrieving usage resulted, up until this point, in a null `StorageVolumeStateUsage` struct. So changing this doesn't break any previously existing behavior and this now matches how we represent unsupported DISK usage, by showing 0 usage.
    
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- shared/api/storage_pool_volume_state.go
+++ shared/api/storage_pool_volume_state.go
@@ -18,7 +18,7 @@ type StorageVolumeState struct {
 type StorageVolumeStateUsage struct {
 	// Used space in bytes. Uses 0 to indicate that the storage driver for the pool does not support retrieving volume usage.
 	// Example: 1693552640
-	Used uint64 `json:"used,omitempty" yaml:"used,omitempty"`
+	Used uint64 `json:"used" yaml:"used"`
 
 	// Storage volume size in bytes. Uses 0 to convey that the volume has access to the entire pool's storage.
 	// Example: 5189222192
