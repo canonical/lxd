5c0ed3452f5b304aea5bf8d2248c674e1a1a8c43
hamistao <pedro.ribeiro@canonical.com>
2025-01-28 00:01:35 -0300

    lxd: Use shutdown context on transaction
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- lxd/api_project.go
+++ lxd/api_project.go
@@ -936,7 +936,7 @@ func projectStateGet(d *Daemon, r *http.Request) response.Response {
 	state := api.ProjectState{}
 
 	// Get current limits and usage.
-	err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err = s.DB.Cluster.Transaction(s.ShutdownCtx, func(ctx context.Context, tx *db.ClusterTx) error {
 		result, err := limits.GetCurrentAllocations(ctx, s.GlobalConfig.Dump(), tx, name)
 		if err != nil {
 			return err
