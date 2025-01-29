46c8c6c4a90c9752f1b4e4b42b1c69d432b1d4d3
Kadin Sayani <kadin.sayani@canonical.com>
2025-01-29 14:20:17 -0700

    lxd/lifecycle: Add cluster link lifecycle events
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

---
+++ lxd/lifecycle/cluster_links.go
@@ -0,0 +1,28 @@
+package lifecycle
+
+import (
+	"github.com/canonical/lxd/shared/api"
+	"github.com/canonical/lxd/shared/version"
+)
+
+// ClusterLinkAction represents a lifecycle event action for a cluster link.
+type ClusterLinkAction string
+
+// All supported lifecycle events for cluster links.
+const (
+	ClusterLinkCreated = ClusterLinkAction(api.EventLifecycleClusterLinkCreated)
+	ClusterLinkDeleted = ClusterLinkAction(api.EventLifecycleClusterLinkDeleted)
+	ClusterLinkUpdated = ClusterLinkAction(api.EventLifecycleClusterLinkUpdated)
+)
+
+// Event creates the lifecycle event for an action on a cluster link.
+func (a ClusterLinkAction) Event(name string, requestor *api.EventLifecycleRequestor, ctx map[string]any) api.EventLifecycle {
+	u := api.NewURL().Path(version.APIVersion, "cluster", "links", name)
+
+	return api.EventLifecycle{
+		Action:    string(a),
+		Source:    u.String(),
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
