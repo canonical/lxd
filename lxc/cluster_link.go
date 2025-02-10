a5ff3199d7c2a8f3789e2fb7b076ddbfbcbb6496
Kadin Sayani <kadin.sayani@canonical.com>
2025-02-10 14:55:57 -0700

    lxc/cluster_link: Add `cluster link` and `cluster link add` commands
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

---
+++ lxc/cluster_link.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+
+	"github.com/canonical/lxd/shared/api"
+	cli "github.com/canonical/lxd/shared/cmd"
+	"github.com/canonical/lxd/shared/i18n"
+	"github.com/canonical/lxd/shared/version"
+)
+
+type cmdClusterLink struct {
+	global  *cmdGlobal
+	cluster *cmdCluster
+}
+
+func (c *cmdClusterLink) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("link")
+	cmd.Short = i18n.G("Manage cluster links")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Manage cluster links`))
+
+	// Add
+	clusterLinkAddCmd := cmdClusterLinkAdd{global: c.global, cluster: c.cluster}
+	cmd.AddCommand(clusterLinkAddCmd.command())
+
+	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
+	cmd.Args = cobra.NoArgs
+	cmd.Run = func(cmd *cobra.Command, args []string) { _ = cmd.Usage() }
+	return cmd
+}
+
+// Add.
+type cmdClusterLinkAdd struct {
+	global  *cmdGlobal
+	cluster *cmdCluster
+
+	flagToken       string
+	flagAuthGroups  []string
+	flagDescription string
+}
+
+func (c *cmdClusterLinkAdd) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("add", i18n.G("<name> [--token <trust_token>] [--auth-group <group1,group2,...>] [--description <description>]"))
+	cmd.Short = i18n.G("Add cluster links")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Add cluster links
+
+When run with a token, creates an active cluster link.
+When run without a token, creates a pending cluster link that must be activated by adding a cluster link on the remote cluster.`))
+	cmd.Example = cli.FormatSection("", i18n.G(`lxc cluster link add backup-cluster --auth-group backups
+	Create a pending cluster link reachable at "10.0.0.0:8443" and "10.0.0.1:8443" called "backup-cluster", belonging to the authentication group "backups".
+
+	lxc cluster link add main-cluster <token from backup-bluster> --auth-group backups
+	Create a cluster link with "backup-cluster" called "main-cluster", belonging to the auth group "backups".
+
+	lxc cluster link add recovery-cluster < config.yaml
+	Create a pending cluster link with the configuration from "config.yaml" called "recovery-cluster".`))
+	cmd.Flags().StringVarP(&c.flagToken, "token", "t", "", "Trust token to use when adding cluster link")
+	cmd.Flags().StringSliceVarP(&c.flagAuthGroups, "auth-group", "g", []string{}, "Authentication groups to add the newly created cluster link identity to")
+	cmd.Flags().StringVarP(&c.flagDescription, "description", "d", "", "Cluster link description")
+
+	cmd.RunE = c.run
+
+	return cmd
+}
+
+func (c *cmdClusterLinkAdd) run(cmd *cobra.Command, args []string) error {
+	// Quick checks
+	exit, err := c.global.CheckArgs(cmd, args, 1, 1)
+	if exit {
+		return err
+	}
+
+	// Parse remote
+	remoteName, clusterLinkName, err := c.global.conf.ParseRemote(args[0])
+	if err != nil {
+		return err
+	}
+
+	transporter, wrapper := newLocationHeaderTransportWrapper()
+	client, err := c.global.conf.GetInstanceServerWithTransportWrapper(remoteName, wrapper)
+	if err != nil {
+		return err
+	}
+
+	clusterLink := api.ClusterLinkPost{
+		Name: clusterLinkName,
+	}
+
+	if c.flagDescription != "" {
+		clusterLink.Description = c.flagDescription
+	}
+
+	// Set auth groups if provided
+	if len(c.flagAuthGroups) > 0 {
+		clusterLink.AuthGroups = c.flagAuthGroups
+	}
+
+	if c.flagToken == "" {
+		token, err := client.CreateIdentityClusterLinkToken(clusterLink)
+		if err != nil {
+			return err
+		}
+
+		// Encode certificate add token to JSON.
+		tokenJSON, err := json.Marshal(token)
+		if err != nil {
+			return fmt.Errorf("Failed to encode identity token: %w", err)
+		}
+
+		if !c.global.flagQuiet {
+			fmt.Printf(i18n.G("Cluster link %s created (pending)")+"\n", clusterLinkName)
+
+			pendingIdentityURL, err := url.Parse(transporter.location)
+			if err != nil {
+				return fmt.Errorf("Received invalid location header %q: %w", transporter.location, err)
+			}
+
+			var pendingIdentityUUIDStr string
+			identityURLPrefix := api.NewURL().Path(version.APIVersion, "auth", "identities", api.AuthenticationMethodTLS).String()
+			_, err = fmt.Sscanf(pendingIdentityURL.Path, identityURLPrefix+"/%s", &pendingIdentityUUIDStr)
+			if err != nil {
+				return fmt.Errorf("Received unexpected location header %q: %w", transporter.location, err)
+			}
+
+			pendingIdentityUUID, err := uuid.Parse(pendingIdentityUUIDStr)
+			if err != nil {
+				return fmt.Errorf("Received invalid pending identity UUID %q: %w", pendingIdentityUUIDStr, err)
+			}
+
+			fmt.Printf(i18n.G("Cluster link %q (%s) pending identity token:")+"\n", clusterLinkName, pendingIdentityUUID.String())
+		}
+
+		// Print the base64 encoded token.
+		fmt.Println(base64.StdEncoding.EncodeToString(tokenJSON))
+	} else {
+		clusterLink.TrustToken = c.flagToken
+		err = client.CreateClusterLink(clusterLink)
+		if err != nil {
+			return err
+		}
+
+		if !c.global.flagQuiet {
+			fmt.Printf(i18n.G("Cluster link %s added")+"\n", clusterLinkName)
+		}
+	}
+
+	return nil
+}
