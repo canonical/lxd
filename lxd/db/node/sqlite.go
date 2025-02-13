d91f32ca0e46d98d771fc24263a11df1468a88aa
Simon Deziel <simon.deziel@canonical.com>
2025-02-13 16:30:36 -0500

    lxd/db/node/sqlite: avoid fmt.Sprintf()
    Change the hardcoded timeout from int to string to avoid needing `fmt.Sprint()`
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/db/node/sqlite.go
+++ lxd/db/node/sqlite.go
@@ -2,7 +2,6 @@ package node
 
 import (
 	"database/sql"
-	"fmt"
 
 	"github.com/mattn/go-sqlite3"
 )
@@ -13,11 +12,11 @@ func init() {
 
 // Opens the node-level database with the correct parameters for LXD.
 func sqliteOpen(path string) (*sql.DB, error) {
-	timeout := 5 // TODO - make this command-line configurable?
+	// TODO - make the timeout command-line configurable?
 
 	// These are used to tune the transaction BEGIN behavior instead of using the
 	// similar "locking_mode" pragma (locking for the whole database connection).
-	openPath := fmt.Sprintf("%s?_busy_timeout=%d&_txlock=exclusive", path, timeout*1000)
+	openPath := path + "?_busy_timeout=5000&_txlock=exclusive"
 
 	// Open the database. If the file doesn't exist it is created.
 	return sql.Open("sqlite3_with_fk", openPath)
