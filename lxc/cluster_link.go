e966d49c29acf41dee610d9ce01efde230456077
Kadin Sayani <kadin.sayani@canonical.com>
2025-02-13 14:13:05 -0700

    lxc/cluster_link: Add `cluster link list` command
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxc/cluster_link.go
+++ lxc/cluster_link.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -26,6 +28,10 @@ func (c *cmdClusterLink) command() *cobra.Command {
 	// Add
 	clusterLinkAddCmd := cmdClusterLinkAdd{global: c.global, cluster: c.cluster}
 	cmd.AddCommand(clusterLinkAddCmd.command())
+
+	// List
+	clusterLinkListCmd := cmdClusterLinkList{global: c.global, cluster: c.cluster}
+	cmd.AddCommand(clusterLinkListCmd.command())
 
 	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 	cmd.Args = cobra.NoArgs
@@ -128,4 +134,77 @@ func (c *cmdClusterLinkAdd) run(cmd *cobra.Command, args []string) error {
 	}
 
 	return nil
+}
+
+// List.
+type cmdClusterLinkList struct {
+	global  *cmdGlobal
+	cluster *cmdCluster
+
+	flagFormat string
+}
+
+func (c *cmdClusterLinkList) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("list", i18n.G("[<remote>:]"))
+	cmd.Aliases = []string{"ls"}
+	cmd.Short = i18n.G("List cluster links")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`List cluster links`))
+
+	cmd.RunE = c.run
+	cmd.Flags().StringVarP(&c.flagFormat, "format", "f", "table", i18n.G("Format (csv|json|table|yaml|compact)")+"``")
+
+	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) != 0 {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		return c.global.cmpRemotes(toComplete, false)
+	}
+
+	return cmd
+}
+
+func (c *cmdClusterLinkList) run(cmd *cobra.Command, args []string) error {
+	// Quick checks
+	exit, err := c.global.CheckArgs(cmd, args, 0, 1)
+	if exit {
+		return err
+	}
+
+	// Parse remote
+	resources, err := c.global.ParseServers(args[0])
+	if err != nil {
+		return err
+	}
+
+	resource := resources[0]
+	client := resource.server
+
+	clusterLinks, err := client.GetClusterLinks()
+	if err != nil {
+		return err
+	}
+
+	data := [][]string{}
+	for _, link := range clusterLinks {
+		details := []string{
+			link.Name,
+			strings.Join(link.Addresses, ","),
+			link.Description,
+		}
+
+		data = append(data, details)
+	}
+
+	sort.Sort(cli.SortColumnsNaturally(data))
+
+	header := []string{
+		i18n.G("NAME"),
+		i18n.G("ADDRESSES"),
+		i18n.G("DESCRIPTION"),
+	}
+
+	return cli.RenderTable(c.flagFormat, header, data, clusterLinks)
 }
