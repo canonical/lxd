3e618667505155c05323698568da488a3ae1c61b
Kadin Sayani <kadin.sayani@canonical.com>
2025-02-14 14:05:15 -0700

    lxc/service: Add `service edit` command
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxc/service.go
+++ lxc/service.go
@@ -3,15 +3,20 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"net"
+	"os"
 	"sort"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 
+	"github.com/canonical/lxd/shared"
 	"github.com/canonical/lxd/shared/api"
 	cli "github.com/canonical/lxd/shared/cmd"
 	"github.com/canonical/lxd/shared/i18n"
+	"github.com/canonical/lxd/shared/termios"
 )
 
 type cmdService struct {
@@ -36,6 +41,10 @@ func (c *cmdService) command() *cobra.Command {
 	// Remove
 	serviceRemoveCmd := cmdServiceRemove{global: c.global, service: c}
 	cmd.AddCommand(serviceRemoveCmd.command())
+
+	// Edit
+	serviceEditCmd := cmdServiceEdit{global: c.global, service: c}
+	cmd.AddCommand(serviceEditCmd.command())
 
 	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 	cmd.Args = cobra.NoArgs
@@ -277,6 +286,133 @@ func (c *cmdServiceRemove) run(cmd *cobra.Command, args []string) error {
 	err = client.DeleteService(args[0])
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// Edit.
+type cmdServiceEdit struct {
+	global  *cmdGlobal
+	service *cmdService
+}
+
+func (c *cmdServiceEdit) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("edit", i18n.G("[<remote>:]<pool>"))
+	cmd.Short = i18n.G("Edit service configurations as YAML")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Edit service configurations as YAML`))
+	cmd.Example = cli.FormatSection("", i18n.G(
+		`lxc service edit [<remote>:]<service> < service.yaml
+    Update a service using the content of service.yaml.`))
+
+	cmd.RunE = c.run
+
+	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) == 0 {
+			return c.global.cmpServices(toComplete)
+		}
+
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	return cmd
+}
+
+func (c *cmdServiceEdit) helpTemplate() string {
+	return i18n.G(
+		`### This is a YAML representation of a service.
+### Any line starting with a '#' will be ignored.
+###
+### A service consists of a set of configuration items.
+###
+### An example would look like:
+### description: backup cluster
+### addresses: [10.0.0.1:8443, 10.0.0.2:8443]
+### config:
+###   `)
+}
+
+func (c *cmdServiceEdit) run(cmd *cobra.Command, args []string) error {
+	// Quick checks.
+	exit, err := c.global.CheckArgs(cmd, args, 1, 1)
+	if exit {
+		return err
+	}
+
+	// Parse remote.
+	resources, err := c.global.ParseServers(args[0])
+	if err != nil {
+		return err
+	}
+
+	resource := resources[0]
+
+	if resource.name == "" {
+		return errors.New(i18n.G("Missing service name"))
+	}
+
+	// If stdin isn't a terminal, read text from it
+	if !termios.IsTerminal(getStdinFd()) {
+		contents, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+
+		newdata := api.ServicePut{}
+		err = yaml.Unmarshal(contents, &newdata)
+		if err != nil {
+			return err
+		}
+
+		return resource.server.UpdateService(resource.name, newdata, "")
+	}
+
+	// Extract the current value
+	service, etag, err := resource.server.GetService(resource.name)
+	if err != nil {
+		return err
+	}
+
+	data, err := yaml.Marshal(&service)
+	if err != nil {
+		return err
+	}
+
+	// Spawn the editor
+	content, err := shared.TextEditor("", []byte(c.helpTemplate()+"\n\n"+string(data)))
+	if err != nil {
+		return err
+	}
+
+	for {
+		// Parse the text received from the editor
+		newdata := api.ServicePut{}
+		err = yaml.Unmarshal(content, &newdata)
+		if err == nil {
+			err = resource.server.UpdateService(resource.name, newdata, etag)
+		}
+
+		// Respawn the editor
+		if err != nil {
+			fmt.Fprintf(os.Stderr, i18n.G("Config parsing error: %s")+"\n", err)
+			fmt.Println(i18n.G("Press enter to open the editor again or ctrl+c to abort change"))
+
+			_, err := os.Stdin.Read(make([]byte, 1))
+			if err != nil {
+				return err
+			}
+
+			content, err = shared.TextEditor("", content)
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		break
 	}
 
 	return nil
