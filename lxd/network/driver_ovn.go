1a69bff9cc9d367ab057e6f967d64d4d4355df71
Thomas Parrott <thomas.parrott@canonical.com>
2025-02-14 08:00:40 +0000

    lxd/network/driver/ovn: Linter fixes
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>
    (cherry picked from commit 3d6b807ebb89c941733042b312a610e594ddddf2)

--- lxd/network/driver_ovn.go
+++ lxd/network/driver_ovn.go
@@ -1334,15 +1334,12 @@ func (n *ovn) allocateUplinkPortIPs(uplinkNet Network, routerMAC net.HardwareAdd
 }
 
 // uplinkAllAllocatedIPs gets a list of all IPv4 and IPv6 addresses allocated to OVN networks connected to uplink.
-func (n *ovn) uplinkAllAllocatedIPs(ctx context.Context, tx *db.ClusterTx, uplinkNetName string) ([]net.IP, []net.IP, error) {
+func (n *ovn) uplinkAllAllocatedIPs(ctx context.Context, tx *db.ClusterTx, uplinkNetName string) (v4IPs []net.IP, v6IPs []net.IP, err error) {
 	// Get all managed networks across all projects.
 	projectNetworks, err := tx.GetCreatedNetworks(ctx)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Failed to load all networks: %w", err)
 	}
-
-	v4IPs := make([]net.IP, 0)
-	v6IPs := make([]net.IP, 0)
 
 	for _, networks := range projectNetworks {
 		for _, netInfo := range networks {
@@ -3512,10 +3509,7 @@ func (n *ovn) getInstanceDevicePortName(instanceUUID string, deviceName string) 
 }
 
 // instanceDevicePortRoutesParse parses the instance NIC device config for internal routes and external routes.
-func (n *ovn) instanceDevicePortRoutesParse(deviceConfig map[string]string) ([]*net.IPNet, []*net.IPNet, error) {
-	var err error
-
-	internalRoutes := []*net.IPNet{}
+func (n *ovn) instanceDevicePortRoutesParse(deviceConfig map[string]string) (internalRoutes []*net.IPNet, externalRoutes []*net.IPNet, err error) {
 	for _, key := range []string{"ipv4.routes", "ipv6.routes"} {
 		if deviceConfig[key] == "" {
 			continue
@@ -3527,7 +3521,6 @@ func (n *ovn) instanceDevicePortRoutesParse(deviceConfig map[string]string) ([]*
 		}
 	}
 
-	externalRoutes := []*net.IPNet{}
 	for _, key := range []string{"ipv4.routes.external", "ipv6.routes.external"} {
 		if deviceConfig[key] == "" {
 			continue
