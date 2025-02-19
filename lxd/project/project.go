458220487d19e2d288eb998afaf1868f9b8aad2f
Simon Deziel <simon.deziel@canonical.com>
2025-02-19 11:09:51 -0500

    lxd/project: add ValidName() helper
    This is copied from `lxd/api_projects.go`'s `projectValidateName()`.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/project/project.go
+++ lxd/project/project.go
@@ -15,6 +15,39 @@ import (
 // separator is used to delimit the project name from the suffix.
 const separator = "_"
 
+// ValidName validates a project name.
+func ValidName(name string) error {
+	if name == "" {
+		return fmt.Errorf("No name provided")
+	}
+
+	if strings.Contains(name, "/") {
+		return fmt.Errorf("Project names may not contain slashes")
+	}
+
+	if strings.Contains(name, " ") {
+		return fmt.Errorf("Project names may not contain spaces")
+	}
+
+	if strings.Contains(name, "_") {
+		return fmt.Errorf("Project names may not contain underscores")
+	}
+
+	if strings.Contains(name, "'") || strings.Contains(name, `"`) {
+		return fmt.Errorf("Project names may not contain quotes")
+	}
+
+	if name == "*" {
+		return fmt.Errorf("Reserved project name")
+	}
+
+	if shared.ValueInSlice(name, []string{".", ".."}) {
+		return fmt.Errorf("Invalid project name %q", name)
+	}
+
+	return nil
+}
+
 // Instance adds the "<project>_" prefix to instance name when the given project name is not "default".
 func Instance(projectName string, instanceName string) string {
 	if projectName != api.ProjectDefaultName {
