73b76cda2e6b612d2ec10787fe0dd9e2c89e4198
Simon Deziel <simon.deziel@canonical.com>
2025-02-20 10:00:48 -0500

    lxd/instance/instancetype/instance: add ValidSnapName() helper
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit ff61c594d8fe43825490b57ae7761117f8d1d003)
    Signed-off-by: Din Music <din.music@canonical.com>
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

---
+++ lxd/instance/instancetype/instance.go
@@ -0,0 +1,19 @@
+package instancetype
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ValidSnapName validates a snapshot instance name which must not include the instance prefix.
+func ValidSnapName(snapshotName string) error {
+	if snapshotName == ".." {
+		return fmt.Errorf("Invalid instance snapshot name %q", snapshotName)
+	}
+
+	if strings.ContainsAny(snapshotName, " /\\") {
+		return fmt.Errorf("Invalid instance snapshot name %q: Cannot contain spaces, forward or back slashes", snapshotName)
+	}
+
+	return nil
+}
