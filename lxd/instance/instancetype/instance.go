df421e75e35e74b2caf61a36bf90952b40ab0ff0
Simon Deziel <simon.deziel@canonical.com>
2025-02-20 10:01:59 -0500

    lxd/instance/instancetype/instance: reject empty snapshot name
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 7846146448740059c04e7991d6a366886a9bce25)
    Signed-off-by: Din Music <din.music@canonical.com>
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/instance/instancetype/instance.go
+++ lxd/instance/instancetype/instance.go
@@ -7,6 +7,10 @@ import (
 
 // ValidSnapName validates a snapshot instance name which must not include the instance prefix.
 func ValidSnapName(snapshotName string) error {
+	if snapshotName == "" {
+		return fmt.Errorf("Invalid instance snapshot name, cannot be empty")
+	}
+
 	if snapshotName == ".." {
 		return fmt.Errorf("Invalid instance snapshot name %q", snapshotName)
 	}
