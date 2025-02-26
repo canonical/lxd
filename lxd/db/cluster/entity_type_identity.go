b7eb18a5794d8ee46fe9dd4cf4909166f1488ae9
Mark Laing <mark.laing@canonical.com>
2025-02-26 16:55:39 +0000

    lxd/db/cluster: Delete certificate permissions on identity deletion.
    Similar to #15066, the splitting of certificate and identity entity
    type definitions complicates the deletion trigger for the identities
    table. We need to delete permissions for both certificate and identity
    types when an identity is deleted.
    
    Note that since certificates and fine-grained identities are stored in
    the same table, it is not possible for this deletion trigger to reference
    an incorrect permission or warning.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/entity_type_identity.go
+++ lxd/db/cluster/entity_type_identity.go
@@ -64,17 +64,18 @@ WHERE '' = ?
 }
 
 func (e entityTypeIdentity) onDeleteTriggerSQL() (name string, sql string) {
+	typeCertificate := entityTypeCertificate{}
 	name = "on_identity_delete"
 	return name, fmt.Sprintf(`
 CREATE TRIGGER %s
 	AFTER DELETE ON identities
 	BEGIN
 	DELETE FROM auth_groups_permissions 
-		WHERE entity_type = %d 
+		WHERE entity_type IN (%d, %d) 
 		AND entity_id = OLD.id;
 	DELETE FROM warnings
-		WHERE entity_type_code = %d
+		WHERE entity_type_code IN (%d, %d)
 		AND entity_id = OLD.id;
 	END
-`, name, e.code(), e.code())
+`, name, e.code(), typeCertificate.code(), e.code(), typeCertificate.code())
 }
