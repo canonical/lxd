72e35c5040bdc1062ad4dfb0761700227097b3b8
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-02-27 10:29:05 +0100

    lxd/backup/config: Add new keys and mark outdated ones as deprecated
    Also move all the deprecated keys to the bottom of the struct for better readability.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/backup/config/backup_config.go
+++ lxd/backup/config/backup_config.go
@@ -11,13 +11,29 @@ const DefaultMetadataVersion = api.BackupMetadataVersion1
 // MaxMetadataVersion represents the latest supported metadata version.
 const MaxMetadataVersion = api.BackupMetadataVersion2
 
+// Volume represents the config of a volume including its snapshots.
+type Volume struct {
+	// Make sure to have the embedded structs fields inline to avoid nesting.
+	api.StorageVolume `yaml:",inline"`
+
+	Snapshots []*api.StorageVolumeSnapshot `yaml:"snapshots,omitempty"`
+}
+
 // Config represents the config of a backup that can be stored in a backup.yaml file (or embedded in index.yaml).
 type Config struct {
-	Container       *api.Instance                `yaml:"container,omitempty"` // Used by VM backups too.
-	Snapshots       []*api.InstanceSnapshot      `yaml:"snapshots,omitempty"`
-	Pool            *api.StoragePool             `yaml:"pool,omitempty"`
-	Profiles        []*api.Profile               `yaml:"profiles,omitempty"`
-	Volume          *api.StorageVolume           `yaml:"volume,omitempty"`
+	Version   uint32                  `yaml:"version,omitempty"`
+	Instance  *api.Instance           `yaml:"instance,omitempty"`
+	Snapshots []*api.InstanceSnapshot `yaml:"snapshots,omitempty"`
+	Pools     []*api.StoragePool      `yaml:"pools,omitempty"`
+	Profiles  []*api.Profile          `yaml:"profiles,omitempty"`
+	Volumes   []*Volume               `yaml:"volumes,omitempty"`
+	Bucket    *api.StorageBucket      `yaml:"bucket,omitempty"`
+	// Deprecated: Use Instance instead.
+	Container *api.Instance `yaml:"container,omitempty"`
+	// Deprecated: Use Pools instead.
+	Pool *api.StoragePool `yaml:"pool,omitempty"`
+	// Deprecated: Use Volumes instead.
+	Volume *api.StorageVolume `yaml:"volume,omitempty"`
+	// Deprecated: Use the list of Snapshots under Volumes.
 	VolumeSnapshots []*api.StorageVolumeSnapshot `yaml:"volume_snapshots,omitempty"`
-	Bucket          *api.StorageBucket           `yaml:"bucket,omitempty"`
 }
