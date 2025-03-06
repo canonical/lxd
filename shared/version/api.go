ecfd721247d99fc4597d91c04f40b387caf60469
Mark Laing <mark.laing@canonical.com>
2025-03-06 10:41:38 +0000

    doc: Adds `clustering_groups_used_by` API extension.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    
    # Conflicts:
    #	doc/api-extensions.md
    #	shared/version/api.go

--- shared/version/api.go
+++ shared/version/api.go
@@ -441,6 +441,7 @@ var APIExtensions = []string{
 	"oidc_scopes",
 	"project_default_network_and_storage",
 	"client_cert_presence",
+	"clustering_groups_used_by",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
