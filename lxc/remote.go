d1eba909d019191ea8158bc5bc9d6af051c7b718
Thomas Parrott <thomas.parrott@canonical.com>
2025-03-07 14:15:44 +0000

    lxc/remote: Ignore deprecated linter warning for Password field
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxc/remote.go
+++ lxc/remote.go
@@ -271,7 +271,7 @@ func (c *cmdRemoteAdd) addRemoteFromToken(addr string, server string, token stri
 		if d.HasExtension("explicit_trust_token") {
 			req.TrustToken = token
 		} else {
-			req.Password = token
+			req.Password = token //nolint:staticcheck
 		}
 
 		err = d.CreateCertificate(req)
