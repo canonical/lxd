7b4013454bc321b99028cda7c20db24d5921907e
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-03-07 10:05:14 +0100

    lxd/backup/config: Add new utility functions to retrieve pools and vols
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/backup/config/backup_config.go
+++ lxd/backup/config/backup_config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/canonical/lxd/shared/api"
 )
 
@@ -32,4 +34,41 @@ type Config struct {
 	// Use the list of Snapshots under Volumes.
 	VolumeSnapshots []*api.StorageVolumeSnapshot `yaml:"volume_snapshots,omitempty"`
 	Bucket          *api.StorageBucket           `yaml:"bucket,omitempty"`
+}
+
+// RootVolumePool returns the pool of the root volume.
+// This is the first pool in the list of pools.
+func (c *Config) RootVolumePool() (*api.StoragePool, error) {
+	if len(c.Pools) == 0 {
+		return nil, fmt.Errorf("No pools are defined in backup config")
+	}
+
+	if c.Pools[0] == nil {
+		return nil, fmt.Errorf("Pool config is empty")
+	}
+
+	return c.Pools[0], nil
+}
+
+// UpdateRootVolumePool updates the root volume's storage pool.
+func (c *Config) UpdateRootVolumePool(pool *api.StoragePool) {
+	if c.Pools == nil {
+		c.Pools = make([]*api.StoragePool, 0, 1)
+	}
+
+	c.Pools[0] = pool
+}
+
+// RootVolume returns the root volume.
+// This is the first volume in the list of volumes.
+func (c *Config) RootVolume() (*VolumeConfig, error) {
+	if len(c.Volumes) == 0 {
+		return nil, fmt.Errorf("No volumes are defined in backup config")
+	}
+
+	if c.Volumes[0] == nil {
+		return nil, fmt.Errorf("Volume config is empty")
+	}
+
+	return c.Volumes[0], nil
 }
