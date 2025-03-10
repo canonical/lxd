8ff30a7173d58730271d240d97ed8ca6f15938d8
Thomas Parrott <thomas.parrott@canonical.com>
2025-03-10 09:03:05 +0000

    lxd/storage/drivers/driver/common: RunCommandContext linter
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/storage/drivers/driver_common.go
+++ lxd/storage/drivers/driver_common.go
@@ -1,6 +1,7 @@
 package drivers
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/url"
@@ -291,7 +292,7 @@ func (d *common) moveGPTAltHeader(devPath string) error {
 		return nil
 	}
 
-	_, err = shared.RunCommand(path, "--move-second-header", devPath)
+	_, err = shared.RunCommandContext(context.TODO(), path, "--move-second-header", devPath)
 	if err == nil {
 		d.logger.Debug("Moved GPT alternative header to end of disk", logger.Ctx{"dev": devPath})
 		return nil
@@ -584,7 +585,7 @@ func (d *common) filesystemFreeze(path string) (func() error, error) {
 		return nil, fmt.Errorf("Failed syncing filesystem %q: %w", path, err)
 	}
 
-	_, err = shared.RunCommand("fsfreeze", "--freeze", path)
+	_, err = shared.RunCommandContext(context.TODO(), "fsfreeze", "--freeze", path)
 	if err != nil {
 		return nil, fmt.Errorf("Failed freezing filesystem %q: %w", path, err)
 	}
@@ -592,7 +593,7 @@ func (d *common) filesystemFreeze(path string) (func() error, error) {
 	d.logger.Info("Filesystem frozen", logger.Ctx{"path": path})
 
 	unfreezeFS := func() error {
-		_, err := shared.RunCommand("fsfreeze", "--unfreeze", path)
+		_, err := shared.RunCommandContext(context.TODO(), "fsfreeze", "--unfreeze", path)
 		if err != nil {
 			return fmt.Errorf("Failed unfreezing filesystem %q: %w", path, err)
 		}
