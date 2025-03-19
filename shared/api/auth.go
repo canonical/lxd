7771228ef3caea483e148666da45b74da54003b5
Kadin Sayani <kadin.sayani@canonical.com>
2025-03-19 13:13:15 -0600

    shared/api: Add `IdentityTypeClusterLink` and `IdentityTypeClusterLinkPending` API types
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- shared/api/auth.go
+++ shared/api/auth.go
@@ -53,6 +53,12 @@ const (
 
 	// IdentityTypeBearerTokenInitialUI is the identity type used for initial connection to LXD via the UI when conventional authentication is not yet configured.
 	IdentityTypeBearerTokenInitialUI = "Initial UI token bearer"
+
+	// IdentityTypeCertificateClusterLink represents cluster links that authenticate using TLS and whose permissions are managed via group ownership.
+	IdentityTypeCertificateClusterLink = "Cluster link certificate"
+
+	// IdentityTypeCertificateClusterLinkPending represents cluster links for which a token has been issued but who have not yet authenticated with a linked LXD cluster.
+	IdentityTypeCertificateClusterLinkPending = "Cluster link certificate (pending)"
 )
 
 // WithEntitlements is meant to be an embedded struct to API types eligible for entitlement enrichment,
