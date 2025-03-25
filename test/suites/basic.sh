lxc/incus

test: Fix deprecated chown syntax
Signed-off-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

test/suites/basic.sh
@@ -440,7 +440,7 @@ test_basic_usage() {
 
   # test incus file edit doesn't change target file's owner and permissions
   echo "content" | incus file push - foo/tmp/edit_test
-  incus exec foo -- chown 55.55 /tmp/edit_test
+  incus exec foo -- chown 55:55 /tmp/edit_test
   incus exec foo -- chmod 555 /tmp/edit_test
   echo "new content" | incus file edit foo/tmp/edit_test
   [ "$(incus exec foo -- cat /tmp/edit_test)" = "new content" ]

canonical/lxd

(cherry picked from commit c339bbd9aabf650e5dec495c78ccc6bd24d41ff2)
Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
License: Apache-2.0

test/suites/basic.sh
@@ -462,7 +462,7 @@ test_basic_usage() {
 
   # test lxc file edit doesn't change target file's owner and permissions
   echo "content" | lxc file push - foo/tmp/edit_test
-  lxc exec foo -- chown 55.55 /tmp/edit_test
+  lxc exec foo -- chown 55:55 /tmp/edit_test
   lxc exec foo -- chmod 555 /tmp/edit_test
   echo "new content" | lxc file edit foo/tmp/edit_test
   [ "$(lxc exec foo -- cat /tmp/edit_test)" = "new content" ]