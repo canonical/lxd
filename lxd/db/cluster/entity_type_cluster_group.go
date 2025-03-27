e2da867a45731697c4d05609ec1b0f2b7fb42d33
Mark Laing <mark.laing@canonical.com>
2025-03-27 05:57:49 +0000

    lxd/db/cluster: Implement `runSelector` for `cluster_group`.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/entity_type_cluster_group.go
+++ lxd/db/cluster/entity_type_cluster_group.go
@@ -1,7 +1,12 @@
 package cluster
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+
+	"github.com/canonical/lxd/lxd/db/query"
+	"github.com/canonical/lxd/shared/entity"
 )
 
 // entityTypeClusterGroup implements entityTypeDBInfo for a ClusterGroup.
@@ -44,4 +49,44 @@ CREATE TRIGGER %s
 		AND entity_id = OLD.id;
 	END
 `, name, e.code(), e.code())
+}
+
+// runSelector for entityTypeClusterGroup only accepts the "name" matcher key.
+func (e entityTypeClusterGroup) runSelector(ctx context.Context, tx *sql.Tx, selector Selector) ([]int, error) {
+	q, args, err := e.selectorQuery(selector)
+	if err != nil {
+		return nil, err
+	}
+
+	return query.SelectIntegers(ctx, tx, q, args...)
+}
+
+func (e entityTypeClusterGroup) selectorQuery(selector Selector) (string, []any, error) {
+	if entity.Type(selector.EntityType) != entity.TypeClusterGroup {
+		return "", nil, fmt.Errorf("Invalid selector entity type %q (expected %q)", selector.EntityType, entity.TypeClusterGroup)
+	}
+
+	if len(selector.Matchers) == 0 {
+		return "", nil, fmt.Errorf("Selector for entity type %q has no matchers", entity.TypeClusterGroup)
+	}
+
+	if len(selector.Matchers) > 1 {
+		return "", nil, fmt.Errorf("Selectors for entity type %q may only have one matcher", entity.TypeClusterGroup)
+	}
+
+	if selector.Matchers[0].Property != "name" {
+		return "", nil, fmt.Errorf("Selectors for entity type %q may have only one matcher with property %q", entity.TypeClusterGroup, "name")
+	}
+
+	if len(selector.Matchers[0].Values) == 0 {
+		return "", nil, fmt.Errorf("Selector matcher for entity type %q requires at least one value", entity.TypeClusterGroup)
+	}
+
+	q := `SELECT id FROM cluster_groups WHERE name IN ` + query.Params(len(selector.Matchers[0].Values))
+	args := make([]any, 0, len(selector.Matchers[0].Values))
+	for _, v := range selector.Matchers[0].Values {
+		args = append(args, v)
+	}
+
+	return q, args, nil
 }
