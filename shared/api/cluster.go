c6ba470e03bf7d9a1e3fcd8253b5e8d3bdb3f3a5
Gabriel Mougard <gabriel.mougard@canonical.com>
2025-03-27 14:33:50 +0100

    shared/api: Add boolean `MemberOnly` field to `ClusterMemberStatePost`
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- shared/api/cluster.go
+++ shared/api/cluster.go
@@ -289,6 +289,11 @@ type ClusterMemberStatePost struct {
 	//
 	// API extension: clustering_evacuate_mode
 	Mode string `json:"mode" yaml:"mode"`
+
+	// Restore only the member status without affecting instances.
+	//
+	// API extension: clustering_restore_member_only
+	MemberOnly bool `json:"member_only" yaml:"member_only"`
 }
 
 // ClusterGroupsPost represents the fields available for a new cluster group.
