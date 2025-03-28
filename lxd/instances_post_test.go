30c9041c0941eafe15d144a6f7e8cf4b455118cd
Mark Laing <mark.laing@canonical.com>
2025-03-28 15:45:24 +0000

    lxd: Add tests for instance placement rule function.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/instances_post_test.go
+++ lxd/instances_post_test.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/canonical/lxd/lxd/db"
 	dbCluster "github.com/canonical/lxd/lxd/db/cluster"
+	"github.com/canonical/lxd/lxd/instance/instancetype"
+	"github.com/canonical/lxd/shared"
 	"github.com/canonical/lxd/shared/api"
 	"github.com/canonical/lxd/shared/entity"
 )
@@ -189,6 +194,186 @@ func (s *placementSuite) Test_getSortedPlacementRules() {
 		s.Require().Len(got, len(tt.want))
 		for i := range tt.want {
 			s.Require().Equal(tt.want[i], got[i])
+		}
+	}
+}
+
+func (s *placementSuite) Test_applyPlacementRules() {
+	cluster, cleanup := db.NewTestCluster(s.T())
+	defer cleanup()
+
+	nodeNames := []string{"member01", "member02", "member03", "member04", "member05"}
+	candidates := make([]db.NodeInfo, 0, len(nodeNames))
+	for i, nodeName := range nodeNames {
+		candidates = append(candidates, db.NodeInfo{Name: nodeName, Address: fmt.Sprintf("192.0.2.%d", i)})
+	}
+
+	candidatesWithout := func(members ...string) []db.NodeInfo {
+		filteredCandidates := make([]db.NodeInfo, 0, len(candidates))
+		for _, candidate := range candidates {
+			if !shared.ValueInSlice(candidate.Name, members) {
+				filteredCandidates = append(filteredCandidates, candidate)
+			}
+		}
+
+		return filteredCandidates
+	}
+
+	err := cluster.Transaction(context.Background(), func(_ context.Context, tx *db.ClusterTx) error {
+		for i, node := range candidates {
+			id, err := tx.CreateNode(node.Name, node.Address)
+			candidates[i].ID = id
+			s.Require().NoError(err)
+		}
+
+		return nil
+	})
+	s.Require().NoError(err)
+
+	type args struct {
+		candidates             []db.NodeInfo
+		project                string
+		expandedConfig         map[string]string
+		expandedPlacementRules map[string]api.InstancePlacementRule
+	}
+
+	tests := []struct {
+		name         string
+		args         args
+		caseSetup    func()
+		caseTearDown func()
+		want         []db.NodeInfo
+		wantErr      error
+	}{
+		{
+			name: "cluster group affinity",
+			caseSetup: func() {
+				_ = cluster.Transaction(context.Background(), func(ctx context.Context, tx *db.ClusterTx) error {
+					_, err := dbCluster.CreateClusterGroup(ctx, tx.Tx(), dbCluster.ClusterGroup{Name: "g1"})
+					s.Require().NoError(err)
+
+					for _, node := range candidatesWithout("member01", "member02") {
+						err = tx.AddNodeToClusterGroup(ctx, "g1", node.Name)
+						s.Require().NoError(err)
+					}
+
+					return nil
+				})
+			},
+			args: args{
+				candidates:     candidates,
+				project:        "default",
+				expandedConfig: map[string]string{},
+				expandedPlacementRules: map[string]api.InstancePlacementRule{
+					"cluster_group_affinity": {
+						Required: true,
+						Kind:     api.InstancePlacementRuleKindAffinity,
+						Priority: 0,
+						Selector: api.Selector{
+							EntityType: "cluster_group",
+							Matchers: []api.SelectorMatcher{
+								{
+									Property: "name",
+									Values:   []string{"g1"},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: candidatesWithout("member01", "member02"),
+		},
+		{
+			name: "instance anti-affinity (first)",
+			args: args{
+				candidates: candidates,
+				project:    "default",
+				expandedConfig: map[string]string{
+					"user.deployment": "ha",
+				},
+				expandedPlacementRules: map[string]api.InstancePlacementRule{
+					"instance_anti_affinity": {
+						Required: true,
+						Kind:     api.InstancePlacementRuleKindAntiAffinity,
+						Priority: 0,
+						Selector: api.Selector{
+							EntityType: "instance",
+							Matchers: []api.SelectorMatcher{
+								{
+									Property: "config.user.deployment",
+									Values:   []string{"ha"},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: candidates,
+		},
+		{
+			name: "instance anti-affinity (second)",
+			caseSetup: func() {
+				_ = cluster.Transaction(context.Background(), func(ctx context.Context, tx *db.ClusterTx) error {
+					instanceID, err := dbCluster.CreateInstance(ctx, tx.Tx(), dbCluster.Instance{
+						Name:    "c1",
+						Node:    "member01",
+						Project: "default",
+						Type:    instancetype.Container,
+					})
+					s.Require().NoError(err)
+					err = dbCluster.CreateInstanceConfig(ctx, tx.Tx(), instanceID, map[string]string{
+						"user.deployment": "ha",
+					})
+					s.Require().NoError(err)
+					return nil
+				})
+			},
+			args: args{
+				candidates: candidates,
+				project:    "default",
+				expandedConfig: map[string]string{
+					"user.deployment": "ha",
+				},
+				expandedPlacementRules: map[string]api.InstancePlacementRule{
+					"instance_anti_affinity": {
+						Required: true,
+						Kind:     api.InstancePlacementRuleKindAntiAffinity,
+						Priority: 0,
+						Selector: api.Selector{
+							EntityType: "instance",
+							Matchers: []api.SelectorMatcher{
+								{
+									Property: "config.user.deployment",
+									Values:   []string{"ha"},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: candidatesWithout("member01"),
+		},
+	}
+
+	for _, tt := range tests {
+		if tt.caseSetup != nil {
+			tt.caseSetup()
+		}
+
+		_ = cluster.Transaction(context.Background(), func(ctx context.Context, tx *db.ClusterTx) error {
+			got, err := applyPlacementRules(ctx, tx.Tx(), tt.args.candidates, tt.args.project, tt.args.expandedConfig, tt.args.expandedPlacementRules)
+			if tt.wantErr != nil {
+				s.Equal(tt.wantErr, err)
+				return nil
+			}
+
+			s.Require().NoError(err)
+			s.ElementsMatch(tt.want, got)
+			return nil
+		})
+
+		if tt.caseTearDown != nil {
+			tt.caseTearDown()
 		}
 	}
 }
