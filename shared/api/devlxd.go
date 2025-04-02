746d1a28e47189d8f9bc90b88cbfc02dc7c942dd
Din Music <din.music@canonical.com>
2025-04-02 17:29:44 +0000

    shared/api: Add public DevLXD response
    Signed-off-by: Din Music <din.music@canonical.com>

--- shared/api/devlxd.go
+++ shared/api/devlxd.go
@@ -1,5 +1,20 @@
 package api
 
+import (
+	"encoding/json"
+)
+
+// DevLXDResponse represents the response from the devLXD API.
+type DevLXDResponse struct {
+	Content    []byte `json:"content" yaml:"content"`
+	StatusCode int    `json:"status_code" yaml:"status_code"`
+}
+
+// ContentAsStruct unmarshals the response content.
+func (r DevLXDResponse) ContentAsStruct(target any) error {
+	return json.Unmarshal(r.Content, &target)
+}
+
 // DevLXDPut represents the modifiable data.
 type DevLXDPut struct {
 	// Instance state
