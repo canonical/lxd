565c1b6be8777a5a28f91f05745f2f7168509bbd
Kadin Sayani <kadin.sayani@canonical.com>
2025-04-02 14:33:58 -0600

    lxd: Add `isDelegator` to determine if API request is from `UserAgentDelegator`
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/api.go
+++ lxd/api.go
@@ -480,6 +480,11 @@ func isClusterNotification(r *http.Request) bool {
 	return r.Header.Get("User-Agent") == request.UserAgentNotifier
 }
 
+// isDelegator returns true if this is an API request coming from UserAgentDelegator.
+func isDelegator(r *http.Request) bool {
+	return r.Header.Get("User-Agent") == request.UserAgentDelegator
+}
+
 type uiHTTPDir struct {
 	http.FileSystem
 }
