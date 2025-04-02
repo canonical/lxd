a6edd7b0dd3e9e0f8ca9140c0f211bf6f6310fb1
Simon Deziel <simon.deziel@canonical.com>
2025-04-02 12:10:13 -0400

    test/lint/godeps: allow updating lists
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/lint/godeps.sh
+++ test/lint/godeps.sh
@@ -2,6 +2,8 @@
 set -eu
 set -o pipefail
 
+UPDATE_LISTS="${UPDATE_LISTS:-"false"}"
+
 # Ensure predictable sorting
 export LC_ALL=C.UTF-8
 
@@ -15,8 +17,21 @@ for pkg in client lxc/config lxd-agent shared/api; do
   TAGS=""
   [ "${pkg}" = "lxd-agent" ] && TAGS="-tags agent,netgo"
 
-  OUT="$(go list ${TAGS:+${TAGS}} -f '{{ join .Deps "\n" }}' ./${pkg} | grep -F . | sort -u | diff --new-file -u "${DEP_FILE}" - || true)"
+  CURRENT_DEPS="$(go list ${TAGS:+${TAGS}} -f '{{ join .Deps "\n" }}' ./${pkg} | grep -F . | sort -u)"
+  OUT="$(diff --new-file -u "${DEP_FILE}" - <<< "${CURRENT_DEPS}" || true)"
   if [ -n "${OUT}" ]; then
+    if [ "${UPDATE_LISTS:-"false"}" = "true" ]; then
+      echo
+      echo "Changed dependencies for ${pkg}:"
+      echo "${OUT}"
+      echo
+      read -rp "Would you like to update ${DEP_FILE} with the new dependency list (y/N)? " answer
+      if [ "${answer:-n}" = "y" ]; then
+        echo "${CURRENT_DEPS}" > "${DEP_FILE}"
+        continue
+      fi
+    fi
+
     echo "ERROR: you added a new dependency to ${pkg}; please make sure this is what you want"
     echo "${OUT}"
     rc=1
