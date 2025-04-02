8cbf4e8b2957d670a8cface53191ee5dda2d31ed
Mark Laing <mark.laing@canonical.com>
2025-04-02 16:40:17 +0100

    lxc: Update calls to GetInstanceServerWithTransportWrapper.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit d555201e530b1a72421e35bcd853ea00dc914784)

--- lxc/network_load_balancer.go
+++ lxc/network_load_balancer.go
@@ -13,6 +13,7 @@ import (
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 
+	"github.com/canonical/lxd/client"
 	"github.com/canonical/lxd/shared"
 	"github.com/canonical/lxd/shared/api"
 	cli "github.com/canonical/lxd/shared/cmd"
@@ -287,7 +288,7 @@ func (c *cmdNetworkLoadBalancerCreate) run(cmd *cobra.Command, args []string) er
 	}
 
 	transporter, wrapper := newLocationHeaderTransportWrapper()
-	client, err := c.global.conf.GetInstanceServerWithTransportWrapper(remoteName, wrapper)
+	client, err := c.global.conf.GetInstanceServerWithConnectionArgs(remoteName, &lxd.ConnectionArgs{TransportWrapper: wrapper})
 	if err != nil {
 		return err
 	}
