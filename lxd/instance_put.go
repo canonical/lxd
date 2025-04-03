ad9c0b3b8136994e54279cd80d1ef75fa4f16a32
hamistao <pedro.ribeiro@canonical.com>
2025-04-03 18:06:04 -0300

    lxd/instance_put: Handle `DisksMode` and `ExcludeDisks` fields
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- lxd/instance_put.go
+++ lxd/instance_put.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -131,6 +132,15 @@ func instancePut(d *Daemon, r *http.Request) response.Response {
 	var do func(*operations.Operation) error
 	var opType operationtype.Type
 	if configRaw.Restore == "" {
+		if configRaw.DisksMode != "" {
+			return response.BadRequest(errors.New(`No snapshot provided on "restore" and "disks" was provided`))
+		}
+
+		// "excluded_disks" do not apply if not preforming a restore.
+		if len(configRaw.ExcludeDisks) > 0 {
+			return response.BadRequest(errors.New(`No snapshot provided on "restore" and "excluded_disks" was provided`))
+		}
+
 		// Check project limits.
 		apiProfiles := make([]api.Profile, 0, len(configRaw.Profiles))
 		err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
@@ -192,7 +202,7 @@ func instancePut(d *Daemon, r *http.Request) response.Response {
 		do = func(_ *operations.Operation) error {
 			defer unlock()
 
-			return instanceSnapRestore(s, projectName, name, configRaw.Restore, configRaw.Stateful)
+			return instanceSnapRestore(s, projectName, name, configRaw)
 		}
 
 		opType = operationtype.SnapshotRestore
@@ -214,8 +224,9 @@ func instancePut(d *Daemon, r *http.Request) response.Response {
 	return operations.OperationResponse(op)
 }
 
-func instanceSnapRestore(s *state.State, projectName string, name string, snap string, stateful bool) error {
+func instanceSnapRestore(s *state.State, projectName string, name string, req api.InstancePut) error {
 	// normalize snapshot name
+	snap := req.Restore
 	if !shared.IsSnapshot(snap) {
 		snap = name + shared.SnapshotDelimiter + snap
 	}
@@ -235,10 +246,16 @@ func instanceSnapRestore(s *state.State, projectName string, name string, snap s
 		}
 	}
 
+	// Get which disks should be included apart from the root disk.
+	disks, err := instance.ParseSnapshotDisks(source, req.DisksMode, req.ExcludeDisks)
+	if err != nil {
+		return api.NewStatusError(http.StatusBadRequest, err.Error())
+	}
+
 	// Generate a new `volatile.uuid.generation` to differentiate this instance restored from a snapshot from the original instance.
 	source.LocalConfig()["volatile.uuid.generation"] = uuid.New().String()
 
-	err = inst.Restore(source, stateful)
+	err = inst.Restore(source, req.Stateful, disks)
 	if err != nil {
 		return err
 	}
