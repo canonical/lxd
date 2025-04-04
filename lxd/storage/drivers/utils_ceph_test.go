231e9db361a7474c72edcb3aa8acab2fabd052fa
Simon Deziel <simon.deziel@canonical.com>
2025-04-04 16:46:20 -0400

    lxd/storage/drivers/utils_ceph_test: add tests for different messenger versions
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/storage/drivers/utils_ceph_test.go
+++ lxd/storage/drivers/utils_ceph_test.go
@@ -26,6 +26,26 @@ func Test_parseCephMonHost(t *testing.T) {
 			line: "mon host = 192.0.2.1, 192.0.2.2, 192.0.2.3",
 			want: []string{"192.0.2.1:6789", "192.0.2.2:6789", "192.0.2.3:6789"},
 		},
+		{
+			name: "mon_host with IPs and some ports",
+			line: "mon_host=192.0.2.1:6789,192.0.2.2:3300,192.0.2.3",
+			want: []string{"192.0.2.1:6789", "192.0.2.2:3300", "192.0.2.3:6789"},
+		},
+		{
+			name: "mon host with DNS names and some ports",
+			line: "mon host = foo.example.com:3300,bar.example.com:6789,baz.example.com",
+			want: []string{"foo.example.com:3300", "bar.example.com:6789", "baz.example.com:6789"},
+		},
+		{
+			name: "mon host with messenger versions",
+			line: "mon host = v1:192.0.2.1:6789,[v1:192.0.2.2],v2:192.0.2.3,[v2:192.0.2.4]",
+			want: []string{"192.0.2.1:6789", "192.0.2.2:6789", "192.0.2.3:3300", "192.0.2.4:3300"},
+		},
+		{
+			name: "mon host with some messenger versions",
+			line: "mon host = v1:192.0.2.1:6789,v2:192.0.2.2,192.0.2.3",
+			want: []string{"192.0.2.1:6789", "192.0.2.2:3300", "192.0.2.3:6789"},
+		},
 	}
 
 	for _, tt := range tests {
