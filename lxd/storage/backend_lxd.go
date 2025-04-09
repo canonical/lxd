2379bc277fc3fbcfb7bfd3f2acd51a5e47a0a909
hamistao <pedro.ribeiro@canonical.com>
2025-04-09 18:51:02 -0300

    lxd/storage: Fix instance path on `SyncFS`
    `RootfsPath` returns the path of the `rootfs` file, here we want the
    instnace directory, as it is the mountpoint of the filesystem.
    
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>
    (cherry picked from commit dac1e78f3f532a1486da41f4909cbb1c602875fc)

--- lxd/storage/backend_lxd.go
+++ lxd/storage/backend_lxd.go
@@ -1106,7 +1106,7 @@ func (b *lxdBackend) CreateInstanceFromCopy(inst instance.Instance, src instance
 		defer func() { _ = src.Unfreeze() }()
 
 		// Attempt to sync the filesystem.
-		err = filesystem.SyncFS(src.RootfsPath())
+		err = filesystem.SyncFS(src.Path())
 		if err != nil {
 			l.Warn("Failed to flush writes to instance volume", logger.Ctx{"err": err})
 		}
@@ -1670,7 +1670,7 @@ func (b *lxdBackend) RefreshInstance(inst instance.Instance, src instance.Instan
 		defer func() { _ = src.Unfreeze() }()
 
 		// Attempt to sync the filesystem.
-		err = filesystem.SyncFS(src.RootfsPath())
+		err = filesystem.SyncFS(src.Path())
 		if err != nil {
 			l.Warn("Failed to flush writes to instance volume", logger.Ctx{"err": err})
 		}
@@ -3115,7 +3115,7 @@ func (b *lxdBackend) MigrateInstance(inst instance.Instance, conn io.ReadWriteCl
 		defer func() { _ = inst.Unfreeze() }()
 
 		// Attempt to sync the filesystem.
-		err = filesystem.SyncFS(inst.RootfsPath())
+		err = filesystem.SyncFS(inst.Path())
 		if err != nil {
 			l.Warn("Failed to flush writes to instance volume", logger.Ctx{"err": err})
 		}
@@ -3630,7 +3630,7 @@ func (b *lxdBackend) CreateInstanceSnapshot(inst instance.Instance, src instance
 		defer func() { _ = src.Unfreeze() }()
 
 		// Attempt to sync the filesystem.
-		err = filesystem.SyncFS(src.RootfsPath())
+		err = filesystem.SyncFS(src.Path())
 		if err != nil {
 			l.Warn("Failed to flush writes to instance volume", logger.Ctx{"err": err})
 		}
