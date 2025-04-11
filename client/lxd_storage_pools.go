9fd9aca8dd78ab5d9a77dc9cd88198c1e36796fa
Kadin Sayani <kadin.sayani@canonical.com>
2025-04-11 15:39:55 -0600

    client: Update storage pool client methods
    Update client methods to construct URLs using `api.NewURL()` and replace
    HTTP method literals with constants from `http` package.
    
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- client/lxd_storage_pools.go
+++ client/lxd_storage_pools.go
@@ -1,8 +1,7 @@
 package lxd
 
 import (
-	"fmt"
-	"net/url"
+	"net/http"
 
 	"github.com/canonical/lxd/shared/api"
 )
@@ -18,8 +17,9 @@ func (r *ProtocolLXD) GetStoragePoolNames() ([]string, error) {
 
 	// Fetch the raw URL values.
 	urls := []string{}
-	baseURL := "/storage-pools"
-	_, err = r.queryStruct("GET", baseURL, nil, "", &urls)
+	u := api.NewURL().Path("storage-pools")
+	baseURL := u.String()
+	_, err = r.queryStruct(http.MethodGet, baseURL, nil, "", &urls)
 	if err != nil {
 		return nil, err
 	}
@@ -38,7 +38,8 @@ func (r *ProtocolLXD) GetStoragePools() ([]api.StoragePool, error) {
 	pools := []api.StoragePool{}
 
 	// Fetch the raw value
-	_, err = r.queryStruct("GET", "/storage-pools?recursion=1", nil, "", &pools)
+	u := api.NewURL().Path("storage-pools").WithQuery("recursion", "1")
+	_, err = r.queryStruct(http.MethodGet, u.String(), nil, "", &pools)
 	if err != nil {
 		return nil, err
 	}
@@ -56,7 +57,8 @@ func (r *ProtocolLXD) GetStoragePool(name string) (*api.StoragePool, string, err
 	pool := api.StoragePool{}
 
 	// Fetch the raw value
-	etag, err := r.queryStruct("GET", fmt.Sprintf("/storage-pools/%s", url.PathEscape(name)), nil, "", &pool)
+	u := api.NewURL().Path("storage-pools", name)
+	etag, err := r.queryStruct(http.MethodGet, u.String(), nil, "", &pool)
 	if err != nil {
 		return nil, "", err
 	}
@@ -79,7 +81,8 @@ func (r *ProtocolLXD) CreateStoragePool(pool api.StoragePoolsPost) error {
 	}
 
 	// Send the request
-	_, _, err = r.query("POST", "/storage-pools", pool, "")
+	u := api.NewURL().Path("storage-pools")
+	_, _, err = r.query(http.MethodPost, u.String(), pool, "")
 	if err != nil {
 		return err
 	}
@@ -95,7 +98,8 @@ func (r *ProtocolLXD) UpdateStoragePool(name string, pool api.StoragePoolPut, ET
 	}
 
 	// Send the request
-	_, _, err = r.query("PUT", fmt.Sprintf("/storage-pools/%s", url.PathEscape(name)), pool, ETag)
+	u := api.NewURL().Path("storage-pools", name)
+	_, _, err = r.query(http.MethodPut, u.String(), pool, ETag)
 	if err != nil {
 		return err
 	}
@@ -111,7 +115,8 @@ func (r *ProtocolLXD) DeleteStoragePool(name string) error {
 	}
 
 	// Send the request
-	_, _, err = r.query("DELETE", fmt.Sprintf("/storage-pools/%s", url.PathEscape(name)), nil, "")
+	u := api.NewURL().Path("storage-pools", name)
+	_, _, err = r.query(http.MethodDelete, u.String(), nil, "")
 	if err != nil {
 		return err
 	}
@@ -129,7 +134,8 @@ func (r *ProtocolLXD) GetStoragePoolResources(name string) (*api.ResourcesStorag
 	res := api.ResourcesStoragePool{}
 
 	// Fetch the raw value
-	_, err = r.queryStruct("GET", fmt.Sprintf("/storage-pools/%s/resources", url.PathEscape(name)), nil, "", &res)
+	u := api.NewURL().Path("storage-pools", name, "resources")
+	_, err = r.queryStruct(http.MethodGet, u.String(), nil, "", &res)
 	if err != nil {
 		return nil, err
 	}
