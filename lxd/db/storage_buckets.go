a3b4013a882e377293516e9401cd55418c0f2502
Kadin Sayani <kadin.sayani@canonical.com>
2025-04-14 10:58:00 -0600

    lxd/db: Fix lint error (staticcheck - QF1012)
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/db/storage_buckets.go
+++ lxd/db/storage_buckets.go
@@ -110,7 +110,7 @@ func (c *ClusterTx) GetStoragePoolBuckets(ctx context.Context, memberSpecific bo
 				q.WriteString(" OR ")
 			}
 
-			q.WriteString(fmt.Sprintf("(%s)", strings.Join(qFilters, " AND ")))
+			fmt.Fprintf(q, "(%s)", strings.Join(qFilters, " AND "))
 		}
 
 		q.WriteString(")")
@@ -458,7 +458,7 @@ func (c *ClusterTx) GetStoragePoolBucketKeys(ctx context.Context, bucketID int64
 				q.WriteString(" OR ")
 			}
 
-			q.WriteString(fmt.Sprintf("(%s)", strings.Join(qFilters, " AND ")))
+			fmt.Fprintf(q, "(%s)", strings.Join(qFilters, " AND "))
 		}
 
 		q.WriteString(")")
