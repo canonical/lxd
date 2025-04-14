686389992283be2cf339da5b6f8fffe70249d229
Din Music <din.music@canonical.com>
2025-04-14 12:37:09 +0000

    client/events: Introduce EventListenerManager
    Introduction of the eventListenerManager aims to solve two issues, the parent reference and code duplication.
    
    The EventListener has no longer a reference to the parent (ProtocolLXD), which allows us to reuse the
    EventListener for ProtocolDevLXD as well. Another benefit is that the setup and tracking of event listeners
    is now unified for both ProtocolLXD and ProtocolDevLXD, reducing code duplication.
    
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/events.go
+++ client/events.go
@@ -2,15 +2,20 @@ package lxd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"sync"
+	"time"
 
+	"github.com/gorilla/websocket"
+
+	"github.com/canonical/lxd/shared"
 	"github.com/canonical/lxd/shared/api"
+	"github.com/canonical/lxd/shared/revert"
 )
 
 // The EventListener struct is used to interact with a LXD event stream.
 type EventListener struct {
-	r         *ProtocolLXD
 	ctx       context.Context
 	ctxCancel context.CancelFunc
 	err       error
@@ -74,24 +79,6 @@ func (e *EventListener) RemoveHandler(target *EventTarget) error {
 
 // Disconnect must be used once done listening for events.
 func (e *EventListener) Disconnect() {
-	// Handle locking
-	e.r.eventListenersLock.Lock()
-	defer e.r.eventListenersLock.Unlock()
-
-	if e.ctx.Err() != nil {
-		return
-	}
-
-	// Locate and remove it from the global list
-	for i, listener := range e.r.eventListeners[e.projectName] {
-		if listener == e {
-			copy(e.r.eventListeners[e.projectName][i:], e.r.eventListeners[e.projectName][i+1:])
-			e.r.eventListeners[e.projectName][len(e.r.eventListeners[e.projectName])-1] = nil
-			e.r.eventListeners[e.projectName] = e.r.eventListeners[e.projectName][:len(e.r.eventListeners[e.projectName])-1]
-			break
-		}
-	}
-
 	// Turn off the handler
 	e.err = nil
 	e.ctxCancel()
@@ -106,4 +93,211 @@ func (e *EventListener) Wait() error {
 // IsActive returns true if this listener is still connected, false otherwise.
 func (e *EventListener) IsActive() bool {
 	return e.ctx.Err() == nil
+}
+
+// The eventListenerManager is used to manage event listeners.
+type eventListenerManager struct {
+	ctx context.Context
+
+	// eventConns contains event listener connections associated to a project name (or empty for all projects).
+	eventConns map[string]*websocket.Conn
+
+	// eventConnsLock controls write access to the eventConns.
+	eventConnsLock sync.Mutex
+
+	// eventListeners is a slice of event listeners associated to a project name (or empty for all projects).
+	eventListeners     map[string][]*EventListener
+	eventListenersLock sync.Mutex
+}
+
+// newEventListenerManager creates a new event listener manager.
+func newEventListenerManager(ctx context.Context) *eventListenerManager {
+	return &eventListenerManager{
+		ctx:            ctx,
+		eventConns:     make(map[string]*websocket.Conn),
+		eventListeners: make(map[string][]*EventListener),
+	}
+}
+
+// getEvents connects to the LXD monitoring interface.
+func (m *eventListenerManager) getEvents(ctxConnected context.Context, websocketHook func() (*websocket.Conn, error), project string) (*EventListener, error) {
+	// If a specific project is not provided, listen to all projects.
+	allProjects := project == ""
+
+	// Prevent anything else from interacting with the listeners
+	m.eventListenersLock.Lock()
+	defer m.eventListenersLock.Unlock()
+
+	revert := revert.New()
+	defer revert.Fail()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Make sure the context is cancelled in case of an error
+	// to prevent leaking a go routine that waits for it.
+	revert.Add(func() {
+		cancel()
+	})
+
+	// Setup a new listener
+	listener := EventListener{
+		ctx:       ctx,
+		ctxCancel: cancel,
+	}
+
+	if !allProjects {
+		listener.projectName = project
+	}
+
+	// Make sure we remove the listener from the list when it's context is done.
+	go func() {
+		<-ctx.Done()
+		m.eventListenersLock.Lock()
+		defer m.eventListenersLock.Unlock()
+
+		// Locate and remove the listener from the global list
+		for i, l := range m.eventListeners[listener.projectName] {
+			if &listener == l {
+				copy(m.eventListeners[listener.projectName][i:], m.eventListeners[listener.projectName][i+1:])
+				m.eventListeners[listener.projectName][len(m.eventListeners[listener.projectName])-1] = nil
+				m.eventListeners[listener.projectName] = m.eventListeners[listener.projectName][:len(m.eventListeners[listener.projectName])-1]
+				return
+			}
+		}
+	}()
+
+	// There is an existing Go routine for the required project filter, so just add another target.
+	if m.eventListeners[listener.projectName] != nil {
+		m.eventListeners[listener.projectName] = append(m.eventListeners[listener.projectName], &listener)
+		revert.Success()
+		return &listener, nil
+	}
+
+	// Connect to the websocket.
+	wsConn, err := websocketHook()
+	if err != nil {
+		return nil, err
+	}
+
+	m.eventConnsLock.Lock()
+	m.eventConns[listener.projectName] = wsConn // Save for others to use.
+	m.eventConnsLock.Unlock()
+
+	// Initialize the event listener list if we were able to connect to the events websocket.
+	m.eventListeners[listener.projectName] = []*EventListener{&listener}
+
+	// Spawn a watcher that will close the websocket connection after all
+	// listeners are gone.
+	stopCh := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-time.After(time.Minute):
+			case <-ctxConnected.Done():
+			case <-stopCh:
+			}
+
+			m.eventListenersLock.Lock()
+			m.eventConnsLock.Lock()
+			if len(m.eventListeners[listener.projectName]) == 0 {
+				// We don't need the connection anymore, disconnect and clear.
+				if m.eventListeners[listener.projectName] != nil {
+					_ = m.eventConns[listener.projectName].Close()
+					delete(m.eventConns, listener.projectName)
+				}
+
+				m.eventListeners[listener.projectName] = nil
+				m.eventListenersLock.Unlock()
+				m.eventConnsLock.Unlock()
+
+				return
+			}
+
+			m.eventListenersLock.Unlock()
+			m.eventConnsLock.Unlock()
+		}
+	}()
+
+	// Spawn the listener
+	go func() {
+		for {
+			_, data, err := wsConn.ReadMessage()
+			if err != nil {
+				// Prevent anything else from interacting with the listeners
+				m.eventListenersLock.Lock()
+				defer m.eventListenersLock.Unlock()
+
+				// Tell all the current listeners about the failure
+				for _, listener := range m.eventListeners[listener.projectName] {
+					listener.err = err
+					listener.ctxCancel()
+				}
+
+				// And remove them all from the list so that when watcher routine runs it will
+				// close the websocket connection.
+				m.eventListeners[listener.projectName] = nil
+
+				close(stopCh) // Instruct watcher go routine to cleanup.
+				return
+			}
+
+			// Attempt to unpack the message
+			event := api.Event{}
+			err = json.Unmarshal(data, &event)
+			if err != nil {
+				continue
+			}
+
+			// Extract the message type
+			if event.Type == "" {
+				continue
+			}
+
+			// Send the message to all handlers
+			m.eventListenersLock.Lock()
+			for _, listener := range m.eventListeners[listener.projectName] {
+				listener.targetsLock.Lock()
+				for _, target := range listener.targets {
+					if target.types != nil && !shared.ValueInSlice(event.Type, target.types) {
+						continue
+					}
+
+					go target.function(event)
+				}
+
+				listener.targetsLock.Unlock()
+			}
+
+			m.eventListenersLock.Unlock()
+		}
+	}()
+
+	revert.Success()
+	return &listener, nil
+}
+
+// SendEvent send an event to the server via the client's event listener connection.
+func (m *eventListenerManager) SendEvent(event api.Event) error {
+	m.eventConnsLock.Lock()
+	defer m.eventConnsLock.Unlock()
+
+	// Find an available event listener connection.
+	// It doesn't matter which project the event listener connection is using, as this only affects which
+	// events are received from the server, not which events we can send to it.
+	var eventConn *websocket.Conn
+	for _, eventConn = range m.eventConns {
+		break
+	}
+
+	if eventConn == nil {
+		return fmt.Errorf("No available event listener connection")
+	}
+
+	deadline, ok := m.ctx.Deadline()
+	if !ok {
+		deadline = time.Now().Add(5 * time.Second)
+	}
+
+	_ = eventConn.SetWriteDeadline(deadline)
+	return eventConn.WriteJSON(event)
 }
--- client/lxd_events.go
+++ client/lxd_events.go
@@ -1,163 +1,46 @@
 package lxd
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"time"
 
 	"github.com/gorilla/websocket"
 
-	"github.com/canonical/lxd/shared"
 	"github.com/canonical/lxd/shared/api"
 )
 
-// Event handling functions
-
 // getEvents connects to the LXD monitoring interface.
 func (r *ProtocolLXD) getEvents(allProjects bool) (*EventListener, error) {
-	// Prevent anything else from interacting with the listeners
-	r.eventListenersLock.Lock()
-	defer r.eventListenersLock.Unlock()
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	// Setup a new listener
-	listener := EventListener{
-		r:         r,
-		ctx:       ctx,
-		ctxCancel: cancel,
-	}
-
+	// Resolve the project name.
 	connInfo, _ := r.GetConnectionInfo()
 	if connInfo.Project == "" {
 		return nil, fmt.Errorf("Unexpected empty project in connection info")
 	}
 
+	project := ""
 	if !allProjects {
-		listener.projectName = connInfo.Project
+		project = connInfo.Project
 	}
 
-	// There is an existing Go routine for the required project filter, so just add another target.
-	if r.eventListeners[listener.projectName] != nil {
-		r.eventListeners[listener.projectName] = append(r.eventListeners[listener.projectName], &listener)
-		return &listener, nil
-	}
-
-	// Setup a new connection with LXD
-	var url string
-	var err error
-	if allProjects {
-		url, err = r.setQueryAttributes("/events?all-projects=true")
-	} else {
-		url, err = r.setQueryAttributes("/events")
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	// Connect websocket and save.
-	wsConn, err := r.websocket(url)
-	if err != nil {
-		return nil, err
-	}
-
-	r.eventConnsLock.Lock()
-	r.eventConns[listener.projectName] = wsConn // Save for others to use.
-	r.eventConnsLock.Unlock()
-
-	// Initialize the event listener list if we were able to connect to the events websocket.
-	r.eventListeners[listener.projectName] = []*EventListener{&listener}
-
-	// Spawn a watcher that will close the websocket connection after all
-	// listeners are gone.
-	stopCh := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-time.After(time.Minute):
-			case <-r.ctxConnected.Done():
-			case <-stopCh:
-			}
-
-			r.eventListenersLock.Lock()
-			r.eventConnsLock.Lock()
-			if len(r.eventListeners[listener.projectName]) == 0 {
-				// We don't need the connection anymore, disconnect and clear.
-				if r.eventListeners[listener.projectName] != nil {
-					_ = r.eventConns[listener.projectName].Close()
-					delete(r.eventConns, listener.projectName)
-				}
-
-				r.eventListeners[listener.projectName] = nil
-				r.eventListenersLock.Unlock()
-				r.eventConnsLock.Unlock()
-
-				return
-			}
-
-			r.eventListenersLock.Unlock()
-			r.eventConnsLock.Unlock()
+	// Wrap websocket connection in a function to allow the manager to
+	// establish a new connection.
+	getWebsocket := func() (*websocket.Conn, error) {
+		// Resolve LXD events URL.
+		var url string
+		var err error
+		if allProjects {
+			url, err = r.setQueryAttributes("/events?all-projects=true")
+		} else {
+			url, err = r.setQueryAttributes("/events")
 		}
-	}()
 
-	// Spawn the listener
-	go func() {
-		for {
-			_, data, err := wsConn.ReadMessage()
-			if err != nil {
-				// Prevent anything else from interacting with the listeners
-				r.eventListenersLock.Lock()
-				defer r.eventListenersLock.Unlock()
-
-				// Tell all the current listeners about the failure
-				for _, listener := range r.eventListeners[listener.projectName] {
-					listener.err = err
-					listener.ctxCancel()
-				}
-
-				// And remove them all from the list so that when watcher routine runs it will
-				// close the websocket connection.
-				r.eventListeners[listener.projectName] = nil
-
-				close(stopCh) // Instruct watcher go routine to cleanup.
-
-				return
-			}
-
-			// Attempt to unpack the message
-			event := api.Event{}
-			err = json.Unmarshal(data, &event)
-			if err != nil {
-				continue
-			}
-
-			// Extract the message type
-			if event.Type == "" {
-				continue
-			}
-
-			// Send the message to all handlers
-			r.eventListenersLock.Lock()
-			for _, listener := range r.eventListeners[listener.projectName] {
-				listener.targetsLock.Lock()
-				for _, target := range listener.targets {
-					if target.types != nil && !shared.ValueInSlice(event.Type, target.types) {
-						continue
-					}
-
-					go target.function(event)
-				}
-
-				listener.targetsLock.Unlock()
-			}
-
-			r.eventListenersLock.Unlock()
+		if err != nil {
+			return nil, err
 		}
-	}()
 
-	return &listener, nil
+		return r.websocket(url)
+	}
+
+	return r.eventListenerManager.getEvents(r.ctxConnected, getWebsocket, project)
 }
 
 // GetEvents gets the events for the project defined on the client.
@@ -172,26 +55,5 @@ func (r *ProtocolLXD) GetEventsAllProjects() (*EventListener, error) {
 
 // SendEvent send an event to the server via the client's event listener connection.
 func (r *ProtocolLXD) SendEvent(event api.Event) error {
-	r.eventConnsLock.Lock()
-	defer r.eventConnsLock.Unlock()
-
-	// Find an available event listener connection.
-	// It doesn't matter which project the event listener connection is using, as this only affects which
-	// events are received from the server, not which events we can send to it.
-	var eventConn *websocket.Conn
-	for _, eventConn = range r.eventConns {
-		break
-	}
-
-	if eventConn == nil {
-		return fmt.Errorf("No available event listener connection")
-	}
-
-	deadline, ok := r.ctx.Deadline()
-	if !ok {
-		deadline = time.Now().Add(5 * time.Second)
-	}
-
-	_ = eventConn.SetWriteDeadline(deadline)
-	return eventConn.WriteJSON(event)
+	return r.eventListenerManager.SendEvent(event)
 }
