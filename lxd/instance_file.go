6f55c355742dbeadd3997af38156dafaafbfd377
Stanislav Kozina <stanislav.kozina@canonical.com>
2025-04-15 22:09:49 +0200

    api: On file push to containers, only change uid/gid if it fits within uidmap range
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>
    (cherry picked from commit 4d962ac9c0085ae4db677b5d0af3fe024ac4d756)

--- lxd/instance_file.go
+++ lxd/instance_file.go
@@ -16,6 +16,7 @@ import (
 	"github.com/pkg/sftp"
 
 	"github.com/canonical/lxd/lxd/instance"
+	"github.com/canonical/lxd/lxd/instance/instancetype"
 	"github.com/canonical/lxd/lxd/lifecycle"
 	"github.com/canonical/lxd/lxd/request"
 	"github.com/canonical/lxd/lxd/response"
@@ -368,6 +369,48 @@ func instanceFileHead(inst instance.Instance, path string) response.Response {
 	})
 }
 
+// For containers we can only run chown/chgrp if target uid/gid is within uidmap allowed range.
+func effectiveFileOwnership(inst instance.Instance, headers *shared.LXDFileHeaders) (uid, gid int64, err error) {
+	uid = headers.UID
+	gid = headers.GID
+	if inst.Type() != instancetype.Container {
+		return uid, gid, nil
+	}
+
+	c, ok := inst.(instance.Container)
+	if !ok {
+		return 0, 0, fmt.Errorf("Invalid instance type: %T", inst)
+	}
+
+	idmapset, err := c.CurrentIdmap()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if idmapset == nil {
+		return uid, gid, nil
+	}
+
+	idmapranges, err := idmapset.ValidRanges()
+	if len(idmapranges) != 2 {
+		return 0, 0, err
+	}
+
+	for _, idmaprange := range idmapranges {
+		if idmaprange.Isuid && !idmaprange.Contains(headers.UID) {
+			logger.Info(fmt.Sprintf("Requested uid %d not within idmaprange", uid))
+			uid = -1
+		}
+
+		if idmaprange.Isgid && !idmaprange.Contains(headers.GID) {
+			logger.Info(fmt.Sprintf("Requested gid %d not within idmaprange", gid))
+			gid = -1
+		}
+	}
+
+	return uid, gid, nil
+}
+
 // swagger:operation POST /1.0/instances/{name}/files instances instance_files_post
 //
 //	Create or replace a file
@@ -502,8 +545,13 @@ func instanceFilePost(s *state.State, inst instance.Instance, path string, r *ht
 		// Set file ownership.
 		if !exists || headers.UIDModifyExisting || headers.GIDModifyExisting {
 			if headers.UID >= 0 || headers.GID >= 0 {
+				uid, gid, err := effectiveFileOwnership(inst, headers)
+				if err != nil {
+					return response.InternalError(err)
+				}
+
 				// -1 leaves the id unchanged
-				err = file.Chown(int(headers.UID), int(headers.GID))
+				err = file.Chown(int(uid), int(gid))
 				if err != nil {
 					return response.SmartError(err)
 				}
