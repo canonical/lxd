b388385376957e8315f5fe11234efb24886cee5f
Thomas Parrott <thomas.parrott@canonical.com>
2025-04-15 13:47:15 +0100

    lxd/instance/drivers/driver/qemu: Update comments
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>
    (cherry picked from commit b97c4aaf94eb051545393f8a56e1ecce212394ff)

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -4987,7 +4987,7 @@ func (d *qemu) Stop(stateful bool) error {
 		return err
 	}
 
-	// Trigger a rebalance
+	// Trigger a scheduler rebalance after DB changes made.
 	cgroup.TaskSchedulerTrigger(d.dbType, d.name, "stopped")
 
 	return nil
@@ -5913,7 +5913,7 @@ func (d *qemu) Update(args db.InstanceArgs, userRequested bool) error {
 	revert.Success()
 
 	if cpuLimitWasChanged {
-		// Trigger a scheduler re-run
+		// Trigger a scheduler rebalance after DB changes made.
 		cgroup.TaskSchedulerTrigger(d.dbType, d.name, "changed")
 	}
 
