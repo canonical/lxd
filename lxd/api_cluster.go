canonical/lxd

commit cc14d2b45785a58b74315ef581ccd6753d5ed563

lxd/api/cluster: Update clusterNodeGet to use updated ToAPI function
Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

lxd/api_cluster.go
@@ -1264,20 +1264,15 @@ func clusterNodesGet(d *Daemon, r *http.Request) response.Response {
 			return fmt.Errorf("Failed loading failure domains names: %w", err)
 		}
 
-		members, err = tx.GetNodes(ctx)
-		if err != nil {
-			return fmt.Errorf("Failed getting cluster members: %w", err)
-		}
-
 		if recursion {
 			memberFailureDomains, err := tx.GetNodesFailureDomains(ctx)
 			if err != nil {
 				return fmt.Errorf("Failed loading member failure domains: %w", err)
 			}
 
-			maxVersion, err := tx.GetNodeMaxVersion(ctx)
+			members, err = tx.GetNodes(ctx)
 			if err != nil {
-				return fmt.Errorf("Failed getting max member version: %w", err)
+				return fmt.Errorf("Failed getting cluster members: %w", err)
 			}
 
 			args := db.NodeInfoArgs{
@@ -1285,7 +1280,7 @@ func clusterNodesGet(d *Daemon, r *http.Request) response.Response {
 				FailureDomains:       failureDomains,
 				MemberFailureDomains: memberFailureDomains,
 				OfflineThreshold:     s.GlobalConfig.OfflineThreshold(),
-				MaxMemberVersion:     maxVersion,
+				Members:              members,
 				RaftNodes:            raftNodes,
 			}
 
@@ -1550,14 +1545,21 @@ func clusterNodeGet(d *Daemon, r *http.Request) response.Response {
 			return fmt.Errorf("Failed loading member failure domains: %w", err)
 		}
 
-		member, err := tx.GetNodeByName(ctx, name)
+		members, err := tx.GetNodes(ctx)
 		if err != nil {
-			return err
+			return fmt.Errorf("Failed getting cluster members: %w", err)
 		}
 
-		maxVersion, err := tx.GetNodeMaxVersion(ctx)
-		if err != nil {
-			return fmt.Errorf("Failed getting max member version: %w", err)
+		var member db.NodeInfo
+		for _, m := range members {
+			if m.Name == name {
+				member = m
+				break
+			}
+		}
+
+		if member.ID == 0 {
+			return api.StatusErrorf(http.StatusNotFound, "Cluster member not found %v", member)
 		}
 
 		args := db.NodeInfoArgs{
@@ -1565,7 +1567,7 @@ func clusterNodeGet(d *Daemon, r *http.Request) response.Response {
 			FailureDomains:       failureDomains,
 			MemberFailureDomains: memberFailureDomains,
 			OfflineThreshold:     s.GlobalConfig.OfflineThreshold(),
-			MaxMemberVersion:     maxVersion,
+			Members:              members,
 			RaftNodes:            raftNodes,
 		}
 

commit 17b6ae5dae1c19200223880c94dcb04313a6c21f

lxd: Fix regression that prevents listing members in non-recursive mode
Even if recursion isn't set, the list of cluster members should be returned.

The regression got introduced in cc14d2b45785a58b74315ef581ccd6753d5ed563.

Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

lxd/api_cluster.go
@@ -1264,15 +1264,15 @@ func clusterNodesGet(d *Daemon, r *http.Request) response.Response {
 			return fmt.Errorf("Failed loading failure domains names: %w", err)
 		}
 
+		members, err = tx.GetNodes(ctx)
+		if err != nil {
+			return fmt.Errorf("Failed getting cluster members: %w", err)
+		}
+
 		if recursion {
 			memberFailureDomains, err := tx.GetNodesFailureDomains(ctx)
 			if err != nil {
 				return fmt.Errorf("Failed loading member failure domains: %w", err)
-			}
-
-			members, err = tx.GetNodes(ctx)
-			if err != nil {
-				return fmt.Errorf("Failed getting cluster members: %w", err)
 			}
 
 			args := db.NodeInfoArgs{