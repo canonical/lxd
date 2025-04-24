665d21060ac695105d546c82c93b8a0700b45f56
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-04-24 12:04:26 +0200

    lxd/backup: Update package location for types
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit fc16633a27a71ec6ee36ef5501655838fd9fa5ee)

--- lxd/backup.go
+++ lxd/backup.go
@@ -12,6 +12,7 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/canonical/lxd/lxd/backup"
+	backupConfig "github.com/canonical/lxd/lxd/backup/config"
 	"github.com/canonical/lxd/lxd/db"
 	dbCluster "github.com/canonical/lxd/lxd/db/cluster"
 	"github.com/canonical/lxd/lxd/db/operationtype"
@@ -237,7 +238,7 @@ func backupWriteIndex(sourceInst instance.Instance, pool storagePools.Pool, opti
 	}
 
 	backupType := backup.InstanceTypeToBackupType(api.InstanceType(sourceInst.Type().String()))
-	if backupType == backup.TypeUnknown {
+	if backupType == backupConfig.TypeUnknown {
 		return errors.New("Unrecognised instance type for backup type conversion")
 	}
 
@@ -567,7 +568,7 @@ func volumeBackupWriteIndex(projectName string, volumeName string, pool storageP
 		Backend:          pool.Driver().Info().Name,
 		OptimizedStorage: &optimized,
 		OptimizedHeader:  &poolDriverOptimizedHeader,
-		Type:             backup.TypeCustom,
+		Type:             backupConfig.TypeCustom,
 		Config:           config,
 	}
 
