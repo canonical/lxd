21b4dd1ed82e5fc7b58f82bd9ed50662cf83115d
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-04-29 11:26:17 +0200

    lxd/storage: Update instance backup file if custom volume snapshot got deleted
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/storage/backend_lxd.go
+++ lxd/storage/backend_lxd.go
@@ -6864,9 +6864,8 @@ func (b *lxdBackend) DeleteCustomVolumeSnapshot(projectName, volName string, op 
 	l.Debug("DeleteCustomVolumeSnapshot started")
 	defer l.Debug("DeleteCustomVolumeSnapshot finished")
 
-	isSnap := shared.IsSnapshot(volName)
-
-	if !isSnap {
+	parentVolName, _, isSnapshot := api.GetParentAndSnapshotName(volName)
+	if !isSnapshot {
 		return errors.New("Volume name must be a snapshot")
 	}
 
@@ -6915,6 +6914,12 @@ func (b *lxdBackend) DeleteCustomVolumeSnapshot(projectName, volName string, op 
 
 	// Remove the snapshot volume record from the database.
 	err = VolumeDBDelete(b, projectName, volName, vol.Type())
+	if err != nil {
+		return err
+	}
+
+	// Update the backup config file of the corresponding instances.
+	err = b.UpdateCustomVolumeBackupFile(projectName, parentVolName, true, nil, op)
 	if err != nil {
 		return err
 	}
