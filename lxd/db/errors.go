cad7a4c555d767f07c74ee04cbb0b3736f2f929e
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:35 -0400

    lxd/db/errors: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/db/errors.go
+++ lxd/db/errors.go
@@ -1,7 +1,7 @@
 package db
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 
 	"github.com/canonical/go-dqlite/v3/driver"
@@ -10,7 +10,7 @@ import (
 
 var (
 	// ErrNoClusterMember is used to indicate no cluster member has been found for a resource.
-	ErrNoClusterMember = fmt.Errorf("No cluster member found")
+	ErrNoClusterMember = errors.New("No cluster member found")
 )
 
 // SmartErrors are used to return more appropriate errors to the caller.
