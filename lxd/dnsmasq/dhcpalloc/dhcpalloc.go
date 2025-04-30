fc4db6170ab2e098ea77ede530d12e78399f95b8
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:37 -0400

    lxd/dnsmasq/dhcpalloc/dhcpalloc: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/dnsmasq/dhcpalloc/dhcpalloc.go
+++ lxd/dnsmasq/dhcpalloc/dhcpalloc.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"math"
 	"math/big"
 	"net"
@@ -233,7 +232,7 @@ func (t *Transaction) getDHCPFreeIPv4(usedIPs map[[4]byte]dnsmasq.DHCPAllocation
 		}
 	}
 
-	return nil, fmt.Errorf("No available IP could not be found")
+	return nil, errors.New("No available IP could not be found")
 }
 
 // getDHCPFreeIPv6 attempts to find a free IPv6 address for the device.
@@ -319,7 +318,7 @@ func (t *Transaction) getDHCPFreeIPv6(usedIPs map[[16]byte]dnsmasq.DHCPAllocatio
 		}
 	}
 
-	return nil, fmt.Errorf("No available IP could not be found")
+	return nil, errors.New("No available IP could not be found")
 }
 
 // AllocateTask initialises a new locked Transaction for a specific host and executes the supplied function on it.
