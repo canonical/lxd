8e45e7352990b4f8b8753fbab0432d50a98574fa
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:33 -0400

    lxd/archive/archive: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 469a4308a91f3ba73dbc291033471d29f8e4faad)

--- lxd/archive/archive.go
+++ lxd/archive/archive.go
@@ -4,6 +4,7 @@ import (
 	"archive/tar"
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -264,10 +265,10 @@ func Unpack(file string, path string, blockBackend bool, sysOS *sys.OS, tracker 
 		// Check if we're running out of space
 		if int64(fs.Bfree) < 10 {
 			if blockBackend {
-				return fmt.Errorf("Unable to unpack image, run out of disk space (consider increasing your pool's volume.size)")
+				return errors.New("Unable to unpack image, run out of disk space (consider increasing your pool's volume.size)")
 			}
 
-			return fmt.Errorf("Unable to unpack image, run out of disk space")
+			return errors.New("Unable to unpack image, run out of disk space")
 		}
 
 		logger.Warn("Unpack failed", logger.Ctx{"file": file, "allowedCmds": allowedCmds, "extension": extension, "path": path, "err": err})
