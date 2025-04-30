c5cb9500e46956e279897044e23089593fb9768c
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:32 -0400

    lxc/config/errors: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxc/config/errors.go
+++ lxc/config/errors.go
@@ -1,8 +1,8 @@
 package config
 
 import (
-	"fmt"
+	"errors"
 )
 
 // ErrNotLinux is returned when attemping to access the "local" remote on non-Linux systems.
-var ErrNotLinux = fmt.Errorf("Can't connect to a local LXD server on a non-Linux system")
+var ErrNotLinux = errors.New("Can't connect to a local LXD server on a non-Linux system")
