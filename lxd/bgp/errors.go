1917945bc90f0bc8499180585de544c9831b0d4d
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:33 -0400

    lxd/bgp/errors: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/bgp/errors.go
+++ lxd/bgp/errors.go
@@ -1,14 +1,14 @@
 package bgp
 
 import (
-	"fmt"
+	"errors"
 )
 
 // ErrPrefixNotFound is returned when a user provided prefix couldn't be found.
-var ErrPrefixNotFound = fmt.Errorf("Prefix not found")
+var ErrPrefixNotFound = errors.New("Prefix not found")
 
 // ErrPeerNotFound is returned when a user provided peer couldn't be found.
-var ErrPeerNotFound = fmt.Errorf("Peer not found")
+var ErrPeerNotFound = errors.New("Peer not found")
 
 // ErrBadRouterID is returned when an invalid router-id is provided.
-var ErrBadRouterID = fmt.Errorf("Invalid router-id (must be IPv4 address")
+var ErrBadRouterID = errors.New("Invalid router-id (must be IPv4 address")
