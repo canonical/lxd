018818eed52e7277e7ffb5bbb840155f0c690bef
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:27:04 -0400

    lxd/rsync/rsync: avoid shadowing builtin `min`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/rsync/rsync.go
+++ lxd/rsync/rsync.go
@@ -432,7 +432,7 @@ func rsyncFeatureArgs(features []string) []string {
 }
 
 // AtLeast compares the local version to a minimum version.
-func AtLeast(min string) bool {
+func AtLeast(minimum string) bool {
 	// Parse the current version.
 	out, err := shared.RunCommandContext(context.TODO(), "rsync", "--version")
 	if err != nil {
@@ -452,7 +452,7 @@ func AtLeast(min string) bool {
 	}
 
 	// Load minium version.
-	minVer, err := version.NewDottedVersion(min)
+	minVer, err := version.NewDottedVersion(minimum)
 	if err != nil {
 		return false
 	}
