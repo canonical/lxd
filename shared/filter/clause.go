fd8d165bc157a92b1753da52806bdb8752c561fb
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:42 -0400

    shared/filter/clause: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- shared/filter/clause.go
+++ shared/filter/clause.go
@@ -1,7 +1,7 @@
 package filter
 
 import (
-	"fmt"
+	"errors"
 	"regexp"
 	"strings"
 
@@ -34,7 +34,7 @@ type ClauseSet struct {
 // Parse a user-provided filter string.
 func Parse(s string, op OperatorSet) (*ClauseSet, error) {
 	if !op.isValid() {
-		return nil, fmt.Errorf("Invalid operator set")
+		return nil, errors.New("Invalid operator set")
 	}
 
 	clauses := []Clause{}
@@ -51,7 +51,7 @@ func Parse(s string, op OperatorSet) (*ClauseSet, error) {
 			clause.Not = true
 			index++
 			if index == len(parts) {
-				return nil, fmt.Errorf("incomplete not clause")
+				return nil, errors.New("incomplete not clause")
 			}
 		} else {
 			clause.Not = false
@@ -61,14 +61,14 @@ func Parse(s string, op OperatorSet) (*ClauseSet, error) {
 
 		index++
 		if index == len(parts) {
-			return nil, fmt.Errorf("clause has no operator")
+			return nil, errors.New("clause has no operator")
 		}
 
 		clause.Operator = parts[index]
 
 		index++
 		if index == len(parts) {
-			return nil, fmt.Errorf("clause has no value")
+			return nil, errors.New("clause has no value")
 		}
 
 		value := parts[index]
@@ -80,7 +80,7 @@ func Parse(s string, op OperatorSet) (*ClauseSet, error) {
 				for {
 					index++
 					if index == len(parts) {
-						return nil, fmt.Errorf("unterminated quote")
+						return nil, errors.New("unterminated quote")
 					}
 
 					if strings.HasSuffix(parts[index], symbol) {
@@ -102,12 +102,12 @@ func Parse(s string, op OperatorSet) (*ClauseSet, error) {
 		if index < len(parts) {
 			prevLogical = parts[index]
 			if !shared.ValueInSlice(prevLogical, []string{op.And, op.Or}) {
-				return nil, fmt.Errorf("invalid clause composition")
+				return nil, errors.New("invalid clause composition")
 			}
 
 			index++
 			if index == len(parts) {
-				return nil, fmt.Errorf("unterminated compound clause")
+				return nil, errors.New("unterminated compound clause")
 			}
 		}
 
