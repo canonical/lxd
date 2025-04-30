b93b31bca7dffdf8ca006e5ecc62a19a08092504
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:33 -0400

    lxd/cluster/config/config: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/cluster/config/config.go
+++ lxd/cluster/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -792,7 +793,7 @@ func offlineThresholdValidator(value string) error {
 	// which is the lower bound granularity of the offline check.
 	threshold, err := strconv.Atoi(value)
 	if err != nil {
-		return fmt.Errorf("Offline threshold is not a number")
+		return errors.New("Offline threshold is not a number")
 	}
 
 	if threshold <= minThreshold {
@@ -805,11 +806,11 @@ func offlineThresholdValidator(value string) error {
 func imageMinimalReplicaValidator(value string) error {
 	count, err := strconv.Atoi(value)
 	if err != nil {
-		return fmt.Errorf("Minimal image replica count is not a number")
+		return errors.New("Minimal image replica count is not a number")
 	}
 
 	if count < 1 && count != -1 {
-		return fmt.Errorf("Invalid value for image replica count")
+		return errors.New("Invalid value for image replica count")
 	}
 
 	return nil
@@ -818,11 +819,11 @@ func imageMinimalReplicaValidator(value string) error {
 func maxVotersValidator(value string) error {
 	n, err := strconv.Atoi(value)
 	if err != nil {
-		return fmt.Errorf("Value is not a number")
+		return errors.New("Value is not a number")
 	}
 
 	if n < 3 || n%2 != 1 {
-		return fmt.Errorf("Value must be an odd number equal to or higher than 3")
+		return errors.New("Value must be an odd number equal to or higher than 3")
 	}
 
 	return nil
@@ -831,11 +832,11 @@ func maxVotersValidator(value string) error {
 func maxStandByValidator(value string) error {
 	n, err := strconv.Atoi(value)
 	if err != nil {
-		return fmt.Errorf("Value is not a number")
+		return errors.New("Value is not a number")
 	}
 
 	if n < 0 || n > 5 {
-		return fmt.Errorf("Value must be between 0 and 5")
+		return errors.New("Value must be between 0 and 5")
 	}
 
 	return nil
