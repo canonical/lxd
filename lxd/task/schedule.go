e53f80156f00c0f13809def42b9a692f439edf06
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:42 -0400

    lxd/task/schedule: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/task/schedule.go
+++ lxd/task/schedule.go
@@ -1,7 +1,7 @@
 package task
 
 import (
-	"fmt"
+	"errors"
 	"time"
 )
 
@@ -27,7 +27,7 @@ type Schedule func() (time.Duration, error)
 // ErrSkip is a special error that may be returned by a Schedule function to
 // mean to skip a particular execution of the task function, and just wait the
 // returned interval before re-evaluating.
-var ErrSkip = fmt.Errorf("skip execution of task function")
+var ErrSkip = errors.New("skip execution of task function")
 
 // Every returns a Schedule that always returns the given time interval.
 func Every(interval time.Duration, options ...EveryOption) Schedule {
