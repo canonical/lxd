119e438f5859b5a9771f8dfc336dfc03c99f412d
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:31 -0400

    client/simplestreams: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- client/simplestreams.go
+++ client/simplestreams.go
@@ -1,7 +1,7 @@
 package lxd
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 
 	"github.com/canonical/lxd/shared/simplestreams"
@@ -35,7 +35,7 @@ func (r *ProtocolSimpleStreams) GetConnectionInfo() (*ConnectionInfo, error) {
 // GetHTTPClient returns the http client used for the connection. This can be used to set custom http options.
 func (r *ProtocolSimpleStreams) GetHTTPClient() (*http.Client, error) {
 	if r.http == nil {
-		return nil, fmt.Errorf("HTTP client isn't set, bad connection")
+		return nil, errors.New("HTTP client isn't set, bad connection")
 	}
 
 	return r.http, nil
