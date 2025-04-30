f49e9ec92eac3eb9927112cd2580e117a060cf3d
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-04-30 15:33:23 +0200

    lxd/instance/drivers: Update invocations
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/instance/drivers/driver_lxc.go
+++ lxd/instance/drivers/driver_lxc.go
@@ -5356,7 +5356,7 @@ func (d *lxc) MigrateSend(args instance.MigrateSendArgs) error {
 		}
 	}
 
-	srcConfig, err := pool.GenerateInstanceBackupConfig(d, args.Snapshots, d.op)
+	srcConfig, err := pool.GenerateInstanceBackupConfig(d, args.Snapshots, nil, d.op)
 	if err != nil {
 		return fmt.Errorf("Failed generating instance migration config: %w", err)
 	}
@@ -8322,7 +8322,7 @@ func (d *lxc) UpdateBackupFile() error {
 	}
 
 	// Use the global metadata version.
-	return pool.UpdateInstanceBackupFile(d, true, config.DefaultMetadataVersion, nil)
+	return pool.UpdateInstanceBackupFile(d, true, nil, config.DefaultMetadataVersion, nil)
 }
 
 // Info returns "lxc" and the currently loaded version of LXC.
