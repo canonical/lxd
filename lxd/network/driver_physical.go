e58662ee617f9fd26320e3c4ddf7b0a6ec747535
Simon Deziel <simon.deziel@canonical.com>
2025-04-30 16:17:40 -0400

    lxd/network/driver_physical: use `errors.New()` (perfsprint)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 1322863c6c51ba1166522edcbebe495ebf92861f)

--- lxd/network/driver_physical.go
+++ lxd/network/driver_physical.go
@@ -2,6 +2,7 @@ package network
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
@@ -462,7 +463,7 @@ func (n *physical) Update(newNetwork api.NetworkPut, targetNode string, clientTy
 		if hostNameChanged {
 			isUsed, err := n.IsUsed()
 			if isUsed || err != nil {
-				return fmt.Errorf("Cannot update network parent interface when in use")
+				return errors.New("Cannot update network parent interface when in use")
 			}
 
 			inUse, err := n.checkParentUse(newNetwork.Config)
