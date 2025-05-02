a39daa2b3a321cd36ba600e97b91a13c003cfc23
Mark Laing <mark.laing@canonical.com>
2025-05-02 12:24:43 +0100

    lxd/db/generate: Add errors package import.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 4f1ae70e301e07de02321d409a2e8a82e910d9f3)

--- lxd/db/generate/db/constants.go
+++ lxd/db/generate/db/constants.go
@@ -5,6 +5,7 @@ package db
 // Imports is a list of the package imports every generated source file has.
 var Imports = []string{
 	"database/sql",
+	"errors",
 	"fmt",
 	"github.com/canonical/lxd/lxd/db/query",
 	"github.com/canonical/lxd/shared/api",
