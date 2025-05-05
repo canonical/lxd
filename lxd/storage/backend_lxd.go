56d6cd9061b3b8254053052e5009d75177acbd2f
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-05-05 12:15:13 +0200

    lxd/storage: Include the pool for custom volumes in the backup file
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/storage/backend_lxd.go
+++ lxd/storage/backend_lxd.go
@@ -6993,6 +6993,7 @@ func (b *lxdBackend) GenerateCustomVolumeBackupConfig(projectName string, volNam
 	return &backupConfig.Config{
 		Version: api.BackupMetadataVersion2,
 		Volumes: []*backupConfig.Volume{volConfig},
+		Pools:   []*api.StoragePool{&b.db},
 	}, nil
 }
 
