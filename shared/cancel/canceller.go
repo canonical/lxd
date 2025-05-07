9c5299f74c2c86620078c5815700201626bd4ea4
Thomas Parrott <thomas.parrott@canonical.com>
2025-05-07 16:24:33 +0100

    shared/cancel: Reworks Canceller to be an interface and avoid using internal context.WithCancel
    This avoids the associated go routine created with context.WithCancel and that is easy to leak with
    Cancellers stored in long-lived structs.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/cancel/canceller.go
+++ shared/cancel/canceller.go
@@ -2,20 +2,66 @@ package cancel
 
 import (
 	"context"
+	"sync"
+	"time"
 )
 
-// Canceller is a simple wrapper for a cancellable context which makes the associated context.CancelFunc more easily
-// accessible.
-type Canceller struct {
+// Canceller extends context.Context with a built-in Cancel function.
+type Canceller interface {
 	context.Context
-	Cancel context.CancelFunc
+	Cancel()
 }
 
-// New returns a new canceller with the parent context.
-func New(ctx context.Context) *Canceller {
-	ctx, cancel := context.WithCancel(ctx)
-	return &Canceller{
-		Context: ctx,
-		Cancel:  cancel,
+// canceller is an implementation of Canceller that behaves similarly to context.WithCancel()
+// but does not use go routines, which makes it more convenient to store in a long-lived struct.
+// It implements the context.Context interface.
+type canceller struct {
+	ch   chan struct{}
+	once func()
+}
+
+// Err returns nil if Cancel() has not been called yet.
+// If Cancel() has been called then returns context.Canceled.
+// After Err returns a non-nil error, successive calls to Err return the same error.
+func (c *canceller) Err() error {
+	select {
+	case <-c.ch:
+		return context.Canceled
+	default:
+		return nil
+	}
+}
+
+// Cancel the Canceller.
+// Can be called multiple times safely.
+func (c *canceller) Cancel() {
+	c.once()
+}
+
+// Done returns a channel that's closed when the canceller.Cancel() is called.
+// Successive calls to Done return the same value.
+func (c *canceller) Done() <-chan struct{} {
+	return c.ch
+}
+
+// Value is not implemented.
+func (c *canceller) Value(key any) any {
+	return nil
+}
+
+// Deadline is not implemented.
+func (c *canceller) Deadline() (deadline time.Time, ok bool) {
+	return time.Time{}, false
+}
+
+// New returns a new Canceller.
+func New() Canceller {
+	ch := make(chan struct{})
+
+	return &canceller{
+		ch: ch,
+		once: sync.OnceFunc(func() {
+			close(ch)
+		}),
 	}
 }
