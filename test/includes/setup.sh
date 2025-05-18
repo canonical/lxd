cec8eb06a4c1a819e399aad0d163d6ef7ca2aa00
Simon Deziel <simon.deziel@canonical.com>
2025-05-18 17:21:27 -0400

    test/includes/setup: don't abort pipelines early with `grep -q`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 04176417583dcefec7845112a91a3b86099697bb)

--- test/includes/setup.sh
+++ test/includes/setup.sh
@@ -2,28 +2,30 @@
 
 ensure_has_localhost_remote() {
     local addr="${1}"
-    if ! lxc remote list | grep -q "localhost"; then
+    if ! lxc remote list | grep -wF "localhost" >/dev/null; then
         lxc remote add localhost "https://${addr}" --accept-certificate --password foo
     fi
 }
 
 ensure_import_testimage() {
-    if ! lxc image alias list | grep -q "^| testimage\\s*|.*$"; then
-        if [ -e "${LXD_TEST_IMAGE:-}" ]; then
-            lxc image import "${LXD_TEST_IMAGE}" --alias testimage
-        else
-            if [ ! -e "/bin/busybox" ]; then
-                echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
-                exit 1
-            fi
+    if lxc image alias list testimage | grep -wF "testimage" >/dev/null; then
+        return
+    fi
 
-            if ldd /bin/busybox >/dev/null 2>&1; then
-                echo "The testsuite requires /bin/busybox to be a static binary"
-                exit 1
-            fi
-
-            project="$(lxc project list | awk '/(current)/ {print $2}')"
-            deps/import-busybox --alias testimage --project "$project"
+    if [ -e "${LXD_TEST_IMAGE:-}" ]; then
+        lxc image import "${LXD_TEST_IMAGE}" --alias testimage
+    else
+        if [ ! -e "/bin/busybox" ]; then
+            echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
+            exit 1
         fi
+
+        if ldd /bin/busybox >/dev/null 2>&1; then
+            echo "The testsuite requires /bin/busybox to be a static binary"
+            exit 1
+        fi
+
+        project="$(lxc project list | awk '/(current)/ {print $2}')"
+        deps/import-busybox --alias testimage --project "$project"
     fi
 }
