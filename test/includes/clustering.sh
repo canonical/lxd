lxc/incus

tests: Switch clustering test subnet
Avoid conflicts with Microsoft Azure Github runners.

Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

test/includes/clustering.sh
@@ -6,9 +6,9 @@ setup_clustering_bridge() {
   echo "==> Setup clustering bridge ${name}"
 
   ip link add "${name}" up type bridge
-  ip addr add 10.1.1.1/16 dev "${name}"
+  ip addr add 100.64.1.1/16 dev "${name}"
 
-  iptables -w -t nat -A POSTROUTING -s 10.1.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
+  iptables -w -t nat -A POSTROUTING -s 100.64.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
   echo 1 > /proc/sys/net/ipv4/ip_forward
 }
 
@@ -18,7 +18,7 @@ teardown_clustering_bridge() {
   if [ -e "/sys/class/net/${name}" ]; then
       echo "==> Teardown clustering bridge ${name}"
       echo 0 > /proc/sys/net/ipv4/ip_forward
-      iptables -w -t nat -D POSTROUTING -s 10.1.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
+      iptables -w -t nat -D POSTROUTING -s 100.64.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
       ip link del dev "${name}"
   fi
 }
@@ -80,8 +80,8 @@ set -e
 ip link set dev lo up
 ip link set dev "${veth2}" name eth0
 ip link set eth0 up
-ip addr add "10.1.1.10${id}/16" dev eth0
-ip route add default via 10.1.1.1
+ip addr add "100.64.1.10${id}/16" dev eth0
+ip route add default via 100.64.1.1
 EOF
 }
 
@@ -133,11 +133,11 @@ spawn_incus_and_bootstrap_cluster() {
 
     cat > "${INCUS_DIR}/preseed.yaml" <<EOF
 config:
-  core.https_address: 10.1.1.101:8443
+  core.https_address: 100.64.1.101:8443
 EOF
     if [ "${port}" != "" ]; then
       cat >> "${INCUS_DIR}/preseed.yaml" <<EOF
-  cluster.https_address: 10.1.1.101:${port}
+  cluster.https_address: 100.64.1.101:${port}
 EOF
     fi
     if [ "${driver}" = "linstor" ]; then
@@ -248,7 +248,7 @@ spawn_incus_and_join_cluster() {
     # If a custom cluster port was given, we need to first set the REST
     # API address.
     if [ "${port}" != "8443" ]; then
-      incus config set core.https_address "10.1.1.10${index}:8443"
+      incus config set core.https_address "100.64.1.10${index}:8443"
     fi
 
     # If there is a satellite name override, apply it.
@@ -260,8 +260,8 @@ spawn_incus_and_join_cluster() {
 cluster:
   enabled: true
   server_name: node${index}
-  server_address: 10.1.1.10${index}:${port}
-  cluster_address: 10.1.1.10${target}:8443
+  server_address: 100.64.1.10${index}:${port}
+  cluster_address: 100.64.1.10${target}:8443
   cluster_certificate: "$cert"
   cluster_token: ${token}
   member_config:

test/suites/clustering.sh
@@ -252,8 +252,8 @@ test_clustering_membership() {
 
   # Client certificate are shared across all nodes.
   token="$(INCUS_DIR=${INCUS_ONE_DIR} incus config trust add foo -q)"
-  incus remote add cluster 10.1.1.101:8443 --accept-certificate --token "${token}"
-  incus remote set-url cluster https://10.1.1.102:8443
+  incus remote add cluster 100.64.1.101:8443 --accept-certificate --token "${token}"
+  incus remote set-url cluster https://100.64.1.102:8443
   incus network list cluster: | grep -q "${bridge}"
   incus remote remove cluster
 
@@ -820,7 +820,7 @@ test_clustering_storage() {
     # Manually send the join request.
     cert=$(sed ':a;N;$!ba;s/\n/\\n/g' "${INCUS_ONE_DIR}/cluster.crt")
     token="$(incus cluster add node3 --quiet)"
-    op=$(curl --unix-socket "${INCUS_THREE_DIR}/unix.socket" -X PUT "incus/1.0/cluster" -d "{\"server_name\":\"node3\",\"enabled\":true,\"member_config\":[${member_config}],\"server_address\":\"10.1.1.103:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r .operation)
+    op=$(curl --unix-socket "${INCUS_THREE_DIR}/unix.socket" -X PUT "incus/1.0/cluster" -d "{\"server_name\":\"node3\",\"enabled\":true,\"member_config\":[${member_config}],\"server_address\":\"100.64.1.103:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r .operation)
     curl --unix-socket "${INCUS_THREE_DIR}/unix.socket" "incus${op}/wait"
 
     # Ensure that node-specific config appears on all nodes,
@@ -1782,7 +1782,7 @@ test_clustering_join_api() {
   INCUS_NETNS="${ns2}" spawn_incus "${INCUS_TWO_DIR}" false
 
   token="$(incus cluster add node2 --quiet)"
-  op=$(curl --unix-socket "${INCUS_TWO_DIR}/unix.socket" -X PUT "incus/1.0/cluster" -d "{\"server_name\":\"node2\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"10.1.1.102:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r .operation)
+  op=$(curl --unix-socket "${INCUS_TWO_DIR}/unix.socket" -X PUT "incus/1.0/cluster" -d "{\"server_name\":\"node2\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"100.64.1.102:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r .operation)
   curl --unix-socket "${INCUS_TWO_DIR}/unix.socket" "incus${op}/wait"
 
   INCUS_DIR="${INCUS_ONE_DIR}" incus cluster show node2 | grep -q "message: Fully operational"
@@ -1959,7 +1959,7 @@ test_clustering_address() {
 
   # Add a remote using the core.https_address of the bootstrap node, and check
   # that the REST API is exposed.
-  url="https://10.1.1.101:8443"
+  url="https://100.64.1.101:8443"
   token="$(INCUS_DIR="${INCUS_ONE_DIR}" incus config trust add foo --quiet)"
   incus remote add cluster --token "${token}" --accept-certificate "${url}"
   incus storage list cluster: | grep -q data
@@ -1982,13 +1982,13 @@ test_clustering_address() {
 
   # The core.https_address config value can be changed and the REST API is still
   # accessible.
-  INCUS_DIR="${INCUS_ONE_DIR}" incus config set "core.https_address" 10.1.1.101:9999
-  url="https://10.1.1.101:9999"
+  INCUS_DIR="${INCUS_ONE_DIR}" incus config set "core.https_address" 100.64.1.101:9999
+  url="https://100.64.1.101:9999"
   incus remote set-url cluster "${url}"
   incus storage list cluster:| grep -q data
 
   # The cluster.https_address config value can't be changed.
-  ! INCUS_DIR="${INCUS_ONE_DIR}" incus config set "cluster.https_address" "10.1.1.101:8448" || false
+  ! INCUS_DIR="${INCUS_ONE_DIR}" incus config set "cluster.https_address" "100.64.1.101:8448" || false
 
   # Create a container using the REST API exposed over core.https_address.
   INCUS_DIR="${INCUS_ONE_DIR}" deps/import-busybox --alias testimage
@@ -2214,9 +2214,9 @@ test_clustering_recover() {
   sleep 5
 
   # Check the current database nodes
-  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "10.1.1.101:8443"
-  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "10.1.1.102:8443"
-  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "10.1.1.103:8443"
+  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "100.64.1.101:8443"
+  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "100.64.1.102:8443"
+  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "100.64.1.103:8443"
 
   # Create a test project, just to insert something in the database.
   INCUS_DIR="${INCUS_ONE_DIR}" incus project create p1
@@ -2238,8 +2238,8 @@ test_clustering_recover() {
   INCUS_DIR="${INCUS_ONE_DIR}" incus project list | grep -q p1
 
   # The database nodes have been updated
-  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "10.1.1.101:8443"
-  ! INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "10.1.1.102:8443" || false
+  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "100.64.1.101:8443"
+  ! INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster list-database | grep -q "100.64.1.102:8443" || false
 
   # Cleanup the dead node.
   INCUS_DIR="${INCUS_ONE_DIR}" incus cluster remove node2 --force --yes
@@ -2531,7 +2531,7 @@ test_clustering_remove_raft_node() {
   # Remove the second node from the database but not from the raft configuration.
   retries=10
   while [ "${retries}" != "0" ]; do
-    INCUS_DIR="${INCUS_ONE_DIR}" incus admin sql global "DELETE FROM nodes WHERE address = '10.1.1.102:8443'" && break
+    INCUS_DIR="${INCUS_ONE_DIR}" incus admin sql global "DELETE FROM nodes WHERE address = '100.64.1.102:8443'" && break
     sleep 0.5
     retries=$((retries-1))
   done
@@ -2554,10 +2554,10 @@ test_clustering_remove_raft_node() {
   INCUS_DIR="${INCUS_ONE_DIR}" incus cluster show node4 | grep -q "\- database$"
 
   # The second node is still in the raft_nodes table.
-  INCUS_DIR="${INCUS_ONE_DIR}" incus admin sql local "SELECT * FROM raft_nodes" | grep -q "10.1.1.102"
+  INCUS_DIR="${INCUS_ONE_DIR}" incus admin sql local "SELECT * FROM raft_nodes" | grep -q "100.64.1.102"
 
   # Force removing the raft node.
-  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster remove-raft-node -q "10.1.1.102"
+  INCUS_DIR="${INCUS_ONE_DIR}" incusd cluster remove-raft-node -q "100.64.1.102"
 
   # Wait for a heartbeat to propagate and a rebalance to be performed.
   sleep 12
@@ -2569,7 +2569,7 @@ test_clustering_remove_raft_node() {
   INCUS_DIR="${INCUS_ONE_DIR}" incus cluster show node4 | grep -q "\- database$"
 
   # The second node is gone from the raft_nodes_table.
-  ! INCUS_DIR="${INCUS_ONE_DIR}" incus admin sql local "SELECT * FROM raft_nodes" | grep -q "10.1.1.102" || false
+  ! INCUS_DIR="${INCUS_ONE_DIR}" incus admin sql local "SELECT * FROM raft_nodes" | grep -q "100.64.1.102" || false
 
   INCUS_DIR="${INCUS_ONE_DIR}" incus admin shutdown
   INCUS_DIR="${INCUS_THREE_DIR}" incus admin shutdown
@@ -2745,13 +2745,13 @@ test_clustering_image_refresh() {
   dir_configure "${INCUS_REMOTE_DIR}"
   INCUS_DIR="${INCUS_REMOTE_DIR}" deps/import-busybox --alias testimage --public
 
-  INCUS_DIR="${INCUS_REMOTE_DIR}" incus config set core.https_address "10.1.1.104:8443"
+  INCUS_DIR="${INCUS_REMOTE_DIR}" incus config set core.https_address "100.64.1.104:8443"
 
   # Add remotes
   token="$(INCUS_DIR="${INCUS_ONE_DIR}" incus config trust add foo --quiet)"
-  incus remote add public "https://10.1.1.104:8443" --accept-certificate --token foo --public
+  incus remote add public "https://100.64.1.104:8443" --accept-certificate --token foo --public
   token="$(INCUS_DIR="${INCUS_ONE_DIR}" incus config trust add foo --quiet)"
-  incus remote add cluster "https://10.1.1.101:8443" --accept-certificate --token "${token}"
+  incus remote add cluster "https://100.64.1.101:8443" --accept-certificate --token "${token}"
 
   INCUS_DIR="${INCUS_REMOTE_DIR}" incus init testimage c1
 
@@ -3444,7 +3444,7 @@ test_clustering_groups() {
   spawn_incus_and_join_cluster "${ns3}" "${bridge}" "${cert}" 3 1 "${INCUS_THREE_DIR}" "${INCUS_ONE_DIR}"
 
   token="$(INCUS_DIR="${INCUS_ONE_DIR}" incus config trust add foo --quiet)"
-  incus remote add cluster --token "${token}" --accept-certificate "https://10.1.1.101:8443"
+  incus remote add cluster --token "${token}" --accept-certificate "https://100.64.1.101:8443"
 
   # Initially, there is only the default group
   incus cluster group show cluster:default
@@ -3899,7 +3899,7 @@ test_clustering_openfga() {
   INCUS_DIR="${INCUS_ONE_DIR}" incus config set "oidc.issuer=http://127.0.0.1:$(cat "${TEST_DIR}/oidc.port")/"
   INCUS_DIR="${INCUS_ONE_DIR}" incus config set "oidc.client.id=device"
 
-  BROWSER=curl incus remote add --accept-certificate oidc-openfga "https://10.1.1.101:8443" --auth-type oidc
+  BROWSER=curl incus remote add --accept-certificate oidc-openfga "https://100.64.1.101:8443" --auth-type oidc
   ! incus_remote info oidc-openfga: | grep -Fq 'core.https_address' || false
 
   run_openfga
@@ -3926,7 +3926,7 @@ test_clustering_openfga() {
   # After the second node has joined there should exist only one authorization model.
   [ "$(fga model list --store-id "${OPENFGA_STORE_ID}" | jq '.authorization_models | length')" = 1 ]
 
-  BROWSER=curl incus remote add --accept-certificate node2 "https://10.1.1.102:8443" --auth-type oidc
+  BROWSER=curl incus remote add --accept-certificate node2 "https://100.64.1.102:8443" --auth-type oidc
   ! incus_remote info node2: | grep -Fq 'core.https_address' || false
 
   # Add self as server admin. Should be able to see config now.

canonical/lxd

(cherry picked from commit 4fff11cc45dae2a88baea0a87d8164ae163c95c2)
Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
License: Apache-2.0
(cherry picked from commit d41fe9a3bc4a419069eceeed2b0b6dab9c37aeca)

test/includes/clustering.sh
@@ -6,9 +6,9 @@ setup_clustering_bridge() {
   echo "==> Setup clustering bridge ${name}"
 
   ip link add "${name}" up type bridge
-  ip addr add 10.1.1.1/16 dev "${name}"
+  ip addr add 100.64.1.1/16 dev "${name}"
 
-  iptables -w -t nat -A POSTROUTING -s 10.1.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
+  iptables -w -t nat -A POSTROUTING -s 100.64.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
   echo 1 > /proc/sys/net/ipv4/ip_forward
 }
 
@@ -18,7 +18,7 @@ teardown_clustering_bridge() {
   if [ -e "/sys/class/net/${name}" ]; then
       echo "==> Teardown clustering bridge ${name}"
       echo 0 > /proc/sys/net/ipv4/ip_forward
-      iptables -w -t nat -D POSTROUTING -s 10.1.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
+      iptables -w -t nat -D POSTROUTING -s 100.64.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
       ip link del dev "${name}"
   fi
 }
@@ -80,8 +80,8 @@ set -e
 ip link set dev lo up
 ip link set dev "${veth2}" name eth0
 ip link set eth0 up
-ip addr add "10.1.1.10${id}/16" dev eth0
-ip route add default via 10.1.1.1
+ip addr add "100.64.1.10${id}/16" dev eth0
+ip route add default via 100.64.1.1
 ip link add localBridge${id} type bridge
 EOF
 }
@@ -134,11 +134,11 @@ spawn_lxd_and_bootstrap_cluster() {
     cat > "${LXD_DIR}/preseed.yaml" <<EOF
 config:
   core.trust_password: sekret
-  core.https_address: 10.1.1.101:8443
+  core.https_address: 100.64.1.101:8443
 EOF
     if [ "${port}" != "" ]; then
       cat >> "${LXD_DIR}/preseed.yaml" <<EOF
-  cluster.https_address: 10.1.1.101:${port}
+  cluster.https_address: 100.64.1.101:${port}
 EOF
     fi
     cat >> "${LXD_DIR}/preseed.yaml" <<EOF
@@ -227,15 +227,15 @@ spawn_lxd_and_join_cluster() {
     # If a custom cluster port was given, we need to first set the REST
     # API address.
     if [ "${port}" != "8443" ]; then
-      lxc config set core.https_address "10.1.1.10${index}:8443"
+      lxc config set core.https_address "100.64.1.10${index}:8443"
     fi
 
     cat > "${LXD_DIR}/preseed.yaml" <<EOF
 cluster:
   enabled: true
   server_name: node${index}
-  server_address: 10.1.1.10${index}:${port}
-  cluster_address: 10.1.1.10${target}:8443
+  server_address: 100.64.1.10${index}:${port}
+  cluster_address: 100.64.1.10${target}:8443
   cluster_certificate: "$cert"
   cluster_password: ${secret}
   member_config:
@@ -323,15 +323,15 @@ spawn_lxd_and_join_cluster_with_token() {
     # If a custom cluster port was given, we need to first set the REST
     # API address.
     if [ "${port}" != "8443" ]; then
-      lxc config set core.https_address "10.1.1.10${index}:8443"
+      lxc config set core.https_address "100.64.1.10${index}:8443"
     fi
 
     cat > "${LXD_DIR}/preseed.yaml" <<EOF
 cluster:
   enabled: true
   server_name: node${index}
-  server_address: 10.1.1.10${index}:${port}
-  cluster_address: 10.1.1.10${target}:8443
+  server_address: 100.64.1.10${index}:${port}
+  cluster_address: 100.64.1.10${target}:8443
   cluster_certificate: "$cert"
   cluster_token: ${token}
   member_config:

test/suites/clustering.sh
@@ -248,8 +248,8 @@ test_clustering_membership() {
   LXD_DIR="${LXD_TWO_DIR}" lxc cluster show node5 | grep -q "node5"
 
   # Client certificate are shared across all nodes.
-  lxc remote add cluster 10.1.1.101:8443 --accept-certificate --password=sekret
-  lxc remote set-url cluster https://10.1.1.102:8443
+  lxc remote add cluster 100.64.1.101:8443 --accept-certificate --password=sekret
+  lxc remote set-url cluster https://100.64.1.102:8443
   lxc network list cluster: | grep -q "${bridge}"
   lxc remote remove cluster
 
@@ -815,7 +815,7 @@ test_clustering_storage() {
 
     # Manually send the join request.
     cert=$(sed ':a;N;$!ba;s/\n/\\n/g' "${LXD_ONE_DIR}/cluster.crt")
-    op=$(curl --unix-socket "${LXD_THREE_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"node3\",\"enabled\":true,\"member_config\":[${member_config}],\"server_address\":\"10.1.1.103:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_password\":\"sekret\"}" | jq -r .operation)
+    op=$(curl --unix-socket "${LXD_THREE_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"node3\",\"enabled\":true,\"member_config\":[${member_config}],\"server_address\":\"100.64.1.103:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_password\":\"sekret\"}" | jq -r .operation)
     curl --unix-socket "${LXD_THREE_DIR}/unix.socket" "lxd${op}/wait"
 
     # Ensure that node-specific config appears on all nodes,
@@ -1866,7 +1866,7 @@ test_clustering_update_cert_token() {
   cmp -s "${LXD_TWO_DIR}/cluster.key" "${key_path}"
 
   # Verify the token with the wrong cert fingerprint is not usable due to the fingerprint mismatch
-  url="https://10.1.1.101:8443"
+  url="https://100.64.1.101:8443"
   ! lxc remote add cluster "${token}" || false
   [ "$(DEBUG="" lxc remote add cluster "${token}" 2>&1)" = "Error: Certificate fingerprint mismatch between certificate token and server \"${url}\"" ]
   ! lxc remote add cluster --token "${token}" "${url}" || false
@@ -1917,16 +1917,16 @@ test_clustering_join_api() {
   # Check a server with the name 'valid' cannot be joined when modifying the token.
   # Therefore replace the valid name in the token with 'none'.
   malicious_token="$(lxc cluster add valid --quiet | base64 -d | jq '.server_name |= "none"' | base64 --wrap=0)"
-  op=$(curl --unix-socket "${LXD_TWO_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"valid\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"10.1.1.102:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${malicious_token}\"}" | jq -r .operation)
+  op=$(curl --unix-socket "${LXD_TWO_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"valid\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"100.64.1.102:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${malicious_token}\"}" | jq -r .operation)
   [ "$(curl --unix-socket "${LXD_TWO_DIR}/unix.socket" "lxd${op}/wait" | jq '.error_code')" = "403" ]
 
   # Check that the server cannot be joined using a valid token by changing it's name to 'none'.
   token="$(lxc cluster add valid2 --quiet)"
-  [ "$(curl --unix-socket "${LXD_TWO_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"none\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"10.1.1.102:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r '.error_code')" = "400" ]
+  [ "$(curl --unix-socket "${LXD_TWO_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"none\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"100.64.1.102:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r '.error_code')" = "400" ]
 
   # Check the server can be joined.
   token="$(lxc cluster add node2 --quiet)"
-  op=$(curl --unix-socket "${LXD_TWO_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"node2\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"10.1.1.102:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r .operation)
+  op=$(curl --unix-socket "${LXD_TWO_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"node2\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"100.64.1.102:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_token\":\"${token}\"}" | jq -r .operation)
   curl --unix-socket "${LXD_TWO_DIR}/unix.socket" "lxd${op}/wait"
 
   LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node2 | grep -q "message: Fully operational"
@@ -1938,10 +1938,10 @@ test_clustering_join_api() {
   LXD_NETNS="${ns3}" spawn_lxd "${LXD_THREE_DIR}" false
 
   # Check the server cannot be joined using password by changing it's name to 'none'.
-  [ "$(curl --unix-socket "${LXD_THREE_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"none\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"10.1.1.103:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_password\":\"sekret\"}" | jq -r '.error_code')" = "400" ]
+  [ "$(curl --unix-socket "${LXD_THREE_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"none\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"100.64.1.103:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_password\":\"sekret\"}" | jq -r '.error_code')" = "400" ]
 
   # Check the server can be joined using password.
-  op=$(curl --unix-socket "${LXD_THREE_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"node3\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"10.1.1.103:8443\",\"cluster_address\":\"10.1.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_password\":\"sekret\"}" | jq -r .operation)
+  op=$(curl --unix-socket "${LXD_THREE_DIR}/unix.socket" -X PUT "lxd/1.0/cluster" -d "{\"server_name\":\"node3\",\"enabled\":true,\"member_config\":[{\"entity\": \"storage-pool\",\"name\":\"data\",\"key\":\"source\",\"value\":\"\"}],\"server_address\":\"100.64.1.103:8443\",\"cluster_address\":\"100.64.1.101:8443\",\"cluster_certificate\":\"${cert}\",\"cluster_password\":\"sekret\"}" | jq -r .operation)
   curl --unix-socket "${LXD_THREE_DIR}/unix.socket" "lxd${op}/wait"
 
   LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node3 | grep -q "message: Fully operational"
@@ -2190,7 +2190,7 @@ test_clustering_address() {
 
   # Add a remote using the core.https_address of the bootstrap node, and check
   # that the REST API is exposed.
-  url="https://10.1.1.101:8443"
+  url="https://100.64.1.101:8443"
   lxc remote add cluster --password sekret --accept-certificate "${url}"
   lxc storage list cluster: | grep -q data
 
@@ -2211,13 +2211,13 @@ test_clustering_address() {
 
   # The core.https_address config value can be changed and the REST API is still
   # accessible.
-  LXD_DIR="${LXD_ONE_DIR}" lxc config set "core.https_address" 10.1.1.101:9999
-  url="https://10.1.1.101:9999"
+  LXD_DIR="${LXD_ONE_DIR}" lxc config set "core.https_address" 100.64.1.101:9999
+  url="https://100.64.1.101:9999"
   lxc remote set-url cluster "${url}"
   lxc storage list cluster:| grep -q data
 
   # The cluster.https_address config value can't be changed.
-  ! LXD_DIR="${LXD_ONE_DIR}" lxc config set "cluster.https_address" "10.1.1.101:8448" || false
+  ! LXD_DIR="${LXD_ONE_DIR}" lxc config set "cluster.https_address" "100.64.1.101:8448" || false
 
   # Create a container using the REST API exposed over core.https_address.
   LXD_DIR="${LXD_ONE_DIR}" deps/import-busybox --alias testimage
@@ -2523,7 +2523,7 @@ test_clustering_fan() {
   LXD_DIR="${LXD_ONE_DIR}" lxc network show "${fanbridge}"
   [ "$(LXD_DIR="${LXD_ONE_DIR}" lxc network get "${fanbridge}" bridge.mode)" = "fan" ]
   [ "$(LXD_DIR="${LXD_ONE_DIR}" lxc network get "${fanbridge}" dns.domain)" = "fantastic" ]
-  [ "$(LXD_DIR="${LXD_ONE_DIR}" lxc network get "${fanbridge}" fan.underlay_subnet)" = "10.1.0.0/16" ]
+  [ "$(LXD_DIR="${LXD_ONE_DIR}" lxc network get "${fanbridge}" fan.underlay_subnet)" = "100.64.0.0/16" ]
 
   echo "Create 2 containers"
   LXD_DIR="${LXD_ONE_DIR}" lxc launch --target node1 testimage c1 -d "${SMALL_ROOT_DISK}" -n "${fanbridge}"
@@ -2593,9 +2593,9 @@ test_clustering_recover() {
   sleep 5
 
   # Check the current database nodes
-  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "10.1.1.101:8443"
-  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "10.1.1.102:8443"
-  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "10.1.1.103:8443"
+  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "100.64.1.101:8443"
+  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "100.64.1.102:8443"
+  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "100.64.1.103:8443"
 
   # Create a test project, just to insert something in the database.
   LXD_DIR="${LXD_ONE_DIR}" lxc project create p1
@@ -2617,8 +2617,8 @@ test_clustering_recover() {
   LXD_DIR="${LXD_ONE_DIR}" lxc project list | grep -qwF p1
 
   # The database nodes have been updated
-  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "10.1.1.101:8443"
-  ! LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "10.1.1.102:8443" || false
+  LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "100.64.1.101:8443"
+  ! LXD_DIR="${LXD_ONE_DIR}" lxd cluster list-database | grep -qF "100.64.1.102:8443" || false
 
   # Cleanup the dead node.
   LXD_DIR="${LXD_ONE_DIR}" lxc cluster remove node2 --force --yes
@@ -2897,7 +2897,7 @@ test_clustering_remove_raft_node() {
   # Remove the second node from the database but not from the raft configuration.
   retries=10
   while [ "${retries}" != "0" ]; do
-    LXD_DIR="${LXD_ONE_DIR}" lxd sql global "DELETE FROM nodes WHERE address = '10.1.1.102:8443'" && break
+    LXD_DIR="${LXD_ONE_DIR}" lxd sql global "DELETE FROM nodes WHERE address = '100.64.1.102:8443'" && break
     sleep 0.5
     retries=$((retries-1))
   done
@@ -2920,11 +2920,11 @@ test_clustering_remove_raft_node() {
   LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node4 | grep -q "\- database$"
 
   # The second node is still in the raft_nodes table.
-  [ "$(LXD_DIR="${LXD_ONE_DIR}" lxd sql local --format csv "SELECT count(*) FROM raft_nodes WHERE address = '10.1.1.102:8443'")" = 1 ]
+  [ "$(LXD_DIR="${LXD_ONE_DIR}" lxd sql local --format csv "SELECT count(*) FROM raft_nodes WHERE address = '100.64.1.102:8443'")" = 1 ]
   [ "$(LXD_DIR="${LXD_ONE_DIR}" lxd sql local --format csv "SELECT count(*) FROM raft_nodes")" = 4 ]
 
   # Force removing the raft node.
-  LXD_DIR="${LXD_ONE_DIR}" lxd cluster remove-raft-node -q "10.1.1.102"
+  LXD_DIR="${LXD_ONE_DIR}" lxd cluster remove-raft-node -q "100.64.1.102"
 
   # Wait for a heartbeat to propagate and a rebalance to be performed.
   sleep 12
@@ -2936,9 +2936,8 @@ test_clustering_remove_raft_node() {
   LXD_DIR="${LXD_ONE_DIR}" lxc cluster show node4 | grep -q "\- database$"
 
   # The second node is gone from the raft_nodes_table.
-  [ "$(LXD_DIR="${LXD_ONE_DIR}" lxd sql local --format csv "SELECT count(*) FROM raft_nodes WHERE address = '10.1.1.102:8443'")" = 0 ]
+  [ "$(LXD_DIR="${LXD_ONE_DIR}" lxd sql local --format csv "SELECT count(*) FROM raft_nodes WHERE address = '100.64.1.102:8443'")" = 0 ]
   [ "$(LXD_DIR="${LXD_ONE_DIR}" lxd sql local --format csv "SELECT count(*) FROM raft_nodes")" = 3 ]
-
   LXD_DIR="${LXD_ONE_DIR}" lxd shutdown
   LXD_DIR="${LXD_THREE_DIR}" lxd shutdown
   LXD_DIR="${LXD_FOUR_DIR}" lxd shutdown
@@ -3101,11 +3100,11 @@ test_clustering_image_refresh() {
   dir_configure "${LXD_REMOTE_DIR}"
   LXD_DIR="${LXD_REMOTE_DIR}" deps/import-busybox --alias testimage --public
 
-  LXD_DIR="${LXD_REMOTE_DIR}" lxc config set core.https_address "10.1.1.104:8443"
+  LXD_DIR="${LXD_REMOTE_DIR}" lxc config set core.https_address "100.64.1.104:8443"
 
   # Add remotes
-  lxc remote add public "https://10.1.1.104:8443" --accept-certificate --password foo --public
-  lxc remote add cluster "https://10.1.1.101:8443" --accept-certificate --password sekret
+  lxc remote add public "https://100.64.1.104:8443" --accept-certificate --password foo --public
+  lxc remote add cluster "https://100.64.1.101:8443" --accept-certificate --password sekret
 
   LXD_DIR="${LXD_REMOTE_DIR}" lxc init testimage c1
 
@@ -3816,7 +3815,7 @@ test_clustering_groups() {
   ns3="${prefix}3"
   spawn_lxd_and_join_cluster "${ns3}" "${bridge}" "${cert}" 3 1 "${LXD_THREE_DIR}"
 
-  lxc remote add cluster --password sekret --accept-certificate "https://10.1.1.101:8443"
+  lxc remote add cluster --password sekret --accept-certificate "https://100.64.1.101:8443"
 
   # Initially, there is only the default group
   lxc cluster group show cluster:default