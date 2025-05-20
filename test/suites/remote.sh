083b0d5dfa39363f9a10937ae85c5447b97102d5
Simon Deziel <simon.deziel@canonical.com>
2025-05-20 06:18:27 -0400

    test/suites/remote: `echo` what's going on in remote_admin
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit a2648c749a41576aa64d1b566e06effd45e95830)

--- test/suites/remote.sh
+++ test/suites/remote.sh
@@ -167,13 +167,15 @@ test_remote_url_with_token() {
 }
 
 test_remote_admin() {
-  echo "Make sure that bad password fail"
+  echo "Verify that a bad password does not succeed in adding remote"
   ! lxc_remote remote add badpass "${LXD_ADDR}" --accept-certificate --password bad || false
   ! lxc_remote list badpass: || false
 
-  lxc_remote remote add badtoken "${LXD_ADDR}" --token badtoken 2>&1 | grep -F "Error: Failed to decode trust token:"
+  echo "Verify error due to bad token and inspect error message"
   OUTPUT="$(! lxc_remote remote add badtoken "${LXD_ADDR}" --token badtoken 2>&1 || false)"
   echo "${OUTPUT}" | grep -F "Error: Failed to decode trust token:"
+
+  echo "Verify that a bad token does not succeed in adding remote"
   ! lxc_remote remote add badtoken "${LXD_ADDR}" --token badtoken || false
   ! lxc_remote remote list | grep -wF badtoken || false
 
