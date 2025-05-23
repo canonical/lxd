974382dd5ca3bdd660893d6cb464e3ccd4b9f284
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-05-23 12:33:36 +0200

    test/main: Add timeout to ceph status command
    In case you have leftover connection details present on your system, the 'ceph status' command
    will hang for a few minutes until it returns which causes unnecessary wait times when facing errors during test execution.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -102,7 +102,7 @@ cleanup() {
   if [ "${TEST_RESULT}" != "success" ]; then
     if command -v ceph >/dev/null; then
       echo "::group::ceph status"
-      ceph status || true
+      ceph status --connect-timeout 5 || true
       echo "::endgroup::"
     fi
 
