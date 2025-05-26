be3e627c6d6b126d3c0e09b377b2194ecc758b42
Din Music <din.music@canonical.com>
2025-05-26 10:00:29 +0000

    lxd/devlxd: Get storage pool endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ lxd/devlxd_storage.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/canonical/lxd/lxd/response"
+	"github.com/canonical/lxd/shared/api"
+)
+
+var devLXDStoragePoolEndpoint = devLXDAPIEndpoint{
+	Path: "storage-pools/{poolName}",
+	Get:  devLXDAPIEndpointAction{Handler: devLXDStoragePoolGetHandler, AccessHandler: allowDevLXDAuthenticated},
+}
+
+func devLXDStoragePoolGetHandler(d *Daemon, r *http.Request) response.Response {
+	inst, err := getInstanceFromContextAndCheckSecurityFlags(r.Context(), devLXDSecurityKey, devLXDSecurityMgmtVolumesKey)
+	if err != nil {
+		return response.DevLXDErrorResponse(err)
+	}
+
+	// Get storage pool.
+	poolName := mux.Vars(r)["poolName"]
+	projectName := inst.Project().Name
+	pool := api.StoragePool{}
+
+	url := api.NewURL().Path("1.0", "storage-pools", poolName).Project(projectName)
+	req, err := NewRequestWithContext(r.Context(), http.MethodGet, url.String(), nil, "")
+	if err != nil {
+		return response.DevLXDErrorResponse(err)
+	}
+
+	resp := storagePoolGet(d, req)
+	etag, err := RenderToStruct(req, resp, &pool)
+	if err != nil {
+		return response.DevLXDErrorResponse(err)
+	}
+
+	// Map to devLXD response.
+	respPool := api.DevLXDStoragePool{
+		Name:   pool.Name,
+		Driver: pool.Driver,
+		Status: pool.Status,
+	}
+
+	return response.DevLXDResponseETag(http.StatusOK, respPool, "json", etag)
+}
