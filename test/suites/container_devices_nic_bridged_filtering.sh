bf453949cf717da54fc132f43c5169242554f655
Simon Deziel <simon.deziel@canonical.com>
2025-05-26 08:39:18 -0400

    test/suites/container_devices_nic_bridged_filtering: double quote variable
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/container_devices_nic_bridged_filtering.sh
+++ test/suites/container_devices_nic_bridged_filtering.sh
@@ -37,7 +37,7 @@ test_container_devices_nic_bridged_filtering() {
   lxc network set "${brName}" bridge.hwaddr 00:11:22:33:44:55
   lxc network set "${brName}" ipv4.address 192.0.2.1/24
   lxc network set "${brName}" ipv6.address 2001:db8:1::1/64
-  [ "$(cat /sys/class/net/${brName}/address)" = "00:11:22:33:44:55" ]
+  [ "$(cat "/sys/class/net/${brName}/address")" = "00:11:22:33:44:55" ]
 
   # Create profile for new containers.
   lxc profile copy default "${ctPrefix}"
