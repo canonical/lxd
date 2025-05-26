62f216df6d48614698788cd1f065fb107d37a5bd
Din Music <din.music@canonical.com>
2025-05-26 10:14:32 +0000

    lxd/devlxd: Create storage pool volume endpoint
    The endpoint allows the CSI to create new volumes on a particular storage pool.
    
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd_storage.go
+++ lxd/devlxd_storage.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -53,11 +54,13 @@ func devLXDStoragePoolGetHandler(d *Daemon, r *http.Request) response.Response {
 var devLXDStoragePoolVolumesEndpoint = devLXDAPIEndpoint{
 	Path: "storage-pools/{poolName}/volumes",
 	Get:  devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumesGetHandler, AccessHandler: allowDevLXDAuthenticated},
+	Post: devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumesPostHandler, AccessHandler: allowDevLXDAuthenticated},
 }
 
 var devLXDStoragePoolVolumesTypeEndpoint = devLXDAPIEndpoint{
 	Path: "storage-pools/{poolName}/volumes/{type}",
 	Get:  devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumesGetHandler, AccessHandler: allowDevLXDAuthenticated},
+	Post: devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumesPostHandler, AccessHandler: allowDevLXDAuthenticated},
 }
 
 func devLXDStoragePoolVolumesGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -128,4 +131,78 @@ func devLXDStoragePoolVolumesGetHandler(d *Daemon, r *http.Request) response.Res
 	}
 
 	return response.DevLXDResponse(http.StatusOK, respVols, "json")
+}
+
+func devLXDStoragePoolVolumesPostHandler(d *Daemon, r *http.Request) response.Response {
+	inst, err := getInstanceFromContextAndCheckSecurityFlags(r.Context(), devLXDSecurityKey, devLXDSecurityMgmtVolumesKey)
+	if err != nil {
+		return response.DevLXDErrorResponse(err)
+	}
+
+	poolName := mux.Vars(r)["poolName"]
+	volType := mux.Vars(r)["type"]
+	projectName := inst.Project().Name
+
+	// Get identity from the request context.
+	identity, err := getDevLXDIdentity(r.Context())
+	if identity == nil {
+		return response.DevLXDErrorResponse(err)
+	}
+
+	// Decode the request body.
+	vol := api.DevLXDStorageVolumesPost{}
+	err = json.NewDecoder(r.Body).Decode(&vol)
+	if err != nil {
+		return response.DevLXDErrorResponse(api.StatusErrorf(http.StatusInternalServerError, "Failed decoding request body: %w", err))
+	}
+
+	if volType == "" {
+		volType = "custom"
+	}
+
+	// Reject non-custom volume type.
+	if storageDrivers.VolumeType(volType) != storageDrivers.VolumeTypeCustom {
+		return response.DevLXDErrorResponse(api.NewStatusError(http.StatusBadRequest, "Only custom storage volumes can be created"))
+	}
+
+	if vol.Type != "" && vol.Type != volType {
+		return response.DevLXDErrorResponse(api.NewStatusError(http.StatusBadRequest, "URL volume type does not match the volume type in body"))
+	}
+
+	if vol.Config == nil {
+		vol.Config = make(map[string]string)
+	}
+
+	// Set caller's identity ID as the volume owner.
+	vol.Config["volatile.devlxd.owner"] = identity.Identifier
+
+	// Create storage volume.
+	reqBody := api.StorageVolumesPost{
+		Name:        vol.Name,
+		Type:        volType,
+		ContentType: vol.ContentType,
+		StorageVolumePut: api.StorageVolumePut{
+			Config:      vol.Config,
+			Description: vol.Description,
+		},
+	}
+
+	url := api.NewURL().Path("1.0", "storage-pools", poolName, "volumes", volType).Project(projectName).WithQuery("recursion", "1")
+	target := r.URL.Query().Get("target")
+	if target != "" {
+		url = url.WithQuery("target", target)
+	}
+
+	req, err := request.NewRequestWithContext(r.Context(), http.MethodPost, url.String(), reqBody, "")
+	if err != nil {
+		return response.DevLXDErrorResponse(err)
+	}
+
+	resp := storagePoolVolumesPost(d, req)
+	err = Render(req, resp)
+	if err != nil {
+		return response.DevLXDErrorResponse(err)
+	}
+
+	return response.DevLXDResponse(http.StatusOK, "", "raw")
 }
