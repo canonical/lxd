2e1f5c6eb8f19a8b5d3377443a25b3cc4fb66593
Din Music <din.music@canonical.com>
2025-05-26 10:00:29 +0000

    lxd/devlxd: Get storage pool endpoint
    The CSI needs to retrieve information about used storage pool to dynamically
    determine the storage pool driver. This information is compared against supported
    storage drivers to let CSI figure out whether the storage pool is local or remote.
    
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd.go
+++ lxd/devlxd.go
@@ -99,6 +99,7 @@ var apiDevLXD = []devLXDAPIEndpoint{
 	devLXDMetadataEndpoint,
 	devLXDEventsEndpoint,
 	devLXDDevicesEndpoint,
+	devLXDStoragePoolEndpoint,
 	devLXDUbuntuProEndpoint,
 	devLXDUbuntuProTokenEndpoint,
 }
