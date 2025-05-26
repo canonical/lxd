91afdd2916af08ba2570f30fac475c10520c8f82
Simon Deziel <simon.deziel@canonical.com>
2025-05-26 09:35:44 -0400

    test/suites/container_move: double quote variables
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 7609f08bb773e24471e78febd0eef14158cd0314)

--- test/suites/container_move.sh
+++ test/suites/container_move.sh
@@ -23,8 +23,8 @@ test_container_move() {
   # Move to different project with same profile (root disk device and profile are retained).
   lxc init "${image}" c1
   lxc move c1 --target-project "${project}"
-  [ "$(lxc ls --project ${project} --format csv --columns n)" = "c1" ]                          # Verify new project.
-  [ "$(lxc config device get c1 root pool --project ${project})" = "${pool}" ]                  # Verify same pool (new local device).
+  [ "$(lxc ls --project "${project}" --format csv --columns n)" = "c1" ]                          # Verify new project.
+  [ "$(lxc config device get c1 root pool --project "${project}")" = "${pool}" ]                  # Verify same pool (new local device).
   [ "$(lxc ls --project "${project}" -c nP -f csv | awk -F, '/c1/ { print $2 }')" = "default" ] # Verify profile is retained.
   lxc delete -f c1 --project "${project}"
 
@@ -34,8 +34,8 @@ test_container_move() {
   lxc profile add c2 "${source_profile}"
   lxc snapshot c2 snap
   lxc move c2 --target-project "${project}" --no-profiles
-  [ "$(lxc ls --project ${project} --format csv --columns n)" = "c2" ]                    # Verify new project.
-  [ "$(lxc config device get c2 root pool --project ${project})" = "${pool}" ]            # Verify same pool (new local device).
+  [ "$(lxc ls --project "${project}" --format csv --columns n)" = "c2" ]                    # Verify new project.
+  [ "$(lxc config device get c2 root pool --project "${project}")" = "${pool}" ]            # Verify same pool (new local device).
   [ "$(lxc ls --project "${project}" -c nP -f csv | awk -F, '/c2/ { print $2 }')" = "" ]  # Verify no profiles are applied.
   lxc delete -f c2 --project "${project}"
   lxc profile delete "${source_profile}"
@@ -43,8 +43,8 @@ test_container_move() {
   # Move to different project with new profiles (root disk device is retained).
   lxc init "${image}" c3
   lxc move c3 --target-project "${project}" --profile "${profile}"
-  [ "$(lxc ls --project ${project} --format csv --columns n)" = "c3" ]         # Verify new project.
-  [ "$(lxc config device get c3 root pool --project ${project})" = "${pool}" ] # Verify same pool (new local device).
+  [ "$(lxc ls --project "${project}" --format csv --columns n)" = "c3" ]         # Verify new project.
+  [ "$(lxc config device get c3 root pool --project "${project}")" = "${pool}" ] # Verify same pool (new local device).
   lxc config show c3 -e --project "${project}" | grep -- "- ${profile}"        # Verify new profile.
   lxc delete -f c3 --project "${project}"
 
@@ -63,22 +63,22 @@ test_container_move() {
   # Move to different project and storage pool.
   lxc init "${image}" c6
   lxc move c6 --target-project "${project}" --storage "${pool2}"
-  [ "$(lxc ls --project ${project} --format csv --columns n)" = "c6" ]          # Verify new project.
-  [ "$(lxc config device get c6 root pool --project ${project})" = "${pool2}" ] # Verify new pool.
+  [ "$(lxc ls --project "${project}" --format csv --columns n)" = "c6" ]          # Verify new project.
+  [ "$(lxc config device get c6 root pool --project "${project}")" = "${pool2}" ] # Verify new pool.
   lxc delete -f c6 --project "${project}"
 
   # Move to different project and overwrite storage pool using device entry.
   lxc init "${image}" c7 --storage "${pool}" --no-profiles
   lxc move c7 --target-project "${project}" --device "root,pool=${pool2}"
-  [ "$(lxc ls --project ${project} --format csv --columns n)" = "c7" ]          # Verify new project.
-  [ "$(lxc config device get c7 root pool --project ${project})" = "${pool2}" ] # Verify new pool.
+  [ "$(lxc ls --project "${project}" --format csv --columns n)" = "c7" ]          # Verify new project.
+  [ "$(lxc config device get c7 root pool --project "${project}")" = "${pool2}" ] # Verify new pool.
   lxc delete -f c7 --project "${project}"
 
   # Move to different project and apply config entry.
   lxc init "${image}" c8
   lxc move c8 --target-project "${project}" --config user.test=success
-  [ "$(lxc ls --project ${project} --format csv --columns n)" = "c8" ]  # Verify new project.
-  [ "$(lxc config get c8 user.test --project ${project})" = "success" ] # Verify new local config entry.
+  [ "$(lxc ls --project "${project}" --format csv --columns n)" = "c8" ]  # Verify new project.
+  [ "$(lxc config get c8 user.test --project "${project}")" = "success" ] # Verify new local config entry.
   lxc delete -f c8 --project "${project}"
 
   lxc profile delete "${profile}" --project "${project}"
