12d938d2c064633086c82730fac78c5498a72d22
Din Music <din.music@canonical.com>
2025-05-27 10:56:48 +0000

    lxd/devlxd_util: Add responseCapture that captures the rendered response
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd_util.go
+++ lxd/devlxd_util.go
@@ -4,8 +4,13 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+
+	"github.com/canonical/lxd/lxd/response"
+	"github.com/canonical/lxd/shared/api"
 )
 
 // NewRequest creates a new HTTP request with the specified method, URL, data, and ETag.
@@ -62,4 +67,102 @@ func NewRequestWithContext(ctx context.Context, method string, url string, data 
 	}
 
 	return req, nil
+}
+
+// responseCapture is a custom http.ResponseWriter that captures the response
+// header, body, and status code for later inspection.
+type responseCapture struct {
+	header     http.Header
+	body       *bytes.Buffer
+	statusCode int
+	url        url.URL
+}
+
+// NewResponseCapture creates a new responseCapture for the given request.
+func NewResponseCapture(req *http.Request) *responseCapture {
+	return &responseCapture{
+		header: make(http.Header),
+		body:   new(bytes.Buffer),
+		url:    *req.URL,
+	}
+}
+
+// Header returns the header map.
+func (r *responseCapture) Header() http.Header {
+	return r.header
+}
+
+// Write writes the data to the buffer.
+func (r *responseCapture) Write(b []byte) (int, error) {
+	return r.body.Write(b)
+}
+
+// WriteHeader sets the response status code.
+func (r *responseCapture) WriteHeader(statusCode int) {
+	r.statusCode = statusCode
+}
+
+// ToAPIResponse decodes the captured response body into an api.Response
+// and retrieves the ETag from the header.
+func (r *responseCapture) ToAPIResponse() (*api.Response, string, error) {
+	// Get the ETag.
+	etag := r.Header().Get("ETag")
+
+	// Decode the response.
+	decoder := json.NewDecoder(r.body)
+	response := api.Response{}
+
+	err := decoder.Decode(&response)
+	if err != nil {
+		// Check the return value for a cleaner error.
+		if r.statusCode != http.StatusOK {
+			return nil, "", fmt.Errorf("Failed to fetch %s: %d", r.url.String(), r.statusCode)
+		}
+
+		return nil, "", err
+	}
+
+	// Handle errors.
+	if response.Type == api.ErrorResponse {
+		return nil, "", api.NewStatusError(r.statusCode, response.Error)
+	}
+
+	return &response, etag, nil
+}
+
+// Render renders the response and returns a potential error.
+func Render(req *http.Request, resp response.Response) error {
+	rc := NewResponseCapture(req)
+	err := resp.Render(rc, req)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = rc.ToAPIResponse()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RenderToStruct renders the response into a struct and returns the ETag.
+func RenderToStruct(req *http.Request, resp response.Response, target any) (etag string, err error) {
+	rc := NewResponseCapture(req)
+	err = resp.Render(rc, req)
+	if err != nil {
+		return "", err
+	}
+
+	apiResp, etag, err := rc.ToAPIResponse()
+	if err != nil {
+		return "", err
+	}
+
+	err = apiResp.MetadataAsStruct(target)
+	if err != nil {
+		return "", err
+	}
+
+	return etag, nil
 }
