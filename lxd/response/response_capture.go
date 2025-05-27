ffdf0dbb8d91991ec9302cac864cc72e3238b215
Din Music <din.music@canonical.com>
2025-05-27 10:56:48 +0000

    lxd/response: Add responseCapture that captures the rendered response
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ lxd/response/response_capture.go
@@ -0,0 +1,71 @@
+package response
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/canonical/lxd/shared/api"
+)
+
+// responseCapture is a custom http.ResponseWriter that captures the response
+// header, body, and status code for later inspection.
+type responseCapture struct {
+	request    *http.Request
+	header     http.Header
+	body       *bytes.Buffer
+	statusCode int
+}
+
+// NewResponseCapture creates a new responseCapture for the given request.
+func NewResponseCapture(req *http.Request) *responseCapture {
+	return &responseCapture{
+		request: req,
+		header:  make(http.Header),
+		body:    new(bytes.Buffer),
+	}
+}
+
+// Header returns the header map.
+func (rc *responseCapture) Header() http.Header {
+	return rc.header
+}
+
+// Write writes the data to the buffer.
+func (rc *responseCapture) Write(b []byte) (int, error) {
+	return rc.body.Write(b)
+}
+
+// WriteHeader sets the response status code.
+func (rc *responseCapture) WriteHeader(statusCode int) {
+	rc.statusCode = statusCode
+}
+
+// ToAPIResponse decodes the captured response body into an api.Response
+// and retrieves the ETag from the header.
+func (rc *responseCapture) ToAPIResponse() (*api.Response, string, error) {
+	// Get the ETag.
+	etag := rc.Header().Get("ETag")
+
+	// Decode the response.
+	decoder := json.NewDecoder(rc.body)
+	response := api.Response{}
+
+	err := decoder.Decode(&response)
+	if err != nil {
+		// Check the return value for a cleaner error.
+		if rc.statusCode != http.StatusOK {
+			return nil, "", fmt.Errorf("Failed to fetch %s: %d", rc.request.URL.String(), rc.statusCode)
+		}
+
+		return nil, "", err
+	}
+
+	// Handle errors.
+	if response.Type == api.ErrorResponse {
+		return nil, "", api.NewStatusError(rc.statusCode, response.Error)
+	}
+
+	return &response, etag, nil
+}
