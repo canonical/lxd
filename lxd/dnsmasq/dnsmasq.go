21673afb55fa9c8c1f94ada61a7aed6cf8670350
Simon Deziel <simon.deziel@canonical.com>
2025-05-29 12:26:39 -0400

    lxd/dnsmasq/dnsmasq: use simpler `for ... range` loops where applicable (modernize)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit bd775da2f80eb1350f5a7fcbb8b39ae696cda5a2)

--- lxd/dnsmasq/dnsmasq.go
+++ lxd/dnsmasq/dnsmasq.go
@@ -133,7 +133,7 @@ func DHCPStaticAllocation(network string, deviceStaticFileName string) (mac net.
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		fields := strings.SplitSeq(scanner.Text(), ",")
-		for _, field := range fields {
+		for field := range fields {
 			// Check if field is IPv4 or IPv6 address.
 			if strings.Count(field, ".") == 3 {
 				IP := net.ParseIP(field)
