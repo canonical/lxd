7627848e82fc09b195e81667f9a878abdf176744
Simon Deziel <simon.deziel@canonical.com>
2025-05-29 13:18:47 -0400

    lxc/cluster: `s/FieldByJsonTag/FieldByJSONTag/g` (revive)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit e2c3f5bc36f4084e5eb9a43dc058c206086b1f0b)

--- lxc/cluster.go
+++ lxc/cluster.go
@@ -374,7 +374,7 @@ func (c *cmdClusterGet) run(cmd *cobra.Command, args []string) error {
 
 	if c.flagIsProperty {
 		w := member.Writable()
-		res, err := getFieldByJsonTag(&w, args[1])
+		res, err := getFieldByJSONTag(&w, args[1])
 		if err != nil {
 			return fmt.Errorf(i18n.G("The property %q does not exist on the cluster member %q: %v"), args[1], resource.name, err)
 		}
@@ -455,7 +455,7 @@ func (c *cmdClusterSet) run(cmd *cobra.Command, args []string) error {
 	if c.flagIsProperty {
 		if cmd.Name() == "unset" {
 			for k := range keys {
-				err := unsetFieldByJsonTag(&writable, k)
+				err := unsetFieldByJSONTag(&writable, k)
 				if err != nil {
 					return fmt.Errorf(i18n.G("Error unsetting property: %v"), err)
 				}
