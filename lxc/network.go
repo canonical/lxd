18a127ab9913913d32a13ae37fc71ceba02851fd
Simon Deziel <simon.deziel@canonical.com>
2025-05-29 13:19:18 -0400

    lxc/network: `s/FieldByJsonTag/FieldByJSONTag/g` (revive)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit bd56097ad58a78ce1cc3f88cfa1d1446dd1565f6)

--- lxc/network.go
+++ lxc/network.go
@@ -857,7 +857,7 @@ func (c *cmdNetworkGet) run(cmd *cobra.Command, args []string) error {
 
 	if c.flagIsProperty {
 		w := resp.Writable()
-		res, err := getFieldByJsonTag(&w, args[1])
+		res, err := getFieldByJSONTag(&w, args[1])
 		if err != nil {
 			return fmt.Errorf(i18n.G("The property %q does not exist on the network %q: %v"), args[1], resource.name, err)
 		}
@@ -1348,7 +1348,7 @@ func (c *cmdNetworkSet) run(cmd *cobra.Command, args []string) error {
 	if c.flagIsProperty {
 		if cmd.Name() == "unset" {
 			for k := range keys {
-				err := unsetFieldByJsonTag(&writable, k)
+				err := unsetFieldByJSONTag(&writable, k)
 				if err != nil {
 					return fmt.Errorf(i18n.G("Error unsetting property: %v"), err)
 				}
