f47bd2fecdb8d96a609ce02cd70820edc6a15528
Din Music <din.music@canonical.com>
2025-06-05 15:29:45 +0000

    lxd: Update usage of cluster.ConnectIfInstanceIsRemote
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/instance_exec.go
+++ lxd/instance_exec.go
@@ -558,7 +558,7 @@ func instanceExecPost(d *Daemon, r *http.Request) response.Response {
 	}
 
 	// Forward the request if the container is remote.
-	client, err := cluster.ConnectIfInstanceIsRemote(s, projectName, name, r, instanceType)
+	client, err := cluster.ConnectIfInstanceIsRemote(r.Context(), s, projectName, name, instanceType)
 	if err != nil {
 		return response.SmartError(err)
 	}
