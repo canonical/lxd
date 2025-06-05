8857139268854397af4f4a1a7bf442089d85b526
Simon Deziel <simon.deziel@canonical.com>
2025-06-04 22:58:03 -0400

    test/main: run `lxd-benchmark` basic tests in `standalone` group
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -410,6 +410,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_exec_exit_code "exec exit code"
     run_test test_concurrent_exec "concurrent exec"
     run_test test_concurrent "concurrent startup"
+    run_test test_lxd_benchmark_basic "lxd-benchmark basic init/start/stop/delete"
     run_test test_shutdown "lxd shutdown sequence"
     run_test test_snapshots "container snapshots"
     run_test test_snap_restore "snapshot restores"
