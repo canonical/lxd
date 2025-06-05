f6cb5b3f78af978a2db522f5492e4bda5bf25aaa
Stanislav Kozina <stanislav.kozina@canonical.com>
2025-06-05 15:09:21 +0200

    test/suites/cloud-init: Add tests for cloud-config
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- test/suites/cloud-init.sh
+++ test/suites/cloud-init.sh
@@ -27,4 +27,63 @@ test_cloud_init() {
   [ -n "${ID6}" ] && [ "${ID6}" != "${ID5}" ]
 
   lxc delete -f c1 c2
+
+  (
+    cd devlxd-client || return
+    # Use -buildvcs=false here to prevent git complaining about untrusted directory when tests are run as root.
+    CGO_ENABLED=0 go build -tags netgo -v -buildvcs=false ./...
+  )
+
+  lxc launch testimage devlxd
+  lxc file push --quiet --mode 0755 "devlxd-client/devlxd-client" devlxd/bin/
+
+  echo "Check that unknown cloud-init format is passed to the instance unmodified"
+  lxc config set devlxd cloud-init.user-data="invalid-yaml"
+  [ "$(lxc exec devlxd -- devlxd-client cloud-init user-data)" = "invalid-yaml" ]
+  lxc config unset devlxd cloud-init.user-data
+
+  echo  "Check that with SSH keys configured, unknown cloud-init format is passed to the instance unmodified"
+  lxc config set devlxd cloud-init.user-data="invalid-yaml"
+  lxc config set devlxd cloud-init.ssh-keys.mykey="root:gh:user1"
+  [ "$(lxc exec devlxd -- devlxd-client cloud-init user-data)" = "invalid-yaml" ]
+  lxc config unset devlxd cloud-init.ssh-keys.mykey
+  lxc config unset devlxd cloud-init.user-data
+
+  echo "Check that configured ssh-keys in user-data do not replace vendor-data with users provided"
+  CLOUD_INIT="#cloud-config
+users:
+  - name: root
+    ssh-import-id: gh:user2"
+
+  lxc config set devlxd cloud-init.vendor-data="${CLOUD_INIT}"
+  lxc config set devlxd cloud-init.ssh-keys.mykey="root:gh:user1"
+  VENDOR_DATA="$(lxc exec devlxd -- devlxd-client cloud-init vendor-data)"
+  grep "gh:user1" <<< "${VENDOR_DATA}"
+  grep "gh:user2" <<< "${VENDOR_DATA}"
+  [ "$(lxc exec devlxd -- devlxd-client cloud-init user-data)" = "not found" ]
+  lxc config unset devlxd cloud-init.ssh-keys.mykey
+  lxc config unset devlxd cloud-init.vendor-data
+
+  echo  "Check that valid jinja template config works with ssh keys configured"
+  CLOUD_INIT="## template: jinja
+#cloud-config
+runcmd:
+  - echo {{devlxd.local_hostname}} > /var/tmp/runcmd_output
+  - echo {{merged_system_cfg._doc}} >> /var/tmp/runcmd_output"
+  lxc config set devlxd cloud-init.user-data="${CLOUD_INIT}"
+  lxc config set devlxd cloud-init.ssh-keys.mykey="root:gh:user1"
+  EXPECTED_CLOUD_INIT="## template: jinja
+#cloud-config
+runcmd:
+- echo {{devlxd.local_hostname}} > /var/tmp/runcmd_output
+- echo {{merged_system_cfg._doc}} >> /var/tmp/runcmd_output
+users:
+- name: root
+  ssh-import-id:
+  - gh:user1 #lxd:cloud-init.ssh-keys
+  ssh_import_id:
+  - gh:user1 #lxd:cloud-init.ssh-keys"
+  [ "$(lxc exec devlxd -- devlxd-client cloud-init user-data)" = "${EXPECTED_CLOUD_INIT}" ]
+
+  lxc delete -f devlxd
 }
