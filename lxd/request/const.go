76c8c17cf15fcff1a22480a74b8cab406fc84df5
Din Music <din.music@canonical.com>
2025-06-09 12:15:56 +0000

    lxd/request: Remove unused auth context key constants
    Signed-off-by: Din Music <din.music@canonical.com>
    (cherry picked from commit 11680dad3130307904da795f7bd4fa5eb9a2600d)

--- lxd/request/const.go
+++ lxd/request/const.go
@@ -17,40 +17,10 @@ const (
 	// CtxConn is the connection field in the request context.
 	CtxConn CtxKey = "conn"
 
-	// CtxAddress is the address field in request context.
-	CtxAddress CtxKey = "address"
-
-	// CtxUsername is the username field in request context.
-	CtxUsername CtxKey = "username"
-
-	// CtxProtocol is the protocol field in request context.
-	CtxProtocol CtxKey = "protocol"
-
-	// CtxIdentityProviderGroups is the identity provider groups field in the request context.
-	// This contains groups defined by the identity provider if the identity authenticated with OIDC.
-	CtxIdentityProviderGroups CtxKey = "identity_provider_groups"
-
-	// CtxForwardedAddress is the forwarded address field in request context.
-	CtxForwardedAddress CtxKey = "forwarded_address"
-
-	// CtxForwardedUsername is the forwarded username field in request context.
-	CtxForwardedUsername CtxKey = "forwarded_username"
-
-	// CtxForwardedProtocol is the forwarded protocol field in request context.
-	CtxForwardedProtocol CtxKey = "forwarded_protocol"
-
-	// CtxForwardedIdentityProviderGroups is the identity provider groups field in the request context.
-	// This contains groups defined by the identity provider if the identity authenticated with OIDC on another cluster
-	// member.
-	CtxForwardedIdentityProviderGroups CtxKey = "identity_provider_groups"
-
 	// CtxEffectiveProjectName is used to indicate that the effective project of a resource is different from the project
 	// specified in the URL. (For example, if a project has `features.networks=false`, any networks in this project actually
 	// belong to the default project).
 	CtxEffectiveProjectName CtxKey = "effective_project_name"
-
-	// CtxTrusted is a boolean value that indicates whether the request was authenticated or not.
-	CtxTrusted CtxKey = "trusted"
 
 	// CtxMetricsCallbackFunc is a callback function that can be called to mark the request as completed for the API metrics.
 	CtxMetricsCallbackFunc CtxKey = "metrics_callback_function"
