39aa585842403157790d72752d31ecea0af1de12
Simon Deziel <simon.deziel@canonical.com>
2025-06-10 11:37:55 -0400

    shared/api/container_backup: align struct tags (tagalign)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- shared/api/container_backup.go
+++ shared/api/container_backup.go
@@ -7,19 +7,19 @@ import (
 // ContainerBackupsPost represents the fields available for a new LXD container backup
 // API extension: container_backup.
 type ContainerBackupsPost struct {
-	Name             string    `json:"name" yaml:"name"`
-	ExpiresAt        time.Time `json:"expires_at" yaml:"expires_at"`
-	ContainerOnly    bool      `json:"container_only" yaml:"container_only"`
+	Name             string    `json:"name"              yaml:"name"`
+	ExpiresAt        time.Time `json:"expires_at"        yaml:"expires_at"`
+	ContainerOnly    bool      `json:"container_only"    yaml:"container_only"`
 	OptimizedStorage bool      `json:"optimized_storage" yaml:"optimized_storage"`
 }
 
 // ContainerBackup represents a LXD container backup
 // API extension: container_backup.
 type ContainerBackup struct {
-	Name             string    `json:"name" yaml:"name"`
-	CreatedAt        time.Time `json:"created_at" yaml:"created_at"`
-	ExpiresAt        time.Time `json:"expires_at" yaml:"expires_at"`
-	ContainerOnly    bool      `json:"container_only" yaml:"container_only"`
+	Name             string    `json:"name"              yaml:"name"`
+	CreatedAt        time.Time `json:"created_at"        yaml:"created_at"`
+	ExpiresAt        time.Time `json:"expires_at"        yaml:"expires_at"`
+	ContainerOnly    bool      `json:"container_only"    yaml:"container_only"`
 	OptimizedStorage bool      `json:"optimized_storage" yaml:"optimized_storage"`
 }
 
