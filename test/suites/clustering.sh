lxc/incus

tests/clustering: Use correct target project argument
Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

test/suites/clustering.sh
@@ -2747,7 +2747,7 @@ test_clustering_image_refresh() {
 
   for project in default foo bar; do
     # Copy the public image to each project
-    INCUS_DIR="${INCUS_ONE_DIR}" incus image copy public:testimage local: --alias testimage --project "${project}"
+    INCUS_DIR="${INCUS_ONE_DIR}" incus image copy public:testimage local: --alias testimage --target-project "${project}"
 
     # Diable autoupdate for testimage in project foo
     if [ "${project}" = "foo" ]; then

canonical/lxd

(cherry picked from commit 8366dfbe8026f75d4c23cb757b90b12f686614fc)
Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
License: Apache-2.0

test/suites/clustering.sh
@@ -3155,7 +3155,7 @@ test_clustering_image_refresh() {
 
   for project in default foo bar; do
     # Copy the public image to each project
-    LXD_DIR="${LXD_ONE_DIR}" lxc image copy public:testimage local: --alias testimage --project "${project}"
+    LXD_DIR="${LXD_ONE_DIR}" lxc image copy public:testimage local: --alias testimage --target-project "${project}"
 
     # Diable autoupdate for testimage in project foo
     if [ "${project}" = "foo" ]; then