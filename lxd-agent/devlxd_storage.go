9d9dde055639409bb7084d1fd1232a25eb59e097
Din Music <din.music@canonical.com>
2025-06-12 10:07:48 +0000

    lxd-agent/devlxd: Update storage pool volume endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd-agent/devlxd_storage.go
+++ lxd-agent/devlxd_storage.go
@@ -117,6 +117,7 @@ func devLXDStoragePoolVolumesPostHandler(d *Daemon, r *http.Request) *devLXDResp
 var devLXDStoragePoolVolumeTypeEndpoint = devLXDAPIEndpoint{
 	Path:   "storage-pools/{pool}/volumes/{type}/{volume}",
 	Get:    devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumeGetHandler},
+	Put:    devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumePutHandler},
 	Delete: devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumeDeleteHandler},
 }
 
@@ -150,6 +151,46 @@ func devLXDStoragePoolVolumeGetHandler(d *Daemon, r *http.Request) *devLXDRespon
 	}
 
 	return okResponseETag(vol, "json", etag)
+}
+
+func devLXDStoragePoolVolumePutHandler(d *Daemon, r *http.Request) *devLXDResponse {
+	poolName, err := url.PathUnescape(mux.Vars(r)["pool"])
+	if err != nil {
+		return errorResponse(http.StatusBadRequest, err.Error())
+	}
+
+	volType, err := url.PathUnescape(mux.Vars(r)["type"])
+	if err != nil {
+		return errorResponse(http.StatusBadRequest, err.Error())
+	}
+
+	volName, err := url.PathUnescape(mux.Vars(r)["volume"])
+	if err != nil {
+		return errorResponse(http.StatusBadRequest, err.Error())
+	}
+
+	etag := r.Header.Get("If-Match")
+
+	var vol api.DevLXDStorageVolumePut
+	err = json.NewDecoder(r.Body).Decode(&vol)
+	if err != nil {
+		return smartResponse(fmt.Errorf("Failed to parse request: %w", err))
+	}
+
+	client, err := getDevLXDVsockClient(d, r)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	client = client.UseTarget(r.URL.Query().Get("target"))
+	defer client.Disconnect()
+
+	err = client.UpdateStoragePoolVolume(poolName, volType, volName, vol, etag)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	return okResponse("", "raw")
 }
 
 func devLXDStoragePoolVolumeDeleteHandler(d *Daemon, r *http.Request) *devLXDResponse {
