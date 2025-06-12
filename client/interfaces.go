7794e866c6142834ac524a886002c1fe92b3f8ee
Din Music <din.music@canonical.com>
2025-06-12 10:49:27 +0000

    client/devlxd: Create storage volume
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -509,6 +509,7 @@ type DevLXDServer interface {
 	// DevLXD storage volumes.
 	GetStoragePoolVolumes(poolName string) (vols []api.DevLXDStorageVolume, err error)
 	GetStoragePoolVolume(poolName string, volType string, volName string) (vol *api.DevLXDStorageVolume, ETag string, err error)
+	CreateStoragePoolVolume(poolName string, vol api.DevLXDStorageVolumesPost) error
 
 	// DevLXD Ubuntu Pro.
 	GetUbuntuPro() (*api.DevLXDUbuntuProSettings, error)
