a06d0def6452f0dc9b153a07db8f1eab44be0c23
Simon Deziel <simon.deziel@canonical.com>
2025-06-14 15:15:42 -0400

    test/includes/setup: don't hardcode path to `busybox` binary
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/setup.sh
+++ test/includes/setup.sh
@@ -16,12 +16,13 @@ ensure_import_testimage() {
     if [ -e "${LXD_TEST_IMAGE:-}" ]; then
         lxc image import "${LXD_TEST_IMAGE}" --alias testimage
     else
-        if [ ! -e "/bin/busybox" ]; then
+        BUSYBOX="$(command -v busybox)"
+        if [ ! -e "${BUSYBOX}" ]; then
             echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
             exit 1
         fi
 
-        if ldd /bin/busybox >/dev/null 2>&1; then
+        if ldd "${BUSYBOX}" >/dev/null 2>&1; then
             echo "The testsuite requires /bin/busybox to be a static binary"
             exit 1
         fi
