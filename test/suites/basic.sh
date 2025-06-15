b4990d54a6f5656b50f6e23bafe8f41c47c043d4
Simon Deziel <simon.deziel@canonical.com>
2025-06-15 17:10:24 -0400

    test/suites/basic: test bulk deletion of ephemeral instances
    The comment hinted at bulk deletion when stopping ephemeral instances but the
    code didn't do that.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 68064851a3794805ccf49b957bc1c5dfe93f5999)
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- test/suites/basic.sh
+++ test/suites/basic.sh
@@ -736,12 +736,15 @@ EOF
   lxc profile delete foo
 
   # Multiple ephemeral instances delete
-  lxc launch testimage c1
-  lxc launch testimage c2
-  lxc launch testimage c3
+  lxc launch testimage c1 --ephemeral
+  lxc launch testimage c2 --ephemeral
+  lxc launch testimage c3 --ephemeral
 
-  lxc delete -f c1 c2 c3
-  [ "$(lxc list -c n)" = "" ]
+  lxc stop -f c1 c2 c3
+  [ "$(lxc list -f csv -c n)" = "" ]
+
+  fingerprint="$(lxc config trust ls --format csv | cut -d, -f4)"
+  lxc config trust remove "${fingerprint}"
 }
 
 test_basic_version() {
