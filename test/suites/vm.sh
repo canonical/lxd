835d03810423edff010722c45407d86052407704
Simon Deziel <simon.deziel@canonical.com>
2025-06-17 17:20:52 -0400

    test/suites/vm: new test script for VM tests
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

---
+++ test/suites/vm.sh
@@ -0,0 +1,19 @@
+test_vm_empty() {
+  if [ "${LXD_VM_TESTS:-0}" = "0" ]; then
+    echo "==> SKIP: VM tests are disabled"
+    return
+  fi
+
+  # Tiny VMs
+  lxc init --vm --empty v1 -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}"
+  lxc start v1
+  lxc delete --force v1
+
+  lxc launch --vm --empty v1 -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}"
+  lxc delete --force v1
+
+  # Ephemeral cleanup
+  lxc launch --vm --empty --ephemeral v1 -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}"
+  lxc stop -f v1
+  [ "$(lxc list -f csv -c n)" = "" ]
+}
