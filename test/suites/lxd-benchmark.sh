3ee39ca6f360605867dbbef82f70429a19fda836
Simon Deziel <simon.deziel@canonical.com>
2025-06-17 10:57:51 -0400

    test/suites/lxd-benchmark: test report generation
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/lxd-benchmark.sh
+++ test/suites/lxd-benchmark.sh
@@ -1,15 +1,26 @@
 test_lxd_benchmark_basic(){
   local count=5
+  local report_file
+  report_file="$(mktemp -p "${TEST_DIR}" XXX)"
+
   ensure_import_testimage
-  lxd-benchmark init --count "${count}" testimage
+
+  lxd-benchmark init --count "${count}" --report-file "${report_file}" testimage
   [ "$(lxc list -f csv -c n STATUS=stopped | grep -cwF benchmark)" = "${count}" ]
-  lxd-benchmark start
+  lxd-benchmark start --report-file "${report_file}"
   [ "$(lxc list -f csv -c n STATUS=stopped)" = "" ]
   [ "$(lxc list -f csv -c n STATUS=running | grep -cwF benchmark)" = "${count}" ]
-  lxd-benchmark stop
+  lxd-benchmark stop --report-file "${report_file}"
   [ "$(lxc list -f csv -c n STATUS=running)" = "" ]
   [ "$(lxc list -f csv -c n STATUS=stopped | grep -cwF benchmark)" = "${count}" ]
-  lxd-benchmark delete
+  lxd-benchmark delete --report-file "${report_file}"
   [ "$(lxc list -f csv -c n)" = "" ]
+
+  # Check the number of lines matches the number of commands + the header line
+  cat "${report_file}"
+  [ "$(wc -l < "${report_file}")" = "5" ]
+
+  # cleanup
+  rm "${report_file}"
 }
 
