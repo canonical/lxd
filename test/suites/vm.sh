7f4a5a43584ea251f22c25cad08bcbabb8d10acd
Simon Deziel <simon.deziel@canonical.com>
2025-06-17 18:57:54 -0400

    test/suites/vm: avoid using old/incompatible `virtiofsd`
    On 22.04, it just hangs:
    
    ```
    timeout --foreground 120 /home/runner/go/bin/lxc start v1 --verbose
    time="2025-06-17T21:42:51Z" level=warning msg="Using writeback cache I/O" devPath=/tmp/lxd-test.tmp.Lj0X/CXb/storage-pools/lxdtest-CXb/virtual-machines/v1/root.img device=root fsType=btrfs instance=v1 instanceType=virtual-machine project=default
    ```
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/vm.sh
+++ test/suites/vm.sh
@@ -4,6 +4,11 @@ test_vm_empty() {
     return
   fi
 
+  if grep -qxF 'VERSION_ID="22.04"' /etc/os-release; then
+    echo "Using migration.stateful to force 9p config drive thus avoiding the old/incompatible virtiofsd"
+    lxc profile set default migration.stateful=true
+  fi
+
   # Tiny VMs
   lxc init --vm --empty v1 -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}"
   lxc start v1
@@ -16,4 +21,9 @@ test_vm_empty() {
   lxc launch --vm --empty --ephemeral v1 -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}"
   lxc stop -f v1
   [ "$(lxc list -f csv -c n)" = "" ]
+
+  if grep -qxF 'VERSION_ID="22.04"' /etc/os-release; then
+    # Cleanup custom changes from the default profile
+    lxc profile unset default migration.stateful
+  fi
 }
