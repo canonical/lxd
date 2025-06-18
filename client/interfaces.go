afad195efc9e02d6831f4e9d1bd466edd32554cf
Din Music <din.music@canonical.com>
2025-06-18 14:59:30 +0000

    client/devlxd: Create storage pool volume snapshot
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -547,6 +547,7 @@ type DevLXDServer interface {
 	// DevLXD storage volume snapshots.
 	GetStoragePoolVolumeSnapshots(poolName string, volType string, volName string) (snapshots []api.DevLXDStorageVolumeSnapshot, err error)
 	GetStoragePoolVolumeSnapshot(poolName string, volType string, volName string, snapshotName string) (snapshot *api.DevLXDStorageVolumeSnapshot, ETag string, err error)
+	CreateStoragePoolVolumeSnapshot(poolName string, volType string, volName string, snapshot api.DevLXDStorageVolumeSnapshotsPost) (op DevLXDOperation, err error)
 
 	// DevLXD operations.
 	GetOperationWait(uuid string, timeout int) (*api.DevLXDOperation, string, error)
