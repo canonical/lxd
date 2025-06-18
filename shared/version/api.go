ac20fd7a851d6d531b7cf372b2008df1e2bffe04
Mark Laing <mark.laing@canonical.com>
2025-06-18 08:19:09 +0100

    api: Add `auth_secrets` API extension.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -454,6 +454,7 @@ var APIExtensions = []string{
 	"oidc_client_secret",
 	"pci_hotplug",
 	"device_patch_removal",
+	"auth_secrets",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
