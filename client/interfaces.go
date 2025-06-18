402e9f797671dcd6c1aa49176dba5282249446e8
Din Music <din.music@canonical.com>
2025-06-18 15:01:11 +0000

    client/devlxd: Delete storage pool volume snapshot
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -548,6 +548,7 @@ type DevLXDServer interface {
 	GetStoragePoolVolumeSnapshots(poolName string, volType string, volName string) (snapshots []api.DevLXDStorageVolumeSnapshot, err error)
 	GetStoragePoolVolumeSnapshot(poolName string, volType string, volName string, snapshotName string) (snapshot *api.DevLXDStorageVolumeSnapshot, ETag string, err error)
 	CreateStoragePoolVolumeSnapshot(poolName string, volType string, volName string, snapshot api.DevLXDStorageVolumeSnapshotsPost) (op DevLXDOperation, err error)
+	DeleteStoragePoolVolumeSnapshot(poolName string, volType string, volName string, snapshotName string) (op DevLXDOperation, err error)
 
 	// DevLXD operations.
 	GetOperationWait(uuid string, timeout int) (*api.DevLXDOperation, string, error)
