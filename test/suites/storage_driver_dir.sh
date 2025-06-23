ac950fa3a374e96a93c74d47ace2421848b94daa
Simon Deziel <simon.deziel@canonical.com>
2025-06-23 14:43:21 -0400

    test/suites/storage_driver_dir: consistently use `` for temp file and dir
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit ce2377f2cfa3c3ef7d55975c5ee9f5ab91d0e0b9)

--- test/suites/storage_driver_dir.sh
+++ test/suites/storage_driver_dir.sh
@@ -13,12 +13,12 @@ do_dir_on_empty_fs() {
   fi
 
   # Create and mount 32MiB ext4 filesystem.
-  tmp_file=$(mktemp "${TEST_DIR}/disk.XXXXXX")
+  tmp_file="$(mktemp -p "${TEST_DIR}" disk.XXX)"
   dd if=/dev/zero of="${tmp_file}" bs=1M count=64
   mkfs.ext4 "${tmp_file}"
 
-  mount_point=$(mktemp -d "${TEST_DIR}/mountpoint.XXXXXX")
-  sudo mount -o loop "${tmp_file}" "${mount_point}"
+  mount_point="$(mktemp -d -p "${TEST_DIR}" mountpoint.XXX)"
+  mount -o loop "${tmp_file}" "${mount_point}"
 
   if [ ! -d "${mount_point}/lost+found" ]; then
     echo "Error: Expected ${mount_point}/lost+found subdirectory to exist"
