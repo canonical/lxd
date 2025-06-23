fc0257ac0154830a5a8c3f67066bb7ba282c239a
Simon Deziel <simon.deziel@canonical.com>
2025-06-23 14:41:04 -0400

    test/suites/storage: cleanup temporary file
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/storage.sh
+++ test/suites/storage.sh
@@ -145,6 +145,7 @@ EOF
       # Check that we can't create a loop file in a non-LXD owned location.
       INVALID_LOOP_FILE="$(mktemp -p "${TEST_DIR}" invalid-loop-file.XXX)"
       ! lxc storage create "lxdtest-$(basename "${LXD_DIR}")-invalid-loop-file" zfs source="${INVALID_LOOP_FILE}" || false
+      rm "${INVALID_LOOP_FILE}"
 
       # Let LXD use an already existing dataset.
       zfs create -p -o mountpoint=none "lxdtest-$(basename "${LXD_DIR}")-pool1/existing-dataset-as-pool"
