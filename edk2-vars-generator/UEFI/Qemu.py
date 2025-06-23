canonical/lxd-pkg-snap

commit 3e4c24ee2b86061181e6f1233e2259ccdc620643

qemu-ovmf-secureboot: Move to edk2-vars-generator
Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

edk2-vars-generator/UEFI/Qemu.py
@@ -1,0 +1,180 @@
+#
+# Copyright 2019-2021 Canonical Ltd.
+# Authors:
+# - dann frazier <dann.frazier@canonical.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import enum
+import os
+import shutil
+import tempfile
+
+
+class QemuEfiMachine(enum.Enum):
+    OVMF_PC = enum.auto()
+    OVMF_Q35 = enum.auto()
+    OVMF32 = enum.auto()
+    AAVMF = enum.auto()
+    AAVMF32 = enum.auto()
+
+
+class QemuEfiVariant(enum.Enum):
+    MS = enum.auto()
+    SECBOOT = enum.auto()
+    SNAKEOIL = enum.auto()
+
+
+class QemuEfiFlashSize(enum.Enum):
+    DEFAULT = enum.auto
+    SIZE_2MB = enum.auto()
+    SIZE_4MB = enum.auto()
+
+
+class QemuCommand:
+    Qemu_Common_Params = [
+        '-no-user-config', '-nodefaults',
+        '-m', '256',
+        '-smp', '1,sockets=1,cores=1,threads=1',
+        '-display', 'none',
+        '-serial', 'stdio',
+    ]
+    Ovmf_Common_Params = Qemu_Common_Params + [
+        '-chardev', 'pty,id=charserial1',
+        '-device', 'isa-serial,chardev=charserial1,id=serial1',
+    ]
+    Aavmf_Common_Params = Qemu_Common_Params + [
+        '-machine', 'virt', '-device', 'virtio-serial-device',
+    ]
+    Machine_Base_Command = {
+        QemuEfiMachine.AAVMF: [
+            'qemu-system-aarch64', '-cpu', 'cortex-a57',
+        ] + Aavmf_Common_Params,
+        QemuEfiMachine.AAVMF32: [
+            'qemu-system-aarch64', '-cpu', 'cortex-a15',
+        ] + Aavmf_Common_Params,
+        QemuEfiMachine.OVMF_PC: [
+            'qemu-system-x86_64', '-machine', 'pc,accel=tcg',
+        ] + Ovmf_Common_Params,
+        QemuEfiMachine.OVMF_Q35: [
+            'qemu-system-x86_64', '-machine', 'q35,accel=tcg',
+        ] + Ovmf_Common_Params,
+        QemuEfiMachine.OVMF32: [
+            'qemu-system-i386', '-machine', 'q35,accel=tcg',
+        ] + Ovmf_Common_Params,
+    }
+
+    def _get_default_flash_paths(self, machine, variant, flash_size):
+        assert(machine in QemuEfiMachine)
+        assert(variant is None or variant in QemuEfiVariant)
+        assert(flash_size in QemuEfiFlashSize)
+
+        code_ext = vars_ext = ''
+        if variant == QemuEfiVariant.MS:
+            code_ext = vars_ext = '.ms'
+        elif variant == QemuEfiVariant.SECBOOT:
+            code_ext = '.secboot'
+        elif variant == QemuEfiVariant.SNAKEOIL:
+            vars_ext = '.snakeoil'
+
+        if machine == QemuEfiMachine.AAVMF:
+            assert(flash_size == QemuEfiFlashSize.DEFAULT)
+            return (
+                f'/usr/share/AAVMF/AAVMF_CODE{code_ext}.fd',
+                f'/usr/share/AAVMF/AAVMF_VARS{code_ext}.fd',
+            )
+        if machine == QemuEfiMachine.AAVMF32:
+            assert(variant is None)
+            assert(flash_size == QemuEfiFlashSize.DEFAULT)
+            return (
+                '/usr/share/AAVMF/AAVMF32_CODE.fd',
+                '/usr/share/AAVMF/AAVMF32_VARS.fd'
+            )
+        if machine == QemuEfiMachine.OVMF32:
+            assert(variant is None or variant in [QemuEfiVariant.SECBOOT])
+            assert(
+                flash_size in [
+                    QemuEfiFlashSize.DEFAULT, QemuEfiFlashSize.SIZE_4MB
+                ]
+            )
+            return (
+                '/usr/share/OVMF/OVMF32_CODE_4M.secboot.fd',
+                '/usr/share/OVMF/OVMF32_VARS_4M.fd',
+            )
+        # Remaining possibilities are OVMF variants
+        if machine == QemuEfiMachine.OVMF_PC:
+            assert(variant is None)
+        if variant == QemuEfiVariant.SNAKEOIL:
+            # We provide one size - you don't get to pick.
+            assert(flash_size == QemuEfiFlashSize.DEFAULT)
+        size_ext = '' if flash_size == QemuEfiFlashSize.SIZE_2MB else '_4M'
+        return (
+            f'/usr/share/OVMF/OVMF_CODE{size_ext}{code_ext}.fd',
+            f'/usr/share/OVMF/OVMF_VARS{size_ext}{vars_ext}.fd'
+        )
+
+    def __init__(
+            self, machine, variant=None,
+            code_path=None, vars_template_path=None,
+            flash_size=QemuEfiFlashSize.DEFAULT,
+    ):
+        assert(
+            (code_path and vars_template_path) or
+            (not code_path and not vars_template_path)
+        )
+
+        if not code_path:
+            (code_path, vars_template_path) = self._get_default_flash_paths(
+                machine, variant, flash_size)
+
+        self.pflash = self.PflashParams(code_path, vars_template_path)
+        self.command = self.Machine_Base_Command[machine] + self.pflash.params
+        if variant in [QemuEfiVariant.MS, QemuEfiVariant.SECBOOT] and \
+           flash_size == QemuEfiFlashSize.SIZE_2MB:
+            # 2MB images have 64-bit PEI that does not support S3 w/ SMM
+            self.command.extend(['-global', 'ICH9-LPC.disable_s3=1'])
+
+    def add_disk(self, path):
+        self.command = self.command + [
+            '-drive', 'file=%s,format=raw' % (path)
+        ]
+
+    def add_oem_string(self, type, string):
+        string = string.replace(",", ",,")
+        self.command = self.command + [
+            '-smbios', f'type={type},value={string}'
+        ]
+
+    class PflashParams:
+        '''
+        Used to generate the appropriate -pflash arguments for QEMU. Mostly
+        used as a fancy way to generate a per-instance vars file and have it
+        be automatically cleaned up when the object is destroyed.
+        '''
+        def __init__(self, code_path, vars_template_path):
+            with tempfile.NamedTemporaryFile(delete=False) as varfile:
+                self.varfile_path = varfile.name
+                with open(vars_template_path, 'rb') as template:
+                    shutil.copyfileobj(template, varfile)
+                self.params = [
+                    '-drive',
+                    'file=%s,if=pflash,format=raw,unit=0,readonly=on' %
+                    (code_path),
+                    '-drive',
+                    'file=%s,if=pflash,format=raw,unit=1,readonly=off' %
+                    (varfile.name)
+                ]
+
+        def __del__(self):
+            os.unlink(self.varfile_path)

edk2-vars-generator/UEFI/Filesystems.py
@@ -1,0 +1,101 @@
+#
+# Copyright 2019-2022 Canonical Ltd.
+# Authors:
+# - dann frazier <dann.frazier@canonical.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import shutil
+import subprocess
+import tempfile
+
+
+class FatFsImage:
+    def __init__(self, size_in_mb):
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            self.path = f.name
+
+        subprocess.check_call(
+            [
+                'dd', 'if=/dev/zero', 'of=%s' % (self.path),
+                'count=0', 'bs=1M', 'seek=%d' % (size_in_mb), 'status=none'
+            ]
+        )
+        new_env = os.environ.copy()
+        new_env['PATH'] = f"{os.environ['PATH']}:/sbin"
+        subprocess.check_call(['mkdosfs', '-F', '32', self.path], env=new_env)
+
+    def __del__(self):
+        os.unlink(self.path)
+
+    def mkdir(self, dir):
+        subprocess.run(['mmd', '-i', self.path, dir])
+
+    def makedirs(self, dir):
+        dirs = dir.split(os.path.sep)
+        for dir_idx in range(1, len(dirs)+1):
+            next_dir = os.path.sep.join(dirs[:dir_idx])
+            self.mkdir(next_dir)
+
+    def insert_file(self, src, dest):
+        subprocess.check_call(
+            [
+                'mcopy', '-i', self.path, src, '::%s' % (dest)
+            ]
+        )
+
+
+class EfiBootableIsoImage:
+    def __init__(self, eltorito_img):
+        with tempfile.TemporaryDirectory() as iso_root:
+            eltorito_iso_root = 'boot'
+            eltorito_iso_path = os.path.join(eltorito_iso_root, 'efi.img')
+            eltorito_local_root = os.path.join(iso_root, eltorito_iso_root)
+            eltorito_local_path = os.path.join(iso_root, eltorito_iso_path)
+
+            os.makedirs(eltorito_local_root)
+            shutil.copyfile(eltorito_img.path, eltorito_local_path)
+
+            with tempfile.NamedTemporaryFile(delete=False) as f:
+                self.path = f.name
+
+            subprocess.check_call(
+                [
+                    'xorriso', '-as', 'mkisofs', '-J', '-l',
+                    '-c', 'boot/boot.cat',
+                    '-partition_offset', '16', '-append_partition', '2',
+                    '0xef', eltorito_local_path,
+                    '-e', '--interval:appended_partition_2:all::',
+                    '-no-emul-boot', '-o', self.path, iso_root
+                ]
+            )
+
+    def __del__(self):
+        os.unlink(self.path)
+
+
+class GrubShellBootableIsoImage(EfiBootableIsoImage):
+    def __init__(self, efi_arch, shim_path, grub_path):
+        efi_img = FatFsImage(64)
+        efi_img.makedirs(os.path.join('EFI', 'BOOT'))
+        removable_media_path = os.path.join(
+            'EFI', 'BOOT', 'BOOT%s.EFI' % (efi_arch.upper())
+        )
+        grub_dest = os.path.join(
+            'EFI', 'BOOT', 'GRUB%s.EFI' % (efi_arch.upper())
+        )
+        efi_img.insert_file(shim_path, removable_media_path)
+        efi_img.insert_file(grub_path, grub_dest)
+        super().__init__(efi_img)

edk2-vars-generator/UEFI/SignedBinary.py
@@ -1,0 +1,52 @@
+#
+# Copyright 2022 Canonical Ltd.
+# Authors:
+# - dann frazier <dann.frazier@canonical.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import subprocess
+import tempfile
+
+
+class SignedBinary:
+    def __init__(self, binary_path, key_path, cert_path, password=None):
+        openssl_password_args = []
+        if password:
+            openssl_password_args = [
+                "-passin", f"pass:{password}"
+            ]
+        with tempfile.NamedTemporaryFile() as keytmp:
+            subprocess.check_call(
+                [
+                    "openssl", "rsa",
+                ] + openssl_password_args + [
+                    "-in", f"{key_path}",
+                    "-out", f"{keytmp.name}",
+                ]
+            )
+            with tempfile.NamedTemporaryFile(delete=False) as f:
+                self.path = f.name
+
+            subprocess.check_call(
+                [
+                    "sbsign", "--key", f"{keytmp.name}",
+                    "--cert", f"{cert_path}",
+                    binary_path, "--output", f"{self.path}"
+                ]
+            )
+
+    def __del__(self):
+        os.unlink(self.path)

edk2-vars-generator/edk2-vars-generator
@@ -1,0 +1,140 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Canonical Ltd.
+# Authors:
+# - dann frazier <dann.frazier@canonical.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import argparse
+import os.path
+import pexpect
+import shutil
+import sys
+from UEFI.Filesystems import FatFsImage, EfiBootableIsoImage
+from UEFI.Qemu import QemuEfiMachine, QemuEfiVariant, QemuEfiFlashSize
+from UEFI import Qemu
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", "--flavor", help="UEFI Flavor",
+        choices=['AAVMF', 'OVMF', 'OVMF_4M'],
+        required=True,
+    )
+    parser.add_argument(
+        "-e", "--enrolldefaultkeys",
+        help='Path to "EnrollDefaultKeys" EFI binary',
+        required=True,
+    )
+    parser.add_argument(
+        "-s", "--shell",
+        help='Path to "Shell" EFI binary',
+        required=True,
+    )
+    parser.add_argument(
+        "-C", "--certificate",
+        help='base64-encoded PK/KEK1 certificate',
+        required=True,
+    )
+    parser.add_argument(
+        "-c", "--code",
+        help='UEFI code image',
+        required=True,
+    )
+    parser.add_argument(
+        "--no-default",
+        action="store_true",
+        help='Do not enroll the default keys, just the PK/KEK1 certificate',
+    )
+    parser.add_argument(
+        "-V", "--vars-template",
+        help='UEFI vars template',
+        required=True,
+    )
+    parser.add_argument(
+        "-o", "--out-file",
+        help="Output file for generated vars template",
+        required=True,
+    )
+    parser.add_argument("-d", "--debug", action="store_true",
+                        help="Emit debug messages")
+    args = parser.parse_args()
+
+    FlavorConfig = {
+        'AAVMF': {
+            'EfiArch': 'AA64',
+            'QemuCommand': Qemu.QemuCommand(
+                QemuEfiMachine.AAVMF,
+                code_path=args.code,
+                vars_template_path=args.vars_template,
+            ),
+        },
+        'OVMF': {
+            'EfiArch': 'X64',
+            'QemuCommand': Qemu.QemuCommand(
+                QemuEfiMachine.OVMF_Q35,
+                variant=QemuEfiVariant.SECBOOT,
+                flash_size=QemuEfiFlashSize.SIZE_2MB,
+                code_path=args.code,
+                vars_template_path=args.vars_template,
+            ),
+        },
+        'OVMF_4M': {
+            'EfiArch': 'X64',
+            'QemuCommand': Qemu.QemuCommand(
+                QemuEfiMachine.OVMF_Q35,
+                variant=QemuEfiVariant.SECBOOT,
+                flash_size=QemuEfiFlashSize.SIZE_2MB,
+                code_path=args.code,
+                vars_template_path=args.vars_template,
+            ),
+        },
+    }
+
+    eltorito = FatFsImage(64)
+    eltorito.makedirs(os.path.join('EFI', 'BOOT'))
+    removable_media_path = os.path.join(
+        'EFI', 'BOOT', f"BOOT{FlavorConfig[args.flavor]['EfiArch']}.EFI"
+    )
+    eltorito.insert_file(args.shell, removable_media_path)
+    eltorito.insert_file(
+        args.enrolldefaultkeys,
+        args.enrolldefaultkeys.split(os.path.sep)[-1]
+    )
+    iso = EfiBootableIsoImage(eltorito)
+
+    q = FlavorConfig[args.flavor]['QemuCommand']
+    q.add_disk(iso.path)
+    q.add_oem_string(11, args.certificate)
+
+    child = pexpect.spawn(' '.join(q.command))
+    if args.debug:
+        child.logfile = sys.stdout.buffer
+    child.expect(['Press .* or any other key to continue'], timeout=60)
+    child.sendline('\x1b')
+    child.expect(['Shell> '])
+    child.sendline('FS0:\r')
+    child.expect(['FS0:\\\\> '])
+    enrollcmd = ['EnrollDefaultKeys.efi']
+    if args.no_default:
+        enrollcmd.append("--no-default")
+    child.sendline(f'{" ".join(enrollcmd)}\r')
+    child.expect(['FS0:\\\\> '])
+    # Clear the BootOrder. See #1015759
+    child.sendline('setvar BootOrder =\r')
+    child.expect(['FS0:\\\\> '])
+    child.sendline('reset -s\r')
+    child.wait()
+    shutil.copy(q.pflash.varfile_path, args.out_file)

snapcraft.yaml
@@ -841,11 +841,12 @@ parts:
     after:
       - edk2
       - qemu
-    source: https://github.com/puiterwijk/qemu-ovmf-secureboot
-    source-type: git
-    source-depth: 1
+    source: edk2-vars-generator
     plugin: nil
     build-packages:
+      - dosfstools
+      - mtools
+      - python3-pexpect
       - xorriso
     override-build: |-
       [ "$(uname -m)" != "x86_64" ] && [ "$(uname -m)" != "aarch64" ] && exit 0
@@ -854,33 +855,15 @@ parts:
       export LD_LIBRARY_PATH="${CRAFT_STAGE}/lib:${CRAFT_STAGE}/lib/${ARCH}"
 
       set -ex
-      rm -Rf iso-root vfat-root shell.iso
-      mkdir -p iso-root vfat-root/efi/boot
-      cp ../../edk2/build/Build/*/*/*/Shell.efi vfat-root/efi/boot/bootx64.efi
-      cp ../../edk2/build/Build/*/*/*/EnrollDefaultKeys.efi vfat-root/
-      "${CRAFT_STAGE}/bin/qemu-img" convert --image-opts driver=vvfat,floppy=on,fat-type=12,label=UEFI_SHELL,dir=vfat-root iso-root/shell.img
-      xorriso --as mkisofs -input-charset ASCII -J -rational-rock -e shell.img -no-emul-boot -o shell.iso iso-root/
-
-      # Basic aarch64 support
-      if [ "$(uname -m)" = "aarch64" ]; then
-          sed -i ovmf-vars-generator \
-              -e "s/'-machine', machinetype,/'-machine', 'virt', '-cpu', 'cortex-a57',/" \
-              -e "/charserial1/d" \
-              -e "s/ide-cd/scsi-cd/" \
-              -e "s/'-device',$/'-device', 'virtio-scsi-pci,id=scsi', '-device',/"
-      elif [ "$(uname -m)" = "x86_64" ]; then
-          cp -f "${CRAFT_STAGE}/share/qemu/kvmvapic.bin" .
-      fi
 
       mkdir -p "${CRAFT_PART_INSTALL}/share/qemu/"
-      python3 ovmf-vars-generator \
-        --qemu-binary "${CRAFT_STAGE}/bin/qemu-system-$(uname -m)" \
-        --print-output --disable-smm --skip-testing \
-        --oem-string "$(cat ${CRAFT_PROJECT_DIR}/snapcraft/etc/ubuntu-sb.crt)" \
-        --ovmf-binary "${CRAFT_STAGE}/share/qemu/OVMF_CODE.fd" \
-        --ovmf-template-vars "${CRAFT_STAGE}/share/qemu/OVMF_VARS.fd" \
-        --uefi-shell-iso shell.iso \
-        "${CRAFT_PART_INSTALL}/share/qemu/OVMF_VARS.ms.fd"
+      ./edk2-vars-generator -f OVMF \
+        -e ../../edk2/build/Build/*/*/*/EnrollDefaultKeys.efi \
+        -s ../../edk2/build/Build/*/*/*/Shell.efi \
+        -c "${CRAFT_STAGE}/share/qemu/OVMF_CODE.fd" \
+        -V "${CRAFT_STAGE}/share/qemu/OVMF_VARS.fd" \
+        -C "$(cat ubuntu-sb.crt)" \
+        -o "${CRAFT_PART_INSTALL}/share/qemu/OVMF_VARS.ms.fd"
     prime:
       - share/qemu/*
 

canonical/lxd

commit 1db5d97812f68c5ce0f805fe175ce43ab9bf7ce8

edk2-vars-generator/UEFI/Qemu.py: correct QemuEfiFlashSize
Add missing parentheses to avoid an error

    TypeError: unable to increment <class 'enum.auto'>

when running ./edk2-vars-generator --help.

Fixes: 3e4c24ee2b86 ("qemu-ovmf-secureboot: Move to edk2-vars-generator")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

edk2-vars-generator/UEFI/Qemu.py
@@ -37,7 +37,7 @@ class QemuEfiVariant(enum.Enum):
 
 
 class QemuEfiFlashSize(enum.Enum):
-    DEFAULT = enum.auto
+    DEFAULT = enum.auto()
     SIZE_2MB = enum.auto()
     SIZE_4MB = enum.auto()
 