502909870f414f944ab6de11b705fbc8d95b24ff
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-06-24 15:06:03 +0200

    lxd/instance/instancetype: Remove device specific IsRootDiskDevice filter
    Instead use the dedicated filter package.
    For historical reasons the comment is moved too.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/instance/instancetype/instance.go
+++ lxd/instance/instancetype/instance.go
@@ -75,20 +75,6 @@ func ValidSnapName(snapshotName string) error {
 	return nil
 }
 
-// IsRootDiskDevice returns true if the given device representation is configured as root disk for
-// an instance. It typically get passed a specific entry of api.Instance.Devices.
-func IsRootDiskDevice(device map[string]string) bool {
-	// Root disk devices also need a non-empty "pool" property, but we can't check that here
-	// because this function is used with clients talking to older servers where there was no
-	// concept of a storage pool, and also it is used for migrating from old to new servers.
-	// The validation of the non-empty "pool" property is done inside the disk device itself.
-	if device["type"] == "disk" && device["path"] == "/" && device["source"] == "" {
-		return true
-	}
-
-	return false
-}
-
 // ErrNoRootDisk means there is no root disk device found.
 var ErrNoRootDisk = errors.New("No root device could be found")
 
