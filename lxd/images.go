80c3175be88fe581e2145ed34dc28917abf91ba5
Thomas Parrott <thomas.parrott@canonical.com>
2025-06-26 12:56:00 +0100

    lxd/images: Allow binary image upload content types for imagesPost
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/images.go
+++ lxd/images.go
@@ -65,7 +65,7 @@ var imagesCmd = APIEndpoint{
 	MetricsType: entity.TypeImage,
 
 	Get:  APIEndpointAction{Handler: imagesGet, AllowUntrusted: true},
-	Post: APIEndpointAction{Handler: imagesPost, AllowUntrusted: true},
+	Post: APIEndpointAction{Handler: imagesPost, AllowUntrusted: true, ContentTypes: []string{"application/json", "application/octet-stream", "multipart/form-data"}},
 }
 
 var imageCmd = APIEndpoint{
