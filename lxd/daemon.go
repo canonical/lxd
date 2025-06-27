b75c05477bf744f1493fd185fcc0a5a929f7d997
Kadin Sayani <kadin.sayani@canonical.com>
2025-06-27 09:32:50 -0600

    lxd: Add update cluster link volatile addresses daily cluster task
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -2070,6 +2070,9 @@ func (d *Daemon) startClusterTasks() {
 	// Perform automatic evacuation for offline cluster members
 	d.clusterTasks.Add(autoHealClusterTask(d.State))
 
+	// Update cluster link volatile addresses (daily).
+	d.clusterTasks.Add(autoUpdateClusterLinkVolatileAddressesTask(d.State))
+
 	// Start all background tasks
 	d.clusterTasks.Start(d.shutdownCtx)
 }
