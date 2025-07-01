a9df1c88cbc5207aa390a88d27554da57371baec
Simon Deziel <simon.deziel@canonical.com>
2025-07-01 08:15:38 -0400

    test/suites/container_devices_nic_ipvlan: take LXC `network_ipvlan` feature for granted
    It is available in LXD's `4.0/stable` snap so it should be safe.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit bb57dc1082840ff95cd41ce6d4375a2831c51d15)

--- test/suites/container_devices_nic_ipvlan.sh
+++ test/suites/container_devices_nic_ipvlan.sh
@@ -2,11 +2,6 @@ test_container_devices_nic_ipvlan() {
   ensure_import_testimage
   ensure_has_localhost_remote "${LXD_ADDR}"
 
-  if ! lxc info | grep 'network_ipvlan: "true"' ; then
-    echo "==> SKIP: No IPVLAN support"
-    return
-  fi
-
   ctName="nt$$"
   ipRand=$(shuf -i 0-9 -n 1)
 
