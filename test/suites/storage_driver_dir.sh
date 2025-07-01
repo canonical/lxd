34c6d81fab73f68da2546ce6664bb5538d63cef4
Simon Deziel <simon.deziel@canonical.com>
2025-07-01 15:32:48 -0400

    test/suites/storage_driver_dir: skip early if not supported
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/storage_driver_dir.sh
+++ test/suites/storage_driver_dir.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 test_storage_driver_dir() {
-  do_dir_on_empty_fs
-}
-
-do_dir_on_empty_fs() {
   local lxd_backend
 
   lxd_backend=$(storage_backend "${LXD_DIR}")
   if [ "${lxd_backend}" != "dir" ]; then
+    echo "==> SKIP: test_storage_driver_dir only supports 'dir', not ${lxd_backend}"
     return
   fi
 
+  do_dir_on_empty_fs
+}
+
+do_dir_on_empty_fs() {
   # Create and mount a small ext4 filesystem.
   tmp_file="$(mktemp -p "${TEST_DIR}" disk.XXX)"
   fallocate -l 64MiB "${tmp_file}"
