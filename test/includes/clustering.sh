c98d3405d044642e91556fd3ed13325f4dbd18cb
Simon Deziel <simon.deziel@canonical.com>
2025-07-03 16:42:21 -0400

    test/includes: move `is_uuid_v4()` to `clustering`
    That is the only user and it also allow dropping the now obsolete `shell=sh`
    directive.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/clustering.sh
+++ test/includes/clustering.sh
@@ -301,3 +301,9 @@ respawn_lxd_cluster_member() {
 
   LXD_NETNS="${ns}" respawn_lxd "${LXD_DIR}" true
 }
+
+is_uuid_v4() {
+  # Case insensitive match for a v4 UUID. The third group must start with 4, and the fourth group must start with 8, 9,
+  # a, or b. This accounts for the version and variant. See https://datatracker.ietf.org/doc/html/rfc9562#name-uuid-version-4.
+  printf '%s' "${1}" | grep --ignore-case '^[0-9a-f]\{8\}-[0-9a-f]\{4\}-4[0-9a-f]\{3\}-[89ab][0-9a-f]\{3\}-[0-9a-f]\{12\}$'
+}
