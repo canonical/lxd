3e59e1e9781fb8f97b3cf012056530728b0480fe
Mark Laing <mark.laing@canonical.com>
2025-07-07 08:10:57 +0100

    test/suites: Add tests for public image handling.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -471,6 +471,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_image_import_existing_alias "import existing image from alias"
     run_test test_image_refresh "image refresh"
     run_test test_image_acl "image acl"
+    run_test test_images_public "public images"
     run_test test_cloud_init "cloud-init"
     run_test test_exec "exec"
     run_test test_exec_exit_code "exec exit code"
