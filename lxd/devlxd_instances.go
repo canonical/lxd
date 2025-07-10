8b247ebb3520a5757721484a60f2a90fb38f5ced
Din Music <din.music@canonical.com>
2025-07-10 12:10:05 +0000

    lxd/devlxd: Helper function to extract owned devices
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd_instances.go
+++ lxd/devlxd_instances.go
@@ -3,9 +3,28 @@ package main
 import (
 	"github.com/canonical/lxd/lxd/device/filters"
 	"github.com/canonical/lxd/lxd/instance"
+	"github.com/canonical/lxd/shared/api"
 )
 
 type devLXDDeviceAccessValidator func(device map[string]string) bool
+
+// getOwnedDevices extracts instance devices that are owned by the provided identity.
+// Two maps are returned, one containing owned devices, and the other containing the remaining
+// devices.
+func getOwnedDevices(inst api.Instance, identityID string) (ownedDevices map[string]map[string]string, otherDevices map[string]map[string]string) {
+	ownedDevices = make(map[string]map[string]string)
+	otherDevices = make(map[string]map[string]string)
+
+	for name, device := range inst.Devices {
+		if inst.Config["volatile."+name+".devlxd.owner"] == identityID {
+			ownedDevices[name] = device
+		} else {
+			otherDevices[name] = device
+		}
+	}
+
+	return ownedDevices, otherDevices
+}
 
 // newDevLXDDeviceAccessValidator returns a device validator function that
 // checks if the given device is accessible by the devLXD.
