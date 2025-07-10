f9ef0bd979146ce79403401dd13421d43d703f9a
Din Music <din.music@canonical.com>
2025-07-10 12:24:48 +0000

    lxd-agent/devlxd: Get instance endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd-agent/devlxd.go
+++ lxd-agent/devlxd.go
@@ -59,6 +59,7 @@ var devLXDEndpoints = []devLXDAPIEndpoint{
 	devLXDEventsEndpoint,
 	devLXDDevicesEndpoint,
 	devLXDImageExportEndpoint,
+	devLXDInstanceEndpoint,
 	devLXDOperationsWaitEndpoint,
 	devLXDStoragePoolEndpoint,
 	devLXDStoragePoolVolumeTypeEndpoint,
