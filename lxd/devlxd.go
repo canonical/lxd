5487b6d53be7851048c403c4c22ec593408999a2
Din Music <din.music@canonical.com>
2025-07-10 12:05:20 +0000

    lxd/devlxd: Helper function to check instance security flags
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd.go
+++ lxd/devlxd.go
@@ -721,23 +721,32 @@ func allowDevLXDPermission(entityType entity.Type, entitlement auth.Entitlement,
 	}
 }
 
-// getInstanceFromContextAndCheckSecurityFlags checks if the instance has the provided devLXD security features enabled.
+// getInstanceFromContextAndCheckSecurityFlags retrieves the instance from the provided request
+// context and verifies that the instance has the provided devLXD security features enabled.
 func getInstanceFromContextAndCheckSecurityFlags(ctx context.Context, keys ...DevLXDSecurityKey) (instance.Instance, error) {
 	inst, err := request.GetContextValue[instance.Instance](ctx, request.CtxDevLXDInstance)
 	if err != nil {
 		return nil, err
 	}
 
-	config := inst.ExpandedConfig()
+	if !hasInstanceSecurityFeatures(inst.ExpandedConfig(), keys...) {
+		return nil, api.NewGenericStatusError(http.StatusForbidden)
+	}
+
+	return inst, nil
+}
+
+// hasInstanceSecurityFeatures checks whether the instance has the provided devLXD security features enabled.
+func hasInstanceSecurityFeatures(expandedConfig map[string]string, keys ...DevLXDSecurityKey) bool {
 	for _, key := range keys {
-		value := config[string(key)]
+		value := expandedConfig[string(key)]
 
 		// The devLXD is enabled by default, therefore we only prevent access if the feature
 		// is explicitly disabled (set to "false"). All other features must be explicitly enabled.
 		if shared.IsFalse(value) || (value == "" && key != devLXDSecurityKey) {
-			return nil, api.NewGenericStatusError(http.StatusForbidden)
+			return false
 		}
 	}
 
-	return inst, nil
+	return true
 }
