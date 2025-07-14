c38a47874d95e100d711fc7b066543295cff025e
Stanislav Kozina <stanislav.kozina@canonical.com>
2025-07-14 15:22:11 +0200

    lxd/rsync: Introduce CopyFile() to rsync single file
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>
    (cherry picked from commit 1f40de5122b707f74081305e83b2c048ff11830d)

--- lxd/rsync/rsync.go
+++ lxd/rsync/rsync.go
@@ -64,8 +64,7 @@ func rsync(args ...string) (string, error) {
 	return stdout.String(), nil
 }
 
-// LocalCopy copies a directory using rsync (with the --devices option).
-func LocalCopy(source string, dest string, bwlimit string, xattrs bool, rsyncArgs ...string) (string, error) {
+func runRsync(source string, dest string, bwlimit string, xattrs bool, rsyncArgs ...string) (string, error) {
 	err := os.MkdirAll(dest, 0755)
 	if err != nil {
 		return "", err
@@ -104,7 +103,7 @@ func LocalCopy(source string, dest string, bwlimit string, xattrs bool, rsyncArg
 
 	args = append(args,
 		rsyncVerbosity,
-		shared.AddSlash(source),
+		source,
 		dest)
 
 	msg, err := rsync(args...)
@@ -123,6 +122,16 @@ func LocalCopy(source string, dest string, bwlimit string, xattrs bool, rsyncArg
 	}
 
 	return msg, nil
+}
+
+// LocalCopy copies a directory using rsync (with the --devices option).
+func LocalCopy(source string, dest string, bwlimit string, xattrs bool, rsyncArgs ...string) (string, error) {
+	return runRsync(shared.AddSlash(source), dest, bwlimit, xattrs, rsyncArgs...)
+}
+
+// CopyFile copies a single file using rsync (with the --devices option).
+func CopyFile(source string, dest string, bwlimit string, xattrs bool, rsyncArgs ...string) (string, error) {
+	return runRsync(strings.TrimSuffix(source, "/"), dest, bwlimit, xattrs, rsyncArgs...)
 }
 
 // Send sets up the sending half of an rsync, to recursively send the
