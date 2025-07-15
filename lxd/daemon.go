912c471fe4f98e239ef6cce71f30982bda9ba878
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-07-15 15:08:53 +0200

    lxd: Update invocations of networkStop
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit 90ed1c7ee8511746a544da065de08db1b88bab75)

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -2134,7 +2134,7 @@ func (d *Daemon) Stop(ctx context.Context, sig os.Signal) error {
 			}
 
 			// Stop networks.
-			networkStop(s)
+			networkStop(s, false)
 
 			// Unmount daemon image and backup volumes if set.
 			logger.Info("Stopping daemon storage volumes")
