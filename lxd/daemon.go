9cf623460fbd13c928d72102563c42203976084e
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-07-15 15:05:32 +0200

    lxd/networks: Rename networkingStop to networkStop
    This is to be consistent with networkStartup.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -2181,7 +2181,7 @@ func (d *Daemon) Stop(ctx context.Context, sig os.Signal) error {
 			}
 
 			// Stop networks.
-			networkingStop(s)
+			networkStop(s)
 
 			// Unmount daemon image and backup volumes if set.
 			logger.Info("Stopping daemon storage volumes")
