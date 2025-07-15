5f80bef175ead5efcd1467f7ee749f00fc1d1abc
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-07-15 12:25:59 +0200

    lxd: Update invocations of networkStartup
    When starting the networks pass information whether or not the network should be fully started
    or just requires to be restored.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit c5e124b9b40fcac0a1ec1dcd8149693351560d3e)

--- lxd/api_cluster.go
+++ lxd/api_cluster.go
@@ -426,7 +426,7 @@ func clusterPutBootstrap(d *Daemon, r *http.Request, req api.ClusterPut) respons
 		}
 
 		// Restart the networks (to pickup forkdns and the like).
-		err = networkStartup(d.State)
+		err = networkStartup(d.State, false)
 		if err != nil {
 			return err
 		}
@@ -856,7 +856,7 @@ func clusterPutJoin(d *Daemon, r *http.Request, req api.ClusterPut) response.Res
 
 		// Start up networks so any post-join changes can be applied now that we have a Node ID.
 		logger.Debug("Starting networks after cluster join")
-		err = networkStartup(d.State)
+		err = networkStartup(d.State, false)
 		if err != nil {
 			logger.Errorf("Failed starting networks: %v", err)
 		}
@@ -3511,8 +3511,8 @@ func restoreClusterMember(d *Daemon, r *http.Request, mode string) response.Resp
 
 		metadata := make(map[string]any)
 
-		// Restart the networks.
-		err = networkStartup(d.State)
+		// Restore the networks.
+		err = networkStartup(d.State, true)
 		if err != nil {
 			return err
 		}
