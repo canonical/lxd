bb7d099134ec762c16cbc29e3f5702ed5cc05ba0
Kadin Sayani <kadin.sayani@canonical.com>
2025-07-15 09:52:18 -0600

    tests: Move instance to a new project and location
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- test/suites/clustering_move.sh
+++ test/suites/clustering_move.sh
@@ -65,7 +65,11 @@ test_clustering_move() {
 
   # c1 can be moved to a new target project.
   lxc move cluster:c1 --target-project test-project
-  lxc move cluster:c1 --target-project default --project test-project
+
+  # c1 can be moved to a new target project and location.
+  lxc move cluster:c1 --target node3 --target-project default --project test-project
+  lxc info cluster:c1 | grep -xF "Location: node3"
+  lxc query cluster:/1.0/instances/c1 | jq -re '.project == "default"'
 
   lxc move cluster:c1 --target @foobar1
   lxc info cluster:c1 | grep -xF "Location: node1"
