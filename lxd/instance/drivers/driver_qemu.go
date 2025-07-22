20f65057fccf454563b6e4f740ba0d4e4351fe92
Thomas Parrott <thomas.parrott@canonical.com>
2025-07-22 08:51:36 +0100

    lxd/instance/drivers/driver/qemu: Simplify multifunction mode selection in addDriveConfig
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -4229,10 +4229,7 @@ func (d *qemu) addDriveConfig(busAllocate busAllocator, bootIndexes map[string]i
 		// Populate the qemu device with port info.
 		qemuDev["bus"] = devBus
 		qemuDev["addr"] = devAddr
-
-		if multi {
-			qemuDev["multifunction"] = true
-		}
+		qemuDev["multifunction"] = multi
 	}
 
 	if bootIndexes != nil {
