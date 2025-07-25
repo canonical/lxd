327d2f467c35aa69e9befd0ea0298a253e903374
Simon Deziel <simon.deziel@canonical.com>
2025-07-25 10:25:29 -0400

    lxd/sys/os: mention that `AppArmorVersion` is nil if `AppArmorAvailable` is false
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/sys/os.go
+++ lxd/sys/os.go
@@ -110,7 +110,7 @@ type OS struct {
 
 	// Version info
 	KernelVersion   version.DottedVersion
-	AppArmorVersion *version.DottedVersion
+	AppArmorVersion *version.DottedVersion // AppArmorVersion is nil if AppArmorAvailable is false.
 }
 
 // DefaultOS returns a fresh uninitialized OS instance with default values.
