bad1f717d8b2f2af163fd792714b1fbe05d7da25
Thomas Parrott <thomas.parrott@canonical.com>
2025-07-28 10:50:13 +0100

    lxd/instance/drivers/driver/qemu/bus: Adds busDeviceVolatileSuffix bus order number suffix constant
    Going to be used for storing volatile device bus order number values.
    
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu_bus.go
+++ lxd/instance/drivers/driver_qemu_bus.go
@@ -8,6 +8,7 @@ const busFunctionGroupNone = ""           // Add a non multi-function port.
 const busFunctionGroupGeneric = "generic" // Add multi-function port to generic group (used for internal devices).
 const busFunctionGroupConfig = "config"   // Add multi-function port to config group (used for config shares).
 const busDevicePortPrefix = "qemu_pcie"   // Prefix used for name of PCIe ports.
+const busDeviceVolatileSuffix = ".bus"    // Suffix used for volatile device bus number record.
 
 // busAllocator is a function signature used for allocating PCI(e) slots in QEMU.
 // There are different implementations for pre-start QEMU config file generation and for hotplugging of devices.
