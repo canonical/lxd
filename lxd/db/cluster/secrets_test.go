3750972e15d1b10f9c0bf9b36d9b4af60aff6293
Mark Laing <mark.laing@canonical.com>
2025-08-06 17:34:12 +0100

    lxd/db/cluster: Add unit tests for secret rotation.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/secrets_test.go
+++ lxd/db/cluster/secrets_test.go
@@ -2,345 +2,78 @@ package cluster
 
 import (
 	"context"
-	"database/sql/driver"
-	"fmt"
-	"github.com/canonical/lxd/shared/entity"
-	"github.com/stretchr/testify/assert"
+	"database/sql"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestAuthSecretValue_Scan(t *testing.T) {
-	type args struct {
-		value any
-	}
-	tests := []struct {
-		name    string
-		s       AuthSecretValue
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, tt.s.Scan(tt.args.value), fmt.Sprintf("Scan(%v)", tt.args.value))
-		})
-	}
-}
+func TestAuthSecrets(t *testing.T) {
+	// Test setup
+	db := newDB(t)
+	doTx := func(f func(ctx context.Context, tx *sql.Tx)) {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 
-func TestAuthSecretValue_ScanText(t *testing.T) {
-	type args struct {
-		str string
-	}
-	tests := []struct {
-		name    string
-		s       AuthSecretValue
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, tt.s.ScanText(tt.args.str), fmt.Sprintf("ScanText(%v)", tt.args.str))
-		})
-	}
-}
+		tx, err := db.Begin()
+		require.NoError(t, err)
 
-func TestAuthSecretValue_String(t *testing.T) {
-	tests := []struct {
-		name string
-		s    AuthSecretValue
-		want string
-	}{
-		// TODO: Add test cases.
+		f(ctx, tx)
+		require.NoError(t, tx.Commit())
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, tt.s.String(), "String()")
-		})
-	}
-}
 
-func TestAuthSecretValue_Validate(t *testing.T) {
-	tests := []struct {
-		name    string
-		s       AuthSecretValue
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, tt.s.Validate(), fmt.Sprintf("Validate()"))
-		})
-	}
-}
+	// Create two secrets, secret1 is the first to be created (two months ago), secret two is the second to be created
+	// (one month ago).
+	now := time.Now().UTC()
+	secret1 := newAuthSecret()
+	secret1.CreationDate = now.AddDate(0, -2, 0)
+	secret2 := newAuthSecret()
+	secret2.CreationDate = now.AddDate(0, -1, 0)
 
-func TestAuthSecretValue_Value(t *testing.T) {
-	tests := []struct {
-		name    string
-		s       AuthSecretValue
-		want    driver.Value
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.s.Value()
-			if !tt.wantErr(t, err, fmt.Sprintf("Value()")) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "Value()")
-		})
-	}
-}
+	doTx(func(ctx context.Context, tx *sql.Tx) {
+		id, err := createCoreAuthSecret(ctx, tx, secret1)
+		require.NoError(t, err)
+		secret1.ID = id
 
-func TestAuthSecret_Validate(t *testing.T) {
-	type fields struct {
-		ID           int
-		Value        AuthSecretValue
-		CreationDate time.Time
-	}
-	type args struct {
-		expiry string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := AuthSecret{
-				ID:           tt.fields.ID,
-				Value:        tt.fields.Value,
-				CreationDate: tt.fields.CreationDate,
-			}
-			tt.wantErr(t, s.Validate(tt.args.expiry), fmt.Sprintf("Validate(%v)", tt.args.expiry))
-		})
-	}
-}
+		id, err = createCoreAuthSecret(ctx, tx, secret2)
+		require.NoError(t, err)
+		secret2.ID = id
+	})
 
-func TestAuthSecrets_Rotate(t *testing.T) {
-	type args struct {
-		ctx context.Context
-		tx  *sql.Tx
-	}
-	tests := []struct {
-		name    string
-		s       AuthSecrets
-		args    args
-		want    AuthSecrets
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.s.Rotate(tt.args.ctx, tt.args.tx)
-			if !tt.wantErr(t, err, fmt.Sprintf("Rotate(%v, %v)", tt.args.ctx, tt.args.tx)) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "Rotate(%v, %v)", tt.args.ctx, tt.args.tx)
-		})
-	}
-}
+	// Create an AuthSecrets slice with the most recent secret first.
+	authSecrets := AuthSecrets{secret2, secret1}
 
-func TestAuthSecrets_Validate(t *testing.T) {
-	type args struct {
-		expiry string
-	}
-	tests := []struct {
-		name    string
-		s       AuthSecrets
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, tt.s.Validate(tt.args.expiry), fmt.Sprintf("Validate(%v)", tt.args.expiry))
-		})
-	}
-}
+	// Check expiry validation
+	err := authSecrets.Validate("20d")
+	require.Error(t, err)
+	require.Equal(t, "Secret has expired", err.Error())
 
-func TestGetCoreAuthSecrets(t *testing.T) {
-	type args struct {
-		ctx context.Context
-		tx  *sql.Tx
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    AuthSecrets
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetCoreAuthSecrets(tt.args.ctx, tt.args.tx)
-			if !tt.wantErr(t, err, fmt.Sprintf("GetCoreAuthSecrets(%v, %v)", tt.args.ctx, tt.args.tx)) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "GetCoreAuthSecrets(%v, %v)", tt.args.ctx, tt.args.tx)
-		})
-	}
-}
+	// Rotate the secrets
+	var rotatedSecrets AuthSecrets
+	doTx(func(ctx context.Context, tx *sql.Tx) {
+		var err error
+		rotatedSecrets, err = authSecrets.Rotate(ctx, tx)
+		require.NoError(t, err)
+	})
 
-func TestSecretType_Scan(t *testing.T) {
-	type args struct {
-		value any
-	}
-	tests := []struct {
-		name    string
-		s       SecretType
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, tt.s.Scan(tt.args.value), fmt.Sprintf("Scan(%v)", tt.args.value))
-		})
-	}
-}
+	// Expect the rotated secrets to have length two, and for the most recent secret to be second in the list.
+	require.Len(t, rotatedSecrets, 2)
+	require.Equal(t, rotatedSecrets[1], authSecrets[0])
 
-func TestSecretType_ScanInteger(t *testing.T) {
-	type args struct {
-		code int64
-	}
-	tests := []struct {
-		name    string
-		s       SecretType
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, tt.s.ScanInteger(tt.args.code), fmt.Sprintf("ScanInteger(%v)", tt.args.code))
-		})
-	}
-}
+	// Get the current secrets from the database.
+	var dbSecrets AuthSecrets
+	doTx(func(ctx context.Context, tx *sql.Tx) {
+		var err error
+		dbSecrets, err = GetCoreAuthSecrets(ctx, tx)
+		require.NoError(t, err)
+	})
 
-func TestSecretType_Value(t *testing.T) {
-	tests := []struct {
-		name    string
-		s       SecretType
-		want    driver.Value
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.s.Value()
-			if !tt.wantErr(t, err, fmt.Sprintf("Value()")) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "Value()")
-		})
-	}
-}
-
-func Test_createCoreAuthSecret(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		tx     *sql.Tx
-		secret AuthSecret
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, createCoreAuthSecret(tt.args.ctx, tt.args.tx, tt.args.secret), fmt.Sprintf("createCoreAuthSecret(%v, %v, %v)", tt.args.ctx, tt.args.tx, tt.args.secret))
-		})
-	}
-}
-
-func Test_createSecret(t *testing.T) {
-	type args struct {
-		ctx        context.Context
-		tx         *sql.Tx
-		entityType entity.Type
-		entityID   int
-		secretType SecretType
-		value      any
-		createdAt  time.Time
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, createSecret(tt.args.ctx, tt.args.tx, tt.args.entityType, tt.args.entityID, tt.args.secretType, tt.args.value, tt.args.createdAt), fmt.Sprintf("createSecret(%v, %v, %v, %v, %v, %v, %v)", tt.args.ctx, tt.args.tx, tt.args.entityType, tt.args.entityID, tt.args.secretType, tt.args.value, tt.args.createdAt))
-		})
-	}
-}
-
-func Test_deleteSecretsByID(t *testing.T) {
-	type args struct {
-		ctx context.Context
-		tx  *sql.Tx
-		ids []int
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.wantErr(t, deleteSecretsByID(tt.args.ctx, tt.args.tx, tt.args.ids...), fmt.Sprintf("deleteSecretsByID(%v, %v, %v)", tt.args.ctx, tt.args.tx, tt.args.ids...))
-		})
-	}
-}
-
-func Test_newAuthSecret(t *testing.T) {
-	tests := []struct {
-		name string
-		want AuthSecret
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, newAuthSecret(), "newAuthSecret()")
-		})
-	}
-}
-
-func Test_newAuthSecretValue(t *testing.T) {
-	tests := []struct {
-		name string
-		want AuthSecretValue
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, newAuthSecretValue(), "newAuthSecretValue()")
-		})
+	// These should exactly match, in order, the rotated secrets.
+	require.Len(t, dbSecrets, 2)
+	for i := range dbSecrets {
+		require.Equal(t, rotatedSecrets[i].ID, dbSecrets[i].ID)
+		require.Equal(t, rotatedSecrets[i].Value.String(), dbSecrets[i].Value.String())
+		require.Equal(t, rotatedSecrets[i].CreationDate.String(), dbSecrets[i].CreationDate.String())
 	}
 }
