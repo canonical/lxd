662edf201c9402445649f890a1ba1f886e024f00
Kadin Sayani <kadin.sayani@canonical.com>
2025-08-06 09:27:29 -0600

    lxd/identity: Remove unused util functions
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/identity/util.go
+++ lxd/identity/util.go
@@ -1,41 +1,11 @@
 package identity
 
 import (
-	"fmt"
 	"net/http"
 	"slices"
 
 	"github.com/canonical/lxd/shared/api"
 )
-
-// IsFineGrainedIdentityType returns true if permissions of the identity type are managed via group membership.
-func IsFineGrainedIdentityType(identityType string) bool {
-	return slices.Contains([]string{api.IdentityTypeOIDCClient, api.IdentityTypeCertificateClient, api.IdentityTypeCertificateClientPending}, identityType)
-}
-
-// IsRestrictedIdentityType returns whether the given identity is restricted or not. Identity types that are not
-// restricted have full access to LXD. An error is returned if the identity type is not recognised.
-func IsRestrictedIdentityType(identityType string) (bool, error) {
-	_, err := AuthenticationMethodFromIdentityType(identityType)
-	if err != nil {
-		return false, err
-	}
-
-	return !slices.Contains([]string{api.IdentityTypeCertificateClientUnrestricted, api.IdentityTypeCertificateServer}, identityType), nil
-}
-
-// AuthenticationMethodFromIdentityType returns the authentication method corresponding to the given identity type. All
-// identity types must correspond to an authentication method. An error is returned if the identity type is not recognised.
-func AuthenticationMethodFromIdentityType(identityType string) (string, error) {
-	switch identityType {
-	case api.IdentityTypeCertificateClientRestricted, api.IdentityTypeCertificateClientUnrestricted, api.IdentityTypeCertificateServer, api.IdentityTypeCertificateMetricsRestricted, api.IdentityTypeCertificateMetricsUnrestricted, api.IdentityTypeCertificateClient:
-		return api.AuthenticationMethodTLS, nil
-	case api.IdentityTypeOIDCClient:
-		return api.AuthenticationMethodOIDC, nil
-	}
-
-	return "", fmt.Errorf("Identity type %q not recognized", identityType)
-}
 
 // ValidateAuthenticationMethod returns an api.StatusError with http.StatusBadRequest if the given authentication
 // method is not recognised.
