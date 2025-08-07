36ddd33354112279fd73a5eee18b380dfb0022ad
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-08-07 17:19:07 +0200

    lxd/storage/backend: Move vol type lookup down
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/storage/backend_lxd.go
+++ lxd/storage/backend_lxd.go
@@ -7559,8 +7559,6 @@ func (b *lxdBackend) detectUnknownInstanceVolume(vol *drivers.Volume, projectVol
 		}
 	}
 
-	volType := vol.Type()
-
 	if backupConf.Instance == nil {
 		return fmt.Errorf("Instance on volume %q has no instance information in its backup file", vol.Name())
 	}
@@ -7583,6 +7581,8 @@ func (b *lxdBackend) detectUnknownInstanceVolume(vol *drivers.Volume, projectVol
 			return fmt.Errorf("Instance %q in project %q has pool driver mismatch in its backup file (%q doesn't match's pool's %q)", instName, projectName, rootVolPool.Driver, b.Driver().Name())
 		}
 	}
+
+	volType := vol.Type()
 
 	apiInstType, err := VolumeTypeToAPIInstanceType(volType)
 	if err != nil {
