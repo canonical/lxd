5f38dc154ce5b48f0b82c2cffa736bd52301cf10
Kadin Sayani <kadin.sayani@canonical.com>
2025-08-08 13:34:59 -0600

    lxd/db/cluster: Use `(Type).IsFineGrained()` for query construction
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    (cherry picked from commit bed5015163034d9120a062673a946be0fd220235)

--- lxd/db/cluster/entity_type_identity.go
+++ lxd/db/cluster/entity_type_identity.go
@@ -3,11 +3,24 @@ package cluster
 import (
 	"fmt"
 
+	"github.com/canonical/lxd/lxd/db/query"
+	"github.com/canonical/lxd/lxd/identity"
 	"github.com/canonical/lxd/shared/api"
 )
 
 // entityTypeIdentity implements entityTypeDBInfo for an Identity.
 type entityTypeIdentity struct{}
+
+// identityTypes returns the list of identity type codes that are considered fine-grained.
+func (e entityTypeIdentity) identityTypes() (types []int64) {
+	for _, t := range identity.Types() {
+		if t.IsFineGrained() {
+			types = append(types, t.Code())
+		}
+	}
+
+	return types
+}
 
 func (e entityTypeIdentity) code() int64 {
 	return entityTypeCodeIdentity
@@ -15,26 +28,20 @@ func (e entityTypeIdentity) code() int64 {
 
 func (e entityTypeIdentity) allURLsQuery() string {
 	return fmt.Sprintf(`
-SELECT 
-	%d, 
-	identities.id, 
-	'', 
-	'', 
+SELECT
+	%d,
+	identities.id,
+	'',
+	'',
 	json_array(
-		CASE identities.auth_method
-			WHEN %d THEN '%s'
-			WHEN %d THEN '%s'
-		END,
+		%s,
 		identities.identifier
-	) 
-FROM identities
-WHERE type IN (%d, %d, %d)
-`,
-		e.code(),
-		authMethodTLS, api.AuthenticationMethodTLS,
-		authMethodOIDC, api.AuthenticationMethodOIDC,
-		identityTypeOIDCClient, identityTypeCertificateClient, identityTypeCertificateClientPending,
 	)
+FROM identities
+WHERE type IN %s`,
+		e.code(),
+		authMethodCaseClause(),
+		query.IntParams(e.identityTypes()...))
 }
 
 func (e entityTypeIdentity) urlsByProjectQuery() string {
@@ -47,19 +54,15 @@ func (e entityTypeIdentity) urlByIDQuery() string {
 
 func (e entityTypeIdentity) idFromURLQuery() string {
 	return fmt.Sprintf(`
-SELECT ?, identities.id 
-FROM identities 
-WHERE '' = ? 
-	AND '' = ? 
-	AND CASE identities.auth_method 
-		WHEN %d THEN '%s' 
-		WHEN %d THEN '%s' 
-	END = ? 
+SELECT ?, identities.id
+FROM identities
+WHERE '' = ?
+	AND '' = ?
+	AND %s = ?
 	AND identities.identifier = ?
-	AND identities.type IN (%d, %d, %d)
-`, authMethodTLS, api.AuthenticationMethodTLS,
-		authMethodOIDC, api.AuthenticationMethodOIDC,
-		identityTypeOIDCClient, identityTypeCertificateClient, identityTypeCertificateClientPending,
+	AND identities.type IN %s`,
+		authMethodCaseClause(),
+		query.IntParams(e.identityTypes()...),
 	)
 }
 
@@ -70,12 +73,20 @@ func (e entityTypeIdentity) onDeleteTriggerSQL() (name string, sql string) {
 CREATE TRIGGER %s
 	AFTER DELETE ON identities
 	BEGIN
-	DELETE FROM auth_groups_permissions 
-		WHERE entity_type IN (%d, %d) 
+	DELETE FROM auth_groups_permissions
+		WHERE entity_type IN (%d, %d)
 		AND entity_id = OLD.id;
 	DELETE FROM warnings
 		WHERE entity_type_code IN (%d, %d)
 		AND entity_id = OLD.id;
 	END
 `, name, e.code(), typeCertificate.code(), e.code(), typeCertificate.code())
+}
+
+// authMethodCaseClause returns the SQL CASE clause for auth method mapping.
+func authMethodCaseClause() string {
+	return fmt.Sprintf(`CASE identities.auth_method
+		WHEN %d THEN '%s'
+		WHEN %d THEN '%s'
+	END`, authMethodTLS, api.AuthenticationMethodTLS, authMethodOIDC, api.AuthenticationMethodOIDC)
 }
