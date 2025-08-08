d9f8f741e3f99d3885b97e90c0214e49f24f8e09
Kadin Sayani <kadin.sayani@canonical.com>
2025-08-08 08:02:47 -0600

    lxd/identity: Add `LegacyCertificateType()` to `Type` interface and its common implementation
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/identity/type_common.go
+++ lxd/identity/type_common.go
@@ -1,5 +1,11 @@
 package identity
 
+import (
+	"errors"
+
+	"github.com/canonical/lxd/lxd/certificate"
+)
+
 // typeInfoCommon is a common implementation of the [Type] interface.
 type typeInfoCommon struct{}
 
@@ -21,4 +27,10 @@ func (typeInfoCommon) IsFineGrained() bool {
 // IsPending returns false by default.
 func (typeInfoCommon) IsPending() bool {
 	return false
+}
+
+// LegacyCertificateType returns the legacy certificate type for this identity type.
+// If (-1, error) is returned, it indicates that the identity type does not correspond to a legacy certificate type.
+func (typeInfoCommon) LegacyCertificateType() (certificate.Type, error) {
+	return -1, errors.New("Identity type is not a certificate")
 }
