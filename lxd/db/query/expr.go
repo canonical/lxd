9dad685fee0b7fb5aad2afe910558f7692a91f49
Kadin Sayani <kadin.sayani@canonical.com>
2025-08-12 09:56:05 -0600

    lxd/db/query: Add `IntParams()` util function
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/db/query/expr.go
+++ lxd/db/query/expr.go
@@ -3,6 +3,7 @@
 package query
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -16,4 +17,20 @@ func Params(n int) string {
 	}
 
 	return "(" + strings.Join(tokens, ", ") + ")"
+}
+
+// IntParams returns a parameters expression with the given integer(s).
+// E.g. IntParams(1, 2, 3) -> "(1, 2, 3)". Useful for IN expressions and VALUES expressions.
+func IntParams[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](args ...T) string {
+	strs := make([]string, 0, len(args))
+	for _, a := range args {
+		if a > 0 {
+			strs = append(strs, strconv.FormatUint(uint64(a), 10))
+			continue
+		}
+
+		strs = append(strs, strconv.FormatInt(int64(a), 10))
+	}
+
+	return "(" + strings.Join(strs, ", ") + ")"
 }
