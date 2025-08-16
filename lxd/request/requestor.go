b5d302b0978f1fd2f3f3eadb61351f23611389a4
Mark Laing <mark.laing@canonical.com>
2025-08-16 09:43:06 +0100

    lxd/request: Add methods to the requestor.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 440bb28db74071acb2ede7074e26bc7b6ddc17e5)

--- lxd/request/requestor.go
+++ lxd/request/requestor.go
@@ -2,9 +2,14 @@ package request
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"net/url"
+	"slices"
 
 	"github.com/canonical/lxd/lxd/identity"
+	"github.com/canonical/lxd/shared"
+	"github.com/canonical/lxd/shared/api"
 )
 
 // RequestorArgs contains information that is gathered when the requestor is initially authenticated.
@@ -41,6 +46,121 @@ type Requestor struct {
 	forwardedIdentityProviderGroups []string
 	identity                        *identity.CacheEntry
 	identityType                    identity.Type
+}
+
+// IsTrusted returns true if the caller is authenticated and false otherwise.
+func (r *Requestor) IsTrusted() bool {
+	return r.trusted
+}
+
+// IsAdmin returns true if the caller is an administrator and false otherwise.
+func (r *Requestor) IsAdmin() bool {
+	if slices.Contains([]string{ProtocolUnix, ProtocolPKI}, r.CallerProtocol()) {
+		return true
+	}
+
+	if r.identityType == nil {
+		return false
+	}
+
+	return r.identityType.IsAdmin()
+}
+
+// CallerAddress returns the original caller address.
+func (r *Requestor) CallerAddress() string {
+	if r.forwardedSourceAddress != "" {
+		return r.forwardedSourceAddress
+	}
+
+	return r.sourceAddress
+}
+
+// CallerUsername returns the original caller Username.
+func (r *Requestor) CallerUsername() string {
+	if r.forwardedUsername != "" {
+		return r.forwardedUsername
+	}
+
+	return r.username
+}
+
+// CallerProtocol returns the original caller protocol.
+func (r *Requestor) CallerProtocol() string {
+	if r.forwardedProtocol != "" {
+		return r.forwardedProtocol
+	}
+
+	return r.protocol
+}
+
+// CallerIdentityProviderGroups returns the original caller identity provider groups.
+func (r *Requestor) CallerIdentityProviderGroups() []string {
+	if r.forwardedIdentityProviderGroups != nil {
+		return r.forwardedIdentityProviderGroups
+	}
+
+	return r.identityProviderGroups
+}
+
+// EventLifecycleRequestor returns an api.EventLifecycleRequestor representing the original caller.
+func (r *Requestor) EventLifecycleRequestor() *api.EventLifecycleRequestor {
+	return &api.EventLifecycleRequestor{
+		Username: r.CallerUsername(),
+		Protocol: r.CallerProtocol(),
+		Address:  r.CallerAddress(),
+	}
+}
+
+// CallerIdentity returns the identity.CacheEntry for the caller. It may be nil (e.g. if the protocol is ProtocolUnix).
+func (r *Requestor) CallerIdentity() *identity.CacheEntry {
+	return r.identity
+}
+
+// CallerIdentityType returns the identity.Type corresponding to the CallerIdentity. It may be nil (e.g. if the protocol is ProtocolUnix).
+func (r *Requestor) CallerIdentityType() identity.Type {
+	return r.identityType
+}
+
+// IsForwarded returns true if the request was forwarded from another cluster member and false otherwise.
+func (r *Requestor) IsForwarded() bool {
+	return r.forwardedSourceAddress != ""
+}
+
+// ForwardProxy returns a proxy function that adds the requestor details as headers to be inspected by the receiving cluster member.
+func (r *Requestor) ForwardProxy() func(req *http.Request) (*url.URL, error) {
+	return func(req *http.Request) (*url.URL, error) {
+		req.Header.Add(HeaderForwardedAddress, r.CallerAddress())
+
+		username := r.CallerUsername()
+		if username != "" {
+			req.Header.Add(HeaderForwardedUsername, username)
+		}
+
+		protocol := r.CallerProtocol()
+		if protocol != "" {
+			req.Header.Add(HeaderForwardedProtocol, protocol)
+		}
+
+		identityProviderGroups := r.CallerIdentityProviderGroups()
+		if identityProviderGroups != nil {
+			b, err := json.Marshal(identityProviderGroups)
+			if err == nil {
+				req.Header.Add(HeaderForwardedIdentityProviderGroups, string(b))
+			}
+		}
+
+		return shared.ProxyFromEnvironment(req)
+	}
+}
+
+// ForwardingMemberFingerprint returns the fingerprint of the cluster member that forwarded the request. It returns
+// an error if the request was not sent by another cluster member.
+func (r *Requestor) ForwardingMemberFingerprint() (string, error) {
+	if r.protocol != ProtocolCluster {
+		return "", ErrRequestNotInternal
+	}
+
+	return r.username, nil
 }
 
 // InitContextInfo sets an empty Info in the request context.
