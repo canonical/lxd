313c986443f81ec98a22b871f5e430ce81d0f04f
Mark Laing <mark.laing@canonical.com>
2025-08-16 09:38:13 +0100

    lxd/request: Add Requestor interface.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/request/requestor.go
+++ lxd/request/requestor.go
@@ -3,7 +3,53 @@ package request
 import (
 	"context"
 	"net/http"
+	"net/url"
+
+	"github.com/canonical/lxd/lxd/identity"
+	"github.com/canonical/lxd/shared/api"
 )
+
+// Requestor is a view of the original caller. An interface is used here to make the caller think twice about accessing
+// the raw details (which can be done via type assertion if needed) because e.g. RequestorDetails.Username might be the
+// fingerprint of another cluster member, and not the callers actual username.
+type Requestor interface {
+	// IsTrusted returns true if the caller is authenticated and false otherwise.
+	IsTrusted() bool
+
+	// IsAdmin returns true if the caller is an administrator and false otherwise.
+	IsAdmin() bool
+
+	// CallerAddress returns the original caller address.
+	CallerAddress() string
+
+	// CallerUsername returns the original caller username.
+	CallerUsername() string
+
+	// CallerProtocol returns the original caller protocol.
+	CallerProtocol() string
+
+	// CallerIdentityProviderGroups returns the original caller identity provider groups
+	CallerIdentityProviderGroups() []string
+
+	// CallerIdentity returns the identity.CacheEntry for the caller. It may be nil (e.g. if the protocol is ProtocolUnix).
+	CallerIdentity() *identity.CacheEntry
+
+	// CallerIdentityType returns the identity.Type corresponding to the CallerIdentity. It may be nil (e.g. if the protocol is ProtocolUnix).
+	CallerIdentityType() identity.Type
+
+	// EventLifecycleRequestor returns an api.EventLifecycleRequestor representing the original caller.
+	EventLifecycleRequestor() *api.EventLifecycleRequestor
+
+	// ForwardProxy returns a proxy function that adds the requestor details as headers to be inspected by the receiving cluster member.
+	ForwardProxy() func(req *http.Request) (*url.URL, error)
+
+	// IsForwarded returns true if the request was forwarded from another cluster member and false otherwise.
+	IsForwarded() bool
+
+	// ForwardingMemberFingerprint returns the fingerprint of the cluster member that forwarded the request. It returns
+	// an error if the request was not sent by another cluster member.
+	ForwardingMemberFingerprint() (string, error)
+}
 
 // RequestorDetails contains information that is gathered when the requestor is initially authenticated.
 type RequestorDetails struct {
