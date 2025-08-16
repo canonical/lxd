6583fc4f280c9bbed8fe61893dadeaaa27a5f178
Mark Laing <mark.laing@canonical.com>
2025-08-16 09:36:38 +0100

    lxd: Remove forwarding details from Info, rename to RequestorArgs.
    This will now represent only the initial details gathered during
    authentication.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/request/requestor.go
+++ lxd/request/requestor.go
@@ -5,53 +5,35 @@ import (
 	"net/http"
 )
 
-// Info represents the request information that are stored in the request
-// context, which is passed around.
-type Info struct {
-	// SourceAddress is the request's source address.
-	SourceAddress string
+// RequestorArgs contains information that is gathered when the requestor is initially authenticated.
+type RequestorArgs struct {
+	// Trusted indicates whether the request was authenticated or not. This is always set (and is false by default).
+	Trusted bool
 
-	// Address represents the final destination address of the request.
-	Address string
-
-	// Username used for the original connection.
+	// Username is the caller username. If the request was forwarded this may be the certificate fingerprint of another
+	// cluster member. It is only set if the Trusted is true.
 	Username string
 
-	// Protocol used for the original connection.
+	// Protocol is the caller protocol. If the request was forwarded this may be the certificate fingerprint of another
+	// cluster member. It is only set if the Trusted is true.
 	Protocol string
 
-	// IdentityProviderGroups represent identity provider groups defined by the
-	// identity provider if the identity authenticated with OIDC.
+	// IdentityProviderGroups contains identity provider groups. These are only set if the caller protocol is
+	// [api.AuthenticationMethodOIDC]. They are centrally defined groups that may map to LXD groups via identity
+	// provider group mappings.
 	IdentityProviderGroups []string
-
-	// ForwardedAddress represents an address of a cluster member from where the request was forwarded.
-	ForwardedAddress string
-
-	// ForwardedUsername represents username used on another cluster member.
-	ForwardedUsername string
-
-	// ForwardedProtocol represents protocol used on another cluster member.
-	ForwardedProtocol string
-
-	// ForwardedIdentityProviderGroups represents identity provider groups defined by
-	// the identity provider if the identity authenticated with OIDC on another cluster
-	// member.
-	ForwardedIdentityProviderGroups []string
-
-	// Trusted indicates whether the request was authenticated or not.
-	Trusted bool
 }
 
 // InitContextInfo sets an empty Info in the request context.
-func InitContextInfo(r *http.Request) *Info {
-	info := &Info{}
+func InitContextInfo(r *http.Request) *RequestorArgs {
+	info := &RequestorArgs{}
 	SetContextValue(r, CtxRequestInfo, info)
 	return info
 }
 
 // GetContextInfo gets the request information from the request context.
-func GetContextInfo(ctx context.Context) *Info {
-	info, ok := ctx.Value(CtxRequestInfo).(*Info)
+func GetContextInfo(ctx context.Context) *RequestorArgs {
+	info, ok := ctx.Value(CtxRequestInfo).(*RequestorArgs)
 	if !ok {
 		return nil
 	}
