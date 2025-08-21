6e098e73bb8eb0d9a6fd8fac17a550c505bda4a0
Mark Laing <mark.laing@canonical.com>
2025-08-21 09:33:53 +0100

    lxd: Improve error handling when reporting entitlements.
    This is a follow up of https://github.com/canonical/lxd/pull/16223#discussion_r2287314061.
    
    When the identity type is not present, it is not necessarily an
    internal error. It is a bad request if the calling identity is
    not fine-grained. This checks if the identity is a global admin
    (Unix, unrestricted TLS, CA signed cert with
    `core.trust_ca_certificates=true`) before getting the identity
    type and checking if it is fine-grained.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -390,13 +390,20 @@ func reportEntitlements(ctx context.Context, authorizer auth.Authorizer, identit
 		return err
 	}
 
+	// No fine-grained identities are global admins. Check this first in case the caller is using e.g. the unix socket.
+	if requestor.IsAdmin() {
+		return api.NewStatusError(http.StatusBadRequest, "Cannot report entitlements for identities that do not use fine-grained authorization")
+	}
+
+	// Any other requestor should have an identity type present.
 	identityType := requestor.CallerIdentityType()
 	if identityType == nil {
 		return errors.New("No identity type present in request details")
 	}
 
+	// Check the identity type is fine-grained (it could be a restricted client certificate).
 	if !identityType.IsFineGrained() {
-		return errors.New("Not fine grained")
+		return api.NewStatusError(http.StatusBadRequest, "Cannot report entitlements for identities that do not use fine-grained authorization")
 	}
 
 	// In the case where we have only one entity URL, we'll use the authorizer's CheckPermission method
