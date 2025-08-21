6be0cc574f6ad87d2353823467ce3ae232f9487c
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-08-21 18:10:05 +0200

    lxd/backup: Don't always populate volumes in the new format
    In case the given backup config (using the old format) doesn't contain any volume,
    don't try setting it in the new format to prevent a panic when trying to get the value from the pointer.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/backup/backup_config_utils.go
+++ lxd/backup/backup_config_utils.go
@@ -130,7 +130,8 @@ func ConvertFormat(backupConf *config.Config, version uint32) (*config.Config, e
 		// Rewrite the volumes only in case the old format is used.
 		// We can indicate this by checking whether or not the .Volumes key is set.
 		// This is applicable for both instances and custom storage volumes.
-		if len(copyBackupConf.Volumes) == 0 {
+		// In case there is no volume set we also don't populate one in the new file format.
+		if len(copyBackupConf.Volumes) == 0 && copyBackupConf.Volume != nil { //nolint:staticcheck
 			copyBackupConf.Volumes = []*config.Volume{
 				{
 					StorageVolume: *copyBackupConf.Volume,         //nolint:staticcheck
