ff08e7cce9759edf548fbef673347411c0ad4c91
Simon Deziel <simon.deziel@canonical.com>
2025-08-22 15:47:46 -0400

    test/includes/oidc: use already built `mini-oidc` binary
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/oidc.sh
+++ test/includes/oidc.sh
@@ -4,18 +4,12 @@ spawn_oidc() {
   # Return if OIDC is already set up.
   [ -e "${TEST_DIR}/oidc.pid" ] && return
 
-  (
-    cd mini-oidc || return
-    # Use -buildvcs=false here to prevent git complaining about untrusted directory when tests are run as root.
-    go build -v -buildvcs=false ./
+  PORT="$(local_tcp_port)"
+  echo "${PORT}" > "${TEST_DIR}/oidc.port"
+  mini-oidc "${PORT}" "${TEST_DIR}/oidc.user" &
+  echo $! > "${TEST_DIR}/oidc.pid"
 
-    PORT="$(local_tcp_port)"
-    echo "${PORT}" > "${TEST_DIR}/oidc.port"
-    ./mini-oidc "${PORT}" "${TEST_DIR}/oidc.user" &
-    echo $! > "${TEST_DIR}/oidc.pid"
-
-    sleep 3
-  )
+  sleep 3
 }
 
 kill_oidc() {
