2e74da930423619cf6159b712d8af9974970363f
Simon Deziel <simon.deziel@canonical.com>
2025-08-22 15:48:26 -0400

    test/suites/container_syscall_interception: use already built `sysinfo` binary
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/container_syscall_interception.sh
+++ test/suites/container_syscall_interception.sh
@@ -2,14 +2,8 @@ test_container_syscall_interception() {
   ensure_import_testimage
   ensure_has_localhost_remote "${LXD_ADDR}"
 
-  (
-    cd syscall/sysinfo || return
-    # Use -buildvcs=false here to prevent git complaining about untrusted directory when tests are run as root.
-    go build -v -buildvcs=false ./...
-  )
-
   lxc launch testimage c1 -c limits.memory=123MiB
-  lxc file push --quiet syscall/sysinfo/sysinfo c1/root/sysinfo
+  lxc file push --quiet "$(command -v sysinfo)" c1/root/sysinfo
   lxc exec c1 -- /root/sysinfo
   ! lxc exec c1 -- /root/sysinfo | grep "Totalram:128974848 " || false
   lxc stop -f c1
