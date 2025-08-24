a5098fcf27d79f06b539b8ea48377bf4b8fea518
Simon Deziel <simon.deziel@canonical.com>
2025-08-24 09:21:57 -0400

    client/lxd: check error from `tcp.ExtractConn()`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- client/lxd.go
+++ client/lxd.go
@@ -449,8 +449,8 @@ func (r *ProtocolLXD) rawWebsocket(url string) (*websocket.Conn, error) {
 	}
 
 	// Set TCP timeout options.
-	remoteTCP, _ := tcp.ExtractConn(conn.UnderlyingConn())
-	if remoteTCP != nil {
+	remoteTCP, err := tcp.ExtractConn(conn.UnderlyingConn())
+	if err == nil && remoteTCP != nil {
 		err = tcp.SetTimeouts(remoteTCP, 0)
 		if err != nil {
 			logger.Warn("Failed setting TCP timeouts on remote connection", logger.Ctx{"err": err})
