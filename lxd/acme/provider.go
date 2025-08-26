1205b5154bca2262a2a6bacc1c7a44e9b879d4b7
Simon Deziel <simon.deziel@canonical.com>
2025-08-25 21:43:33 -0400

    lxd/acme: add doc to exported functions
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/acme/provider.go
+++ lxd/acme/provider.go
@@ -27,6 +27,7 @@ func NewHTTP01Provider() HTTP01Provider {
 	return &http01Provider{}
 }
 
+// Present sets the provider's domain, token, and keyAuth values.
 func (p *http01Provider) Present(domain string, token string, keyAuth string) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -38,6 +39,7 @@ func (p *http01Provider) Present(domain string, token string, keyAuth string) er
 	return nil
 }
 
+// CleanUp clears the provider's domain, token, and keyAuth values.
 func (p *http01Provider) CleanUp(domain string, token string, keyAuth string) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -49,6 +51,7 @@ func (p *http01Provider) CleanUp(domain string, token string, keyAuth string) er
 	return nil
 }
 
+// KeyAuth returns the key authorization value.
 func (p *http01Provider) KeyAuth() string {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -56,6 +59,7 @@ func (p *http01Provider) KeyAuth() string {
 	return p.keyAuth
 }
 
+// Domain returns the domain value.
 func (p *http01Provider) Domain() string {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -63,6 +67,7 @@ func (p *http01Provider) Domain() string {
 	return p.domain
 }
 
+// Token returns the token value.
 func (p *http01Provider) Token() string {
 	p.mu.Lock()
 	defer p.mu.Unlock()
