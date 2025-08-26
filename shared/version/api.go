7323631ec5ba65d866d8c28274b1ff5fc86fc6a2
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2025-08-26 10:37:20 -0400

    api: Add ovn_dhcp_ranges extension
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -460,6 +460,7 @@ var APIExtensions = []string{
 	"devlxd_volume_management",
 	"storage_driver_alletra",
 	"resources_disk_used_by",
+	"ovn_dhcp_ranges",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
