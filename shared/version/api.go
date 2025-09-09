c35edba1322cd02b3316f22d384fa020f5a05894
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2025-09-09 15:09:53 +0200

    api: add HPE Alletra Storage driver API extension
    Co-Authored-By: Minae Lee <minae.lee@canonical.com>
    Signed-off-by: Minae Lee <minae.lee@canonical.com>
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -458,6 +458,7 @@ var APIExtensions = []string{
 	"ovn_internal_load_balancer",
 	"auth_bearer_devlxd",
 	"devlxd_volume_management",
+	"storage_driver_alletra",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
