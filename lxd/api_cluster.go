91eb08acb977464c68645c79cd3cc9cfc73b472d
Simon Deziel <simon.deziel@canonical.com>
2025-09-10 16:10:21 -0400

    lxd/api_cluster: lookup local cluster address closer to where it's needed in `clusterNodeDelete()`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit fec26a5c758d1975cb55168e988346c0c67de1de)

--- lxd/api_cluster.go
+++ lxd/api_cluster.go
@@ -1998,7 +1998,6 @@ func clusterNodeDelete(d *Daemon, r *http.Request) response.Response {
 
 	// Redirect all requests to the leader, which is the one with
 	// knowledge of which nodes are part of the raft cluster.
-	localClusterAddress := s.LocalConfig.ClusterAddress()
 	leaderInfo, err := s.LeaderInfo()
 	if err != nil {
 		return response.SmartError(err)
@@ -2017,6 +2016,8 @@ func clusterNodeDelete(d *Daemon, r *http.Request) response.Response {
 	if err != nil {
 		force = 0
 	}
+
+	localClusterAddress := s.LocalConfig.ClusterAddress()
 
 	var localInfo, leaderNodeInfo db.NodeInfo
 	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
