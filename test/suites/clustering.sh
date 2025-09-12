ef212b9c8566695f94b0140e92e0e4a35a347c19
Kadin Sayani <kadin.sayani@canonical.com>
2025-09-12 11:11:39 -0600

    tests: Check "volatile.cluster.group" updated post-migration
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- test/suites/clustering.sh
+++ test/suites/clustering.sh
@@ -3642,6 +3642,10 @@ test_clustering_evacuation() {
   [ "$(LXD_DIR="${LXD_TWO_DIR}" lxc list -f csv -c sL c6)" = "RUNNING,node2" ]
   [ "$(LXD_DIR="${LXD_TWO_DIR}" lxc list -f csv -c sL c7)" = "STOPPED,node1" ]
 
+  # Move c7 to @default to check "volatile.cluster.group" is updated.
+  LXD_DIR="${LXD_TWO_DIR}" lxc move c7 --target=@default
+  [ "$(LXD_DIR="${LXD_ONE_DIR}" lxc config get c7 volatile.cluster.group)" = "@default" ]
+
   # Clean up
   LXD_DIR="${LXD_TWO_DIR}" lxc delete -f c1 c2 c3 c4 c5 c6 c7
   LXD_DIR="${LXD_TWO_DIR}" lxc image delete testimage
