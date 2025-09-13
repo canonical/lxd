447b96bcf05aa7fde63ad77bfeb86e00cf3a7185
Simon Deziel <simon.deziel@canonical.com>
2025-09-13 12:29:33 -0400

    test/includes/setup: do not repeatedly check if `busybox` meets the requirements
    This is now done only once by `test/main.sh`.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit c67ff21f08fd68ce670fca8ef4560ddd15680440)

--- test/includes/setup.sh
+++ test/includes/setup.sh
@@ -16,17 +16,6 @@ ensure_import_testimage() {
         echo "Importing ${LXD_TEST_IMAGE} test image from disk"
         lxc image import "${LXD_TEST_IMAGE}" --alias testimage
     else
-        BUSYBOX="$(command -v busybox)"
-        if [ ! -e "${BUSYBOX}" ]; then
-            echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
-            exit 1
-        fi
-
-        if ldd "${BUSYBOX}" >/dev/null 2>&1; then
-            echo "The testsuite requires ${BUSYBOX} to be a static binary"
-            exit 1
-        fi
-
         project="$(lxc project list | awk '/(current)/ {print $2}')"
         deps/import-busybox --alias testimage --project "$project"
     fi
