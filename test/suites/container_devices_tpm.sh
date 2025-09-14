55940cf362c3d0a9e5586421e4e2eee9816c02b0
Simon Deziel <simon.deziel@canonical.com>
2025-09-13 20:25:14 -0400

    test/suites/container_devices_tpm: skip known broken on `lvm`
    This test is skipped on GH runners due to not having the `tpm_vtpm_proxy`
    module.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/container_devices_tpm.sh
+++ test/suites/container_devices_tpm.sh
@@ -4,6 +4,25 @@ test_container_devices_tpm() {
     return
   fi
 
+  lxd_backend=$(storage_backend "$LXD_DIR")
+  if [ "${lxd_backend}" = "lvm" ]; then
+    #+ lxc config device rm ct843376 test-dev1
+    #++ timeout --foreground 120 /root/go/bin/lxc config device rm ct843376 test-dev1
+    #Device test-dev1 removed from ct843376
+    #+ lxc exec ct843376 -- stat /dev/tpm0
+    #++ timeout --foreground 120 /root/go/bin/lxc exec ct843376 --force-local -- stat /dev/tpm0
+    #stat: can't stat '/dev/tpm0': No such file or directory
+    #+ lxc exec ct843376 -- stat /dev/tpmrm0
+    #++ timeout --foreground 120 /root/go/bin/lxc exec ct843376 --force-local -- stat /dev/tpmrm0
+    #stat: can't stat '/dev/tpmrm0': No such file or directory
+    #+ lxc delete -f ct843376
+    #++ timeout --foreground 120 /root/go/bin/lxc delete -f ct843376
+    #INFO   [2025-09-14T00:02:59Z] Stopping instance                  action=stop created="2025-09-14 00:02:58.11670028 +0000 UTC" ephemeral=false instance=ct843376 instanceType=container project=default stateful=false used="2025-09-14 00:02:58.916546684 +0000 UTC"
+    #Error: Stopping the instance failed: Failed unmounting instance: Failed to unmount LVM logical volume: Failed to unmount "/tmp/lxd-test.tmp.GyyF/9wL/storage-pools/lxdtest-9wL/containers/ct843376": device or resource busy
+    echo "==> SKIP: known broken test on 'lvm'"
+    return
+  fi
+
   ensure_import_testimage
   ctName="ct$$"
   lxc launch testimage "${ctName}"
