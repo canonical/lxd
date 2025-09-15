c05fc5eefaa72db8d6430b17089fa52e839566d4
Mark Laing <mark.laing@canonical.com>
2025-09-15 11:10:01 +0100

    lxd/encryption: Extract common functionality from GetDevLXDBearerToken.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/auth/encryption/sign.go
+++ lxd/auth/encryption/sign.go
@@ -36,20 +36,38 @@ func LXDAudience(clusterUUID string) string {
 // - Issued at (iat): time now (UTC)
 // - Expiry (exp): The given time (UTC).
 func GetDevLXDBearerToken(secret []byte, identityIdentifier string, clusterUUID string, expiresAt time.Time) (string, error) {
-	if expiresAt.Location() != time.UTC {
-		expiresAt = expiresAt.UTC()
-	}
+	return getToken(secret, nil, identityIdentifier, clusterUUID, DevLXDAudience, expiresAt)
+}
 
+// getToken generates and signs a token for use with the LXD. If a salt is provided, a signing key will be generated
+// using [TokenSigningKey] with the secret, otherwise the given secret will be used directly.
+// For claims it has:
+// - Subject (sub): Identity identifier (UUID)
+// - Issuer (iss): "lxd:{cluster_uuid}"
+// - Audience (aud): Result of audienceFunc(clusterUUID)
+// - Not before (nbf): time now (UTC)
+// - Issued at (iat): time now (UTC)
+// - Expiry (exp): The given time (UTC).
+func getToken(secret []byte, salt []byte, subject string, clusterUUID string, audienceFunc func(string) string, expiresAt time.Time) (string, error) {
 	claims := jwt.RegisteredClaims{
 		Issuer:    Issuer(clusterUUID),
-		Subject:   identityIdentifier,
-		Audience:  jwt.ClaimStrings{DevLXDAudience(clusterUUID)},
+		Subject:   subject,
+		Audience:  jwt.ClaimStrings{audienceFunc(clusterUUID)},
 		NotBefore: jwt.NewNumericDate(time.Now().UTC()),
 		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
-		ExpiresAt: jwt.NewNumericDate(expiresAt),
+		ExpiresAt: jwt.NewNumericDate(expiresAt.UTC()),
 	}
 
-	signedToken, err := jwt.NewWithClaims(jwt.SigningMethodHS512, claims).SignedString(secret)
+	var err error
+	signingKey := secret
+	if salt != nil {
+		signingKey, err = TokenSigningKey(secret, salt)
+		if err != nil {
+			return "", fmt.Errorf("Failed to issue token: %w", err)
+		}
+	}
+
+	signedToken, err := jwt.NewWithClaims(jwt.SigningMethodHS512, claims).SignedString(signingKey)
 	if err != nil {
 		return "", fmt.Errorf("Failed to sign JWT: %w", err)
 	}
