2cdf2425f2f3f20afbf8010e9ae4efdadbc5c1f1
Mark Laing <mark.laing@canonical.com>
2025-09-15 16:27:29 +0100

    lxd: Add /1.0/auth/oidc-sessions API.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -152,6 +152,8 @@ var api10 = []APIEndpoint{
 	permissionsCmd,
 	storageVolumesCmd,
 	storageVolumesTypeCmd,
+	oidcSessionsCmd,
+	oidcSessionCmd,
 }
 
 // swagger:operation GET /1.0?public server server_get_untrusted
