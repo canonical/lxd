bcd85a25b91fbea39318848d1be0f4e5f3d56644
Simon Deziel <simon.deziel@canonical.com>
2025-09-23 19:25:40 -0400

    test/lint/unneeded-format-variant: report violating line numbers
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 84ffae544259274bb9f4a31adf763cdc903f3379)

--- test/lint/unneeded-format-variant.sh
+++ test/lint/unneeded-format-variant.sh
@@ -6,6 +6,7 @@ echo "Checking usage of unneeded format variant of 'logger.(Debug|Info|Warn|Erro
 # XXX: ignore format specifier args (%x or slice expansion like `a...)`)
 OUT="$(git grep -n --untracked -E '(l|log|logger)\.(Debug|Info|Warn|Error)f\(' '*.go' | grep -v '%.' | grep -vF '...)' || true)"
 if [ -n "${OUT}" ]; then
-  echo "ERROR: unneeded format variant of logger.(Debug|Info|Warn|Error)f() function in script: ${OUT}"
+  echo "ERROR: unneeded format variant of logger.(Debug|Info|Warn|Error)f() function in script:"
+  echo "${OUT}"
   exit 1
 fi
