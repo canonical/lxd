81a576a9eb78caeb75a360bc857ef6ae3af47f42
Simon Deziel <simon.deziel@canonical.com>
2025-09-23 08:27:55 -0400

    test/suites/query: improve `grep` pattern
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 2c0637612b1aae8c29cc58413cf32251e739d6ec)

--- test/suites/query.sh
+++ test/suites/query.sh
@@ -3,6 +3,6 @@ test_query() {
 
   lxc init --empty querytest
   lxc query --wait -X POST -d '{"name": "snap-test"}' /1.0/containers/querytest/snapshots
-  lxc info querytest | grep snap-test
+  lxc info querytest | grep -wF snap-test
   lxc delete querytest
 }
