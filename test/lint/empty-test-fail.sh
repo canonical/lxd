7e68c710a8ce1f8c29d4735ea00144908d2eb70f
Simon Deziel <simon.deziel@canonical.com>
2025-09-23 19:09:05 -0400

    test/lint/empty-test-fail: report violating line numbers
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit db4e56dd22e8e3d95f2d55faea70ccea1107c519)

--- test/lint/empty-test-fail.sh
+++ test/lint/empty-test-fail.sh
@@ -20,7 +20,7 @@ set -o pipefail
 # POSIX:  [ "$(cmd) = "" ]    or  [ -z "$(cmd)" ]
 # bash:  [[ "$(cmd)" == "" ]] or [[ -z "$(cmd)" ]]
 #
-out="$(grep -rE '\[\s+(-z\s+"\$\(.*\)"|"\$\(.*\)"\s+=+\s+["'\'']{2})\s+\]' --exclude="$(basename "${0}")" test/ | grep -Fv ' || echo fail)' || true)"
+out="$(grep -rnE '\[\s+(-z\s+"\$\(.*\)"|"\$\(.*\)"\s+=+\s+["'\'']{2})\s+\]' --exclude="$(basename "${0}")" test/ | grep -Fv ' || echo fail)' || true)"
 if [ "${out}" != '' ] ; then
   # shellcheck disable=SC2016
   echo 'One or more assertions of type [ "$(cmd)" = '' ] require `|| echo fail`'
