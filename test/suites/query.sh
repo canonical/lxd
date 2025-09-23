4df2781cf8bf04d6adacd5cc93134d71c4dbfc74
Simon Deziel <simon.deziel@canonical.com>
2025-09-23 08:26:58 -0400

    test/suites/query: simplify quoting
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/query.sh
+++ test/suites/query.sh
@@ -3,7 +3,7 @@ test_query() {
   ensure_has_localhost_remote "${LXD_ADDR}"
 
   lxc init testimage querytest
-  lxc query --wait -X POST -d "{\\\"name\\\": \\\"snap-test\\\"}" /1.0/containers/querytest/snapshots
+  lxc query --wait -X POST -d '{"name": "snap-test"}' /1.0/containers/querytest/snapshots
   lxc info querytest | grep snap-test
   lxc delete querytest
 }
