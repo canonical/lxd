f6eb2497c462eb9cb878497ff346a0ffce0f1621
Simon Deziel <simon.deziel@canonical.com>
2025-09-24 14:54:13 -0400

    test/suites/filemanip: simplify `filemanip_req_content_type()`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/filemanip.sh
+++ test/suites/filemanip.sh
@@ -186,11 +186,8 @@ test_filemanip_req_content_type() {
   # This ensures strings.Reader works correctly with the content-type check.
   # The specific here is that the net/http package will configure the
   # content-length on the request, which in LXD triggers content-type check.
-  (
-    cd lxd-client
-    go run . file-push "${inst}" /tmp/status.txt "success"
-    [ "$(lxc exec "${inst}" -- cat /tmp/status.txt)" = "success" ]
-  )
+  go run -C lxd-client . file-push "${inst}" /tmp/status.txt "success"
+  [ "$(lxc exec "${inst}" -- cat /tmp/status.txt)" = "success" ]
 
   lxc delete "${inst}" --force
 }
