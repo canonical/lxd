83f4c5aa01927f74daed7413a825a503ebd36920
Simon Deziel <simon.deziel@canonical.com>
2025-09-24 16:20:44 -0400

    test/includes/net: directly execute `python3` in `local_tcp_port()`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/net.sh
+++ test/includes/net.sh
@@ -2,13 +2,12 @@
 
 # Return an available random local port
 local_tcp_port() {
-    cat << EOF | python3
+    exec python3 -c "
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 0))
 print(s.getsockname()[1])
-s.close()
-EOF
+s.close()"
 }
 
 # Certificate-aware curl wrapper
