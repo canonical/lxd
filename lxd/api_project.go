35ab953b2de2880ff9e68575262b8168745697de
Kadin Sayani <kadin.sayani@canonical.com>
2025-09-26 16:42:17 -0600

    lxd: add param to projectIsEmpty() to skip checking for cached images
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- lxd/api_project.go
+++ lxd/api_project.go
@@ -1361,10 +1361,43 @@ func projectStateGet(d *Daemon, r *http.Request) response.Response {
 }
 
 // Check if a project is empty.
-func projectIsEmpty(ctx context.Context, project *dbCluster.Project, tx *db.ClusterTx) (bool, error) {
+func projectIsEmpty(ctx context.Context, project *dbCluster.Project, tx *db.ClusterTx, skipCachedImages bool) (bool, error) {
 	usedBy, err := projectUsedBy(ctx, tx, project)
 	if err != nil {
 		return false, err
+	}
+
+	if skipCachedImages {
+		for _, u := range usedBy {
+			parsedURL, err := url.Parse(u)
+			if err != nil {
+				return false, fmt.Errorf("Failed parsing URL %q: %w", u, err)
+			}
+
+			// Build an entity reference directly from the URL.
+			ref, err := entity.ReferenceFromURL(*parsedURL)
+			if err != nil {
+				return false, err
+			}
+
+			// Check for cached images.
+			if ref.EntityType == entity.TypeImage {
+				fingerprint := ref.Name()
+
+				image, err := dbCluster.GetImage(ctx, tx.Tx(), project.Name, fingerprint)
+				if err != nil {
+					return false, err
+				}
+
+				if image.Cached {
+					// Remove cached image from usedBy list.
+					idx := slices.Index(usedBy, u)
+					if idx != -1 {
+						usedBy = slices.Delete(usedBy, idx, idx+1)
+					}
+				}
+			}
+		}
 	}
 
 	if isProjectInUse(usedBy) {
