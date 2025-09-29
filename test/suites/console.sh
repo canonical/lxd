be0444fc4f2a8fd501b6f2c4851a4d11c86b6f10
Simon Deziel <simon.deziel@canonical.com>
2025-09-29 16:31:51 -0400

    test/suites/console: simulate console interactions with `expect`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/console.sh
+++ test/suites/console.sh
@@ -5,6 +5,36 @@ test_console() {
 
   lxc launch testimage cons1
 
+  # Simulate console interactions with 'expect' and use 'tr' and 'grep' to
+  # filter out leaked (control) chars. To debug, use 'expect -d'.
+  console_output_file="$(mktemp -p "${TEST_DIR}" console.XXX)"
+  cat << EOF | expect | tr -cd '[:print:]\n\t' | grep -vF '[6n' > "${console_output_file}"
+set timeout 3
+spawn lxc console cons1
+sleep 0.1
+expect "To detach from the console, press: <ctrl>+a q"
+send "reset\r"
+sleep 0.1
+expect "\n\r"
+send "\r"
+sleep 0.1
+expect "/ # *"
+send "env\r"
+sleep 0.1
+expect "/ # *"
+send "exit\r"
+expect "Please press Enter to activate this console."
+# ctrl+a q
+send "\001q"
+EOF
+
+  if ! grep -xF 'TERM=vt102' "${console_output_file}"; then
+    echo "Unexpected console output"
+    cat --show-nonprinting "${console_output_file}"
+    false
+  fi
+  rm "${console_output_file}"
+
   # Make sure there's something in the console ringbuffer.
   echo 'some content' | lxc exec cons1 -- tee /dev/console
   echo 'some more content' | lxc exec cons1 -- tee /dev/console
