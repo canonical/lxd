3679948c78a006fe820c6bad722755de73698c7d
Simon Deziel <simon.deziel@canonical.com>
2025-09-30 12:31:56 -0400

    test/includes/check: optimize `check_empty_table()`
    `SELECT 1 FROM ... LIMIT 1` is the most efficient way to check for the
    existence of a single row which is what we care to confirm when checking for
    emptiness.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit cc0a74c874c283ac0c1a06780bf3e7b51fdd91f8)

--- test/includes/check.sh
+++ test/includes/check.sh
@@ -32,7 +32,7 @@ check_empty_table() {
     # The profiles table will never be empty since the `default` profile cannot
     # be deleted.
     if [ "$2" = 'profiles' ]; then
-        if [ -n "$(sqlite3 "${1}" "SELECT * FROM ${2} WHERE name != 'default';")" ]; then
+        if [ -n "$(sqlite3 "${1}" "SELECT 1 FROM ${2} WHERE name != 'default' LIMIT 1;")" ]; then
           echo "DB table ${2} is not empty, content:"
           sqlite3 "${1}" "SELECT * FROM ${2} WHERE name != 'default';"
           return 1
@@ -40,7 +40,7 @@ check_empty_table() {
         return 0
     fi
 
-    if [ -n "$(sqlite3 "${1}" "SELECT * FROM ${2};")" ]; then
+    if [ -n "$(sqlite3 "${1}" "SELECT 1 FROM ${2} LIMIT 1;")" ]; then
         echo "DB table ${2} is not empty, content:"
         sqlite3 "${1}" "SELECT * FROM ${2};"
         return 1
