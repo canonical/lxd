8eff959a543ed829dcd8e72ece4c06db3314cd2d
Simon Deziel <simon.deziel@canonical.com>
2025-10-02 12:46:00 -0400

    test/suites/basic: simplify Apparmor handling
    The Ubuntu 20.04 GA kernel (5.4.0) has the same Appamor
    `features/domain/version` (1.2) as a modern 24.04 HWE kernel (6.14.0).
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/basic.sh
+++ test/suites/basic.sh
@@ -545,23 +545,11 @@ test_basic_usage() {
     # unloaded on stop, and that it is deleted when the container is deleted
     lxc launch testimage lxd-apparmor-test
 
-    MAJOR=0
-    MINOR=0
-    if [ -f /sys/kernel/security/apparmor/features/domain/version ]; then
-      MAJOR=$(awk -F. '{print $1}' < /sys/kernel/security/apparmor/features/domain/version)
-      MINOR=$(awk -F. '{print $2}' < /sys/kernel/security/apparmor/features/domain/version)
-    fi
+    aa_namespace="lxd-lxd-apparmor-test_<$(echo "${LXD_DIR}" | sed -e 's/\//-/g' -e 's/^.//')>"
+    aa-status | grep -F ":${aa_namespace}:unconfined" || aa-status | grep -F ":${aa_namespace}://unconfined"
+    lxc stop lxd-apparmor-test --force
+    ! aa-status | grep -F ":${aa_namespace}:" || false
 
-    if [ "${MAJOR}" -gt "1" ] || { [ "${MAJOR}" = "1" ] && [ "${MINOR}" -ge "2" ]; }; then
-      aa_namespace="lxd-lxd-apparmor-test_<$(echo "${LXD_DIR}" | sed -e 's/\//-/g' -e 's/^.//')>"
-      aa-status | grep -F ":${aa_namespace}:unconfined" || aa-status | grep -F ":${aa_namespace}://unconfined"
-      lxc stop lxd-apparmor-test --force
-      ! aa-status | grep -F ":${aa_namespace}:" || false
-    else
-      aa-status | grep "lxd-lxd-apparmor-test_<${LXD_DIR}>"
-      lxc stop lxd-apparmor-test --force
-      ! aa-status | grep -F "lxd-lxd-apparmor-test_<${LXD_DIR}>" || false
-    fi
     lxc delete lxd-apparmor-test
     [ ! -f "${LXD_DIR}/security/apparmor/profiles/lxd-lxd-apparmor-test" ]
   else
