1da3d41484f7a0786fad6d1d4b0393b8f92d014b
Simon Deziel <simon.deziel@canonical.com>
2025-10-02 17:18:35 -0400

    test/suites/serverconfig: `curl` uses `GET` by default
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit fcb5d174a5e4930a406bc90d84b3479c6a904a4e)

--- test/suites/serverconfig.sh
+++ test/suites/serverconfig.sh
@@ -27,7 +27,7 @@ _server_config_password() {
 
 _server_config_access() {
   # test untrusted server GET
-  ! my_curl -X GET "https://$(cat "${LXD_SERVERCONFIG_DIR}/lxd.addr")/1.0" | grep -wF "environment" || false
+  ! my_curl "https://$(< "${LXD_SERVERCONFIG_DIR}/lxd.addr")/1.0" | grep -wF "environment" || false
 
   # test authentication type, only tls is enabled by default
   [ "$(curl --silent --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0" | jq -r '.metadata.auth_methods | .[]')" = "tls" ]
