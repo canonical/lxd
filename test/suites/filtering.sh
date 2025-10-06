b5f1973e64d2a82b80f50551fefd3d76c723ff15
Simon Deziel <simon.deziel@canonical.com>
2025-10-06 12:53:11 -0400

    test/suites/filtering: use empty instances
    And logically group operations.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 2780beb9d4776c607e5117349cb1d68d85b5b4b1)

--- test/suites/filtering.sh
+++ test/suites/filtering.sh
@@ -1,20 +1,17 @@
 # Test API filtering.
 test_filtering() {
-    ensure_import_testimage
-
-    lxc init testimage c1
-    lxc init testimage c2
-
+    echo "===> Instance filtering"
+    lxc init --empty c1
+    lxc init --empty c2
     curl --silent --get --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=0" --data-urlencode "filter=name eq c1" | jq --exit-status ".metadata | length == 1"
-
     curl --silent --get --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=1" --data-urlencode "filter=name eq c1" | jq --exit-status ".metadata | length == 1"
-
     curl --silent --get --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/instances" --data-urlencode "recursion=2" --data-urlencode "filter=name eq c1" | jq --exit-status ".metadata | length == 1"
 
+    echo "===> Image filtering"
+    ensure_import_testimage
     curl --silent --get --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/images" --data-urlencode "recursion=0" --data-urlencode "filter=properties.os eq BusyBox" | jq --exit-status ".metadata | length == 1"
-
     curl --silent --get --unix-socket "$LXD_DIR/unix.socket" "lxd/1.0/images" --data-urlencode "recursion=1" --data-urlencode "filter=properties.os eq Ubuntu" | jq --exit-status ".metadata == []"
 
-    lxc delete c1
-    lxc delete c2
+    # Cleanup
+    lxc delete c1 c2
 }
