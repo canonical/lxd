b46877bf9808bce9a79af8fe8c7c3ff155d42fb0
Simon Deziel <simon.deziel@canonical.com>
2025-10-07 17:36:27 -0400

    test/backends/lvm: allow using use bigger volumes to accomodate VMs
    Also tweak the fallback from 25MiB down to 24MiB as LVM was silently rounding
    it to the next extent resulting in 28MiB.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/backends/lvm.sh
+++ test/backends/lvm.sh
@@ -7,10 +7,11 @@ lvm_setup() {
 lvm_configure() {
   local LXD_DIR="${1}"
   local POOL_NAME="${2:-"lxdtest-${LXD_DIR##*/}"}" # Use the last part of the LXD_DIR as pool name
+  local VOL_SIZE="${3:-"24MiB"}"
 
   echo "==> Configuring lvm backend in ${LXD_DIR}"
 
-  lxc storage create "${POOL_NAME}" lvm volume.size=25MiB size=3GiB
+  lxc storage create "${POOL_NAME}" lvm volume.size="${VOL_SIZE}" size=3GiB
   lxc profile device add default root disk path="/" pool="${POOL_NAME}"
 }
 
