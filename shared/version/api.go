ebc0a403c077bf5781ff234166f0f96fe9f6d019
Kadin Sayani <kadin.sayani@canonical.com>
2025-10-08 10:31:40 -0600

    api: Add instance_placement_groups extension
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -467,6 +467,7 @@ var APIExtensions = []string{
 	"auth_oidc_sessions",
 	"instance_snapshots_multi_volume",
 	"vm_persistent_bus",
+	"instance_placement_groups",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
