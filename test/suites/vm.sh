86e410d3b693b5fc0eaacc782761284f7f5d5f1b
Simon Deziel <simon.deziel@canonical.com>
2025-10-10 17:43:57 -0400

    test/suites/vm: add `snap_vm_empty` test
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/vm.sh
+++ test/suites/vm.sh
@@ -65,3 +65,48 @@ test_vm_empty() {
   ! lxc start v1 || false
   lxc delete v1
 }
+
+test_snap_vm_empty() {
+  echo "==> Invalid VM names"
+  ! lxc init --vm --empty ".." -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}" || false
+  # Escaping `\` multiple times due to `lxc` wrapper script munging the first layer
+  ! lxc init --vm --empty "\\\\" -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}" || false
+  ! lxc init --vm --empty "/" -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}" || false
+  ! lxc init --vm --empty ";" -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}" || false
+
+  echo "==> Too small VMs"
+  ! lxc launch --vm --empty v1 -c limits.memory=0 -d "${SMALL_ROOT_DISK}" || false
+  ! lxc launch --vm --empty v1 -c limits.memory=0% -d "${SMALL_ROOT_DISK}" || false
+
+  echo "==> Tiny VMs with snapshots"
+  lxc init --vm --empty v1 -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}"
+  lxc snapshot v1
+  # Invalid snapshot names
+  ! lxc snapshot v1 ".." || false
+  # Escaping `\` multiple times due to `lxc` wrapper script munging the first layer
+  ! lxc snapshot v1 "\\\\" || false
+  ! lxc snapshot v1 "/" || false
+  [ "$(lxc list -f csv -c S v1)" = "1" ]
+  lxc start v1
+  lxc snapshot v1
+  [ "$(lxc list -f csv -c S v1)" = "2" ]
+
+  echo "==> Pause (freeze)/resume"
+  lxc pause v1
+  [ "$(lxc list -f csv -c s v1)" = "FROZEN" ]
+  ! lxc stop v1 || false
+  lxc start v1
+  [ "$(lxc list -f csv -c s v1)" = "RUNNING" ]
+  lxc pause v1
+  [ "$(lxc list -f csv -c s v1)" = "FROZEN" ]
+  lxc stop -f v1
+  lxc delete v1
+
+  lxc launch --vm --empty v1 -c limits.memory=1% -d "${SMALL_ROOT_DISK}"
+  lxc delete --force v1
+
+  echo "==> Ephemeral cleanup"
+  lxc launch --vm --empty --ephemeral v1 -c limits.memory=128MiB -d "${SMALL_ROOT_DISK}"
+  lxc stop -f v1
+  [ "$(lxc list -f csv -c n || echo fail)" = "" ]
+}
