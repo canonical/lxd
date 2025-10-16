59be1cb160a669e372b84a545c6199a766fbc5f1
Thomas Parrott <thomas.parrott@canonical.com>
2025-10-16 14:06:43 +0100

    lxd/instance/drivers/driver/qemu: d.busAllocatePCIeHotplug usage in deviceAttachPath
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>
    (cherry picked from commit 7399adf5a90418b6749599605538b3f32438a154)

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -2366,9 +2366,6 @@ func (d *qemu) deviceAttachPath(deviceName string) (mountTag string, err error) 
 		return "", errors.New("Virtiofsd isn't running")
 	}
 
-	reverter := revert.New()
-	defer reverter.Fail()
-
 	// Check if the agent is running.
 	monitor, err := qmp.Connect(d.monitorPath(), qemuSerialChardevName, d.getMonitorEventHandler())
 	if err != nil {
@@ -2400,6 +2397,9 @@ func (d *qemu) deviceAttachPath(deviceName string) (mountTag string, err error) 
 		return "", fmt.Errorf("Error opening virtiofs socket %q: %w", virtiofsdSockPath, err)
 	}
 
+	reverter := revert.New()
+	defer reverter.Fail()
+
 	err = monitor.SendFile(virtiofsdSockPath, virtiofsFile)
 	if err != nil {
 		return "", fmt.Errorf("Failed to send virtiofs file descriptor: %w", err)
@@ -2429,9 +2429,13 @@ func (d *qemu) deviceAttachPath(deviceName string) (mountTag string, err error) 
 	reverter.Add(func() { _ = monitor.RemoveCharDevice(deviceID) })
 
 	// Try to get a PCI address for hotplugging.
-	busName, busAddr, _, err := d.busAllocatePCIeHotplug(deviceName, false)
+	busCleanup, busName, busAddr, _, err := d.busAllocatePCIeHotplug(deviceName, false)
 	if err != nil {
 		return "", err
+	}
+
+	if busCleanup != nil {
+		reverter.Add(busCleanup)
 	}
 
 	qemuDev := map[string]any{
