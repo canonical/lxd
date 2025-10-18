d6ba29f0ee9e871f4468f3d59cc71af93571e2d5
Simon Deziel <simon.deziel@canonical.com>
2025-10-17 23:09:55 -0400

    test/suites/devlxd: use bigger `volume.size` for larger VM
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit ef602e43ce94d81e44c37314ca7de196ff3fd5f3)

--- test/suites/devlxd.sh
+++ test/suites/devlxd.sh
@@ -164,6 +164,13 @@ test_devlxd_vm() {
     lxc profile set default migration.stateful=true
   fi
 
+  pool="lxdtest-$(basename "${LXD_DIR}")"
+  orig_volume_size="$(lxc storage get "${pool}" volume.size)"
+  if [ -n "${orig_volume_size:-}" ]; then
+    echo "==> Override the volume.size to accommodate a large VM"
+    lxc storage set "${pool}" volume.size "${SMALLEST_VM_ROOT_DISK}"
+  fi
+
   lxc launch ubuntu-minimal-daily:24.04 v1 --vm -c limits.memory=384MiB -d "${SMALL_VM_ROOT_DISK}"
   waitInstanceReady v1
 
@@ -231,6 +238,10 @@ runcmd:
   # Cleanup
   lxc image delete "$(lxc config get v1 volatile.base_image)"
   lxc delete v1
+  if [ -n "${orig_volume_size:-}" ]; then
+    echo "==> Restore the volume.size"
+    lxc storage set "${pool}" volume.size "${orig_volume_size}"
+  fi
 
   if grep -qxF 'VERSION_ID="22.04"' /etc/os-release; then
     # Cleanup custom changes from the default profile
