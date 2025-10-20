7b2fb52c32b469f6767df9300d79a67014b0ef2d
Kadin Sayani <kadin.sayani@canonical.com>
2025-10-20 10:05:26 -0600

    lxd: Add comment justifying UpdataMetadata() usage
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    (cherry picked from commit a435da70b8c78b5067b4a3a65a4fd3025b596e91)

--- lxd/images.go
+++ lxd/images.go
@@ -412,6 +412,7 @@ func imgPostInstanceInfo(s *state.State, req api.ImagesPost, op *operations.Oper
 
 				metadata := make(map[string]any)
 				shared.SetProgressMetadata(metadata, "create_image_from_instance_pack", "Image pack", percent, processed, speed)
+				// Replace metadata to show only current progress, avoiding stale keys from previous stages.
 				_ = op.UpdateMetadata(metadata)
 			},
 			Length: totalSize,
@@ -475,6 +476,7 @@ func imgPostInstanceInfo(s *state.State, req api.ImagesPost, op *operations.Oper
 		Handler: func(value, speed int64) {
 			metadata := make(map[string]any)
 			shared.SetProgressMetadata(metadata, "create_image_from_instance_pack", "Exporting", value, 0, 0)
+			// Replace metadata to show only current progress, avoiding stale keys from previous stages.
 			_ = op.UpdateMetadata(metadata)
 		},
 	}
