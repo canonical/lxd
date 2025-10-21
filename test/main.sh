ddf93d6ad0ee1e4d0b4a3236484b9d4c293fe3aa
Kadin Sayani <kadin.sayani@canonical.com>
2025-10-21 15:51:47 -0600

    tests: Include test_clustering_heal_networks_stop() in cluster suite
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    (cherry picked from commit eccd64bcea490452d6a6217f7f9e6df070846811)

--- test/main.sh
+++ test/main.sh
@@ -451,6 +451,7 @@ if [ "${1:-"all"}" != "snap" ] && [ "${1:-"all"}" != "standalone" ]; then
     run_test test_clustering_uuid "clustering uuid"
     run_test test_clustering_trust_add "clustering trust add"
     run_test test_clustering_waitready "clustering waitready"
+    run_test test_clustering_heal_networks_stop "clustering heal networks stop"
 fi
 
 if [ "${1:-"all"}" != "snap" ] && [ "${1:-"all"}" != "cluster" ]; then
