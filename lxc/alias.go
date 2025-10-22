af56cff4a734d59ecf8283f1453de9a684a6c3c4
tugbataluy <tugba.taluy@canonical.com>
2025-10-22 14:18:51 +0300

    lxc/alias: Add additional libraries used by  export command
    Signed-off-by: tugbataluy <tugba.taluy@canonical.com>

--- lxc/alias.go
+++ lxc/alias.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
 	"sort"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
+	"go.yaml.in/yaml/v2"
 
+	"github.com/canonical/lxd/lxc/config"
 	cli "github.com/canonical/lxd/shared/cmd"
 	"github.com/canonical/lxd/shared/i18n"
+	"github.com/canonical/lxd/shared/logger"
 )
 
 type cmdAlias struct {
