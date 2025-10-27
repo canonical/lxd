f514785f5dc98cde667d32f91518efdcd023339c
Simon Deziel <simon.deziel@canonical.com>
2025-10-27 16:57:21 -0400

    github: move `download_minio` helper to action dir
    This should make the action self-contained and thus easily reusable in other repos.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 18d2b7a458ffdbf2212c7673651411590934e4cc)

--- test/includes/setup.sh
+++ test/includes/setup.sh
@@ -88,21 +88,6 @@ download_test_images() {
     )
 }
 
-# download_minio: downloads minio server and mc client binaries to GOPATH/bin or /usr/local/bin.
-download_minio() {
-    local arch dir
-    dir="${GOPATH:-${HOME}/go}/bin"
-    mkdir -p "${dir}"
-
-    arch="${ARCH:-$(dpkg --print-architecture || echo "amd64")}"
-
-    # Download minio and mc binaries
-    curl --show-error --silent --retry 3 --retry-delay 5 \
-        --continue-at - "https://dl.min.io/server/minio/release/linux-${arch}/minio" --output "${dir}/minio" \
-        --continue-at - "https://dl.min.io/client/mc/release/linux-${arch}/mc"       --output "${dir}/mc"
-    chmod +x "${dir}/minio" "${dir}/mc"
-}
-
 install_tools() {
     local pkg="${1}"
 
