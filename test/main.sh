d32f4b1f41e6233d1e005077e96412b6660a7bc8
Kadin Sayani <kadin.sayani@canonical.com>
2025-10-29 11:40:34 -0600

    tests: Include clustering evacuation/restore operations test in cluster suite
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    (cherry picked from commit da418a21f69baaed7ae71d8690ae1c646a4b174f)

--- test/main.sh
+++ test/main.sh
@@ -447,6 +447,7 @@ if [ "${1:-"all"}" != "snap" ] && [ "${1:-"all"}" != "standalone" ]; then
     run_test test_clustering_image_refresh "clustering image refresh"
     run_test test_clustering_evacuation "clustering evacuation"
     run_test test_clustering_instance_placement_scriptlet "clustering instance placement scriptlet"
+    run_test test_clustering_evacuation_restore_operations "clustering evacuation/restore operations"
     run_test test_clustering_move "clustering move"
     run_test test_clustering_remove_members "clustering config remove members"
     run_test test_clustering_autotarget "clustering autotarget member"
