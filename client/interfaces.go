ff452d2770871ceeb2f8ddb15a7f6c752e2cacc6
Din Music <din.music@canonical.com>
2025-11-12 13:17:12 +0000

    client/lxd_storage_volumes: Update volume returns an operation
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -388,7 +388,7 @@ type InstanceServer interface {
 	GetStoragePoolVolume(pool string, volType string, name string) (volume *api.StorageVolume, ETag string, err error)
 	GetStoragePoolVolumeState(pool string, volType string, name string) (state *api.StorageVolumeState, err error)
 	CreateStoragePoolVolume(pool string, volume api.StorageVolumesPost) (op Operation, err error)
-	UpdateStoragePoolVolume(pool string, volType string, name string, volume api.StorageVolumePut, ETag string) (err error)
+	UpdateStoragePoolVolume(pool string, volType string, name string, volume api.StorageVolumePut, ETag string) (op Operation, err error)
 	DeleteStoragePoolVolume(pool string, volType string, name string) (err error)
 	RenameStoragePoolVolume(pool string, volType string, name string, volume api.StorageVolumePost) (err error)
 	CopyStoragePoolVolume(pool string, source InstanceServer, sourcePool string, volume api.StorageVolume, args *StoragePoolVolumeCopyArgs) (op RemoteOperation, err error)
