22d6ad73ebcb454699b1569bc8addedb83e057d5
Stanislav Kozina <stanislav.kozina@canonical.com>
2025-11-28 14:33:48 +0100

    lxd: Introduce durable operation hooks map
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -1194,6 +1194,9 @@ func (d *Daemon) init() error {
 
 	var err error
 
+	// Hack to make a durable operations handler map defined in the main package available to the operations
+	operations.InitDurableOperations(DurableOperations)
+
 	// Set default authorizer.
 	d.authorizer, err = authDrivers.LoadAuthorizer(d.shutdownCtx, authDrivers.DriverTLS, logger.Log, authDrivers.WithSendSecurity(d.events.SendSecurity))
 	if err != nil {
