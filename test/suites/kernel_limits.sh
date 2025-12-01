ae5668aaa19ecbb42455f8c47a6e2a31bd6cfc2b
Simon Deziel <simon.deziel@canonical.com>
2025-12-01 17:48:55 -0500

    test/suites/kernel_limits: simplify test
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/kernel_limits.sh
+++ test/suites/kernel_limits.sh
@@ -2,16 +2,15 @@ test_kernel_limits() {
   echo "==> API extension kernel_limits"
 
   ensure_import_testimage
-  lxc init testimage limits
-  # Set it to a limit < 65536 because older systemd's do not have my nofile
+  lxc launch testimage limits --config limits.kernel.nofile=3000
+  # Set it to a limit < 65536 because older systemd's do not have any nofile
   # limit patch.
-  lxc config set limits limits.kernel.nofile 3000
-  lxc start limits
-  pid="$(lxc info limits | awk '/^PID/ {print $2}')"
-  soft="$(awk '/^Max open files/ {print $4}' /proc/"${pid}"/limits)"
-  hard="$(awk '/^Max open files/ {print $5}' /proc/"${pid}"/limits)"
+  pid="$(lxc list -f csv -c p limits)"
+
+  # Extract soft and hard limits from /proc/<pid>/limits
+  limits="$(awk '/^Max open files/ {print $4 " " $5}' "/proc/${pid}/limits")"
 
   lxc delete --force limits
 
-  [ "${soft}" = "3000" ] && [ "${hard}" = "3000" ]
+  [ "${limits}" = "3000 3000" ]
 }
