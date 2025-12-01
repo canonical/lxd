1ef61ffd165fdbdd826afcb6ec2f6488e7d66ffa
Simon Deziel <simon.deziel@canonical.com>
2025-12-01 09:16:39 -0500

    test/suites/query: no need to import `localhost` remote
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/query.sh
+++ test/suites/query.sh
@@ -1,6 +1,4 @@
 test_query() {
-  ensure_has_localhost_remote "${LXD_ADDR}"
-
   lxc init --empty querytest
   lxc query --wait -X POST -d '{"name": "snap-test"}' /1.0/containers/querytest/snapshots
   lxc info querytest | grep -wF snap-test
