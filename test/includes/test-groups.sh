f043ff47f85e2d122fbf86d6b0b4e35d4e4539ab
Din Music <din.music@canonical.com>
2025-12-03 10:52:39 +0000

    test/live_migration: Add VM live migration test
    Signed-off-by: Din Music <din.music@canonical.com>

--- test/includes/test-groups.sh
+++ test/includes/test-groups.sh
@@ -51,6 +51,7 @@ readonly test_group_cluster=(
 readonly test_group_cluster_storage=(
     "clustering_image_refresh"
     "clustering_image_replication"
+    "clustering_live_migration"
     "clustering_publish"
     "clustering_recovery"
     "clustering_storage"
