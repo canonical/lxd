0384552ab1c2169667dffb7af9411d465190457b
Minae Lee <minae.lee@canonical.com>
2025-12-03 23:03:21 -0700

    doc: set html_baseurl dynamically
    Replaces the statically set value for html_baseurl to use RTD
    environment variable, or "/" if not available (for local builds). This
    ensures that the HTML metadata canonical URL is set correctly, including
    the version in the URL.
    
    This change required also updating the sitemap_url_scheme to prevent
    duplicating the version string, since the sitemap extension uses
    html_baseurl to create the link.
    
    References:
    - https://docs.readthedocs.com/platform/stable/canonical-urls.html
    - https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_baseurl
    
    Signed-off-by: Minae Lee <minae.lee@canonical.com>

--- doc/conf.py
+++ doc/conf.py
@@ -108,19 +108,15 @@ slug = "lxd"
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
 #######################
 
-# Base URL of RTD hosted project
-html_baseurl = 'https://documentation.ubuntu.com/lxd/'
+# Use RTD canonical URL to ensure duplicate pages have a single canonical URL 
+# that includes the version (such as /latest/); helps SEO.
+# See: https://docs.readthedocs.com/platform/stable/canonical-urls.html and
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_baseurl
+# Second argument is for local builds where the READTHEDOCS variable is not available
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
-# When configured with RTD variables, check for RTD environment so manual runs succeed
-# NOTE: The Sphinx starter pack uses `version`; LXD uses `rtd_version` because `version`
-# is already used
-
-if 'READTHEDOCS_VERSION' in os.environ:
-    rtd_version = os.environ["READTHEDOCS_VERSION"]
-    sitemap_url_scheme = f'{rtd_version}/{{link}}'
-else:
-    sitemap_url_scheme = '{link}'
-
+# The sitemap extension uses html_baseurl to generate the full URL for each page
+sitemap_url_scheme = '{link}'
 
 #############
 # Redirects #
