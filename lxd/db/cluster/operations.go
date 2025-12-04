18d3a8abb065f9721639e068814c1098195b0bc7
Stanislav Kozina <stanislav.kozina@canonical.com>
2025-12-04 13:01:21 +0100

    lxd/db/cluster/operations: Introduce `UpdateOperationNodeID()`
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/db/cluster/operations.go
+++ lxd/db/cluster/operations.go
@@ -62,6 +62,27 @@ type OperationFilter struct {
 	Class     *int64
 }
 
+// UpdateOperationNodeID updates the node_id field of an existing operation in the cluster db.
+func UpdateOperationNodeID(ctx context.Context, tx *sql.Tx, opReference string, newNodeID int64) error {
+	stmt := `UPDATE operations SET node_id = ? WHERE reference = ?`
+
+	result, err := tx.ExecContext(ctx, stmt, newNodeID, opReference)
+	if err != nil {
+		return fmt.Errorf("Failed updating operation node ID: %w", err)
+	}
+
+	n, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Fetch affected rows: %w", err)
+	}
+
+	if n != 1 {
+		return fmt.Errorf("Query updated %d rows instead of 1", n)
+	}
+
+	return nil
+}
+
 // GetDurableOperationMetadata retrieves metadata key/value pairs for a durable operation from the cluster db.
 func GetDurableOperationMetadata(ctx context.Context, tx *sql.Tx, opID int64) (map[string]string, error) {
 	stmt := `SELECT key, value FROM operations_metadata WHERE operation_id = ?`
