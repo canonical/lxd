1194a46e2e174f3cfaf7c0698c57fe6d0d2f2974
Simon Deziel <simon.deziel@canonical.com>
2025-12-05 14:54:51 -0500

    lxd-benchmark: use `runtime.NumCPU()` for default batch size
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd-benchmark/benchmark/batch.go
+++ lxd-benchmark/benchmark/batch.go
@@ -1,24 +1,17 @@
 package benchmark
 
 import (
-	"os"
+	"runtime"
 	"sync"
 	"time"
 )
 
 func getBatchSize(parallel int) (int, error) {
-	batchSize := parallel
-	if batchSize < 1 {
-		// Detect the number of parallel actions
-		cpus, err := os.ReadDir("/sys/bus/cpu/devices")
-		if err != nil {
-			return -1, err
-		}
-
-		batchSize = len(cpus)
+	if parallel > 0 {
+		return parallel, nil
 	}
 
-	return batchSize, nil
+	return runtime.NumCPU(), nil
 }
 
 func processBatch(count int, batchSize int, process func(index int, wg *sync.WaitGroup)) time.Duration {
