fc771aae2c308f425ad113872a0cb262e6b7b4b1
Simon Deziel <simon.deziel@canonical.com>
2025-12-12 15:35:31 -0500

    lxc/main: remove `i18n`
    Also replace https://multipass.run by https://canonical.com/multipass to avoid
    the permanent redirection.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxc/main.go
+++ lxc/main.go
@@ -18,7 +18,6 @@ import (
 	"github.com/canonical/lxd/shared"
 	"github.com/canonical/lxd/shared/api"
 	cli "github.com/canonical/lxd/shared/cmd"
-	"github.com/canonical/lxd/shared/i18n"
 	"github.com/canonical/lxd/shared/logger"
 	"github.com/canonical/lxd/shared/version"
 )
@@ -82,25 +81,24 @@ func main() {
 	// Setup the parser
 	app := &cobra.Command{}
 	app.Use = "lxc"
-	app.Short = i18n.G("Command line client for LXD")
-	app.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
-		`Command line client for LXD
+	app.Short = "Command line client for LXD"
+	app.Long = cli.FormatSection("Description", `Command line client for LXD
 
 All of LXD's features can be driven through the various commands below.
-For help with any of those, simply call them with --help.`))
+For help with any of those, simply call them with --help.`)
 	app.SilenceUsage = true
 	app.SilenceErrors = true
 
 	// Global flags
 	globalCmd := cmdGlobal{cmd: app, asker: cli.NewAsker(bufio.NewReader(os.Stdin), nil)}
-	app.PersistentFlags().BoolVar(&globalCmd.flagVersion, "version", false, i18n.G("Print version number"))
-	app.PersistentFlags().BoolVarP(&globalCmd.flagHelp, "help", "h", false, i18n.G("Print help"))
-	app.PersistentFlags().BoolVar(&globalCmd.flagForceLocal, "force-local", false, i18n.G("Force using the local unix socket"))
-	app.PersistentFlags().StringVar(&globalCmd.flagProject, "project", "", i18n.G("Override the source project")+"``")
-	app.PersistentFlags().BoolVar(&globalCmd.flagLogDebug, "debug", false, i18n.G("Show all debug messages"))
-	app.PersistentFlags().BoolVarP(&globalCmd.flagLogVerbose, "verbose", "v", false, i18n.G("Show all information messages"))
-	app.PersistentFlags().BoolVarP(&globalCmd.flagQuiet, "quiet", "q", false, i18n.G("Don't show progress information"))
-	app.PersistentFlags().BoolVar(&globalCmd.flagSubCmds, "sub-commands", false, i18n.G("Use with help or --help to view sub-commands"))
+	app.PersistentFlags().BoolVar(&globalCmd.flagVersion, "version", false, "Print version number")
+	app.PersistentFlags().BoolVarP(&globalCmd.flagHelp, "help", "h", false, "Print help")
+	app.PersistentFlags().BoolVar(&globalCmd.flagForceLocal, "force-local", false, "Force using the local unix socket")
+	app.PersistentFlags().StringVar(&globalCmd.flagProject, "project", "", "Override the source project"+"``")
+	app.PersistentFlags().BoolVar(&globalCmd.flagLogDebug, "debug", false, "Show all debug messages")
+	app.PersistentFlags().BoolVarP(&globalCmd.flagLogVerbose, "verbose", "v", false, "Show all information messages")
+	app.PersistentFlags().BoolVarP(&globalCmd.flagQuiet, "quiet", "q", false, "Don't show progress information")
+	app.PersistentFlags().BoolVar(&globalCmd.flagSubCmds, "sub-commands", false, "Use with help or --help to view sub-commands")
 
 	_ = app.RegisterFlagCompletionFunc("project", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		// Default
@@ -307,8 +305,8 @@ For help with any of those, simply call them with --help.`))
 	}
 
 	// Help flags
-	app.Flags().BoolVar(&globalCmd.flagHelpAll, "all", false, i18n.G("Show less common commands"))
-	help.Flags().BoolVar(&globalCmd.flagHelpAll, "all", false, i18n.G("Show less common commands"))
+	app.Flags().BoolVar(&globalCmd.flagHelpAll, "all", false, "Show less common commands")
+	help.Flags().BoolVar(&globalCmd.flagHelpAll, "all", false, "Show less common commands")
 
 	// Deal with --all flag and --sub-commands flag
 	err = app.ParseFlags(os.Args[1:])
@@ -338,11 +336,11 @@ For help with any of those, simply call them with --help.`))
 	if err != nil {
 		// Handle non-Linux systems
 		if err == config.ErrNotLinux {
-			msg := i18n.G(`This client hasn't been configured to use a remote LXD server yet.
+			msg := `This client hasn't been configured to use a remote LXD server yet.
 As your platform can't run native Linux instances, you must connect to a remote LXD server.
 
 If you already added a remote server, make it the default with "lxc remote switch NAME".
-To easily setup a local LXD server in a virtual machine, consider using: https://multipass.run`)
+To easily setup a local LXD server in a virtual machine, consider using: https://canonical.com/multipass`
 			fmt.Fprintln(os.Stderr, msg)
 			os.Exit(1)
 		}
@@ -407,7 +405,7 @@ func (c *cmdGlobal) PreRun(cmd *cobra.Command, args []string) error {
 
 	// Setup password helper
 	c.conf.PromptPassword = func(filename string) (string, error) {
-		return c.asker.AskPasswordOnce(fmt.Sprintf(i18n.G("Password for %s: "), filename)), nil
+		return c.asker.AskPasswordOnce(fmt.Sprintf("Password for %s: ", filename)), nil
 	}
 
 	// If the user is running a command that may attempt to connect to the local daemon
@@ -446,14 +444,14 @@ func (c *cmdGlobal) PreRun(cmd *cobra.Command, args []string) error {
 
 			flush := false
 			if runInit {
-				msg := i18n.G("If this is your first time running LXD on this machine, you should also run: lxd init")
+				msg := "If this is your first time running LXD on this machine, you should also run: lxd init"
 				fmt.Fprintln(os.Stderr, msg)
 				flush = true
 			}
 
 			if !slices.Contains([]string{"init", "launch"}, cmd.Name()) {
-				msg := i18n.G(`To start your first container, try: lxc launch ubuntu:24.04
-Or for a virtual machine: lxc launch ubuntu:24.04 --vm`)
+				msg := `To start your first container, try: lxc launch ubuntu:24.04
+Or for a virtual machine: lxc launch ubuntu:24.04 --vm`
 				fmt.Fprintln(os.Stderr, msg)
 				flush = true
 			}
@@ -555,7 +553,7 @@ func (c *cmdGlobal) CheckArgs(cmd *cobra.Command, args []string, minArgs int, ma
 			return true, nil
 		}
 
-		msg := i18n.G("Invalid number of arguments")
+		msg := "Invalid number of arguments"
 		return true, errors.New(msg)
 	}
 
