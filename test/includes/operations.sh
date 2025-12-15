cd8765d0ed2522be64d4efea55d616db0da00424
Simon Deziel <simon.deziel@canonical.com>
2025-12-15 10:55:12 -0500

    test/includes/operations: add `check_registered_operations()` and `terminate_leftovers()` helpers
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit ce56eef57f566d1ebe7be8235c5128a8242789bc)

--- test/includes/operations.sh
+++ test/includes/operations.sh
@@ -20,3 +20,23 @@ lxd_volume_operation() {
 
   lxc query --wait -X POST -d '{"duration": "'"${duration}"'", "op_class": 1, "op_type": 48, "resources": {"storage_volumes": ["/1.0/storage-pools/'"${pool_name}"'/volumes/custom/'"${volume_name}"'?project='"${project_name}"'"]}}' "/internal/testing/operation-wait?project=${project_name}"
 }
+
+# check_registered_operations checks for registered operations.
+# It waits for operations to be registered and then ensures that all the PIDs
+# provided correspond to active PIDs.
+check_registered_operations() {
+  sleep 0.5
+  local pid
+  for pid in "$@"; do
+    [ -d "/proc/${pid}" ] || return 1
+  done
+}
+
+# terminate_leftovers terminates any leftover processes given their PIDs.
+# Missing PIDs are ignored.
+terminate_leftovers() {
+  local pid
+  for pid in "$@"; do
+    kill -9 "${pid}" || true
+  done
+}
