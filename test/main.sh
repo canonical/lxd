b52d4ff9015530e762db1117599b8b49532e50f5
Simon Deziel <simon.deziel@canonical.com>
2025-12-17 09:10:06 -0500

    test/main: dependency checks to a function
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -77,6 +77,44 @@ import_subdir_files() {
     done
 }
 
+run_dependency_checks() {
+  echo "==> Checking for dependencies"
+  check_dependencies lxd lxc curl busybox dnsmasq expect iptables jq nc ping python3 yq git s3cmd sqlite3 rsync shuf setfacl setfattr socat swtpm dig tar2sqfs unsquashfs xz
+  if [ "${LXD_VM_TESTS}" = "1" ]; then
+    check_dependencies qemu-img "qemu-system-$(uname -m)" sgdisk
+  fi
+  if ! check_dependencies minio mc; then
+    download_minio
+  fi
+
+  echo "==> Checking test dependencies"
+  if ! check_dependencies devlxd-client lxd-client fuidshift mini-oidc sysinfo; then
+    make -C "${MAIN_DIR}/.." test-binaries
+  fi
+
+  # If no test image is specified, busybox-static will be needed by test/deps/import-busybox
+  if [ -z "${LXD_TEST_IMAGE:-}" ]; then
+    BUSYBOX="$(command -v busybox)"
+    if [ ! -e "${BUSYBOX}" ]; then
+        echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
+        exit 1
+    fi
+
+    if ldd "${BUSYBOX}" >/dev/null 2>&1; then
+        echo "The testsuite requires ${BUSYBOX} to be a static binary"
+        exit 1
+    fi
+
+    # Cache the busybox testimage for reuse
+    deps/import-busybox --save-image
+
+    # Avoid `.tar.xz` extension that may conflict with some tests
+    mv busybox.tar.xz busybox.tar.xz.cache
+    export LXD_TEST_IMAGE="busybox.tar.xz.cache"
+    echo "==> Saving testimage for reuse (${LXD_TEST_IMAGE})"
+  fi
+}
+
 # `main.sh` needs to be executed from inside the `test/` directory
 if [ "${PWD}" != "$(dirname "${0}")" ]; then
     cd "$(dirname "${0}")"
@@ -92,41 +130,7 @@ install_storage_driver_tools
 # Install needed instance drivers
 install_instance_drivers
 
-echo "==> Checking for dependencies"
-check_dependencies lxd lxc curl busybox dnsmasq expect iptables jq nc ping python3 yq git s3cmd sqlite3 rsync shuf setfacl setfattr socat swtpm dig tar2sqfs unsquashfs xz
-if [ "${LXD_VM_TESTS}" = "1" ]; then
-  check_dependencies qemu-img "qemu-system-$(uname -m)" sgdisk
-fi
-if ! check_dependencies minio mc; then
-  download_minio
-fi
-
-echo "==> Checking test dependencies"
-if ! check_dependencies devlxd-client lxd-client fuidshift mini-oidc sysinfo; then
-  make -C "${MAIN_DIR}/.." test-binaries
-fi
-
-# If no test image is specified, busybox-static will be needed by test/deps/import-busybox
-if [ -z "${LXD_TEST_IMAGE:-}" ]; then
-  BUSYBOX="$(command -v busybox)"
-  if [ ! -e "${BUSYBOX}" ]; then
-      echo "Please install busybox (busybox-static) or set LXD_TEST_IMAGE"
-      exit 1
-  fi
-
-  if ldd "${BUSYBOX}" >/dev/null 2>&1; then
-      echo "The testsuite requires ${BUSYBOX} to be a static binary"
-      exit 1
-  fi
-
-  # Cache the busybox testimage for reuse
-  deps/import-busybox --save-image
-
-  # Avoid `.tar.xz` extension that may conflict with some tests
-  mv busybox.tar.xz busybox.tar.xz.cache
-  export LXD_TEST_IMAGE="busybox.tar.xz.cache"
-  echo "==> Saving testimage for reuse (${LXD_TEST_IMAGE})"
-fi
+run_dependency_checks
 
 # find the path to lxc binary, not the shell wrapper function
 _LXC="$(unset -f lxc; command -v lxc)"
