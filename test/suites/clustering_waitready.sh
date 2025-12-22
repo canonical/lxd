9fff6236d9d22f718680cc140f5a3559f9f688e5
Simon Deziel <simon.deziel@canonical.com>
2025-12-22 09:55:17 -0500

    test/suites/clustering_waitready: simplify how `LXD_ONE_DIR` is set
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/clustering_waitready.sh
+++ test/suites/clustering_waitready.sh
@@ -8,9 +8,9 @@ test_clustering_waitready() {
   bridge="${prefix}"
 
   setup_clustering_netns 1
-  LXD_ONE_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
   ns1="${prefix}1"
-  spawn_lxd_and_bootstrap_cluster "${ns1}" "${bridge}" "${LXD_ONE_DIR}"
+  spawn_lxd_and_bootstrap_cluster "${ns1}" "${bridge}"
+  LXD_ONE_DIR="${LXD_DIR}"
 
   # Get used storage backend.
   lxd_backend=$(storage_backend "${LXD_ONE_DIR}")
