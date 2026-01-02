09216ad171a9c5ad3393fc961a0890d8abf5356d
Simon Deziel <simon.deziel@canonical.com>
2026-01-02 17:31:33 -0500

    test/includes/storage: use a specific file name pattern in `configure_loop_device`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/storage.sh
+++ test/includes/storage.sh
@@ -88,7 +88,7 @@ configure_loop_device() {
     local lv_loop_file pvloopdev
 
     # shellcheck disable=SC2153
-    lv_loop_file="$(mktemp -p "${TEST_DIR}" XXXX.img)"
+    lv_loop_file="$(mktemp -p "${TEST_DIR}" lxdtest-XXX.img)"
     truncate -s 10G "${lv_loop_file}"
     if ! pvloopdev="$(losetup --show -f "${lv_loop_file}")"; then
         echo "failed to setup loop" >&2
