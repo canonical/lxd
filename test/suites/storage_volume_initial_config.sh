ce5aa89b5dc8136da73390d3f508376a56259361
Simon Deziel <simon.deziel@canonical.com>
2026-01-05 14:43:16 -0500

    test/suites/storage_volume_initial_config: avoid unneeded `--force`
    Also replace `lxc rm` by `lxc delete`.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/storage_volume_initial_config.sh
+++ test/suites/storage_volume_initial_config.sh
@@ -30,27 +30,27 @@ test_storage_volume_initial_config() {
   # Test default configuration (without initial configuration).
   lxc init "${image}" c --profile "${profile}"
   [ "$(lxc storage volume get "${pool}" container/c block.filesystem)" = "ext4" ]
-  lxc rm c
+  lxc delete c
 
   lxc init c --empty --profile "${profile}"
   [ "$(lxc storage volume get "${pool}" container/c block.filesystem)" = "ext4" ]
-  lxc rm c
+  lxc delete c
 
   # Test profile initial configuration.
   lxc profile device set "${profile}" root initial.block.filesystem=btrfs
 
   lxc init "${image}" c --profile "${profile}"
   [ "$(lxc storage volume get "${pool}" container/c block.filesystem)" = "btrfs" ]
-  lxc rm c
+  lxc delete c
 
   lxc init c --empty --profile "${profile}"
   [ "$(lxc storage volume get "${pool}" container/c block.filesystem)" = "btrfs" ]
-  lxc rm c
+  lxc delete c
 
   # Test instance initial configuration.
   lxc init "${image}" c -s "${pool}" --no-profiles --device root,initial.block.filesystem=btrfs
   [ "$(lxc storage volume get "${pool}" container/c block.filesystem)" = "btrfs" ]
-  lxc rm c
+  lxc delete c
 
   lxc init c --empty -s "${pool}" --no-profiles --device root,initial.block.filesystem=btrfs
   [ "$(lxc storage volume get "${pool}" container/c block.filesystem)" = "btrfs" ]
@@ -60,7 +60,7 @@ test_storage_volume_initial_config() {
   ! lxc config device set c root initial.block.filesystem=xfs || false         # NOK: Modify existing configuration.
   lxc config device set c root initial.block.filesystem=btrfs                  # OK:  No change.
   lxc config device unset c root initial.block.filesystem                      # OK:  Remove existing configuration.
-  lxc rm c
+  lxc delete c
 
   if [ "$lxd_backend" = "zfs" ]; then
     # Clear profile and storage options.
@@ -75,14 +75,14 @@ test_storage_volume_initial_config() {
 
     lxc init c --empty --profile "${profile}"
     [ "$(lxc storage volume get "${pool}" container/c zfs.block_mode)" = "true" ]
-    lxc delete c --force
+    lxc delete c
 
     # Verify "zfs.block_mode=false" is applied from pool configuration.
     lxc storage set "${pool}" volume.zfs.block_mode=false
 
     lxc init c --empty --profile "${profile}"
     [ "$(lxc storage volume get "${pool}" container/c zfs.block_mode)" = "false" ]
-    lxc delete c --force
+    lxc delete c
 
 
     # > Overwrite zfs.block_mode with initial configuration in profile.
@@ -93,7 +93,7 @@ test_storage_volume_initial_config() {
 
     lxc init c --empty --profile "${profile}"
     [ "$(lxc storage volume get "${pool}" container/c zfs.block_mode)" = "true" ]
-    lxc delete c --force
+    lxc delete c
 
     # Verify profile "initial.zfs.block_mode=false" configuration is applied.
     lxc storage set "${pool}" volume.zfs.block_mode=true
@@ -101,7 +101,7 @@ test_storage_volume_initial_config() {
 
     lxc init c --empty --profile "${profile}"
     [ "$(lxc storage volume get "${pool}" container/c zfs.block_mode)" = "false" ]
-    lxc delete c --force
+    lxc delete c
 
 
     # > Verify instance overwrite of initial.* configuration.
@@ -112,7 +112,7 @@ test_storage_volume_initial_config() {
 
     lxc init c --empty --profile "${profile}" --device root,initial.zfs.block_mode=true
     [ "$(lxc storage volume get "${pool}" container/c zfs.block_mode)" = "true" ]
-    lxc delete c --force
+    lxc delete c
 
     # Verify instance "initial.zfs.block_mode=false" configuration is applied.
     lxc storage set "${pool}" volume.zfs.block_mode=true
@@ -120,7 +120,7 @@ test_storage_volume_initial_config() {
 
     lxc init c --empty --profile "${profile}" --device root,initial.zfs.block_mode=false
     [ "$(lxc storage volume get "${pool}" container/c zfs.block_mode)" = "false" ]
-    lxc delete c --force
+    lxc delete c
 
 
     # > Verify initial.zfs.blocksize configuration.
@@ -129,13 +129,13 @@ test_storage_volume_initial_config() {
     lxc init "${image}" c --no-profiles --storage "${pool}" --device root,initial.zfs.blocksize=64KiB
     [ "$(lxc storage volume get "${pool}" container/c zfs.blocksize)" = "64KiB" ]
     [ "$(zfs get volblocksize "${pool}/containers/c" -H -o value)" = "64K" ]
-    lxc delete c --force
+    lxc delete c
 
     # Custom blocksize that exceeds maximum allowed blocksize.
     lxc init "${image}" c --no-profiles --storage "${pool}" --device root,initial.zfs.blocksize=512KiB
     [ "$(lxc storage volume get "${pool}" container/c zfs.blocksize)" = "512KiB" ]
     [ "$(zfs get volblocksize "${pool}/containers/c" -H -o value)" = "128K" ]
-    lxc delete c --force
+    lxc delete c
   fi
 
   # Cleanup
