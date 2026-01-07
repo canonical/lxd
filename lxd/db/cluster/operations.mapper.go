5088260ba467c05c7c81ad0f399dc83e6dbe9863
Stanislav Kozina <stanislav.kozina@canonical.com>
2026-01-07 11:11:36 +0100

    lxd/db/cluster: Add directive to get operations by Class
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/db/cluster/operations.mapper.go
+++ lxd/db/cluster/operations.mapper.go
@@ -49,6 +49,14 @@ SELECT operations.id, operations.uuid, coalesce(nodes.address, '') AS node_addre
   ORDER BY operations.id, operations.uuid
 `)
 
+var operationObjectsByClass = RegisterStmt(`
+SELECT operations.id, operations.uuid, coalesce(nodes.address, '') AS node_address, operations.project_id, operations.node_id, operations.type, operations.requestor_protocol, operations.requestor_identity_id, operations.entity_id, operations.metadata, operations.class, operations.created_at, operations.updated_at, operations.inputs, operations.status_code, operations.conflict_reference, operations.error, operations.parent, operations.stage
+  FROM operations
+  LEFT JOIN nodes ON operations.node_id = nodes.id
+  WHERE ( operations.class = ? )
+  ORDER BY operations.id, operations.uuid
+`)
+
 var operationObjectsByID = RegisterStmt(`
 SELECT operations.id, operations.uuid, coalesce(nodes.address, '') AS node_address, operations.project_id, operations.node_id, operations.type, operations.requestor_protocol, operations.requestor_identity_id, operations.entity_id, operations.metadata, operations.class, operations.created_at, operations.updated_at, operations.inputs, operations.status_code, operations.conflict_reference, operations.error, operations.parent, operations.stage
   FROM operations
@@ -260,6 +268,30 @@ func GetOperations(ctx context.Context, tx *sql.Tx, filters ...OperationFilter) 
 			}
 
 			query, err := StmtString(operationObjectsByConflictReference)
+			if err != nil {
+				return nil, fmt.Errorf("Failed to get \"operationObjects\" prepared statement: %w", err)
+			}
+
+			parts := strings.SplitN(query, "ORDER BY", 2)
+			if i == 0 {
+				copy(queryParts[:], parts)
+				continue
+			}
+
+			_, where, _ := strings.Cut(parts[0], "WHERE")
+			queryParts[0] += "OR" + where
+		} else if filter.Class != nil && filter.ID == nil && filter.NodeID == nil && filter.UUID == nil && filter.ConflictReference == nil {
+			args = append(args, []any{filter.Class}...)
+			if len(filters) == 1 {
+				sqlStmt, err = Stmt(tx, operationObjectsByClass)
+				if err != nil {
+					return nil, fmt.Errorf("Failed to get \"operationObjectsByClass\" prepared statement: %w", err)
+				}
+
+				break
+			}
+
+			query, err := StmtString(operationObjectsByClass)
 			if err != nil {
 				return nil, fmt.Errorf("Failed to get \"operationObjects\" prepared statement: %w", err)
 			}
