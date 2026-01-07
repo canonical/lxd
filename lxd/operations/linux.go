d1cbbe8cd732501aa29c69951c51ba202bd7c6c7
Stanislav Kozina <stanislav.kozina@canonical.com>
2026-01-07 15:13:29 +0100

    lxd/operations: Introduce `NewDurableOperation()` constructor
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/operations/linux.go
+++ lxd/operations/linux.go
@@ -4,12 +4,17 @@ package operations
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 
 	"github.com/canonical/lxd/lxd/db"
 	"github.com/canonical/lxd/lxd/db/cluster"
 	"github.com/canonical/lxd/lxd/db/operationtype"
+	"github.com/canonical/lxd/lxd/state"
 	"github.com/canonical/lxd/shared/api"
+	"github.com/canonical/lxd/shared/cancel"
+	"github.com/canonical/lxd/shared/logger"
+	"github.com/canonical/lxd/shared/version"
 )
 
 func registerDBOperation(op *Operation, opType operationtype.Type) error {
@@ -77,4 +82,44 @@ func (op *Operation) sendEvent(eventMessage any) {
 	}
 
 	_ = op.events.Send(op.projectName, api.EventTypeOperation, eventMessage)
+}
+
+// NewDurableOperation is a constructor of the Operation object based on its database representation.
+func NewDurableOperation(ctx context.Context, tx *sql.Tx, s *state.State, dbOp *cluster.Operation, projectName string) (*Operation, error) {
+	if dbOp.Class != int64(OperationClassDurable) {
+		return nil, fmt.Errorf("Operation %s is not of durable class", dbOp.Reference)
+	}
+
+	op := Operation{}
+	op.projectName = projectName
+	op.id = dbOp.Reference
+	op.description = dbOp.Type.Description()
+	op.entityType, op.entitlement = dbOp.Type.Permission()
+	op.dbOpType = dbOp.Type
+	op.class = OperationClass(dbOp.Class)
+	op.createdAt = dbOp.CreatedAt
+	op.updatedAt = dbOp.CreatedAt
+	op.status = api.StatusCode(dbOp.Status)
+	op.url = api.NewURL().Path(version.APIVersion, "operations", dbOp.Reference).String()
+	op.finished = cancel.New()
+	op.running = cancel.New()
+	op.state = s
+	op.requestor = nil
+	op.logger = logger.AddContext(logger.Ctx{"operation": op.id, "project": op.projectName, "class": op.class.String(), "description": op.description})
+
+	if s != nil {
+		op.SetEventServer(s.Events)
+	}
+
+	op.resources = nil
+	op.metadata = nil
+
+	runHook, ok := durableOperations[op.dbOpType]
+	if !ok {
+		return nil, fmt.Errorf("No durable operation handlers defined for operation type %q", op.dbOpType)
+	}
+
+	op.onRun = runHook
+
+	return &op, nil
 }
