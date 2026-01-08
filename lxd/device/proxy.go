12099dd02fdec6d5d68aeac3bbf0e739dd80ff94
Simon Deziel <simon.deziel@canonical.com>
2026-01-08 12:07:49 -0500

    lxd/device/proxy: use `strings.Cut` in `rewriteHostAddr()`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/device/proxy.go
+++ lxd/device/proxy.go
@@ -572,9 +572,7 @@ func (d *proxy) setupNAT() error {
 }
 
 func (d *proxy) rewriteHostAddr(addr string) string {
-	fields := strings.SplitN(addr, ":", 2)
-	proto := fields[0]
-	addr = fields[1]
+	proto, addr, _ := strings.Cut(addr, ":")
 	if proto == "unix" && !strings.HasPrefix(addr, "@") {
 		// Unix non-abstract sockets need to be addressed to the host
 		// filesystem, not be scoped inside the LXD snap.
