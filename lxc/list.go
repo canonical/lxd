842b32bf6af89e1400ca7da52daa46325142c4aa
tugbataluy <tugba.taluy@canonical.com>
2026-01-08 13:46:50 +0300

    lxc/list.go: Fix lint-checker errors.
    Signed-off-by: tugbataluy <tugba.taluy@canonical.com>

--- lxc/list.go
+++ lxc/list.go
@@ -422,7 +422,7 @@ func (c *cmdList) showInstances(instances []api.InstanceFull, filters []string, 
 
 		instancesFiltered = append(instancesFiltered, inst)
 
-		col := []string{}
+		col := make([]string, 0, len(columns))
 		for _, column := range columns {
 			col = append(col, column.Data(inst))
 		}
@@ -432,7 +432,7 @@ func (c *cmdList) showInstances(instances []api.InstanceFull, filters []string, 
 
 	sort.Sort(cli.SortColumnsNaturally(data))
 
-	headers := []string{}
+	headers := make([]string, 0, len(columns))
 	for _, column := range columns {
 		headers = append(headers, column.Name)
 	}
