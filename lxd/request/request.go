a02af32f224c905f55e56861ac7342a17663d7cd
Mark Laing <mark.laing@canonical.com>
2026-01-12 15:37:14 +0000

    lxd/request: Remove identity.CacheEntry from Requestor.
    The authorization details contained within the CacheEntry
    will instead be stored in the Requestor itself.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/request/request.go
+++ lxd/request/request.go
@@ -2,10 +2,8 @@ package request
 
 import (
 	"context"
-	"errors"
 	"net"
 
-	"github.com/canonical/lxd/lxd/identity"
 	"github.com/canonical/lxd/shared/api"
 )
 
@@ -23,24 +21,4 @@ func CreateRequestor(ctx context.Context) *api.EventLifecycleRequestor {
 // in the request context for later use.
 func SaveConnectionInContext(ctx context.Context, connection net.Conn) context.Context {
 	return context.WithValue(ctx, CtxConn, connection)
-}
-
-// GetCallerIdentityFromContext extracts the identity of the caller from the request context.
-// The identity is expected to be present and have a valid identifier. Otherwise, an error is returned.
-func GetCallerIdentityFromContext(ctx context.Context) (*identity.CacheEntry, error) {
-	requestor, err := GetRequestor(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	identity := requestor.CallerIdentity()
-	if identity == nil {
-		return nil, errors.New("Request context identity is missing")
-	}
-
-	if identity.Identifier == "" {
-		return nil, errors.New("Request context identity is missing an identifier")
-	}
-
-	return identity, nil
 }
