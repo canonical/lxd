c7cce3b4f0f0fac02747b8cd048e9cde50cf00b4
Thomas Parrott <thomas.parrott@canonical.com>
2026-01-16 14:16:55 +0000

    lxd/instance/drivers/driver/lxc: Switch to unified d.validateConfig function
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_lxc.go
+++ lxd/instance/drivers/driver_lxc.go
@@ -3958,7 +3958,7 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 	}
 
 	// Diff the devices
-	removeDevices, addDevices, updateDevices, allUpdatedKeys := oldExpandedDevices.Update(d.expandedDevices, func(oldDevice deviceConfig.Device, newDevice deviceConfig.Device) []string {
+	removeDevices, addDevices, updateDevices, allUpdatedDeviceKeys := oldExpandedDevices.Update(d.expandedDevices, func(oldDevice deviceConfig.Device, newDevice deviceConfig.Device) []string {
 		// This function needs to return a list of fields that are excluded from differences
 		// between oldDevice and newDevice. The result of this is that as long as the
 		// devices are otherwise identical except for the fields returned here, then the
@@ -3976,58 +3976,13 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 		return newDevType.UpdatableFields(oldDevType)
 	})
 
-	err = d.initialConfigValidate(allUpdatedKeys, addDevices, removeDevices)
+	err = d.validateConfig(allUpdatedDeviceKeys, addDevices, removeDevices, oldExpandedDevices, changedConfig, oldExpandedConfig, userRequested)
 	if err != nil {
 		return err
 	}
 
 	if userRequested {
-		// Look for deleted idmap keys.
-		protectedKeys := map[string]struct{}{
-			"volatile.idmap.base":       {},
-			"volatile.idmap.current":    {},
-			"volatile.idmap.next":       {},
-			"volatile.last_state.idmap": {},
-		}
-
-		for _, k := range changedConfig {
-			_, protected := protectedKeys[k]
-			if !protected {
-				continue
-			}
-
-			if d.expandedConfig[k] == "" {
-				return errors.New("Volatile idmap keys can't be deleted by the user")
-			}
-		}
-
-		// Do some validation of the config diff (allows mixed instance types for profiles).
-		err = instance.ValidConfig(d.state.OS, d.expandedConfig, true, instancetype.Any)
-		if err != nil {
-			return fmt.Errorf("Invalid expanded config: %w", err)
-		}
-
-		// Do full expanded validation of the devices diff.
-		err = instance.ValidDevices(d.state, d.project, d.Type(), d.localDevices, d.expandedDevices)
-		if err != nil {
-			return fmt.Errorf("Invalid expanded devices: %w", err)
-		}
-
-		// Validate root device
-		_, oldRootDev, oldErr := instancetype.GetRootDiskDevice(oldExpandedDevices.CloneNative())
-		_, newRootDev, newErr := instancetype.GetRootDiskDevice(d.expandedDevices.CloneNative())
-		if oldErr == nil && newErr == nil && oldRootDev["pool"] != newRootDev["pool"] {
-			return fmt.Errorf("Cannot update root disk device pool name to %q", newRootDev["pool"])
-		}
-
-		// Ensure the instance has a root disk.
-		if newErr != nil {
-			return fmt.Errorf("Invalid root disk device: %w", newErr)
-		}
-	}
-
-	// Run through initLXC to catch anything we missed
-	if userRequested {
+		// Run through initLXC to catch anything we missed
 		d.release()
 		d.cConfig = false
 		_, err = d.initLXC(true)
@@ -4112,7 +4067,7 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 	// Update MAAS (must run after the MAC addresses have been generated).
 	updateMAAS := false
 	for _, key := range []string{"maas.subnet.ipv4", "maas.subnet.ipv6", "ipv4.address", "ipv6.address"} {
-		if slices.Contains(allUpdatedKeys, key) {
+		if slices.Contains(allUpdatedDeviceKeys, key) {
 			updateMAAS = true
 			break
 		}
