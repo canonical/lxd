fdf59ba1b22b5b2cad3742f79cf50bd3536fd675
Rory McNamara <rory.mcnamara@snyk.io>
2026-01-17 09:05:04 -0500

    lxd/instance/drivers/lxc: Use ShellQuote instead of Quote
    This avoids potential shell expansion of the strings should some special
    characters manage to make it through.
    
    Signed-off-by: Rory McNamara <rory.mcnamara@snyk.io>
    (cherry picked from commit 0e0cf45ecdcc902a6f319f11971ed27df81bd29f)
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>
    License: Apache-2.0
    (cherry picked from commit dbc132381807a8fee20cd4cfbd12065acb6c9971)
    (cherry picked from commit 8f07a1dca0455c591555d06357c49bc0fd00d38c)
    (cherry picked from commit 0de3ea7a00b156bff3b6025434e87b1bed3863b2)

--- lxd/instance/drivers/driver_lxc.go
+++ lxd/instance/drivers/driver_lxc.go
@@ -929,19 +929,19 @@ func (d *lxc) initLXC(config bool) error {
 	}
 
 	// Call the onstart hook on start.
-	err = lxcSetConfigItem(cc, "lxc.hook.pre-start", fmt.Sprintf("/proc/%d/exe callhook %s %s %s start", os.Getpid(), shared.VarPath(""), strconv.Quote(d.Project()), strconv.Quote(d.Name())))
+	err = lxcSetConfigItem(cc, "lxc.hook.pre-start", fmt.Sprintf("/proc/%d/exe callhook %s %s %s start", os.Getpid(), shared.VarPath(""), shared.ShellQuote(d.Project()), shared.ShellQuote(d.Name())))
 	if err != nil {
 		return err
 	}
 
 	// Call the onstopns hook on stop but before namespaces are unmounted.
-	err = lxcSetConfigItem(cc, "lxc.hook.stop", fmt.Sprintf("%s callhook %s %s %s stopns", d.state.OS.ExecPath, shared.VarPath(""), strconv.Quote(d.Project()), strconv.Quote(d.Name())))
+	err = lxcSetConfigItem(cc, "lxc.hook.stop", fmt.Sprintf("%s callhook %s %s %s stopns", d.state.OS.ExecPath, shared.VarPath(""), shared.ShellQuote(d.Project()), shared.ShellQuote(d.Name())))
 	if err != nil {
 		return err
 	}
 
 	// Call the onstop hook on stop.
-	err = lxcSetConfigItem(cc, "lxc.hook.post-stop", fmt.Sprintf("%s callhook %s %s %s stop", d.state.OS.ExecPath, shared.VarPath(""), strconv.Quote(d.Project()), strconv.Quote(d.Name())))
+	err = lxcSetConfigItem(cc, "lxc.hook.post-stop", fmt.Sprintf("%s callhook %s %s %s stop", d.state.OS.ExecPath, shared.VarPath(""), shared.ShellQuote(d.Project()), shared.ShellQuote(d.Name())))
 	if err != nil {
 		return err
 	}
