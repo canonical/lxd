1639fe96bf62bfe21d855c4bc0cd726c80fae2dd
Simon Deziel <simon.deziel@canonical.com>
2026-01-19 14:57:12 -0500

    test/suites/image_acl: avoid noise from `getfacl`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/image_acl.sh
+++ test/suites/image_acl.sh
@@ -17,8 +17,8 @@ test_image_acl() {
 
   # Check if the ACLs are still present
   C2_PID="$(lxc list -f csv -c p c2)"
-  getfacl "/proc/${C2_PID}/root/root/foo" | grep -xF "user:1000001:rwx"
-  getfacl "/proc/${C2_PID}/root/root/foo" | grep -xF "group:1000001:rwx"
+  getfacl --omit-header --absolute-names "/proc/${C2_PID}/root/root/foo" | grep -xF "user:1000001:rwx"
+  getfacl --omit-header --absolute-names "/proc/${C2_PID}/root/root/foo" | grep -xF "group:1000001:rwx"
 
   lxc delete -f c1 c2
   lxc image delete c1-with-acl
