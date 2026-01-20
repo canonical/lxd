d39abf50661a3efef04f6e8f86a70b0905b41b55
Stanislav Kozina <stanislav.kozina@canonical.com>
2026-01-20 13:55:40 +0100

    lxd/operations: Remove unnecessary argument from registerDBOperation()
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/operations/operations.go
+++ lxd/operations/operations.go
@@ -207,7 +207,7 @@ func operationCreate(s *state.State, requestor *request.Requestor, args Operatio
 	operations[op.id] = &op
 	operationsLock.Unlock()
 
-	err = registerDBOperation(&op, args.Type)
+	err = registerDBOperation(&op)
 	if err != nil {
 		return nil, err
 	}
