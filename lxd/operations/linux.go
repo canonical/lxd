6f0f72e3b8b5e12c6ed764f76e60cd8c0283ca69
Stanislav Kozina <stanislav.kozina@canonical.com>
2026-01-21 11:00:01 +0100

    lxd/operations: Use `Create` rather than `CreateOrReplace` to create operatons
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/operations/linux.go
+++ lxd/operations/linux.go
@@ -61,7 +61,7 @@ func registerDBOperation(op *Operation) error {
 
 		opInfo.Metadata = string(metadataJSON)
 
-		_, err = cluster.CreateOrReplaceOperation(ctx, tx.Tx(), opInfo)
+		_, err = cluster.CreateOperation(ctx, tx.Tx(), opInfo)
 		return err
 	})
 	if err != nil {
