c7ce49df6479058ab2df45ad02273c981c113803
Kadin Sayani <kadin.sayani@canonical.com>
2026-01-21 14:15:14 -0800

    tests: Use single quoting for readability
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- test/suites/clustering_waitready.sh
+++ test/suites/clustering_waitready.sh
@@ -98,7 +98,7 @@ test_clustering_waitready() {
 
   # The first cluster member cannot be restored as the network and storage pool are not ready yet.
   # As the network is checked first, it is returned first.
-  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster restore node1 --force 2>&1)" = "Error: Failed updating cluster member state: Cannot restore \"node1\" because some networks have not started yet" ]
+  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster restore node1 --force 2>&1)" = 'Error: Failed updating cluster member state: Cannot restore "node1" because some networks have not started yet' ]
 
   echo "==> Restore the network by unsetting the external interface"
   LXD_DIR="${LXD_ONE_DIR}" lxc network unset br1 bridge.external_interfaces --target "node1"
@@ -115,7 +115,7 @@ test_clustering_waitready() {
   LXD_DIR="${LXD_ONE_DIR}" lxd waitready
 
   # The first cluster member cannot be restored as the storage pool isn't ready yet.
-  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster restore node1 --force 2>&1)" = "Error: Failed updating cluster member state: Cannot restore \"node1\" because some storage pools have not started yet" ]
+  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster restore node1 --force 2>&1)" = 'Error: Failed updating cluster member state: Cannot restore "node1" because some storage pools have not started yet' ]
 
   echo "==> Restore the storage pool directory"
   rm "${LXD_ONE_DIR}/storage-pools/pool1"
@@ -148,7 +148,7 @@ test_clustering_waitready() {
   LXD_NETNS="${ns1}" respawn_lxd "${LXD_ONE_DIR}" true
 
   # The cluster member cannot be evacuated as long as its networks and storage pools have not started.
-  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster evacuate "node1" --force 2>&1)" = "Error: Failed updating cluster member state: Cannot evacuate \"node1\" because some networks have not started yet" ]
+  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster evacuate "node1" --force 2>&1)" = 'Error: Failed updating cluster member state: Cannot evacuate "node1" because some networks have not started yet' ]
 
   echo "==> Restore the network by unsetting the external interface"
   LXD_DIR="${LXD_ONE_DIR}" lxc network unset br1 bridge.external_interfaces --target "node1"
@@ -160,7 +160,7 @@ test_clustering_waitready() {
   LXD_NETNS="${ns1}" respawn_lxd "${LXD_ONE_DIR}" true
 
   # The cluster member cannot be evacuated as long as its storage pools have not started.
-  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster evacuate "node1" --force 2>&1)" = "Error: Failed updating cluster member state: Cannot evacuate \"node1\" because some storage pools have not started yet" ]
+  [ "$(CLIENT_DEBUG="" SHELL_TRACING="" LXD_DIR="${LXD_ONE_DIR}" lxc cluster evacuate "node1" --force 2>&1)" = 'Error: Failed updating cluster member state: Cannot evacuate "node1" because some storage pools have not started yet' ]
 
   echo "==> Restore the storage pool directory"
   rm "${LXD_ONE_DIR}/storage-pools/pool1"
