f323157a0001894779dac453336c2d6287597d28
Simon Deziel <simon.deziel@canonical.com>
2026-01-22 15:33:28 -0500

    test/includes/lxc: rewrite `lxc_remote` helper
    The main change besides being cleaner and simpler is to have `LXC_LOCAL`
    accessed in a way that will not trip `set -eu`.
    
    This makes it easier to directly invoke `lxc_remote` in conjuction with `--` as
    it is typical for `lxc_remote exec c1 -- ...`.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/lxc.sh
+++ test/includes/lxc.sh
@@ -7,33 +7,31 @@ lxc() {
 
 lxc_remote() {
     { set +x; } 2>/dev/null
-    local injected arg
     local cmd_args=()
 
-    injected=0
-    # _LXC contains the path to the lxc binary
-    cmd_args+=("${_LXC}")
+    # Inject client debug flag if needed
+    if [ -n "${CLIENT_DEBUG:-}" ]; then
+        cmd_args+=("${CLIENT_DEBUG}")
+    fi
 
-    for arg in "$@"; do
-        if [ "${arg}" = "--" ]; then
-            injected=1
-            [ -n "${CLIENT_DEBUG:-}" ] && cmd_args+=("${CLIENT_DEBUG}")
-            [ -n "${LXC_LOCAL}" ] && cmd_args+=('--force-local')
-            cmd_args+=('--')
-        elif [ "${arg}" = "--force-local" ]; then
-            continue
-        else
-            cmd_args+=("${arg}")
-        fi
-    done
-
-    if [ "${injected}" = "0" ]; then
-        [ -n "${CLIENT_DEBUG:-}" ] && cmd_args+=("${CLIENT_DEBUG}")
+    # Inject --force-local if needed
+    # This is only done when "--" is present to avoid breaking commands that
+    # rely on the config file (e.g. lxc project switch).
+    if [ -n "${LXC_LOCAL:-}" ]; then
+        local arg
+        # Scan all args looking for "--" marker
+        for arg in "$@"; do
+            if [ "${arg}" = "--" ]; then
+                cmd_args+=("--force-local")
+                break
+            fi
+        done
     fi
 
     if [ -n "${SHELL_TRACING:-}" ]; then
         set -x
     fi
 
-    timeout --foreground 120 "${cmd_args[@]}"
+    # _LXC contains the path to the lxc binary
+    timeout --foreground 120 "${_LXC}" "${cmd_args[@]}" "$@"
 }
