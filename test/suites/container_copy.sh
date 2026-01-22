8f18a0240c156825c3fd7b12489cc28f30b3bae2
Julian Pelizäus <julian.pelizaeus@canonical.com>
2026-01-22 15:59:44 +0100

    test/suites/container_copy: Add new test_container_copy_start
    This new test verifies the 'lxc copy' command allows starting instances in the various scenarios.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- test/suites/container_copy.sh
+++ test/suites/container_copy.sh
@@ -101,3 +101,153 @@ do_copy() {
 
   lxc delete c1 c2
 }
+
+test_container_copy_start() {
+  local lxd_backend
+  lxd_backend="$(storage_backend "${LXD_DIR}")"
+
+  ensure_import_testimage
+  lxc init testimage c1 -d "${SMALL_ROOT_DISK}"
+
+  echo "==> Check that a copied container by default is stopped"
+  lxc copy c1 c2
+  [ "$(lxc list -f csv -c s c2)" = "STOPPED" ]
+
+  lxc delete -f c2
+
+  echo "==> Check that a container can be copied and started with the same command"
+  lxc copy c1 c2 --start
+
+  echo "==> Check the copied container is running"
+  [ "$(lxc list -f csv -c s c2)" = "RUNNING" ]
+
+  lxc stop -f c2
+
+  echo "==> Check --refresh cannot be used together with --start"
+  ! lxc copy c1 c2 --refresh --start || false
+
+  lxc delete -f c2
+  lxc snapshot c1
+
+  echo "==> Check that a container snapshot can be copied and started with the same command"
+  lxc copy c1/snap0 c2 --start
+
+  echo "==> Check the copied container is running"
+  [ "$(lxc list -f csv -c s c2)" = "RUNNING" ]
+
+  lxc delete -f c2
+
+  poolDriver="$(storage_backend "${LXD_DIR}")"
+
+  # Spawn an additional cluster to allow copy through migration.
+  spawn_lxd_and_bootstrap_cluster "${poolDriver}"
+
+  local cert
+  cert="$(cert_to_yaml "${LXD_ONE_DIR}/cluster.crt")"
+
+  # Spawn a second node.
+  spawn_lxd_and_join_cluster "${cert}" 2 1 "${LXD_ONE_DIR}" "${poolDriver}"
+
+  # Set up a TLS identity with admin permissions.
+  LXD_DIR="${LXD_ONE_DIR}" lxc auth group create copy
+  LXD_DIR="${LXD_ONE_DIR}" lxc auth group permission add copy server admin
+
+  # Create a token on the LXD cluster.
+  token="$(LXD_DIR="${LXD_ONE_DIR}" lxc auth identity create tls/copy --group=copy --quiet)"
+
+  # Add the LXD cluster as a remote.
+  lxc remote add cls 100.64.1.101:8443 --token="${token}"
+
+  # Create a temp network for binding the local LXD to allow pull mode migration.
+  lxc network create foo
+  new_address="$(lxc network get foo ipv4.address | cut -d/ -f1)"
+  old_address="$(lxc config get core.https_address)"
+  lxc config set core.https_address "${new_address}:8443"
+
+  for mode in pull push relay; do
+    echo "==> Check that a container can be copied to a remote and started with the same command (${mode} mode)"
+    lxc copy c1 cls:c2 --mode "${mode}" --start
+
+    echo "==> Check the copied container is running"
+    [ "$(lxc list -f csv -c s cls:c2)" = "RUNNING" ]
+
+    lxc delete -f cls:c2
+
+    echo "==> Check that a container snapshot can be copied to a remote and started with the same command (${mode} mode)"
+    lxc copy c1/snap0 cls:c2 --mode "${mode}" --start
+
+    echo "==> Check the copied container is running"
+    [ "$(lxc list -f csv -c s cls:c2)" = "RUNNING" ]
+
+    lxc delete -f cls:c2
+  done
+
+  # Ensure we can do cross-pool copy.
+  local other_backend="dir"
+  if [ "${lxd_backend}" = "dir" ]; then
+    other_backend="btrfs"
+  fi
+  local new_pool
+  new_pool="lxdtest-$(basename "${LXD_DIR}")-${other_backend}-pool"
+  lxc storage create "${new_pool}" "${other_backend}"
+
+  echo "==> Check that a container which is copied between pools can be started with the same command"
+  lxc copy c1 c2 --storage "${new_pool}" --start
+
+  echo "==> Check the copied container is running"
+  [ "$(lxc list -f csv -c s c2)" = "RUNNING" ]
+
+  lxc delete -f c2
+
+  echo "==> Check that a container snapshot which is copied between pools can be started with the same command"
+  lxc copy c1/snap0 c2 --storage "${new_pool}" --start
+
+  echo "==> Check the copied container is running"
+  [ "$(lxc list -f csv -c s c2)" = "RUNNING" ]
+
+  lxc delete -f c2
+
+  # Use the cluster to check start after copy on the same pool but between different members.
+  lxc copy c1 cls:c1 --target node1
+  lxc snapshot cls:c1
+
+  # When the server decides to use copy instead of migration, the mode is ignored.
+  # This is the case when using a remote pool.
+  for mode in pull push relay; do
+    echo "==> Check that a container can be copied to a different member and started with the same command (${mode} mode)"
+    LXD_DIR="${LXD_ONE_DIR}" lxc copy c1 c2 --target node2 --mode "${mode}" --start
+
+    echo "==> Check the copied container is running"
+    [ "$(LXD_DIR="${LXD_ONE_DIR}" lxc list -f csv -c s c2)" = "RUNNING" ]
+
+    LXD_DIR="${LXD_ONE_DIR}" lxc delete -f c2
+
+    echo "==> Check that a container snapshot can be copied to a different member and started with the same command (${mode} mode)"
+    LXD_DIR="${LXD_ONE_DIR}" lxc copy c1/snap0 c2 --target node2 --mode "${mode}" --start
+
+    echo "==> Check the copied container is running"
+    [ "$(LXD_DIR="${LXD_ONE_DIR}" lxc list -f csv -c s c2)" = "RUNNING" ]
+
+    LXD_DIR="${LXD_ONE_DIR}" lxc delete -f c2
+  done
+
+  # Cleanup
+  lxc delete -f c1
+  lxc delete -f cls:c1
+  lxc remote remove cls
+  lxc config set core.https_address "${old_address}"
+  lxc network delete foo
+  lxc storage delete "${new_pool}"
+
+  LXD_DIR="${LXD_ONE_DIR}" lxd shutdown
+  LXD_DIR="${LXD_TWO_DIR}" lxd shutdown
+
+  rm -f "${LXD_ONE_DIR}/unix.socket"
+  rm -f "${LXD_TWO_DIR}/unix.socket"
+
+  teardown_clustering_netns
+  teardown_clustering_bridge
+
+  kill_lxd "${LXD_ONE_DIR}"
+  kill_lxd "${LXD_TWO_DIR}"
+}
