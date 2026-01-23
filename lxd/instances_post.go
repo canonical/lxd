0777b1f130f8c3e2c583a69fbaa81a8c1abf9868
Julian Pelizäus <julian.pelizaeus@canonical.com>
2026-01-23 09:31:09 +0100

    lxd/instances_post: Update callsites of instanceCreateFinish
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/instances_post.go
+++ lxd/instances_post.go
@@ -137,7 +137,7 @@ func createFromImage(ctx context.Context, s *state.State, p api.Project, profile
 			return err
 		}
 
-		return instanceCreateFinish(s, req, args)
+		return instanceCreateFinish(s, req, args, nil)
 	}
 
 	resources := map[string][]api.URL{}
@@ -207,7 +207,7 @@ func createFromNone(ctx context.Context, s *state.State, projectName string, pro
 			return err
 		}
 
-		return instanceCreateFinish(s, req, args)
+		return instanceCreateFinish(s, req, args, nil)
 	}
 
 	resources := map[string][]api.URL{}
@@ -941,7 +941,7 @@ func createFromBackup(s *state.State, r *http.Request, projectName string, data 
 
 		runRevert.Success()
 
-		return instanceCreateFinish(s, &req, db.InstanceArgs{Name: bInfo.Name, Project: bInfo.Project})
+		return instanceCreateFinish(s, &req, db.InstanceArgs{Name: bInfo.Name, Project: bInfo.Project}, nil)
 	}
 
 	resources := map[string][]api.URL{}
