f134517d518c0672422558656f346cf3c85a5092
Simon Deziel <simon.deziel@canonical.com>
2026-01-24 15:56:20 -0500

    test/includes/oidc: cleanly exit Go processes if coverage is enabled
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/includes/oidc.sh
+++ test/includes/oidc.sh
@@ -15,7 +15,7 @@ spawn_oidc() {
 kill_oidc() {
   [ ! -e "${TEST_DIR}/oidc.pid" ] && return
 
-  kill -9 "$(< "${TEST_DIR}/oidc.pid")"
+  kill_go_proc "$(< "${TEST_DIR}/oidc.pid")"
   rm -f "${TEST_DIR}/oidc.pid"
   rm -f "${TEST_DIR}/oidc.port"
   rm -f "${TEST_DIR}/oidc.user"
