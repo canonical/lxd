32e68bd42bcfdfd7ccdd8d1f65ed3cdbd18a6293
Stanislav Kozina <stanislav.kozina@canonical.com>
2026-01-26 14:33:27 +0100

    lxd/operations: Reload durable operation from DB when created
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/operations/operations.go
+++ lxd/operations/operations.go
@@ -385,6 +385,35 @@ func scheduleOperation(s *state.State, args OperationArgs) (*Operation, error) {
 		return nil, err
 	}
 
+	// Durable operations need to be able to be reloaded from the database.
+	// To ease debugging in case of issues, we want to ensure the reloaded operation will be identical to the one originally created.
+	// Therefore, reload the operation from the database here to ensure everything is properly persisted and can be reloaded correctly.
+	// Notably, when unix socket is used for auth, the op.requestor.OriginAddress is set to '@'. This is not persisted in the database,
+	// so reloading the operation ensures we work with empty ("") OriginAddress instead of "@".
+	if op.class == OperationClassDurable {
+		newOp, err := loadDurableOperationFromDB(op)
+		if err != nil {
+			return nil, fmt.Errorf("Failed reloading durable operation from database: %w", err)
+		}
+
+		// Clear the run hook if it's a parent operation.
+		if len(op.children) > 0 {
+			newOp.onRun = nil
+		}
+
+		newOp.children = make([]*Operation, 0, len(op.children))
+		for _, childOp := range op.children {
+			reconstructedChildOp, err := loadDurableOperationFromDB(childOp)
+			if err != nil {
+				return nil, fmt.Errorf("Failed reloading durable child operation from database: %w", err)
+			}
+
+			newOp.AddChildren(reconstructedChildOp)
+		}
+
+		op = newOp
+	}
+
 	op.logger.Debug("New operation")
 
 	operationsLock.Lock()
