4106bd302e834dc752d58447b5c893130113db50
Simon Deziel <simon.deziel@canonical.com>
2026-02-03 14:40:41 -0500

    lxd/api_cluster: ignore failures to update the user agent
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/api_cluster.go
+++ lxd/api_cluster.go
@@ -451,7 +451,7 @@ func clusterPutBootstrap(d *Daemon, r *http.Request, req api.ClusterPut) respons
 	}
 
 	// Add the cluster flag from the agent
-	version.UserAgentFeatures([]string{"cluster"})
+	_ = version.UserAgentFeatures([]string{"cluster"})
 
 	return operations.OperationResponse(op)
 }
@@ -825,7 +825,7 @@ func clusterPutJoin(d *Daemon, r *http.Request, req api.ClusterPut) response.Res
 		}
 
 		// Add the cluster flag from the agent
-		version.UserAgentFeatures([]string{"cluster"})
+		_ = version.UserAgentFeatures([]string{"cluster"})
 
 		// Notify the leader of successful join, possibly triggering
 		// role changes.
