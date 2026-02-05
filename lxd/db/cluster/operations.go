0a0a640f323c9557209c15542411a3089e618166
Stanislav Kozina <stanislav.kozina@canonical.com>
2026-02-05 21:04:29 +0100

    lxd/db/cluster/operations: Introduce functions to handle operations resources
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/db/cluster/operations.go
+++ lxd/db/cluster/operations.go
@@ -7,12 +7,14 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/canonical/lxd/lxd/db/operationtype"
 	"github.com/canonical/lxd/lxd/db/query"
 	"github.com/canonical/lxd/lxd/request"
 	"github.com/canonical/lxd/shared/api"
+	"github.com/canonical/lxd/shared/entity"
 )
 
 // Code generation directives.
@@ -168,6 +170,98 @@ func UpdateOperation(ctx context.Context, tx *sql.Tx, opUUID string,
 
 	if n != 1 {
 		return fmt.Errorf("Query updated %d rows instead of 1", n)
+	}
+
+	return nil
+}
+
+// GetOperationResources loads operation resources from the cluster db.
+// The entity type is used as the key of the map, as the actual key is not stored in the DB.
+func GetOperationResources(ctx context.Context, tx *sql.Tx, opID int64) (map[string][]api.URL, error) {
+	stmt := `SELECT entity_id, entity_type FROM operations_resources WHERE operation_id = ?`
+
+	// We cannot call GetEntityURL from within the scan function because it would start a new transaction.
+	// So first we read all the entity IDs and types into a slice, then we loop over that slice to get the URLs.
+	resources := []*struct {
+		EntityID   int
+		EntityType EntityType
+	}{}
+	err := query.Scan(ctx, tx, stmt, func(scan func(dest ...any) error) error {
+		r := struct {
+			EntityID   int
+			EntityType EntityType
+		}{}
+
+		err := scan(&r.EntityID, &r.EntityType)
+		if err != nil {
+			return err
+		}
+
+		resources = append(resources, &r)
+
+		return nil
+	}, opID)
+	if err != nil {
+		return nil, fmt.Errorf("Failed reading operation resources: %w", err)
+	}
+
+	var result map[string][]api.URL
+	for _, r := range resources {
+		entityURL, err := GetEntityURL(ctx, tx, entity.Type(r.EntityType), r.EntityID)
+		if err != nil {
+			return nil, fmt.Errorf("Failed getting resource URL for entity type %q and ID %d: %w", r.EntityType, r.EntityID, err)
+		}
+
+		if result == nil {
+			result = map[string][]api.URL{}
+		}
+
+		_, ok := result[string(r.EntityType)]
+		if !ok {
+			result[string(r.EntityType)] = []api.URL{}
+		}
+
+		// TODO we're using entityType as the key of the map, as the actual key is not stored in the DB.
+		// Fix this.
+		result[string(r.EntityType)] = append(result[string(r.EntityType)], *entityURL)
+	}
+
+	return result, nil
+}
+
+// CreateOperationResources registers operation resources in the cluster db.
+// Returns entity_id of the first resource registered, or nil if no resources were registered.
+func CreateOperationResources(ctx context.Context, tx *sql.Tx, opID int64, resources map[string][]api.URL) error {
+	// No resources to register.
+	if len(resources) == 0 {
+		return nil
+	}
+
+	sb := strings.Builder{}
+	sb.WriteString(`INSERT INTO operations_resources (operation_id, entity_id, entity_type) VALUES `)
+	for _, entityURLs := range resources {
+		for _, entityURL := range entityURLs {
+			entityReference, err := GetEntityReferenceFromURL(ctx, tx, &entityURL)
+			if err != nil {
+				return fmt.Errorf("Failed getting entity ID from resource URL %q: %w", entityURL.String(), err)
+			}
+
+			entityTypeCode, err := entityReference.EntityType.Value()
+			if err != nil {
+				return fmt.Errorf("Failed getting entity type code for entity type %q: %w", entityReference.EntityType, err)
+			}
+
+			fmt.Fprintf(&sb, "(%d, %d, %d),", opID, entityReference.EntityID, entityTypeCode)
+		}
+	}
+
+	// Get the final stmt and replace the trailing comma with a semicolon.
+	insertStmt := sb.String()
+	insertStmt = insertStmt[:len(insertStmt)-1] + ";"
+
+	_, err := tx.ExecContext(ctx, insertStmt)
+	if err != nil {
+		return fmt.Errorf("Failed inserting operation resources: %w", err)
 	}
 
 	return nil
