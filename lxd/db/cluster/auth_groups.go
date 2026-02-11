18a8c6d6b9ef9dd1f7a598a69d30e82759111a16
Mark Laing <mark.laing@canonical.com>
2026-02-11 17:38:55 +0000

    lxd/db/cluster: Change AuthGroup to use simplegen.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/auth_groups.go
+++ lxd/db/cluster/auth_groups.go
@@ -11,42 +11,12 @@ import (
 	"github.com/canonical/lxd/shared/entity"
 )
 
-// Code generation directives.
-//
-//go:generate -command mapper lxd-generate db mapper -t auth_groups.mapper.go
-//go:generate mapper reset -i -b "//go:build linux && cgo && !agent"
-//
-//go:generate mapper stmt -e auth_group objects table=auth_groups
-//go:generate mapper stmt -e auth_group objects-by-ID table=auth_groups
-//go:generate mapper stmt -e auth_group objects-by-Name table=auth_groups
-//go:generate mapper stmt -e auth_group id table=auth_groups
-//go:generate mapper stmt -e auth_group create table=auth_groups
-//go:generate mapper stmt -e auth_group delete-by-Name table=auth_groups
-//go:generate mapper stmt -e auth_group update table=auth_groups
-//go:generate mapper stmt -e auth_group rename table=auth_groups
-//
-//go:generate mapper method -i -e auth_group GetMany
-//go:generate mapper method -i -e auth_group GetOne
-//go:generate mapper method -i -e auth_group ID
-//go:generate mapper method -i -e auth_group Exists
-//go:generate mapper method -i -e auth_group Create
-//go:generate mapper method -i -e auth_group DeleteOne-by-Name
-//go:generate mapper method -i -e auth_group Update
-//go:generate mapper method -i -e auth_group Rename
-//go:generate goimports -w auth_groups.mapper.go
-//go:generate goimports -w auth_groups.interface.mapper.go
-
 // AuthGroup is the database representation of an api.AuthGroup.
+// db:model auth_groups
 type AuthGroup struct {
-	ID          int
-	Name        string `db:"primary=true"`
-	Description string
-}
-
-// AuthGroupFilter contains fields upon which an AuthGroup can be filtered.
-type AuthGroupFilter struct {
-	ID   *int
-	Name *string
+	ID          int64  `db:"id"`
+	Name        string `db:"name"`
+	Description string `db:"description"`
 }
 
 // ToAPI converts the Group to an api.AuthGroup, making extra database queries as necessary.
