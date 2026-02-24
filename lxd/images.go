07f560b652ce1ff281f303e29c57040e5734613e
Din Music <din.music@canonical.com>
2026-02-24 16:28:15 +0000

    lxd/images: Validate compression algorithm in compressFile
    Signed-off-by: Din Music <din.music@canonical.com>
    (cherry picked from commit 417241c8d44ca422bbff3f0036e80427488ff665)

--- lxd/images.go
+++ lxd/images.go
@@ -57,6 +57,7 @@ import (
 	"github.com/canonical/lxd/shared/ioprogress"
 	"github.com/canonical/lxd/shared/logger"
 	"github.com/canonical/lxd/shared/osarch"
+	"github.com/canonical/lxd/shared/validate"
 	"github.com/canonical/lxd/shared/version"
 )
 
@@ -299,6 +300,11 @@ func compressFile(compress string, infile io.Reader, outfile io.Writer) error {
 
 	// Parse the command.
 	fields, err := shellquote.Split(compress)
+	if err != nil {
+		return err
+	}
+
+	err = validate.IsCompressionAlgorithm(fields[0])
 	if err != nil {
 		return err
 	}
