a74f3ee6f56f9d834b30a45f04f4f5b471e80a40
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-02-25 17:53:12 -0500

    lxc/image_registry: Add `image registry create` command
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- lxc/image_registry.go
+++ lxc/image_registry.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/canonical/lxd/shared"
+	"github.com/canonical/lxd/shared/api"
 	cli "github.com/canonical/lxd/shared/cmd"
 )
 
@@ -19,6 +23,10 @@ func (c *cmdImageRegistry) command() *cobra.Command {
 	cmd.Short = "Manage image registries"
 	cmd.Long = cli.FormatSection("Description", `Manage image registries`)
 
+	// Create
+	imageRegistryCreateCmd := cmdImageRegistryCreate{global: c.global, imageRegistry: c}
+	cmd.AddCommand(imageRegistryCreateCmd.command())
+
 	// List
 	imageRegistryListCmd := cmdImageRegistryList{global: c.global, imageRegistry: c}
 	cmd.AddCommand(imageRegistryListCmd.command())
@@ -27,6 +35,88 @@ func (c *cmdImageRegistry) command() *cobra.Command {
 	cmd.Args = cobra.NoArgs
 	cmd.Run = func(cmd *cobra.Command, args []string) { _ = cmd.Usage() }
 	return cmd
+}
+
+// Create.
+type cmdImageRegistryCreate struct {
+	global        *cmdGlobal
+	imageRegistry *cmdImageRegistry
+
+	flagDescription string
+	flagProtocol    string
+	flagProject     string
+}
+
+func (c *cmdImageRegistryCreate) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("create", "[<remote>:]<registry> [key=value...]")
+	cmd.Short = "Create image registry"
+	cmd.Long = cli.FormatSection("Description", cmd.Short+`
+	
+URL must be HTTPS (https://). Basic authentication is not allowed.
+`)
+
+	cmd.RunE = c.run
+	cmd.Flags().StringVar(&c.flagDescription, "description", "", cli.FormatStringFlagLabel("Description for the image registry"))
+	cmd.Flags().StringVar(&c.flagProtocol, "protocol", "", cli.FormatStringFlagLabel("Registry server protocol (lxd or simplestreams)"))
+	cmd.Flags().StringVar(&c.flagProject, "project", "", cli.FormatStringFlagLabel("Source project for the LXD registry"))
+
+	return cmd
+}
+
+func (c *cmdImageRegistryCreate) run(cmd *cobra.Command, args []string) error {
+	// Quick checks.
+	exit, err := c.global.CheckArgs(cmd, args, 1, -1)
+	if exit {
+		return err
+	}
+
+	// Parse remote.
+	resources, err := c.global.ParseServers(args[0])
+	if err != nil {
+		return err
+	}
+
+	resource := resources[0]
+	client := resource.server
+
+	registryName := resource.name
+	if registryName == "" {
+		return errors.New("Image registry name must not be empty")
+	}
+
+	// Create the image registry.
+	imageRegistry := api.ImageRegistriesPost{}
+	imageRegistry.Name = registryName
+	imageRegistry.Description = c.flagDescription
+	imageRegistry.Protocol = c.flagProtocol
+	imageRegistry.Config = map[string]string{}
+
+	// Populate config.
+	for i := 1; i < len(args); i++ {
+		key, value, found := strings.Cut(args[i], "=")
+		if !found {
+			return fmt.Errorf("Bad key/value pair: %s", args[i])
+		}
+
+		imageRegistry.Config[key] = value
+	}
+
+	if c.flagProject != "" {
+		imageRegistry.Config["source_project"] = c.flagProject
+	}
+
+	// Create the image registry.
+	err = client.CreateImageRegistry(imageRegistry)
+	if err != nil {
+		return err
+	}
+
+	if !c.global.flagQuiet {
+		fmt.Printf("Image registry %s created\n", registryName)
+	}
+
+	return nil
 }
 
 // List.
