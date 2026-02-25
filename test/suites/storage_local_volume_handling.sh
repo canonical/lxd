a50a817c61f86d96a6b1dff9b8fac23a258ea330
Simon Deziel <simon.deziel@canonical.com>
2026-02-25 10:28:32 -0500

    test/suites/storage_local_volume_handling: improve check for absence of volume snapshot
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/storage_local_volume_handling.sh
+++ test/suites/storage_local_volume_handling.sh
@@ -245,7 +245,7 @@ test_storage_local_volume_handling() {
           # copy without snapshots
           lxc storage volume copy "${source_pool}/vol2" "${target_pool}/vol3" --volume-only
           lxc storage volume show "${target_pool}" vol3 | grep -xF 'content_type: block'
-          ! lxc storage volume show "${target_pool}" vol3/snap0 | grep -F 'content_type: block' || false
+          ! lxc storage volume show "${target_pool}" vol3/snap0 || false
 
           # move images
           lxc storage volume move "${source_pool}/vol2" "${target_pool}/vol4"
