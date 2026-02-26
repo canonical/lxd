0408bc468485d9910dab3167904b9cbebe004f10
Simon Deziel <simon.deziel@canonical.com>
2026-02-26 16:50:03 -0500

    lxc/config/cert: merge `HasClientCertificate` into `GenerateClientCertificate`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxc/config/cert.go
+++ lxc/config/cert.go
@@ -8,27 +8,16 @@ import (
 	"github.com/canonical/lxd/shared"
 )
 
-// HasClientCertificate will return true if a client certificate has already been generated.
-func (c *Config) HasClientCertificate() bool {
-	certf := c.ConfigPath("client.crt")
-	keyf := c.ConfigPath("client.key")
-	if !shared.PathExists(certf) || !shared.PathExists(keyf) {
-		return false
-	}
-
-	return true
-}
-
 // GenerateClientCertificate will generate the needed client.crt and client.key if needed.
 func (c *Config) GenerateClientCertificate() error {
-	if c.HasClientCertificate() {
+	certf := c.ConfigPath("client.crt")
+	keyf := c.ConfigPath("client.key")
+
+	if shared.PathExists(certf) && shared.PathExists(keyf) {
 		return nil
 	}
 
 	fmt.Fprint(os.Stderr, "Generating a client certificate. This may take a moment...\n")
-
-	certf := c.ConfigPath("client.crt")
-	keyf := c.ConfigPath("client.key")
 
 	return shared.FindOrGenCert(certf, keyf, true, shared.CertOptions{})
 }
