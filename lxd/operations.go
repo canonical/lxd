ec3e9f47df5cfaf4487dd92a5a115ed4de047510
Stanislav Kozina <stanislav.kozina@canonical.com>
2026-02-27 13:56:58 +0100

    lxd/operations: Return parent and Children for /1.0/operations/{id}
    Signed-off-by: Stanislav Kozina <stanislav.kozina@canonical.com>

--- lxd/operations.go
+++ lxd/operations.go
@@ -165,46 +165,100 @@ func operationGet(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(err)
 	}
 
+	recursion, _ := util.IsRecursionRequest(r)
+
 	// Load the operation from the database.
-	var body *api.Operation
-	var dbLocation string
-	var operation dbCluster.Operation
+	opsLocations := make(map[string]string)
 	var op *operations.Operation
+	var ok bool
 	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		projectNames := make(map[int64]string)
+		nodeNames := make(map[int64]string)
+		constructOperation := func(dbOp *dbCluster.Operation) (*operations.Operation, error) {
+			// Get project name from the cache of project IDs to names, or load it from the DB if not present.
+			var projectName string
+			if dbOp.ProjectID != nil {
+				projectName, ok = projectNames[*dbOp.ProjectID]
+				if !ok {
+					project, err := dbCluster.GetProjectByID(ctx, tx.Tx(), int(*dbOp.ProjectID))
+					if err != nil {
+						return nil, err
+					}
+
+					projectNames[*dbOp.ProjectID] = project.Name
+					projectName = project.Name
+				}
+			}
+
+			// Get location from the cache of node IDs to names, or load it from the DB if not present.
+			location, ok := nodeNames[dbOp.NodeID]
+			if !ok {
+				ni, err := tx.GetNodeByID(ctx, dbOp.NodeID)
+				if err != nil {
+					return nil, err
+				}
+
+				nodeNames[dbOp.NodeID] = ni.Name
+				location = ni.Name
+			}
+
+			opsLocations[dbOp.UUID] = location
+
+			op, err := operations.ConstructOperationFromDB(ctx, tx.Tx(), s, dbOp, projectName)
+			if err != nil {
+				return nil, err
+			}
+
+			return op, nil
+		}
+
 		filter := dbCluster.OperationFilter{UUID: &id}
-		ops, err := dbCluster.GetOperations(ctx, tx.Tx(), filter)
+		dbOps, err := dbCluster.GetOperations(ctx, tx.Tx(), filter)
 		if err != nil {
 			return err
 		}
 
 		// Make sure we have loaded exactly one operation from the DB.
-		switch len(ops) {
+		var dbOp *dbCluster.Operation
+		switch len(dbOps) {
 		case 0:
 			return api.StatusErrorf(http.StatusNotFound, "Operation not found")
 		case 1:
-			operation = ops[0]
+			dbOp = &dbOps[0]
 		default:
 			return errors.New("More than one operation matches")
 		}
 
-		var projectName string
-		if operation.ProjectID != nil {
-			project, err := dbCluster.GetProjectByID(ctx, tx.Tx(), int(*operation.ProjectID))
-			if err != nil {
-				return err
-			}
-
-			projectName = project.Name
+		// Don't return child operations directly.
+		// Child operations can be returned embedded in their parents with recursion=1.
+		if dbOp.Parent != nil {
+			return api.StatusErrorf(http.StatusNotFound, "Operation not found")
 		}
 
-		ni, err := tx.GetNodeByID(ctx, operation.NodeID)
+		op, err = constructOperation(dbOp)
 		if err != nil {
 			return err
 		}
 
-		dbLocation = ni.Name
+		// Load children if needed
+		if recursion > 0 {
+			// Load all child operations.
+			childFilter := dbCluster.OperationFilter{Parent: &dbOp.ID}
+			childDbOps, err := dbCluster.GetOperations(ctx, tx.Tx(), childFilter)
+			if err != nil {
+				return err
+			}
 
-		op, err = operations.ConstructOperationFromDB(ctx, tx.Tx(), s, &operation, projectName)
+			for _, childDbOp := range childDbOps {
+				childOp, err := constructOperation(&childDbOp)
+				if err != nil {
+					return err
+				}
+
+				op.AddChildOperation(childOp)
+			}
+		}
+
 		return err
 	})
 	if err != nil {
@@ -216,11 +270,26 @@ func operationGet(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(err)
 	}
 
-	_, body = op.RenderWithoutProgress()
+	_, body := op.RenderFullWithoutProgress()
 
 	// The [operations.Operation] doesn't contain the node where the operation is running.
 	// If we're loading operations from the DB, we need to set the location here.
-	body.Location = dbLocation
+	body.Location, ok = opsLocations[op.ID()]
+	if !ok {
+		logger.Warn("Failed to find operation location", logger.Ctx{"operationID": op.ID()})
+	}
+
+	newChildren := make([]api.Operation, 0, len(body.Children))
+	for _, child := range body.Children {
+		child.Location, ok = opsLocations[child.ID]
+		if !ok {
+			logger.Warn("Failed to find child operation location", logger.Ctx{"operationID": child.ID})
+		}
+
+		newChildren = append(newChildren, child)
+	}
+
+	body.Children = newChildren
 
 	return response.SyncResponse(true, body)
 }
