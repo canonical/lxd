269b8d53fdfa65a7745bd121345ac62c2aa3028b
Simon Deziel <simon.deziel@canonical.com>
2026-03-06 17:59:02 -0500

    lxd/storage/s3/headers: use `strings.Cut`/`CutPrefix`/LastIndex` in `AuthorizationHeaderAccessKey`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/storage/s3/headers.go
+++ lxd/storage/s3/headers.go
@@ -10,8 +10,8 @@ func AuthorizationHeaderAccessKey(authorizationHeader string) string {
 	// AWS4-HMAC-SHA256 Credential=PRL470D7Q93X1ZA1L82X/20220825/US/s3/aws4_request,SignedHeaders=host;x-amz-content-sha256;x-amz-date,Signature=d8fdaf67c5072d4ff7ac56e4529e66fb08255aaa79193b212cba4670d058fade
 	authHeaders, found := strings.CutPrefix(authorizationHeader, "AWS4-HMAC-SHA256")
 	if found {
-		authHeaderParts := strings.Split(strings.TrimSpace(authHeaders), ",")
-		credential, found := strings.CutPrefix(authHeaderParts[0], "Credential=")
+		firstPart, _, _ := strings.Cut(strings.TrimSpace(authHeaders), ",")
+		credential, found := strings.CutPrefix(firstPart, "Credential=")
 		if found {
 			credParts := strings.Split(credential, "/")
 			credPartsLen := len(credParts)
@@ -25,10 +25,10 @@ func AuthorizationHeaderAccessKey(authorizationHeader string) string {
 		if found {
 			// Parses an older Authorization header as below, to extract the access key "PRL470D7Q93X1ZA1L82X".
 			// AWS PRL470D7Q93X1ZA1L82X:dC5GcyRFCyQIr+y9BdpAwBjkOK0=
-			authHeaderParts := strings.Split(strings.TrimSpace(authHeaders), ":")
-			authHeaderPartsLen := len(authHeaderParts)
-			if authHeaderPartsLen > 1 {
-				return strings.Join(authHeaderParts[:authHeaderPartsLen-1], ":")
+			trimmed := strings.TrimSpace(authHeaders)
+			lastColon := strings.LastIndex(trimmed, ":")
+			if lastColon >= 0 {
+				return trimmed[:lastColon]
 			}
 		}
 	}
