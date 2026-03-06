386abb929ab5dfff68eb77fc5513c52854bd26dc
Simon Deziel <simon.deziel@canonical.com>
2026-03-05 19:35:30 -0500

    lxd/storage/drivers/cephobject_utils: add `S3` helpers
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/storage/drivers/driver_cephobject_utils.go
+++ lxd/storage/drivers/driver_cephobject_utils.go
@@ -2,9 +2,18 @@ package drivers
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
+	"os"
+	"path"
 	"slices"
 	"strconv"
 	"strings"
@@ -194,6 +203,118 @@ func (d *cephobject) radosgwadminSubUserDelete(ctx context.Context, user string,
 	_, err := d.radosgwadmin(ctx, "subuser", "rm", "--uid", user, "--subuser", subuser)
 
 	return err
+}
+
+// s3CreateBucket creates a bucket via the S3 API using an HTTP PUT request with AWS Signature V4 authentication.
+func (d *cephobject) s3CreateBucket(ctx context.Context, creds S3Credentials, bucket string) error {
+	u, err := url.ParseRequestURI(d.config["cephobject.radosgw.endpoint"])
+	if err != nil {
+		return fmt.Errorf("Failed parsing cephobject.radosgw.endpoint: %w", err)
+	}
+
+	u.Path = path.Join(u.Path, url.PathEscape(bucket))
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, u.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	s3SignRequest(req, creds, "")
+
+	transport, err := d.s3Transport()
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{Transport: transport}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Failed sending S3 create bucket request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Failed creating S3 bucket (HTTP %d)", resp.StatusCode)
+	}
+
+	return nil
+}
+
+// s3Transport returns an HTTP transport configured for the radosgw endpoint.
+func (d *cephobject) s3Transport() (http.RoundTripper, error) {
+	u, err := url.ParseRequestURI(d.config["cephobject.radosgw.endpoint"])
+	if err != nil {
+		return nil, fmt.Errorf("Failed parsing cephobject.radosgw.endpoint: %w", err)
+	}
+
+	certFilePath := d.config["cephobject.radosgw.endpoint_cert_file"]
+	if u.Scheme == "https" && certFilePath != "" {
+		certFilePath = shared.HostPath(certFilePath)
+
+		certs, err := os.ReadFile(certFilePath)
+		if err != nil {
+			return nil, fmt.Errorf("Failed reading %q: %w", certFilePath, err)
+		}
+
+		rootCAs := x509.NewCertPool()
+		if !rootCAs.AppendCertsFromPEM(certs) {
+			return nil, errors.New("Failed adding S3 client certificates")
+		}
+
+		return &http.Transport{TLSClientConfig: &tls.Config{RootCAs: rootCAs}}, nil
+	}
+
+	return http.DefaultTransport, nil
+}
+
+// s3SignRequest signs an HTTP request using AWS Signature V4.
+func s3SignRequest(req *http.Request, creds S3Credentials, payloadHash string) {
+	if payloadHash == "" {
+		payloadHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" // SHA-256 of empty string.
+	}
+
+	now := time.Now().UTC()
+	datestamp := now.Format("20060102")
+	amzDate := now.Format("20060102T150405Z")
+	region := "us-east-1"
+	service := "s3"
+	credentialScope := datestamp + "/" + region + "/" + service + "/aws4_request"
+
+	req.Header.Set("x-amz-date", amzDate)
+	req.Header.Set("x-amz-content-sha256", payloadHash)
+	req.Header.Set("Host", req.URL.Host)
+
+	// Build canonical request.
+	canonicalURI := req.URL.Path
+	if canonicalURI == "" {
+		canonicalURI = "/"
+	}
+
+	canonicalQueryString := req.URL.Query().Encode()
+	signedHeaders := "host;x-amz-content-sha256;x-amz-date"
+	canonicalHeaders := "host:" + req.URL.Host + "\n" + "x-amz-content-sha256:" + payloadHash + "\n" + "x-amz-date:" + amzDate + "\n"
+
+	canonicalRequest := req.Method + "\n" + canonicalURI + "\n" + canonicalQueryString + "\n" + canonicalHeaders + "\n" + signedHeaders + "\n" + payloadHash
+
+	// Build string to sign.
+	canonicalRequestHash := sha256.Sum256([]byte(canonicalRequest))
+	stringToSign := "AWS4-HMAC-SHA256\n" + amzDate + "\n" + credentialScope + "\n" + hex.EncodeToString(canonicalRequestHash[:])
+
+	// Calculate signature.
+	signingKey := hmacSHA256(hmacSHA256(hmacSHA256(hmacSHA256([]byte("AWS4"+creds.SecretKey), datestamp), region), service), "aws4_request")
+	signature := hex.EncodeToString(hmacSHA256(signingKey, stringToSign))
+
+	// Set Authorization header.
+	req.Header.Set("Authorization", "AWS4-HMAC-SHA256 Credential="+creds.AccessKey+"/"+credentialScope+", SignedHeaders="+signedHeaders+", Signature="+signature)
+}
+
+// hmacSHA256 returns the HMAC-SHA256 of the data using the given key.
+func hmacSHA256(key []byte, data string) []byte {
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(data))
+	return h.Sum(nil)
 }
 
 // radosgwadminBucketDelete deletes radosgw bucket.
