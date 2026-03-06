194c8e4008f638b30f1e0019c468e29a20177d81
Simon Deziel <simon.deziel@canonical.com>
2026-03-06 16:57:15 -0500

    lxd/instance/drivers/qemu: use `strings.Cut` in `Rename`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -5619,7 +5619,7 @@ func (d *qemu) Rename(newName string, applyTemplateTrigger bool) error {
 
 			for _, sname := range results {
 				// Rename the snapshot.
-				oldSnapName := strings.SplitN(sname, shared.SnapshotDelimiter, 2)[1]
+				_, oldSnapName, _ := strings.Cut(sname, shared.SnapshotDelimiter)
 				baseSnapName := filepath.Base(sname)
 
 				err := dbCluster.RenameInstanceSnapshot(ctx, tx.Tx(), d.project.Name, oldName, oldSnapName, baseSnapName)
@@ -5639,9 +5639,9 @@ func (d *qemu) Rename(newName string, applyTemplateTrigger bool) error {
 	// Rename the instance database entry.
 	err = d.state.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
 		if d.IsSnapshot() {
-			oldParts := strings.SplitN(oldName, shared.SnapshotDelimiter, 2)
-			newParts := strings.SplitN(newName, shared.SnapshotDelimiter, 2)
-			return dbCluster.RenameInstanceSnapshot(ctx, tx.Tx(), d.project.Name, oldParts[0], oldParts[1], newParts[1])
+			oldParent, oldSnap, _ := strings.Cut(oldName, shared.SnapshotDelimiter)
+			_, newSnap, _ := strings.Cut(newName, shared.SnapshotDelimiter)
+			return dbCluster.RenameInstanceSnapshot(ctx, tx.Tx(), d.project.Name, oldParent, oldSnap, newSnap)
 		}
 
 		return dbCluster.RenameInstance(ctx, tx.Tx(), d.project.Name, oldName, newName)
@@ -5684,7 +5684,7 @@ func (d *qemu) Rename(newName string, applyTemplateTrigger bool) error {
 	for _, backup := range backups {
 		b := backup
 		oldName := b.Name()
-		backupName := strings.Split(oldName, "/")[1]
+		_, backupName, _ := strings.Cut(oldName, "/")
 		newName := newName + "/" + backupName
 
 		err = b.Rename(newName)
