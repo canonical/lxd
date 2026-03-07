9748d31a2676bb96b7818ec1feeeb7858582c026
Simon Deziel <simon.deziel@canonical.com>
2026-03-07 17:43:28 -0500

    lxd/main_forkstart: avoid TOCTOU by relying on `O_CREATE|O_TRUNC` to create the log file
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/main_forkstart.go
+++ lxd/main_forkstart.go
@@ -82,11 +82,7 @@ func (c *cmdForkstart) run(cmd *cobra.Command, args []string) error {
 
 	// Redirect stdout and stderr to a log file
 	logPath := shared.LogPath(name, "forkstart.log")
-	if shared.PathExists(logPath) {
-		_ = os.Remove(logPath)
-	}
-
-	logFile, err := os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0644)
+	logFile, err := os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC|os.O_SYNC, 0644)
 	if err == nil {
 		_ = unix.Dup3(int(logFile.Fd()), 1, 0)
 		_ = unix.Dup3(int(logFile.Fd()), 2, 0)
