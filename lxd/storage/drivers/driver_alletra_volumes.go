e77584c794f3b6b15ef888f3095d4e215d7f091a
Simon Deziel <simon.deziel@canonical.com>
2026-03-08 14:23:21 -0400

    lxd/storage/drivers/alletra_volumes: avoid TOCTOU in `DeleteVolume`
    `wipeDirectory` already returns `nil` for non-existent paths.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/storage/drivers/driver_alletra_volumes.go
+++ lxd/storage/drivers/driver_alletra_volumes.go
@@ -1071,7 +1071,7 @@ func (d *alletra) DeleteVolume(vol Volume, op *operations.Operation) error {
 
 	mountPath := vol.MountPath()
 
-	if vol.contentType == ContentTypeFS && shared.PathExists(mountPath) {
+	if vol.contentType == ContentTypeFS {
 		err := wipeDirectory(mountPath)
 		if err != nil {
 			return err
