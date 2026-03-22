41c7d152a24703752704ab0962eb5fe23c727de9
Simon Deziel <simon.deziel@canonical.com>
2026-03-22 11:19:32 -0400

    lxd/instance/drivers/edk2: rework how deprecated vars are cleaned up
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/instance/drivers/edk2/edk2.go
+++ lxd/instance/drivers/edk2/edk2.go
@@ -39,6 +39,16 @@ const (
 // OVMFDebugFirmware is the debug version of the "preferred" firmware.
 const OVMFDebugFirmware = "OVMF_CODE.4MB.debug.fd"
 
+// legacyFirmwareVarsCandidates lists firmware vars file names that were previously used but have since been
+// deprecated. They are still returned by GetAchitectureFirmwareVarsCandidates to allow cleanup of any
+// orphaned files left over on existing VM instances.
+// These are OVMF EDK2-based CSM firmware vars files, which were replaced by SeaBIOS for BIOS mode.
+var legacyFirmwareVarsCandidates = []string{
+	"OVMF_VARS.4MB.CSM.fd",
+	"OVMF_VARS.2MB.CSM.fd",
+	"OVMF_VARS.CSM.fd",
+}
+
 var architectureInstallations = map[int][]Installation{
 	osarch.ARCH_64BIT_INTEL_X86: {{
 		Paths: GetenvEdk2Paths("/usr/share/OVMF"),
@@ -64,11 +74,6 @@ var architectureInstallations = map[int][]Installation{
 			},
 			CSM: {
 				{Code: "seabios.bin", Vars: "seabios.bin"},
-				{Code: "OVMF_CODE.4MB.CSM.fd", Vars: "OVMF_VARS.4MB.CSM.fd"},
-				{Code: "OVMF_CODE.csm.4m.fd", Vars: "OVMF_VARS.4m.fd"},
-				{Code: "OVMF_CODE.2MB.CSM.fd", Vars: "OVMF_VARS.2MB.CSM.fd"},
-				{Code: "OVMF_CODE.CSM.fd", Vars: "OVMF_VARS.CSM.fd"},
-				{Code: "OVMF_CODE.csm.fd", Vars: "OVMF_VARS.fd"},
 			},
 		},
 	}, {
@@ -104,10 +109,6 @@ var architectureInstallations = map[int][]Installation{
 			GENERIC: {
 				{Code: "OVMF_CODE.4m.fd", Vars: "OVMF_VARS.4m.fd"},
 				{Code: "OVMF_CODE.fd", Vars: "OVMF_VARS.fd"},
-			},
-			CSM: {
-				{Code: "OVMF_CODE.csm.4m.fd", Vars: "OVMF_VARS.4m.fd"},
-				{Code: "OVMF_CODE.csm.fd", Vars: "OVMF_VARS.fd"},
 			},
 			SECUREBOOT: {
 				{Code: "OVMF_CODE.secboot.4m.fd", Vars: "OVMF_VARS.4m.fd"},
@@ -153,6 +154,7 @@ var architectureInstallations = map[int][]Installation{
 // GetAchitectureFirmwareVarsCandidates returns a unique list of candidate vars names for hostArch for all usages.
 // It does not check whether the associated firmware files are present on the host now.
 // This can be used to check for the existence of previously used firmware vars files in an existing VM instance.
+// Legacy (deprecated) vars file names are also included to allow cleanup of orphaned files.
 func GetAchitectureFirmwareVarsCandidates(hostArch int) (varsNames []string) {
 	for _, installation := range architectureInstallations[hostArch] {
 		for _, usage := range installation.Usage {
@@ -160,6 +162,15 @@ func GetAchitectureFirmwareVarsCandidates(hostArch int) (varsNames []string) {
 				if !slices.Contains(varsNames, fwPair.Vars) {
 					varsNames = append(varsNames, fwPair.Vars)
 				}
+			}
+		}
+	}
+
+	// The legacy OVMF and CSM vars files were only ever created on x86-64.
+	if hostArch == osarch.ARCH_64BIT_INTEL_X86 {
+		for _, legacyVars := range legacyFirmwareVarsCandidates {
+			if !slices.Contains(varsNames, legacyVars) {
+				varsNames = append(varsNames, legacyVars)
 			}
 		}
 	}
