4e69baf0251d6501500f85f42fc9f7d67697499c
Din Music <din.music@canonical.com>
2026-03-23 15:52:15 +0000

    client: Return operations from network forward functions
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/lxd_network_forwards.go
+++ client/lxd_network_forwards.go
@@ -65,61 +65,97 @@ func (r *ProtocolLXD) GetNetworkForward(networkName string, listenAddress string
 }
 
 // CreateNetworkForward defines a new network forward using the provided struct.
-func (r *ProtocolLXD) CreateNetworkForward(networkName string, forward api.NetworkForwardsPost) error {
+func (r *ProtocolLXD) CreateNetworkForward(networkName string, forward api.NetworkForwardsPost) (Operation, error) {
 	err := r.CheckExtension("network_forward")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	listenAddressIP := net.ParseIP(forward.ListenAddress)
 	if listenAddressIP == nil {
-		return fmt.Errorf("Invalid network forward listen address: %s", forward.ListenAddress)
+		return nil, fmt.Errorf("Invalid network forward listen address: %s", forward.ListenAddress)
 	}
 
 	if listenAddressIP.IsUnspecified() {
 		err := r.CheckExtension("network_allocate_external_ips")
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
+	path := api.NewURL().Path("networks", networkName, "forwards")
+
+	var op Operation
+
 	// Send the request.
-	_, _, err = r.query(http.MethodPost, "/networks/"+url.PathEscape(networkName)+"/forwards", forward, "")
+	err = r.CheckExtension("storage_and_network_operations")
 	if err != nil {
-		return err
+		// Fallback to older behavior without operations.
+		op = noopOperation{}
+		_, _, err = r.query(http.MethodPost, path.String(), forward, "")
+	} else {
+		op, _, err = r.queryOperation(http.MethodPost, path.String(), forward, "", true)
 	}
 
-	return nil
+	if err != nil {
+		return nil, err
+	}
+
+	return op, nil
 }
 
 // UpdateNetworkForward updates the network forward to match the provided struct.
-func (r *ProtocolLXD) UpdateNetworkForward(networkName string, listenAddress string, forward api.NetworkForwardPut, ETag string) error {
+func (r *ProtocolLXD) UpdateNetworkForward(networkName string, listenAddress string, forward api.NetworkForwardPut, ETag string) (Operation, error) {
 	err := r.CheckExtension("network_forward")
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	path := api.NewURL().Path("networks", networkName, "forwards", listenAddress)
+
+	var op Operation
 
 	// Send the request.
-	_, _, err = r.query(http.MethodPut, "/networks/"+url.PathEscape(networkName)+"/forwards/"+url.PathEscape(listenAddress), forward, ETag)
+	err = r.CheckExtension("storage_and_network_operations")
 	if err != nil {
-		return err
+		// Fallback to older behavior without operations.
+		op = noopOperation{}
+		_, _, err = r.query(http.MethodPut, path.String(), forward, ETag)
+	} else {
+		op, _, err = r.queryOperation(http.MethodPut, path.String(), forward, ETag, true)
 	}
 
-	return nil
+	if err != nil {
+		return nil, err
+	}
+
+	return op, nil
 }
 
 // DeleteNetworkForward deletes an existing network forward.
-func (r *ProtocolLXD) DeleteNetworkForward(networkName string, listenAddress string) error {
+func (r *ProtocolLXD) DeleteNetworkForward(networkName string, listenAddress string) (Operation, error) {
 	err := r.CheckExtension("network_forward")
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	path := api.NewURL().Path("networks", networkName, "forwards", listenAddress)
+
+	var op Operation
 
 	// Send the request.
-	_, _, err = r.query(http.MethodDelete, "/networks/"+url.PathEscape(networkName)+"/forwards/"+url.PathEscape(listenAddress), nil, "")
+	err = r.CheckExtension("storage_and_network_operations")
 	if err != nil {
-		return err
+		// Fallback to older behavior without operations.
+		op = noopOperation{}
+		_, _, err = r.query(http.MethodDelete, path.String(), nil, "")
+	} else {
+		op, _, err = r.queryOperation(http.MethodDelete, path.String(), nil, "", true)
 	}
 
-	return nil
+	if err != nil {
+		return nil, err
+	}
+
+	return op, nil
 }
