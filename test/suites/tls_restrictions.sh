8d020089fae6d13b4904392e416fd9e6598534ae
Mark Laing <mark.laing@canonical.com>
2026-03-23 09:55:36 +0000

    test/suites: Test improved validation.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 5f4b8e20afa321e0be436a908a8a92b8b8700557)

--- test/suites/tls_restrictions.sh
+++ test/suites/tls_restrictions.sh
@@ -24,8 +24,24 @@ test_tls_restrictions() {
   lxc_remote project list localhost: | grep -q default
   lxc_remote project list localhost: | grep -q blah
 
+  # Confirm certificates cannot be edited to have "server" type.
+  ! lxc_remote config trust show "localhost:${FINGERPRINT}" | sed -e "s/type: client/type: server/" | lxc_remote config trust edit "localhost:${FINGERPRINT}" || false
+
   # Apply restrictions
   lxc config trust show "${FINGERPRINT}" | sed -e "s/restricted: false/restricted: true/" | lxc config trust edit "${FINGERPRINT}"
+
+  # Confirm restricted client cannot edit certificate type, name, restrictions, or projects.
+  cert_name="$(lxc query "localhost:/1.0/certificates/${FINGERPRINT}" | jq -r '.name')"
+  ! lxc_remote config trust show "localhost:${FINGERPRINT}" | sed -e "s/type: client/type: server/" | lxc_remote config trust edit "localhost:${FINGERPRINT}" || false
+  ! lxc_remote config trust show "localhost:${FINGERPRINT}" | sed -e "s/type: client/type: metrics/" | lxc_remote config trust edit "localhost:${FINGERPRINT}" || false
+  ! lxc_remote config trust show "localhost:${FINGERPRINT}" | sed -e "s/name: ${cert_name}/name: bar/" | lxc_remote config trust edit "localhost:${FINGERPRINT}" || false
+  ! lxc_remote config trust show "localhost:${FINGERPRINT}" | sed -e "s/restricted: true/restricted: false/" | lxc_remote config trust edit "localhost:${FINGERPRINT}" || false
+  ! lxc_remote config trust show "localhost:${FINGERPRINT}" | sed -e "s/projects: \[\]/projects: ['default']/" | lxc_remote config trust edit "localhost:${FINGERPRINT}" || false
+  ! lxc_remote query -X PATCH "localhost:/1.0/certificates/${FINGERPRINT}" -d '{"type": "server"}' || false
+  ! lxc_remote query -X PATCH "localhost:/1.0/certificates/${FINGERPRINT}" -d '{"type": "metrics"}' || false
+  ! lxc_remote query -X PATCH "localhost:/1.0/certificates/${FINGERPRINT}" -d '{"restricted": false, "projects": []}' || false
+  ! lxc_remote query -X PATCH "localhost:/1.0/certificates/${FINGERPRINT}" -d '{"projects": ["default"]}' || false
+  ! lxc_remote query -X PATCH "localhost:/1.0/certificates/${FINGERPRINT}" -d '{"name": "bar"}' || false
 
   # Confirm no project visible when none listed
   [ "$(lxc_remote project list localhost: --format csv | wc -l)" = 0 ]
