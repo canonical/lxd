79dbba5fed73a08d0a8c0781c7ab2f9701e71f5a
Julian Pelizäus <julian.pelizaeus@canonical.com>
2026-03-23 10:39:47 +0100

    lxd/api_internal: Don't use backup config from disk
    Instead use the already validated backup index which contains the same set of information.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- lxd/api_internal.go
+++ lxd/api_internal.go
@@ -789,23 +789,9 @@ func internalImportFromBackup(ctx context.Context, s *state.State, bInfo *backup
 		return fmt.Errorf(`The instance %q does not seem to exist on any storage pool`, instName)
 	}
 
-	// User needs to make sure that we can access the directory where backup.yaml lives.
-	instanceMountPoint := instanceMountPoints[0]
-	isEmpty, err := shared.PathIsEmpty(instanceMountPoint)
-	if err != nil {
-		return err
-	}
-
-	if isEmpty {
-		return fmt.Errorf(`The instance's directory %q appears to be empty. Please ensure that the instance's storage volume is mounted`, instanceMountPoint)
-	}
-
-	// Read in the backup.yaml file.
-	backupYamlPath := filepath.Join(instanceMountPoint, "backup.yaml")
-	backupConf, err := backup.ParseConfigYamlFile(backupYamlPath)
-	if err != nil {
-		return err
-	}
+	// Use the information from the backup index.
+	// The backup config later gets persisted to disk too.
+	backupConf := bInfo.Config
 
 	if backupConf.Instance == nil {
 		return errors.New("Instance definition in backup config is missing")
@@ -975,6 +961,7 @@ func internalImportFromBackup(ctx context.Context, s *state.State, bInfo *backup
 	instancePath := storagePools.InstancePath(instanceType, projectName, backupConf.Instance.Name, false)
 	isPrivileged := backupConf.Instance.Config["security.privileged"] == ""
 
+	instanceMountPoint := instanceMountPoints[0]
 	err = storagePools.CreateContainerMountpoint(instanceMountPoint, instancePath, isPrivileged)
 	if err != nil {
 		return err
