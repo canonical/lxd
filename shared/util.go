4dbb63cacb43b1df0c4eff7ee8f6d90a3fef0e2c
Simon Deziel <simon.deziel@canonical.com>
2026-03-26 13:47:06 -0400

    shared/util: don't leak output from failed pongo2 template execution
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 88955af7d309258dfb5003905c7677402c6c30bb)

--- shared/util.go
+++ shared/util.go
@@ -1159,7 +1159,7 @@ func RenderTemplate(template string, ctx pongo2.Context) (string, error) {
 	// Get rendered template
 	ret, err := tpl.Execute(ctx)
 	if err != nil {
-		return ret, err
+		return "", err
 	}
 
 	// Looks like we're nesting templates so run pongo again
