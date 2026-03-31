a29592bac1848634969e80755ef97a1a1fb283b4
Mark Laing <mark.laing@canonical.com>
2026-03-31 17:27:13 +0000

    lxd/db/cluster: Add function to get certificates by identity ID.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/certificates.go
+++ lxd/db/cluster/certificates.go
@@ -3,7 +3,12 @@
 package cluster
 
 import (
+	"context"
+	"database/sql"
+	"strings"
 	"time"
+
+	"github.com/canonical/lxd/lxd/db/query"
 )
 
 // CertificatesRow represents a row of the certificates table.
@@ -33,4 +38,30 @@ type IdentityCertificate struct {
 
 	// db:join JOIN identities_certificates ON certificates.id = identities_certificates.certificate_id
 	IdentityID int64 `db:"identities_certificates.identity_id"`
+}
+
+// GetIdentitiesPEMCertificates returns a map of identity ID to list of PEM encoded certificates for each identity.
+// The optional identity ID field can be used to filter the output (in which case the output map will contain only one key).
+// The slice of certificates for each identity is ordered with the most recent certificate first.
+func GetIdentitiesPEMCertificates(ctx context.Context, tx *sql.Tx, identityID *int64) (map[int64][]string, error) {
+	var b strings.Builder
+	var args []any
+	if identityID != nil {
+		args = []any{*identityID}
+		b.WriteString("WHERE identities_certificates.identity_id = ? ")
+	}
+
+	// Sorting by certificates.id DESC as this will be more efficient than sorting by creation date.
+	// Since id is the auto-incrementing primary key, it has the same effect.
+	b.WriteString("ORDER BY identities_certificates.identity_id, certificates.id DESC")
+	out := make(map[int64][]string)
+	err := query.SelectFunc[IdentityCertificate](ctx, tx, b.String(), func(t IdentityCertificate) error {
+		out[t.IdentityID] = append(out[t.IdentityID], t.Row.Certificate)
+		return nil
+	}, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
 }
