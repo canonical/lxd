bff115d57e830b5269ac3d6e44dfd33f63f3e695
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-04-01 16:21:55 -0400

    lxd/db/cluster: Add update patch for image registries
    This change contains a database migration patch to
    update the schema and populate the tables with built-in
    image registries.
    
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- lxd/db/cluster/update.go
+++ lxd/db/cluster/update.go
@@ -128,6 +128,35 @@ var updates = map[int]schema.Update{
 	82: updateFromV81,
 	83: updateFromV82,
 	84: updateFromV83,
+	85: updateFromV84,
+}
+
+func updateFromV84(ctx context.Context, tx *sql.Tx) error {
+	// Create the new tables.
+	_, err := tx.ExecContext(ctx, `
+CREATE TABLE image_registries (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	protocol INTEGER NOT NULL,
+	builtin INTEGER NOT NULL DEFAULT 0,
+	UNIQUE (name)
+);
+
+CREATE TABLE image_registries_config (
+	image_registry_id INTEGER NOT NULL,
+	key TEXT NOT NULL,
+	value TEXT NOT NULL,
+	FOREIGN KEY (image_registry_id) REFERENCES image_registries (id) ON DELETE CASCADE,
+	PRIMARY KEY (image_registry_id, key)
+) WITHOUT ROWID;
+`)
+	if err != nil {
+		return err
+	}
+
+	// Populate the tables with the built-in image registries.
+	return createBuiltinImageRegistries(ctx, tx)
 }
 
 func updateFromV83(ctx context.Context, tx *sql.Tx) error {
