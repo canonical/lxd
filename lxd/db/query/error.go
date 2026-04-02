51ef06429e1e049b522edc2bd7d9bbe8387ad024
Mark Laing <mark.laing@canonical.com>
2026-04-02 09:09:56 +0000

    lxd/db/query: Add error type indicating multiple matches/affected rows.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/query/error.go
+++ lxd/db/query/error.go
@@ -24,3 +24,42 @@ func IsConflictErr(err error) bool {
 
 	return slices.Contains(conflictErrorCodes, driverErr.Code)
 }
+
+// ErrMultipleMatches should be used where a query expected to return or modify a single row, but more than one row
+// is returned/modified.
+type ErrMultipleMatches struct {
+	err error
+}
+
+func (e ErrMultipleMatches) defaultErrMsg() string {
+	return "Multiple matches were found but only one was expected"
+}
+
+// Error implements the error builtin, making ErrMultipleMatches a valid error type.
+func (e ErrMultipleMatches) Error() string {
+	if e.err == nil {
+		return e.defaultErrMsg()
+	}
+
+	return e.err.Error()
+}
+
+// Unwrap implements [errors.Unwrap] for error cause inspection.
+func (e ErrMultipleMatches) Unwrap() error {
+	if e.err == nil {
+		return errors.New(e.defaultErrMsg())
+	}
+
+	return e.err
+}
+
+// NewMultipleMatchErr returns a new [ErrMultipleMatches] with the given error as a cause.
+func NewMultipleMatchErr(err error) ErrMultipleMatches {
+	return ErrMultipleMatches{err: err}
+}
+
+// IsMultipleMatchErr returns true if the given error is an [ErrMultipleMatches] and false otherwise.
+func IsMultipleMatchErr(err error) bool {
+	_, ok := errors.AsType[ErrMultipleMatches](err)
+	return ok
+}
