7af1789ce9332fe3a4c58ea6d33c71ac213f13ee
Salih Muhammed <root@lr0.org>
2026-04-09 02:39:20 +0200

    lxd: remove APIEndpointAlias support
    The last endpoints that used the Aliases field (/1.0/containers and
    /1.0/virtual-machines) were removed previously, leaving no callers.
    Drop the struct and the registration loop.
    
    Signed-off-by: Salih Muhammed <root@lr0.org>

--- lxd/daemon.go
+++ lxd/daemon.go
@@ -220,22 +220,15 @@ func defaultDaemon() *Daemon {
 
 // APIEndpoint represents a URL in our API.
 type APIEndpoint struct {
-	Name        string             // Name for this endpoint.
-	Path        string             // Path pattern for this endpoint.
-	MetricsType entity.Type        // Main entity type related to this endpoint. Used by the API metrics.
-	Aliases     []APIEndpointAlias // Any aliases for this endpoint.
+	Name        string      // Name for this endpoint.
+	Path        string      // Path pattern for this endpoint.
+	MetricsType entity.Type // Main entity type related to this endpoint. Used by the API metrics.
 	Get         APIEndpointAction
 	Head        APIEndpointAction
 	Put         APIEndpointAction
 	Post        APIEndpointAction
 	Delete      APIEndpointAction
 	Patch       APIEndpointAction
-}
-
-// APIEndpointAlias represents an alias URL of and APIEndpoint in our API.
-type APIEndpointAlias struct {
-	Name string // Name for this alias.
-	Path string // Path pattern for this alias.
 }
 
 // APIEndpointAction represents an action on an API endpoint.
