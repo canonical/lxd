dfd35c4d3d64d81280f336a612be28910e489958
Piotr Resztak <piotr.resztak@futurfusion.io>
2026-04-12 17:56:38 +0200

    lxd/storage: Add InstanceByVolumeName
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>
    (cherry picked from commit 1de4ab1bce4b539275ebe4ec993acd3be52df4ed)
    Signed-off-by: Din Music <din.music@canonical.com>
    License: Apache-2.0

--- lxd/storage/errors.go
+++ lxd/storage/errors.go
@@ -9,3 +9,6 @@ var ErrNilValue = errors.New("Nil value provided")
 
 // ErrBackupSnapshotsMismatch is the "Backup snapshots mismatch" error.
 var ErrBackupSnapshotsMismatch = errors.New("Backup snapshots mismatch")
+
+// ErrVolumeNotAttached is the "Volume is not attached to any instance" error.
+var ErrVolumeNotAttached = errors.New("Volume is not attached to any instance")
