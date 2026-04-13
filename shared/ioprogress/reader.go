ac0834991932952fe351d14c8f3b6fbaf3532c9e
Mark Laing <mark.laing@canonical.com>
2026-04-13 15:03:44 +0000

    shared/ioprogress: Add methods to create readers/writers and wrappers.
    Wrappers are used throughout the storage subsystem. These methods
    accept tracker options since that is the only thing that is
    configurable.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- shared/ioprogress/reader.go
+++ shared/ioprogress/reader.go
@@ -5,7 +5,45 @@ import (
 	"io"
 )
 
-// ProgressReader is a wrapper around ReadCloser which allows for progress tracking.
+// ReaderWrapper is a function that returns a [ProgressReader] from a given [io.ReadCloser] using a previously
+// configured [ProgressReader].
+type ReaderWrapper func(closer io.ReadCloser) io.ReadCloser
+
+// NewProgressReader returns a [ProgressReader] with the given list of [TrackerOption].
+func NewProgressReader(readCloser io.ReadCloser, trackerOpts ...TrackerOption) io.ReadCloser {
+	tracker := NewProgressTracker(trackerOpts...)
+
+	// If the caller did not set a handler via any options then there is nothing to report to, so avoid the wrapper
+	// and have the caller read directly from the input reader.
+	if tracker.Handler == nil {
+		return readCloser
+	}
+
+	return &ProgressReader{
+		ReadCloser: readCloser,
+		Tracker:    tracker,
+	}
+}
+
+// NewProgressReaderWrapper returns a [ReaderWrapper] with the given list of [TrackerOption].
+func NewProgressReaderWrapper(trackerOpts ...TrackerOption) ReaderWrapper {
+	tracker := NewProgressTracker(trackerOpts...)
+
+	return func(readCloser io.ReadCloser) io.ReadCloser {
+		// If the caller did not set a handler via any options then there is nothing to report to, so avoid the wrapper
+		// and have the caller read directly from the input reader.
+		if tracker.Handler == nil {
+			return readCloser
+		}
+
+		return &ProgressReader{
+			ReadCloser: readCloser,
+			Tracker:    tracker,
+		}
+	}
+}
+
+// ProgressReader is a wrapper around [io.ReadCloser] which allows for progress tracking.
 type ProgressReader struct {
 	io.ReadCloser
 	Tracker *ProgressTracker
--- shared/ioprogress/writer.go
+++ shared/ioprogress/writer.go
@@ -5,7 +5,44 @@ import (
 	"io"
 )
 
-// ProgressWriter is a wrapper around WriteCloser which allows for progress tracking.
+// WriterWrapper is a function that returns a [ProgressWriter] from a given [io.WriteCloser] using a previously
+// configured [ProgressWriter].
+type WriterWrapper func(closer io.WriteCloser) io.WriteCloser
+
+// NewProgressWriter returns a [ProgressWriter] with the given list of [TrackerOption].
+func NewProgressWriter(writeCloser io.WriteCloser, trackerOpts ...TrackerOption) io.WriteCloser {
+	tracker := NewProgressTracker(trackerOpts...)
+
+	// If the caller did not set a handler via any options then there is nothing to report to, so avoid the wrapper
+	// and have the caller write directly to the input writer.
+	if tracker.Handler == nil {
+		return writeCloser
+	}
+
+	return &ProgressWriter{
+		WriteCloser: writeCloser,
+		Tracker:     tracker,
+	}
+}
+
+// NewProgressWriterWrapper returns a [WriterWrapper] with the given list of [TrackerOption].
+func NewProgressWriterWrapper(trackerOpts ...TrackerOption) func(io.WriteCloser) io.WriteCloser {
+	tracker := NewProgressTracker(trackerOpts...)
+	return func(writeCloser io.WriteCloser) io.WriteCloser {
+		// If the caller did not set a handler via any options then there is nothing to report to, so avoid the wrapper
+		// and have the caller write directly to the input writer.
+		if tracker.Handler == nil {
+			return writeCloser
+		}
+
+		return &ProgressWriter{
+			WriteCloser: writeCloser,
+			Tracker:     tracker,
+		}
+	}
+}
+
+// ProgressWriter is a wrapper around an [io.WriteCloser] which allows for progress tracking.
 type ProgressWriter struct {
 	io.WriteCloser
 	Tracker *ProgressTracker
