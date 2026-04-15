ae1080eea0066b31c1b7f63e1b973abe1b25814e
Mark Laing <mark.laing@canonical.com>
2026-04-15 12:43:27 +0000

    lxd/storage/drivers: Use ioprogress.ProgressReporter for driver interface.
    Additionally, remove operations from interface where they are unused.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/storage/drivers/interface.go
+++ lxd/storage/drivers/interface.go
@@ -7,9 +7,9 @@ import (
 	"github.com/canonical/lxd/lxd/backup"
 	"github.com/canonical/lxd/lxd/instancewriter"
 	"github.com/canonical/lxd/lxd/migration"
-	"github.com/canonical/lxd/lxd/operations"
 	"github.com/canonical/lxd/lxd/state"
 	"github.com/canonical/lxd/shared/api"
+	"github.com/canonical/lxd/shared/ioprogress"
 	"github.com/canonical/lxd/shared/logger"
 	"github.com/canonical/lxd/shared/revert"
 )
@@ -42,7 +42,7 @@ type Driver interface {
 	// Pool.
 	FillConfig() error
 	Create() error
-	Delete(op *operations.Operation) error
+	Delete(progressReporter ioprogress.ProgressReporter) error
 	// Mount mounts a storage pool if needed, returns true if we caused a new mount, false if already mounted.
 	Mount() (bool, error)
 
@@ -57,34 +57,34 @@ type Driver interface {
 	// Buckets.
 	ValidateBucket(bucket Volume) error
 	GetBucketURL(bucketName string) *url.URL
-	CreateBucket(bucket Volume, op *operations.Operation) error
-	DeleteBucket(bucket Volume, op *operations.Operation) error
+	CreateBucket(bucket Volume) error
+	DeleteBucket(bucket Volume) error
 	UpdateBucket(bucket Volume, changedConfig map[string]string) error
 	ValidateBucketKey(keyName string, creds S3Credentials, roleName string) error
-	CreateBucketKey(bucket Volume, keyName string, creds S3Credentials, roleName string, op *operations.Operation) (*S3Credentials, error)
-	UpdateBucketKey(bucket Volume, keyName string, creds S3Credentials, roleName string, op *operations.Operation) (*S3Credentials, error)
-	DeleteBucketKey(bucket Volume, keyName string, op *operations.Operation) error
+	CreateBucketKey(bucket Volume, keyName string, creds S3Credentials, roleName string) (*S3Credentials, error)
+	UpdateBucketKey(bucket Volume, keyName string, creds S3Credentials, roleName string) (*S3Credentials, error)
+	DeleteBucketKey(bucket Volume, keyName string) error
 
 	// Volumes.
 	FillVolumeConfig(vol Volume) error
 	ValidateVolume(vol Volume, removeUnknownKeys bool) error
-	CreateVolume(vol Volume, filler *VolumeFiller, op *operations.Operation) error
-	CreateVolumeFromCopy(vol VolumeCopy, srcVol VolumeCopy, allowInconsistent bool, op *operations.Operation) error
-	CreateVolumeFromImage(vol Volume, imgVol *Volume, filler *VolumeFiller, op *operations.Operation) error
-	RefreshVolume(vol VolumeCopy, srcVol VolumeCopy, refreshSnapshots []string, allowInconsistent bool, op *operations.Operation) error
-	DeleteVolume(vol Volume, op *operations.Operation) error
-	RenameVolume(vol Volume, newName string, op *operations.Operation) error
+	CreateVolume(vol Volume, filler *VolumeFiller, progressReporter ioprogress.ProgressReporter) error
+	CreateVolumeFromCopy(vol VolumeCopy, srcVol VolumeCopy, allowInconsistent bool, progressReporter ioprogress.ProgressReporter) error
+	CreateVolumeFromImage(vol Volume, imgVol *Volume, filler *VolumeFiller, progressReporter ioprogress.ProgressReporter) error
+	RefreshVolume(vol VolumeCopy, srcVol VolumeCopy, refreshSnapshots []string, allowInconsistent bool, progressReporter ioprogress.ProgressReporter) error
+	DeleteVolume(vol Volume, progressReporter ioprogress.ProgressReporter) error
+	RenameVolume(vol Volume, newName string, progressReporter ioprogress.ProgressReporter) error
 	UpdateVolume(vol Volume, changedConfig map[string]string) error
 	GetVolumeUsage(vol Volume) (int64, error)
-	SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, op *operations.Operation) error
+	SetVolumeQuota(vol Volume, size string, allowUnsafeResize bool, progressReporter ioprogress.ProgressReporter) error
 	GetVolumeDiskPath(vol Volume) (string, error)
 	ListVolumes() ([]Volume, error)
 
 	// MountVolume mounts a storage volume (if not mounted) and increments reference counter.
-	MountVolume(vol Volume, op *operations.Operation) error
+	MountVolume(vol Volume, progressReporter ioprogress.ProgressReporter) error
 
 	// MountVolumeSnapshot mounts a storage volume snapshot as readonly.
-	MountVolumeSnapshot(snapVol Volume, op *operations.Operation) error
+	MountVolumeSnapshot(snapVol Volume, progressReporter ioprogress.ProgressReporter) error
 
 	// CanDelegateVolume checks whether the volume can be delegated.
 	CanDelegateVolume(vol Volume) bool
@@ -94,25 +94,25 @@ type Driver interface {
 
 	// UnmountVolume unmounts a storage volume, returns true if unmounted, false if was not
 	// mounted.
-	UnmountVolume(vol Volume, keepBlockDev bool, op *operations.Operation) (bool, error)
+	UnmountVolume(vol Volume, keepBlockDev bool, progressReporter ioprogress.ProgressReporter) (bool, error)
 
 	// UnmountVolume unmounts a storage volume snapshot, returns true if unmounted, false if was
 	// not mounted.
-	UnmountVolumeSnapshot(snapVol Volume, op *operations.Operation) (bool, error)
+	UnmountVolumeSnapshot(snapVol Volume, progressReporter ioprogress.ProgressReporter) (bool, error)
 
-	CreateVolumeSnapshot(snapVol Volume, op *operations.Operation) error
-	DeleteVolumeSnapshot(snapVol Volume, op *operations.Operation) error
-	RenameVolumeSnapshot(snapVol Volume, newSnapshotName string, op *operations.Operation) error
-	VolumeSnapshots(vol Volume, op *operations.Operation) ([]string, error)
-	CheckVolumeSnapshots(vol Volume, snapVols []Volume, op *operations.Operation) error
-	RestoreVolume(vol Volume, snapVol Volume, op *operations.Operation) error
+	CreateVolumeSnapshot(snapVol Volume, progressReporter ioprogress.ProgressReporter) error
+	DeleteVolumeSnapshot(snapVol Volume, progressReporter ioprogress.ProgressReporter) error
+	RenameVolumeSnapshot(snapVol Volume, newSnapshotName string, progressReporter ioprogress.ProgressReporter) error
+	VolumeSnapshots(vol Volume) ([]string, error)
+	CheckVolumeSnapshots(vol Volume, snapVols []Volume) error
+	RestoreVolume(vol Volume, snapVol Volume, progressReporter ioprogress.ProgressReporter) error
 
 	// Migration.
 	MigrationTypes(contentType ContentType, refresh bool, copySnapshots bool) []migration.Type
-	MigrateVolume(vol VolumeCopy, conn io.ReadWriteCloser, volSrcArgs *migration.VolumeSourceArgs, op *operations.Operation) error
-	CreateVolumeFromMigration(vol VolumeCopy, conn io.ReadWriteCloser, volTargetArgs migration.VolumeTargetArgs, preFiller *VolumeFiller, op *operations.Operation) error
+	MigrateVolume(vol VolumeCopy, conn io.ReadWriteCloser, volSrcArgs *migration.VolumeSourceArgs, progressReporter ioprogress.ProgressReporter) error
+	CreateVolumeFromMigration(vol VolumeCopy, conn io.ReadWriteCloser, volTargetArgs migration.VolumeTargetArgs, preFiller *VolumeFiller, progressReporter ioprogress.ProgressReporter) error
 
 	// Backup.
-	BackupVolume(vol VolumeCopy, projectName string, tarWriter *instancewriter.InstanceTarWriter, optimized bool, snapshots []string, op *operations.Operation) error
-	CreateVolumeFromBackup(vol VolumeCopy, srcBackup backup.Info, srcData io.ReadSeeker, op *operations.Operation) (VolumePostHook, revert.Hook, error)
+	BackupVolume(vol VolumeCopy, projectName string, tarWriter *instancewriter.InstanceTarWriter, optimized bool, snapshots []string, progressReporter ioprogress.ProgressReporter) error
+	CreateVolumeFromBackup(vol VolumeCopy, srcBackup backup.Info, srcData io.ReadSeeker, progressReporter ioprogress.ProgressReporter) (VolumePostHook, revert.Hook, error)
 }
