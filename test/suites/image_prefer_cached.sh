d2115243bbbe90152f6ea8cf378c6d6479d3273a
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-04-21 18:32:05 -0400

    test/suites/image_prefer_cached: Use image registry
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- test/suites/image_prefer_cached.sh
+++ test/suites/image_prefer_cached.sh
@@ -13,8 +13,9 @@ test_image_prefer_cached() {
   LXD_DIR=${LXD2_DIR} deps/import-busybox --alias testimage --public
   fp1="$(LXD_DIR=${LXD2_DIR} lxc image info testimage | awk '/^Fingerprint/ {print $2}')"
 
-  token="$(LXD_DIR=${LXD2_DIR} lxc config trust add --name foo -q)"
-  lxc remote add l2 "${LXD2_ADDR}" --token "${token}"
+  # Create an image registry backed by a public cluster link to the second LXD.
+  create_public_cluster_link l2-link "${LXD2_ADDR}"
+  lxc image registry create l2 cluster=l2-link source_project=default
   lxc init l2:testimage c1
 
   # Now the first image image is in the local store, since it was
@@ -39,8 +40,9 @@ test_image_prefer_cached() {
   fi
 
   lxc delete c1 c2
-  lxc remote remove l2
   lxc image delete "${fp1}"
+  lxc image registry delete l2
+  lxc cluster link delete l2-link
 
   kill_lxd "$LXD2_DIR"
 }
