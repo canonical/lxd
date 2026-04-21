a2a70d3f4e72ded2a72c42c1b0804df0af29864f
Simon Deziel <simon.deziel@canonical.com>
2026-04-21 09:45:03 -0400

    lxd/storage/drivers: Register `storage_zfs_remove_local_bucket_datasets` patch in all drivers
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/storage/drivers/driver_dir.go
+++ lxd/storage/drivers/driver_dir.go
@@ -29,6 +29,7 @@ func (d *dir) load() error {
 		"storage_delete_old_snapshot_records":                nil,
 		"storage_zfs_drop_block_volume_filesystem_extension": nil,
 		"storage_prefix_bucket_names_with_project":           nil,
+		"storage_zfs_remove_local_bucket_datasets":           nil,
 	}
 
 	return nil
