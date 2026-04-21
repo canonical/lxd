d0b97b0a56851d8d4022b50fd33f27e33c556481
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-04-21 11:38:12 -0400

    lxd/instances_post: Check if project has access to image registry in instancesPost handler
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- lxd/instances_post.go
+++ lxd/instances_post.go
@@ -1653,9 +1653,24 @@ func instancesPost(d *Daemon, r *http.Request) response.Response {
 	// A connected image registry is needed to determined suitable architectures for instance creation.
 	if req.Source.ImageRegistry != "" {
 		err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+			projectConfig, err := dbCluster.GetProjectConfig(ctx, tx.Tx(), targetProjectName)
+			if err != nil {
+				return fmt.Errorf("Failed loading config for project %q: %w", targetProjectName, err)
+			}
+
 			dbImageRegistry, err := dbCluster.GetImageRegistry(ctx, tx.Tx(), req.Source.ImageRegistry)
 			if err != nil {
+				if response.IsNotFoundError(err) {
+					return api.StatusErrorf(http.StatusNotFound, "Image registry not found")
+				}
+
 				return fmt.Errorf("Failed fetching image registry %q: %w", req.Source.ImageRegistry, err)
+			}
+
+			// TODO: centralize this restricted.registries enforcement inside ImageDownload so that
+			// every image download path is covered by a single check.
+			if !project.RegistryAllowed(projectConfig, req.Source.ImageRegistry, dbImageRegistry.Builtin) {
+				return api.StatusErrorf(http.StatusForbidden, "Image registry %q is not allowed in this project", req.Source.ImageRegistry)
 			}
 
 			config, err := dbCluster.GetImageRegistryConfig(ctx, tx.Tx(), &dbImageRegistry.ID)
