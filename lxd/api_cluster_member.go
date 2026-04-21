d612029b54f30d192457d50c992ffb95757aa60f
Thomas Parrott <thomas.parrott@canonical.com>
2026-04-21 14:00:42 +0100

    lxd/api/cluster/member: Fix cluster healing functionality
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/api_cluster_member.go
+++ lxd/api_cluster_member.go
@@ -1628,6 +1628,8 @@ func evacuateInstances(ctx context.Context, opts evacuateOpts) error {
 		// Apply overrides.
 		if opts.mode != "" {
 			switch opts.mode {
+			case api.ClusterEvacuateModeHeal:
+				live = false // The source member is offline when healing.
 			case api.ClusterEvacuateModeStop:
 				migrate = false
 				live = false
