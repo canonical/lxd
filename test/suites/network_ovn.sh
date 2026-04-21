b19ce23c7e915726bb1dc0aafa693de0ca055347
Simon Deziel <simon.deziel@canonical.com>
2026-04-21 12:35:15 -0400

    test/suites/network_ovn: reuse VM to reattach to a different network
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 892bf460fa14b28c61297afd0c3ef8ca08d2c7a1)

--- test/suites/network_ovn.sh
+++ test/suites/network_ovn.sh
@@ -164,14 +164,15 @@ test_network_ovn() {
     [ "$(< "/sys/class/net/${v1Eth0Hostname}/mtu")" = "1500" ]
     waitInstanceReady v1
     [ "$(lxc exec v1 -- cat /sys/class/net/enp5s0/mtu)" = "1442" ]
-    lxc delete --force v1
+    lxc stop --force v1
 
-    lxc launch ubuntu-vm v2 --vm --config limits.memory=384MiB --device "${SMALL_VM_ROOT_DISK}" -n "${ovn_network}_jumbo"
-    v2Eth0Hostname="$(lxc config get v2 volatile.eth0.host_name)"
-    [ "$(< "/sys/class/net/${v2Eth0Hostname}/mtu")" = "8942" ]
-    waitInstanceReady v2
-    [ "$(lxc exec v2 -- cat /sys/class/net/enp5s0/mtu)" = "8942" ]
-    lxc delete --force v2
+    lxc config device set v1 eth0 network "${ovn_network}_jumbo"
+    lxc start v1
+    v1Eth0Hostname="$(lxc config get v1 volatile.eth0.host_name)"
+    [ "$(< "/sys/class/net/${v1Eth0Hostname}/mtu")" = "8942" ]
+    waitInstanceReady v1
+    [ "$(lxc exec v1 -- cat /sys/class/net/enp5s0/mtu)" = "8942" ]
+    lxc delete --force v1
 
     if [ -n "${orig_volume_size:-}" ]; then
       echo "==> Restore the volume.size"
