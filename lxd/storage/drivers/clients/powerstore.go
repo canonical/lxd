3afd4213e229f0ec034f9dde4818369dbd8140a9
Din Music <din.music@canonical.com>
2026-04-21 10:02:36 +0000

    lxd/storage/drivers/clients/powerstore: Helpers for login and issuing authenticated requests
    Co-authored-by: Din Music <din.music@canonical.com>
    Co-authored-by: Marek Dalewski <marek.dalewski@canonical.com>
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/drivers/clients/powerstore.go
+++ lxd/storage/drivers/clients/powerstore.go
@@ -2,6 +2,7 @@ package clients
 
 import (
 	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -12,8 +13,17 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/canonical/lxd/shared/api"
+)
+
+const (
+	// powerStoreAuthCookieName is the name of the cookie which contains PowerStore auth token.
+	powerStoreAuthCookieName = "auth_cookie"
+
+	// powerStoreCSRFHeaderName is the name of the header which contains PowerStore CSRF token.
+	powerStoreCSRFHeaderName = "dell-emc-token"
 )
 
 type powerStoreErrorMessage struct {
@@ -94,6 +104,16 @@ func isPowerStoreError(err error, statusCode int, substrings ...string) bool {
 	return false
 }
 
+var powerStoreSessions = make(map[string]powerStoreSession)
+var powerStoreSessionsLock = &sync.RWMutex{}
+
+// powerStoreSession describes PowerStore login session.
+type powerStoreSession struct {
+	ID        string
+	AuthToken string
+	CSRFToken string
+}
+
 // PowerStoreClient holds the PowerStore HTTP API client.
 type PowerStoreClient struct {
 	url                string
@@ -101,6 +121,11 @@ type PowerStoreClient struct {
 	username           string
 	password           string
 	resourceNamePrefix string
+
+	// currentSession holds the current session, which is set after successful login
+	// and used for subsequent requests. Use [PowerStoreClient.session] to retrieve
+	// the current session.
+	currentSession *powerStoreSession
 }
 
 // NewPowerStoreClient creates a new instance of the PowerStore HTTP API client.
@@ -112,6 +137,126 @@ func NewPowerStoreClient(url string, username string, password string, skipTLSVe
 		password:           password,
 		resourceNamePrefix: resourceNamePrefix,
 	}
+}
+
+// sessionKey generates a hashtable key for a session.
+func (c *PowerStoreClient) sessionKey() string {
+	return c.url + c.username + c.password
+}
+
+// session retrieves the current session.
+func (c *PowerStoreClient) session() *powerStoreSession {
+	if c.currentSession != nil {
+		return c.currentSession
+	}
+
+	key := c.sessionKey()
+
+	powerStoreSessionsLock.RLock()
+	defer powerStoreSessionsLock.RUnlock()
+
+	session, ok := powerStoreSessions[key]
+	if ok {
+		s := session
+		c.currentSession = &s
+		return c.currentSession
+	}
+
+	return nil
+}
+
+// setSession sets the current session.
+func (c *PowerStoreClient) setSession(session powerStoreSession) {
+	key := c.sessionKey()
+
+	powerStoreSessionsLock.Lock()
+	defer powerStoreSessionsLock.Unlock()
+
+	powerStoreSessions[key] = session
+	c.currentSession = &session
+}
+
+// invalidateSession invalidates the current session.
+func (c *PowerStoreClient) invalidateSession() {
+	key := c.sessionKey()
+
+	powerStoreSessionsLock.Lock()
+	defer powerStoreSessionsLock.Unlock()
+
+	delete(powerStoreSessions, key)
+	c.currentSession = nil
+}
+
+// login initiates request() using PowerStore username and password.
+// If successful, the session key is retrieved and stored within client structure.
+// Once stored, the session key is reused for further requests.
+func (c *PowerStoreClient) login() (*powerStoreSession, error) {
+	session := c.session()
+	if session != nil {
+		return session, nil
+	}
+
+	url := api.NewURL().Path("/api/rest/login_session")
+	url = url.WithQuery("select", "id,user,role_ids,idle_timeout,is_password_change_required,is_built_in_user")
+
+	// Base64 encode username and password for basic authentication.
+	reqHeaders := map[string]string{
+		"Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte(c.username+":"+c.password)),
+	}
+
+	respHeaders := make(http.Header)
+	respBody := []struct {
+		ID                       string `json:"id"`
+		User                     string `json:"user"`
+		IsPasswordChangeRequired bool   `json:"is_password_change_required"`
+	}{}
+
+	err := c.request(http.MethodGet, url.URL, nil, reqHeaders, &respBody, respHeaders)
+	if err != nil {
+		return nil, fmt.Errorf("Failed logging into PowerStore: %w", err)
+	}
+
+	if len(respBody) < 1 {
+		return nil, errors.New("Failed logging into PowerStore: Login response is missing session information")
+	}
+
+	sessionInfo := respBody[0]
+	if sessionInfo.IsPasswordChangeRequired {
+		return nil, errors.New("Failed logging into PowerStore: Password change required")
+	}
+
+	resp := &http.Response{Header: http.Header{}}
+
+	// Parse CSRF token from response headers.
+	csrf := resp.Header.Get(powerStoreCSRFHeaderName)
+	if csrf == "" {
+		return nil, errors.New("Failed logging into PowerStore: Login response missing CSRF token")
+	}
+
+	// Parse auth cookie.
+	var authCookie *http.Cookie
+	for _, c := range resp.Cookies() {
+		if c.Name != powerStoreAuthCookieName {
+			continue
+		}
+
+		authCookie = c
+		break
+	}
+
+	if authCookie == nil {
+		return nil, errors.New("Starting PowerStore session: Missing PowerStore authorization cookie")
+	}
+
+	// Cache new session.
+	session = &powerStoreSession{
+		ID:        sessionInfo.ID,
+		AuthToken: authCookie.Value,
+		CSRFToken: csrf,
+	}
+
+	c.setSession(*session)
+	return session, nil
 }
 
 // request issues a HTTP request against the PowerStore gateway.
@@ -188,4 +333,44 @@ func (c *PowerStoreClient) request(method string, url url.URL, reqBody map[strin
 	}
 
 	return nil
+}
+
+// requestAuthenticated issues an authenticated HTTP request against the PowerStore API gateway.
+// In case the access token is expired, the function automatically attempts to obtain a new one.
+func (c *PowerStoreClient) requestAuthenticated(method string, url url.URL, reqBody map[string]any, respBody any, respHeaders http.Header) error {
+	// If request fails with an unauthorized error, the request will be retried after
+	// requesting a new access token.
+	retries := 1
+
+	for {
+		// Ensure we are logged into the PowerStore.
+		session, err := c.login()
+		if err != nil {
+			return err
+		}
+
+		// Set access token as request header.
+		reqHeaders := map[string]string{
+			"Cookie":                 powerStoreAuthCookieName + "=" + session.AuthToken,
+			powerStoreCSRFHeaderName: session.CSRFToken,
+		}
+
+		// Initiate request.
+		err = c.request(method, url, reqBody, reqHeaders, respBody, respHeaders)
+		if err != nil {
+			if api.StatusErrorCheck(err, http.StatusUnauthorized) && retries > 0 {
+				// The failure is likely due to an expired session.
+				// Invalidate session and try again.
+				c.invalidateSession()
+				retries--
+				continue
+			}
+
+			// Either the error is not of type unauthorized or the maximum number of
+			// retries has been exceeded.
+			return err
+		}
+
+		return nil
+	}
 }
