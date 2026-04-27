d83800da1b1141976b8994c9ca05e8d24c9d9ad1
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-04-27 17:17:55 -0400

    test: Add image_registries_basic
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- test/includes/test-groups.sh
+++ test/includes/test-groups.sh
@@ -149,6 +149,7 @@ readonly test_group_standalone=(
     "fdleak"
     "filtering"
     "get_operations"
+    "image_registries_basic"
     "operations_conflict_reference"
     "instances_selective_recursion"
     "kernel_limits"
