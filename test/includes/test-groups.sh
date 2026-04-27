edc6f74fc08188f0066e8f46269b1fa4f6cf7e34
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-04-27 17:17:55 -0400

    test: Add image_registries_basic
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- test/includes/test-groups.sh
+++ test/includes/test-groups.sh
@@ -173,6 +173,7 @@ readonly test_group_standalone=(
     "filtering"
     "bulk_operation_children"
     "get_operations"
+    "image_registries_basic"
     "operations_conflict_reference"
     "instances_selective_recursion"
     "image_backup_confined"
