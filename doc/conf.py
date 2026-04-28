f98429c4a8b4d05fd07176d2bcf483374b070150
Minae Lee <minae.lee@canonical.com>
2026-04-28 11:45:01 -0700

    doc: append version number in RTD version switcher flyout menu
    Signed-off-by: Minae Lee <minae.lee@canonical.com>
    (cherry picked from commit 85076dd14293216d232e2633f4c5ed1dd4e928f3)

--- doc/conf.py
+++ doc/conf.py
@@ -262,8 +262,11 @@ html_css_files = [
     'cookie-banner.css',
 ]
 
-# Adds custom JavaScript files, located under 'html_static_path'
-html_js_files = ['js/bundle.js']
+# Adds custom JavaScript files, located under 'html_static_path' or from external link
+html_js_files = [
+    'js/bundle.js',
+    'rtd-versions-flyout.js',
+]
 
 # Feedback button at the top; enabled by default
 # To disable the button, uncomment the line below:
@@ -309,6 +312,10 @@ if os.path.exists('./substitutions.yaml'):
 if os.path.exists('./related_topics.yaml'):
     with open('./related_topics.yaml', 'r') as fd:
         myst_substitutions.update(yaml.safe_load(fd.read()))
+
+# Version label shown in the RTD flyout next to "default", in parentheses.
+# Set the FLYOUT_DEFAULT_VERSION_LABEL environment variable in the RTD project dashboard.
+html_context['flyout_default_version_label'] = os.environ.get('FLYOUT_DEFAULT_VERSION_LABEL', '')
 
 # Add configuration for intersphinx mapping
 intersphinx_mapping = {
