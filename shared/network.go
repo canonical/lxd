a54a9e6adf8325acd9081e68139ac809f190f134
Simon Deziel <simon.deziel@canonical.com>
2026-05-26 15:51:45 -0400

    shared/network: remove `AllocatePort` (only used in tests)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- shared/network.go
+++ shared/network.go
@@ -150,18 +150,3 @@ func GetTLSConfigMem(tlsClientCert string, tlsClientKey string, tlsClientCA stri
 func IsLoopback(iface *net.Interface) bool {
 	return int(iface.Flags&net.FlagLoopback) > 0
 }
-
-// AllocatePort asks the kernel for a free open port that is ready to use.
-func AllocatePort() (int, error) {
-	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
-	if err != nil {
-		return -1, err
-	}
-
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return -1, err
-	}
-
-	return l.Addr().(*net.TCPAddr).Port, l.Close()
-}
