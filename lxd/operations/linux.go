2b64929c4f9ee32f16f3bd672ced6191b59fe732
Mark Laing <mark.laing@canonical.com>
2026-05-29 16:11:30 +0100

    lxd/operations: Set the requestor if the protocol is set.
    We should at least set the protocol to differentiate that the
    operation was user requested. The username is loaded if the
    identity ID is set.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/operations/linux.go
+++ lxd/operations/linux.go
@@ -14,6 +14,7 @@ import (
 	"github.com/canonical/lxd/lxd/db/cluster"
 	"github.com/canonical/lxd/lxd/db/operationtype"
 	"github.com/canonical/lxd/lxd/db/query"
+	"github.com/canonical/lxd/lxd/request"
 	"github.com/canonical/lxd/lxd/state"
 	"github.com/canonical/lxd/shared/api"
 	"github.com/canonical/lxd/shared/cancel"
@@ -249,25 +250,21 @@ func ConstructOperationFromDB(ctx context.Context, tx *sql.Tx, s *state.State, d
 
 	op.metadata = metadata
 
-	// Load the requestor identity if provided.
-	if dbOp.Row.RequestorIdentityID != nil {
-		identity, err := cluster.GetIdentityByID(ctx, tx, *dbOp.Row.RequestorIdentityID)
-		if err != nil {
-			return nil, fmt.Errorf("Failed loading identity for operation %d: %w", dbOp.Row.ID, err)
+	// Load the requestor identity if a protocol was set.
+	// Note that the origin address is not saved, so cannot be set on the reconstructed operation.
+	if dbOp.Row.RequestorProtocol != nil && *dbOp.Row.RequestorProtocol != "" {
+		op.requestor = &request.RequestorAuditor{
+			Protocol: string(*dbOp.Row.RequestorProtocol),
 		}
 
-		// Reconstruct the requestor.
-		protocol := ""
-		if dbOp.Row.RequestorProtocol != nil {
-			protocol = string(*dbOp.Row.RequestorProtocol)
-		}
+		if dbOp.Row.RequestorIdentityID != nil {
+			identity, err := cluster.GetIdentityByID(ctx, tx, *dbOp.Row.RequestorIdentityID)
+			if err != nil {
+				return nil, fmt.Errorf("Failed loading identity for operation %d: %w", dbOp.Row.ID, err)
+			}
 
-		op.requestor = &opRequestor{
-			identityID: *dbOp.Row.RequestorIdentityID,
-			r: &api.OperationRequestor{
-				Username: identity.Identifier,
-				Protocol: protocol,
-			},
+			op.requestor.IdentityID = &identity.ID
+			op.requestor.Username = identity.Identifier
 		}
 	}
 
