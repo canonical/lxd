e891605f546499d7a4b4647da3c556f57c6f4703
Simon Deziel <simon.deziel@canonical.com>
2026-06-02 15:44:26 -0400

    test/mini-oidc/storage/user: use `strings.Cut` (modernize)
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/mini-oidc/storage/user.go
+++ test/mini-oidc/storage/user.go
@@ -40,7 +40,7 @@ type userStore struct {
 
 // NewUserStore creates a new user store.
 func NewUserStore(issuer string) UserStore {
-	hostname := strings.Split(strings.Split(issuer, "://")[1], ":")[0]
+	hostname, _, _ := strings.Cut(strings.Split(issuer, "://")[1], ":")
 	return userStore{
 		users: map[string]*User{
 			"id1": {
