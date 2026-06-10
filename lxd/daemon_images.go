d1d73eb13af257c75f4acae10a465f8df9279500
Mark Laing <mark.laing@canonical.com>
2026-06-10 13:24:49 +0100

    lxd: Validate image fingerprint on download.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>
    (cherry picked from commit 11c00dc60a69f5979123055977fdf307abb95ca2)

--- lxd/daemon_images.go
+++ lxd/daemon_images.go
@@ -139,6 +139,12 @@ func ImageDownload(ctx context.Context, s *state.State, op *operations.Operation
 		}
 	}
 
+	// Ensure the fingerprint is valid.
+	err = validateImageFingerprint(fp)
+	if err != nil {
+		return nil, err
+	}
+
 	// Ensure we are the only ones operating on this image.
 	unlock, err := imageOperationLock(fp)
 	if err != nil {
