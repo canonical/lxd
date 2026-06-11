89ebaee57a571ba75550e6ca84eb033bbfa6e6fb
Din Music <din.music@canonical.com>
2026-06-11 12:57:05 +0000

    test: Image with templates symlink
    Signed-off-by: Din Music <din.music@canonical.com>

--- test/main.sh
+++ test/main.sh
@@ -324,6 +324,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_image_refresh "image refresh"
     run_test test_image_acl "image acl"
     run_test test_image_with_rootfs_symlink "image with rootfs symlink"
+    run_test test_image_with_templates_symlink "image with templates symlink"
     run_test test_cloud_init "cloud-init"
     run_test test_exec "exec"
     run_test test_exec_exit_code "exec exit code"
