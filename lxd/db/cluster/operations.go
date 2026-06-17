bd7f1e2d98af15eba34f8de9a23a947c3d312282
Mark Laing <mark.laing@canonical.com>
2026-06-17 13:36:46 +0100

    lxd/db/cluster: Add function to get operations by node and class.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/operations.go
+++ lxd/db/cluster/operations.go
@@ -416,3 +416,8 @@ func CountOperationChildren(ctx context.Context, tx *sql.Tx, parentID int64) (in
 
 	return count, nil
 }
+
+// GetOperationsByNodeIDAndClass gets all operations on the given node ID and operation class.
+func GetOperationsByNodeIDAndClass(ctx context.Context, tx *sql.Tx, nodeID int64, class operationtype.Class) ([]Operation, error) {
+	return query.Select[Operation](ctx, tx, "WHERE operations.node_id = ? AND operations.class = ?", nodeID, class)
+}
