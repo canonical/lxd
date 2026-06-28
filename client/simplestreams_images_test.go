23a04560c2ccfab065035af489cafb8c3e22ea05
immanuwell <pchpr.00@list.ru>
2026-06-28 16:46:21 +0400

    client: Skip delta image tests without xdelta3 (except on Linux)
    Signed-off-by: immanuwell <pchpr.00@list.ru>
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- client/simplestreams_images_test.go
+++ client/simplestreams_images_test.go
@@ -133,6 +133,21 @@ func computeCombinedFingerprint(meta, rootfs []byte) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+func requireXdelta3(t *testing.T) {
+	t.Helper()
+
+	_, err := exec.LookPath("xdelta3")
+	if err == nil {
+		return
+	}
+
+	if runtime.GOOS != "linux" {
+		t.Skip("Missing xdelta3")
+	}
+
+	t.Fatalf("Missing xdelta3")
+}
+
 func TestGetImageFile_CombinedFingerprintValid(t *testing.T) {
 	metaContent := []byte("fake-metadata-content")
 	rootfsContent := []byte("fake-rootfs-content")
@@ -384,10 +399,7 @@ func newTestSimpleStreamsServerWithDeltaPaths(t *testing.T, newMeta []byte, srcR
 }
 
 func TestGetImageFile_DeltaCombinedFingerprintValid(t *testing.T) {
-	_, err := exec.LookPath("xdelta3")
-	if err != nil && runtime.GOOS != "linux" {
-		t.Skip("Missing xdelta3")
-	}
+	requireXdelta3(t)
 
 	srcRootfs := []byte("source-rootfs-content-for-delta-test")
 	newRootfs := []byte("new-rootfs-content-for-delta-test")
@@ -511,10 +523,7 @@ func TestGetImageFile_DeltaSanitizesRootfsPath(t *testing.T) {
 }
 
 func TestGetImageFile_DeltaPerFileHashMismatch(t *testing.T) {
-	_, err := exec.LookPath("xdelta3")
-	if err != nil && runtime.GOOS != "linux" {
-		t.Skip("Missing xdelta3")
-	}
+	requireXdelta3(t)
 
 	srcRootfs := []byte("source-rootfs-content-for-delta-test")
 	newRootfs := []byte("new-rootfs-content-for-delta-test")
@@ -585,10 +594,7 @@ func TestGetImageFile_DeltaPerFileHashMismatch(t *testing.T) {
 // validation still catches a mismatch. This is done by advertising a wrong combined fingerprint
 // in the simplestreams index while keeping all per-file hashes correct.
 func TestGetImageFile_DeltaCombinedFingerprintMismatch(t *testing.T) {
-	_, err := exec.LookPath("xdelta3")
-	if err != nil && runtime.GOOS != "linux" {
-		t.Skip("Missing xdelta3")
-	}
+	requireXdelta3(t)
 
 	srcRootfs := []byte("source-rootfs-for-valid-hash-test")
 	newRootfs := []byte("new-rootfs-for-valid-hash-test")
