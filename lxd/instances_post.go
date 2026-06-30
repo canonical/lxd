e9506926a0c42a61f7f624a2d3c6fbf9c7bd3e1c
Kadin Sayani <kadin.sayani@canonical.com>
2026-06-30 13:07:35 -0700

    lxd: enforce snapshot creation restrictions on backup restore
    When restoring a backup that includes snapshots into a project with
    restrictions, verify that snapshot creation is permitted before
    proceeding with the per-snapshot AllowInstanceCreation checks.
    
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    (cherry picked from commit dc0ef88e1144ae408e4e7caebdc8b9a9e4c4b946)
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- lxd/instances_post.go
+++ lxd/instances_post.go
@@ -687,6 +687,14 @@ func createFromBackup(s *state.State, r *http.Request, projectName string, data 
 				return err
 			}
 
+			// Verify snapshot creation is permitted before iterating over individual snapshots.
+			if len(bInfo.Config.Snapshots) > 0 {
+				err = project.AllowSnapshotCreation(&restrictions.Project)
+				if err != nil {
+					return err
+				}
+			}
+
 			for i, snapshot := range bInfo.Config.Snapshots {
 				if snapshot == nil {
 					return fmt.Errorf("Nil instance snapshot definition found at index %d", i)
