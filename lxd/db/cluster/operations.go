5de6165a894f2d7e814bf6fdb24b0cb8259813ae
Mark Laing <mark.laing@canonical.com>
2026-07-03 12:40:09 +0100

    lxd/db/cluster: Add function to delete many operations.
    Delete in batches of 500 to avoid issues with SQLite max bind
    parameters.
    
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/db/cluster/operations.go
+++ lxd/db/cluster/operations.go
@@ -352,6 +352,38 @@ func DeleteOperation(ctx context.Context, tx *sql.Tx, operationUUID string) erro
 	return query.DeleteOne[OperationsRow](ctx, tx, "WHERE operations.uuid = ?", operationUUID)
 }
 
+// operationDeleteBatchSize is used to limit the number of bind arguments passed to SQLite when deleting operations
+// (see [DeleteOperations]).
+const operationDeleteBatchSize = 500
+
+// DeleteOperations deletes all operations with the given UUIDs.
+func DeleteOperations(ctx context.Context, tx *sql.Tx, operationUUIDs ...string) error {
+	// Operations are cleared in a background task that runs every minute.
+	// It's possible for the number of operations to be pruned to be larger than the SQLite max bind-parameter limit.
+	// Delete operations in batches to avoid this.
+	for len(operationUUIDs) > 0 {
+		batch := operationUUIDs
+		if len(batch) > operationDeleteBatchSize {
+			batch = batch[:operationDeleteBatchSize]
+		}
+
+		args := make([]any, len(batch))
+		for i, id := range batch {
+			args[i] = id
+		}
+
+		_, err := query.DeleteMany[OperationsRow](ctx, tx, "WHERE operations.uuid IN "+query.Params(len(args)), args...)
+		if err != nil {
+			return err
+		}
+
+		// Remove the batch from the list.
+		operationUUIDs = operationUUIDs[len(batch):]
+	}
+
+	return nil
+}
+
 // GetOperation gets an [Operation] by UUID.
 func GetOperation(ctx context.Context, tx *sql.Tx, operationUUID string) (*Operation, error) {
 	return query.SelectOne[Operation](ctx, tx, "WHERE operations.uuid = ?", operationUUID)
