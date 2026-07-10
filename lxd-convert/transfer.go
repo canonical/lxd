1ec8b471ebfe68725eadadd023a7ffa644a39a44
Simon Deziel <simon.deziel@canonical.com>
2026-07-10 12:35:06 -0400

    lxd-convert/transfer: provide better error if `--rsync-args` contains `--`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd-convert/transfer.go
+++ lxd-convert/transfer.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net"
 	"os"
 	"os/exec"
+	"slices"
 	"strings"
 
 	"github.com/google/uuid"
@@ -96,7 +98,15 @@ func rsyncSendSetup(ctx context.Context, path string, rsyncArgs string, instance
 	}
 
 	if rsyncArgs != "" {
-		args = append(args, strings.Split(rsyncArgs, " ")...)
+		extraArgs := strings.Fields(rsyncArgs)
+
+		// Reject an explicit end-of-options marker ("--"): it would stop rsync from
+		// parsing the "-e" remote shell option appended below, breaking the transfer.
+		if slices.Contains(extraArgs, "--") {
+			return nil, nil, nil, errors.New("Rsync arguments cannot contain an end-of-options marker (--)")
+		}
+
+		args = append(args, extraArgs...)
 	}
 
 	// The remote shell command (-e) must be passed as an option, so it has to
