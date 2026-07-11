b653e1c1f8851cb78175fabec4f4c70a8fe0a8a7
Simon Deziel <simon.deziel@canonical.com>
2026-07-11 10:37:09 -0400

    test/lint/newline-after-block: ignore error in command pipeline
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/lint/newline-after-block.sh
+++ test/lint/newline-after-block.sh
@@ -20,7 +20,7 @@ do
   # 2. xargs: Print the filename next to the line number of the matches (piped).
   # 3. If there were no matches, the file name without the line number is printed, use grep to filter it out.
   # 4. Replace the space with a colon to make a clickable link.
-  RESULT=$(sed -n -e '/^\s*}\s*$/{n;/^\s*\w/{;=}}' "$file" | xargs -L 1 echo "$file" | grep -v '\.go$' | sed 's/ /:/g')
+  RESULT=$(sed -n -e '/^\s*}\s*$/{n;/^\s*\w/{;=}}' "$file" | xargs -L 1 echo "$file" | grep -v '\.go$' | sed 's/ /:/g' || true)
   if [ -n "${RESULT}" ]; then
     echo "${RESULT}"
     exit_code=1
