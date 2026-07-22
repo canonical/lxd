689bdf1a17591e5edffea599cb30ac8b11b62944
Julian Pelizäus <julian.pelizaeus@canonical.com>
2026-07-22 11:55:02 +0200

    Release LXD 5.0.8
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- shared/version/flex.go
+++ shared/version/flex.go
@@ -1,7 +1,7 @@
 package version
 
 // Version contains the LXD version number.
-var Version = "5.0.7"
+var Version = "5.0.8"
 
 // IsLTSVersion indicates this is an LTS version of LXD.
 var IsLTSVersion = true
