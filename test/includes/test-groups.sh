3425aadf5826b9e4db034a268099489ec78df01c
Thomas Parrott <thomas.parrott@canonical.com>
2026-07-27 14:28:55 +0100

    test: Adds tests for instance metadata confinement
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>
    (cherry picked from commit 55e02553efd031fb40c3985b6db4193a9a533b83)

--- test/includes/test-groups.sh
+++ test/includes/test-groups.sh
@@ -110,6 +110,7 @@ readonly test_group_image=(
     "image_import_metadata_not_regular_file"
     "image_backup_confined"
     "image_metadata_confined"
+    "image_metadata_template_target_confined"
     "image_list_all_aliases"
     "image_list_all_projects"
     "image_list_remotes"
