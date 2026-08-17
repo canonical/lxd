180c3a2d5251390decc4f4e1fc57c4ab4593ed6c
Joseph Robert Torsella <joey.torsella@canonical.com>
2026-08-17 12:50:46 -0400

    lxd/subprocess: Verify process identity on .ImportProcess()
    This addresses pid-reuse bugs that can occur for subprocesses that are unconditionally imported like dnsmasq or when lxd restarts on top of its long-running subprocesses.
    
    Signed-off-by: Joseph Robert Torsella <joey.torsella@canonical.com>

--- lxd/subprocess/errors.go
+++ lxd/subprocess/errors.go
@@ -8,3 +8,6 @@ import (
 
 // ErrNotRunning is returned when performing an action against a stopped process.
 var ErrNotRunning = errors.New("The process is not running")
+
+// ErrBadPID is returned when an import file contains a facially incorrect/dangerous PID <= 0.
+var ErrBadPID = errors.New("Invalid PID")
