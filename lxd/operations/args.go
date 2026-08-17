7e810659b1b1866770d218895c7b4c63b0a7db02
Mark Laing <mark.laing@canonical.com>
2026-08-17 13:37:22 +0100

    lxd/operations: Add method to add multiple input values at once.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/operations/args.go
+++ lxd/operations/args.go
@@ -214,18 +214,21 @@ func (a OperationArgs) validate(isChild bool) error {
 	return nil
 }
 
-// SetInputValue sets the given value on the operation inputs. This enforces that the value can be serialized.
+// SetInputValues sets the given values on the operation inputs. This enforces that the values can be serialized.
 // Values can be retrieved via [GetOperationInputValue].
-func (a *OperationArgs) SetInputValue(key InputKey, value any) error {
+func (a *OperationArgs) SetInputValues(values map[InputKey]any) error {
 	if a.inputs == nil {
 		a.inputs = map[InputKey]json.RawMessage{}
 	}
 
-	b, err := json.Marshal(value)
-	if err != nil {
-		return fmt.Errorf("Failed setting operation input value: %w", err)
+	for k, v := range values {
+		b, err := json.Marshal(v)
+		if err != nil {
+			return fmt.Errorf("Failed setting operation input value for key %q: %w", k, err)
+		}
+
+		a.inputs[k] = b
 	}
 
-	a.inputs[key] = b
 	return nil
 }
