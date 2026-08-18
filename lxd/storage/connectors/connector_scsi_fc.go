8698ee620db6235d79986c8c9915d3f014bfbd68
Din Music <din.music@canonical.com>
2026-08-18 12:14:12 +0000

    lxd/storage/connectors/scsi_fc: Require target WWPNs for discovery
    The SCSI/FC was falling back to scanning all available rports instead only those
    that are associated with the given storage driver (PowerStore in this case).
    
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/connectors/connector_scsi_fc.go
+++ lxd/storage/connectors/connector_scsi_fc.go
@@ -171,9 +171,13 @@ func (c *connectorSCSIFC) findSession(targetQN string) (*session, error) {
 	return nil, nil
 }
 
-// Discover returns the FC target ports visible on the fabric.
-// If WWPNs are provided they act as an allowlist.
+// Discover returns the FC target ports that are visible on the fabric and whose
+// port name matches one of the allowed WWPNs.
 func (c *connectorSCSIFC) Discover(ctx context.Context, wwpns ...string) ([]any, error) {
+	if len(wwpns) == 0 {
+		return nil, errors.New("No FC target WWPNs provided")
+	}
+
 	remotePorts, err := fcRemotePorts()
 	if err != nil {
 		return nil, err
@@ -183,14 +187,13 @@ func (c *connectorSCSIFC) Discover(ctx context.Context, wwpns ...string) ([]any,
 	for _, port := range remotePorts {
 		portName := block.NormalizeWWN(port.portName)
 
-		if len(wwpns) > 0 {
-			portFound := slices.ContainsFunc(wwpns, func(wwpn string) bool {
-				return portName == block.NormalizeWWN(wwpn)
-			})
+		portFound := slices.ContainsFunc(wwpns, func(wwpn string) bool {
+			return portName == block.NormalizeWWN(wwpn)
+		})
 
-			if !portFound {
-				continue
-			}
+		if !portFound {
+			// Skip ports that are not in the list of WWPNs that should be scanned.
+			continue
 		}
 
 		if port.portState != "Online" {
