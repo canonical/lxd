01cabe40668c444a930b5a65e630ab3bd832ddf3
Thomas Parrott <thomas.parrott@canonical.com>
2026-08-20 11:32:51 +0100

    Release LXD 5.0.9
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- shared/version/flex.go
+++ shared/version/flex.go
@@ -1,7 +1,7 @@
 package version
 
 // Version contains the LXD version number.
-var Version = "5.0.8"
+var Version = "5.0.9"
 
 // IsLTSVersion indicates this is an LTS version of LXD.
 var IsLTSVersion = true
