1254104f655d3ad9d75306d640dde2aab8794a98
Thomas Parrott <thomas.parrott@canonical.com>
2026-08-25 09:48:55 +0100

    lxd/subprocess/proc: Adds enableSetgroups argument to SetUserns
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/subprocess/proc_others.go
+++ lxd/subprocess/proc_others.go
@@ -7,6 +7,7 @@ import (
 )
 
 // SetUserns allows running inside of a user namespace.
-func (p *Process) SetUserns(userns *idmap.IdmapSet) {
+// If enableSetgroups is true, the process is allowed to use the setgroups syscall inside the user namespace.
+func (p *Process) SetUserns(userns *idmap.IdmapSet, enableSetgroups bool) {
 	return
 }
