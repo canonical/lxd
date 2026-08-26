cb00a7af10b11441987a064efd8857cc3d8893bc
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-08-26 19:31:02 -0400

    lxd/lifecycle: Pass the requestor to replicator events
    The replicator-run event is going to be emitted from an operation hook
    rather than from a request handler. An operation has no request context to
    derive the requestor from, so take the requestor as an argument and let
    each caller supply it from whichever source it has.
    
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- lxd/api_replicators.go
+++ lxd/api_replicators.go
@@ -522,7 +522,7 @@ func replicatorsPost(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(err)
 	}
 
-	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorCreated.Event(r.Context(), req.Name, projectName, nil))
+	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorCreated.Event(req.Name, projectName, request.CreateRequestor(r.Context()), nil))
 
 	return response.EmptySyncResponse
 }
@@ -586,7 +586,7 @@ func replicatorPost(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(err)
 	}
 
-	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorRenamed.Event(r.Context(), req.Name, projectName, logger.Ctx{"old_name": name}))
+	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorRenamed.Event(req.Name, projectName, request.CreateRequestor(r.Context()), logger.Ctx{"old_name": name}))
 
 	return response.EmptySyncResponse
 }
@@ -703,7 +703,7 @@ func replicatorStatePut(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(err)
 	}
 
-	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorRun.Event(r.Context(), name, projectName, nil))
+	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorRun.Event(name, projectName, request.CreateRequestor(r.Context()), nil))
 
 	return response.OperationResponse(op)
 }
@@ -788,7 +788,7 @@ func updateReplicator(d *Daemon, r *http.Request, isPatch bool) response.Respons
 		return response.SmartError(err)
 	}
 
-	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorUpdated.Event(r.Context(), name, projectName, nil))
+	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorUpdated.Event(name, projectName, request.CreateRequestor(r.Context()), nil))
 
 	return response.EmptySyncResponse
 }
@@ -909,7 +909,7 @@ func replicatorDelete(d *Daemon, r *http.Request) response.Response {
 		return response.SmartError(fmt.Errorf("Failed deleting replicator %q: %w", name, err))
 	}
 
-	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorDeleted.Event(r.Context(), name, projectName, nil))
+	s.Events.SendLifecycle(projectName, lifecycle.ReplicatorDeleted.Event(name, projectName, request.CreateRequestor(r.Context()), nil))
 
 	return response.EmptySyncResponse
 }
