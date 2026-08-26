aec96c4990690dea034faf29e239c3b83a1cf4ae
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-08-26 17:52:00 -0400

    api: Add metrics_replicators extension
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -502,6 +502,7 @@ var APIExtensions = []string{
 	"access_management_expiry",
 	"cluster_links_public",
 	"durable_operations",
+	"metrics_replicators",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
