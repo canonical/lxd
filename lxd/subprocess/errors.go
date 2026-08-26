d57da1a777a25d53e0f7ffacd68757e9e0f77989
Thomas Parrott <thomas.parrott@canonical.com>
2026-08-26 11:27:51 +0100

    lxd/subprocess: Adds ErrAlreadyRunning error
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/subprocess/errors.go
+++ lxd/subprocess/errors.go
@@ -9,5 +9,8 @@ import (
 // ErrNotRunning is returned when performing an action against a stopped process.
 var ErrNotRunning = errors.New("The process is not running")
 
+// ErrAlreadyRunning is returned when trying to start a process that is already running.
+var ErrAlreadyRunning = errors.New("The process is already running")
+
 // ErrBadPID is returned when an import file contains a facially incorrect/dangerous PID <= 0.
 var ErrBadPID = errors.New("Invalid PID")
