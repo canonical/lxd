b76deae6ce8cc19c7efab75df291f9602b8e4491
Din Music <din.music@canonical.com>
2026-08-28 06:46:13 +0000

    lxd/storage_volumes: Add the bitmap endpoints
    License: Apache-2.0
    Inspired-by: https://github.com/lxc/incus/commit/a642e83ca0ef98634d9c35d10b7c39bd6f8a5924
    Inspired-by: https://github.com/lxc/incus/commit/5f26b611e468d269f3c4e54865e520c0e5d88571
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -167,7 +167,10 @@ var api10 = []APIEndpoint{
 
 // changedBlockTrackingCmds are the API endpoints gated behind the changed_block_tracking
 // feature preview. restServer registers them only when the preview is enabled.
-var changedBlockTrackingCmds = []APIEndpoint{}
+var changedBlockTrackingCmds = []APIEndpoint{
+	storagePoolVolumeTypeBitmapsCmd,
+	storagePoolVolumeTypeBitmapCmd,
+}
 
 // swagger:operation GET /1.0?public server server_get_untrusted
 //
