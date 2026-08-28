d2f5ac05a053d00ad4366fb13cee264039e268ef
Din Music <din.music@canonical.com>
2026-08-28 07:13:28 +0000

    lxd/instances: Add the instance NBD endpoint
    The handler sets the session's conflict reference on the operation and returns a conflict as a SmartError so it keeps its status.
    
    License: Apache-2.0
    Inspired-by: https://github.com/lxc/incus/commit/fd3c3215e96614377efc5edb5d6747dc9bfd7ea3
    Inspired-by: https://github.com/lxc/incus/commit/23f1a504a8e67afe9c7365bf37eca0fe056682e6
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/api_1.0.go
+++ lxd/api_1.0.go
@@ -172,6 +172,7 @@ var api10 = []APIEndpoint{
 // feature preview. restServer registers them only when the preview is enabled.
 var changedBlockTrackingCmds = []APIEndpoint{
 	instanceBitmapsCmd,
+	instanceNBDCmd,
 	storagePoolVolumeTypeBitmapsCmd,
 	storagePoolVolumeTypeBitmapCmd,
 	storagePoolVolumeTypeNBDCmd,
