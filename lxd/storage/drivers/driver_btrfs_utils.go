427af7291c1758de84f8624e6be3f7802b714a4e
Julian Pelizäus <julian.pelizaeus@canonical.com>
2026-09-08 18:31:59 +0200

    lxd/storage/drivers/btrfs: Confine btrfs receive using -C (chroot)
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- lxd/storage/drivers/driver_btrfs_utils.go
+++ lxd/storage/drivers/driver_btrfs_utils.go
@@ -472,7 +472,9 @@ func (d *btrfs) receiveSubVolume(r io.Reader, receivePath string) (string, error
 		return "", fmt.Errorf("Target path is not empty %q", receivePath)
 	}
 
-	args := []string{"receive", "-e", receivePath}
+	// -C confines the receive process to receivePath using chroot, as an extra layer of
+	// confinement on top of the AppArmor profile applied below.
+	args := []string{"receive", "-e", "-C", receivePath}
 
 	cmd := exec.Command("btrfs", args...)
 	cmd.Stdin = r
