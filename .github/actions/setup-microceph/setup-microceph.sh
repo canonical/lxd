f11b64a977536233e5a58c7bc56d08d8449355a2
Simon Deziel <simon.deziel@canonical.com>
2026-09-10 17:13:49 -0400

    github: avoid microceph's `latest/edge` in `actions/setup-microceph`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit 2afe262511579473069a8ef73e8706b52e87ed18)

--- .github/actions/setup-microceph/setup-microceph.sh
+++ .github/actions/setup-microceph/setup-microceph.sh
@@ -176,7 +176,9 @@ setup_microceph() {
     return 1
   fi
   local osd_count="${2:-1}"
-  local channel="${3:-latest/edge}"
+  # XXX: avoid Ceph 20.2.4 due to new authentication for CVE-2025-30156 not compatible with the ceph-common version shipped in the LXD snap
+  # Revert the default to latest/edge once https://bugs.launchpad.net/ubuntu/+source/ceph/+bug/2166817 is fixed and propagated in the LXD snap
+  local channel="${3:-tentacle/stable}"
 
   install_microceph "${channel}"
   configure_microceph "${disk}" "${osd_count}"
