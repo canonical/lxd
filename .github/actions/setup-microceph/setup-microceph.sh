92c7aeb90fd680d37124c23be2242d4ec8378b16
Simon Deziel <simon.deziel@canonical.com>
2026-09-10 18:04:36 -0400

    github: install `core26` in `actions/setup-microceph`
    `tentacle/stable` uses `core24` but `latest/edge` uses `core26` so install
    both.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    (cherry picked from commit c22545e2135073f4d31af64396029515036b53c8)

--- .github/actions/setup-microceph/setup-microceph.sh
+++ .github/actions/setup-microceph/setup-microceph.sh
@@ -10,6 +10,7 @@ install_microceph() {
       . test/includes/snap.sh
       install_snap snapd latest/beta
       install_snap core24 latest/candidate
+      install_snap core26 latest/candidate
       install_snap microceph "${channel}"
   else
     snap install microceph --channel="${channel}"
