fc59432c3985bf1f2088a7edbc3beb2e19de4229
Izzaldin <izzaldinsamir@gmail.com>
2026-09-10 16:57:25 +0300

    tests: Fix devlxd wait failure assertion
    Signed-off-by: Izzaldin <izzaldinsamir@gmail.com>

--- test/suites/devlxd.sh
+++ test/suites/devlxd.sh
@@ -508,10 +508,10 @@ EOF
       lxc exec "${inst}" --project "${project}" --env DEVLXD_BEARER_TOKEN="${token}" -- devlxd-client instance get "${inst}" | jq --exit-status '.devices."block-vol".source == "block-vol"'
 
       # Try increasing block volume size while the volume is attached to a running VM (in use).
-      # Ensure the returned status code is 423 (StatusLocked).
+      # Ensure the failed operation state is returned in the response metadata.
       patchReq='{"config": {"size": "12MiB"}}'
       opID="$(lxc exec "${inst}" --project "${project}" -- curl -s --unix-socket /dev/lxd/sock -H "Authorization: Bearer ${token}" -X PATCH "lxd/1.0/storage-pools/${pool}/volumes/custom/block-vol" -d "${patchReq}" | jq --raw-output --exit-status .id)"
-      lxc exec "${inst}" --project "${project}" -- curl -s --unix-socket /dev/lxd/sock -H "Authorization: Bearer ${token}" -X GET "lxd/1.0/operations/${opID}/wait?timeout=5" -d "${patchReq}" | jq --exit-status '.metadata.error_code == 423' >/dev/null
+      lxc exec "${inst}" --project "${project}" -- curl -s --unix-socket /dev/lxd/sock -H "Authorization: Bearer ${token}" -X GET "lxd/1.0/operations/${opID}/wait?timeout=5" -d "${patchReq}" | jq --exit-status '.metadata.status == "Failure" and .metadata.err == "In use"' >/dev/null
 
       # Detach device.
       detachReq='{"devices": {"block-vol": null}}'
