9692ec62d5bbb1cd88f6af940f56e41a36410d4d
David Edler <david.edler@canonical.com>
2026-09-14 09:12:14 +0200

    shared/api: Introduce server state type
    Signed-off-by: David Edler <david.edler@canonical.com>

---
+++ shared/api/state.go
@@ -0,0 +1,8 @@
+package api
+
+// ServerState represents the current server state in an unclustered environment.
+//
+// swagger:model
+//
+// API extension: server_state.
+type ServerState = ClusterMemberState
