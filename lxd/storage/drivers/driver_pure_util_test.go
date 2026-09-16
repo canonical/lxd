ec117d62208f0c58ec8c0dd65af881cf2ef811d9
Din Music <din.music@canonical.com>
2026-09-16 11:49:28 +0000

    lxd/storage/pure: Use shared WWN normalization func
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/drivers/driver_pure_util_test.go
+++ lxd/storage/drivers/driver_pure_util_test.go
@@ -120,46 +120,6 @@ func Test_pure_serverName(t *testing.T) {
 	}
 }
 
-func Test_pureNormalizeWWN(t *testing.T) {
-	tests := []struct {
-		Name string
-		WWN  string
-		Want string
-	}{
-		{
-			Name: "Uppercase without separators, as reported by Pure Storage hosts",
-			WWN:  "10000000C9A1B2C3",
-			Want: "10000000c9a1b2c3",
-		},
-		{
-			Name: "Colon-separated byte format, as reported by Pure Storage ports",
-			WWN:  "21:00:34:80:0d:70:35:b3",
-			Want: "210034800d7035b3",
-		},
-		{
-			Name: "Linux sysfs format with 0x prefix",
-			WWN:  "0x210034800d7035b3",
-			Want: "210034800d7035b3",
-		},
-		{
-			Name: "Surrounding whitespace",
-			WWN:  "  0x210034800D7035B3  ",
-			Want: "210034800d7035b3",
-		},
-		{
-			Name: "Plain hex without prefix or separators",
-			WWN:  "210034800d7035b3",
-			Want: "210034800d7035b3",
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			assert.Equal(t, test.Want, pureNormalizeWWN(test.WWN))
-		})
-	}
-}
-
 func Test_pureHost_matchesQualifiedName(t *testing.T) {
 	host := pureHost{
 		Name: "server01-scsi-fc",
