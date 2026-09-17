506c9e9f4944cda75177f52c1a7037b902c860a3
Selim Masmoudi <selim.masmoudi@auroraiq.cloud>
2026-09-17 11:06:37 +0100

    api: Add vm_volatile_maxcpus extension
    Declares the vm_volatile_maxcpus extension for the volatile.cpu.maxcpus VM
    configuration key, so peers can detect support for it (for example, to skip
    sending the key when migrating to a server that predates it).
    
    Signed-off-by: Selim Masmoudi <selim.masmoudi@auroraiq.cloud>

--- shared/version/api.go
+++ shared/version/api.go
@@ -507,6 +507,7 @@ var APIExtensions = []string{
 	"metrics_replicators",
 	"storage_driver_pure_scsifc",
 	"operation_wait_status_code",
+	"vm_volatile_maxcpus",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
