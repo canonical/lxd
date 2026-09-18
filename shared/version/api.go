eaf1326342ff5ddd2053c161b0034f6137ff5e9e
Din Music <din.music@canonical.com>
2026-09-18 14:16:11 +0000

    api: Add API extension devlxd_operation_err_code
    Signed-off-by: Din Music <din.music@canonical.com>

--- shared/version/api.go
+++ shared/version/api.go
@@ -506,6 +506,7 @@ var APIExtensions = []string{
 	"replicator_custom_volumes",
 	"metrics_replicators",
 	"storage_driver_pure_scsifc",
+	"devlxd_operation_err_code",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
